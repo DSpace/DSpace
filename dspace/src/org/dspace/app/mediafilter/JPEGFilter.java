1ed8713a288ea532793fb69fce03b7a2b46d911d
David Stuve <david.stuve@hp.com>
2004-03-02 22:07:44 +0000

    Makes thumbnails of JPEGs and GIFs
    git-svn-id: http://scm.dspace.org/svn/repo/trunk@769 9c30dcfa-912a-0410-8fc2-9e0234be79fd

---
+++ dspace/src/org/dspace/app/mediafilter/JPEGFilter.java
@@ -0,0 +1,151 @@
+/*
+ * JPEGFilter.java
+ *
+ * $Id$
+ *
+ * Version: $Revision$
+ *
+ * Date: $Date$
+ *
+ * Copyright (c) 2002, Hewlett-Packard Company and Massachusetts
+ * Institute of Technology.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the Hewlett-Packard Company nor the name of the
+ * Massachusetts Institute of Technology nor the names of their
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+package org.dspace.app.mediafilter;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.Image;
+import java.awt.Graphics2D;
+
+import org.dspace.content.Bitstream;
+import org.dspace.content.BitstreamFormat;
+import org.dspace.content.Bundle;
+import org.dspace.content.Item;
+import org.dspace.core.Context;
+
+/*
+
+to do:
+    helpful error messages
+        - can't find mediafilter.cfg
+        - can't instantiate filter
+        - bitstream format doesn't exist
+
+ */
+
+
+public class JPEGFilter extends MediaFilter
+{
+    /**
+     * @param filename string filename
+     *
+     * @return string filtered filename
+     */
+    public String getFilteredName(String oldFilename)
+    {
+        return oldFilename + ".jpg";
+    }
+
+
+    /**
+     * @return String bundle name
+     *
+     */
+    public String getBundleName()
+    {
+        return "THUMBNAIL";
+    }
+    
+
+    /**
+     * @return String bitstreamformat
+     */
+    public String getFormatString()
+    {
+        return "JPEG";
+    }
+
+
+    /**
+     * @return String description
+     */
+    public String getDescription()
+    {
+        return "Generated Thumbnail";
+    }
+
+
+    /**
+     * @param source source input stream
+     *
+     * @return InputStream the resulting input stream
+     */
+    
+    public InputStream getDestinationStream(InputStream source)
+        throws Exception
+    {
+        // read in bitstream's image
+        BufferedImage buf = ImageIO.read(source);
+
+        // width, should be a configuration parameter
+        int xsize = 120;
+        
+        // now make a new one, but with a different size
+        
+        Image thumb = buf.getScaledInstance(xsize,-1,Image.SCALE_SMOOTH);
+
+        // create a BufferedImage to draw this image into...
+        
+        BufferedImage thumbnail = new BufferedImage(thumb.getWidth(null), thumb.getHeight(null), buf.getType());
+        
+        Graphics2D g2d = thumbnail.createGraphics();
+        
+        // draw thumb into thumbnail
+        g2d.drawImage(thumb, null, null);
+
+        // now create an input stream and return it
+        // there has got to be a better way than this!
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        
+        ImageIO.write(thumbnail, "jpeg", baos);
+
+        // now get the array
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        
+        return bais;  // will this work? or will the byte array be out of scope?
+    }
+}
