b1f9fd2d4f8f5a194f5770c224795f089f004bd4
Scott Yeadon <scott.yeadon@anu.edu.au>
2005-03-16 22:07:26 +0000

    Patch to fix problem introduced in #1145527 which stopped the Unknown bitstream format being changed on initial format regsitry load
    git-svn-id: http://scm.dspace.org/svn/repo/trunk@1129 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- dspace/src/org/dspace/content/BitstreamFormat.java
+++ dspace/src/org/dspace/content/BitstreamFormat.java
@@ -412,9 +412,22 @@ public class BitstreamFormat
        throws SQLException
     {
         // You can not reset the unknown's registry's name
-        BitstreamFormat unknown = findUnknown(bfContext);
-        if (unknown.getID() != getID())
+        BitstreamFormat unknown = null;;
+		try {
+			unknown = findUnknown(bfContext);
+		} catch (IllegalStateException e) {
+			// No short_description='Unknown' found in bitstreamformatregistry
+			// table. On first load of registries this is expected because it
+			// hasn't been inserted yet! So, catch but ignore this runtime 
+			// exception thrown by method findUnknown.
+		}
+		
+		// If the exception was thrown, unknown will == null so goahead and 
+		// load s. If not, check that the unknown's registry's name is not
+		// being reset.
+		if (unknown == null || unknown.getID() != getID()) {
             bfRow.setColumn("short_description", s);
+		}
     }
 
     /**
