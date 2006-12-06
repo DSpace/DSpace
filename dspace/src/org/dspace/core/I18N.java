d2f6e0d6604886bedd04b5c5921bfe9641229d5a
Robert Tansley <roberttansley@google.com>
2006-12-06 17:25:29 +0000

    Added header + fixed formatting
    git-svn-id: http://scm.dspace.org/svn/repo/trunk@1705 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- dspace/src/org/dspace/core/I18N.java
+++ dspace/src/org/dspace/core/I18N.java
@@ -1,4 +1,42 @@
-package org.dspace.core;
+/*
+ * I18N.java
+ *
+ * Version: $Revision$
+ *
+ * Date: $Date$
+ *
+ * Copyright (c) 2002-2005, Hewlett-Packard Company and Massachusetts
+ * Institute of Technology.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the Hewlett-Packard Company nor the name of the
+ * Massachusetts Institute of Technology nor the names of their
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */package org.dspace.core;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -10,7 +48,8 @@ import java.util.ResourceBundle;
  * 
  * @author Jim Downing
  */
-public class I18N {
+public class I18N
+{
 
 	private static final I18N DEFAULT = new I18N(Locale.getDefault());
 
@@ -55,7 +94,8 @@ public class I18N {
 	 *            The class object the messages are filed under
 	 * @return Localized message for the JVM default Locale
 	 */
-	public static String message(String msg, Class clazz) {
+	public static String message(String msg, Class clazz)
+    {
 		return DEFAULT.getMessage(msg, clazz);
 	}
 
@@ -80,7 +120,8 @@ public class I18N {
 	 *            the calling object
 	 * @return Localized message for the JVM default Locale
 	 */
-	public static String message(String msg, Object obj) {
+	public static String message(String msg, Object obj)
+    {
 		return DEFAULT.getMessage(msg, obj);
 	}
 
@@ -95,17 +136,20 @@ public class I18N {
 	 *            fully qualified class name.
 	 * @return Localized message for the JVM default Locale
 	 */
-	public static String message(String msg, String classname) {
+	public static String message(String msg, String classname)
+    {
 		return DEFAULT.getMessage(msg, classname);
 	}
 
 	private ResourceBundle messages = null;
 
-	private I18N() {
-		;
-	}
+	private I18N()
+    {
+        ;
+    }
 
-	public I18N(Locale locale) {
+	public I18N(Locale locale)
+    {
 		messages = ResourceBundle.getBundle("Messages", locale);
 	}
 
@@ -134,7 +178,8 @@ public class I18N {
 	 *            The class object the messages are filed under
 	 * @return Localized message 
 	 */
-	public String getMessage(String msg, Class clazz) {
+	public String getMessage(String msg, Class clazz)
+    {
 		String className = clazz.getName();
 		return messages.getString(new StringBuffer(50).append(className)
 				.append(".").append(msg).toString());
@@ -161,7 +206,8 @@ public class I18N {
 	 *            the calling object
 	 * @return Localized message 
 	 */
-	public String getMessage(String msg, Object obj) {
+	public String getMessage(String msg, Object obj)
+    {
 		return getMessage(msg, obj.getClass());
 	}
 
