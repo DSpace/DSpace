15db86801effd1e4fc888fc1b77b3691466e20a1
James Rutherford <jrutherford@users.sourceforge.net>
2007-04-09 10:47:06 +0000

    (Richard Jones) - SF Patch #1659796 Create administrator wtih command line options
    Also updated create-adminstrator script and simplified dsprop script.
    
    
    git-svn-id: http://scm.dspace.org/svn/repo/trunk@1776 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- dspace/src/org/dspace/administer/CreateAdministrator.java
+++ dspace/src/org/dspace/administer/CreateAdministrator.java
@@ -42,6 +42,10 @@ package org.dspace.administer;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
 import org.dspace.core.Context;
 import org.dspace.eperson.EPerson;
 import org.dspace.eperson.Group;
@@ -52,139 +56,191 @@ import org.dspace.eperson.Group;
  * password from standard input. An administrator group is then created and the
  * data passed in used to create an e-person in that group.
  * <P>
- * Takes no arguments.
+ * Alternatively, it can be used to take the email, first name, last name and
+ * desired password as arguments thus:
+ * 
+ * CreateAdministrator -e [email] -f [first name] -l [last name] -p [password]
+ * 
+ * This is particularly convenient for automated deploy scripts that require an 
+ * initial administrator, for example, before deployment can be completed
  * 
  * @author Robert Tansley
+ * @author Richard Jones
+ * 
  * @version $Revision$
  */
 public class CreateAdministrator
 {
+	/** DSpace Context object */
+	private Context context;
+	
     /**
-     * For invoking via the command line
+     * For invoking via the command line.  If called with no command line arguments,
+     * it will negotiate with the user for the administrator details
      * 
      * @param argv
      *            command-line arguments
      */
     public static void main(String[] argv)
+    	throws Exception
     {
-        Context context = null;
-        BufferedReader input = null;
-
-        try
+    	CommandLineParser parser = new PosixParser();
+    	Options options = new Options();
+    	
+    	CreateAdministrator ca = new CreateAdministrator();
+    	
+    	options.addOption("e", "email", true, "administrator email address");
+    	options.addOption("f", "first", true, "administrator first name");
+    	options.addOption("l", "last", true, "administrator lastt name");
+    	options.addOption("p", "password", true, "administrator password");
+    	
+    	CommandLine line = parser.parse(options, argv);
+    	
+    	if (line.hasOption("e") && line.hasOption("f") && line.hasOption("l") && line.hasOption("p"))
+    	{
+    		ca.createAdministrator(line.getOptionValue("e"), line.getOptionValue("f"), 
+    				line.getOptionValue("l"), line.getOptionValue("p"));
+    	}
+    	else
+    	{
+    		ca.negotiateAdministratorDetails();
+    	}
+    }
+    
+    /** 
+     * constructor, which just creates and object with a ready context
+     * 
+     * @throws Exception
+     */
+    private CreateAdministrator()
+    	throws Exception
+    {
+    	context = new Context();
+    }
+    
+    /**
+     * Method which will negotiate with the user via the command line to 
+     * obtain the administrator's details
+     * 
+     * @throws Exception
+     */
+    private void negotiateAdministratorDetails()
+    	throws Exception
+    {
+    	// For easier reading of typing
+    	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	System.out.println("Creating an initial administrator account");
+    	
+    	boolean dataOK = false;
+    	
+    	String email = null;
+    	String firstName = null;
+    	String lastName = null;
+    	String password1 = null;
+    	String password2 = null;
+    	
+    	while (!dataOK)
+    	{
+    		System.out.print("E-mail address: ");
+    		System.out.flush();
+    		
+    		email = input.readLine().trim();
+    		
+    		System.out.print("First name: ");
+    		System.out.flush();
+    		
+    		firstName = input.readLine().trim();
+    		
+    		System.out.print("Last name: ");
+    		System.out.flush();
+    		
+    		lastName = input.readLine().trim();
+    		
+    		System.out.println("WARNING: Password will appear on-screen.");
+    		System.out.print("Password: ");
+    		System.out.flush();
+    		
+    		password1 = input.readLine().trim();
+    		
+    		System.out.print("Again to confirm: ");
+    		System.out.flush();
+    		
+    		password2 = input.readLine().trim();
+    		
+    		if (!password1.equals("") && password1.equals(password2))
+    		{
+    			// password OK
+    			System.out.print("Is the above data correct? (y or n): ");
+    			System.out.flush();
+    			
+    			String s = input.readLine().trim();
+    			
+    			if (s.toLowerCase().startsWith("y"))
+    			{
+    				dataOK = true;
+    			}
+    		}
+    		else
+    		{
+    			System.out.println("Passwords don't match");
+    		}
+    	}
+    	
+    	// if we make it to here, we are ready to create an administrator
+    	createAdministrator(email, firstName, lastName, password1);
+    }
+    
+    /**
+     * Create the administrator with the given details.  If the user
+     * already exists then they are simply upped to administrator status
+     * 
+     * @param email	the email for the user
+     * @param first	user's first name
+     * @param last	user's last name
+     * @param ps	desired password
+     * 
+     * @throws Exception
+     */
+    private void createAdministrator(String email, String first, String last, String pw)
+    	throws Exception
+    {
+    	// Of course we aren't an administrator yet so we need to
+    	// circumvent authorisation
+    	context.setIgnoreAuthorization(true);
+    	
+    	// Find administrator group
+    	Group admins = Group.find(context, 1);
+    	
+    	if (admins == null)
+    	{
+    		throw new Exception("Error, no admin group (group 1) found");
+    	}
+    	
+    	// Create the administrator e-person
+        EPerson eperson = EPerson.findByEmail(context,email);
+        
+        // check if the email belongs to a registered user,
+        // if not create a new user with this email
+        if (eperson == null)
         {
-            // For easier reading of typing
-            input = new BufferedReader(new InputStreamReader(System.in));
-
-            context = new Context();
-
-            // Of course we aren't an administrator yet so we need to
-            // circumvent authorisation
-            context.setIgnoreAuthorization(true);
-
-            System.out.println("Creating an initial administrator account");
-
-            boolean dataOK = false;
-
-            String email = null;
-            String firstName = null;
-            String lastName = null;
-            String password1 = null;
-            String password2 = null;
-
-            while (!dataOK)
-            {
-                System.out.print("E-mail address: ");
-                System.out.flush();
-
-                email = input.readLine().trim();
-
-                System.out.print("First name: ");
-                System.out.flush();
-
-                firstName = input.readLine().trim();
-
-                System.out.print("Last name: ");
-                System.out.flush();
-
-                lastName = input.readLine().trim();
-
-                System.out.println("WARNING: Password will appear on-screen.");
-                System.out.print("Password: ");
-                System.out.flush();
-
-                password1 = input.readLine().trim();
-
-                System.out.print("Again to confirm: ");
-                System.out.flush();
-
-                password2 = input.readLine().trim();
-
-                if (!password1.equals("") && password1.equals(password2))
-                {
-                    // password OK
-                    System.out.print("Is the above data correct? (y or n): ");
-                    System.out.flush();
-
-                    String s = input.readLine().trim();
-
-                    if (s.toLowerCase().startsWith("y"))
-                    {
-                        dataOK = true;
-                    }
-                }
-                else
-                {
-                    System.out.println("Passwords don't match");
-                }
-            }
-
-            // Find administrator group
-            Group admins = Group.find(context, 1);
-
-            if (admins == null)
-            {
-                System.out.println("Error, no admin group (group 1) found");
-                System.exit(1);
-            }
-
-            // Create the administrator e-person
-            EPerson eperson = EPerson.findByEmail(context,email);
-            
-            // check if the email belongs to a registered user,
-            // if not create a new user with this email
-            if (eperson == null)
-            {
-                eperson = EPerson.create(context);
-                eperson.setEmail(email);
-                eperson.setCanLogIn(true);
-                eperson.setRequireCertificate(false);
-                eperson.setSelfRegistered(false);
-            }
-            eperson.setLastName(lastName);
-            eperson.setFirstName(firstName);
-            eperson.setPassword(password1);
-            eperson.update();
-
-            admins.addMember(eperson);
-            admins.update();
-
-            context.complete();
-
-            System.out.println("Administrator account created");
-
-            System.exit(0);
+            eperson = EPerson.create(context);
+            eperson.setEmail(email);
+            eperson.setCanLogIn(true);
+            eperson.setRequireCertificate(false);
+            eperson.setSelfRegistered(false);
         }
-        catch (Exception e)
-        {
-            System.err.println("Exception occurred:" + e);
-            e.printStackTrace();
-
-            if (context != null)
-            {
-                context.abort();
-            }
-
-            System.exit(1);
-        }
+    	
+    	eperson.setLastName(last);
+    	eperson.setFirstName(first);
+    	eperson.setPassword(pw);
+    	eperson.update();
+    	
+    	admins.addMember(eperson);
+    	admins.update();
+    	
+    	context.complete();
+    	
+    	System.out.println("Administrator account created");
     }
 }
