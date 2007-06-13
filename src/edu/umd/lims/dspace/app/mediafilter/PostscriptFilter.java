71fab1800c451d1398c9745d8dadc4bc368fd778
Ben Wallberg <wallberg@umd.edu>
2007-06-13 11:25:07 +0000

    - undo commit of PostscriptFilter.java in r14
    git-svn-id: https://svn.lib.umd.edu/drum/trunk/src@15 e8f66b91-402e-0410-ac90-b3834309c80d

--- src/edu/umd/lims/dspace/app/mediafilter/PostscriptFilter.java
+++ src/edu/umd/lims/dspace/app/mediafilter/PostscriptFilter.java
@@ -6,9 +6,10 @@
 package edu.umd.lims.dspace.app.mediafilter;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 
 import org.dspace.app.mediafilter.MediaFilter;
 
@@ -82,30 +83,85 @@ public class PostscriptFilter extends MediaFilter {
    */
 
   public InputStream getDestinationStream(InputStream source) throws Exception {
-    // Start the process
-    Process p = Runtime.getRuntime().exec(new String[]{"/usr/local/bin/ps2pdf","-","-"});
-       
-    // Create a thread to send the input to the process
-    StreamThread tin = new StreamThread(source, p.getOutputStream());
-    tin.start();
+    PipedInputStream pin = new PipedInputStream();
+    PipedOutputStream pout = new PipedOutputStream(pin);
 
-    // Create a thread to read the output from the process
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    StreamThread tout = new StreamThread(p.getInputStream(), baos);
-    tout.start();
+    Convert t = new Convert(source, pout);
+    t.start();
 
-    // Wait for completion
-    p.waitFor();
-    tin.join();
-    tout.join();
+    return pin;
+  }
 
-    // Get the return value
-    if (p.exitValue() != 0) {
-      throw new Exception("ps2pdf returned an error: " + p.exitValue());
+
+  /**********************************************************************
+   **********************************************************************/
+
+  static class Convert extends Thread {
+
+    InputStream in = null;
+    OutputStream out = null;
+
+
+    /************************************************************ Convert */
+    /**
+     * Constructor.
+     */
+
+    public Convert(InputStream in, OutputStream out) {
+      this.in = in;
+      this.out = out;
     }
 
-    // Return the pdf
-    return new ByteArrayInputStream(baos.toByteArray());
+
+   /**************************************************************** run */
+   /**
+    * Run the logic of the thread.
+    */
+
+   public void run() {
+     // Not closing out (a PipedOutputStream) will cause an IOException
+     // which will cause MediaFilter to abort the new bitstream creation
+     boolean bCloseOut = true;
+
+     try {
+       // Start the process
+       Process p = Runtime.getRuntime().exec(new String[]{"/usr/local/bin/ps2pdf","-","-"});
+       
+       // Create a thread to send the input to the process
+       StreamThread t = new StreamThread(in, p.getOutputStream());
+       t.start();
+
+       // Read the results and send to the output
+       InputStream pin = p.getInputStream();
+       byte b[] = new byte[2048];
+       int n;
+       while ((n = pin.read(b)) != -1) {
+	 out.write(b, 0, n);
+       }
+
+       // Wait for completion
+       p.waitFor();
+
+       // Get the return value
+       if (p.exitValue() != 0) {
+	 bCloseOut = false;
+       }
+     }       
+     catch (Exception e) {
+       e.printStackTrace();
+       bCloseOut = false;
+     }
+     finally {
+       try {
+	 if (out != null && bCloseOut) {
+	   out.close();
+	 }
+       }
+       catch (Exception e) {
+	 e.printStackTrace();
+       }
+     }
+   }
   }
 }
 
