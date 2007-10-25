f7a83f2a024602bc24dd3927fc4b11991daf417e
Andrea Bollini <bollini@cilea.it>
2007-10-25 12:41:44 +0000

    [1818000] Pluggable Style Selection strategy for item display
    git-svn-id: http://scm.dspace.org/svn/repo/branches/dspace-1_5_x@2264 9c30dcfa-912a-0410-8fc2-9e0234be79fd

---
+++ dspace-jspui/src/main/java/org/dspace/app/webui/util/AKeyBasedStyleSelection.java
@@ -0,0 +1,60 @@
+/*
+ * AKeyBasedStyleSelection.java
+ *
+ * Version: $Revision: 1 $
+ *
+ * Date: $Date: 2007-10-25 09:00:00 +0100 (thu, 25 oct 2007) $
+ *
+ * Copyright (c) 2002-2005, Hewlett-Packard Company and Massachusetts
+ * Institute of Technology.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the Hewlett-Packard Company nor the name of the
+ * Massachusetts Institute of Technology nor the names of their
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+package org.dspace.app.webui.util;
+
+import org.dspace.core.ConfigurationManager;
+/**
+ * Simple abstract class that provide utility method for get/check style configuration from dspace.cfg file 
+ * @author Andrea Bollini
+ * @version $Revision: 1 $
+ *
+ */
+public abstract class AKeyBasedStyleSelection implements StyleSelection
+{   
+    public String getConfigurationForStyle(String style)
+    {
+        return ConfigurationManager.getProperty("webui.itemdisplay." + style);
+    }
+    
+    protected boolean isConfigurationDefinedForStyle(String style)
+    {
+        return ConfigurationManager.getProperty("webui.itemdisplay." + style) == null;
+    }
+}
