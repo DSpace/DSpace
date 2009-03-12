7b7f1a8a66a1a143706894f5f8564de41135561d
Tim Donohue <tdonohue@duraspace.org>
2009-03-12 19:05:26 +0000

    Fix for Jira bug [DS-86] XMLUI Feedback form does not include any protection from spamming.   Basically just copied over the same logic that is used already in the JSPUI and tested that it works for XMLUI as well.
    git-svn-id: http://scm.dspace.org/svn/repo/branches/dspace-1_5_x@3581 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/SendFeedbackAction.java
+++ dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/SendFeedbackAction.java
@@ -39,6 +39,7 @@
  */
 package org.dspace.app.xmlui.aspect.artifactbrowser;
 
+import java.net.InetAddress;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -50,6 +51,7 @@ import org.apache.cocoon.environment.Redirector;
 import org.apache.cocoon.environment.Request;
 import org.apache.cocoon.environment.SourceResolver;
 import org.dspace.app.xmlui.utils.ContextUtil;
+import org.dspace.authorize.AuthorizeException;
 import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Context;
 import org.dspace.core.Email;
@@ -64,50 +66,74 @@ public class SendFeedbackAction extends AbstractAction
 {
 
     /**
-     * 
+     *
      */
     public Map act(Redirector redirector, SourceResolver resolver, Map objectModel,
             String source, Parameters parameters) throws Exception
     {
         Request request = ObjectModelHelper.getRequest(objectModel);
-            
+
         String page = request.getParameter("page");
         String address = request.getParameter("email");
         String agent = request.getHeader("User-Agent");
         String session = request.getSession().getId();
         String comments = request.getParameter("comments");
-        
+
+        // Obtain information from request
+        // The page where the user came from
+        String fromPage = request.getHeader("Referer");
+
+        // Prevent spammers and splogbots from poisoning the feedback page
+        String host = ConfigurationManager.getProperty("dspace.hostname");
+
+        String basicHost = "";
+        if (host.equals("localhost") || host.equals("127.0.0.1")
+        		|| host.equals(InetAddress.getLocalHost().getHostAddress()))
+            basicHost = host;
+        else
+        {
+            // cut off all but the hostname, to cover cases where more than one URL
+            // arrives at the installation; e.g. presence or absence of "www"
+            int lastDot = host.lastIndexOf(".");
+            basicHost = host.substring(host.substring(0, lastDot).lastIndexOf("."));
+        }
+
+        if (fromPage == null || fromPage.indexOf(basicHost) == -1)
+        {
+            throw new AuthorizeException();
+        }
+
         // User email from context
         Context context = ContextUtil.obtainContext(objectModel);
         EPerson loggedin = context.getCurrentUser();
         String eperson = null;
         if (loggedin != null)
             eperson = loggedin.getEmail();
-        
+
         if (page == null || page.equals(""))
         {
-            page = request.getHeader("Referer");
+            page = fromPage;
         }
-        
+
         // Check all data is there
         if ((address == null) || address.equals("")
                 || (comments == null) || comments.equals(""))
         {
-            // Either the user did not fill out the form or this is the 
-            // first time they are visiting the page. 
+            // Either the user did not fill out the form or this is the
+            // first time they are visiting the page.
             Map<String,String> map = new HashMap<String,String>();
             map.put("page",page);
-            
+
             if (address == null || address.equals(""))
                 map.put("email",eperson);
             else
                 map.put("email",address);
-            
+
             map.put("comments",comments);
-            
+
             return map;
         }
-        
+
         // All data is there, send the email
         Email email = ConfigurationManager.getEmail(I18nUtil.getEmailFilename(context.getCurrentLocale(), "feedback"));
         email.addRecipient(ConfigurationManager
