0c47cc5e8caacf13d3ea68a659bfa8b399755958
Mark Diggory <mdiggory@atmire.com>
2009-07-07 22:44:57 +0000

    [#DS-247] Contribution of @MIRE Solr Based Statistics Engine to DSpace. Restriction Filter to stop non-localhost access to Solr (can be disabled in web.xml).

---
+++ src/main/java/org/dspace/solr/filters/LocalHostRestrictionFilter.java
@@ -0,0 +1,64 @@
+package org.dspace.solr.filters;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class LocalHostRestrictionFilter implements Filter {
+
+	private static Log log = LogFactory
+			.getLog(LocalHostRestrictionFilter.class);
+
+	private boolean enabled = true;
+	
+	public LocalHostRestrictionFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		if(enabled){
+			InetAddress ia = InetAddress.getLocalHost();
+			String localAddr = ia.getHostAddress();
+			String remoteAddr = request.getRemoteAddr();
+
+			if(!(localAddr.equals(remoteAddr) || remoteAddr.equals("127.0.0.1")))
+			{
+				((HttpServletResponse)response).sendError(403);
+			}
+			
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * 
+	 */
+	public void init(FilterConfig arg0) throws ServletException {
+		String restrict = arg0.getServletContext().getInitParameter("LocalHostRestrictionFilter.localhost");
+		if("false".equalsIgnoreCase(restrict))
+		{
+			enabled = false;
+		}
+		
+	}
+
+}
