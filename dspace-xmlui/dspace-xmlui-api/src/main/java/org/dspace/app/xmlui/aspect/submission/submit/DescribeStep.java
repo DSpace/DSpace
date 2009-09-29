0a9b03fd7a1b41c338f4018847aa0e6dbd18192c
Larry Stone <lcs@users.sourceforge.net>
2009-09-29 23:21:17 +0000

    [DS-317] fix DCDate usage to work with corrected DCDate behavior in embargo patch - submission describe page used local time instead of GMT
    git-svn-id: http://scm.dspace.org/svn/repo/dspace/trunk@4303 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/DescribeStep.java
+++ dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/DescribeStep.java
@@ -505,9 +505,9 @@ public class DescribeStep extends AbstractSubmissionStep
 			{
 				DCDate dcDate = new DCDate(dcValue.value);
 
-				year.addInstance().setValue(String.valueOf(dcDate.getYear()));
-				month.addInstance().setOptionSelected(dcDate.getMonth());
-				day.addInstance().setValue(String.valueOf(dcDate.getDay()));
+                                year.addInstance().setValue(String.valueOf(dcDate.getYearGMT()));
+                                month.addInstance().setOptionSelected(dcDate.getMonthGMT());
+                                day.addInstance().setValue(String.valueOf(dcDate.getDayGMT()));
 				fullDate.addInstance().setValue(dcDate.toString());
 			}
 		}
@@ -515,15 +515,15 @@ public class DescribeStep extends AbstractSubmissionStep
 		{
 			DCDate dcDate = new DCDate(dcValues[0].value);
 
-			year.setValue(String.valueOf(dcDate.getYear()));
-			month.setOptionSelected(dcDate.getMonth());
+                        year.setValue(String.valueOf(dcDate.getYearGMT()));
+                        month.setOptionSelected(dcDate.getMonthGMT());
 			
 			// Check if the day field is not specified, if so then just 
 			// put a blank value in instead of the wiered looking -1.
-			if (dcDate.getDay() == -1)
+                        if (dcDate.getDayGMT() == -1)
 				day.setValue("");
 			else
-				day.setValue(String.valueOf(dcDate.getDay()));
+                                day.setValue(String.valueOf(dcDate.getDayGMT()));
 		}
 	}
 	
