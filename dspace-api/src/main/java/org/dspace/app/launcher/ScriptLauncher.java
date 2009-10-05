51010c269b42dd575cd75239fa9f1b7d6952f7c0
Stuart Lewis <stuart@stuartlewis.com>
2009-10-05 22:14:51 +0000

    [DS-247] Addition of DSpace Services to DSpace 1.6 - startup service manager in DSpace script launcher
    git-svn-id: http://scm.dspace.org/svn/repo/dspace/trunk@4360 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- dspace-api/src/main/java/org/dspace/app/launcher/ScriptLauncher.java
+++ dspace-api/src/main/java/org/dspace/app/launcher/ScriptLauncher.java
@@ -34,13 +34,17 @@
  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
- */package org.dspace.app.launcher;
+ */
+
+package org.dspace.app.launcher;
 
 import org.dspace.core.ConfigurationManager;
+import org.dspace.servicemanager.DSpaceKernelImpl;
+import org.dspace.servicemanager.DSpaceKernelInit;
+import org.dspace.services.RequestService;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.input.SAXBuilder;
-
 import java.util.List;
 import java.lang.reflect.Method;
 
@@ -48,9 +52,13 @@ import java.lang.reflect.Method;
  * A DSpace script launcher.
  *
  * @author Stuart Lewis
+ * @author Mark Diggory
  */
 public class ScriptLauncher
 {
+    /** The service manager kernel */
+    private static transient DSpaceKernelImpl kernelImpl;
+
     /**
      * Execute the DSpace script launcher
      *
@@ -65,6 +73,29 @@ public class ScriptLauncher
             System.err.println("You must provide at least one command argument");
             display();
             System.exit(1);
+        }
+
+        // Initalise the service manager kernel
+        try {
+            kernelImpl = DSpaceKernelInit.getKernel(null);
+            if (!kernelImpl.isRunning())
+            {
+                kernelImpl.start(ConfigurationManager.getProperty("dspace.dir"));
+            }
+        } catch (Exception e)
+        {
+            // Failed to start so destroy it and log and throw an exception
+            try
+            {
+                kernelImpl.destroy();
+            }
+            catch (Exception e1)
+            {
+                // Nothing to do
+            }
+            String message = "Failure during filter init: " + e.getMessage();
+            System.err.println(message + ":" + e);
+            throw new RuntimeException(message, e);
         }
 
         // Parse the configuration file looking for the command entered
@@ -136,6 +167,16 @@ public class ScriptLauncher
                         useargs = argsnew;
                     }
 
+                    // Establish the request service startup
+                    RequestService requestService = kernelImpl.getServiceManager().getServiceByName(RequestService.class.getName(), RequestService.class);
+                    if (requestService == null) {
+                        throw new IllegalStateException("Could not get the DSpace RequestService to start the request transaction");
+                    }
+
+                    // Establish a request related to the current session
+                    // that will trigger the various request listeners
+                    requestService.startRequest();
+                    
                     // Run the main() method
                     try
                     {
@@ -151,24 +192,59 @@ public class ScriptLauncher
                         
                         Method main = target.getMethod("main", argTypes);
                         Object output = main.invoke(null, arguments);
+
+                        // ensure we close out the request (happy request)
+                        requestService.endRequest(null);
+                       
+                        
                     }
                     catch (Exception e)
                     {
+                        // Failure occurred in the request so we destroy it
+                        requestService.endRequest(e);
+                        
+                    	if (kernelImpl != null)
+                        {
+                            kernelImpl.destroy();
+                            kernelImpl = null;
+                        }
+                    	
                         System.err.println("Exception: " + e.getMessage());
                         System.exit(1);
                     }
+                    
                 }
+
+                // Destroy the service kernel
+                if (kernelImpl != null)
+                {
+                    kernelImpl.destroy();
+                    kernelImpl = null;
+                }
+                
                 System.exit(0);
             }
         }
 
+        // Destroy the service kernel if it is still alive
+        if (kernelImpl != null)
+        {
+            kernelImpl.destroy();
+            kernelImpl = null;
+        }
+        
         // The command wasn't found
         System.err.println("Command not found: " + args[0]);
         display();
         System.exit(1);
     }
 
-    public static Document getConfig()
+    /**
+     * Load the launcher configuration file
+     *
+     * @return The XML configuration file Document
+     */
+    private static Document getConfig()
     {
         // Load the launcher configuration file
         String config = ConfigurationManager.getProperty("dspace.dir") +
@@ -189,7 +265,10 @@ public class ScriptLauncher
         return doc;
     }
 
-    public static void display()
+    /**
+     * Display the commands that the current launcher config file knows about
+     */
+    private static void display()
     {
         Document doc = getConfig();
         List<Element> commands = doc.getRootElement().getChildren("command");
