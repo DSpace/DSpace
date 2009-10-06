0cd02aeaa668cc1c39add722047cb7146cc61e4a
Mark Diggory <mdiggory@atmire.com>
2009-10-06 19:48:36 +0000

    [DS-247] License Cleanup in solrstatistics.

--- src/main/java/org/dspace/solr/filters/LocalHostRestrictionFilter.java
+++ src/main/java/org/dspace/solr/filters/LocalHostRestrictionFilter.java
@@ -1,3 +1,13 @@
+/**
+ * $Id: $
+ * $URL: $
+ * *************************************************************************
+ * Copyright (c) 2002-2009, DuraSpace.  All rights reserved
+ * Licensed under the DuraSpace Foundation License.
+ *
+ * A copy of the DuraSpace License has been included in this
+ * distribution and is available at: http://scm.dspace.org/svn/repo/licenses/LICENSE.txt
+ */
 package org.dspace.solr.filters;
 
 import java.io.IOException;
