408d8ab7500ae90acdc3e729f0ea1a30576cc407
Graham Triggs <grahamtriggs@gmail.com>
2009-10-22 22:53:26 +0000

    [DS-356] Antispam for suggest item feature
    git-svn-id: http://scm.dspace.org/svn/repo/dspace/trunk@4462 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- dspace-jspui/dspace-jspui-api/src/main/java/org/dspace/app/webui/servlet/SuggestServlet.java
+++ dspace-jspui/dspace-jspui-api/src/main/java/org/dspace/app/webui/servlet/SuggestServlet.java
@@ -41,6 +41,7 @@
 package org.dspace.app.webui.servlet;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.MissingResourceException;
@@ -81,6 +82,30 @@ public class SuggestServlet extends DSpaceServlet
     					   HttpServletResponse response)
         throws ServletException, IOException, SQLException, AuthorizeException
     {
+        // Obtain information from request
+        // The page where the user came from
+        String fromPage = request.getHeader("Referer");
+
+        // Prevent spammers and splogbots from poisoning the feedback page
+        String host = ConfigurationManager.getProperty("dspace.hostname");
+
+        String basicHost = "";
+        if (host.equals("localhost") || host.equals("127.0.0.1")
+                || host.equals(InetAddress.getLocalHost().getHostAddress()))
+            basicHost = host;
+        else
+        {
+            // cut off all but the hostname, to cover cases where more than one URL
+            // arrives at the installation; e.g. presence or absence of "www"
+            int lastDot = host.lastIndexOf(".");
+            basicHost = host.substring(host.substring(0, lastDot).lastIndexOf("."));
+        }
+
+        if (fromPage == null || fromPage.indexOf(basicHost) == -1)
+        {
+            throw new AuthorizeException();
+        }
+
         // Obtain information from request
         String handle = request.getParameter("handle");
         
