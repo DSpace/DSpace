9f8eb52288fad9b7943bf3d1b59a3a70ecbe9c21
Mark Diggory <mdiggory@atmire.com>
2009-11-04 19:02:06 +0000

    [DS-247] Contribution of @MIRE Solr Based Statistics Engine to DSpace. Move statistics to separate page. Externalize Strings.
    git-svn-id: http://scm.dspace.org/svn/repo/dspace/trunk@4501 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/statistics/StatisticsTransformer.java
+++ dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/statistics/StatisticsTransformer.java
@@ -20,38 +20,61 @@ import org.dspace.app.xmlui.cocoon.AbstractDSpaceTransformer;
 import org.dspace.app.xmlui.utils.HandleUtil;
 import org.dspace.app.xmlui.utils.UIException;
 import org.dspace.app.xmlui.wing.WingException;
-import org.dspace.app.xmlui.wing.element.Body;
-import org.dspace.app.xmlui.wing.element.Cell;
-import org.dspace.app.xmlui.wing.element.Division;
-import org.dspace.app.xmlui.wing.element.List;
-import org.dspace.app.xmlui.wing.element.Row;
-import org.dspace.app.xmlui.wing.element.Table;
+import org.dspace.app.xmlui.wing.Message;
+import org.dspace.app.xmlui.wing.element.*;
 import org.dspace.authorize.AuthorizeException;
-import org.dspace.content.Collection;
-import org.dspace.content.Community;
 import org.dspace.content.DSpaceObject;
-import org.dspace.content.Item;
 import org.dspace.core.Constants;
 import org.dspace.statistics.Dataset;
 import org.dspace.statistics.content.DatasetDSpaceObjectGenerator;
 import org.dspace.statistics.content.DatasetTimeGenerator;
-import org.dspace.statistics.content.DatasetTypeGenerator;
 import org.dspace.statistics.content.StatisticsDataVisits;
 import org.dspace.statistics.content.StatisticsListing;
 import org.dspace.statistics.content.StatisticsTable;
+import org.dspace.handle.HandleManager;
 import org.xml.sax.SAXException;
 
 public class StatisticsTransformer extends AbstractDSpaceTransformer {
 
 	private static Logger log = Logger.getLogger(StatisticsTransformer.class);
 
-	/**
+    private static final Message T_dspace_home = message("xmlui.general.dspace_home");
+    private static final Message T_head_title = message("xmlui.statistics.title");
+    private static final Message T_statistics_trail = message("xmlui.statistics.trail");
+    private static final String T_head_visits_total = "xmlui.statistics.visits.total";
+    private static final String T_head_visits_month = "xmlui.statistics.visits.month";
+    private static final String T_head_visits_views = "xmlui.statistics.visits.views";
+
+    /**
+     * Add a page title and trail links
+     */
+    public void addPageMeta(PageMeta pageMeta) throws SAXException, WingException, UIException, SQLException, IOException, AuthorizeException {
+        String handle = parameters.getParameter("handle", null);
+        DSpaceObject dso = null;
+        if(handle != null)
+            dso = HandleManager.resolveToObject(context, handle);
+
+        pageMeta.addTrailLink(contextPath + "/",T_dspace_home);
+        
+        if(dso != null)
+            HandleUtil.buildHandleTrail(dso,pageMeta,contextPath);
+        pageMeta.addTrailLink(contextPath + "/handle" + (dso != null && dso.getHandle() != null ? "/" + dso.getHandle() : "/statistics"), T_statistics_trail);
+
+        // Add the page title
+        pageMeta.addMetadata("title").addContent(T_head_title);
+    }
+
+    /**
 	 * What to add at the end of the body
 	 */
 	public void addBody(Body body) throws SAXException, WingException,
 			UIException, SQLException, IOException, AuthorizeException {
 
-		DSpaceObject dso = HandleUtil.obtainHandle(objectModel);
+        //Try to find our dspace object
+        String handle = parameters.getParameter("handle", null);
+        DSpaceObject dso = null;
+        if(handle != null)
+            dso = HandleManager.resolveToObject(context, handle);
 
 		try
 		{
@@ -77,14 +100,14 @@ public class StatisticsTransformer extends AbstractDSpaceTransformer {
 		
 		Division home = body.addDivision("home", "primary repository");
 		Division division = home.addDivision("stats", "secondary stats");
-		division.setHead("Statistics");
+		division.setHead(T_head_title);
 
 		try {
 
 			StatisticsTable statisticsTable = new StatisticsTable(
 					new StatisticsDataVisits());
 
-			statisticsTable.setTitle("Total Visits Per Month");
+			statisticsTable.setTitle(T_head_visits_month);
 			statisticsTable.setId("tab1");
 
 			DatasetTimeGenerator timeAxis = new DatasetTimeGenerator();
@@ -102,7 +125,7 @@ public class StatisticsTransformer extends AbstractDSpaceTransformer {
 			StatisticsListing statListing = new StatisticsListing(
 					new StatisticsDataVisits());
 
-			statListing.setTitle("Total Visits");
+			statListing.setTitle(T_head_visits_total);
 			statListing.setId("list1");
 
 			addDisplayListing(division, statListing);
@@ -121,14 +144,14 @@ public class StatisticsTransformer extends AbstractDSpaceTransformer {
 		
 		// Build the collection viewer division.
 		Division division = home.addDivision("stats", "secondary stats");
-		division.setHead("Statistics");
+		division.setHead(T_head_title);
 
 		
 		try {
 			StatisticsListing statListing = new StatisticsListing(
 					new StatisticsDataVisits(dso));
 
-			statListing.setTitle("Total Visits");
+			statListing.setTitle(T_head_visits_total);
 			statListing.setId("list1");
 
 			DatasetDSpaceObjectGenerator dsoAxis = new DatasetDSpaceObjectGenerator();
@@ -150,7 +173,7 @@ public class StatisticsTransformer extends AbstractDSpaceTransformer {
 
 			StatisticsTable statisticsTable = new StatisticsTable(new StatisticsDataVisits(dso));
 			
-			statisticsTable.setTitle("Total Visits Per Month");
+			statisticsTable.setTitle(T_head_visits_month);
 			statisticsTable.setId("tab1");
 
 			DatasetTimeGenerator timeAxis = new DatasetTimeGenerator();
@@ -255,12 +278,11 @@ public class StatisticsTransformer extends AbstractDSpaceTransformer {
 			Table table = wrapper.addTable("list-table", 1, 1,
 					title == null ? "" : "tableWithTitle");
 			if (title != null)
-				table.setHead(title);
+				table.setHead(message(title));
 
 			/** Generate Header Row */
 			Row headerRow = table.addRow();
-			headerRow.addCell("spacer", Cell.ROLE_DATA, "labelcell")
-					.addContent("&nbsp;");
+			headerRow.addCell("spacer", Cell.ROLE_DATA, "labelcell");
 
 			String[] cLabels = dataset.getColLabels().toArray(new String[0]);
 			for (int row = 0; row < cLabels.length; row++) {
@@ -310,15 +332,13 @@ public class StatisticsTransformer extends AbstractDSpaceTransformer {
 			Table table = mainDiv.addTable("list-table", matrix.length, 2,
 					title == null ? "" : "tableWithTitle");
 			if (title != null)
-				table.setHead(title);
+				table.setHead(message(title));
 
 			Row headerRow = table.addRow();
 
-			String label = "FIX ME";
-
-			headerRow.addCell("", Cell.ROLE_DATA, "labelcell").addContent(label);
+			headerRow.addCell("", Cell.ROLE_DATA, "labelcell");
 			
-			headerRow.addCell("", Cell.ROLE_DATA, "labelcell").addContent("Views");
+			headerRow.addCell("", Cell.ROLE_DATA, "labelcell").addContent(message(T_head_visits_views));
 
 			/** Generate Table Body */
 			for (int row = 0; row < matrix.length; row++) {
