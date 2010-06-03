81e4ca83990e03c9aa1c2db6fbc3715b7a831d06
Mark Diggory <mdiggory@atmire.com>
2010-06-03 00:05:57 +0000

    Add license headers.
    git-svn-id: http://scm.dspace.org/svn/repo/modules/dspace-services/trunk@5054 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- impl/src/main/java/org/dspace/services/caching/model/package-info.java
+++ impl/src/main/java/org/dspace/services/caching/model/package-info.java
@@ -1,4 +1,14 @@
 /**
+ * $Id: $
+ * $URL: $
+ * *************************************************************************
+ * Copyright (c) 2002-2009, DuraSpace.  All rights reserved
+ * Licensed under the DuraSpace License.
+ *
+ * A copy of the DuraSpace License has been included in this
+ * distribution and is available at: http://scm.dspace.org/svn/repo/licenses/LICENSE.txt
+ */
+/**
  * Implementations of the Cache type, for various purposes.
  */
 
--- utils/src/main/java/org/dspace/utils/package-info.java
+++ utils/src/main/java/org/dspace/utils/package-info.java
@@ -1,3 +1,13 @@
+/**
+ * $Id: $
+ * $URL: $
+ * *************************************************************************
+ * Copyright (c) 2002-2009, DuraSpace.  All rights reserved
+ * Licensed under the DuraSpace License.
+ *
+ * A copy of the DuraSpace License has been included in this
+ * distribution and is available at: http://scm.dspace.org/svn/repo/licenses/LICENSE.txt
+ */
 /** Utility classes and starting points.
  * Standalone code, for example, can use the framework by instantiating 
  * a DSpace object and using its methods to locate other parts of the 
