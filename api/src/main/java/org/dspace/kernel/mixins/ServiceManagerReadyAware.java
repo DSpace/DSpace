8b21e4922880e84701f69540bf73a0a274d98ad3
Mark Diggory <mdiggory@atmire.com>
2010-09-30 05:13:22 +0000

    Adjust Licensing.
    git-svn-id: http://scm.dspace.org/svn/repo/modules/dspace-services/trunk@5368 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- api/src/main/java/org/dspace/kernel/mixins/ServiceManagerReadyAware.java
+++ api/src/main/java/org/dspace/kernel/mixins/ServiceManagerReadyAware.java
@@ -1,12 +1,9 @@
 /**
- * $Id$
- * $URL$
- * *************************************************************************
- * Copyright (c) 2002-2009, DuraSpace.  All rights reserved
+ * Copyright (c) 2002-2010, DuraSpace.  All rights reserved
  * Licensed under the DuraSpace License.
  *
  * A copy of the DuraSpace License has been included in this
- * distribution and is available at: http://scm.dspace.org/svn/repo/licenses/LICENSE.txt
+ * distribution and is available at: http://www.dspace.org/license
  */
 package org.dspace.kernel.mixins;
 
