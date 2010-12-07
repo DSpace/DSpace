95484efe69c6ac8329650df0e5df0f7303e0ac20
Mark Wood <mwood@IUPUI.Edu>
2010-12-07 17:36:09 +0000

    Tell the Javadoc reader what this package is for.
    git-svn-id: http://scm.dspace.org/svn/repo/dspace/trunk@5927 9c30dcfa-912a-0410-8fc2-9e0234be79fd

---
+++ dspace-api/src/main/java/org/dspace/app/packager/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>Tools for exporting and importing DSpace objects (Community, Collection,
+ * Item, etc.) wrapped in various kinds of packaging.</p>
+ * 
+ * @see org.dspace.content.packager
+ */
+
+package org.dspace.app.packager;
