0cbeef401028c56623d92a4e684fb48beb9b403e
Peter Dietz <dietz.72@osu.edu>
2011-03-15 18:45:52 +0000

    [DS-776] Collection admin cannot add bitstreams unless there is at least one bundle. Additionally did whitespace and formatting fix to the file being touched.
    git-svn-id: http://scm.dspace.org/svn/repo/dspace/branches/dspace-1_7_x@6143 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/administrative/item/AddBitstreamForm.java
+++ dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/administrative/item/AddBitstreamForm.java
@@ -31,16 +31,16 @@ import org.dspace.core.Constants;
 import org.xml.sax.SAXException;
 
 /**
- * 
- * Show a form that allows the user to upload a new bitstream. The 
- * user can select the new bitstream's bundle (which is unchangable 
+ *
+ * Show a form that allows the user to upload a new bitstream. The
+ * user can select the new bitstream's bundle (which is unchangable
  * after upload) and a description for the file.
- * 
+ *
  * @author Scott Phillips
  */
 public class AddBitstreamForm extends AbstractDSpaceTransformer
 {
-	
+
 	/** Language strings */
 	private static final Message T_dspace_home = message("xmlui.general.dspace_home");
 	private static final Message T_submit_cancel = message("xmlui.general.cancel");
@@ -58,130 +58,129 @@ public class AddBitstreamForm extends AbstractDSpaceTransformer
 
 	private static final Message T_no_bundles = message("xmlui.administrative.item.AddBitstreamForm.no_bundles");
 
-	
+
 	private static final String DEFAULT_BUNDLE_LIST = "ORIGINAL, METADATA, THUMBNAIL, LICENSE, CC_LICENSE";
-		
+
 	public void addPageMeta(PageMeta pageMeta) throws WingException
 	{
-		pageMeta.addMetadata("title").addContent(T_title);
+            pageMeta.addMetadata("title").addContent(T_title);
 
-		pageMeta.addTrailLink(contextPath + "/", T_dspace_home);
-		pageMeta.addTrailLink(contextPath + "/admin/item",T_item_trail);
-		pageMeta.addTrail().addContent(T_trail);
-	}
-
-	public void addBody(Body body) throws SAXException, WingException,
-	UIException, SQLException, IOException, AuthorizeException
-	{    	
-		int itemID = parameters.getParameterAsInteger("itemID",-1);
-		org.dspace.content.Item item = org.dspace.content.Item.find(context,itemID);
-		
-		// DIVISION: main div
-		Division div = body.addInteractiveDivision("add-bitstream", contextPath+"/admin/item", Division.METHOD_MULTIPART, "primary administrative item");    	
-
-		// LIST: upload form
-		List upload = div.addList("submit-upload-new", List.TYPE_FORM);
-		upload.setHead(T_head1);    
-
-		int bundleCount = 0; // record how many bundles we are able to upload too.
-		Select select = upload.addItem().addSelect("bundle");
-		select.setLabel(T_bundle_label);
-		
-		// Get the list of bundles to allow the user to upload too. Either use the default 
-		// or one supplied from the dspace.cfg.
-		String bundleString = ConfigurationManager.getProperty("xmlui.bundle.upload");
-        if (bundleString == null || bundleString.length() == 0)
-        {
-            bundleString = DEFAULT_BUNDLE_LIST;
+            pageMeta.addTrailLink(contextPath + "/", T_dspace_home);
+            pageMeta.addTrailLink(contextPath + "/admin/item", T_item_trail);
+            pageMeta.addTrail().addContent(T_trail);
         }
-        String[] parts = bundleString.split(",");
-        for (String part : parts)
-        {
-        	if (addBundleOption(item,select,part.trim()))
-            {
-                bundleCount++;
-            }
-        }
-        select.setOptionSelected("ORIGINAL");
-		
-		if (bundleCount == 0)
-        {
-            select.setDisabled();
-        }
-		
 
-		File file = upload.addItem().addFile("file");
-		file.setLabel(T_file_label);
-		file.setHelp(T_file_help);
-		file.setRequired();
-
-		if (bundleCount == 0)
-        {
-            file.setDisabled();
-        }
-		
-		Text description = upload.addItem().addText("description");
-		description.setLabel(T_description_label);
-		description.setHelp(T_description_help);
-
-		if (bundleCount == 0)
-        {
-            description.setDisabled();
-        }
-		
-		if (bundleCount == 0)
-        {
-            upload.addItem().addContent(T_no_bundles);
-        }
-		
-		// ITEM: actions
-		Item actions = upload.addItem();
-		Button button = actions.addButton("submit_upload");
-		button.setValue(T_submit_upload);
-		if (bundleCount == 0)
-        {
-            button.setDisabled();
-        }
-		
-		actions.addButton("submit_cancel").setValue(T_submit_cancel);
-
-		div.addHidden("administrative-continue").setValue(knot.getId()); 
-	}	
-	
-	public boolean addBundleOption(org.dspace.content.Item item, Select select, String bundleName) throws SQLException, WingException
+	public void addBody(Body body) throws SAXException, WingException, UIException, SQLException, IOException, AuthorizeException
 	{
-		
-		// For some crazzy reason multiple bundles can share the same name
-		Bundle[] bundles = item.getBundles(bundleName);
-		if (bundles == null || bundles.length == 0)
-		{
-			// If the bundle does not exist then you have to be supper admin to be able
-			// to upload to this bundle because at upload time the bundle will be created but
-			// there is no way anyone but super admin could have access to add to the bundle.
-			if ( ! AuthorizeManager.isAdmin(context))
+            int itemID = parameters.getParameterAsInteger("itemID", -1);
+            org.dspace.content.Item item = org.dspace.content.Item.find(context, itemID);
+
+            // DIVISION: main div
+            Division div = body.addInteractiveDivision("add-bitstream", contextPath + "/admin/item", Division.METHOD_MULTIPART, "primary administrative item");
+
+            // LIST: upload form
+            List upload = div.addList("submit-upload-new", List.TYPE_FORM);
+            upload.setHead(T_head1);
+
+            int bundleCount = 0; // record how many bundles we are able to upload too.
+            Select select = upload.addItem().addSelect("bundle");
+            select.setLabel(T_bundle_label);
+
+            // Get the list of bundles to allow the user to upload too. Either use the default
+            // or one supplied from the dspace.cfg.
+            String bundleString = ConfigurationManager.getProperty("xmlui.bundle.upload");
+            if (bundleString == null || bundleString.length() == 0)
             {
-                return false;  // you can't upload to this bundle.
+                bundleString = DEFAULT_BUNDLE_LIST;
             }
-		}
-		else
-		{
-			// At least one bundle exists, does the user have privleges to upload to it?
-			Bundle bundle = bundles[0];
-			if ( ! AuthorizeManager.authorizeActionBoolean(context, bundle, Constants.ADD))
+            String[] parts = bundleString.split(",");
+            for (String part : parts)
             {
-                return false; // you can't upload to this bundle.
+                if (addBundleOption(item, select, part.trim()))
+                {
+                    bundleCount++;
+                }
             }
-			
-			// You also need the write privlege on the bundle.
-			if ( ! AuthorizeManager.authorizeActionBoolean(context, bundle, Constants.WRITE))
+            select.setOptionSelected("ORIGINAL");
+
+            if (bundleCount == 0) {
+                select.setDisabled();
+            }
+
+
+            File file = upload.addItem().addFile("file");
+            file.setLabel(T_file_label);
+            file.setHelp(T_file_help);
+            file.setRequired();
+
+            if (bundleCount == 0) {
+                file.setDisabled();
+            }
+
+            Text description = upload.addItem().addText("description");
+            description.setLabel(T_description_label);
+            description.setHelp(T_description_help);
+
+            if (bundleCount == 0) {
+                description.setDisabled();
+            }
+
+            if (bundleCount == 0) {
+                upload.addItem().addContent(T_no_bundles);
+            }
+
+            // ITEM: actions
+            Item actions = upload.addItem();
+            Button button = actions.addButton("submit_upload");
+            button.setValue(T_submit_upload);
+            if (bundleCount == 0) {
+                button.setDisabled();
+            }
+
+            actions.addButton("submit_cancel").setValue(T_submit_cancel);
+
+            div.addHidden("administrative-continue").setValue(knot.getId());
+        }
+
+	/**
+         * Add the bundleName to the list of bundles available to submit to.
+         * Performs an authorization check that the current user has privileges
+         * @param item DSO item being evaluated
+         * @param select DRI wing select box that is being added to
+         * @param bundleName
+         * @return boolean indicating whether user can upload to bundle
+         * @throws SQLException
+         * @throws WingException
+         */
+        public boolean addBundleOption(org.dspace.content.Item item, Select select, String bundleName) throws SQLException, WingException
+	{
+            Bundle[] bundles = item.getBundles(bundleName);
+            if (bundles == null || bundles.length == 0)
             {
-                return false;  // you can't upload
+                // No bundle, so the user has to be authorized to add to item.
+                if(!AuthorizeManager.authorizeActionBoolean(context, item, Constants.ADD))
+                {
+                    return false;
+                }
+            } else
+            {
+                // At least one bundle exists, does the user have privleges to upload to it?
+                Bundle bundle = bundles[0];
+                if (!AuthorizeManager.authorizeActionBoolean(context, bundle, Constants.ADD))
+                {
+                    return false; // you can't upload to this bundle.
+                }
+
+                // You also need the write privlege on the bundle.
+                if (!AuthorizeManager.authorizeActionBoolean(context, bundle, Constants.WRITE))
+                {
+                    return false;  // you can't upload
+                }
             }
-		}
-		
-		// It's okay to upload.
-		select.addOption(bundleName, message("xmlui.administrative.item.AddBitstreamForm.bundle."+bundleName));
-		return true;
-	}
-	
+
+            // It's okay to upload.
+            select.addOption(bundleName, message("xmlui.administrative.item.AddBitstreamForm.bundle." + bundleName));
+            return true;
+        }
+
 }
