0ad274e348e0e2e8a86f030bac6bcb52c8df6406
Tim Donohue <tdonohue@duraspace.org>
2011-08-24 21:56:42 +0000

    New DSpace Javascript file (DS-749) missing our DSpace license header.
    git-svn-id: http://scm.dspace.org/svn/repo/dspace/trunk@6588 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- dspace-xmlui/dspace-xmlui-webapp/src/main/webapp/static/js/bitstream-ordering.js
+++ dspace-xmlui/dspace-xmlui-webapp/src/main/webapp/static/js/bitstream-ordering.js
@@ -1,3 +1,10 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 (function ($) {
     $(document).ready(function() {
         var bitstreamTable = $("table#aspect_administrative_item_EditItemBitstreamsForm_table_editItemBitstreams");
