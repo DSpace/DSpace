320b61af01fc650f18577b1452c119394477f00f
Ryan Scherle <ryan@scherle.org>
2012-01-25 19:40:52 +0000

    revert r5228, because the javascript depends on the asterisks to function properly

--- dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/SelectPublicationStep.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/SelectPublicationStep.java
@@ -255,7 +255,9 @@ public class SelectPublicationStep extends AbstractSubmissionStep {
         for (int i = 0; i < journalVals.size(); i++) {
             String val =  journalVals.get(i);
             String name =  journalNames.get(i);
-            journalID.addOption(val.equals(selectedJournalId), val, name);
+	    if(org.dspace.submit.step.SelectPublicationStep.integratedJournals.contains(val))
+		name += "*";
+	    journalID.addOption(val.equals(selectedJournalId), val, name);
         }
 
         journalID.setLabel(T_SELECT_LABEL_NEW);
