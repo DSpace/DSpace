07b7668c1d07a56bb5e7fffbded84a73bc29b99f
Nicolás Romagnoli <romagnoli.nicolas@gmail.com>
2012-02-17 13:56:17 +0000

    Task #1241 - Se terminó de programar el edireccionador de urls del viejo sedici al nuevo en dspace.

--- xmlui/src/main/java/ar/edu/unlp/sedici/aspect/redirect/RedirectAction.java
+++ xmlui/src/main/java/ar/edu/unlp/sedici/aspect/redirect/RedirectAction.java
@@ -15,6 +15,7 @@
  */
 package ar.edu.unlp.sedici.aspect.redirect;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -33,6 +34,10 @@ import org.apache.cocoon.environment.SourceResolver;
 import org.apache.cocoon.environment.http.HttpEnvironment;
 import org.apache.cocoon.sitemap.PatternException;
 import org.dspace.app.xmlui.utils.AuthenticationUtil;
+import org.dspace.app.xmlui.utils.ContextUtil;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Item;
+import org.dspace.content.ItemIterator;
 import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Context;
 import org.dspace.eperson.EPerson;
@@ -63,6 +68,17 @@ import org.dspace.eperson.EPerson;
 public class RedirectAction extends AbstractAction
 {
 
+	private static String PREFIX="sedici.redirect";
+	private static String MAP_PREFIX="map.url";
+	private static String MAP_PREFIX_PARAMS="params";
+	private static String OLD_URL="old_url";
+	private static String NEW_URL="new_url";
+	private static String URL_PART="url_part";
+	private static String TYPE="type";
+	private static String OLD_PARAM_NAME="old_name";
+	private static String NEW_PARAM_NAME="new_name";
+	
+	
 	private static Map<String, Map<String, Object>> properties=new HashMap<String, Map<String,Object>>();
 	private static Map<String, String> urls=null;
 
@@ -81,30 +97,40 @@ public class RedirectAction extends AbstractAction
 		String clave;
 		String valor;
 		Boolean valorBooleano;
+		String type;
 		//cargo la key de la nueva url y el valor
-		clave=prefijo+".new_url";
-		valor=ConfigurationManager.getProperty("sedici.redirect", clave);
+		clave=prefijo+"."+RedirectAction.NEW_URL;
+		valor=ConfigurationManager.getProperty(RedirectAction.PREFIX, clave);
 		retorno.put(clave,  valor);
 		//cargo los parametros
 		int inicial=1;
-		clave=prefijo+".params."+inicial+".old_name";
-		valor=ConfigurationManager.getProperty("sedici.redirect", clave);
+		clave=prefijo+"."+RedirectAction.MAP_PREFIX_PARAMS+"."+inicial+"."+RedirectAction.OLD_PARAM_NAME;
+		valor=ConfigurationManager.getProperty(RedirectAction.PREFIX, clave);
 		while (valor!=null){
 			//agrego el nombre del parametro viejo al map
 			retorno.put(clave,  valor);
-			clave=prefijo+".params."+inicial+".url_part";
-			valorBooleano=ConfigurationManager.getBooleanProperty("sedici.redirect", clave);
+			
+			//agrego si el aprametro va a formar parte de la nueva url
+			clave=prefijo+"."+RedirectAction.MAP_PREFIX_PARAMS+"."+inicial+"."+RedirectAction.URL_PART;			
+			valorBooleano=ConfigurationManager.getBooleanProperty(RedirectAction.PREFIX, clave);
 			retorno.put(clave, valorBooleano);
+			
+			//agrego si el dato requiere la transformacion de algun tipo
+			clave=prefijo+"."+RedirectAction.MAP_PREFIX_PARAMS+"."+inicial+"."+RedirectAction.TYPE;
+			type=ConfigurationManager.getProperty(RedirectAction.PREFIX, clave);
+			retorno.put(clave, type);
+			
 			if (!valorBooleano){
 				//si no es parte de la url el parametro, tengo que guardar el new name
-				clave=prefijo+".params."+inicial+".new_name";
-				valor=ConfigurationManager.getProperty("sedici.redirect", clave);
+				clave=prefijo+"."+RedirectAction.MAP_PREFIX_PARAMS+"."+inicial+"."+RedirectAction.NEW_PARAM_NAME;
+				valor=ConfigurationManager.getProperty(RedirectAction.PREFIX, clave);
 				retorno.put(clave,  valor);
 			}
+			
 			//recupero el proximo parametro
 			inicial+=1;
-			clave=prefijo+".params."+inicial+".old_name";
-			valor=ConfigurationManager.getProperty("sedici.redirect", clave);
+			clave=prefijo+"."+RedirectAction.MAP_PREFIX_PARAMS+"."+inicial+"."+RedirectAction.OLD_PARAM_NAME;
+			valor=ConfigurationManager.getProperty(RedirectAction.PREFIX, clave);
 		}
        properties.put(prefijo,  retorno);
 		
@@ -125,11 +151,11 @@ public class RedirectAction extends AbstractAction
 	private static Map<String, String> generarUrls() {
 		int inicial=1;
 		Map<String, String> retorno=new HashMap<String, String>();
-		String valor=ConfigurationManager.getProperty("sedici.redirect", "map.url"+inicial);
+		String valor=ConfigurationManager.getProperty(RedirectAction.PREFIX, RedirectAction.MAP_PREFIX + "." + inicial);
 		while (valor!=null){
-			retorno.put(valor, "map.url"+inicial);
+			retorno.put(valor, RedirectAction.MAP_PREFIX + "." + inicial);
 			inicial+=1;
-			valor=ConfigurationManager.getProperty("sedici.redirect", "map.url"+inicial);
+			valor=ConfigurationManager.getProperty(RedirectAction.PREFIX, RedirectAction.MAP_PREFIX + "." + inicial);
 		}
 		RedirectAction.setUrls(retorno);
 		return retorno;
@@ -141,9 +167,10 @@ public class RedirectAction extends AbstractAction
 	public Map act(Redirector redirector, SourceResolver resolver, Map objectModel,
             String source, Parameters parameters) throws Exception
     {
+		
         Request request = ObjectModelHelper.getRequest(objectModel);
 
-        String old_url = request.getParameter("old_url");
+        String old_url = request.getParameter(RedirectAction.OLD_URL);
         
         //recupero la clave para la vieja url
         String prefijo=this.recuperarClave(old_url);
@@ -151,7 +178,7 @@ public class RedirectAction extends AbstractAction
         Map<String, Object> propertiesPrefijo=this.recuperarProperties(prefijo);
         
         //recupero los valores
-        String new_url=(String)propertiesPrefijo.get(prefijo+".new_url");
+        String new_url=(String)propertiesPrefijo.get(prefijo+"."+RedirectAction.NEW_URL);
         
         //recupero la cantidad de parámetros que van a ser pasados al redirect   
     	Hashtable<String, String> new_url_params=new Hashtable<String, String>();
@@ -160,30 +187,39 @@ public class RedirectAction extends AbstractAction
         String param_old_name="";
         String param_new_name="";
     	Boolean url_part=false;
+    	String type;
     	String param_value="";
-    	String prefijo_parametro=prefijo+".params."+inicial;
+    	String prefijo_parametro=prefijo+"."+RedirectAction.MAP_PREFIX_PARAMS+"."+inicial;
 
-        while (propertiesPrefijo.containsKey(prefijo_parametro+".old_name")){
-            param_old_name=(String)propertiesPrefijo.get(prefijo_parametro+".old_name");
+        while (propertiesPrefijo.containsKey(prefijo_parametro+"."+RedirectAction.OLD_PARAM_NAME)){
+            param_old_name=(String)propertiesPrefijo.get(prefijo_parametro+"."+RedirectAction.OLD_PARAM_NAME);
         	
     		//debo verificar si el parametro es parte de la url o se pasa como parámetro.
     		//IMPORTANTE: aca podriamos extender el config en el caso de que varios parametros puedan formar parte de la url, para
     		//            agregarle el orden y escribir bien la URL nueva.
     		//            En este caso, solo UN parámetro es parte de la url, los demás se pasan como parámetros.
-    		url_part=(Boolean)propertiesPrefijo.get(prefijo_parametro+".url_part");
+    		url_part=(Boolean)propertiesPrefijo.get(prefijo_parametro+"."+RedirectAction.URL_PART);
     		
     		//recupero del request el valor del parametro con nomre old_param
     		param_value=request.getParameter(param_old_name);
     		
-			if (!url_part){
-				//si no es parte de la url se agrega como parametro a la url
-        		param_new_name=(String)propertiesPrefijo.get(prefijo_parametro+".new_name");
-        		new_url_params.put(param_new_name, param_value);
-			}else{
+    		//Transformo en caso de ser necesario el valor del parametro
+    		type=(String)propertiesPrefijo.get(prefijo_parametro+"."+RedirectAction.TYPE); 
+    		if (type!=null){
+    			param_value=this.transformParamValue(request, type, param_value);
+    		}
+    		
+    		//Si el parametro es parte de la url lo agrego separado por /, sino lo agrego como parametro del request
+			if (url_part){
 				new_url=new_url+"/"+param_value;
+			}else{
+				//si no es parte de la url se agrega como parametro a la url
+        		param_new_name=(String)propertiesPrefijo.get(prefijo_parametro+"."+RedirectAction.NEW_PARAM_NAME);
+        		new_url_params.put(param_new_name, param_value);				
 			}
+			
 			inicial+=1;
-	    	prefijo_parametro=prefijo+".params."+inicial;
+	    	prefijo_parametro=prefijo+"."+RedirectAction.MAP_PREFIX_PARAMS+"."+inicial;
 
 		};
 		//recorro el hashtable creado con los parametros y lo agrego a la url
@@ -214,7 +250,33 @@ public class RedirectAction extends AbstractAction
         return null;
     }
     
-    private Map<String, Object> recuperarProperties(String prefijo) {
+	/*
+	 * Transforma el valor del parametro al valor correspondiente el nueva base de datos.
+	 */
+    private String transformParamValue(Request request, String type, String param_value) {
+	   	if (type.equals("item")){
+    		//Obtengo el contexto para hacer la consulta a la BD
+    		try {
+				Context contexto=ContextUtil.obtainContext(request);
+				ItemIterator items=Item.findByMetadataField(contexto, "sedici2003", "identifier", null, param_value);
+				if (items.hasNext()){
+					param_value=items.next().getHandle();					
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+			} catch (AuthorizeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return param_value;
+	}
+
+	private Map<String, Object> recuperarProperties(String prefijo) {
 		if (!RedirectAction.getProperties().containsKey(prefijo)){
 			RedirectAction.generarProperties(prefijo);
 		};
@@ -227,19 +289,5 @@ public class RedirectAction extends AbstractAction
      */
     public String recuperarClave(String old_url){
     	return this.getUrls().get(old_url);
-
-    	/*Hashtable<Object, Object> propiedades=ConfigurationManager.getProperties("sedici.redirect");
-    	Enumeration<Object> claves=propiedades.keys();
-    	boolean encontrado=false;
-    	String valor=null;
-    	String clave_retorno=null;
-    	while (claves.hasMoreElements()){
-    		clave_retorno=(String)claves.nextElement();
-    		valor=(String)(propiedades.get(clave_retorno));
-    		if (valor.equals(old_url)){    			
-    			return clave_retorno;
-    		}
-    	};
-    	return null;*/
     }
 }
