f59481ae0c663f5b5740d78c613ae69cba24332f
Nestor Oviedo <oviedonestor@gmail.com>
2012-02-29 14:46:28 +0000

    Por error se commiteo el archivo armar_share_pa_lucas.sh

--- armar_share_pa_lucas.sh
+++
@@ -1,8 +0,0 @@
-#smb://163.10.34.143/dspacethemepalucas/
-#lucas
-#lucasEfo
-
-cd /home/dspace/dspace-sedici/install/webapps/xmlui/themes
-ln -s ../../../../xmlui/src/main/webapp/themes/Sedici Sedici
-sudo chmod ug+rw -R .
-
