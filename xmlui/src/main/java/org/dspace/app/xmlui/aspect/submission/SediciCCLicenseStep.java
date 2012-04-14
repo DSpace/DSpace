68db40dffa26abd2f082025321baa9fb6f7023df
Nicolás Romagnoli <romagnoli.nicolas@gmail.com>
2012-04-14 00:10:04 +0000

    Bug #1486
    - Se agregó el step de la licencia de creative commons nuestro, se agregaron los mensajes de internacionalización, y se agrego un nuevo workflow de submission para la colección de autoarchivo. Se agregó además una entrada en dspace.cfg para obtener la descripción de la jurisdicción de la licenciay guardarlo en la BD

---
+++ xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/SediciCCLicenseStep.java
@@ -0,0 +1,235 @@
+package org.dspace.app.xmlui.aspect.submission;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.persistence.criteria.CriteriaBuilder.Case;
+import javax.servlet.http.HttpSession;
+
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Request;
+import org.dspace.app.xmlui.utils.UIException;
+import org.dspace.app.xmlui.aspect.submission.AbstractSubmissionStep;
+import org.dspace.app.xmlui.wing.Message;
+import org.dspace.app.xmlui.wing.WingException;
+import org.dspace.app.xmlui.wing.element.Body;
+import org.dspace.app.xmlui.wing.element.Division;
+import org.dspace.app.xmlui.wing.element.List;
+import org.dspace.app.xmlui.wing.element.Radio;
+import org.dspace.app.xmlui.wing.element.Select;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Collection;
+import org.dspace.content.DCValue;
+import org.dspace.content.Item;
+import org.dspace.license.CreativeCommons;
+import org.dspace.license.CCLicenseField;
+import org.dspace.license.CCLookup;
+import org.dspace.license.CCLicense;
+import org.dspace.core.ConfigurationManager;
+import org.xml.sax.SAXException;
+
+/**
+ * This is an optional page of the item submission processes. The Creative 
+ * Commons license may be added to an item in addition to the standard distribution 
+ * license. This step will allow the user to go off to the creative commons website 
+ * select a license and then when returned view what license was added to the item.
+ * <P>
+ * This class is called by org.dspace.app.xmlui.submission.step.LicenseStep
+ * when the Creative Commons license is enabled
+ * <P>
+ * The form is divided into three major divisions: 1) A global div surrounds the 
+ * whole page, 2) a specific interactive div displays the button that goes off to the 
+ * creative commons website to select a license, and 3) a local division that displays 
+ * the selected license and standard action bar.
+ * 
+ * @author Scott Phillips
+ * @author Tim Donohue (updated for Configurable Submission)
+ * @author Wendy Bossons (updated for DSpace 1.8)
+ */
+public class SediciCCLicenseStep extends AbstractSubmissionStep
+{
+	/** Language Strings **/
+    protected static final Message T_head = 
+        message("xmlui.Submission.submit.CCLicenseStep.head");
+        protected static final Message T_select =
+            message("xmlui.Submission.submit.CCLicenseStep.submit_choose_creative_commons");
+    protected static final Message T_info1 = 
+        message("xmlui.Submission.submit.CCLicenseStep.info1");
+    protected static final Message T_submit_to_creative_commons = 
+        message("xmlui.Submission.submit.CCLicenseStep.submit_to_creative_commons");
+        protected static final Message T_submit_issue_creative_commons =
+            message("xmlui.Submission.submit.CCLicenseStep.submit_issue_creative_commons");
+    protected static final Message T_license = 
+        message("xmlui.Submission.submit.CCLicenseStep.license");
+        protected static final Message T_submit_remove = message("xmlui.Submission.submit.CCLicenseStep.submit_remove");
+        protected static final Message T_no_license    = message("xmlui.Submission.submit.CCLicenseStep.no_license");
+        protected static final Message T_select_change = message("xmlui.Submission.submit.CCLicenseStep.select_change");
+        protected static final Message T_save_changes  = message("xmlui.Submission.submit.CCLicenseStep.save_changes");
+        protected static final Message T_ccws_error  = message("xmlui.Submission.submit.CCLicenseStep.ccws_error");
+        
+        protected static final Message Commercial_question  = message("xmlui.Submission.submit.SediciCCLicenseStep.CommercialQuestion");
+        protected static final Message Commercial_question_answer_no  = message("xmlui.Submission.submit.SediciCCLicenseStep.CommercialAnswerNo");
+        protected static final Message Commercial_question_answer_yes  = message("xmlui.Submission.submit.SediciCCLicenseStep.CommercialAnswerYes");
+        protected static final Message Derivatives_question  = message("xmlui.Submission.submit.SediciCCLicenseStep.DerivativesQuestion");
+        protected static final Message Derivatives_question_answer_no  = message("xmlui.Submission.submit.SediciCCLicenseStep.DerivativesAnswerNo");
+        protected static final Message Derivatives_question_answer_yes  = message("xmlui.Submission.submit.SediciCCLicenseStep.DerivativesAnswerYes");
+        protected static final Message Derivatives_question_answer_sa  = message("xmlui.Submission.submit.SediciCCLicenseStep.DerivativesAnswerShareALike");
+	/**
+	 * Establish our required parameters, abstractStep will enforce these.
+	 */
+	public SediciCCLicenseStep()
+	{
+	    this.requireSubmission = true;
+	    this.requireStep = true;
+	}
+	
+	
+	public void addBody(Body body) throws SAXException, WingException,
+	UIException, SQLException, IOException, AuthorizeException
+	{
+	    // Build the url to and from creative commons
+	    Item item = submission.getItem();
+	    String ccUri=ConfigurationManager.getProperty("cc.license.uri");
+	    DCValue[] carga=item.getMetadata(ccUri);
+	    String dato;
+	    String commercial="";
+	    String derivatives="";
+	    if (carga.length>0){
+	    	dato=carga[0].value;
+	    	int inicio=dato.indexOf("/by")+1;
+	    	String substring=dato.substring(inicio);
+	    	int fin= substring.indexOf("/");	    	
+	    	substring=substring.substring(0,fin);
+	    	HashMap<String, Integer> arreglo=new HashMap<String, Integer>();
+	    	arreglo.put("by-nc-nd", 1);
+	    	arreglo.put("by-nc-sa", 2);
+	    	arreglo.put("by-nc", 3);
+	    	arreglo.put("by-nd", 4);
+	    	arreglo.put("by-sa", 5);
+	    	arreglo.put("by", 6);
+	    	switch (arreglo.get(substring)) {
+			case 1:
+				commercial="nc";
+				derivatives="nd";
+				break;
+			case 2:
+				commercial="nc";
+				derivatives="sa";
+				break;
+			case 3:
+				commercial="nc";
+				derivatives="y";
+				break;
+			case 4:
+				commercial="y";
+				derivatives="nd";
+				break;
+			case 5:
+				commercial="y";
+				derivatives="sa";
+				break;
+			default:
+				commercial="y";
+				derivatives="y";
+				break;
+			}
+	    }
+	    Collection collection = submission.getCollection();
+	    String actionURL = contextPath + "/handle/"+collection.getHandle() + "/submit/" + knot.getId() + ".continue";
+	    Request request = ObjectModelHelper.getRequest(objectModel);
+	    
+	    Division div = body.addInteractiveDivision("submit-cclicense", actionURL, Division.METHOD_POST, "primary submission");
+	    div.setHead(T_submission_head);
+	    addSubmissionProgressList(div);
+	    HttpSession session = request.getSession();
+        
+	    // output the license selection options
+	    List list = div.addList("licenseclasslist", List.TYPE_FORM);	    
+	    list.addItem(T_info1);
+	    list.setHead(T_head);
+	    
+	    //cargo las opciones de CC
+	    //Opcion 1
+	    List edit = div.addList("selectlist1", List.TYPE_SIMPLE, "horizontalVanilla");
+	    edit.addItem(Commercial_question);
+	    edit.addItem().addFigure(contextPath + "/themes/Reference/images/information.png", "javascript:void(0)", "El licenciador permite copiar, distribuir y comunicar públicamente la obra. A cambio, esta obra no puede ser utilizada con finalidades comerciales -- a menos que se obtenga el permiso del licenciador.", "information");
+	    List subList = div.addList("sublist2", List.TYPE_SIMPLE, "horizontalVanilla");
+	    Radio radio  = subList.addItem().addRadio("commercial_chooser");
+	    radio.setRequired();
+	    radio.addOption("nc", Commercial_question_answer_no);
+		radio.addOption("y", Commercial_question_answer_yes);
+		if (!(commercial.equals(""))){			
+			radio.setOptionSelected(commercial);
+		}
+		div.addSimpleHTMLFragment(true, "&#160;");
+		
+		edit = div.addList("selectlist2", List.TYPE_SIMPLE, "horizontalVanilla");
+		edit.addItem(Derivatives_question);
+	    edit.addItem().addFigure(contextPath + "/themes/Reference/images/information.png", "javascript:void(0)", "El licenciador permite copiar, distribuir y comunicar públicamente solamente copias inalteradas de la obra -- no obras derivadas basadas en ella.", "information");
+	    subList = div.addList("sublist2", List.TYPE_SIMPLE, "horizontalVanilla");
+	    radio  = subList.addItem().addRadio("derivatives_chooser");
+	    radio.setRequired();
+		radio.addOption("sa", Derivatives_question_answer_sa);
+		radio.addOption("nd", Derivatives_question_answer_no);
+		radio.addOption("y", Derivatives_question_answer_yes);
+		if (!(derivatives.equals(""))){			
+			radio.setOptionSelected(derivatives);
+		}
+		div.addSimpleHTMLFragment(true, "&#160;");
+
+		Division statusDivision = div.addDivision("statusDivision");
+		List statusList = statusDivision.addList("statusList", List.TYPE_FORM);
+
+		if (session.getAttribute("isFieldRequired") != null && 	
+		    session.getAttribute("isFieldRequired").equals("TRUE") && 
+		    session.getAttribute("ccError") != null) 
+		{
+		    statusList.addItem().addHighlight("error").addContent(message((String)session.getAttribute("ccError")));
+		    session.removeAttribute("ccError");
+		    session.removeAttribute("isFieldRequired");
+		} 
+
+        addControlButtons(statusList);
+        
+	}
+
+    /** 
+     * Each submission step must define its own information to be reviewed
+     * during the final Review/Verify Step in the submission process.
+     * <P>
+     * The information to review should be tacked onto the passed in 
+     * List object.
+     * <P>
+     * NOTE: To remain consistent across all Steps, you should first
+     * add a sub-List object (with this step's name as the heading),
+     * by using a call to reviewList.addList().   This sublist is
+     * the list you return from this method!
+     * 
+     * @param reviewList
+     *      The List to which all reviewable information should be added
+     * @return 
+     *      The new sub-List object created by this step, which contains
+     *      all the reviewable information.  If this step has nothing to
+     *      review, then return null!   
+     */
+    public List addReviewSection(List reviewList) throws SAXException,
+        WingException, UIException, SQLException, IOException,
+        AuthorizeException
+    {
+        //nothing to review for CC License step
+        return null;
+    }
+    
+    
+	/**
+	 * Recycle
+	 */
+	public void recycle() 
+	{
+		super.recycle();
+	}
+}
+
---
+++ xmlui/src/main/java/org/dspace/submit/step/SediciCCLicenseStep.java
@@ -0,0 +1,248 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.submit.step;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.cocoon.util.StringUtils;
+import org.apache.log4j.Logger;
+
+import org.dspace.app.util.SubmissionInfo;
+import org.dspace.app.util.Util;
+import org.dspace.app.xmlui.wing.Message;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.DCValue;
+import org.dspace.content.Item;
+import org.dspace.core.ConfigurationManager;
+import org.dspace.core.Context;
+import org.dspace.license.CreativeCommons;
+import org.dspace.license.CCLookup;
+import org.dspace.submit.AbstractProcessingStep;
+
+import com.ibm.icu.util.StringTokenizer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import org.dspace.app.xmlui.wing.Message;
+
+/**
+ * CCLicense step for DSpace Submission Process. 
+ *
+ * Processes the
+ * user response to the license.
+ * <P>
+ * This class performs all the behind-the-scenes processing that
+ * this particular step requires.  This class's methods are utilized 
+ * by both the JSP-UI and the Manakin XML-UI
+ * <P>
+ * 
+ * @see org.dspace.app.util.SubmissionConfig
+ * @see org.dspace.app.util.SubmissionStepConfig
+ * @see org.dspace.submit.AbstractProcessingStep
+ * 
+ * @author Tim Donohue
+ * @author Wendy Bossons (based on earlier CC license step work, but updated
+ * for DSpace 1.8 and CC web services api + curation)
+ * @version $Revision: 6785 $
+ */
+public class SediciCCLicenseStep extends AbstractProcessingStep
+{
+    /***************************************************************************
+     * STATUS / ERROR FLAGS (returned by doProcessing() if an error occurs or
+     * additional user interaction may be required)
+     * 
+      * (Do NOT use status of 0, since it corresponds to STATUS_COMPLETE flag
+     * defined in the JSPStepManager class)
+     **************************************************************************/
+    // user rejected the license
+    public static final int STATUS_LICENSE_REJECTED = 1;
+
+    /** log4j logger */
+    private static Logger log = Logger.getLogger(CCLicenseStep.class);
+    
+
+    
+    /**
+     * Do any processing of the information input by the user, and/or perform
+     * step processing (if no user interaction required)
+     * <P>
+     * It is this method's job to save any data to the underlying database, as
+     * necessary, and return error messages (if any) which can then be processed
+     * by the appropriate user interface (JSP-UI or XML-UI)
+     * <P>
+     * NOTE: If this step is a non-interactive step (i.e. requires no UI), then
+     * it should perform *all* of its processing in this method!
+     * 
+     * @param context
+     *            current DSpace context
+     * @param request
+     *            current servlet request object
+     * @param response
+     *            current servlet response object
+     * @param subInfo
+     *            submission info object
+     * @return Status or error flag which will be processed by
+     *         doPostProcessing() below! (if STATUS_COMPLETE or 0 is returned,
+     *         no errors occurred!)
+     */
+    public int doProcessing(Context context, HttpServletRequest request,
+            HttpServletResponse response, SubmissionInfo subInfo)
+            throws ServletException, IOException, SQLException,
+            AuthorizeException,java.io.IOException
+    {
+	    HttpSession session = request.getSession();		
+        String buttonPressed = Util.getSubmitButton(request, NEXT_BUTTON);
+        
+        if (buttonPressed.equals("submit_grant"))
+        {
+            return processSediciCC(context, request, response, subInfo);
+        }        
+        
+        if (buttonPressed.startsWith(PROGRESS_BAR_PREFIX) || buttonPressed.equals(PREVIOUS_BUTTON))
+        {
+            removeRequiredAttributes(session);
+        }
+        if (buttonPressed.equals(NEXT_BUTTON) || buttonPressed.equals(CANCEL_BUTTON)  || buttonPressed.equals(PREVIOUS_BUTTON))
+        {
+            return processSediciCC(context, request, response, subInfo);
+        }
+        else
+        {  
+            removeRequiredAttributes(session);
+            //session.removeAttribute("inProgress");
+            return STATUS_COMPLETE;
+        }
+    }
+
+
+    /**
+     * Process the input from the CC license page using CC Web service
+     * 
+     * @param context
+     *            current DSpace context
+     * @param request
+     *            current servlet request object
+     * @param response
+     *            current servlet response object
+     * @param subInfo
+     *            submission info object
+     *
+     * 
+     * @return Status or error flag which will be processed by
+     *         doPostProcessing() below! (if STATUS_COMPLETE or 0 is returned,
+     *         no errors occurred!)
+     */
+    protected int processSediciCC(Context context, HttpServletRequest request,
+            HttpServletResponse response, SubmissionInfo subInfo)
+            throws ServletException, IOException, SQLException,
+            AuthorizeException {
+       
+        HttpSession session = request.getSession();
+        	
+    	String jurisdictionId = (ConfigurationManager.getProperty("cc.license.jurisdiction") != null) ? ConfigurationManager.getProperty("cc.license.jurisdiction") : "";
+    	String jurisdictionDescription = (ConfigurationManager.getProperty("cc.license.jurisdiction.description") != null) ? ConfigurationManager.getProperty("cc.license.jurisdiction.description") : "";
+        String commercial = request.getParameter("commercial_chooser");
+        String derivatives = request.getParameter("derivatives_chooser");
+        
+        if (commercial!=null && derivatives!=null){
+        
+	    	String[] uriField=(ConfigurationManager.getProperty("cc.license.uri")).split("\\.");
+	        String uriFieldSchema=(uriField.length>0)?uriField[0]:null;
+	    	String uriFieldElement=(uriField.length>1)?uriField[1]:null;
+	    	String uriFieldQualifier=(uriField.length>2)?uriField[2]:null;
+	    	
+	    	String[] uriName=(ConfigurationManager.getProperty("cc.license.name")).split("\\.");
+	    	String uriNameSchema=(uriName.length>0)?uriName[0]:null;
+	    	String uriNameElement=(uriName.length>1)?uriName[1]:null;
+	    	String uriNameQualifier=(uriName.length>2)?uriName[2]:null;
+	
+	    	Item item = subInfo.getSubmissionItem().getItem();
+	    	item.clearMetadata(uriFieldSchema, uriFieldElement, uriFieldQualifier, null);
+	    	item.clearMetadata(uriNameSchema, uriNameElement, uriNameQualifier, null);
+	    	
+	    	//cargo los nuevos valores para el metadata 
+	    	String licenseUri="http://creativecommons.org/licenses/by";
+	    	String licenseDescription="Atribución";
+	    	
+	    	if (!commercial.equals("y")){
+	    		licenseUri=licenseUri+"-"+commercial; 
+	    		licenseDescription=licenseDescription+"-NoComercial";
+	    	}
+	    	if (!derivatives.equals("y")){
+	    		licenseUri=licenseUri + "-" + derivatives; 
+	    		if (derivatives=="nd"){
+	    			licenseDescription=licenseDescription+"-SinDerivadas";
+	    		} else {
+	    			licenseDescription=licenseDescription+"-CompartirIgual";
+	    		}
+	    	};
+	    	licenseUri=licenseUri+"/2.5/";
+	    	licenseDescription=licenseDescription+" 2.5";
+	    	if (!jurisdictionId.equals("")){
+	    		licenseUri=licenseUri+jurisdictionId+"/";
+	    		licenseDescription=licenseDescription+" "+ jurisdictionDescription;
+	    	};
+	    	item.addMetadata(uriFieldSchema, uriFieldElement, uriFieldQualifier, null, licenseUri);
+	    	item.addMetadata(uriNameSchema, uriNameElement, uriNameQualifier, null, licenseDescription);
+	    	
+			item.update();
+			context.commit();
+			removeRequiredAttributes(session);
+			return STATUS_COMPLETE;
+       } else {
+    	   request.getSession().setAttribute("ccError", "xmlui.Submission.submit.SediciCCLicenseStep.campos_obligatorios");
+    	   session.setAttribute("isFieldRequired", "TRUE");
+    	   return STATUS_LICENSE_REJECTED;
+       }
+
+    	
+    }
+	
+	private void removeRequiredAttributes(HttpSession session) {
+		session.removeAttribute("ccError");
+		session.removeAttribute("isFieldRequired");
+	}
+     
+     
+     /**
+     * Retrieves the number of pages that this "step" extends over. This method
+     * is used to build the progress bar.
+     * <P>
+     * This method may just return 1 for most steps (since most steps consist of
+     * a single page). But, it should return a number greater than 1 for any
+     * "step" which spans across a number of HTML pages. For example, the
+     * configurable "Describe" step (configured using input-forms.xml) overrides
+     * this method to return the number of pages that are defined by its
+     * configuration file.
+     * <P>
+     * Steps which are non-interactive (i.e. they do not display an interface to
+     * the user) should return a value of 1, so that they are only processed
+     * once!
+     * 
+     * @param request
+     *            The HTTP Request
+     * @param subInfo
+     *            The current submission information object
+     * 
+     * @return the number of pages in this step
+     */
+    public int getNumberOfPages(HttpServletRequest request,
+            SubmissionInfo subInfo) throws ServletException
+    {
+		return 1;
+    }
+   
+}
