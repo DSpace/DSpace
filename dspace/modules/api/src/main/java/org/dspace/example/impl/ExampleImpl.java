eacbec71740f719b763cf7d5033e9f2777e02cd2
Mark Diggory <mdiggory@gmail.com>
2012-04-19 23:49:10 -0700

    [DS-981] API Project and build assembly restructure

---
+++ dspace/modules/api/src/main/java/org/dspace/example/impl/ExampleImpl.java
@@ -0,0 +1,11 @@
+package org.dspace.example.impl;
+
+import org.dspace.example.Example;
+
+/**
+ * TODO: Add Description
+ *
+ * @author mdiggory @ atmire.com
+ */
+public class ExampleImpl implements Example {
+}
