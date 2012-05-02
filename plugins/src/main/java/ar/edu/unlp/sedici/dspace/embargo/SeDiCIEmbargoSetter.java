e9b832d5a4540f4d854829f685b6b9957f4382cb
Ariel Lira <arieljlira@gmail.com>
2012-05-02 18:23:48 +0000

    cambio el embargo setter para que cuando se procese un doc importado, se tome como inicio del embargo la fecha de creacion #1573

--- plugins/src/main/java/ar/edu/unlp/sedici/dspace/embargo/SeDiCIEmbargoSetter.java
+++ plugins/src/main/java/ar/edu/unlp/sedici/dspace/embargo/SeDiCIEmbargoSetter.java
@@ -1,11 +1,14 @@
 package ar.edu.unlp.sedici.dspace.embargo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.log4j.Logger;
 import org.dspace.content.DCDate;
 import org.dspace.content.DCValue;
 import org.dspace.content.Item;
+import org.dspace.curate.Curator;
 
 /**
 
@@ -13,6 +16,8 @@ import org.dspace.content.Item;
 public class SeDiCIEmbargoSetter extends DaysEmbargoSetter {
 
 	private static Logger log = Logger.getLogger(SeDiCIEmbargoSetter.class);
+	private static SimpleDateFormat sediciDatetimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	private static SimpleDateFormat sediciDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
 	private static String exposureDateMetadata = "sedici.date.exposure";
 	private static String typeMetadata = "dc.type";
@@ -25,6 +30,20 @@ public class SeDiCIEmbargoSetter extends DaysEmbargoSetter {
 
 	protected Date getEmbargoStartDate(Item item) {
 		DCValue docTypes[] = item.getMetadata(typeMetadata);
+		
+		if (item.getMetadata("sedici2003.identifier").length != 0){
+			//Es un doc importado
+			DCValue embargosViejos[] = item.getMetadata("sedici2003.fecha-creacion");
+			if (embargosViejos.length > 0){
+				try{
+        			return new DCDate(sediciDatetimeFormat.parse(embargosViejos[0].value)).toDate();
+        		}catch(ParseException e){
+        			log.warn("Error de parseo de fecha al procesar (sedici2003.fecha-creacion)==("+embargosViejos[0].value+") del documento importado con id "+item.getMetadata("sedici2003.identifier")[0]);
+                } 
+			}
+			
+			throw new IllegalArgumentException("No se pudo procesar la fecha de creacion (sedici2003.fecha-creacion) del documento importado con id "+item.getMetadata("sedici2003.identifier")[0].value);
+		}
 		
 		if (docTypes.length == 0){
 			log.info("No se encontro un type para el doc "+item.getHandle());
@@ -43,4 +62,6 @@ public class SeDiCIEmbargoSetter extends DaysEmbargoSetter {
 		
 		return new DCDate(exposureDates[0].value).toDate();
 	}
+	
+	
 }
