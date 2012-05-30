7c6313e6f9f53c0a5b78d7c9c5ec545f6530c0eb
Nicolás Romagnoli <romagnoli.nicolas@gmail.com>
2012-05-30 14:56:31 +0000

    Bug #1710
    - Se modificó el selectCollectionStep para que ver las colecciones de una comunidad muestre las colecciones de las subcomunidades tambien

--- xmlui/src/main/java/org/dspace/content/CollectionSearchSedici.java
+++ xmlui/src/main/java/org/dspace/content/CollectionSearchSedici.java
@@ -1,3 +1,4 @@
+
 package org.dspace.content;
 
 import java.sql.SQLException;
@@ -19,68 +20,86 @@ public class CollectionSearchSedici {
     public static CollectionsWithCommunities findAuthorizedWithCommunitiesName(Context context, Community comm,
             int actionID) throws java.sql.SQLException
     {
-        List<Collection> collections = new ArrayList<Collection>();
-        List<String> communitiesName = new ArrayList<String>();
-        	//Son las colecciones de todas las comunidades las que tengo que filtrar
-        TableRowIterator tri;
+
 		try {
-			if (comm==null){
+			if (comm!=null){
+				return CollectionSearchSedici.findAuthorizedWithCommunitiesNameRecursive(context, comm, actionID);
+			} else {
+		        List<Collection> collections = new ArrayList<Collection>();
+		        List<String> communitiesName = new ArrayList<String>();
+		        TableRowIterator tri;		        
+
 				tri = DatabaseManager.queryTable(context,null,
 				                "SELECT collection.*, community.name as community_name FROM collection, community, community2collection WHERE " +
 				                "community.community_id=community2collection.community_id "+
 				                " AND collection.collection_id=community2collection.collection_id" +
 				                " ORDER BY community_name, collection.name"
 				                );
-			} else {
-				tri = DatabaseManager.queryTable(context,null,
-		                "SELECT collection.*, community.name as community_name FROM collection, community, community2collection WHERE " +
-		                " AND community2collection.community_id = "+comm.getID()+
-		                " AND community.community_id=community2collection.community_id "+		                
-		                " AND collection.collection_id=community2collection.collection_id" +
-		                " ORDER BY community_name, collection.name"
-		                );
+	
+		        // Build a list of Community objects
+		        Collection fromCache;
+		        String communityName;
+		        try
+		        {
+		            while (tri.hasNext())
+		            {
+		                TableRow row = tri.next();
+		
+		                // First check the cache
+		                fromCache = (Collection) context.fromCache(
+		                        Collection.class, row.getIntColumn("collection_id"));
+		                communityName=row.getStringColumn("community_name");
+		
+		                if (fromCache == null)
+		                {
+		                	fromCache=new Collection(context, row);
+		                }
+		            	if (AuthorizeManager.authorizeActionBoolean(context, fromCache, actionID)){	    					
+		                	collections.add(fromCache);
+		                    communitiesName.add(communityName);
+		            	}                
+		                
+		            }
+		        }
+		        finally
+		        {
+		            // close the TableRowIterator to free up resources
+		            if (tri != null)
+		            {
+		                tri.close();
+		            }
+		        }
+		        return new CollectionsWithCommunities(collections, communitiesName);
 			}
-
-
-        // Build a list of Community objects
-        Collection fromCache;
-        String communityName;
-        try
-        {
-            while (tri.hasNext())
-            {
-                TableRow row = tri.next();
-
-                // First check the cache
-                fromCache = (Collection) context.fromCache(
-                        Collection.class, row.getIntColumn("collection_id"));
-                communityName=row.getStringColumn("community_name");
-
-                if (fromCache == null)
-                {
-                	fromCache=new Collection(context, row);
-                }
-            	if (AuthorizeManager.authorizeActionBoolean(context, fromCache, actionID)){	    					
-                	collections.add(fromCache);
-                    communitiesName.add(communityName);
-            	}
-                
-                
-            }
-        }
-        finally
-        {
-            // close the TableRowIterator to free up resources
-            if (tri != null)
-            {
-                tri.close();
-            }
-        }
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw e;
 		}
 
+
+	}
+    
+    public static CollectionsWithCommunities findAuthorizedWithCommunitiesNameRecursive(Context context, Community comm,
+            int actionID) throws java.sql.SQLException
+    {
+        List<Collection> collections = new ArrayList<Collection>();
+        List<String> communitiesName = new ArrayList<String>();
+        Collection[] colecciones=comm.getCollections();
+        Community[] subcomunidades=comm.getSubcommunities();
+        String communityName=comm.getName();
+        CollectionsWithCommunities recursividad;
+        for (Collection coleccion : colecciones) {
+        	if (AuthorizeManager.authorizeActionBoolean(context, coleccion, actionID)){
+        		collections.add(coleccion);
+        		communitiesName.add(communityName);
+        	}    
+		}
+        for (Community community : subcomunidades) {
+			recursividad=CollectionSearchSedici.findAuthorizedWithCommunitiesNameRecursive(context, community, actionID);
+			collections.addAll(recursividad.getCollections());
+			communitiesName.addAll(recursividad.getCommunitiesName());
+		}
         return new CollectionsWithCommunities(collections, communitiesName);
 	}
 	
