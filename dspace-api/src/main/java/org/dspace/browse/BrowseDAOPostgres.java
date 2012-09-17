fe1fb3081031c04976f038230b806ec239cbb8ff
EKT <kstamatis@ekt.gr>
2012-09-17 13:05:38 +0300

    Fix oracle conflict with "values" name, renamed it to dvalues

--- dspace-api/src/main/java/org/dspace/browse/BrowseDAOPostgres.java
+++ dspace-api/src/main/java/org/dspace/browse/BrowseDAOPostgres.java
@@ -787,9 +787,9 @@ public class BrowseDAOPostgres implements BrowseDAO
 
         //If we want frequencies and this is not a count query, enchance the query accordingly
         if (isEnableBrowseFrequencies() && countValues==null){
-            String before = "SELECT count(*) AS num, values.value, values.authority, values."+orderField+" FROM (";
-            String after = ") values , "+tableMap+" WHERE values.id = "+tableMap+".distinct_id GROUP BY "+tableMap+
-                    ".distinct_id, values.value, values.authority, values.sort_value";
+            String before = "SELECT count(*) AS num, dvalues.value, dvalues.authority FROM (";
+ 	        String after = ") dvalues , "+tableMap+" WHERE dvalues.id = "+tableMap+".distinct_id GROUP BY "+tableMap+
+ 	 	            ".distinct_id, dvalues.value, dvalues.authority, dvalues.sort_value";
             
             queryBuf.insert(0, before);
             queryBuf.append(after);
