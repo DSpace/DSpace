8d6f9c3c3c06a0999b854250443ebd0b44e6dead
Ariel Lira <arieljlira@gmail.com>
2012-10-22 16:02:37 +0000

    Faltaba enviar el context al MailReporter desde el ExceptionAction #2056

--- xmlui/src/main/java/ar/edu/unlp/sedici/aspect/redirect/ExceptionAction.java
+++ xmlui/src/main/java/ar/edu/unlp/sedici/aspect/redirect/ExceptionAction.java
@@ -16,6 +16,7 @@
 package ar.edu.unlp.sedici.aspect.redirect;
 
 import java.io.PrintStream;
+import java.sql.SQLException;
 import java.util.Map;
 
 import org.apache.avalon.framework.parameters.Parameters;
@@ -26,29 +27,40 @@ import org.apache.cocoon.environment.Request;
 import org.apache.cocoon.environment.SourceResolver;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.dspace.app.xmlui.utils.ContextUtil;
+import org.dspace.core.Context;
 
 import ar.edu.unlp.sedici.dspace.utils.MailReporter;
 
-
 /**
- * Clase encargada de reportar las excepciones que se detectan durante la ejecucion de un pipeline cocoon
+ * Clase encargada de reportar las excepciones que se detectan durante la
+ * ejecucion de un pipeline cocoon
  */
 public class ExceptionAction extends AbstractAction {
 	public Map act(Redirector redirector, SourceResolver resolver, Map objectModel, String source, Parameters parameters) {
-		
-			Request request = ObjectModelHelper.getRequest(objectModel);
-			String url = request.getRequestURI();
-			ByteArrayOutputStream os = new ByteArrayOutputStream();
-			MapUtils.verbosePrint(new PrintStream(os), "Parameters:", request.getParameters());
-			
-			Throwable thr = ObjectModelHelper.getThrowable(objectModel);
-			if (thr == null) {
-				this.getLogger().error("En teoria, se produjo una exception, pero no se puede recuperar la excepcion para imprimir su stack trace. Es raro");
-			}else{
-				MailReporter.reportUnknownException("Error en xmlui", thr, url,os.toString());
-			}
+
+		Context context;
+		try {
+			context = ContextUtil.obtainContext(objectModel);
+		} catch (SQLException e) {
+			this.getLogger().error("Se produjo un error al levantar el context desde ExceptionAction", e);
+			return null;
+		}
+
+		Request request = ObjectModelHelper.getRequest(objectModel);
+		String url = request.getRequestURI();
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		MapUtils.verbosePrint(new PrintStream(os), "Parameters:", request.getParameters());
+
+		Throwable thr = ObjectModelHelper.getThrowable(objectModel);
+		if (thr == null) {
+			this.getLogger().error(
+					"En teoria, se produjo una exception, pero no se puede recuperar la excepcion para imprimir su stack trace. Es raro");
+		} else {
+			MailReporter.reportUnknownException(context, "Error en xmlui", thr, url, os.toString());
+		}
 		return null;
-		
+
 	}
 
 }
