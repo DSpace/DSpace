d11c3cf29f9bc0ee1fffffe72a079d3b5128fb06
Mark H. Wood <mwood@IUPUI.Edu>
2012-11-09 15:59:07 -0500

    License headers

--- dspace-api/src/main/java/org/dspace/identifier/ezid/package-info.java
+++ dspace-api/src/main/java/org/dspace/identifier/ezid/package-info.java
@@ -1,4 +1,11 @@
 /**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+/**
  * Make requests to the <a href='http://n2t.net/ezid/'>EZID</a> DOI service,
  * and analyze the responses.
  * 
