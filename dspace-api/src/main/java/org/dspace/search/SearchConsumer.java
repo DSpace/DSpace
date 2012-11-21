d9df145a4e9628fcb649d8bdeb7454085ae2cc53
KevinVdV <kevin@mire.be>
2012-11-21 10:51:39 +0100

    [DS-1345, DS-1346] Item Level Versioning, all versions are searchable & recent submission list in collections shows all the versions

--- dspace-api/src/main/java/org/dspace/search/SearchConsumer.java
+++ dspace-api/src/main/java/org/dspace/search/SearchConsumer.java
@@ -17,6 +17,7 @@ import org.dspace.core.Constants;
 import org.dspace.core.Context;
 import org.dspace.event.Consumer;
 import org.dspace.event.Event;
+import org.dspace.handle.HandleManager;
 
 /**
  * Class for updating search indices from content events.
