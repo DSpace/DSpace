5c1f99d0389191d4dfff4339220f405cdf4c8f38
Mark H. Wood <mwood@IUPUI.Edu>
2013-01-18 19:48:07 -0500

    Add the Version class to report on DSpace's version, build environment, and runtime environment.

---
+++ dspace-api/src/main/java/org/dspace/app/util/Version.java
@@ -0,0 +1,72 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.dspace.core.ConfigurationManager;
+
+/**
+ * Display information about this DSpace, its environment, and how it was built.
+ *
+ * @author mwood
+ */
+public class Version
+{
+    public static void main(String[] argv)
+        throws IOException
+    {
+        Properties sys = System.getProperties();
+
+        // DSpace version
+        System.out.printf("DSpace version:  %s\n",
+                          Util.getSourceVersion());
+
+        // OS version
+        System.out.printf("            OS:  %s(%s) version %s\n",
+                          sys.get("os.name"),
+                          sys.get("os.arch"),
+                          sys.get("os.version"));
+
+        // TODO UIs used
+
+        // TODO is Discovery available?
+
+        // Java version
+        System.out.printf("           JRE:  %s version %s\n",
+                          sys.get("java.vendor"),
+                          sys.get("java.version"));
+
+        InputStream propStream;
+
+        // ant version
+        Properties ant = new Properties();
+        propStream = Version.class.getResourceAsStream("/ant.properties");
+        if (null != propStream)
+        {
+            ant.load(propStream);
+        }
+        System.out.printf("   Ant version:  %s\n",
+                          ant.get("ant.version"));
+
+        // maven version
+        Properties maven = new Properties();
+        propStream = Version.class.getResourceAsStream("/maven.properties");
+        if (null != propStream)
+        {
+            maven.load(propStream);
+        }
+        System.out.printf(" Maven version:  %s\n",
+                          maven.get("maven.version"));
+
+        // DSpace directory path
+        System.out.printf("   DSpace home:  %s\n",
+                          ConfigurationManager.getProperty("dspace.dir"));
+    }
+}
