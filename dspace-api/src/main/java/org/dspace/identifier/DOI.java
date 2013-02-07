264de565ca85fb19a0e66bebe279abd737e5cba1
Pascal-Nicolas Becker <pnb@pascal-becker.de>
2013-02-07 19:45:17 +0100

    Continued work on DOIDataCiteIdentifier.  * Added a lot of functionality to DOIDataCiteIdentifierProvider.  * Added a table Doi to the database.  * Added use of Database table doi to DOIDataCiteIdentifierProvider.  * Moved DOIDataCiteIdentifierProvider.DOIToExternalForm(String) to DOI.DOIToExterenalForm.

--- dspace-api/src/main/java/org/dspace/identifier/DOI.java
+++ dspace-api/src/main/java/org/dspace/identifier/DOI.java
@@ -17,4 +17,37 @@ public class DOI
         implements Identifier
 {
     public static final String SCHEME = "doi:";
+    
+    
+    /**
+     * This method helps to convert a DOI into a URL. It takes DOIs in one of
+     * the following formats  and returns it as URL (f.e. 
+     * http://dx.doi.org/10.123/456). Allowed formats are:
+     * <ul>
+     *   <li>doi:10.123/456</li>
+     *   <li>10.123/456</li>
+     *   <li>http://dx.doi.org/10.123/456</li>
+     * </ul>
+     * 
+     * @param identifier  A DOI that should be returned in external form.
+     * @return A String containing a URL to the official DOI resolver.
+     * @throws IllegalArgumentException If identifier is null or an empty String.
+     * @throws IdentifierException If identifier could not be recognized as valid DOI.
+     */
+    public static String DOIToExternalForm(String identifier)
+            throws IdentifierException
+    {
+        if (null == identifier) 
+            throw new IllegalArgumentException("Identifier is null.", new NullPointerException());
+        if (identifier.isEmpty())
+            throw new IllegalArgumentException("Cannot format an empty identifier.");
+        if (identifier.startsWith(SCHEME))
+            return "http://dx.doi.org/" + identifier.substring(SCHEME.length());
+        if (identifier.startsWith("10.") && identifier.contains("/"))
+            return "http://dx.doi.org/" + identifier;
+        if (identifier.startsWith("http://dx.doi.org/10."))
+            return identifier;
+        
+        throw new IdentifierException(identifier + "does not seem to be a DOI.");
+    }
 }
