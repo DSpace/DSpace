0eb9ca3b055b2f34fbe9aabd6458fe4cd45e4ad5
aroman <aroman@arvo.es>
2013-07-22 12:29:57 +0200

    Request copy of protected item.

---
+++ dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/ItemRequestResponseTrueForm.java
@@ -0,0 +1,115 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ * 
+ * Original Concept, JSPUI version:    Universidade do Minho   at www.uminho.pt
+ * Sponsorship of XMLUI version:    Instituto Oceanográfico de España at www.ieo.es
+ */
+package org.dspace.app.xmlui.aspect.artifactbrowser;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.sql.SQLException;
+
+import org.apache.cocoon.caching.CacheableProcessingComponent;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.util.HashUtil;
+import org.apache.excalibur.source.SourceValidity;
+import org.apache.excalibur.source.impl.validity.NOPValidity;
+import org.dspace.app.xmlui.cocoon.AbstractDSpaceTransformer;
+import org.dspace.app.xmlui.utils.UIException;
+import org.dspace.app.xmlui.wing.Message;
+import org.dspace.app.xmlui.wing.WingException;
+import org.dspace.app.xmlui.wing.element.Body;
+import org.dspace.app.xmlui.wing.element.Division;
+import org.dspace.app.xmlui.wing.element.List;
+import org.dspace.app.xmlui.wing.element.PageMeta;
+import org.dspace.app.xmlui.wing.element.TextArea;
+import org.dspace.authorize.AuthorizeException;
+import org.xml.sax.SAXException;
+
+/**
+ * Display to the user a simple form letting the user send a document with a message.
+ * 
+ * @author Adán Román Ruiz at arvo.es
+ */
+public class ItemRequestResponseTrueForm extends AbstractDSpaceTransformer implements CacheableProcessingComponent
+{
+    /** Language Strings */
+    private static final Message T_title =
+        message("xmlui.ArtifactBrowser.ItemRequestResponseTrueForm.title");
+    
+    private static final Message T_dspace_home =
+        message("xmlui.general.dspace_home");
+    
+    private static final Message T_trail =
+        message("xmlui.ArtifactBrowser.ItemRequestResponseTrueForm.trail");
+    
+    private static final Message T_head = 
+        message("xmlui.ArtifactBrowser.ItemRequestResponseTrueForm.head");
+    
+    private static final Message T_para1 =
+        message("xmlui.ArtifactBrowser.ItemRequestResponseTrueForm.para1");
+    
+    private static final Message T_mail =
+        message("xmlui.ArtifactBrowser.ItemRequestResponseTrueForm.mail");
+
+    private static final Message T_back =
+        message("xmlui.ArtifactBrowser.ItemRequestResponseTrueForm.back");
+    
+    private static final Message T_message = 
+        message("xmlui.ArtifactBrowser.ItemRequestResponseTrueForm.message");
+        
+    /**
+     * Generate the unique caching key.
+     * This key must be unique inside the space of this component.
+     */
+    public Serializable getKey() {      
+		String token = parameters.getParameter("token", "");
+		String decision = parameters.getParameter("decision", "");
+
+		return HashUtil.hash(token+"-"+decision);
+    }
+
+    /**
+     * Generate the cache validity object.
+     */
+    public SourceValidity getValidity() 
+    {
+        return NOPValidity.SHARED_INSTANCE;
+    }
+    
+    public void addPageMeta(PageMeta pageMeta) throws SAXException,
+            WingException, UIException, SQLException, IOException,
+            AuthorizeException
+    {       
+        pageMeta.addMetadata("title").addContent(T_title);
+ 
+        pageMeta.addTrailLink(contextPath + "/",T_dspace_home);
+        pageMeta.addTrail().addContent(T_trail);
+    }
+
+    public void addBody(Body body) throws SAXException, WingException,
+            UIException, SQLException, IOException, AuthorizeException
+    {
+    	Request request = ObjectModelHelper.getRequest(objectModel);
+        
+        Division itemRequest = body.addInteractiveDivision("itemRequest-form", request.getRequestURI(),Division.METHOD_POST,"primary");
+        itemRequest.setHead(T_head);
+        
+        itemRequest.addPara(T_para1);
+                
+        List form = itemRequest.addList("form",List.TYPE_FORM);
+                
+        TextArea message = form.addItem().addTextArea("message");
+        message.setLabel(T_message);
+        message.setValue(parameters.getParameter("message",""));
+        form.addItem().addHidden("decision").setValue(parameters.getParameter("decision",""));
+        form.addItem().addButton("back").setValue(T_back);
+        form.addItem().addButton("mail").setValue(T_mail);
+    }
+}
