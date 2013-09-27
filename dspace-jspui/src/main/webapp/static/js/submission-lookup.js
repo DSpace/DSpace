c88663c097ad09764a6cc9f6ee39cf424389b07b
Pascarelli Luigi Andrea <l.pascarelli@cineca.it>
2013-09-27 09:19:37 +0200

    Added license

--- dspace-jspui/src/main/webapp/static/js/submission-lookup.js
+++ dspace-jspui/src/main/webapp/static/js/submission-lookup.js
@@ -1,3 +1,10 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 submissionLookupIdentifiers = function(identInputs){
 	var mydata = new Object();
 	mydata['s_uuid'] = j('#suuid').val();
