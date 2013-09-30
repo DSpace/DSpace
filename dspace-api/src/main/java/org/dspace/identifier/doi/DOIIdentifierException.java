cd2f2feffaef3999b10b4235db8aacc9e6fb374a
Marsa Haoua <haoueclf@mailbox.tu-berlin.de>
2013-09-30 11:56:58 +0200

    Added license header to DOIIdentifierException.

--- dspace-api/src/main/java/org/dspace/identifier/doi/DOIIdentifierException.java
+++ dspace-api/src/main/java/org/dspace/identifier/doi/DOIIdentifierException.java
@@ -1,6 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
  */
 package org.dspace.identifier.doi;
 
