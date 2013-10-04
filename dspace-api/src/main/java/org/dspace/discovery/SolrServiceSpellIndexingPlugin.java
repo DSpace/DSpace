7dbd71543b9e98b404b2405941bbb5b22204dc3f
KevinVdV <kevin@mire.be>
2013-10-04 10:26:38 +0200

    [DS-1683] Add spell checker to discovery

---
+++ dspace-api/src/main/java/org/dspace/discovery/SolrServiceSpellIndexingPlugin.java
@@ -0,0 +1,29 @@
+package org.dspace.discovery;
+
+import org.apache.solr.common.SolrInputDocument;
+import org.dspace.content.DCValue;
+import org.dspace.content.DSpaceObject;
+import org.dspace.content.Item;
+import org.dspace.core.Context;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kevin
+ * Date: 03/10/13
+ * Time: 15:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class SolrServiceSpellIndexingPlugin implements SolrServiceIndexPlugin {
+
+    @Override
+    public void additionalIndex(Context context, DSpaceObject dso, SolrInputDocument document) {
+        if(dso instanceof Item){
+            DCValue[] dcValues = ((Item) dso).getMetadata(Item.ANY, Item.ANY, Item.ANY, Item.ANY);
+            for (DCValue dcValue : dcValues) {
+                document.addField("a_spell", dcValue.value);
+
+            }
+        }
+
+    }
+}
