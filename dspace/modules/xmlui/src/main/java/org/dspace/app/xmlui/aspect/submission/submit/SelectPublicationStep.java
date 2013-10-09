827af23da356397437815b8b72e3f9319981e29a
lantian <lantian@atmire.com>
2013-10-09 14:32:39 -0700

    atmire-1691

--- dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/SelectPublicationStep.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/SelectPublicationStep.java
@@ -131,6 +131,10 @@ public class SelectPublicationStep extends AbstractSubmissionStep {
 
         // retrieve request parameters: journlaID, manuscriptNumber
         String selectedJournalId = request.getParameter("journalID");
+        if(selectedJournalId!=null)
+        {
+            selectedJournalId = selectedJournalId.toLowerCase();
+        }
         String manuscriptNumber = request.getParameter("manu");
 	log.debug("initializing submission UI for journal " + selectedJournalId + ", manu " + manuscriptNumber);
         PublicationBean pBean = null;
@@ -370,6 +374,10 @@ public class SelectPublicationStep extends AbstractSubmissionStep {
                 }
                 else if(request.getParameter("journalIDStatusInReview")!=null&&!request.getParameter("journalIDStatusInReview").equals("")){
                     selectedJournalId = request.getParameter("journalIDStatusInReview");
+                    if(selectedJournalId!=null)
+                    {
+                        selectedJournalId = selectedJournalId.toLowerCase();
+                    }
                     journalID.addOption(val.equals(selectedJournalId), val, name);
                 }
                 else{
