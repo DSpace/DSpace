f2d77ef872b3be5040266776aaab1d9873db2330
Ryan Scherle <ryan@scherle.org>
2013-10-15 10:33:39 -0400

    Initial framework for Odin's Hamr

---
+++ dspace/modules/api/src/main/java/org/dspace/curate/OdinsHamr.java
@@ -0,0 +1,208 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.curate;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import org.dspace.handle.HandleManager;
+import org.dspace.app.util.DCInput;
+import org.dspace.app.util.DCInputSet;
+import org.dspace.app.util.DCInputsReader;
+import org.dspace.app.util.DCInputsReaderException;
+import org.dspace.content.DCValue;
+import org.dspace.content.DSpaceObject;
+import org.dspace.content.Item;
+import org.dspace.content.Bundle;
+import org.dspace.content.Bitstream;
+import org.dspace.content.crosswalk.MetadataValidationException;
+import org.dspace.core.ConfigurationManager;
+import org.dspace.core.Context;
+import org.dspace.core.Constants;
+import org.dspace.identifier.IdentifierService;
+import org.dspace.identifier.IdentifierNotFoundException;
+import org.dspace.identifier.IdentifierNotResolvableException;
+import org.dspace.utils.DSpace;
+
+import org.apache.log4j.Logger;
+
+/**
+ * OdinsHamr helps users reconcile author metadata with metadata stored in ORCID.
+ *
+ * Input: a single DSpace item OR a collection
+ * Output: CSV file that maps author names in the DSpace item to author names in ORCID.
+ * @author Ryan Scherle
+ */
+@Suspendable
+public class OdinsHamr extends AbstractCurationTask {
+
+    private static Logger log = Logger.getLogger(OdinsHamr.class);
+    private IdentifierService identifierService = null;
+    DocumentBuilderFactory dbf = null;
+    DocumentBuilder docb = null;
+    static long total = 0;
+    private Context context;
+    
+    @Override 
+    public void init(Curator curator, String taskId) throws IOException {
+        super.init(curator, taskId);
+	
+        identifierService = new DSpace().getSingletonService(IdentifierService.class);            
+	
+	// init xml processing
+	try {
+	    dbf = DocumentBuilderFactory.newInstance();
+	    docb = dbf.newDocumentBuilder();
+	} catch (ParserConfigurationException e) {
+	    throw new IOException("unable to initiate xml processor", e);
+	}
+    }
+    
+    /**
+     * Perform the curation task upon passed DSO
+     *
+     *	input: an item
+     *  output: several lines of authors, can show all or only identified matches
+     *
+     * @param dso the DSpace object
+     * @throws IOException
+     */
+    @Override
+    public int perform(DSpaceObject dso) throws IOException {
+	log.info("performing ODIN's Hamr task " + total++ );
+
+	String handle = "\"[no handle found]\"";
+	String itemDOI = "\"[no item DOI found]\"";
+	String articleDOI = "\"[no article DOI found]\"";
+		
+	try {
+	    context = new Context();
+        } catch (SQLException e) {
+	    log.fatal("Unable to open database connection", e);
+	    return Curator.CURATE_FAIL;
+	}
+	
+	if (dso.getType() == Constants.COLLECTION) {
+	    // output headers for the CSV file that will be created by processing all items in this collection
+	    report("itemDOI, articleDOI, orcidID, orcidName, dryadORCID, dryadName");
+	} else if (dso.getType() == Constants.ITEM) {
+            Item item = (Item)dso;
+
+	    try {
+		handle = item.getHandle();
+		log.info("handle = " + handle);
+		
+		if (handle == null) {
+		    // this item is still in workflow - no handle assigned
+		    context.abort(); 
+		    return Curator.CURATE_SKIP;
+		}
+		
+		// item DOI
+		DCValue[] vals = item.getMetadata("dc.identifier");
+		if (vals.length == 0) {
+		    setResult("Object has no dc.identifier available " + handle);
+		    log.error("Skipping -- no dc.identifier available for " + handle);
+		    context.abort(); 
+		    return Curator.CURATE_SKIP;
+		} else {
+		    for(int i = 0; i < vals.length; i++) {
+			if (vals[i].value.startsWith("doi:")) {
+			    itemDOI = vals[i].value;
+			}
+		    }
+		}
+		log.debug("itemDOI = " + itemDOI);
+
+		// article DOI
+		vals = item.getMetadata("dc.relation.isreferencedby");
+		if (vals.length == 0) {
+		    log.debug("Object has no articleDOI (dc.relation.isreferencedby) " + handle);
+		} else {
+		    articleDOI = vals[0].value;
+		}
+		log.debug("articleDOI = " + articleDOI);
+
+		
+		log.info(handle + " done.");
+	    } catch (Exception e) {
+		log.fatal("Skipping -- Exception in processing " + handle, e);
+		setResult("Object has a fatal error: " + handle + "\n" + e.getMessage());
+		report("Object has a fatal error: " + handle + "\n" + e.getMessage());
+		
+		context.abort();
+		return Curator.CURATE_SKIP;
+	    }
+	} else {
+	    log.info("Skipping -- non-item DSpace object");
+	    setResult("Object skipped (not an item)");
+	    context.abort();
+	    return Curator.CURATE_SKIP;
+        }
+
+	setResult("Last processed item = " + handle + " -- " + itemDOI);
+
+
+	//TODO: lookup names in ORCID
+	//TODO: reconcile names between Dryad and ORCID
+	//TODO: add processing for article DOIs, not just item DOIs
+	String orcidID = "";
+	String orcidName = "";
+	String dryadORCID = "";
+	String dryadName = "";
+	
+	report(itemDOI + ", " + articleDOI + ", " + orcidID + ", \"" + orcidName + "\", " +
+	       dryadORCID + ", \"" + dryadName + "\"");
+
+	log.debug("ODIN's Hamr complete");
+
+	try { 
+	    context.complete();
+        } catch (SQLException e) {
+	    log.fatal("Unable to close database connection", e);
+	}
+	return Curator.CURATE_SUCCESS;
+    }
+
+    /**
+       An XML utility method that returns the text content of a node.
+    **/
+    private String getNodeText(Node aNode) {
+	return aNode.getChildNodes().item(0).getNodeValue();
+    }
+
+    private Item getDSpaceItem(String itemID) {
+	Item dspaceItem = null;
+	try {
+	    dspaceItem = (Item)identifierService.resolve(context, itemID);  
+        } catch (IdentifierNotFoundException e) {
+	    log.fatal("Unable to get DSpace Item for " + itemID, e);
+	} catch (IdentifierNotResolvableException e) {
+	    log.fatal("Unable to get DSpace Item for " + itemID, e);
+	}
+
+	return dspaceItem;
+    }
+    
+}
