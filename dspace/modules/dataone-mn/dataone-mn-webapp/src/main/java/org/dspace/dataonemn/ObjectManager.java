3a1fa85735d04bd1d7bfd98c767610dd073fe913
Dan Leehr <dan.leehr@nescent.org>
2013-10-15 10:25:17 -0400

    Include : in time zone for ISO-8601 formatting
    Addresses https://redmine.dataone.org/issues/4085#change-17448
    
    Java 7 supports this formatting with the 'X' component (http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html#iso8601timezone).
    Since this is not available in Java 6, the SimpleDateFormatter objects are customized to insert a colon, courtesy http://stackoverflow.com/questions/2375222/java-simpledateformat-for-time-zone-with-a-colon-seperator/14422046#14422046

--- dspace/modules/dataone-mn/dataone-mn-webapp/src/main/java/org/dspace/dataonemn/ObjectManager.java
+++ dspace/modules/dataone-mn/dataone-mn-webapp/src/main/java/org/dspace/dataonemn/ObjectManager.java
@@ -49,7 +49,12 @@ import org.jdom.input.SAXBuilder;
 public class ObjectManager implements Constants {
     
     private static final Logger log = Logger.getLogger(ObjectManager.class);
-    private static final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
+    private static final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ") {
+        public StringBuffer format(Date date, StringBuffer toAppendTo, java.text.FieldPosition pos) {
+            StringBuffer toFix = super.format(date, toAppendTo, pos);
+            return toFix.insert(toFix.length()-2, ':');
+        }
+    };
     public static final int DEFAULT_START = 0;
     public static final int DEFAULT_COUNT = 20;
     
