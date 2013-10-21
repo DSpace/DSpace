7126b21d6cc19c8666f9255e62274b2c15a22215
Peter Dietz <dietz.72@osu.edu>
2013-10-21 11:53:18 -0400

    Initial attempt at item search.
    Not guaranteed to only get Items.
    It is hard-coded to use Lucene, would like to use Dependency Injection to allow for solr

--- dspace-rest/src/main/java/org/dspace/rest/ItemsResource.java
+++ dspace-rest/src/main/java/org/dspace/rest/ItemsResource.java
@@ -10,11 +10,19 @@ package org.dspace.rest;
 import org.apache.log4j.Logger;
 import org.dspace.authorize.AuthorizeManager;
 import org.dspace.core.Context;
+import org.dspace.handle.HandleManager;
+import org.dspace.rest.common.DSpaceObject;
+import org.dspace.search.DSQuery;
+import org.dspace.search.QueryArgs;
+import org.dspace.search.QueryResults;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -50,6 +58,42 @@ public class ItemsResource {
             }
 
         } catch (SQLException e)  {
+            log.error(e.getMessage());
+            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // /items/search?q=Albert Einstein
+    @GET
+    @Path("/search")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public DSpaceObject[] search(@QueryParam("q") String query) {
+        try {
+            if(context == null || !context.isValid()) {
+                context = new Context();
+                //Failed SQL is ignored as a failed SQL statement, prevent: current transaction is aborted, commands ignored until end of transaction block
+                context.getDBConnection().setAutoCommit(true);
+            }
+
+            //TODO, I'm not 100% sure that results are limited to just Items...
+
+            QueryArgs queryArgs = new QueryArgs();
+            queryArgs.setQuery(query);
+            QueryResults queryResults = DSQuery.doQuery(context, queryArgs);
+
+            List<String> handleList = queryResults.getHitHandles();
+            List<DSpaceObject> dsoList = new ArrayList<DSpaceObject>();
+            for(String handle : handleList) {
+                org.dspace.content.DSpaceObject dso = HandleManager.resolveToObject(context, handle);
+                dsoList.add(new DSpaceObject(dso));
+            }
+
+            return dsoList.toArray(new org.dspace.rest.common.DSpaceObject[0]);
+
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
+        } catch (IOException e) {
             log.error(e.getMessage());
             throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
         }
