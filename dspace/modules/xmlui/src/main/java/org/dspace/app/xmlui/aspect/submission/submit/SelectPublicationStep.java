14c2aceb8a2a66bcef7f545c8ca15bbeb670ea45
lantian <lantian@atmire.com>
2013-11-08 16:37:05 -0800

    1927 : test with xml,  journalID and manu , fix bugs

--- dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/SelectPublicationStep.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/SelectPublicationStep.java
@@ -140,14 +140,16 @@ public class SelectPublicationStep extends AbstractSubmissionStep {
         if(selectedJournalId!=null){
             String journalPath = "";
             try{
+                final java.util.List<String> journalVals = org.dspace.submit.step.SelectPublicationStep.journalVals;
+                final java.util.List<String> journalDirs = org.dspace.submit.step.SelectPublicationStep.journalDirs;
                 journalPath = org.dspace.submit.step.SelectPublicationStep.journalDirs.get(org.dspace.submit.step.SelectPublicationStep.journalVals.indexOf(selectedJournalId));
                 pBean = ModelPublication.getDataFromPublisherFile(manuscriptNumber, selectedJournalId, journalPath);
                 journalStatus = pBean.getStatus();
                 journalName = pBean.getJournalName();
-                if(journalName!=null && !journalName.equals("")) {
-                    if(org.dspace.submit.step.SelectPublicationStep.integratedJournals.contains(selectedJournalId))
-                        journalName += "*";
-                }
+//                if(journalName!=null && !journalName.equals("")) {
+//                    if(org.dspace.submit.step.SelectPublicationStep.integratedJournals.contains(selectedJournalId))
+//                        journalName += "*";
+//                }
             }catch (Exception e)
             {
                  //invalid journalID
@@ -512,7 +514,7 @@ public class SelectPublicationStep extends AbstractSubmissionStep {
             String name =  journalNames.get(i);
             String no_asterisk = name;
             if(org.dspace.submit.step.SelectPublicationStep.integratedJournals.contains(val))
-                name += "*";
+                //name += "*";
                 journalID.addOption(val.equals(selectedJournalId), no_asterisk, name);
 
         }
