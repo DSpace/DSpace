bd01bdb048b87a2611513949bae3767b1be36f53
Mark H. Wood <mwood@IUPUI.Edu>
2013-11-22 12:56:09 -0500

    Commandline operation to list or delete disused accounts

--- dspace-api/src/main/java/org/dspace/eperson/Groomer.java
+++ dspace-api/src/main/java/org/dspace/eperson/Groomer.java
@@ -9,7 +9,12 @@
 package org.dspace.eperson;
 
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import org.apache.commons.cli.*;
+import org.dspace.authorize.AuthorizeException;
 import org.dspace.core.Context;
 import org.dspace.storage.rdbms.DatabaseManager;
 import org.dspace.storage.rdbms.TableRow;
@@ -33,10 +38,14 @@ public class Groomer
         OptionGroup verbs = new OptionGroup();
         verbs.setRequired(true);
         verbs.addOption(new Option("h", "help", false, "explain this tool"));
+        verbs.addOption(new Option("a", "aging", false, "discover accounts not used recently"));
         verbs.addOption(new Option("u", "unsalted", false, "list accounts with unsalted password hashes"));
 
         Options options = new Options();
         options.addOptionGroup(verbs);
+
+        options.addOption("b", "last-used-before", true, "date of last login was before this");
+        options.addOption("d", "delete", false, "delete matching epersons");
 
         PosixParser parser = new PosixParser();
         CommandLine command = null;
@@ -54,18 +63,96 @@ public class Groomer
         {
             new HelpFormatter().printHelp(USAGE, options);
         }
-        // Scan for unsalted hashes
+        // Scan for disused accounts
+        else if (command.hasOption('a'))
+        {
+            aging(command);
+        }
+        // List accounts with unsalted passwords
         else if (command.hasOption('u'))
         {
-            Context myContext = new Context();
-            final TableRowIterator tri = DatabaseManager.query(myContext,
-                    "SELECT email FROM EPerson WHERE password IS NOT NULL AND digest_algorithm IS NULL");
-            for (TableRow row = tri.next(); tri.hasNext(); row = tri.next())
-                System.out.println(row.getStringColumn("email"));
-            myContext.abort(); // No changes to commit
+            findUnsalted(command);
         }
         // Should not happen:  verb option defined but no code!
         else
             System.err.println("Unimplemented verb:  " + verbs.getSelected());
+    }
+
+    private static void aging(CommandLine command) throws SQLException
+    {
+            if (!command.hasOption('b'))
+            {
+                System.err.println("A last login date is required.");
+                System.exit(1);
+            }
+
+            Date before = null;
+            try {
+                before = new SimpleDateFormat().parse(command.getOptionValue('b'));
+            } catch (java.text.ParseException ex) {
+                System.err.println(ex.getMessage());
+                System.exit(1);
+            }
+
+            boolean delete = command.hasOption('d');
+
+            Context myContext = new Context();
+            final TableRowIterator tri = DatabaseManager.queryTable(myContext,
+                    "EPerson",
+                    "SELECT eperson_id, email, netid FROM EPerson WHERE last_login < ?",
+                    before);
+
+            myContext.ignoreAuthorization();
+            for (TableRow row = tri.next(); tri.hasNext(); row = tri.next())
+            {
+                int id = row.getIntColumn("eperson_id");
+                EPerson account = EPerson.find(myContext, id);
+
+                System.out.print(id);
+                System.out.print(' ');
+                System.out.print(account.getEmail());
+                System.out.print('\t');
+                System.out.print(account.getNetid());
+                System.out.print('\t');
+                System.out.print(account.getFullName());
+                System.out.println();
+
+                if (delete)
+                {
+                    List<String> whyNot = account.getDeleteConstraints();
+                    if (!whyNot.isEmpty())
+                    {
+                        System.out.print("  Cannot be deleted; referenced in");
+                        for (String table : whyNot)
+                        {
+                            System.out.print(' ');
+                            System.out.print(table);
+                        }
+                        System.out.println();
+                    }
+                    else
+                        try {
+                            account.delete();
+                        } catch (AuthorizeException ex) {
+                            // XXX SNH
+                        } catch (EPersonDeletionException ex) {
+                            System.err.println(ex.getMessage());
+                        }
+                    }
+            }
+
+            myContext.restoreAuthSystemState();
+            myContext.complete();
+    }
+
+    private static void findUnsalted(CommandLine command)
+            throws SQLException
+    {
+        Context myContext = new Context();
+        final TableRowIterator tri = DatabaseManager.query(myContext,
+                "SELECT email FROM EPerson WHERE password IS NOT NULL AND digest_algorithm IS NULL");
+        for (TableRow row = tri.next(); tri.hasNext(); row = tri.next())
+            System.out.println(row.getStringColumn("email"));
+        myContext.abort(); // No changes to commit
     }
 }
