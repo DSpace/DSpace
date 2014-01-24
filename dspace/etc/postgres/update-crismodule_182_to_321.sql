b695c07cc640d26f07f4db72962538c6bd2268a9
Pascarelli Luigi Andrea <l.pascarelli@cineca.it>
2014-01-24 17:03:33 +0100

    Update db script to mantain the new attribute for external sources information

--- dspace/etc/postgres/update-crismodule_182_to_321.sql
+++ dspace/etc/postgres/update-crismodule_182_to_321.sql
@@ -2,3 +2,8 @@ alter table cris_do_wpointer add column urlPath varchar(255);
 alter table cris_ou_wpointer add column urlPath varchar(255);
 alter table cris_pj_wpointer add column urlPath varchar(255);
 alter table cris_rp_wpointer add column urlPath varchar(255);
+
+alter table cris_do add column sourceRef varchar(255);
+alter table cris_orgunit add column sourceRef varchar(255);
+alter table cris_project add column sourceRef varchar(255);
+alter table cris_rpage add column sourceRef varchar(255)
