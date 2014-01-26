8621a5d4904456fd350908fa3efd5d44bbaf2244
Pascarelli Luigi Andrea <l.pascarelli@cineca.it>
2014-01-26 23:59:33 +0100

    Update sql script to add source reference in nested, update submodule

--- dspace/etc/postgres/update-crismodule_321_to_4.sql
+++ dspace/etc/postgres/update-crismodule_321_to_4.sql
@@ -1,7 +1,15 @@
 alter table cris_do add column sourceRef varchar(255);
+alter table cris_do_no add column sourceID varchar(255);
+alter table cris_do_no add column sourceRef varchar(255);
 alter table cris_orgunit add column sourceRef varchar(255);
+alter table cris_ou_no add column sourceID varchar(255);
+alter table cris_ou_no add column sourceRef varchar(255);
+alter table cris_pj_no add column sourceID varchar(255);
+alter table cris_pj_no add column sourceRef varchar(255);
 alter table cris_project add column sourceRef varchar(255);
-alter table cris_rpage add column sourceRef varchar(255)
+alter table cris_rp_no add column sourceID varchar(255);
+alter table cris_rp_no add column sourceRef varchar(255);
+alter table cris_rpage add column sourceRef varchar(255);
 
 ALTER TABLE cris_do ADD CONSTRAINT cris_do_sourceid_sourceref_key UNIQUE (sourceid, sourceref);
 ALTER TABLE cris_orgunit ADD CONSTRAINT cris_orgunit_sourceid_sourceref_key UNIQUE (sourceid, sourceref);
