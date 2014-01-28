a46b60963eb0170647e96a23b1d9d4417d193cdf
Ryan Scherle <ryan@scherle.org>
2014-01-28 10:41:39 -0500

    Removed unneeded method

--- dspace/modules/api/src/main/java/org/dspace/curate/OdinsHamr.java
+++ dspace/modules/api/src/main/java/org/dspace/curate/OdinsHamr.java
@@ -346,19 +346,6 @@ public class OdinsHamr extends AbstractCurationTask {
     }
 
     
-    private Item getDSpaceItem(String itemID) {
-	Item dspaceItem = null;
-	try {
-	    dspaceItem = (Item)identifierService.resolve(context, itemID);  
-        } catch (IdentifierNotFoundException e) {
-	    log.fatal("Unable to get DSpace Item for " + itemID, e);
-	} catch (IdentifierNotResolvableException e) {
-	    log.fatal("Unable to get DSpace Item for " + itemID, e);
-	}
-
-	return dspaceItem;
-    }
-
     class OrcidName extends Object {
 	private String orcid = "";
 	private String name = "";
