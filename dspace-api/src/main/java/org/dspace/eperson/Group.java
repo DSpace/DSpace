df7d95fc705553fc1d12e6cf71ed308f8151f5c4
Monika Mevenkamp <monikam@princeton.edu>
2014-03-24 18:02:54 -0400

    define Group.ANONYMOUS_ID and Group.ADMIN_ID and use instead of 0,1
    Conflicts:
    	dspace-api/src/main/java/org/dspace/authorize/AuthorizeManager.java
    	dspace-api/src/main/java/org/dspace/embargo/DefaultEmbargoSetter.java

--- dspace-api/src/main/java/org/dspace/eperson/Group.java
+++ dspace-api/src/main/java/org/dspace/eperson/Group.java
@@ -48,6 +48,12 @@ public class Group extends DSpaceObject
     /** log4j logger */
     private static Logger log = Logger.getLogger(Group.class);
 
+    /** ID of Anonymous Group */
+    public static final int ANONYMOUS_ID = 0;
+
+    /** ID of Administrator Group */
+    public static final int ADMIN_ID = 1;
+
     /** Our context */
     private Context myContext;
 
@@ -332,7 +338,7 @@ public class Group extends DSpaceObject
     public boolean isMember(EPerson e)
     {
         // special, group 0 is anonymous
-        if (getID() == 0)
+        if (getID() == Group.ANONYMOUS_ID)
         {
             return true;
         }
