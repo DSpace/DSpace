037931de3287f358de4ff188983674f175421aaa
Dan Leehr <dan.leehr@nescent.org>
2014-04-14 13:25:49 -0400

    Add @deprecated to DOIMigrator

--- dspace/modules/doi/dspace-doi-api/src/main/java/org/dspace/doi/DOIMigrator.java
+++ dspace/modules/doi/dspace-doi-api/src/main/java/org/dspace/doi/DOIMigrator.java
@@ -8,7 +8,9 @@ import org.apache.log4j.Logger;
 /**
  * Migrates DOIs from perst DOI.db file to Postgres
  * @author Dan Leehr <dan.leehr@nescent.org>
+ * @deprecated uses old DOIDatabase (doi.db file)
  */
+@Deprecated
 public class DOIMigrator {
     private static Logger log = Logger.getLogger(DOIMigrator.class);
 
