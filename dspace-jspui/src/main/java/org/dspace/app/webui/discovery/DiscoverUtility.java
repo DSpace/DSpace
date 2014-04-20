3bf90abf720a6e20a9efe46b77b57ed8fac756ac
Keiji Suzuki <zuki.ebetsu@gmail.com>
2014-04-20 14:43:57 +0900

    Fixed bugs related to setting default filter queries

--- dspace-jspui/src/main/java/org/dspace/app/webui/discovery/DiscoverUtility.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/discovery/DiscoverUtility.java
@@ -199,7 +199,7 @@ public class DiscoverUtility
         {
             for (String f : defaultFilterQueries)
             {
-                queryArgs.addFacetQuery(f);
+                queryArgs.addFilterQueries(f);
             }
         }
         List<String[]> filters = getFilters(request);
