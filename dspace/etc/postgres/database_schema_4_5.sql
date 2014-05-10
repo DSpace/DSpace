76cb04628049bff66c0c0a46a6f9f160655d8a51
Peter Dietz <peter@longsight.com>
2014-05-10 08:37:33 -0400

    DS-1945 Add license headers for requestitem changes

--- dspace/etc/postgres/database_schema_4_5.sql
+++ dspace/etc/postgres/database_schema_4_5.sql
@@ -1,4 +1,9 @@
+-- The contents of this file are subject to the license and copyright
+-- detailed in the LICENSE and NOTICE files at the root of the source
+-- tree and available online at
+--
 -- http://www.dspace.org/license/
+
 -- SQL commands to upgrade the Postgres database schema from DSpace 4.x to 5.x
 -- DUMP YOUR DATABASE FIRST. DUMP YOUR DATABASE FIRST. DUMP YOUR DATABASE FIRST. DUMP YOUR DATABASE FIRST.
 BEGIN;
