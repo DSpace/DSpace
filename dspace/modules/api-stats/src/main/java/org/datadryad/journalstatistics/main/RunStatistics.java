64d577eb2ce0d253a2f3ab1c299cb6ac07c44bf9
Dan Leehr <dan.leehr@nescent.org>
2014-05-21 13:33:13 -0400

    Main class to run journal statistics

--- dspace/modules/api-stats/src/main/java/org/datadryad/journalstatistics/main/RunStatistics.java
+++ dspace/modules/api-stats/src/main/java/org/datadryad/journalstatistics/main/RunStatistics.java
@@ -2,7 +2,14 @@
  */
 package org.datadryad.journalstatistics.main;
 
+import java.sql.SQLException;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
 import org.datadryad.journalstatistics.statistics.DefaultStatisticsPackage;
+import org.dspace.core.Context;
 
 /**
  *
@@ -10,7 +17,31 @@ import org.datadryad.journalstatistics.statistics.DefaultStatisticsPackage;
  */
 public class RunStatistics {
 
-    public static void main(String args[]) {
-        DefaultStatisticsPackage.getInstance().run();
+    public static void main(String args[]) throws Exception {
+        CommandLineParser parser = new PosixParser();
+        Options options = new Options();
+        options.addOption("j", true, "Journal Name");
+        options.addOption("f", "from", true, "Start Date");
+        options.addOption("t", "to", true, "End Date");
+        options.addOption("h", "help", false, "help");
+        CommandLine line = parser.parse(options, args);
+
+        // Did the user ask to see the help?
+        if(line.hasOption("j")) {
+            String journalName = line.getOptionValue("j");
+            Context c = new Context();
+            c.turnOffAuthorisationSystem();
+            DefaultStatisticsPackage statisticsPackage = new DefaultStatisticsPackage(c);
+            statisticsPackage.run(journalName);
+        } else {
+            printHelp(options, 0);
+        }
     }
+
+    private static void printHelp(Options options, int exitCode) {
+        HelpFormatter myhelp = new HelpFormatter();
+        myhelp.printHelp("RunStatistics\n", options);
+        System.exit(exitCode);
+    }
+
 }
