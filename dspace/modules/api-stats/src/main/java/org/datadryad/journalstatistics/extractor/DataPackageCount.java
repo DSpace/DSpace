130356823df7a9c6ecee576065dbdb78c327afc3
Dan Leehr <dan.leehr@nescent.org>
2014-05-21 13:32:36 -0400

    Implementation of DataPackageCount

--- dspace/modules/api-stats/src/main/java/org/datadryad/journalstatistics/extractor/DataPackageCount.java
+++ dspace/modules/api-stats/src/main/java/org/datadryad/journalstatistics/extractor/DataPackageCount.java
@@ -2,6 +2,13 @@
  */
 package org.datadryad.journalstatistics.extractor;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import org.apache.log4j.Logger;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Collection;
+import org.dspace.content.Item;
+import org.dspace.content.ItemIterator;
 import org.dspace.core.Context;
 
 /**
@@ -9,11 +16,52 @@ import org.dspace.core.Context;
  * @author Dan Leehr <dan.leehr@nescent.org>
  */
 public class DataPackageCount extends DatabaseExtractor<Integer> {
+    private static Logger log = Logger.getLogger(DataPackageCount.class);
+
+    private static final String JOURNAL_SCHEMA = "prism";
+    private static final String JOURNAL_ELEMENT = "publicationName";
+    private static final String JOURNAL_QUALIFIER = null;
+
+    public DataPackageCount(Context context) {
+        super(context);
+    }
 
     @Override
-    public Integer extract(String journalName) {
+    public Integer extract(final String journalName) {
         Context context = this.getContext();
-        throw new RuntimeException("Not yet implemented");
+        Collection dataPackages;
+        Integer count = 0;
+        try {
+            /*
+             * Initial implementation is to fetch items by metadata field ID
+             * then filter on collection. This may be too slow since it will
+             * result in a lot of queries and could be done with a single join
+             */
+            ItemIterator itemsByJournal = Item.findByMetadataField(
+                    this.getContext(),
+                    JOURNAL_SCHEMA,
+                    JOURNAL_ELEMENT,
+                    JOURNAL_QUALIFIER,
+                    journalName);
+
+            dataPackages = getDataPackagesCollection();
+
+            while(itemsByJournal.hasNext()) {
+                Item item = itemsByJournal.next();
+                if(item.isOwningCollection(dataPackages)) {
+                    count++;
+                }
+            }
+
+        } catch (AuthorizeException ex) {
+            log.error("AuthorizeException getting data package count per journal", ex);
+        } catch (IOException ex) {
+            log.error("IOException getting data package count per journal", ex);
+        } catch (SQLException ex) {
+            log.error("SQLException getting data package count per journal", ex);
+        }
+
+        return count;
     }
 
 }
--- dspace/modules/api-stats/src/main/java/org/datadryad/journalstatistics/extractor/DatabaseExtractor.java
+++ dspace/modules/api-stats/src/main/java/org/datadryad/journalstatistics/extractor/DatabaseExtractor.java
@@ -2,8 +2,13 @@
  */
 package org.datadryad.journalstatistics.extractor;
 
+import java.sql.SQLException;
 import org.dspace.content.Collection;
+import org.dspace.content.DSpaceObject;
+import org.dspace.core.ConfigurationManager;
+import org.dspace.core.Constants;
 import org.dspace.core.Context;
+import org.dspace.handle.HandleManager;
 
 /**
  * Base class for extracting statistics from a database
@@ -11,6 +16,9 @@ import org.dspace.core.Context;
  */
 public abstract class DatabaseExtractor<T> implements ExtractorInterface<T> {
 
+    private static final String PACKAGES_COLLECTION_HANDLE_KEY = "stats.datapkgs.coll";
+    private static final String FILES_COLLECTION_HANDLE_KEY = "stats.datafiles.coll";
+    
     private Context context;
     public DatabaseExtractor(Context context) {
         this.context = context;
@@ -23,20 +31,31 @@ public abstract class DatabaseExtractor<T> implements ExtractorInterface<T> {
         return context;
     }
 
+    private Collection collectionFromHandle(String handle) throws SQLException {
+        DSpaceObject object = HandleManager.resolveToObject(context, handle);
+        if(object.getType() == Constants.COLLECTION) {
+            return (Collection)object;
+        } else {
+            return null;
+        }
+    }
+
     /**
      * Dryad Data packages are in stats.datapkgs.coll
      * @return
      */
-    protected Collection getDataPackagesCollection() {
-        throw new RuntimeException("Not yet implemented");
+    protected Collection getDataPackagesCollection() throws SQLException {
+        String handle = ConfigurationManager.getProperty(PACKAGES_COLLECTION_HANDLE_KEY);
+        return collectionFromHandle(handle);
     }
 
     /**
      * Dryad Data files are in stats.datafiles.coll
      * @return
      */
-    protected Collection getDataFilesCollection() {
-        throw new RuntimeException("Not yet implemented");
+    protected Collection getDataFilesCollection() throws SQLException {
+        String handle = ConfigurationManager.getProperty(FILES_COLLECTION_HANDLE_KEY);
+        return collectionFromHandle(handle);
     }
 
     /**
