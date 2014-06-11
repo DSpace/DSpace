a348bf9da5d8db6997f2d8561558e9071df9d74e
Dan Leehr <dan.leehr@nescent.org>
2014-06-11 10:52:42 -0400

    Call Update after setting publicationName

--- dspace/modules/api-stats/src/main/java/org/datadryad/api/DryadObject.java
+++ dspace/modules/api-stats/src/main/java/org/datadryad/api/DryadObject.java
@@ -2,16 +2,16 @@
  */
 package org.datadryad.api;
 
+import java.io.IOException;
 import java.sql.SQLException;
+import org.apache.log4j.Logger;
+import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.Collection;
-import org.dspace.content.DCValue;
 import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
 import org.dspace.content.WorkspaceItem;
-import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Constants;
 import org.dspace.core.Context;
-import org.dspace.doi.DOI;
 import org.dspace.handle.HandleManager;
 import org.dspace.identifier.DOIIdentifierProvider;
 
@@ -22,7 +22,8 @@ import org.dspace.identifier.DOIIdentifierProvider;
 public abstract class DryadObject {
     private static final String PUBLICATION_NAME_SCHEMA = "prism";
     private static final String PUBLICATION_NAME_ELEMENT = "publicationName";
-    private static final String PUBLICATION_NAME_QUALIFIER = Item.ANY;
+    private static final String PUBLICATION_NAME_QUALIFIER = null;
+    private static Logger log = Logger.getLogger(DryadObject.class);
 
     protected WorkspaceItem workspaceItem;
     protected DryadObject(WorkspaceItem workspaceItem) {
@@ -52,9 +53,16 @@ public abstract class DryadObject {
         return DOIIdentifierProvider.getDoiValue(getItem());
     }
 
-    public void setPublicationName(String publicationName) {
+    public void setPublicationName(String publicationName) throws SQLException {
         getItem().clearMetadata(PUBLICATION_NAME_SCHEMA, PUBLICATION_NAME_ELEMENT, PUBLICATION_NAME_QUALIFIER, null);
         getItem().addMetadata(PUBLICATION_NAME_SCHEMA, PUBLICATION_NAME_ELEMENT, PUBLICATION_NAME_QUALIFIER, null, publicationName);
+        try {
+            getWorkspaceItem().update();
+        } catch (AuthorizeException ex) {
+            log.error("Authorize exception setting publication name", ex);
+        } catch (IOException ex) {
+            log.error("IO exception setting publication name", ex);
+        }
     }
 
 }
