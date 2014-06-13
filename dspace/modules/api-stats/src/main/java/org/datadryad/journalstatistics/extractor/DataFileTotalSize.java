ff4521b4da55f1c8bf5e10026a421e6eb3d713bf
Dan Leehr <dan.leehr@nescent.org>
2014-06-13 11:33:40 -0400

    Extractor to get total size of data files

---
+++ dspace/modules/api-stats/src/main/java/org/datadryad/journalstatistics/extractor/DataFileTotalSize.java
@@ -0,0 +1,48 @@
+/*
+ */
+package org.datadryad.journalstatistics.extractor;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import org.apache.log4j.Logger;
+import org.datadryad.api.DryadDataFile;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Collection;
+import org.dspace.content.Item;
+import org.dspace.content.ItemIterator;
+import org.dspace.core.Context;
+
+/**
+ * Counts the total size (in bytes) of Dryad Data Files in the archive
+ * associated with a specified Journal
+ * @author Dan Leehr <dan.leehr@nescent.org>
+ */
+public class DataFileTotalSize  extends DatabaseExtractor<Long> {
+    private static Logger log = Logger.getLogger(DataItemCount.class);
+    public DataFileTotalSize(Context context) {
+        super(context);
+    }
+
+    @Override
+    public Long extract(final String journalName) {
+        Context context = this.getContext();
+        Collection collection;
+        Long totalSize = 0L;
+        try {
+            ItemIterator itemsByJournal = Item.findByMetadataField(this.getContext(), JOURNAL_SCHEMA, JOURNAL_ELEMENT, JOURNAL_QUALIFIER, journalName);
+            collection = DryadDataFile.getCollection(context);
+            while (itemsByJournal.hasNext()) {
+                DryadDataFile dataFile = new DryadDataFile(itemsByJournal.next());
+                totalSize += dataFile.getTotalStorageSize();
+            }
+        } catch (AuthorizeException ex) {
+            log.error("AuthorizeException getting total size per journal", ex);
+        } catch (IOException ex) {
+            log.error("IOException getting total size per journal", ex);
+        } catch (SQLException ex) {
+            log.error("SQLException getting total size per journal", ex);
+        }
+        return totalSize;
+    }
+
+}
---
+++ dspace/modules/api-stats/src/test/java/org/datadryad/journalstatistics/extractor/DataFileTotalSizeTest.java
@@ -0,0 +1,74 @@
+/*
+ */
+package org.datadryad.journalstatistics.extractor;
+
+import java.io.File;
+import java.io.FileInputStream;
+import org.datadryad.api.DryadDataFile;
+import org.datadryad.test.ContextUnitTest;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author Dan Leehr <dan.leehr@nescent.org>
+ */
+public class DataFileTotalSizeTest extends ContextUnitTest {
+    private File file1, file2, file3;
+
+    @Before
+    public void setUp() {
+        super.setUp();
+        try {
+            file1 = new File(DataFileTotalSizeTest.class.getClassLoader().getResource("Logo-one-tone-dark.jpg").toURI());
+            file2 = new File(DataFileTotalSizeTest.class.getClassLoader().getResource("world.js").toURI());
+            file3 = new File(DataFileTotalSizeTest.class.getClassLoader().getResource("world.js").toURI());
+        } catch (Exception ex) {
+            fail("Exception setting up files for total size test " + ex);
+        }
+    }
+    /**
+     * Test of extract method, of class DataFileTotalSize.
+     */
+    @Test
+    public void testCountDataFileTotalSize() throws Exception {
+        System.out.println("countDataFileTotalSize");
+        String journalName1 = "Test Journal 1";
+        String journalName2 = "Test Journal 2";
+        DataFileTotalSize instance = new DataFileTotalSize(context);
+
+        // We will add some files to both journals and verify that only the
+        // size for one journal is counted
+
+        Long initialSizeForJournalName1 = instance.extract(journalName1);
+        Long initialSizeForJournalName2 = instance.extract(journalName2);
+
+        Long expectedSizeIncreaseForJournalName1 = file1.length() + file2.length();
+        Long expectedSizeIncreaseForJournalName2 = file3.length();
+        assertTrue(expectedSizeIncreaseForJournalName1 > 0);
+        assertTrue(expectedSizeIncreaseForJournalName2 > 0);
+        assertTrue(expectedSizeIncreaseForJournalName1 != expectedSizeIncreaseForJournalName2);
+
+        // Create a file and associate it with journal 1
+
+        DryadDataFile dataFile1 = DryadDataFile.create(context);
+        dataFile1.setPublicationName(journalName1);
+        dataFile1.addBitstream(new FileInputStream(file1));
+        dataFile1.addBitstream(new FileInputStream(file2));
+
+        // Create a second file, associate with journal2 and add file3
+
+        DryadDataFile dataFile2 = DryadDataFile.create(context);
+        dataFile2.setPublicationName(journalName2);
+        dataFile2.addBitstream(new FileInputStream(file3));
+        
+        Long totalSizeForJournalName1 = instance.extract(journalName1);
+        Long sizeIncrease1 = totalSizeForJournalName1 - initialSizeForJournalName1;
+        assertEquals(expectedSizeIncreaseForJournalName1, sizeIncrease1);
+
+        Long totalSizeForJournalName2 = instance.extract(journalName2);
+        Long sizeIncrease2 = totalSizeForJournalName2 - initialSizeForJournalName2;
+        assertEquals(expectedSizeIncreaseForJournalName2, sizeIncrease2);
+    }
+}
