7e7402c0054f6988402c2c1c9844adea65efcfce
Dan Leehr <dan.leehr@nescent.org>
2014-06-17 10:31:44 -0400

    Tests for counting data packages by date

--- dspace/modules/api-stats/src/test/java/org/datadryad/journalstatistics/extractor/DataPackageCountByDateTest.java
+++ dspace/modules/api-stats/src/test/java/org/datadryad/journalstatistics/extractor/DataPackageCountByDateTest.java
@@ -18,35 +18,134 @@ import org.junit.Before;
  */
 public class DataPackageCountByDateTest extends ContextUnitTest{
     private Date futureDate;
-
+    private Date lastYearDate;
+    private Date customDate_2008_07_25;
+    private Date customDate_2012_03_21;
+    private Date customDate_2010_01_01;
+    private Date customDate_2010_01_02;
+    private final static String journalName = "Test Journal";
     @Before
     @Override
     public void setUp() {
         super.setUp();
         Calendar calendar = new GregorianCalendar();
         calendar.set(Calendar.YEAR, 9999);
-        calendar.set(Calendar.MONTH, 1);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
         calendar.set(Calendar.DAY_OF_MONTH, 1);
         futureDate = calendar.getTime();
+        calendar = new GregorianCalendar();
+        calendar.add(Calendar.YEAR, -1);
+        lastYearDate = calendar.getTime();
+        calendar.set(Calendar.YEAR, 2008);
+        calendar.set(Calendar.MONTH, Calendar.JULY);
+        calendar.set(Calendar.DAY_OF_MONTH, 25);
+        customDate_2008_07_25 = calendar.getTime();
+        calendar.set(Calendar.YEAR, 2012);
+        calendar.set(Calendar.MONTH, Calendar.MARCH);
+        calendar.set(Calendar.DAY_OF_MONTH, 21);
+        customDate_2012_03_21 = calendar.getTime();
+        calendar.set(Calendar.YEAR, 2010);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        customDate_2010_01_01 = calendar.getTime();
+        calendar.set(Calendar.DAY_OF_MONTH, 2);
+        customDate_2010_01_02 = calendar.getTime();
     }
 
+    /**
+     * Test that a date range count ending in the distant future includes
+     * a package deposited today.
+     * @throws SQLException
+     */
     @Test
     public void testCountDataPackagesByDate() throws SQLException {
         // Count the initial number of data packages
-        String journalName = "Test Journal";
         DataPackageCount instance = new DataPackageCount(this.context);
         instance.setEndDate(futureDate);
+        // Assert we're filtering on dates
+        assert(instance.filterOnDates);
         Integer initialCount = instance.extract(journalName);
-        // Create a new data package. It will have today's date
+
+        // Create a new data package. It will have today's date as accessioned
         DryadDataPackage dataPackage = DryadDataPackage.create(context);
         dataPackage.setPublicationName(journalName);
         Integer expResult = initialCount + 1;
         Integer result = instance.extract(journalName);
         assertEquals(expResult, result);
+    }
 
-        // update the date accessioned of the data package, and assert the count
-        // goes down
-        fail("write this part of the test");
+    /**
+     * Test that a date-range count ending last year does not include
+     * a package added today.
+     * @throws SQLException
+     */
+    @Test
+    public void testCountDataPackagesByDatePast() throws SQLException {
+        DataPackageCount instance = new DataPackageCount(this.context);
+        instance.setEndDate(lastYearDate);
+        // Assert we're filtering on dates
+        assert(instance.filterOnDates);
+        Integer initialCount = instance.extract(journalName);
 
+        // Create a new data package. It will have today's date as accessioned
+        DryadDataPackage dataPackage = DryadDataPackage.create(context);
+        dataPackage.setPublicationName(journalName);
+        Integer expResult = initialCount;
+        Integer result = instance.extract(journalName);
+        assertEquals(expResult, result);
+    }
+
+    @Test(expected=IllegalStateException.class)
+    public void testThrowsExceptionWithBadDateOrder() {
+        DataPackageCount instance = new DataPackageCount(this.context);
+        assert(futureDate.after(lastYearDate));
+        instance.setBeginDate(futureDate);
+        instance.setEndDate(lastYearDate);
+        instance.extract(journalName);
+    }
+
+    /**
+     * Test that a custom date range is working
+     * @throws SQLException
+     */
+    @Test
+    public void testCountDataPackagesByDateCustom() throws SQLException {
+        DataPackageCount instance = new DataPackageCount(this.context);
+        instance.setBeginDate(customDate_2008_07_25);
+        instance.setEndDate(customDate_2012_03_21);
+        Integer initialCount = instance.extract(journalName);
+        
+        DryadDataPackage dataPackage = DryadDataPackage.create(this.context);
+        dataPackage.setPublicationName(journalName);
+        dataPackage.setDateAccessioned(customDate_2010_01_01);
+
+        Integer expResult = initialCount + 1;
+        Integer result = instance.extract(journalName);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test that the endpoints are included in the date range
+     * @throws SQLException
+     */
+    @Test
+    public void testCountDataPackagesFencePost() throws SQLException {
+        DataPackageCount instance = new DataPackageCount(this.context);
+        instance.setBeginDate(customDate_2008_07_25);
+        instance.setEndDate(customDate_2010_01_01);
+        Integer initialCount = instance.extract(journalName);
+
+        DryadDataPackage dataPackage = DryadDataPackage.create(this.context);
+        dataPackage.setPublicationName(journalName);
+        dataPackage.setDateAccessioned(customDate_2010_01_01);
+
+        Integer expResult = initialCount + 1;
+        Integer result = instance.extract(journalName);
+        assertEquals(expResult, result);
+
+        dataPackage.setDateAccessioned(customDate_2010_01_02);
+        expResult = initialCount;
+        result = instance.extract(journalName);
+        assertEquals(expResult, result);
     }
 }
