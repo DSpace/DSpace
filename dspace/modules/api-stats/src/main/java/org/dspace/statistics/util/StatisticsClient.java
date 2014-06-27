222b29058bd38ab9b69db301a02320aa3adba804
Dan Leehr <dan.leehr@nescent.org>
2014-06-27 13:50:59 -0400

    Remove call to optimizeSOLR() since it does not exist in SolrLogger
    It exists in the original DSpace class, but the version we customized for Dryad has never had it.

--- dspace/modules/api-stats/src/main/java/org/dspace/statistics/util/StatisticsClient.java
+++ dspace/modules/api-stats/src/main/java/org/dspace/statistics/util/StatisticsClient.java
@@ -58,7 +58,6 @@ public class StatisticsClient
         options.addOption("m", "mark-spiders", false, "Update isBot Flag in Solr");
         options.addOption("f", "delete-spiders-by-flag", false, "Delete Spiders in Solr By isBot Flag");
         options.addOption("i", "delete-spiders-by-ip", false, "Delete Spiders in Solr By IP Address");
-        options.addOption("o", "optimize", false, "Run maintenance on the SOLR index");
         options.addOption("r", "delete-review-identifiers", false, "Delete ip/dns/token from downloads of in-review items");
         options.addOption("h", "help", false, "help");
 
@@ -85,10 +84,6 @@ public class StatisticsClient
         else if(line.hasOption('i'))
         {
             SolrLogger.deleteRobotsByIP();
-        }
-        else if(line.hasOption('o'))
-        {
-            SolrLogger.optimizeSOLR();
         }
         else if(line.hasOption('r'))
         {
