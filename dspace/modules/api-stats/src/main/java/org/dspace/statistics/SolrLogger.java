be2a270c17880becaca1e876e0a7266bf02d755c
Dan Leehr <dan.leehr@nescent.org>
2014-06-27 10:39:21 -0400

    Rename static variables, add javadoc comments

--- dspace/modules/api-stats/src/main/java/org/dspace/statistics/SolrLogger.java
+++ dspace/modules/api-stats/src/main/java/org/dspace/statistics/SolrLogger.java
@@ -67,8 +67,8 @@ public class SolrLogger
     private static int count = 0;
     private static int commit = 10;
 
-    private static final Pattern reviewTokenPattern = Pattern.compile("^(http.*token=)(.*)$");
-    private static final String dummyToken = "00000000-0000-0000-0000-000000000000";
+    private static final Pattern REVIEW_TOKEN_PATTERN = Pattern.compile("^(http.*token=)(.*)$");
+    static final String DUMMY_TOKEN = "00000000-0000-0000-0000-000000000000";
 
     static
     {
@@ -140,16 +140,31 @@ public class SolrLogger
             commit = ConfigurationManager.getIntProperty("solr.statistics.interval");
     }
 
+    /**
+     * Replaces the review token with replacement text if token is present
+     * @param refererUri A referer string
+     * @param replacementText The text to replace the token with
+     * @return a referer string, with the token replaced
+     */
     static String replaceReviewToken(String refererUri, String replacementText) {
-        Matcher matcher = reviewTokenPattern.matcher(refererUri);
-        return matcher.group(1) + replacementText;
+        Matcher matcher = REVIEW_TOKEN_PATTERN.matcher(refererUri);
+        if(matcher.matches()) {
+            return matcher.group(1) + replacementText;
+        } else {
+            return refererUri;
+        }
     }
 
+    /**
+     * Returns true if a string looks like a URL and contains 'token=...'
+     * @param refererUrl The string to test
+     * @return True if a token is found, false if not.
+     */
     static Boolean isReviewTokenPresent(String refererUrl) {
         if(refererUrl == null) {
             return false;
         } else {
-            return reviewTokenPattern.matcher(refererUrl).matches();
+            return REVIEW_TOKEN_PATTERN.matcher(refererUrl).matches();
         }
     }
 
@@ -194,7 +209,7 @@ public class SolrLogger
             if(originalReferer!=null) {
                 String cleanedReferer = originalReferer;
                 if(reviewTokenPresent) {
-                    cleanedReferer = replaceReviewToken(originalReferer, dummyToken);
+                    cleanedReferer = replaceReviewToken(originalReferer, DUMMY_TOKEN);
                 }
                 doc1.addField("referrer", cleanedReferer);
             }
