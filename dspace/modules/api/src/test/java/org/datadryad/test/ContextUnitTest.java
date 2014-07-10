d436213c73be7b2114a6249bd32d2068ad4000c8
Dan Leehr <dan.leehr@nescent.org>
2014-07-10 16:12:53 -0400

    Startup dspace kernel in ContextUnitTest

--- dspace/modules/api/src/test/java/org/datadryad/test/ContextUnitTest.java
+++ dspace/modules/api/src/test/java/org/datadryad/test/ContextUnitTest.java
@@ -3,9 +3,14 @@
 package org.datadryad.test;
 
 import java.sql.SQLException;
+import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Context;
+import org.dspace.servicemanager.DSpaceKernelImpl;
+import org.dspace.servicemanager.DSpaceKernelInit;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import static org.junit.Assert.*;
 
 /**
@@ -14,6 +19,7 @@ import static org.junit.Assert.*;
  */
 public abstract class ContextUnitTest {
     protected Context context;
+    private static DSpaceKernelImpl kernel;
 
     @Before
     public void setUp() {
@@ -22,6 +28,14 @@ public abstract class ContextUnitTest {
             context.turnOffAuthorisationSystem();
         } catch (SQLException ex) {
             fail("Unable to instantiate context " + ex);
+        }
+    }
+
+    @BeforeClass
+    public static void setupClass() {
+        kernel = DSpaceKernelInit.getKernel(null);
+        if(!kernel.isRunning()) {
+            kernel.start(ConfigurationManager.getProperty("dspace.dir"));
         }
     }
 
@@ -34,4 +48,8 @@ public abstract class ContextUnitTest {
         }
     }
 
+    @AfterClass
+    public static void tearDownClass() {
+        kernel.destroy();
+    }
 }
