0a1fcfbea514419bd72bfda0c37cd57479e1b491
Dan Leehr <dan.leehr@nescent.org>
2014-08-29 10:02:28 -0400

    Switched organization storage back to JSON

--- dspace/modules/dryad-rest-webapp/src/main/java/org/datadryad/rest/storage/resolvers/OrganizationStorageResolver.java
+++ dspace/modules/dryad-rest-webapp/src/main/java/org/datadryad/rest/storage/resolvers/OrganizationStorageResolver.java
@@ -4,9 +4,11 @@ package org.datadryad.rest.storage.resolvers;
 
 import org.datadryad.rest.storage.rdbms.DatabaseOrganizationStorageImpl;
 import com.sun.jersey.spi.inject.SingletonTypeInjectableProvider;
+import java.io.File;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
 import org.datadryad.rest.storage.AbstractOrganizationStorage;
+import org.datadryad.rest.storage.json.OrganizationJSONStorageImpl;
 
 /**
  *
@@ -14,7 +16,12 @@ import org.datadryad.rest.storage.AbstractOrganizationStorage;
  */
 @Provider
 public class OrganizationStorageResolver extends SingletonTypeInjectableProvider<Context, AbstractOrganizationStorage> {
+    private static final String PATH = "/tmp/dryad_rest";
     public OrganizationStorageResolver() {
-        super(AbstractOrganizationStorage.class, new DatabaseOrganizationStorageImpl("/opt/dryad/config/dspace.cfg"));
+        super(AbstractOrganizationStorage.class, new OrganizationJSONStorageImpl(new File(PATH)));
+        File directory = new File(PATH);
+        if(!directory.exists()) {
+            directory.mkdir();
+        }
     }
 }
