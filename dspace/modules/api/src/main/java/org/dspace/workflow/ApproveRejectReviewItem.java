a09c80f9196fbc2e37cebbb59a9d1484aee8a9ec
Dan Leehr <dan.leehr@nescent.org>
2014-09-04 11:24:34 -0400

    Refactor ApproveRejectReviewItem to expose methods
    Previously was CLI-only.
    Added reviewItem methods for manuscript ID or workflow ID

--- dspace/modules/api/src/main/java/org/dspace/workflow/ApproveRejectReviewItem.java
+++ dspace/modules/api/src/main/java/org/dspace/workflow/ApproveRejectReviewItem.java
@@ -2,22 +2,13 @@ package org.dspace.workflow;
 
 import org.apache.commons.cli.*;
 import org.dspace.authorize.AuthorizeException;
-import org.dspace.content.DCValue;
 import org.dspace.content.DSpaceObject;
-import org.dspace.content.Item;
-import org.dspace.content.WorkspaceItem;
 import org.dspace.core.Context;
 import org.dspace.discovery.SearchService;
-import org.dspace.eperson.EPerson;
-import org.dspace.handle.HandleManager;
 import org.dspace.utils.DSpace;
 import org.dspace.workflow.actions.WorkflowActionConfig;
-import org.xml.sax.SAXException;
 
 import javax.mail.MessagingException;
-import javax.servlet.http.HttpServletRequest;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
@@ -29,10 +20,12 @@ import java.util.List;
  *
  * An executable class which determines if an item is to be rejected or accepted,
  * when accepted the submission is sent to the next step
+ *
+ * Updated by dcl9 (dan.leehr@nescent.org) 04-Sep-2014 - exposing reviewItem as static methods
  */
 public class ApproveRejectReviewItem {
 
-    public static void main(String[] args) throws SQLException, AuthorizeException, IOException, WorkflowConfigurationException, TransformerException, SAXException, ParserConfigurationException, WorkflowException, MessagingException, ParseException {
+    public static void main(String[] args) throws ApproveRejectReviewItemException, ParseException {
         // create an options object and populate it
         CommandLineParser parser = new PosixParser();
 
@@ -60,31 +53,60 @@ public class ApproveRejectReviewItem {
             return;
         }
 
-        Context c = new Context();
-        c.turnOffAuthorisationSystem();
-
-	WorkflowItem wfi = null;
         if(line.hasOption('m')){
 	    // get a WorkflowItem using a manuscript number
-	    String manuScriptNumber;
-	    manuScriptNumber = line.getOptionValue('m');
-	    List<DSpaceObject> manuscriptItems =
-		getSearchService().search(c, "dc.identifier.manuscriptNumber: " + manuScriptNumber, 0, 2, false);
-	    if(0 < manuscriptItems.size()){
-		wfi = WorkflowItem.findByItemId(c, manuscriptItems.get(0).getID());
-	    } else {
-		System.out.println("No items found with the matching manuscript number.");
-	    }
+	    String manuscriptNumber = line.getOptionValue('m');
+            reviewItem(approved, manuscriptNumber);
 	} else if(line.hasOption('i')) {
 	    // get a WorkflowItem using a workflow ID
-	    int wfItemId = Integer.parseInt(line.getOptionValue('i'));
-	    wfi = WorkflowItem.find(c, wfItemId);
+	    Integer wfItemId = Integer.parseInt(line.getOptionValue('i'));
+	    reviewItem(approved, wfItemId);
 	} else {
             System.out.println("No manuscript number or workflow ID was given. One of these must be provided to identify the correct item in the review stage.");
             System.exit(1);
             return;
         }
-	
+    }
+
+    public static void reviewItem(Boolean approved, String manuscriptNumber) throws ApproveRejectReviewItemException  {
+        WorkflowItem wfi = null;
+        try {
+            Context c = new Context();
+            c.turnOffAuthorisationSystem();
+            List<DSpaceObject> manuscriptItems =
+                    getSearchService().search(c, "dc.identifier.manuscriptNumber: " + manuscriptNumber, 0, 2, false);
+            if(manuscriptItems.size() > 0){
+                wfi = WorkflowItem.findByItemId(c, manuscriptItems.get(0).getID());
+            } else {
+                throw new IllegalArgumentException("No item found with manuscript number: " + manuscriptNumber);
+            }
+        } catch (SQLException ex) {
+            throw new ApproveRejectReviewItemException(ex);
+        }
+    }
+
+    public static void reviewItem(Boolean approved, Integer workflowItemId) throws ApproveRejectReviewItemException {
+        try {
+            Context c = new Context();
+            c.turnOffAuthorisationSystem();
+            WorkflowItem wfi = WorkflowItem.find(c, workflowItemId);
+            reviewItem(c, approved, wfi);
+        } catch (SQLException ex) {
+            throw new ApproveRejectReviewItemException(ex);
+        } catch (AuthorizeException ex) {
+            throw new ApproveRejectReviewItemException(ex);
+        } catch (WorkflowConfigurationException ex) {
+            throw new ApproveRejectReviewItemException(ex);
+        } catch (IOException ex) {
+            throw new ApproveRejectReviewItemException(ex);
+        } catch (MessagingException ex) {
+            throw new ApproveRejectReviewItemException(ex);
+        } catch (WorkflowException ex) {
+            throw new ApproveRejectReviewItemException(ex);
+        }
+    }
+
+    private static void reviewItem(Context c, Boolean approved, WorkflowItem wfi) throws SQLException, IOException, WorkflowConfigurationException, AuthorizeException, MessagingException, WorkflowException {
 	// get a List of ClaimedTasks, using the WorkflowItem
         List<ClaimedTask> claimedTasks = null;
 
@@ -92,13 +114,11 @@ public class ApproveRejectReviewItem {
             claimedTasks = ClaimedTask.findByWorkflowId(c, wfi.getID());
 	}
 
-	
         //Check for a valid task
         // There must be a claimed actions & it must be in the review stage, else it isn't a valid workflowitem
-        if(claimedTasks == null || claimedTasks.size() == 0 || !claimedTasks.get(0).getActionID().equals("reviewAction")){
-            System.out.println("Invalid manuscript number");
-            System.exit(1);
-        }else{
+        if(claimedTasks == null || claimedTasks.isEmpty() || !claimedTasks.get(0).getActionID().equals("reviewAction")){
+            throw new IllegalArgumentException("Item not found or not in review");
+        } else {
             ClaimedTask claimedTask = claimedTasks.get(0);
             Workflow workflow = WorkflowFactory.getWorkflow(wfi.getCollection());
             WorkflowActionConfig actionConfig = workflow.getStep(claimedTask.getStepID()).getActionConfig(claimedTask.getActionID());
@@ -106,20 +126,6 @@ public class ApproveRejectReviewItem {
             wfi.getItem().addMetadata(WorkflowRequirementsManager.WORKFLOW_SCHEMA, "step", "approved", null, approved.toString());
 
             WorkflowManager.doState(c, c.getCurrentUser(), null, claimedTask.getWorkflowItemID(), workflow, actionConfig);
-
-            //Now make sure that any hanging workspace items get removed
-            /*
-            WorkspaceItem[] workspaceItems = WorkspaceItem.findByEPerson(c, wfi.getSubmitter());
-            for (WorkspaceItem workspaceItem : workspaceItems) {
-                //Find out which ones are linked to our publication
-                Item dataPackage = DryadWorkflowUtils.getDataPackage(c, workspaceItem.getItem());
-                if(dataPackage != null && dataPackage.getID() == wfi.getItem().getID())
-                {
-                    //Remove the workspace item
-                    workspaceItem.deleteAll();
-                }
-            }
-            */
 
         }
         c.commit();
