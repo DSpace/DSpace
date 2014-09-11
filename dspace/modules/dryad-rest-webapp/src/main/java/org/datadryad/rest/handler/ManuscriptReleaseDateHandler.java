d62d9ae477acc995b76749e0ea96bee830e53df3
Dan Leehr <dan.leehr@nescent.org>
2014-09-11 16:56:36 -0400

    Handler for updating data package with pub date

---
+++ dspace/modules/dryad-rest-webapp/src/main/java/org/datadryad/rest/handler/ManuscriptReleaseDateHandler.java
@@ -0,0 +1,74 @@
+/*
+ */
+package org.datadryad.rest.handler;
+
+import java.sql.SQLException;
+import org.apache.log4j.Logger;
+import org.datadryad.api.DryadDataPackage;
+import org.datadryad.rest.models.Manuscript;
+import org.datadryad.rest.storage.StoragePath;
+import org.dspace.core.Context;
+
+/**
+ * Extracts publication date from manuscript and places in data package metadata
+ * (if exists)
+ * @author Dan Leehr <dan.leehr@nescent.org>
+ */
+public class ManuscriptReleaseDateHandler implements HandlerInterface<Manuscript>{
+    private static final Logger log = Logger.getLogger(ManuscriptReleaseDateHandler.class);
+
+    // Move to an abstract base class
+    private static Context getContext() {
+        Context context = null;
+        try {
+            context = new Context();
+        } catch (SQLException ex) {
+            log.error("Unable to instantiate DSpace context", ex);
+        }
+        return context;
+    }
+
+    private static void completeContext(Context context) throws SQLException {
+        try {
+            context.complete();
+        } catch (SQLException ex) {
+            // Abort the context to force a new connection
+            abortContext(context);
+            throw ex;
+        }
+    }
+
+    private static void abortContext(Context context) {
+        context.abort();
+    }
+
+    @Override
+    public void handleCreate(StoragePath path, Manuscript manuscript) throws HandlerException {
+        processChange(manuscript);
+    }
+
+    @Override
+    public void handleUpdate(StoragePath path, Manuscript manuscript) throws HandlerException {
+        processChange(manuscript);
+    }
+
+    @Override
+    public void handleDelete(StoragePath path, Manuscript object) throws HandlerException {
+
+    }
+
+    private void processChange(Manuscript manuscript) throws HandlerException {
+        try {
+            Context context = getContext();
+            String manuscriptId = manuscript.manuscriptId;
+            DryadDataPackage dataPackage = DryadDataPackage.findByManuscriptId(context, manuscriptId);
+            if(dataPackage == null) {
+                throw new HandlerException("Data package not found with manuscriptId: " + manuscriptId);
+            }
+            dataPackage.setPublicationDate(manuscript.publicationDate);
+            completeContext(context);
+        } catch (SQLException ex) {
+            throw new HandlerException("SQLException updating publicationDate", ex);
+        }
+    }
+}
