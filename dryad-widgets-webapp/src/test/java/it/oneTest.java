d1b574fec0d2e3529894321c7a65501e13338edd
Nathan Day <rnathanday@yahoo.com>
2014-09-25 12:03:46 -0500

    updating build script to separate unit tests from integration tests

---
+++ dryad-widgets-webapp/src/test/java/it/oneTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it;
+
+import com.thoughtworks.selenium.*;
+import static junit.framework.Assert.assertTrue;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class oneTest extends TestCase {
+    public void setUp() throws Exception {
+    }
+    @Test
+    public void testNew() throws Exception {
+        assertTrue(false);
+    }
+}
--- test/build_test.sh
+++ test/build_test.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env bash
 #
 
+set -x  # verbose bash
+set -e  # return fail on a failed command, for travis-ci
+
 TEST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE_DIR="$TEST_DIR/.."
 
+MVN="mvn -e"
+#MVN="mvn --quiet"
+
 # build and run junit tests
 cd "$BASE_DIR"
-mvn --quiet package -DskipTests=false
+$MVN clean package -DskipTests=true
+$MVN test
 
-# run Selenium tests
-export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
-sleep 3
-echo
-sleep 3
+# start virtual frame buffer if in Travis-CI env 
+if [ "$travis" ]; then
+    echo "Starting xvfb"
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start
+    # TODO: ping framebuffer for start instead of sleeping
+    sleep 3
+fi 
 
+# run selenium/integration tests
+$MVN failsafe:integration-test -DskipTests=true
+
+# Travis-CI clenup
+if [ "$travis" ]; then
+    sh -e /etc/init.d/xvfb stop
+fi
 
