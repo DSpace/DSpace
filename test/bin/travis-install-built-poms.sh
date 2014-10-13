a5e8dd35d4383a04e7284765f3ffb088cf2dbc62
Nathan Day <rnathanday@yahoo.com>
2014-10-13 19:31:05 +0000

    use groupId from jar file when installing jars to local m2 repo

--- test/bin/travis-install-built-poms.sh
+++ test/bin/travis-install-built-poms.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-set -euo pipefail
+#set -euo pipefail
+set -e
 
 VERSION="1.7.3-SNAPSHOT";
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -10,11 +11,11 @@ echo BIN_DIR: $BIN_DIR
 echo REPO_DIR: $REPO_DIR
 
 function install_file() {
-    GROUP="$1"
-    ARTIFACT="$2"
-    VERSION="$3"
-    FILE="$4"
+    FILE="$1"
+    ARTIFACT=`basename $g | sed "s/-${VERSION}.jar//"`;
+    GROUP=$(jar tf $FILE | grep "META-INF/maven/" | grep -v "$ARTIFACT" | grep -v pom  | sed 's/META-INF\/maven//g' | perl -pe 's:[/\s]+::g');
     echo Installing $GROUP $ARTIFACT
+    echo FILE: $FILE
     mvn install:install-file            \
         -DgroupId="$GROUP"              \
         -DartifactId="$ARTIFACT"        \
@@ -24,18 +25,18 @@ function install_file() {
     echo Success for mvn: $?
 }
 
+# install from dryad-repo dirs starting dspace-*
 for f in `ls "$REPO_DIR" | grep "dspace-"`;
 do
     for g in `find "$f" -name "*${VERSION}.jar" | grep -v lib`;
     do
-        ARTIFACT=`basename $g | sed "s/-${VERSION}.jar//"`;
-        install_file "org.dspace" "$ARTIFACT" "$VERSION" "$g";
+        install_file "$g";
     done;
 done;
 
+# install from dryad-repo/dspace/modules dirs
 for f in `find "$REPO_DIR/dspace/modules" -name "*${VERSION}.jar" | grep -v lib`;
 do
-    ARTIFACT=`basename $f | sed "s/-${VERSION}.jar//"`;
-    install_file "org.dspace.modules" "$ARTIFACT" "$VERSION" "$f";
+    install_file "$f";
 done;
 
