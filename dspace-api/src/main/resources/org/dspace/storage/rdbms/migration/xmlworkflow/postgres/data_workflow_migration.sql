c0fa55d9f6bc15b7fc99706b172d3e15422f151c
Tim Donohue <tdonohue@duraspace.org>
2014-10-14 21:49:11 +0000

    Add missing license headers

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/migration/xmlworkflow/postgres/data_workflow_migration.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/migration/xmlworkflow/postgres/data_workflow_migration.sql
@@ -1,4 +1,26 @@
-  -- Convert workflow groups:
+--
+-- The contents of this file are subject to the license and copyright
+-- detailed in the LICENSE and NOTICE files at the root of the source
+-- tree and available online at
+--
+-- http://www.dspace.org/license/
+--
+
+----------------------------------------------------
+-- Data Migration for XML/Configurable Workflow
+--
+-- This file will automatically migrate existing
+-- classic workflows to XML/Configurable workflows.
+-- NOTE however that the corresponding 
+-- "xml_workflow_migration.sql" script must FIRST be 
+-- called to create the appropriate database tables.
+--
+-- This script is called automatically by the following
+-- Flyway Java migration class:
+-- org.dspace.storage.rdbms.migration.V5_0_2014_01_01__XMLWorkflow_Migration
+----------------------------------------------------
+
+-- Convert workflow groups:
 INSERT INTO cwf_collectionrole (role_id, group_id, collection_id)
 SELECT
 'reviewer' AS role_id,
