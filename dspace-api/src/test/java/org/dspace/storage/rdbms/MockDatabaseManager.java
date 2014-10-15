ccad925b1a45cba4d8952a1ed2d9d93972971c0d
Tim Donohue <tdonohue@duraspace.org>
2014-10-15 11:37:08 -0500

    Tweak MockDatabaseManager to allow Unit Testing against a PostgreSQL or Oracle DB (for easier DB migration testing). Fix problematic H2 migration.

--- dspace-api/src/test/java/org/dspace/storage/rdbms/MockDatabaseManager.java
+++ dspace-api/src/test/java/org/dspace/storage/rdbms/MockDatabaseManager.java
@@ -13,6 +13,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Types;
 import java.util.List;
+import mockit.Invocation;
 
 import mockit.Mock;
 import mockit.MockUp;
@@ -23,11 +24,18 @@ import org.dspace.core.Context;
 import static org.dspace.storage.rdbms.DatabaseManager.getColumnNames;
 
 /**
- * Mocks a DatabaseManager so unit tests can be run without a real DB connection
- * The code is basically the same as the original DatabaseManager but it
- * establishes a connection to an in-memory database.
+ * Mocks a DatabaseManager to add some custom logic / queries to support the
+ * H2 in-memory database for Unit Testing. By default DSpace does not fully
+ * support H2, so this MockDatabaseManager modifies some methods of DatabaseManager
+ * which are currently problematic in H2.
+ * <P>
+ * Any methods which are NOT "mocked" below are just used as-is from the
+ * original DatabaseManager class. Also note that, in order to support running
+ * Unit Tests on PostgreSQL & Oracle, any H2-specific logic is only executed
+ * after verifying your Unit Testing database reports that it is indeed H2.
  *
  * @author pvillega
+ * @author tdonohue
  */
 public final class MockDatabaseManager
         extends MockUp<DatabaseManager>
@@ -35,8 +43,11 @@ public final class MockDatabaseManager
     // Set our logger to specify the Mock class, so we know which logs are from the "real" vs "mock" class
     private static final Logger log = Logger.getLogger(MockDatabaseManager.class);
 
-    /** Is our DBMS Oracle-like? */
+    // Is our DBMS Oracle-like? Determine this from the (initialized) DatabaseManager class
     private static final boolean isOracle = DatabaseManager.isOracle();
+
+    // Get the database type keyword from the (initialized) DatabaseManager class
+    private static final String dbms_keyword = DatabaseManager.getDbKeyword();
 
     /**
      * Override/Mock the default "setConstraintDeferred()" method in order to
@@ -51,32 +62,42 @@ public final class MockDatabaseManager
      * @throws SQLException
      */
     @Mock
-    public static void setConstraintDeferred(Context context,
+    public static void setConstraintDeferred(Invocation inv, Context context,
             String constraintName) throws SQLException
     {
-        log.debug("Mocked setContraintDeferred() method for H2 database");
-
-        Statement statement = null;
-        try
+        if(dbms_keyword!=null && !dbms_keyword.equals(DatabaseManager.DBMS_H2))
         {
-            statement = context.getDBConnection().createStatement();
-            //statement.execute("SET CONSTRAINTS " + constraintName + " DEFERRED");
-            // H2 does NOT support "SET CONSTRAINTS" syntax.
-            // Instead it requires the following SQL
-            statement.execute("SET REFERENTIAL_INTEGRITY FALSE");
-            statement.close();
-
+            // If we are unit testing with a non-H2 database, just proceed to
+            // DatabaseManager method of the same name
+            inv.proceed(context, constraintName);
         }
-        finally
+        else
         {
-            if (statement != null)
+            // Otherwise, we'll run slightly customized code in order to support H2
+            log.debug("Mocked setContraintDeferred() method for H2 database");
+
+            Statement statement = null;
+            try
             {
-                try
+                statement = context.getDBConnection().createStatement();
+                //statement.execute("SET CONSTRAINTS " + constraintName + " DEFERRED");
+                // H2 does NOT support "SET CONSTRAINTS" syntax.
+                // Instead it requires the following SQL
+                statement.execute("SET REFERENTIAL_INTEGRITY FALSE");
+                statement.close();
+
+            }
+            finally
+            {
+                if (statement != null)
                 {
-                    statement.close();
-                }
-                catch (SQLException sqle)
-                {
+                    try
+                    {
+                        statement.close();
+                    }
+                    catch (SQLException sqle)
+                    {
+                    }
                 }
             }
         }
@@ -95,31 +116,41 @@ public final class MockDatabaseManager
      * @throws SQLException
      */
     @Mock
-    public static void setConstraintImmediate(Context context,
+    public static void setConstraintImmediate(Invocation inv, Context context,
             String constraintName) throws SQLException
     {
-        log.debug("Mocked setContraintImmediate() method for H2 database");
-
-        Statement statement = null;
-        try
+        if(dbms_keyword!=null && !dbms_keyword.equals(DatabaseManager.DBMS_H2))
         {
-            statement = context.getDBConnection().createStatement();
-            //statement.execute("SET CONSTRAINTS " + constraintName + " IMMEDIATE");
-            // H2 does NOT support "SET CONSTRAINTS" syntax.
-            // Instead it requires the following SQL
-            statement.execute("SET REFERENTIAL_INTEGRITY TRUE");
-            statement.close();
+            // If we are unit testing with a non-H2 database, just proceed to
+            // DatabaseManager method of the same name
+            inv.proceed(context, constraintName);
         }
-        finally
+        else
         {
-            if (statement != null)
+            // Otherwise, we'll run slightly customized code in order to support H2
+            log.debug("Mocked setContraintImmediate() method for H2 database");
+
+            Statement statement = null;
+            try
             {
-                try
+                statement = context.getDBConnection().createStatement();
+                //statement.execute("SET CONSTRAINTS " + constraintName + " IMMEDIATE");
+                // H2 does NOT support "SET CONSTRAINTS" syntax.
+                // Instead it requires the following SQL
+                statement.execute("SET REFERENTIAL_INTEGRITY TRUE");
+                statement.close();
+            }
+            finally
+            {
+                if (statement != null)
                 {
-                    statement.close();
-                }
-                catch (SQLException sqle)
-                {
+                    try
+                    {
+                        statement.close();
+                    }
+                    catch (SQLException sqle)
+                    {
+                    }
                 }
             }
         }
@@ -142,134 +173,142 @@ public final class MockDatabaseManager
      *                If a database error occurs
      */
     @Mock
-    static TableRow process(ResultSet results, String table, List<String> pColumnNames) throws SQLException
+    static TableRow process(Invocation inv, ResultSet results, String table, List<String> pColumnNames) throws SQLException
     {
-        // Added for H2 debugging
-        log.debug("Mocked process() method for H2 database");
-
-        ResultSetMetaData meta = results.getMetaData();
-        int columns = meta.getColumnCount() + 1;
-
-        // If we haven't been passed the column names try to generate them from the metadata / table
-        List<String> columnNames = pColumnNames != null ? pColumnNames :
-                                        ((table == null) ? getColumnNames(meta) : getColumnNames(table));
-
-        TableRow row = new TableRow(DatabaseManager.canonicalize(table), columnNames);
-
-        // Process the columns in order
-        // (This ensures maximum backwards compatibility with
-        // old JDBC drivers)
-        for (int i = 1; i < columns; i++)
+        if(dbms_keyword!=null && !dbms_keyword.equals(DatabaseManager.DBMS_H2))
         {
-            String name = meta.getColumnName(i);
-            int jdbctype = meta.getColumnType(i);
+            // If we are unit testing with a non-H2 database, just proceed to
+            // DatabaseManager method of the same name
+            return inv.proceed(results, table, pColumnNames);
+        }
+        else
+        {
+            // Otherwise, we'll run slightly customized code in order to support H2
+            log.debug("Mocked process() method for H2 database");
 
-            // Added for H2 debugging
-            log.debug("In mocked process(), column '" + name + "' is of SQL Type " + jdbctype);
+            ResultSetMetaData meta = results.getMetaData();
+            int columns = meta.getColumnCount() + 1;
 
-            switch (jdbctype)
+            // If we haven't been passed the column names try to generate them from the metadata / table
+            List<String> columnNames = pColumnNames != null ? pColumnNames :
+                                            ((table == null) ? getColumnNames(meta) : getColumnNames(table));
+
+            TableRow row = new TableRow(DatabaseManager.canonicalize(table), columnNames);
+
+            // Process the columns in order
+            // (This ensures maximum backwards compatibility with
+            // old JDBC drivers)
+            for (int i = 1; i < columns; i++)
             {
-                case Types.BOOLEAN:
-                case Types.BIT:
-                    row.setColumn(name, results.getBoolean(i));
-                    break;
+                String name = meta.getColumnName(i);
+                int jdbctype = meta.getColumnType(i);
 
-                case Types.INTEGER:
-                    if (isOracle)
-                    {
-                        long longValue = results.getLong(i);
-                        if (longValue <= (long)Integer.MAX_VALUE)
+                // Added for H2 debugging
+                log.debug("In mocked process(), column '" + name + "' is of SQL Type " + jdbctype);
+
+                switch (jdbctype)
+                {
+                    case Types.BOOLEAN:
+                    case Types.BIT:
+                        row.setColumn(name, results.getBoolean(i));
+                        break;
+
+                    case Types.INTEGER:
+                        if (isOracle)
                         {
-                            row.setColumn(name, (int) longValue);
+                            long longValue = results.getLong(i);
+                            if (longValue <= (long)Integer.MAX_VALUE)
+                            {
+                                row.setColumn(name, (int) longValue);
+                            }
+                            else
+                            {
+                                row.setColumn(name, longValue);
+                            }
                         }
                         else
                         {
-                            row.setColumn(name, longValue);
+                            row.setColumn(name, results.getInt(i));
                         }
-                    }
-                    else
-                    {
-                        row.setColumn(name, results.getInt(i));
-                    }
-                    break;
+                        break;
 
-                case Types.NUMERIC:
-                case Types.DECIMAL:
-                    row.setColumn(name, results.getBigDecimal(i));
-                    break;
+                    case Types.NUMERIC:
+                    case Types.DECIMAL:
+                        row.setColumn(name, results.getBigDecimal(i));
+                        break;
 
-                case Types.BIGINT:
-                    row.setColumn(name, results.getLong(i));
-                    break;
+                    case Types.BIGINT:
+                        row.setColumn(name, results.getLong(i));
+                        break;
 
-                case Types.DOUBLE:
-                    row.setColumn(name, results.getDouble(i));
-                    break;
+                    case Types.DOUBLE:
+                        row.setColumn(name, results.getDouble(i));
+                        break;
 
-                case Types.CLOB:
-                    if (isOracle)
-                    {
-                        row.setColumn(name, results.getString(i));
-                    }
-                    else
-                    {
-                        throw new IllegalArgumentException("Unsupported JDBC type: " + jdbctype);
-                    }
-                    break;
-
-                case Types.VARCHAR:
-                    /*try
-                    {
-                        byte[] bytes = results.getBytes(i);
-
-                        if (bytes != null)
-                        {
-                            String mystring = new String(results.getBytes(i), "UTF-8");
-                            row.setColumn(name, mystring);
-                        }
-                        else
+                    case Types.CLOB:
+                        if (isOracle)
                         {
                             row.setColumn(name, results.getString(i));
                         }
-                    }
-                    catch (UnsupportedEncodingException e)
-                    {
-                        log.error("Unable to parse text from database", e);
-                    }*/
+                        else
+                        {
+                            throw new IllegalArgumentException("Unsupported JDBC type: " + jdbctype);
+                        }
+                        break;
 
-                    // H2 assumes that "getBytes()" should return hexidecimal.
-                    // So, the above commented out code will throw a JdbcSQLException:
-                    // "Hexadecimal string contains non-hex character"
-                    // Instead, we just want to get the value as a string.
-                    row.setColumn(name, results.getString(i));
-                    // END ADDED for H2
-                    break;
+                    case Types.VARCHAR:
+                        /*try
+                        {
+                            byte[] bytes = results.getBytes(i);
 
-                case Types.DATE:
-                    row.setColumn(name, results.getDate(i));
-                    break;
+                            if (bytes != null)
+                            {
+                                String mystring = new String(results.getBytes(i), "UTF-8");
+                                row.setColumn(name, mystring);
+                            }
+                            else
+                            {
+                                row.setColumn(name, results.getString(i));
+                            }
+                        }
+                        catch (UnsupportedEncodingException e)
+                        {
+                            log.error("Unable to parse text from database", e);
+                        }*/
 
-                case Types.TIME:
-                    row.setColumn(name, results.getTime(i));
-                    break;
+                        // H2 assumes that "getBytes()" should return hexidecimal.
+                        // So, the above commented out code will throw a JdbcSQLException:
+                        // "Hexadecimal string contains non-hex character"
+                        // Instead, we just want to get the value as a string.
+                        row.setColumn(name, results.getString(i));
+                        // END ADDED for H2
+                        break;
 
-                case Types.TIMESTAMP:
-                    row.setColumn(name, results.getTimestamp(i));
-                    break;
+                    case Types.DATE:
+                        row.setColumn(name, results.getDate(i));
+                        break;
 
-                default:
-                    throw new IllegalArgumentException("Unsupported JDBC type: " + jdbctype);
+                    case Types.TIME:
+                        row.setColumn(name, results.getTime(i));
+                        break;
+
+                    case Types.TIMESTAMP:
+                        row.setColumn(name, results.getTimestamp(i));
+                        break;
+
+                    default:
+                        throw new IllegalArgumentException("Unsupported JDBC type: " + jdbctype);
+                }
+
+                // Determines if the last column was null, and sets the tablerow accordingly
+                if (results.wasNull())
+                {
+                    row.setColumnNull(name);
+                }
             }
-
-            // Determines if the last column was null, and sets the tablerow accordingly
-            if (results.wasNull())
-            {
-                row.setColumnNull(name);
-            }
+            // Now that we've prepped the TableRow, reset the flags so that we can detect which columns have changed
+            row.resetChanged();
+            return row;
         }
-
-        // Now that we've prepped the TableRow, reset the flags so that we can detect which columns have changed
-        row.resetChanged();
-        return row;
     }
 }
