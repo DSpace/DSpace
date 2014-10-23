b82122e51722bb873011364b005ed992d3d99309
Adán Román Ruiz <aroman@arvo.es>
2014-10-23 11:32:07 +0200

    [DS-2099] Request-correction

---
+++ dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/SendRequestCorrectionAction.java
@@ -0,0 +1,103 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.xmlui.aspect.artifactbrowser;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.acting.AbstractAction;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.SourceResolver;
+import org.dspace.app.xmlui.utils.ContextUtil;
+import org.dspace.core.ConfigurationManager;
+import org.dspace.core.Context;
+import org.dspace.core.Email;
+import org.dspace.core.I18nUtil;
+import org.dspace.eperson.EPerson;
+
+/**
+ * @author Scott Phillips
+ * @author AdÃ¡n RomÃ¡n Ruiz at arvo.es
+ */
+
+public class SendRequestCorrectionAction extends AbstractAction
+{
+
+    /**
+     *
+     */
+    public Map act(Redirector redirector, SourceResolver resolver, Map objectModel,
+            String source, Parameters parameters) throws Exception
+    {
+        Request request = ObjectModelHelper.getRequest(objectModel);
+
+        String page = request.getParameter("page");
+       // String address = request.getParameter("email");
+        String agent = request.getHeader("User-Agent");
+        String session = request.getSession().getId();
+        String comments = request.getParameter("comments");
+        String handle = parameters.getParameter("handle");
+        
+        // Obtain information from request
+        // The page where the user came from
+        String fromPage = request.getHeader("Referer");
+
+        // User email from context
+        Context context = ContextUtil.obtainContext(objectModel);
+        EPerson loggedin = context.getCurrentUser();
+        String eperson = null;
+        if (loggedin != null)
+        {
+            eperson = loggedin.getEmail();
+        }
+
+        if (page == null || page.equals(""))
+        {
+            page = fromPage;
+        }
+
+        // Check all data is there
+        if ( (comments == null) || comments.equals("") 
+                || (handle == null) || handle.equals(""))
+        {
+            // Either the user did not fill out the form or this is the
+            // first time they are visiting the page.
+            Map<String,String> map = new HashMap<String,String>();
+            map.put("page",page);
+            map.put("comments",comments);
+            map.put("handle",handle);
+            
+            return map;
+        }
+
+        // All data is there, send the email
+        Email email = Email.getEmail(I18nUtil.getEmailFilename(context.getCurrentLocale(), "requestCorrection"));
+        email.addRecipient(ConfigurationManager.getProperty("requestCorrection.recipient"));
+
+        email.addArgument(new Date()); // Date
+        email.addArgument(eperson);    // Logged in as
+        email.addArgument(page.replace("/requestCorrection/", "/handle/"));       // Referring page
+        email.addArgument(agent);      // User agent
+        email.addArgument(session);    // Session ID
+        email.addArgument(comments);   // The feedback itself
+
+        // Replying to feedback will reply to email on form
+        email.setReplyTo(eperson);
+
+        // May generate MessageExceptions.
+        email.send();
+
+        // Finished, allow to pass.
+        return null;
+    }
+
+}
