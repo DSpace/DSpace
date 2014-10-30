cc97d79e9facb85ba3dcd756afb841f031e7a85b
riccardo fazio <r.fazio@cineca.it>
2014-10-30 14:28:20 +0100

    Remove unused imports

--- dspace-cris-ametrics/pubmed/lookup-jspui-api/src/main/java/org/dspace/app/cris/pmc/servlet/PMCServlet.java
+++ dspace-cris-ametrics/pubmed/lookup-jspui-api/src/main/java/org/dspace/app/cris/pmc/servlet/PMCServlet.java
@@ -10,10 +10,8 @@ package org.dspace.app.cris.pmc.servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
