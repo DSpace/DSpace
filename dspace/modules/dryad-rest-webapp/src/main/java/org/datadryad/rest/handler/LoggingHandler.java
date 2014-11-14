c4640b84c1a6dea3968f0db970517eece09824ec
Dan Leehr <dan.leehr@nescent.org>
2014-11-14 09:51:48 -0500

    Add date formatting to JSON logging

--- dspace/modules/dryad-rest-webapp/src/main/java/org/datadryad/rest/handler/LoggingHandler.java
+++ dspace/modules/dryad-rest-webapp/src/main/java/org/datadryad/rest/handler/LoggingHandler.java
@@ -3,14 +3,17 @@
 package org.datadryad.rest.handler;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
+import org.codehaus.jackson.util.DefaultPrettyPrinter;
 import org.datadryad.rest.storage.StoragePath;
 
 /**
  *
  * @author Dan Leehr <dan.leehr@nescent.org>
+ * @param <T> A type that can be serialized to JSON with jackson
  */
 public class LoggingHandler<T> implements HandlerInterface<T>{
     private static final Logger log = Logger.getLogger(LoggingHandler.class);
@@ -18,7 +21,8 @@ public class LoggingHandler<T> implements HandlerInterface<T>{
 
     public LoggingHandler() {
         ObjectMapper mapper = new ObjectMapper();
-        writer = mapper.writerWithDefaultPrettyPrinter();
+        mapper.setSerializationConfig(mapper.getSerializationConfig().withDateFormat(new SimpleDateFormat("yyyy-MM-dd")));
+        this.writer = mapper.writer(new DefaultPrettyPrinter());
     }
 
     private void logObject(String method, T object) {
@@ -34,19 +38,19 @@ public class LoggingHandler<T> implements HandlerInterface<T>{
 
     @Override
     public void handleCreate(StoragePath path, T object) throws HandlerException {
-        String method = "REST Create " + path.toString();
+        String method = "REST Create: " + path.toString();
         logObject(method, object);
     }
 
     @Override
     public void handleUpdate(StoragePath path, T object) throws HandlerException {
-        String method = "REST Update " + path.toString();
+        String method = "REST Update: " + path.toString();
         logObject(method, object);
     }
 
     @Override
     public void handleDelete(StoragePath path, T object) throws HandlerException {
-        String method = "REST Delete " + path.toString();
+        String method = "REST Delete: " + path.toString();
         logObject(method, object);
     }
 
