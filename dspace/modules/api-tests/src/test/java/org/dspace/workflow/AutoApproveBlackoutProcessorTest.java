a4b63b5415df9cbe9347f1b95ae6a4a5e88e419d
Dan Leehr <dan.leehr@nescent.org>
2014-12-02 11:01:40 -0500

    Fixes for testing through payment system workflow
    Prevent item getting stuck in reauth by setting journal subscription on
    the shopping cart
    shopping cart creation requires a current user on the context, so
    reorganized some code in the test
    
    Now passing

--- dspace/modules/api-tests/src/test/java/org/dspace/workflow/AutoApproveBlackoutProcessorTest.java
+++ dspace/modules/api-tests/src/test/java/org/dspace/workflow/AutoApproveBlackoutProcessorTest.java
@@ -9,6 +9,10 @@ import org.apache.log4j.Logger;
 import org.datadryad.api.DryadDataPackage;
 import org.datadryad.test.ContextUnitTest;
 import org.dspace.content.WorkspaceItem;
+import org.dspace.eperson.EPerson;
+import org.dspace.paymentsystem.PaymentSystemService;
+import org.dspace.paymentsystem.ShoppingCart;
+import org.dspace.utils.DSpace;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -36,22 +40,40 @@ public class AutoApproveBlackoutProcessorTest extends ContextUnitTest {
         futureDate = calendar.getTime();
     }
 
+    private void enableJournalSubscription(DryadDataPackage dataPackage) throws Exception {
+        PaymentSystemService paymentSystemService = new DSpace().getSingletonService(PaymentSystemService.class);
+        ShoppingCart shoppingCart = paymentSystemService.getShoppingCartByItemId(context,dataPackage.getItem().getID());
+        shoppingCart.setJournalSub(true);
+        shoppingCart.update();
+    }
+
     /**
      * Tests that an item in blackout can be approved.
      * Creates a bare item in the workspace and invokes two processors to
-     * send it to blackout then archive
+     * send it to blackout then archive. Works around payment system by
+     * setting all shopping carts with journal subscription = true
      * @throws Exception
      */
     @Test
     public void testApproveFromBlackoutWithPastDate() throws Exception {
+        // Payment system cannot create a shopping cart unless
+        // context.currentUser is set
+        EPerson systemCurator = AutoWorkflowProcessor.getSystemCurator(context);
+        context.setCurrentUser(systemCurator);
+
         DryadDataPackage dataPackage = DryadDataPackage.createInWorkspace(context);
-        WorkspaceItem wsi = dataPackage.getWorkspaceItem(context);
         // ReviewStep email fails if no submitter, so we need to set one.
         dataPackage.getItem().setSubmitter(AutoWorkflowProcessor.getSystemCurator(context));
 
+        // Current workflow moves items through payment processing steps/actions
+        // To prevent the item from requiring real payment processing, we update
+        // its shopping cart record as soon as it exists.
+        enableJournalSubscription(dataPackage);
+
         // Uses WorkflowManager.start(), which invokes the entire workflow process
-        // Other tests using DryadDataPackage simply createInWorkflow, as they do not
-        // need the entire set of tasks/steps.
+        // Other tests that use DryadDataPackage simply createInWorkflow, as
+        // they do not need the entire set of tasks/steps.
+        WorkspaceItem wsi = dataPackage.getWorkspaceItem(context);
         WorkflowManager.start(context, wsi);
 
         AutoCurateToBlackoutProcessor curateToBlackoutProcessor = new AutoCurateToBlackoutProcessor(context);
