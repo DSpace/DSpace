c4927660e203da64bea1c64d8b95357192a788e2
Dan Leehr <dan.leehr@nescent.org>
2014-12-02 11:35:04 -0500

    Always dereference symlinks when installing test dir

--- test/bin/travis-install-dryad-dir.sh
+++ test/bin/travis-install-dryad-dir.sh
@@ -2,4 +2,4 @@
 
 sudo mkdir -p -m 0755 /opt/dryad-test
 sudo chown -R $USER /opt/dryad-test
-cp -r $TRAVIS_BUILD_DIR/test/config /opt/dryad-test/config
+cp -L -r $TRAVIS_BUILD_DIR/test/config /opt/dryad-test/config
