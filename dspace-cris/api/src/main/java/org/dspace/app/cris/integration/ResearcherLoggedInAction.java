48f707383cb2f3cdddce8d9d6bea915c620f343d
Andrea Bollini <a.bollini@cineca.it>
2014-12-13 18:31:38 +0100

    avoid NPE if there are not matching researcher profile by source

--- dspace-cris/api/src/main/java/org/dspace/app/cris/integration/ResearcherLoggedInAction.java
+++ dspace-cris/api/src/main/java/org/dspace/app/cris/integration/ResearcherLoggedInAction.java
@@ -2,6 +2,7 @@ package org.dspace.app.cris.integration;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.StringUtils;
 import org.dspace.app.cris.model.ResearcherPage;
 import org.dspace.app.cris.service.ApplicationService;
 import org.dspace.authenticate.PostLoggedInAction;
@@ -19,29 +20,32 @@ public class ResearcherLoggedInAction implements PostLoggedInAction
     public void loggedIn(Context context, HttpServletRequest request,
             EPerson eperson)
     {
-        
         try
         {
             boolean save = false;
-            ResearcherPage rp = applicationService.getResearcherPageByEPersonId(eperson.getID());
-            if(rp==null) {
-				rp = applicationService.getEntityBySourceId(netidSourceRef,
-						eperson.getNetid(), ResearcherPage.class);
-                if(rp.getEpersonID()!=null) {
-                    if (rp.getEpersonID() != eperson.getID())
-                    {
-                        rp.setEpersonID(eperson.getID());
-                        save = true;
-                    }
-                }
-                else {
-                    rp.setEpersonID(eperson.getID());
-                    save = true;
-                }
-            }
-            if (save)
-            {
-                applicationService.saveOrUpdate(ResearcherPage.class, rp);
+            if (StringUtils.isNotBlank(eperson.getNetid())) {
+	            ResearcherPage rp = applicationService.getResearcherPageByEPersonId(eperson.getID());
+	            if(rp==null) {
+					rp = applicationService.getEntityBySourceId(netidSourceRef,
+							eperson.getNetid(), ResearcherPage.class);
+	                if(rp != null) {
+	                	if (rp.getEpersonID()!=null) {
+		                    if (rp.getEpersonID() != eperson.getID())
+		                    {
+		                        rp.setEpersonID(eperson.getID());
+		                        save = true;
+		                    }
+		                }
+		                else {
+		                    rp.setEpersonID(eperson.getID());
+		                    save = true;
+		                }
+	                }
+	            }
+	            if (save)
+	            {
+	                applicationService.saveOrUpdate(ResearcherPage.class, rp);
+	            }
             }
         }
         catch (Exception e)
