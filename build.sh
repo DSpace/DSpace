7256f9f2b00afe2c61603ad3e445d94bb53044e0
Ariel Lira <arieljlira@gmail.com>
2014-12-15 06:06:07 -0300

    Habilito el script helper para deploy. Por ahora solo uso update

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 set -e
 
 
-JAVA_OPTS="-Xmx1024m"
-MVN_OPTS="-q -P \!dspace-jspui,\!dspace-rdf,\!dspace-sword,\!dspace-swordv2"
+#JAVA_OPTS="JAVA_OPTS=-Xmx1024m"
+
+ANT_OPTS="-q"
+MVN_OPTS="-q"
+MVN_PROFILES=\!dspace-jspui,\!dspace-rdf,\!dspace-sword,\!dspace-swordv2,\!dspace-rest
 # -Dhttp.proxyHost=10.1.0.27 -Dhttp.proxyPort=3128 -Dhttps.proxyHost=10.1.0.27 -Dhttps.proxyPort=3128"
 
 cwd=`pwd`
@@ -14,9 +17,57 @@ TOMCAT="tomcat7"
 #/var/dspace/source
 #/var/dspace/install
 
-install()
-{
-mvn package  -P $MVN_PROFILES
+
+install() { 
+
+        if [ -d "$DSPACE_DIR" ] ; then
+                read -n 1 -p "El directorio de instalacion ( $DSPACE_DIR ) ya existe, desea eliminarlo [YySs]? "
+                if [[ ! $REPLY =~ ^[YySs]$ ]]; then
+                    exit 1
+                fi
+                rm -fr $DSPACE_DIR
+        fi
+        mkdir $DSPACE_DIR
+
+#si ya existe una instalacion vieja
+#if db exists
+
+#test: 
+psql -h data.sedici.unlp.edu.ar -Udspace_cic -W
+dropdb --username=pg_root --host=data.sedici.unlp.edu.ar dspace_cic --if-exists -i
+#endif
+createdb --username=pg_root --encoding=UNICODE --owner=dspace_cic --host=data.sedici.unlp.edu.ar dspace_cic
+
+#En caso que sea una versión snapshot, se compila en el nivel base de dspace-src para que maven encuentre las dependencias
+cd $DSPACE_SRC
+mvn install clean $MVN_OPTS -P $MVN_PROFILES
+
+cd $DSPACE_SRC/dspace 
+mvn package $MVN_OPTS -P $MVN_PROFILES
+
+sudo /etc/init.d/$TOMCAT stop
+
+cd $DSPACE_SRC/dspace/target/dspace-installer/
+ant fresh_install $ANT_OPTS
+
+#importación de metadatos CIC
+$DSPACE_DIR/bin/dspace dsrun org.dspace.administer.MetadataImporter -f $DSPACE_DIR/config/registries/cic-types.xml
+
+
+$DSPACE_DIR/bin/dspace create-administrator
+
+#if --deploy
+rm -r /var/lib/$TOMCAT/webapps/ROOT
+ln -s $DSPACE_DIR/webapps/xmlui/ /var/lib/$TOMCAT/webapps/ROOT
+ln -s $DSPACE_DIR/webapps/solr/ /var/lib/$TOMCAT/webapps/solr
+#endif
+
+
+sudo /etc/init.d/$TOMCAT start
+
+#Inicialización de los índices de discovery
+./dspace index-discovery
+
 
 }
 
@@ -30,7 +81,7 @@ update()
 	
 	show_message "Empaquetamos dspace"
 	cd dspace 
-	$JAVA_OPTS mvn package $MVN_OPTS
+	$JAVA_OPTS mvn package $MVN_OPTS -P $MVN_PROFILES
 	
 	show_message "Paramos el tomcat"
 	sudo /etc/init.d/$TOMCAT stop
@@ -41,13 +92,14 @@ update()
 	
 	show_message "actualizamos los sources"
 	cd target/dspace-installer
-	ANT_OPTS=$JAVA_OPTS ant update -q 
+	#TODO reusar el /var/dspace/install/config/GeoLiteCity.dat
+	$JAVA_OPTS ant update $ANT_OPTS 
 	
 	show_message "eliminamos directorios de bkp viejos"
 	ant clean_backups -Ddspace.dir=$DSPACE_DIR
 	#rm -r $DSPACE_DIR/bin.bak-* $DSPACE_DIR/etc.bak-* $DSPACE_DIR/lib.bak-* $DSPACE_DIR/webapps.bak-*
 	cd $DSPACE_SRC
-	$JAVA_OPTS mvn clean $MAVEN_OPTS
+	$JAVA_OPTS mvn clean
 	
 	show_message "iniciamos tomcat"
 	sudo /etc/init.d/$TOMCAT start
@@ -68,4 +120,21 @@ show_message()
    echo "#################################################################################################"
    echo "#################################################################################################"
 }
+
+
+
+case "$1" in
+  install)
+	install
+        ;;
+  update)
+	update
+        ;;
+  *)
+        echo "Usage: $0 {install|update}" >&2
+        exit 3
+        ;;
+esac
+
+exit 0
 
