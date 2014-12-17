2b9d749384d272751cf6fed28513c54d55e2e6b2
Nathan Day <rnathanday@yahoo.com>
2014-12-17 17:11:41 +0000

    add 'human-subjects' and 'large-data-packages' options to the DRI '/document/options' section for the data-file submisison page

--- dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/DescribeDatasetStep.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/DescribeDatasetStep.java
@@ -230,6 +230,12 @@ public class DescribeDatasetStep extends AbstractSubmissionStep {
             actions.addButton(AbstractProcessingStep.NEXT_BUTTON).setValue(T_next);
     }
 
+    public void addOptions(Options options) throws SAXException, WingException,
+            SQLException, IOException, AuthorizeException
+    {
+        options.addList("human-subjects");
+        options.addList("large-data-packages");
+    }
 
 
     public List addReviewSection(List list) throws SAXException, WingException, SQLException, IOException, AuthorizeException {
