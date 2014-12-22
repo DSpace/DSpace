b474e0104b2aebeba69c3066031be0e2e0167937
lantian <lantian@atmire.com>
2014-12-22 13:50:33 -0800

    Fix the journal manuscript doesn't fill in automaticly problem

--- dspace/modules/xmlui/src/main/webapp/themes/Mirage/lib/js/utils.js
+++ dspace/modules/xmlui/src/main/webapp/themes/Mirage/lib/js/utils.js
@@ -271,12 +271,15 @@ function initFirstSubmissionForm() {
                 }
                 //else if (journal.indexOf('*') != -1) {
                 else if(integrated == true){
-                    jQuery("#aspect_submission_StepTransformer_item_manu-number-status-accepted").show();
-                    jQuery("#aspect_submission_StepTransformer_item_manu_accepted-cb").hide();
-                }
-                else {
                     jQuery("#aspect_submission_StepTransformer_item_manu_accepted-cb").show();
                     jQuery("#aspect_submission_StepTransformer_item_manu-number-status-accepted").hide();
+                    //update the order form after enter journal name
+                    updateOrder();
+                }
+                else {
+                    jQuery("#aspect_submission_StepTransformer_item_manu-number-status-accepted").show();
+                    jQuery("#aspect_submission_StepTransformer_item_manu_accepted-cb").hide();
+
                     //update the order form after enter journal name
                     updateOrder();
                 }
