c03d4dacddb10f79e638b905b3abb2a4e5f0ead4
Nathan Day <rnathanday@yahoo.com>
2014-12-29 21:33:29 +0000

    display formatted citation only in the 'Your data package' section of the 'Review' page during submission

--- dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/OverviewStep.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/OverviewStep.java
@@ -84,9 +84,7 @@ public class OverviewStep extends AbstractStep {
             pubDiv.setHead(T_STEPS_HEAD_1);
             //TODO: expand this !
 
-            pubDiv.addPara().addXref(HandleManager.resolveToURL(context, publication.getHandle()), publication.getName());
-
-            // display a formatted reference
+            // display a formatted reference to the data package
             ReferenceSet refSet = pubDiv.addReferenceSet("submission", ReferenceSet.TYPE_SUMMARY_LIST);
             refSet.addReference(publication);
 
