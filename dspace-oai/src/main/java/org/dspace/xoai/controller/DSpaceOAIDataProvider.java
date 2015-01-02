dc4b9b7c193c6e5e985652a16c8b107d2a59a0e8
Joao Melo <jmelo@lyncode.com>
2015-01-02 18:39:16 +0000

    DS-2262 Checking if the context is valid before aborting it.

--- dspace-oai/src/main/java/org/dspace/xoai/controller/DSpaceOAIDataProvider.java
+++ dspace-oai/src/main/java/org/dspace/xoai/controller/DSpaceOAIDataProvider.java
@@ -144,7 +144,7 @@ public class DSpaceOAIDataProvider
     }
 
     private void closeContext(Context context) {
-        if (context != null)
+        if (context != null && context.isValid())
             context.abort();
     }
 
