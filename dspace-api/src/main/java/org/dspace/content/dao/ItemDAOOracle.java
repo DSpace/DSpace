2141711e1c991729912985c85c6d89183c17c639
Mark H. Wood <mwood@IUPUI.Edu>
2015-01-08 13:53:45 -0500

    [DS-2395] Fix missing close parenthesis, replace subquery with the value it always returns

--- dspace-api/src/main/java/org/dspace/content/dao/ItemDAOOracle.java
+++ dspace-api/src/main/java/org/dspace/content/dao/ItemDAOOracle.java
@@ -14,67 +14,75 @@ import org.dspace.storage.rdbms.TableRowIterator;
 import org.dspace.storage.rdbms.TableRow;
 
 import java.sql.SQLException;
+import org.dspace.content.MetadataField;
 import org.dspace.content.MetadataSchema;
 import org.dspace.core.Constants;
 
 public class ItemDAOOracle extends ItemDAO
 {
-    private static final String selectPrimaryBitstreamID =
-        "SELECT bundle.primary_bitstream_id"
-            + " FROM item2bundle"
-            + "  JOIN bundle USING (bundle_id)"
-            + "  JOIN metadatavalue MD1 ON (MD1.resource_type_id = " +  Constants.BUNDLE
-            + "   AND MD1.resource_id = bundle_id"
-            + "   AND MD1.metadata_field_id ="
-            + "   (SELECT metadata_field_id FROM MetadataFieldRegistry"
-            + "     WHERE metadata_schema_id = " + MetadataSchema.DC_SCHEMA_ID
-            + "      AND element = 'title' AND qualifier IS NULL)"
-            + "   )"
-            + " WHERE item2bundle.item_id=?"
-            + "  AND MD1.text_value=?";
+    private static String selectPrimaryBitstreamID;
 
-    private static final String selectFirstBitstreamID =
-        "SELECT bundle2bitstream.bitstream_id"
-            + " FROM item2bundle"
-            + "  JOIN bundle USING (bundle_id)"
-            + "  JOIN bundle2bitstream USING (bundle_id)"
-            + "  JOIN metadatavalue MD1 ON (MD1.resource_type_id = " +  Constants.BUNDLE
-            + "   AND MD1.resource_id = bundle_id"
-            + "   AND MD1.metadata_field_id ="
-            + "   (SELECT metadata_field_id FROM MetadataFieldRegistry"
-            + "     WHERE metadata_schema_id = " + MetadataSchema.DC_SCHEMA_ID
-            + "      AND element = 'title' AND qualifier IS NULL)"
-            + "   )"
-            + " WHERE item2bundle.item_id=?"
-            + "  AND MD1.text_value=?";
+    private static String selectFirstBitstreamID;
 
-    private static final String selectNamedBitstreamID =
-        "SELECT bitstream.bitstream_id"
-            + " FROM item2bundle"
-            + "  JOIN bundle USING (bundle_id)"
-            + "  JOIN bundle2bitstream USING (bundle_id)"
-            + "  JOIN bitstream USING (bitstream_id)"
-            + "  JOIN metadatavalue MD1 ON (MD1.resource_type_id = " +  Constants.BUNDLE
-            + "   AND MD1.resource_id = bundle_id"
-            + "   AND MD1.metadata_field_id ="
-            + "   (SELECT metadata_field_id FROM MetadataFieldRegistry"
-            + "     WHERE metadata_schema_id = " + MetadataSchema.DC_SCHEMA_ID
-            + "      AND element = 'title' AND qualifier IS NULL)"
-            + "   )"
-            + "  JOIN metadatavalue MD2 ON (MD2.resource_type_id = " +  Constants.BITSTREAM
-            + "   AND MD2.resource_id = bitstream_id"
-            + "   AND MD2.metadata_field_id ="
-            + "   (SELECT metadata_field_id FROM MetadataFieldRegistry"
-            + "     WHERE metadata_schema_id = " + MetadataSchema.DC_SCHEMA_ID
-            + "      AND element = 'title' AND qualifier IS NULL)"
-            + "   )"
-            + " WHERE item2bundle.item_id=?"
-            + "  AND MD1.text_value=? "
-            + "  AND MD2.text_value=?";
+    private static String selectNamedBitstreamID;
 
     ItemDAOOracle(Context ctx)
     {
         super(ctx);
+
+        int MD_FIELD_ID_NAME = -1;
+        try {
+            MD_FIELD_ID_NAME = MetadataField.findByElement(context,
+                    MetadataSchema.DC_SCHEMA_ID, "title", null).getFieldID();
+        } catch (SQLException ex) { /* SNH */ }
+
+        if (null == selectPrimaryBitstreamID)
+            selectPrimaryBitstreamID =
+                "SELECT bundle.primary_bitstream_id"
+                    + " FROM item2bundle"
+                    + "  JOIN bundle USING (bundle_id)"
+                    + "  JOIN metadatavalue MD1 ON ("
+                    + "   MD1.resource_type_id = " +  Constants.BUNDLE
+                    + "   AND MD1.resource_id = bundle_id"
+                    + "   AND MD1.metadata_field_id = " + MD_FIELD_ID_NAME
+                    + "  )"
+                    + " WHERE item2bundle.item_id=?"
+                    + "  AND MD1.text_value=?";
+
+        if (null == selectFirstBitstreamID)
+            selectFirstBitstreamID =
+                "SELECT bundle2bitstream.bitstream_id"
+                    + " FROM item2bundle"
+                    + "  JOIN bundle USING (bundle_id)"
+                    + "  JOIN bundle2bitstream USING (bundle_id)"
+                    + "  JOIN metadatavalue MD1 ON ("
+                    + "   MD1.resource_type_id = " +  Constants.BUNDLE
+                    + "   AND MD1.resource_id = bundle_id"
+                    + "   AND MD1.metadata_field_id = " + MD_FIELD_ID_NAME
+                    + "  )"
+                    + " WHERE item2bundle.item_id=?"
+                    + "  AND MD1.text_value=?";
+
+        if (null == selectNamedBitstreamID)
+            selectNamedBitstreamID =
+                "SELECT bitstream.bitstream_id"
+                    + " FROM item2bundle"
+                    + "  JOIN bundle USING (bundle_id)"
+                    + "  JOIN bundle2bitstream USING (bundle_id)"
+                    + "  JOIN bitstream USING (bitstream_id)"
+                    + "  JOIN metadatavalue MD1 ON ("
+                    + "   MD1.resource_type_id = " +  Constants.BUNDLE
+                    + "   AND MD1.resource_id = bundle_id"
+                    + "   AND MD1.metadata_field_id = " + MD_FIELD_ID_NAME
+                    + "  )"
+                    + "  JOIN metadatavalue MD2 ON ("
+                    + "   MD2.resource_type_id = " +  Constants.BITSTREAM
+                    + "   AND MD2.resource_id = bitstream_id"
+                    + "   AND MD2.metadata_field_id = " + MD_FIELD_ID_NAME
+                    + "  )"
+                    + " WHERE item2bundle.item_id=?"
+                    + "  AND MD1.text_value=? "
+                    + "  AND MD2.text_value=?";
     }
 
     @Override
