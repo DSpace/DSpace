81a8035df638deb2e26a63e314f630a0eacc783d
Nathan Day <rnathanday@yahoo.com>
2015-02-10 19:58:32 +0000

    fix merge error

--- dspace/modules/journal-landing/journal-landing-webapp/src/main/java/org/dspace/app/xmlui/aspect/journal/landing/ValidateRequest.java
+++ dspace/modules/journal-landing/journal-landing-webapp/src/main/java/org/dspace/app/xmlui/aspect/journal/landing/ValidateRequest.java
@@ -7,33 +7,40 @@ package org.dspace.app.xmlui.aspect.journal.landing;
 
 import java.util.Map;
 import java.util.HashMap;
+
 import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.cocoon.acting.AbstractAction;
 import org.apache.cocoon.environment.Redirector;
 import org.apache.cocoon.environment.SourceResolver;
+
+import org.apache.log4j.Logger;
+
 import static org.dspace.app.xmlui.aspect.journal.landing.Const.*;
+import org.dspace.submit.utils.DryadJournalSubmissionUtils;
 
 /**
  *
  * @author Nathan Day
  */
 public class ValidateRequest extends AbstractAction {
+
+    private static final Logger log = Logger.getLogger(ValidateRequest.class);
+
     @Override
-    public Map act(Redirector redirector, SourceResolver resolver, Map objectModel, 
-                    String source, Parameters parameters) throws Exception 
+    public Map act(Redirector redirector, SourceResolver resolver, Map objectModel,
+                    String source, Parameters parameters) throws Exception
     {
-        String journalAbbr = parameters.getParameter(PARAM_JOURNAL_ABBR);
-        if (journalAbbr == null || journalAbbr.length() == 0) return null;
+        String journalName = parameters.getParameter(PARAM_JOURNAL_NAME);
+        if (journalName == null || journalName.length() == 0) return null;
 
         // verify we have an accurate journal
-        if (true) {
+        String journalAbbr = DryadJournalSubmissionUtils.findKeyByFullname(journalName);
+        if (journalAbbr != null && journalAbbr.length() != 0) {
             Map map = new HashMap();
-            map.put(PARAM_JOURNAL_NAME, "Dryad Testing Journal");
-            map.put(PARAM_JOURNAL_ABBR, "DTJ");
+            map.put(PARAM_JOURNAL_NAME, journalName);
+            map.put(PARAM_JOURNAL_ABBR, journalAbbr);
             return map;
         }
-        // journal not found, signal invalid request with failed action
-        // by passing back a non-map object
         return null;
     }
 }
