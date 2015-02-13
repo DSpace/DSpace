39988987900de91318a15617d9336e00d9d20a27
Hardy Pottinger <pottingerhj@missouri.edu>
2015-02-13 22:40:54 +0000

    [DS-2034] added emptystring handling to the notnull method in ControlPanel.java

--- dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/administrative/ControlPanel.java
+++ dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/administrative/ControlPanel.java
@@ -482,7 +482,7 @@ public class ControlPanel extends AbstractDSpaceTransformer implements Serviceab
      * @param value candidate string.
      * @return {@code value} or a constant indicating an unset value.
      */
-    private static String notnull(String value) { return null == value ? T_UNSET : value; }
+    private static String notnull(String value) { return "".equals(value) ? T_UNSET : value; }
 
     /**
      * List important DSpace configuration parameters.
