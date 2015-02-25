0b4d5b81db8267ecbc466d76b58ddb3fd0e70f72
Christian David Criollo <cdcriollo@icesi.edu.co>
2015-02-25 10:51:34 -0500

    -Se modifico el servlet para adicionar el metodo que se encarga de realizar la exportación en formato csv.

--- dspace/modules/jspui/src/main/java/org/dspace/app/webui/servlet/BibliographyServlet.java
+++ dspace/modules/jspui/src/main/java/org/dspace/app/webui/servlet/BibliographyServlet.java
@@ -544,34 +544,92 @@ public class BibliographyServlet extends DSpaceServlet{
      
          public String renderCSV(Item item, HttpServletRequest request)  {
              
-          String header= "Authors,Title,Year,Abstract,Author Keywords,Editors,Publisher,DOI,Language of Original Document,Document Type,Source";    
-          String filename= "prueba.csv";
+          //String header= "Authors,Title,Year,Abstract,Author Keywords,Editors,Publisher,Document Type,Source";    
+          //Header para articulos o articulos de revista
+          String headerArticle="Authors,Title,Year,Link,Abstract,Author Keywords,Publisher,language of Original Document,Issue,Page start,Page end,Document Type";
+          //Header para trabajos de grado,tesis de maestria, doctorado
+          String headerThesis="Authors,Title,Year,Link,Abstract,Author Keywords,Editors,Publisher,pubplace,Language of Original Document,Document Type";
+          //Header para libros o partes de libro
+          String headerChapBook="Authors,Title,Year,Link,Abstract,Author Keywords,Editors,isbn,Publisher, pubplace, Document Type"; 
+          
           String delimitador = ",";
           String separador = "\n";
           String schema = "dc";
+          String header="";
           
           //define needed metadatafields
             int csvType = 1;
             String[] DC2Bib = new String[5];
             // Metadatos para articulos o articulos de revista
-            DC2Bib[1] = "dc.creator, dc.title, dc.relation.ispartofseries, dc.publisher, dc.date.issued, dc.identifier.issn, dc.description.abstract, dc.subject, dc.identifier.uri";
+            DC2Bib[1] = "dc.creator, dc.title, dc.date.issued, dc.identifier.uri, dc.description.abstract, dc.subject, dc.Publisher, dc.language.iso, dc.citation.issue, dc.citation.spage, dc.citation.epage, dc.type";
             // Metadatos para libros o partes de libro
-            DC2Bib[2] = "dc.creator, dc.contributor, dc.title, dc.publisher, dc.pubplace, dc.date.issued, dc.identifier.isbn,  dc.identifier.uri, dc.description.abstract, dc.subject";
+            DC2Bib[2] = "dc.creator, dc.title, dc.date.issued, dc.identifier.uri, dc.description.abstract, dc.subject, dc.contributor, dc.identifier.isbn, dc.publisher,dc.pubplace, dc.type";
             //Metadatos para trabajos de grado, tesis de maestria, doctorado
-            DC2Bib[3] = "dc.creator, dc.contributor, dc.title, dc.publisher, dc.date.issued, dc.identifier.isbn,  dc.identifier.uri, dc.description.abstract, dc.subject";
+            DC2Bib[3] = "dc.creator, dc.title, dc.date.issued, dc.identifier.uri, dc.description.abstract, dc.subject, dc.contributor, dc.publisher, dc.pubplace, dc.language.iso, dc.type";
             
             StringBuffer sb = new StringBuffer();
+            
+            //Parsing metadatafields
+            boolean tyFound = false;
 
-         
-            //FileWriter fileWriter= new FileWriter(filename);
-            //fileWriter.append(header.toString());
-            //fileWriter.append(separador);
-              sb.append(header.toString());
-              sb.append(separador);
+        //First needed for BibTex: dc.type
+        DCValue[] types = item.getMetadata(schema, "type", Item.ANY, Item.ANY);
+
+        for (int j = 0; (j < types.length) && !tyFound; j++) {
+            String type = types[j].value;
+
+            //log.debug("DEBUG, Der gefundene Typ: " + type);//DEBUG
+
+            if (type.equalsIgnoreCase("Article")) {
+                csvType = 1;
+                header= headerArticle;
+                sb.append(header.toString());
+                sb.append(separador);
+                tyFound = true;
+            } else if (type.equalsIgnoreCase("Book")) {
+                csvType = 2;
+                header= headerChapBook;
+                sb.append(header.toString());
+                sb.append(separador);
+                tyFound = true;
+            } else if (type.equalsIgnoreCase("BookChapter") || type.equalsIgnoreCase("bookPart")) {
+                csvType = 2;
+                header= headerChapBook;
+                sb.append(header.toString());
+                sb.append(separador);
+                tyFound = true;
+            } else if (type.equalsIgnoreCase("Thesis")) {
+                csvType = 3;
+                header= headerThesis;
+                sb.append(header.toString());
+                sb.append(separador);
+                tyFound = true;
+            } else if (type.equalsIgnoreCase("Preprint")) {
+                csvType = 1;
+                header= headerArticle;
+                sb.append(header.toString());
+                sb.append(separador);
+                tyFound = true;
+            } else if(type.equalsIgnoreCase("bachelorThesis") || (type.equalsIgnoreCase("masterThesis") || (type.equalsIgnoreCase("doctoralThesis")))){
+                csvType = 3;
+                header= headerThesis;
+                sb.append(header.toString());
+                sb.append(separador);
+                tyFound = true; 
+            }
+        } 
+
+        //set type in case no type is given 
+        if (!tyFound) {
+            sb.append(headerArticle.toString());
+            sb.append(separador);
+            csvType = 1;
+        }
+            //sb.append(header.toString());
+            //sb.append(separador);
            
             StringTokenizer st = new StringTokenizer(DC2Bib[csvType], ",");
-            //String[] st=DC2Bib[bibType].split("\\,");
-
+           
         while (st.hasMoreTokens()) {
             String field = st.nextToken().trim();
             String[] eq = field.split("\\.");
@@ -585,35 +643,29 @@ public class BibliographyServlet extends DSpaceServlet{
             } else {
                 qualifier = null;
             }
+           
             //log.info("Field:"+field+";Tokens:"+eq.toString()+";Element:"+eq[1]+";Qualifier:"+qualifier);
             DCValue[] values = item.getMetadata(schema, element, qualifier, Item.ANY);
-                //log.info("DCVALUES:"+values);
-
+             // log.info("DCVALUES:"+values);
+            
             //Parse the metadata into a record
             for (int k = 0; k < values.length; k++) {
-                if (element.equals("contributor") || element.equals("creator")) {
+                if (element.equals("creator")) {
                     if (k == 0) {
-                        if (element.equals("creator")) {
-                             sb.append(values[k].value);
-                             sb.append(delimitador);
-                        } else {
-                            sb.append(values[k].value);
-                            sb.append(delimitador);
-                        }
-                    }
-                    if (k > 0) {
-                        if (element.equals("creator")) {
-                            sb.append(values[k].value);
-                            sb.append(delimitador);
-                        } else {
-                            sb.append(values[k].value);
-                            sb.append(delimitador);
-                        }
-                    }
+                        sb.append("\"");
+                        sb.append(values[k].value);
+                        sb.append("\"");
+                      } else if (k > 0) {
+                         sb.append("\"");
+                         sb.append(values[k].value);
+                         sb.append("\"");
+                       }
+                    
                 } else if (element.equals("title")) {
                     if (k == 0) {
+                        sb.append("\"");
                         sb.append(values[k].value);
-                        sb.append(delimitador);
+                        sb.append("\"");
                     }
                 } else if (element.equals("date")) {
                     if (k == 0) {
@@ -623,50 +675,83 @@ public class BibliographyServlet extends DSpaceServlet{
                         String date = dd.displayDate(false, false, UIUtil.getSessionLocale(request)).trim();
                         int last = date.length();
                         date = date.substring((last - 4), (last));
-
-                        sb.append(values[k].value);
-                        sb.append(delimitador);
+                        sb.append(date);
+                     
                     }
-                } else if (element.equals("description") && qualifier.equals("abstract")) {
+                    else {
                     if (k == 0) {
-                       sb.append(values[k].value);
-                       sb.append(delimitador);
-                    }
-                } else if (element.equals("subject")) {
-                    if (k == 0){
-                      sb.append(values[k].value);
-                      sb.append(delimitador);  
-                    }
-                    else{
-                      sb.append(values[k].value);
-                      sb.append(delimitador);  
-                    }
-                    
-                } else if(element.equals("publisher")){
-                    sb.append(values[k].value);
-                    sb.append(delimitador);
-                } else if (element.equals("identifier") && qualifier.equals("issn")) {
-                    if (k == 0) {
-                        sb.append(values[k].value);
-                        sb.append(delimitador);
-                    }
-                } else if (element.equals("relation") && qualifier.equals("ispartofseries")) {
-                    if (k == 0) {
-                        sb.append(values[k].value);
-                        sb.append(delimitador);
-                    }
-                } else if (element.equals("identifier") && qualifier.equals("uri")) {
-                    sb.append(values[k].value);
-                    sb.append(delimitador);
-                }  else if(element.equals("pubplace")){
-                    sb.append(values[k].value);
-                    sb.append(delimitador);
-                } else {
-                    if (k == 0) {
-                        sb.append(values[k].value);
-                        sb.append(delimitador);
+                        sb.append(qualifier + " " + values[k].value);
                     }
                   }
+                }  else if(element.equals("identifier") && qualifier.equals("uri")){
+                    if(k==0){
+                      sb.append(values[k].value);
+                    }
+                }
+                  else if (element.equals("description") && qualifier.equals("abstract")) {
+                    if (k == 0) {
+                       sb.append("\""); 
+                       sb.append(values[k].value);
+                       sb.append("\""); 
+                       //sb.append(delimitador);
+                    }
+                } else if (element.equals("subject")) {
+                    if ( k==0){
+                      sb.append("\""); 
+                      sb.append(values[k].value);
+                    }
+                    else if (k > 0){
+                      sb.append(values[k].value);
+                    }
+                     if(k == values.length-1){
+                       sb.append("\"");  
+                     }  
+                } else if(element.equals("contributor")){
+                   if (k == 0){
+                      sb.append("\"");  
+                      sb.append(values[k].value);
+                      sb.append("\"");  
+                    }
+                } else if(element.equals("publisher")){
+                   if(k==0){ 
+                    sb.append("\"");
+                    sb.append(values[k].value);
+                    sb.append("\"");
+                   }
+                    
+                } else if(element.equals("pubplace")){
+                   if(k==0){ 
+                    sb.append("\"");
+                    sb.append(values[k].value);
+                    sb.append("\"");
+                   }
+                    
+                } else if (element.equals("language") && qualifier.equals("iso")) {
+                    if (k == 0) {
+                        sb.append("\"");
+                        sb.append(values[k].value);
+                        sb.append("\"");
+                    }
+                } else if(element.equals("citation") && qualifier.equals("issue")){
+                    if(k==0){
+                      sb.append(values[k].value);
+                     
+                    }
+                } else if(element.equals("citation") && qualifier.equals("spage")){
+                    if(k==0){
+                      sb.append(values[k].value);
+                      
+                    }
+                } else if(element.equals("citation") && qualifier.equals("epage")){
+                    if(k==0){
+                      sb.append(values[k].value); 
+                    }
+                } else if (element.equals("type")) {
+                    if (k == 0) {
+                        sb.append(values[k].value);
+                    }
+                }
+                sb.append(delimitador);
             }
           }
           String csvData= sb.toString();
