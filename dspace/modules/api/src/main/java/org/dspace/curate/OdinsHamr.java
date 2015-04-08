29960a8185d9d836e185fce6b47e12f56a901a66
daisie_local <daisieh@gmail.com>
2015-04-08 00:21:01 -0700

    indents

--- dspace/modules/api/src/main/java/org/dspace/curate/OdinsHamr.java
+++ dspace/modules/api/src/main/java/org/dspace/curate/OdinsHamr.java
@@ -200,8 +200,8 @@ public class OdinsHamr extends AbstractCurationTask {
 	    log.fatal("Unable to close database connection", e);
 	}
 
-	log.info("ODIN's Hamr complete");
-	return Curator.CURATE_SUCCESS;
+        log.info("ODIN's Hamr complete");
+        return Curator.CURATE_SUCCESS;
 	}
 
     /**
@@ -215,25 +215,22 @@ public class OdinsHamr extends AbstractCurationTask {
        Levenshtein distance algorithm, borrowed from
        http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance
     **/
-    public static int computeLevenshteinDistance(CharSequence str1,
-						 CharSequence str2) {
-	int[][] distance = new int[str1.length() + 1][str2.length() + 1];
+    public static int computeLevenshteinDistance(CharSequence str1, CharSequence str2) {
+        int[][] distance = new int[str1.length() + 1][str2.length() + 1];
 
-	for (int i = 0; i <= str1.length(); i++)
-	    distance[i][0] = i;
-	for (int j = 1; j <= str2.length(); j++)
-	    distance[0][j] = j;
+        for (int i = 0; i <= str1.length(); i++)
+            distance[i][0] = i;
+        for (int j = 1; j <= str2.length(); j++)
+            distance[0][j] = j;
 
-	for (int i = 1; i <= str1.length(); i++)
-	    for (int j = 1; j <= str2.length(); j++)
-		distance[i][j] = minimum(
-					 distance[i - 1][j] + 1,
-					 distance[i][j - 1] + 1,
-					                                                 distance[i - 1][j - 1]
-					 + ((str1.charAt(i - 1) == str2.charAt(j - 1)) ? 0
-					    : 1));
+        for (int i = 1; i <= str1.length(); i++)
+            for (int j = 1; j <= str2.length(); j++)
+            distance[i][j] = minimum(
+                         distance[i - 1][j] + 1,
+                         distance[i][j - 1] + 1,
+                         distance[i - 1][j - 1] + ((str1.charAt(i - 1) == str2.charAt(j - 1)) ? 0 : 1));
 
-	return distance[str1.length()][str2.length()];
+        return distance[str1.length()][str2.length()];
     }
     
     /**
