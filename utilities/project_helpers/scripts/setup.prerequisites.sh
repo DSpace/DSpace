ac41496b65796acad2d0b7bffb737fd0e7e8e532
Ondrej Kosarko <ko_ok@centrum.cz>
2015-04-13 22:50:39 +0200

    adding jai_{core,imageio} libs back #3 & travis complains?

---
+++ utilities/project_helpers/scripts/setup.prerequisites.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# LINDAT/CLARIN setup libs
+#
+
+OUTDIR="../bits/libs"
+if [ ! -d "${OUTDIR}" ]; then
+    mkdir ${OUTDIR}
+fi
+
+
+download() {
+    VER=$1
+    if [ ! -f "${OUTDIR}/${VER}" ]; then
+        echo "Downloading ${VER} library"
+        wget -q http://www.java2s.com/Code/JarDownload/jai/${VER}.zip
+        unzip -q  ${VER}.zip
+        mv ${VER} ${OUTDIR}
+        rm -f ${VER}.zip
+    fi
+}
+
+
+VER_JAI_CORE=jai_core-1.1.2_01.jar
+VER_JAI_IMAGE=jai_imageio-1.0_01.jar
+
+download ${VER_JAI_CORE}
+download ${VER_JAI_IMAGE}
+
+
+echo "Installing libraries"
+
+# leave the version cause of dspace pom
+mvn install:install-file -Dfile=${OUTDIR}/${VER_JAI_IMAGE} -DgroupId=com.sun.media -DartifactId=jai_imageio -Dversion=1.0_01 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=${OUTDIR}/${VER_JAI_CORE} -DgroupId=javax.media -DartifactId=jai_core -Dversion=1.1.2_01 -Dpackaging=jar -DgeneratePom=true
+
+#VER_JARGON=jargon.jar
+#if [ -f "${OUTDIR}/${VER_JARGON}" ]; then
+#    mvn install:install-file -Dfile=${OUTDIR}/${VER_JARGON} -DgroupId=edu.sdsc.grid -DartifactId=jargon -Dversion=3.0.0 -Dpackaging=jar -DgeneratePom=true
+#else
+#    echo "\nWARNING: You must download ${VER_JARGON} version 3.0.0 and install it!"
+#fi
+
+
+
