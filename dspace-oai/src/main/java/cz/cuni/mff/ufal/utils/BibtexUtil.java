d3e526b2cba47615ed94d0f3f4a1c66c53881f50
jm <jm@server>
2015-05-01 09:10:44 +0200

    fixed #76 by changing the notation from \v{} to {\v}

--- dspace-oai/src/main/java/cz/cuni/mff/ufal/utils/BibtexUtil.java
+++ dspace-oai/src/main/java/cz/cuni/mff/ufal/utils/BibtexUtil.java
@@ -88,17 +88,22 @@ class BibtexString {
 		s = news;
 
 		// change accents with uppercase too
-		for (String ch : accents_todo_uppercase) {
+		for (String ch : accents) {
 			String to_find = ch.substring(0, 1);
 			String to_change_with = ch.substring(1).replaceAll("\\\\",
 					"\\\\\\\\");
 			s = s.replaceAll(to_find, to_change_with);
-			// uppercase only chars after {
-			int lbr_idx = to_change_with.indexOf("{");
+			// uppercase only chars before } without space
+			int lbr_idx = to_change_with.length() -1 ;
+			for ( ; 0 < lbr_idx; --lbr_idx ) {
+				char c = to_change_with.charAt(lbr_idx);
+				if ( ' ' == c || '\\' == c ) {
+					break;
+				}
+			}
 			// or only the last char
-			lbr_idx = lbr_idx == -1 ? to_change_with.length() - 2 : lbr_idx;
 			String to_change_with_upper = to_change_with.substring(0, lbr_idx)
-					+ to_change_with.substring(lbr_idx).toUpperCase();
+				+ to_change_with.substring(lbr_idx).toUpperCase();
 			s = s.replaceAll(to_find.toUpperCase(), to_change_with_upper);
 		}
 		// change accents with uppercase too
@@ -114,31 +119,31 @@ class BibtexString {
 
 	//CZ - Příliš žluťoučký kůň úpěl ďábelské ódy 
 	// lower case, will do uppercase automatically
-	public static final String[] accents_todo_uppercase = new String[] {
-			"à\\`{a}", "á\\'{a}", "â\\^{a}", "ã\\~{a}", "ā\\={a}", "ä\\\"{a}",
+	public static final String[] accents = new String[] {
+			"à{\\`a}", "á{\\'a}", "â{\\^a}", "ã{\\~a}", "ā{\\=a}", "ä{\\\"a}",
 
-			"è\\`{e}", "é\\'{e}", "ê\\^{e}", "ẽ\\~{e}", "ē\\={e}", "ë\\\"{e}", "ě\\v{e}",
+			"è{\\`e}", "é{\\'e}", "ê{\\^e}", "ẽ{\\~e}", "ē{\\=e}", "ë{\\\"e}", "ě{\\v e}",
 
-			"ì\\`{i}", "í\\'{i}", "î\\^{i}", "ĩ\\~{i}", "ī\\={i}", "ï\\\"{i}",
+			"ì{\\`i}", "í{\\'i}", "î{\\^i}", "ĩ{\\~i}", "ī{\\=i}", "ï{\\\"i}",
 
-			"ò\\`{o}", "ó\\'{o}", "ô\\^{o}", "õ\\~{o}", "ō\\={o}", "ö\\\"{o}",
+			"ò{\\`o}", "ó{\\'o}", "ô{\\^o}", "õ{\\~o}", "ō{\\=o}", "ö{\\\"o}",
 
-			"ĺ\\'{l}",
+			"ĺ{\\'l}",
 
-			"ù\\`{u}", "ú\\'{u}", "û\\^{u}", "ũ\\~{u}", "ū\\={u}", "ü\\\"{u}", "ů\\r{u}",
+			"ù{\\`u}", "ú{\\'u}", "û{\\^u}", "ũ{\\~u}", "ū{\\=u}", "ü{\\\"u}", "ů{\\r u}",
 
-			"ý\\'{y}", "ÿ\\\"{y}", "ñ\\~{n}",
+			"ý{\\'y}", "ÿ{\\\"y}", "ñ{\\~n}",
 
-			"ś\\'{s}", "ń\\'{n}", "ć\\'{c}",
+			"ś{\\'s}", "ń{\\'n}", "ć{\\'c}",
 
-			"ç\\c{c}", "ȩ\\c{e}",
+			"ç{\\c c}", "ȩ{\\c e}",
 
-			"ọ\\d{o}",
+			"ọ{\\d o}",
 
-			"ŏ\\v{o}", "č\\v{c}", "ž\\v{z}", "š\\v{s}", "ň\\v{n}", "ď\\v{d}",
-			"ť\\v{t}", "ľ\\v{l}", "ř\\v{r}",
+			"ŏ{\\v o}", "č{\\v c}", "ž{\\v z}", "š{\\v s}", "ň{\\v n}", "ď{\\v d}",
+			"ť{\\v t}", "ľ{\\v l}", "ř{\\v r}",
 
-			"œ\\oe", "æ\\ae", "å\\aa", "ø\\o", "þ\\th", "ł\\l", };
+			"œ{\\oe}", "æ{\\ae}", "å{\\aa}", "ø{\\o}", "þ{\\t h}", "ł{\\l}", };
 
 	// without automatic uppercase
 	public static final String[] symbols_final = new String[] { "ß\\ss",
