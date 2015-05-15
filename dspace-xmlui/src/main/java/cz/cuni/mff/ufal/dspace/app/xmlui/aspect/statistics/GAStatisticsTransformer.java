5b5eb23a1e914f450542bd428594d1c1aaa6d1b2
Jozef Misutka <misutka@ufal.mff.cuni.cz>
2015-05-15 14:01:40 +0200

    fixed #174 by adding google libraries (probably not all are required)

--- dspace-xmlui/src/main/java/cz/cuni/mff/ufal/dspace/app/xmlui/aspect/statistics/GAStatisticsTransformer.java
+++ dspace-xmlui/src/main/java/cz/cuni/mff/ufal/dspace/app/xmlui/aspect/statistics/GAStatisticsTransformer.java
@@ -40,7 +40,7 @@ import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.analytics.Analytics;
 import com.google.api.services.analytics.Analytics.Data.Ga.Get;
 import com.google.api.services.analytics.AnalyticsScopes;
