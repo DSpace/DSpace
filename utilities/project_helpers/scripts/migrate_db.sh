518cdc7a587ed3503a89b40622c8f1ef1b3666e0
Jozef Misutka <misutka@ufal.mff.cuni.cz>
2015-05-16 23:53:28 +0200

    made pg_dump generic, minor migrate db changes

--- utilities/project_helpers/scripts/migrate_db.sh
+++ utilities/project_helpers/scripts/migrate_db.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-dbname="$1"
+dbname_new="$1"
+dbname_old="$2"
 NEWPORT=5433
-if sudo -u postgres createdb -p $NEWPORT -O dspace $dbname ;then
-	echo "Created db $dbname";
+if sudo -u postgres createdb -p $NEWPORT -O dspace $dbname_new ;then
+	echo "Created db $dbname_new";
 else
-	echo "Failed to create db $dbname, maybe it exists.";
+	echo "Failed to create db $dbname_new, maybe it exists.";
 	echo "Should we drop it?"
-	echo "This will run \"sudo -u postgres dropdb -p $NEWPORT $dbname\""
+	echo "This will run \"sudo -u postgres dropdb -p $NEWPORT $dbname_new\""
 	read -p "Are you sure? [y/n] " -n 1 -r
 	echo    # (optional) move to a new line
 	if [[ $REPLY =~ ^[Yy]$ ]]
 	then
 	    # do dangerous stuff
-	    sudo -u postgres dropdb -p $NEWPORT $dbname
-	    if ! sudo -u postgres createdb -p $NEWPORT -O dspace $dbname;then
-		echo "Failed to create db $dbname";
+	    sudo -u postgres dropdb -p $NEWPORT $dbname_new
+	    if ! sudo -u postgres createdb -p $NEWPORT -O dspace $dbname_new;then
+		echo "Failed to create db $dbname_new";
 		exit 1;
 	    fi
 	else
@@ -23,4 +24,4 @@ else
 	fi
 fi
 
-sudo -u postgres pg_dump $dbname | sudo -u postgres psql -p $NEWPORT $dbname
+sudo -u postgres pg_dump $dbname_old | sudo -u postgres psql -p $NEWPORT $dbname_new
