bad616e13789b2333a88fa7922fe7719ecdbd224
Ondřej Košarko <kosarko@ufal.mff.cuni.cz>
2015-05-21 14:18:08 +0200

    fixes DS-2587

--- dspace-api/src/main/java/org/dspace/authorize/AuthorizeManager.java
+++ dspace-api/src/main/java/org/dspace/authorize/AuthorizeManager.java
@@ -852,7 +852,7 @@ public class AuthorizeManager
             throws SQLException
     {
         DatabaseManager.updateQuery(c, "DELETE FROM resourcepolicy WHERE "
-                + "resource_type_id= ? AND resource_id= ? AND rptype <> ? ",
+                + "resource_type_id= ? AND resource_id= ? AND (rptype <> ? OR rptype IS NULL)",
                 o.getType(), o.getID(), type);
     }
 
