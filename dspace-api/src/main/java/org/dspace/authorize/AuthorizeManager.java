985eafa755bb9717a856024e0c0679641839e0b1
Pascal-Nicolas Becker <dspace@pascal-becker.de>
2015-06-12 13:18:36 +0200

    Use Group.ANONYMOUS_ID instead of static int 0.

--- dspace-api/src/main/java/org/dspace/authorize/AuthorizeManager.java
+++ dspace-api/src/main/java/org/dspace/authorize/AuthorizeManager.java
@@ -1171,7 +1171,7 @@ public class AuthorizeManager
             } else
             {
                 // add policy just for anonymous
-                ResourcePolicy rp = AuthorizeManager.createOrModifyPolicy(null, context, null, 0, null, embargoDate, Constants.READ, reason, dso);
+                ResourcePolicy rp = AuthorizeManager.createOrModifyPolicy(null, context, null, Group.ANONYMOUS_ID, null, embargoDate, Constants.READ, reason, dso);
                 if (rp != null)
                     rp.update();
             }
