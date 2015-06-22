c804b988a652a3f7ad8600fdd2716d2d50b6030b
Luigi Andrea Pascarelli <l.pascarelli@cineca.it>
2015-06-22 19:28:09 +0200

    add model classes to manage automatic marshal/unmarshal from orcid (jaxb implementation)

---
+++ dspace-api/src/main/java/it/cineca/surplus/ir/crosswalks/FileNameDisseminator.java
@@ -0,0 +1,12 @@
+package it.cineca.surplus.ir.crosswalks;
+
+/**
+ * Define interface to get file name to showed on download view by configuration.
+ * 
+ * @author pascarelli
+ *
+ */
+public interface FileNameDisseminator
+{    
+    public String getFileName();  
+}
---
+++ dspace-api/src/test/java/org/dspace/authority/OrcidTest.java
@@ -0,0 +1,84 @@
+package org.dspace.authority;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.SQLException;
+
+import javax.xml.bind.JAXBException;
+
+import org.apache.log4j.Logger;
+import org.dspace.authority.orcid.Orcid;
+import org.dspace.authority.orcid.jaxb.Citation;
+import org.dspace.authority.orcid.jaxb.CitationType;
+import org.dspace.authority.orcid.jaxb.OrcidWork;
+import org.dspace.authority.orcid.jaxb.WorkExternalIdentifier;
+import org.dspace.authority.orcid.jaxb.WorkExternalIdentifiers;
+import org.dspace.authority.orcid.jaxb.WorkTitle;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Item;
+import org.dspace.content.crosswalk.CrosswalkException;
+import org.dspace.content.crosswalk.StreamDisseminationCrosswalk;
+import org.dspace.core.Context;
+import org.dspace.core.PluginManager;
+import org.restlet.resource.ResourceException;
+
+public class OrcidTest {
+	
+	private static Logger log = Logger.getLogger(OrcidTest.class);
+	
+	public static void main(String[] args)
+			throws CrosswalkException, IOException, SQLException, AuthorizeException, ResourceException, JAXBException {
+
+		Context context = new Context();
+
+		Orcid orcid = Orcid.getOrcid();
+		OrcidWork work = new OrcidWork();
+
+		Item item = Item.find(context, 211);
+
+		final StreamDisseminationCrosswalk streamCrosswalkDefault = (StreamDisseminationCrosswalk) PluginManager
+				.getNamedPlugin(StreamDisseminationCrosswalk.class, "bibtex");
+
+		OutputStream outputStream = new ByteArrayOutputStream();
+		streamCrosswalkDefault.disseminate(context, item, outputStream);
+		String citationFromBTE = outputStream.toString();
+
+		Citation citation = new Citation();
+		citation.setWorkCitationType(CitationType.BIBTEX);
+		citation.setCitation(citationFromBTE);
+
+		WorkTitle title = new WorkTitle();
+		title.setTitle(item.getName());
+		work.setWorkTitle(title);
+		work.setWorkCitation(citation);
+		work.setWorkType("book");
+
+		WorkExternalIdentifiers externalIdentifiers = new WorkExternalIdentifiers();
+		// WorkExternalIdentifier externalIdentifier = new
+		// WorkExternalIdentifier();
+		// externalIdentifier.setWorkExternalIdentifierId(StringEscapeUtils.escapeXml("<![CDATA["+item.getHandle()+"]]>"));
+		// externalIdentifier.setWorkExternalIdentifierType("﻿handle");
+		// externalIdentifiers.getWorkExternalIdentifier().add(externalIdentifier);
+
+		WorkExternalIdentifier externalIdentifier1 = new WorkExternalIdentifier();
+		externalIdentifier1.setWorkExternalIdentifierId("" + item.getID());
+		externalIdentifier1.setWorkExternalIdentifierType("source-work-id");
+		externalIdentifiers.getWorkExternalIdentifier().add(externalIdentifier1);
+
+		work.setWorkExternalIdentifiers(externalIdentifiers);
+
+		try {
+			orcid.appendWork("0000-0001-9753-8285", "d0c29317-cb20-4a39-a475-2bcbf5cf650b", work);
+		} catch (Exception ex) {
+			log.error(ex.getMessage(), ex);
+		}
+
+		try {
+			orcid.getProfile("0000-0001-9753-8285", "d0c29317-cb20-4a39-a475-2bcbf5cf650b");
+		} catch (Exception ex) {
+			log.error(ex.getMessage(), ex);
+		}
+	}
+
+}
