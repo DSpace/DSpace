53457da542b1f5946f6ff5341091b2cc2096fb54
Luigi Andrea Pascarelli <l.pascarelli@cineca.it>
2015-07-15 12:30:30 +0200

    placeholder to porting into branch 4_x_x

--- dspace/etc/oracle/update-crismodule_43_to_431.sql
+++ dspace/etc/oracle/update-crismodule_43_to_431.sql
@@ -1,9 +1,10 @@
 ALTER TABLE jdyna_values ADD booleanValue number(1,0);
-create table jdyna_widget_boolean (id number(10,0) not null, showAsType varchar2(255), checked number(1,0), hideWhenFalse number(1,0), primary key (id));
-create table jdyna_widget_checkradio (id number(10,0) not null, option4row number(10,0), staticValues text, query varchar2(255), primary key (id));
+create table IF NOT EXISTS jdyna_widget_boolean (id number(10,0) not null, showAsType varchar2(255), checked number(1,0), hideWhenFalse number(1,0), primary key (id));
+create table IF NOT EXISTS jdyna_widget_checkradio (id number(10,0) not null, option4row number(10,0), staticValues text, dropdown number(1,0), primary key (id));
+--ALTER TABLE jdyna_widget_checkradio DROP COLUMN query;
 
 -- Table to mantain the potential match between item and rp --
-CREATE TABLE potentialmatches
+CREATE TABLE IF NOT EXISTS potentialmatches
 (
    potentialmatches_id integer, 
    item_id integer, 
@@ -15,3 +16,22 @@ CREATE SEQUENCE potentialmatches_seq;
 CREATE INDEX rp_idx ON potentialmatches (rp);
 CREATE INDEX pending_idx ON potentialmatches (pending);
 -- END potential matches --
+
+create table IF NOT EXISTS cris_orcid_history (id number(10,0) not null, entityId number(10,0), typeId number(10,0), responseMessage clob, lastAttempt timestamp, lastSuccess timestamp, primary key (id));
+create table IF NOT EXISTS cris_orcid_queue (id number(10,0) not null, owner varchar2(255), entityId number(10,0), typeId number(10,0), mode varchar2(255), primary key (id));
+create sequence CRIS_ORCIDHISTORY_SEQ;
+create sequence CRIS_ORCIDQUEUE_SEQ;
+
+--ALTER TABLE cris_orcid_history ADD COLUMN entityId number(1,0);
+--ALTER TABLE cris_orcid_history ADD COLUMN typeId number(1,0);
+--ALTER TABLE cris_orcid_queue ADD COLUMN entityId number(1,0);
+--ALTER TABLE cris_orcid_queue ADD COLUMN typeId number(1,0);
+--
+--ALTER TABLE cris_orcid_queue DROP COLUMN itemId;
+--ALTER TABLE cris_orcid_queue DROP COLUMN projectId;
+--ALTER TABLE cris_orcid_queue DROP COLUMN researcherId;
+--ALTER TABLE cris_orcid_queue DROP COLUMN send;
+
+--ALTER TABLE cris_orcid_history DROP COLUMN itemId;
+--ALTER TABLE cris_orcid_history DROP COLUMN projectId;
+--ALTER TABLE cris_orcid_history DROP COLUMN researcherId;
--- dspace/etc/postgres/update-crismodule_43_to_431.sql
+++ dspace/etc/postgres/update-crismodule_43_to_431.sql
@@ -1,10 +1,10 @@
 ALTER TABLE jdyna_values ADD COLUMN booleanValue bool;
-create table jdyna_widget_boolean (id int4 not null, showAsType varchar(255), checked bool, hideWhenFalse bool, primary key (id));
-create table jdyna_widget_checkradio (id int4 not null, option4row integer, staticValues text, query varchar(255), primary key (id));
-
+create table IF NOT EXISTS jdyna_widget_boolean (id int4 not null, showAsType varchar(255), checked bool, hideWhenFalse bool, primary key (id));
+create table IF NOT EXISTS jdyna_widget_checkradio (id int4 not null, option4row integer, staticValues text, dropdown integer, primary key (id));
+--ALTER TABLE jdyna_widget_checkradio DROP COLUMN query;
 
 -- Table to mantain the potential match between item and rp --
-CREATE TABLE potentialmatches
+CREATE TABLE IF NOT EXISTS potentialmatches
 (
    potentialmatches_id integer, 
    item_id integer, 
@@ -18,3 +18,22 @@ CREATE INDEX rp_idx
 CREATE INDEX pending_idx
    ON potentialmatches (pending);
 -- END potential matches --
+   
+   
+create table IF NOT EXISTS cris_orcid_history (id int4 not null, entityId int4, typeId int4, responseMessage text, lastAttempt timestamp, lastSuccess timestamp, primary key (id));
+create table IF NOT EXISTS cris_orcid_queue (id int4 not null, owner varchar(255), entityId int4, typeId int4, mode varchar(255), primary key (id));
+create sequence CRIS_ORCIDHISTORY_SEQ;
+create sequence CRIS_ORCIDQUEUE_SEQ;   
+--ALTER TABLE cris_orcid_history ADD COLUMN entityId integer;
+--ALTER TABLE cris_orcid_history ADD COLUMN typeId integer;
+--ALTER TABLE cris_orcid_queue ADD COLUMN entityId integer;
+--ALTER TABLE cris_orcid_queue ADD COLUMN typeId integer;
+--
+--ALTER TABLE cris_orcid_queue DROP COLUMN itemId;
+--ALTER TABLE cris_orcid_queue DROP COLUMN projectId;
+--ALTER TABLE cris_orcid_queue DROP COLUMN researcherId;
+--ALTER TABLE cris_orcid_queue DROP COLUMN send;
+--
+--ALTER TABLE cris_orcid_history DROP COLUMN itemId;
+--ALTER TABLE cris_orcid_history DROP COLUMN projectId;
+--ALTER TABLE cris_orcid_history DROP COLUMN researcherId;
