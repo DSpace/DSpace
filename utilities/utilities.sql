451e828fa4f4708c192c94d3de3d27f9e27a9467
root <root@dspace.lindat.dev>
2015-07-22 09:22:39 +0200

    fixing path to the file via psql variable

--- utilities/utilities.sql
+++ utilities/utilities.sql
@@ -539,7 +539,8 @@ COPY user_registration (eperson_id, email, organization, confirmation) FROM stdi
 -- Data for Name: license_definition; Type: TABLE DATA; Schema: public; Owner: dspace
 --
 
-copy license_definition(name, definition,eperson_id, label_id, created_on, confirmation, required_info) from 'license_definition.txt';
+\set afile :utildir '/license_definition.txt'
+copy license_definition(name, definition,eperson_id, label_id, created_on, confirmation, required_info) from :'afile';
 
 --
 -- Data for Name: license_label; Type: TABLE DATA; Schema: public; Owner: dspace
