f3c000caa135b3274ff1661fa52d3ee0a8c79233
Luigi Andrea Pascarelli <l.pascarelli@cineca.it>
2015-07-24 00:15:02 +0200

    add support for nested metadata on getmetadata method

--- dspace-cris/api/src/main/java/org/dspace/app/cris/model/ACrisObject.java
+++ dspace-cris/api/src/main/java/org/dspace/app/cris/model/ACrisObject.java
@@ -40,373 +40,340 @@ import org.dspace.content.authority.Choices;
 
 @MappedSuperclass
 public abstract class ACrisObject<P extends Property<TP>, TP extends PropertiesDefinition, NP extends ANestedProperty<NTP>, NTP extends ANestedPropertiesDefinition, ACNO extends ACrisNestedObject<NP, NTP, P, TP>, ATNO extends ATypeNestedObject<NTP>>
-        extends DSpaceObject
-        implements
-        ICrisObject<P, TP>,
-        BrowsableDSpaceObject,
-        IExportableDynamicObject<TP, P, ACrisObject<P, TP, NP, NTP, ACNO, ATNO>>,
-        Cloneable
-{
+		extends DSpaceObject implements ICrisObject<P, TP>, BrowsableDSpaceObject,
+		IExportableDynamicObject<TP, P, ACrisObject<P, TP, NP, NTP, ACNO, ATNO>>, Cloneable {
 
-    @Embedded    
-    private SourceReference sourceReference;
+	@Embedded
+	private SourceReference sourceReference;
 
-    /** Cris public unique identifier, must be null */
-    @Column(nullable = true, unique = true)
-    private String crisID;
+	/** Cris public unique identifier, must be null */
+	@Column(nullable = true, unique = true)
+	private String crisID;
 
-    private Boolean status;
+	private Boolean status;
 
-    @Column(nullable = false, unique = true)
-    private String uuid;
+	@Column(nullable = false, unique = true)
+	private String uuid;
 
-    public ACrisObject()
-    {
-        this.status = false;        
-    }
+	public ACrisObject() {
+		this.status = false;
+	}
 
-    public Boolean getStatus()
-    {
-        return status;
-    }
+	public Boolean getStatus() {
+		return status;
+	}
 
-    public void setStatus(Boolean status)
-    {
-        this.status = status;
-    }
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
 
-    public void setUuid(String uuid)
-    {
-        this.uuid = uuid;
-    }
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
 
-    public String getUuid()
-    {
-        return uuid;
-    }
+	public String getUuid() {
+		return uuid;
+	}
 
-    /**
-     * Wrapper method
-     * 
-     * @param sourceID
-     */
-    public void setSourceID(String sourceID)
-    {
-        getSourceReference().setSourceID(sourceID);
-    }
+	/**
+	 * Wrapper method
+	 * 
+	 * @param sourceID
+	 */
+	public void setSourceID(String sourceID) {
+		getSourceReference().setSourceID(sourceID);
+	}
 
-    /**
-     * Wrappre method 
-     * 
-     * @return
-     */
-    public String getSourceID()
-    {
-        return getSourceReference().getSourceID();
-    }
+	/**
+	 * Wrappre method
+	 * 
+	 * @return
+	 */
+	public String getSourceID() {
+		return getSourceReference().getSourceID();
+	}
 
-    public abstract String getPublicPath();
+	public abstract String getPublicPath();
 
-    public abstract String getAuthorityPrefix();
+	public abstract String getAuthorityPrefix();
 
-    @Override
-    public String getHandle()
-    {
-        return uuid;
-    }
+	@Override
+	public String getHandle() {
+		return uuid;
+	}
 
-    @Override
-    public int getID()
-    {
-        return getId() != null ? getId().intValue() : -1;
-    }
+	@Override
+	public int getID() {
+		return getId() != null ? getId().intValue() : -1;
+	}
 
-    @Override
-    public boolean isArchived()
-    {
-        return getStatus() != null ? getStatus() : false;
-    }
+	@Override
+	public boolean isArchived() {
+		return getStatus() != null ? getStatus() : false;
+	}
 
-    @Override
-    public boolean isWithdrawn()
-    {
-        return getStatus() != null ? !getStatus() : false;
-    }
+	@Override
+	public boolean isWithdrawn() {
+		return getStatus() != null ? !getStatus() : false;
+	}
 
-    /**
-     * Convenience method to get data from ResearcherPage by a string. For any
-     * existent field name the method must return the relative value (i.e
-     * getMetadata("fullName") is equivalent to getFullName()) but the method
-     * always return a list (with 0, 1 or more elements). For dynamic field it
-     * returns the value of the dynamic field with the shorter name equals to
-     * the argument. Only public values are returned!
-     * 
-     * 
-     * @param dcField
-     *            the field (not null) to retrieve the value
-     * @return a list of 0, 1 or more values
-     */
-    public String getMetadata(String field)
-    {
-    	List<String> result = getMetadataValue(field);
-    	if(result.isEmpty()) {
-    		return null;
-    	}
-        return result.get(0);
-    }
-    
-    public List<String> getMetadataValue(String field) {
-        List<String> result = new ArrayList();
+	/**
+	 * Convenience method to get data from ResearcherPage by a string. For any
+	 * existent field name the method must return the relative value (i.e
+	 * getMetadata("fullName") is equivalent to getFullName()) but the method
+	 * always return a list (with 0, 1 or more elements). For dynamic field it
+	 * returns the value of the dynamic field with the shorter name equals to
+	 * the argument. Only public values are returned!
+	 * 
+	 * 
+	 * @param dcField
+	 *            the field (not null) to retrieve the value
+	 * @return a list of 0, 1 or more values
+	 */
+	public String getMetadata(String field) {
+		List<String> result = getMetadataValue(field);
+		if (result.isEmpty()) {
+			return null;
+		}
+		return result.get(0);
+	}
 
-        List<P> dyna = getAnagrafica4view().get(field);
-        for (P prop : dyna)
-        {
-            if (prop.getVisibility() == VisibilityConstants.PUBLIC)
-                result.add(prop.toString());
-        }
+	public List<String> getMetadataValue(String field) {
+		List<String> result = new ArrayList();
 
-    	return result;
-    }
+		List<P> dyna = getAnagrafica4view().get(field);
+		for (P prop : dyna) {
+			if (prop.getVisibility() == VisibilityConstants.PUBLIC)
+				result.add(prop.toString());
+		}
 
-    @Override
-    public Metadatum[] getMetadata(String schema, String element,
-            String qualifier, String lang)
-    {
-        List values = new ArrayList();
-        String authority = null;
-        if ("crisdo".equals(schema) && "name".equals(element))
-        {
-            values.add(getName());            
-        }
-        else if (!schema.equalsIgnoreCase("cris" + this.getPublicPath()))
-        {
-            return new Metadatum[0];
-        }
-        else
-        {
-            element = getCompatibleJDynAShortName(this, element);
+		return result;
+	}
 
-            List<P> proprieties = this.getAnagrafica4view().get(element);
-            
-            if (proprieties != null)
-            {
-                for (P prop : proprieties)
-                {
-                    Object val = prop.getObject();
-                    if (StringUtils.isNotEmpty(qualifier)
-                            && val instanceof ACrisObject)
-                    {
-                        authority = ResearcherPageUtils
-                                .getPersistentIdentifier((ACrisObject) val);
-                        qualifier = getCompatibleJDynAShortName(
-                                (ACrisObject) val, qualifier);
-                        List pointProps = (List) ((ACrisObject) val)
-                                .getAnagrafica4view().get(qualifier);
-                        if (pointProps != null && pointProps.size() > 0)
-                        {
-                            for (Object pprop : pointProps)
-                            {
-                                values.add(((Property) pprop).getObject());
-                            }
-                        }
-                    }
-                    else if (val instanceof ACrisObject)
-                    {
-                        authority = ResearcherPageUtils
-                                .getPersistentIdentifier((ACrisObject) val);
-                        values.add(((ACrisObject) val).getName());
-                    }
-                    else
-                    {
-                    	PropertyEditor propertyEditor = prop.getTypo().getRendering()
-                                .getPropertyEditor(null);
-                        propertyEditor.setValue(val);
-                        values.add(propertyEditor.getAsText());
-                    }
-                }
-            }
-        }
-        Metadatum[] result = new Metadatum[values.size()];
-        for (int idx = 0; idx < values.size(); idx++)
-        {
-            result[idx] = new Metadatum();
-            result[idx].schema = schema;
-            result[idx].element = element;
-            result[idx].qualifier = qualifier;
-            result[idx].authority = authority;
-            result[idx].confidence = StringUtils.isNotEmpty(authority) ? Choices.CF_ACCEPTED
-                    : Choices.CF_UNSET;
-            result[idx].value = values.get(idx).toString();
-        }
-        return result;
-    }
+	@Override
+	public Metadatum[] getMetadata(String schema, String element, String qualifier, String lang) {
+		List values = new ArrayList();
+		String authority = null;
+		if ("crisdo".equals(schema) && "name".equals(element)) {
+			values.add(getName());
+		} else if (!schema.equalsIgnoreCase("cris" + this.getPublicPath())) {
+			//perhaps is a nested
+			boolean buildMetadata = false;
+			if (StringUtils.isNotEmpty(element)) {
+				List<ACNO> listNestedObject = ResearcherPageUtils
+						.getNestedObjectsByParentIDAndShortname(this.getClassNested(), this.getID(), schema);
+				for (ACNO nestedObject : listNestedObject) {
+					List<NP> nProprieties = nestedObject.getAnagrafica4view().get(element);
 
-    private String getCompatibleJDynAShortName(ACrisObject aCrisObject,
-            String element)
-    {
-        Set<String> keys = aCrisObject.getAnagrafica4view().keySet();
-        if (!keys.contains(element))
-        {
-            // DSpace is case insensitive, metadata are all lowercase
-            for (String key : keys)
-            {
-                if (key.replaceAll("[\\-_]", "").equalsIgnoreCase(element))
-                {
-                    return key;
-                }
-            }
-        }
-        return element;
-    }
+					if (nProprieties != null) {
+						for (NP prop : nProprieties) {
+							Object val = prop.getObject();
+							if (StringUtils.isNotEmpty(qualifier) && val instanceof ACrisObject) {
+								authority = ResearcherPageUtils.getPersistentIdentifier((ACrisObject) val);
+								qualifier = getCompatibleJDynAShortName((ACrisObject) val, qualifier);
+								List pointProps = (List) ((ACrisObject) val).getAnagrafica4view().get(qualifier);
+								if (pointProps != null && pointProps.size() > 0) {
+									for (Object pprop : pointProps) {
+										values.add(((Property) pprop).getObject());
+										buildMetadata = true;
+									}
+								}
+							}
+							else if (val instanceof ACrisObject) {
+								authority = ResearcherPageUtils.getPersistentIdentifier((ACrisObject) val);
+								values.add(((ACrisObject) val).getName());
+								buildMetadata = true;
+							} else {
+								PropertyEditor propertyEditor = prop.getTypo().getRendering()
+										.getPropertyEditor(null);
+								propertyEditor.setValue(val);
+								values.add(propertyEditor.getAsText());
+								buildMetadata = true;
+							}
+						}
+					}
 
-    @Override
-    public String toString()
-    {
-        return getName();
-    }
+				}				
+			}
+			if(!buildMetadata) {
+				return new Metadatum[0];
+			}
+		} else {
+			element = getCompatibleJDynAShortName(this, element);
 
-    public String getCrisID()
-    {
-        return crisID;
-    }
+			List<P> proprieties = this.getAnagrafica4view().get(element);
 
-    public void setCrisID(String crisID)
-    {
-        this.crisID = crisID;
-    }
+			if (proprieties != null && !proprieties.isEmpty()) {
+				for (P prop : proprieties) {
+					Object val = prop.getObject();
+					if (StringUtils.isNotEmpty(qualifier) && val instanceof ACrisObject) {
+						authority = ResearcherPageUtils.getPersistentIdentifier((ACrisObject) val);
+						qualifier = getCompatibleJDynAShortName((ACrisObject) val, qualifier);
+						List pointProps = (List) ((ACrisObject) val).getAnagrafica4view().get(qualifier);
+						if (pointProps != null && pointProps.size() > 0) {
+							for (Object pprop : pointProps) {
+								values.add(((Property) pprop).getObject());
+							}
+						}
+					} else if (val instanceof ACrisObject) {
+						authority = ResearcherPageUtils.getPersistentIdentifier((ACrisObject) val);
+						values.add(((ACrisObject) val).getName());
+					} else {
+						PropertyEditor propertyEditor = prop.getTypo().getRendering().getPropertyEditor(null);
+						propertyEditor.setValue(val);
+						values.add(propertyEditor.getAsText());
+					}
+				}
+			}
+		}
+		Metadatum[] result = new Metadatum[values.size()];
+		for (int idx = 0; idx < values.size(); idx++) {
+			result[idx] = new Metadatum();
+			result[idx].schema = schema;
+			result[idx].element = element;
+			result[idx].qualifier = qualifier;
+			result[idx].authority = authority;
+			result[idx].confidence = StringUtils.isNotEmpty(authority) ? Choices.CF_ACCEPTED : Choices.CF_UNSET;
+			result[idx].value = values.get(idx).toString();
+		}
+		return result;
+	}
 
-    abstract public TimeStampInfo getTimeStampInfo();
+	private String getCompatibleJDynAShortName(ACrisObject aCrisObject, String element) {
+		Set<String> keys = aCrisObject.getAnagrafica4view().keySet();
+		if (!keys.contains(element)) {
+			// DSpace is case insensitive, metadata are all lowercase
+			for (String key : keys) {
+				if (key.replaceAll("[\\-_]", "").equalsIgnoreCase(element)) {
+					return key;
+				}
+			}
+		}
+		return element;
+	}
 
-    public String getNamePublicIDAttribute()
-    {
-        return ExportConstants.NAME_PUBLICID_ATTRIBUTE;
-    }
+	@Override
+	public String toString() {
+		return getName();
+	}
 
-    public String getValuePublicIDAttribute()
-    {
-        return "" + this.getId();
-    }
+	public String getCrisID() {
+		return crisID;
+	}
 
-    public String getNameIDAttribute()
-    {
-        return ExportConstants.NAME_ID_ATTRIBUTE;
-    }
+	public void setCrisID(String crisID) {
+		this.crisID = crisID;
+	}
 
-    public String getValueIDAttribute()
-    {
-        if (this.getUuid() == null)
-        {
-            return "";
-        }
-        return "" + this.getUuid().toString();
-    }
+	abstract public TimeStampInfo getTimeStampInfo();
 
-    public String getNameBusinessIDAttribute()
-    {
-        return ExportConstants.NAME_BUSINESSID_ATTRIBUTE;
-    }
+	public String getNamePublicIDAttribute() {
+		return ExportConstants.NAME_PUBLICID_ATTRIBUTE;
+	}
 
-    public String getValueBusinessIDAttribute()
-    {
-        return this.getSourceID();
-    }
+	public String getValuePublicIDAttribute() {
+		return "" + this.getId();
+	}
 
-    public String getNameTypeIDAttribute()
-    {
-        return ExportConstants.NAME_TYPE_ATTRIBUTE;
-    }
+	public String getNameIDAttribute() {
+		return ExportConstants.NAME_ID_ATTRIBUTE;
+	}
 
-    public String getValueTypeIDAttribute()
-    {
-        return "" + getType();
-    }
+	public String getValueIDAttribute() {
+		if (this.getUuid() == null) {
+			return "";
+		}
+		return "" + this.getUuid().toString();
+	}
 
-    public String getNameSingleRowElement()
-    {
-        return ExportConstants.ELEMENT_SINGLEROW;
-    }
+	public String getNameBusinessIDAttribute() {
+		return ExportConstants.NAME_BUSINESSID_ATTRIBUTE;
+	}
 
-    public ACrisObject<P, TP, NP, NTP, ACNO, ATNO> getAnagraficaSupport()
-    {
-        return this;
-    }
+	public String getValueBusinessIDAttribute() {
+		return this.getSourceID();
+	}
 
-    public abstract Class<ACNO> getClassNested();
+	public String getNameTypeIDAttribute() {
+		return ExportConstants.NAME_TYPE_ATTRIBUTE;
+	}
 
-    public abstract Class<ATNO> getClassTypeNested();
+	public String getValueTypeIDAttribute() {
+		return "" + getType();
+	}
 
-    @Override
-    public void update() throws SQLException, AuthorizeException
-    {
-        // TODO Auto-generated method stub
-        
-    }
+	public String getNameSingleRowElement() {
+		return ExportConstants.ELEMENT_SINGLEROW;
+	}
 
-    @Override
-    public void updateLastModified()
-    {
-        // TODO Auto-generated method stub
-        
-    }
-    
-    public String getSourceRef()
-    {
-        return getSourceReference().getSourceRef();
-    }
+	public ACrisObject<P, TP, NP, NTP, ACNO, ATNO> getAnagraficaSupport() {
+		return this;
+	}
 
-    public void setSourceRef(String sourceRef)
-    {
-        getSourceReference().setSourceRef(sourceRef);
-    }
+	public abstract Class<ACNO> getClassNested();
 
-    public SourceReference getSourceReference()
-    {
-        if(this.sourceReference==null) {
-            this.sourceReference = new SourceReference();
-        }
-        return sourceReference;
-    }
+	public abstract Class<ATNO> getClassTypeNested();
 
-    public void setSourceReference(SourceReference sourceReference)
-    {
-        this.sourceReference = sourceReference;
-    }
+	@Override
+	public void update() throws SQLException, AuthorizeException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateLastModified() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getSourceRef() {
+		return getSourceReference().getSourceRef();
+	}
+
+	public void setSourceRef(String sourceRef) {
+		getSourceReference().setSourceRef(sourceRef);
+	}
+
+	public SourceReference getSourceReference() {
+		if (this.sourceReference == null) {
+			this.sourceReference = new SourceReference();
+		}
+		return sourceReference;
+	}
+
+	public void setSourceReference(SourceReference sourceReference) {
+		this.sourceReference = sourceReference;
+	}
 
 	@Override
 	public Metadatum[] getMetadataValueInDCFormat(String mdString) {
-        StringTokenizer dcf = new StringTokenizer(mdString, ".");
-        
-        String[] tokens = { "", "", "" };
-        int i = 0;
-        while(dcf.hasMoreTokens())
-        {
-            tokens[i] = dcf.nextToken().trim();
-            i++;
-        }
-        String schema = tokens[0];
-        String element = tokens[1];
-        String qualifier = tokens[2];
-        
-        Metadatum[] values;
-        if ("*".equals(qualifier))
-        {
-            values = getMetadata(schema, element, Item.ANY, Item.ANY);
-        }
-        else if ("".equals(qualifier))
-        {
-            values = getMetadata(schema, element, null, Item.ANY);
-        }
-        else
-        {
-            values = getMetadata(schema, element, qualifier, Item.ANY);
-        }
-        
-        return values;
+		StringTokenizer dcf = new StringTokenizer(mdString, ".");
+
+		String[] tokens = { "", "", "" };
+		int i = 0;
+		while (dcf.hasMoreTokens()) {
+			tokens[i] = dcf.nextToken().trim();
+			i++;
+		}
+		String schema = tokens[0];
+		String element = tokens[1];
+		String qualifier = tokens[2];
+
+		Metadatum[] values;
+		if ("*".equals(qualifier)) {
+			values = getMetadata(schema, element, Item.ANY, Item.ANY);
+		} else if ("".equals(qualifier)) {
+			values = getMetadata(schema, element, null, Item.ANY);
+		} else {
+			values = getMetadata(schema, element, qualifier, Item.ANY);
+		}
+
+		return values;
 	}
-	
+
 	public abstract String getMetadataFieldTitle();
-	
+
 	public ACrisObject<P, TP, NP, NTP, ACNO, ATNO> clone() throws CloneNotSupportedException {
-		return ( ACrisObject<P, TP, NP, NTP, ACNO, ATNO> ) super.clone();
+		return (ACrisObject<P, TP, NP, NTP, ACNO, ATNO>) super.clone();
 	}
 }
