3aa62aac54cc2cc3120c82df27f37fc510de8c39
Mark H. Wood <mwood@IUPUI.Edu>
2015-08-19 11:04:25 -0400

    [DS-2701] Provide missing license headers in dspace-api

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/workflow/postgres/xmlworkflow/V6_0_2015_08_11__Xml_Workflow_Migration.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/workflow/postgres/xmlworkflow/V6_0_2015_08_11__Xml_Workflow_Migration.sql
@@ -1,3 +1,11 @@
+--
+-- The contents of this file are subject to the license and copyright
+-- detailed in the LICENSE and NOTICE files at the root of the source
+-- tree and available online at
+--
+-- http://www.dspace.org/license/
+--
+
 
 UPDATE collection SET workflow_step_1 = null;
 UPDATE collection SET workflow_step_2 = null;
