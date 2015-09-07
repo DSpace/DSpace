a2b8b90029a59575f44a159271cac6d6fde01d83
Ivan Masár <helix84@centrum.sk>
2015-09-07 13:17:07 +0200

    DS-2740 upload step fails when Sherpa/Romeo doesn't hold any copyright information

--- dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/UploadStep.java
+++ dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/UploadStep.java
@@ -394,7 +394,7 @@ public class UploadStep extends AbstractSubmissionStep
                     java.util.List<SHERPAJournal> journals = shresp.getJournals();
                     java.util.List<SHERPAPublisher> publishers = shresp.getPublishers();
 
-                    if (CollectionUtils.isNotEmpty(journals)) {
+                    if (CollectionUtils.isNotEmpty(journals) && CollectionUtils.isNotEmpty(publishers)) {
                         for (SHERPAJournal journ : journals) {
                             SHERPAPublisher pub = publishers.get(0);
 
