d85cd56598ca0d271f1d2ec5d4bf88a3f473f6c2
Terry Brady <terrywbrady@gmail.com>
2015-10-01 17:54:26 -0700

    New REST commands

---
+++ dspace-rest/src/main/java/org/dspace/rest/FilteredItemsResource.java
@@ -0,0 +1,156 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.rest;
+
+
+import org.apache.log4j.Logger;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.factory.ContentServiceFactory;
+import org.dspace.content.service.CollectionService;
+import org.dspace.content.service.ItemService;
+import org.dspace.content.service.MetadataFieldService;
+import org.dspace.content.service.MetadataSchemaService;
+import org.dspace.content.service.SiteService;
+import org.dspace.core.ConfigurationManager;
+import org.dspace.rest.exceptions.ContextException;
+import org.dspace.rest.common.ItemFilter;
+import org.dspace.rest.common.ItemFilterQuery;
+import org.dspace.rest.filter.ItemFilterSet;
+import org.dspace.usage.UsageEvent;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.HttpHeaders;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/*
+ * This class retrieves items by a constructed metadata query evaluated against a set of Item Filters.
+ * 
+ * @author Terry Brady, Georgetown University
+ */
+@Path("/filtered-items")
+public class FilteredItemsResource extends Resource {
+    protected ItemService itemService = ContentServiceFactory.getInstance().getItemService();
+    protected MetadataFieldService metadataFieldService = ContentServiceFactory.getInstance().getMetadataFieldService();
+    protected MetadataSchemaService metadataSchemaService = ContentServiceFactory.getInstance().getMetadataSchemaService();
+    protected CollectionService collectionService = ContentServiceFactory.getInstance().getCollectionService();
+    protected SiteService siteService = ContentServiceFactory.getInstance().getSiteService();
+
+    private static Logger log = Logger.getLogger(FilteredItemsResource.class);
+    
+    /**
+     * Return instance of collection with passed id. You can add more properties
+     * through expand parameter.
+     * 
+     * @param expand
+     *            String in which is what you want to add to returned instance
+     *            of collection. Options are: "all", "parentCommunityList",
+     *            "parentCommunity", "items", "license" and "logo". If you want
+     *            to use multiple options, it must be separated by commas.
+     * @param limit
+     *            Limit value for items in list in collection. Default value is
+     *            100.
+     * @param offset
+     *            Offset of start index in list of items of collection. Default
+     *            value is 0.
+     * @param filters
+     *            Comma separated list of Item Filters to use to evaluate against
+     *            the items in a collection
+	 * @param query_field
+	 *            List of metadata fields to evaluate in a metadata query.
+	 *            Each list value is used in conjunction with a query_op and query_field.
+	 * @param query_op
+	 *            List of metadata operators to use in a metadata query.
+	 *            Each list value is used in conjunction with a query_field and query_field.
+	 * @param query_val
+	 *            List of metadata values to evaluate in a metadata query.
+	 *            Each list value is used in conjunction with a query_value and query_op.
+ 	 * @param collSel
+	 *            List of collections to query.
+     * @param headers
+     *            If you want to access to collection under logged user into
+     *            context. In headers must be set header "rest-dspace-token"
+     *            with passed token from login method.
+     * @return Return instance of collection. It can also return status code
+     *         NOT_FOUND(404) if id of collection is incorrect or status code
+     *         UNATHORIZED(401) if user has no permission to read collection.
+     * @throws WebApplicationException
+     *             It is thrown when was problem with database reading
+     *             (SQLException) or problem with creating
+     *             context(ContextException). It is thrown by NOT_FOUND and
+     *             UNATHORIZED status codes, too.
+     */
+	@GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public org.dspace.rest.common.ItemFilter getItemQuery(@QueryParam("expand") String expand, 
+    		@QueryParam("limit") @DefaultValue("100") Integer limit, @QueryParam("offset") @DefaultValue("0") Integer offset,
+    		@QueryParam("userIP") String user_ip, @QueryParam("userAgent") String user_agent, @QueryParam("xforwarderfor") String xforwarderfor,
+    		@QueryParam("filters") @DefaultValue("is_item,all_filters") String filters,
+            @QueryParam("query_field[]") @DefaultValue("dc.title") List<String> query_field,
+            @QueryParam("query_op[]") @DefaultValue("exists") List<String> query_op,
+            @QueryParam("query_val[]") @DefaultValue("") List<String> query_val,
+            @QueryParam("collSel[]") @DefaultValue("") List<String> collSel,
+    		@Context HttpHeaders headers, @Context HttpServletRequest request) {
+        org.dspace.core.Context context = null;
+        try {
+            context = createContext(getUser(headers));
+            if (!ConfigurationManager.getBooleanProperty("rest", "rest-reporting-authenticate", false)) {
+                context.turnOffAuthorisationSystem();            	
+            }
+
+            ItemFilterSet itemFilterSet = new ItemFilterSet(filters, true);
+            ItemFilter result = itemFilterSet.getAllFiltersFilter();
+            
+            int index = Math.min(query_field.size(), Math.min(query_op.size(), query_val.size()));
+            List<ItemFilterQuery> itemFilterQueries = new ArrayList<ItemFilterQuery>();
+            for(int i=0; i<index; i++){
+            	itemFilterQueries.add(new ItemFilterQuery(query_field.get(i), query_op.get(i), query_val.get(i)));
+            }
+
+            String regexClause = ConfigurationManager.getProperty("rest", "rest-regex-clause");
+            if (regexClause == null) {
+            	regexClause = "";
+            }
+            Iterator<org.dspace.content.Item> childItems = itemService.findByMetadataQuery(context, query_field, query_op, query_val, collSel, regexClause);
+             
+            itemFilterSet.processSaveItems(context, childItems, true, expand);
+    	    writeStats(siteService.findSite(context), UsageEvent.Action.VIEW, user_ip, user_agent, xforwarderfor, headers, request, context);
+    	    result.annotateQuery(query_field, query_op, query_val);
+            return result;
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
+        } catch (AuthorizeException e) {
+            log.error(e.getMessage());
+            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
+        } catch (ContextException e) {
+            log.error("Unauthorized filtered item query. " + e.getMessage());
+            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
+		} finally {
+            if(context != null) {
+                try {
+                    context.complete();
+                } catch (SQLException e) {
+                    log.error(e.getMessage() + " occurred while trying to close");
+                }
+            }
+        }
+    }
+    
+}
