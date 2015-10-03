8f40ea7c7d457bc3f0d911002decd7c54aebff97
daisie_local <daisieh@gmail.com>
2015-10-02 20:57:44 -0700

    add writeManuscriptToDB

--- dspace/modules/journal-submit/src/main/java/org/datadryad/submission/EmailParser.java
+++ dspace/modules/journal-submit/src/main/java/org/datadryad/submission/EmailParser.java
@@ -6,6 +6,11 @@ import org.datadryad.rest.converters.ManuscriptToLegacyXMLConverter;
 import org.datadryad.rest.models.Address;
 import org.datadryad.rest.models.Author;
 import org.datadryad.rest.models.Manuscript;
+import org.datadryad.rest.models.Organization;
+import org.datadryad.rest.storage.StorageException;
+import org.datadryad.rest.storage.StoragePath;
+import org.datadryad.rest.storage.rdbms.ManuscriptDatabaseStorageImpl;
+import org.datadryad.rest.storage.rdbms.OrganizationDatabaseStorageImpl;
 
 import javax.xml.bind.JAXBException;
 import java.io.File;
@@ -216,6 +221,42 @@ public class EmailParser {
                 LOGGER.info("wrote xml to file " + file.getAbsolutePath());
             } catch (JAXBException e) {
                 LOGGER.warn("couldn't convert to XML");
+            }
+        }
+    }
+
+    public void writeManuscriptToDB() throws StorageException {
+        StoragePath storagePath = new StoragePath();
+        storagePath.addPathElement(Organization.ORGANIZATION_CODE, manuscript.organization.organizationCode);
+
+        // check to see if this organization exists in the database: if not, add it.
+        OrganizationDatabaseStorageImpl organizationStorage = new OrganizationDatabaseStorageImpl();
+        List<Organization> orgs = organizationStorage.getResults(storagePath, manuscript.organization.organizationCode, 0);
+        if (orgs.size() == 0) {
+            try {
+                LOGGER.info ("creating an organization " + manuscript.organization.organizationCode);
+                organizationStorage.create(storagePath, manuscript.organization);
+            } catch (StorageException ex) {
+                LOGGER.error("Exception creating organizations", ex);
+            }
+        }
+
+        ManuscriptDatabaseStorageImpl manuscriptStorage = new ManuscriptDatabaseStorageImpl();
+        storagePath.addPathElement(Manuscript.MANUSCRIPT_ID, manuscript.manuscriptId);
+        List<Manuscript> manuscripts = manuscriptStorage.getResults(storagePath, manuscript.manuscriptId, 10);
+        if (manuscripts.size() == 0) {
+            try {
+                manuscriptStorage.create(storagePath, manuscript);
+                LOGGER.info("adding manuscript " + manuscript.manuscriptId + " to the database for organization " + manuscript.organization.organizationCode);
+            } catch (StorageException ex) {
+                LOGGER.error("Exception creating manuscript", ex);
+            }
+        } else {
+            try {
+                manuscriptStorage.update(storagePath, manuscript);
+                LOGGER.info("updating manuscript " + manuscript.manuscriptId + " to the database for organization " + manuscript.organization.organizationCode);
+            } catch (StorageException ex) {
+                LOGGER.error("Exception updating manuscript", ex);
             }
         }
     }
