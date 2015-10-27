941e7b2eb912529ca1402b4c4fb50434ae4eb33a
KevinVdV <kevin@mire.be>
2015-10-27 15:22:21 +0100

    [DS-2818] Fix upgrading from before DSpace 5

---
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V4_5_2015.10.26__DS-2818_registry_update.sql
@@ -0,0 +1,36 @@
+-- Special case of migration, we need to the EPerson schema in order to get our metadata for all queries to work
+-- but we cannot a DB connection until our database is up to date, so we need to create our registries manually in sql
+
+INSERT INTO metadataschemaregistry (namespace, short_id) SELECT 'http://dspace.org/eperson', 'eperson'
+  WHERE NOT EXISTS (SELECT metadata_schema_id,namespace,short_id FROM metadataschemaregistry WHERE namespace = 'http://dspace.org/eperson' AND short_id = 'eperson');
+
+
+-- Insert eperson.firstname
+INSERT INTO metadatafieldregistry (metadata_schema_id, element)
+  SELECT (SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='eperson'), 'firstname'
+    WHERE NOT EXISTS (SELECT metadata_field_id,element FROM metadatafieldregistry WHERE element = 'firstname' AND qualifier IS NULL AND metadata_schema_id = (SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='eperson'));
+
+-- Insert eperson.lastname
+INSERT INTO metadatafieldregistry (metadata_schema_id, element)
+  SELECT (SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='eperson'), 'lastname'
+    WHERE NOT EXISTS (SELECT metadata_field_id,element FROM metadatafieldregistry WHERE element = 'lastname' AND qualifier IS NULL AND metadata_schema_id = (SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='eperson'));
+
+-- Insert eperson.phone
+INSERT INTO metadatafieldregistry (metadata_schema_id, element)
+  SELECT (SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='eperson'), 'phone'
+    WHERE NOT EXISTS (SELECT metadata_field_id,element FROM metadatafieldregistry WHERE element = 'phone' AND qualifier IS NULL AND metadata_schema_id = (SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='eperson'));
+
+-- Insert eperson.language
+INSERT INTO metadatafieldregistry (metadata_schema_id, element)
+  SELECT (SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='eperson'), 'language'
+    WHERE NOT EXISTS (SELECT metadata_field_id,element FROM metadatafieldregistry WHERE element = 'language' AND qualifier IS NULL AND metadata_schema_id = (SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='eperson'));
+
+-- Insert into dc.provenance
+INSERT INTO metadatafieldregistry (metadata_schema_id, element)
+  SELECT (SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='dc'), 'provenance'
+    WHERE NOT EXISTS (SELECT metadata_field_id,element FROM metadatafieldregistry WHERE element = 'provenance' AND qualifier IS NULL AND metadata_schema_id = (SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='dc'));
+
+-- Insert into dc.rights.license
+INSERT INTO metadatafieldregistry (metadata_schema_id, element, qualifier)
+  SELECT (SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='dc'), 'rights', 'license'
+    WHERE NOT EXISTS (SELECT metadata_field_id,element,qualifier FROM metadatafieldregistry WHERE element = 'rights' AND qualifier='license' AND metadata_schema_id = (SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='dc'));
