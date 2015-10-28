fde9a32c9decea0e4d3ed7878cac6be0fa6e8417
KevinVdV <kevin@mire.be>
2015-10-28 15:58:16 +0100

    [DS-2846] Increase performance when using iterators: Improved iterator code

--- dspace-api/src/main/java/org/dspace/core/HibernateScrollableResultsIterator.java
+++ dspace-api/src/main/java/org/dspace/core/HibernateScrollableResultsIterator.java
@@ -23,21 +23,51 @@ public class HibernateScrollableResultsIterator<T> implements Iterator<T> {
 
     private ScrollableResults scrollableResults;
 
+    private T nextObject;
+    private boolean retrievedObject = false;
+
+
     public HibernateScrollableResultsIterator(Query query) {
         this.scrollableResults = query.scroll(ScrollMode.FORWARD_ONLY);
     }
 
     @Override
     public boolean hasNext() {
-        return !scrollableResults.isLast();
+        // if we have a next element that was not pulled, just simply return true.
+      		if(!retrievedObject && nextObject != null){
+      			return true;
+      		}
+
+      		if(scrollableResults.next()){
+      			//we remember the element
+                nextObject = (T)scrollableResults.get(0);
+      			retrievedObject = false;
+      			return true;
+      		}else{
+                scrollableResults.close();
+            }
+      		return false;
     }
 
     @Override
     public T next() {
-        scrollableResults.next();
-        @SuppressWarnings("unchecked")
-        T nextObject = (T) scrollableResults.get(0);
-        return nextObject;
+        T toReturn = null;
+      		//next variable could be null because the last element was sent or because we iterate on the next()
+      		//instead of hasNext()
+      		if(nextObject == null){
+      			//if we can retrieve an element, do it
+      			if(scrollableResults.next()){
+      				toReturn = (T)scrollableResults.get(0);
+      			}
+      		}
+      		else{
+                //the element was retrieved by hasNext, return it
+      			toReturn = nextObject;
+                nextObject = null;
+      		}
+
+      		retrievedObject = true;
+      		return toReturn;
     }
 
     @Override
