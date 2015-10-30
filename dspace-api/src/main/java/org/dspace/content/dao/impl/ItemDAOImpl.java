867e87cd1fd4a4b317e11da77a6db010ff003a28
Terry Brady <terrywbrady@gmail.com>
2015-10-30 12:44:44 -0700

    Carry offset/limit to query

--- dspace-api/src/main/java/org/dspace/content/dao/impl/ItemDAOImpl.java
+++ dspace-api/src/main/java/org/dspace/content/dao/impl/ItemDAOImpl.java
@@ -121,8 +121,10 @@ public class ItemDAOImpl extends AbstractHibernateDSODAO<Item> implements ItemDA
     enum OP {equals,not_equals,like,not_like,contains,doesnt_contain,exists,doesnt_exist,matches,doesnt_match;}
     
     @Override
-    public Iterator<Item> findByMetadataQuery(Context context, List<List<MetadataField>> listFieldList, List<String> query_op, List<String> query_val, List<UUID> collectionUuids, String regexClause) throws SQLException {
+    public Iterator<Item> findByMetadataQuery(Context context, List<List<MetadataField>> listFieldList, List<String> query_op, List<String> query_val, List<UUID> collectionUuids, String regexClause, int offset, int limit) throws SQLException {
     	Criteria criteria = createCriteria(context, Item.class, "item");
+    	criteria.setFirstResult(offset);
+    	criteria.setMaxResults(limit);
     	
     	if (!collectionUuids.isEmpty()){
 			DetachedCriteria dcollCriteria = DetachedCriteria.forClass(Collection.class, "coll");
