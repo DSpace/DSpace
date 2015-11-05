84032112500ab123c6b5a84e4926590e9b99cf57
Ryan Scherle <ryan@scherle.org>
2015-11-05 19:52:15 +0000

    Correct indentation issues, add more log statements

--- dspace/modules/payment-system/payment-api/src/main/java/org/dspace/workflow/actions/processingaction/FinalPaymentAction.java
+++ dspace/modules/payment-system/payment-api/src/main/java/org/dspace/workflow/actions/processingaction/FinalPaymentAction.java
@@ -79,34 +79,37 @@ public class FinalPaymentAction extends ProcessingAction {
             }
         }
 
-	    // if journal-based subscription is in place, transaction is paid
-	    if(shoppingCart.getJournalSub()) {
-		log.info("processed journal subscription for Item " + itemID + ", journal = " + shoppingCart.getJournal());
-        log.debug("tally credit for journal = " + shoppingCart.getJournal());
-        String success = "";
-        Scheme scheme = Scheme.findByIdentifier(c,ConfigurationManager.getProperty("solrauthority.searchscheme.prism_publicationName"));
-        Concept[] concepts = Concept.findByPreferredLabel(c,shoppingCart.getJournal(),scheme.getID());
-        if(concepts!=null&&concepts.length!=0){
-            AuthorityMetadataValue[] metadataValues = concepts[0].getMetadata("journal", "customerId", null, Item.ANY);
-            if(metadataValues!=null&&metadataValues.length>0){
-                try{
-		    String packageDOI = DOIIdentifierProvider.getDoiValue(wfi.getItem());
-                    success = AssociationAnywhere.tallyCredit(c, metadataValues[0].value, packageDOI);
-                    shoppingCart.setStatus(ShoppingCart.STATUS_COMPLETED);
-                    Date date= new Date();
-                    shoppingCart.setPaymentDate(date);
-                    shoppingCart.update();
-                    sendPaymentApprovedEmail(c,wfi,shoppingCart);
-                    return new ActionResult(ActionResult.TYPE.TYPE_OUTCOME, ActionResult.OUTCOME_COMPLETE);
-                }catch (Exception e)
-                {
-                    log.error(e.getMessage(),e);
-                    sendPaymentErrorEmail(c, wfi, shoppingCart,"problem: credit not tallied successfully. \\n \\n " + e.getMessage());
-                    return new ActionResult(ActionResult.TYPE.TYPE_OUTCOME, 2);
-                }
-            }
-        }
+	// if journal-based subscription is in place, transaction is paid
+	if(shoppingCart.getJournalSub()) {
+	    log.info("processed journal subscription for Item " + itemID + ", journal = " + shoppingCart.getJournal());
+	    log.debug("tally credit for journal = " + shoppingCart.getJournal());
+	    String success = "";
+	    Scheme scheme = Scheme.findByIdentifier(c,ConfigurationManager.getProperty("solrauthority.searchscheme.prism_publicationName"));
+	    Concept[] concepts = Concept.findByPreferredLabel(c,shoppingCart.getJournal(),scheme.getID());
+	    if(concepts!=null&&concepts.length!=0){
+		AuthorityMetadataValue[] metadataValues = concepts[0].getMetadata("journal", "customerId", null, Item.ANY);
+		if(metadataValues!=null&&metadataValues.length>0){
+		    try{
+			String packageDOI = DOIIdentifierProvider.getDoiValue(wfi.getItem());
+			success = AssociationAnywhere.tallyCredit(c, metadataValues[0].value, packageDOI);
+			shoppingCart.setStatus(ShoppingCart.STATUS_COMPLETED);
+			Date date= new Date();
+			shoppingCart.setPaymentDate(date);
+			shoppingCart.update();
+			sendPaymentApprovedEmail(c,wfi,shoppingCart);
+			return new ActionResult(ActionResult.TYPE.TYPE_OUTCOME, ActionResult.OUTCOME_COMPLETE);
+		    } catch (Exception e) {
+			log.error(e.getMessage(),e);
+			sendPaymentErrorEmail(c, wfi, shoppingCart,"problem: credit not tallied successfully. \\n \\n " + e.getMessage());
+			return new ActionResult(ActionResult.TYPE.TYPE_OUTCOME, 2);
+		    }
+		} else {
+		    log.error("unable to tally credit due to missing customerID");
+		}
+	    } else {
+		log.error("unable to tally credit due to missing concept");
 	    }
+	}
 
 			  
 
