55bd840f189d8a8b94b9e9e1ed6e9eba5f4ecbf6
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2015-11-06 14:41:15 +0100

    ugly hack for switching the language during submission (continuations...)

--- dspace-xmlui/src/main/webapp/themes/UFAL/lib/js/ufal-submission.js
+++ dspace-xmlui/src/main/webapp/themes/UFAL/lib/js/ufal-submission.js
@@ -25,10 +25,28 @@ ufal.submission = {
         });
 
         // hook to value change
-    }
+    },
+
+    fix_l10n: function (){
+        //because of continuation do a form submit
+        // - it shouldn't take us back and the data should remain filled
+        var jForm = jQuery("form[action*='continue']");
+        if(jForm && jForm.length > 0){
+            var action = jForm.attr("action");
+            jQuery("a[href*='locale-attribute']").each(function(){
+                var jAnchor = jQuery(this);
+                jAnchor.click(function(event){
+                    event.preventDefault();
+                    jForm.attr("action", action + jAnchor.attr("href"));
+                    jForm.submit();
+                })
+            });
+        }
+    },
 
 };
 
 jQuery(document).ready(function () {
     ufal.submission.init_openaire();
+    ufal.submission.fix_l10n();
 }); // ready
