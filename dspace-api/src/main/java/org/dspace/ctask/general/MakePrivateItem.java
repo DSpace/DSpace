d474ca855cb4a05c5e331fc00b2436a73511ab28
Hardy Pottinger <pottingerhj@missouri.edu>
2015-11-12 10:32:38 -0600

    re #1524 restyle MakePrivateItem.java curation task after https://github.com/osulibraries/DSpace/blob/osukb/dspace-api/src/main/java/org/dspace/curate/MediaDuration.java

--- dspace-api/src/main/java/org/dspace/ctask/general/MakePrivateItem.java
+++ dspace-api/src/main/java/org/dspace/ctask/general/MakePrivateItem.java
@@ -10,26 +10,29 @@ package org.dspace.ctask.general;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import org.dspace.content.Bitstream;
+import org.dspace.content.BitstreamFormat;
+import org.dspace.content.Bundle;
 import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
-import org.dspace.core.Constants;
+import org.dspace.core.Context;
 import org.dspace.curate.AbstractCurationTask;
 import org.dspace.curate.Curator;
-
-import java.io.IOException;
-
-import static org.dspace.curate.Curator.*;
+import org.dspace.curate.Distributive;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * MakePrivateItem is pretty straightforward, it simply sets the
- * discoverable flag to false for all items it touches
- * rendering them "private"
+ * MakePrivateItem is a task that sets the discoverable flag for item DSOs to false, making the item "private"
  *
  * @author hardyoyo
  */
-
+@Distributive
 public class MakePrivateItem extends AbstractCurationTask
 {
+	private List<String> results;
+
+
     /**
      * Perform the curation task upon passed DSO
      *
@@ -37,16 +40,45 @@ public class MakePrivateItem extends AbstractCurationTask
      * @throws IOException
      */
     @Override
-    public int perform(DSpaceObject dso) throws IOException  {
-        if (dso.getType() == Constants.ITEM) {
-                Item item = (Item)dso;
-
-                // no time to explain, HIDE!
-                item.setDiscoverable(false);
-                return CURATE_SUCCESS;
-
-        } else {
-            return CURATE_SKIP;
-        }
+    public int perform(DSpaceObject dso) throws IOException
+    {
+        distribute(dso);
+		formatResults();
+	    return Curator.CURATE_SUCCESS;
     }
+    
+    @Override
+    protected void performItem(Item item) throws SQLException, IOException
+    {
+
+		// set the discoverable flag
+		// no time to explain, HIDE!
+		item.setDiscoverable(false);
+
+		// update the item
+		try {
+			item.update();
+			addResult(item, "success", "Set discoverable to false" );
+			return;
+
+		} catch (Exception e) {
+
+			addResult(item, "error", "Unable to save results");
+			return;
+    	}
+ 
+	}
+
+		private void addResult(Item item, String status, String message) {
+			results.add(item.getHandle() + " (" + status + ") " + message);
+		}   
+
+		private void formatResults() {
+			StringBuilder outputResult = new StringBuilder();
+			for(String result : results) {
+				outputResult.append(result).append("\n");
+			}
+			setResult(outputResult.toString());
+		}
+
 }
