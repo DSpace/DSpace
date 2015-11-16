0712e4e8aeeefaf1b330baf6f41148831d17541a
daisie_local <daisieh@gmail.com>
2015-11-16 13:54:30 -0800

    should return original string, not null, and just log a message

--- dspace/modules/api/src/main/java/org/dspace/JournalUtils.java
+++ dspace/modules/api/src/main/java/org/dspace/JournalUtils.java
@@ -230,7 +230,7 @@ public class JournalUtils {
                 if (manuscriptMatcher.find()) {
                     canonicalID = manuscriptMatcher.group(1);
                 } else {
-                    canonicalID = null;
+                    log.error ("Manuscript " + manuscriptId + " does not match with the regex provided for " + journalCode);
                 }
             } else {
                 // there is no regex specified, just use the manuscript.
