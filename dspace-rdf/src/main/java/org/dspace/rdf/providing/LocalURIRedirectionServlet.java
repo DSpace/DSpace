bec66aae192ffe1dcefb962c249ad3a21f94387b
Mark H. Wood <mwood@IUPUI.Edu>
2015-11-18 14:55:08 -0500

    [DS-2763] Clean up some other artifacts while I'm at it

--- dspace-rdf/src/main/java/org/dspace/rdf/providing/LocalURIRedirectionServlet.java
+++ dspace-rdf/src/main/java/org/dspace/rdf/providing/LocalURIRedirectionServlet.java
@@ -32,8 +32,8 @@ public class LocalURIRedirectionServlet extends HttpServlet
     
     private final static Logger log = Logger.getLogger(LocalURIRedirectionServlet.class);
     
-    protected HandleService handleService = HandleServiceFactory.getInstance().getHandleService();
-    
+    protected final transient HandleService handleService = HandleServiceFactory.getInstance().getHandleService();
+
     /**
      * Processes requests for both HTTP
      * <code>GET</code> and
@@ -112,6 +112,7 @@ public class LocalURIRedirectionServlet extends HttpServlet
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
@@ -126,6 +127,7 @@ public class LocalURIRedirectionServlet extends HttpServlet
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
@@ -136,6 +138,7 @@ public class LocalURIRedirectionServlet extends HttpServlet
      *
      * @return a String containing servlet description
      */
+    @Override
     public String getServletInfo() {
         return "Ensures that URIs used in RDF can be dereferenced.";
     }
