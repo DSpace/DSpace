7a4f095c3aad91c7d74287fd7e85a300ff839146
Jonas Van Goolen <jonas@atmire.com>
2015-11-24 16:47:46 +0100

    DS-2916 BitstreamDAOImpl contains incorrect database linking
    The implementation to find by Collection/Community/Item contains relations that don't exists between the database tables

--- dspace-api/src/main/java/org/dspace/content/dao/impl/BitstreamDAOImpl.java
+++ dspace-api/src/main/java/org/dspace/content/dao/impl/BitstreamDAOImpl.java
@@ -77,8 +77,7 @@ public class BitstreamDAOImpl extends AbstractHibernateDSODAO<Bitstream> impleme
     public Iterator<Bitstream> findByCommunity(Context context, Community community) throws SQLException {
         Query query = createQuery(context, "select b from Bitstream b " +
                 "join b.bundles bitBundles " +
-                "join bitBundles.bundle bundle " +
-                "join bundle.items item " +
+                "join bitBundles.items item " +
                 "join item.collections itemColl " +
                 "join itemColl.communities community " +
                 "WHERE :community IN community");
@@ -92,8 +91,7 @@ public class BitstreamDAOImpl extends AbstractHibernateDSODAO<Bitstream> impleme
     public Iterator<Bitstream> findByCollection(Context context, Collection collection) throws SQLException {
         Query query = createQuery(context, "select b from Bitstream b " +
                 "join b.bundles bitBundles " +
-                "join bitBundles.bundle bundle " +
-                "join bundle.items item " +
+                "join bitBundles.items item " +
                 "join item.collections c " +
                 "WHERE :collection IN c");
 
@@ -106,8 +104,7 @@ public class BitstreamDAOImpl extends AbstractHibernateDSODAO<Bitstream> impleme
     public Iterator<Bitstream> findByItem(Context context, Item item) throws SQLException {
         Query query = createQuery(context, "select b from Bitstream b " +
                 "join b.bundles bitBundles " +
-                "join bitBundles.bundle bundle " +
-                "join bundle.items item " +
+                "join bitBundles.items item " +
                 "WHERE :item IN item");
 
         query.setParameter("item", item);
