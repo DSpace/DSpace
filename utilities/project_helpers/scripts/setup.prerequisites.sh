01e737fb346395780ccf2fb85c9e0f1dcf35ef3a
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2015-12-14 15:13:07 +0100

    adding cog-jglobus-1.8.0.jar to prerequisites, since it's not in maven anymore
    puretls also problematic
    
    jars are binary

--- utilities/project_helpers/scripts/setup.prerequisites.sh
+++ utilities/project_helpers/scripts/setup.prerequisites.sh
@@ -34,6 +34,16 @@ echo "Installing libraries"
 mvn install:install-file -Dfile=${OUTDIR}/${VER_JAI_IMAGE} -DgroupId=com.sun.media -DartifactId=jai_imageio -Dversion=1.0_01 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=${OUTDIR}/${VER_JAI_CORE} -DgroupId=javax.media -DartifactId=jai_core -Dversion=1.1.2_01 -Dpackaging=jar -DgeneratePom=true
 
+COG_JGLOBUS=cog-jglobus-1.8.0.jar
+if [ -f "${OUTDIR}/${COG_JGLOBUS}" ]; then
+    mvn install:install-file -Dfile=${OUTDIR}/${COG_JGLOBUS} -DgroupId=org.globus.jglobus -DartifactId=cog-jglobus -Dversion=1.8.0 -Dpackaging=jar -DgeneratePom=true
+fi
+
+PURETLS=puretls-1.1.jar
+if [ -f "${OUTDIR}/${PURETLS}" ]; then
+    mvn install:install-file -Dfile=${OUTDIR}/${PURETLS} -DgroupId=com.claymoresystems -DartifactId=puretls -Dversion=1.1 -Dpackaging=jar -DgeneratePom=true
+fi
+
 #VER_JARGON=jargon.jar
 #if [ -f "${OUTDIR}/${VER_JARGON}" ]; then
 #    mvn install:install-file -Dfile=${OUTDIR}/${VER_JARGON} -DgroupId=edu.sdsc.grid -DartifactId=jargon -Dversion=3.0.0 -Dpackaging=jar -DgeneratePom=true
