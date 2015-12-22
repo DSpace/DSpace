6caf2122585fd360605cb33ee456b2b0ec6d9699
Terry Brady <terrywbrady@gmail.com>
2015-12-22 12:43:08 -0800

    Handle null pointer in Shibb Authenticate

--- dspace-api/src/main/java/org/dspace/authenticate/ShibAuthentication.java
+++ dspace-api/src/main/java/org/dspace/authenticate/ShibAuthentication.java
@@ -226,7 +226,12 @@ public class ShibAuthentication implements AuthenticationMethod
 			// Step 4: Log the user in.
 			context.setCurrentUser(eperson);
 			request.getSession().setAttribute("shib.authenticated", true);
-            authenticationService.initEPerson(context, request, eperson);
+			
+			//ds-2964 - service may not be authenticated on first use
+			if (authenticationService == null) {
+				authenticationService = AuthenticateServiceFactory.getInstance().getAuthenticationService();
+			}
+                        authenticationService.initEPerson(context, request, eperson);
 
 			log.info(eperson.getEmail()+" has been authenticated via shibboleth.");
 			return AuthenticationMethod.SUCCESS;
