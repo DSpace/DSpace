7c45aad67c8e34447086f2f075c226faf60e851e
KevinVdV <kevin@mire.be>
2016-01-28 12:15:20 +0100

    [DS-2995] Wrong order in breadcrum trail in collection display and item display (Small bugfix when you have multiple communities)

--- dspace-jspui/src/main/java/org/dspace/app/webui/servlet/HandleServlet.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/servlet/HandleServlet.java
@@ -19,6 +19,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.common.collect.Lists;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
@@ -811,6 +812,7 @@ public class HandleServlet extends DSpaceServlet
     {
         // Find all the "parent" communities for the community
         List<Community> parents = communityService.getAllParents(context, c);
+        parents = Lists.reverse(parents);
         if (include)
         {
             parents.add(c);
