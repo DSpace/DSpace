ebbc18221de07c1837da9f4836e21c7a49f860f4
KevinVdV <kevin@mire.be>
2016-02-05 15:14:17 +0100

    [DS-3037] Fix completeStep workflow detection

--- dspace-api/src/main/java/org/dspace/submit/step/CompleteStep.java
+++ dspace-api/src/main/java/org/dspace/submit/step/CompleteStep.java
@@ -87,17 +87,7 @@ public class CompleteStep extends AbstractProcessingStep
         boolean success = false;
         try
         {
-            if(StringUtils.equals(configurationService.getProperty("workflow.framework"), "xmlworkflow"))
-            {
-                try{
-                    workflowService.start(context, (WorkspaceItem) subInfo.getSubmissionItem());
-                }catch (Exception e){
-                    log.error(LogManager.getHeader(context, "Error while starting xml workflow", "Item id: " + subInfo.getSubmissionItem().getItem().getID()), e);
-                    throw new ServletException(e);
-                }
-            }else{
-                workflowService.start(context, (WorkspaceItem) subInfo.getSubmissionItem());
-            }
+            workflowService.start(context, (WorkspaceItem) subInfo.getSubmissionItem());
             success = true;
         }
         catch (Exception e)
