0e70aec96c9ad7d3c24b87f3be5443a189fc5cb5
dylan <dylan.meeus@atmire.com>
2016-03-18 12:05:10 +0100

    Harvester update

--- dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/administrative/FlowContainerUtils.java
+++ dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/administrative/FlowContainerUtils.java
@@ -294,7 +294,8 @@ public class FlowContainerUtils
 		HarvestedCollection hc = harvestedCollectionService.find(context, collection);
 
 		//TODO: is there a cleaner way to do this?
-		try {
+		try
+		{
 			if (!HarvestScheduler.hasStatus(HarvestScheduler.HARVESTER_STATUS_STOPPED)) {
 				synchronized(HarvestScheduler.lock) {
 					HarvestScheduler.setInterrupt(HarvestScheduler.HARVESTER_INTERRUPT_INSERT_THREAD, collectionID);
@@ -302,8 +303,9 @@ public class FlowContainerUtils
 				}
 			}
 			else {
+				// Harvester should return some errors in my opinion..
 				harvester = new OAIHarvester(context, collection, hc);
-				harvester.runHarvest();
+				harvester.runHarvest(); // this throws an exception when fetching bitstreams.
 			}
 		}
 		catch (Exception e) {
@@ -346,12 +348,15 @@ public class FlowContainerUtils
 			//System.out.println("Deleting: " + item.getHandle());
 			//ib.itemRemoved(item);
 			collectionService.removeItem(context, collection, item);
+
 		}
+
 		hc.setLastHarvested(null);
 		hc.setHarvestMessage("");
 		harvestedCollectionService.update(context, hc);
 		collectionService.update(context, collection);
-
+        // update the context?
+		//context.dispatchEvent() // not sure if this is required yet.ts();
 		return processRunCollectionHarvest(context, collectionID, request);
 	}
 
