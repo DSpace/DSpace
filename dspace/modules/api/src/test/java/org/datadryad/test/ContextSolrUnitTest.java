9707543d14d51c1e061c73b0da9cc44f32b35d65
Nathan Day <rnathanday@yahoo.com>
2016-03-23 16:05:17 -0500

    add unit test base class for db access and mock-solr-server

---
+++ dspace/modules/api/src/test/java/org/datadryad/test/ContextSolrUnitTest.java
@@ -0,0 +1,66 @@
+package org.datadryad.test;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+import org.apache.solr.common.util.NamedList;
+import org.junit.After;
+import org.junit.Before;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.impl.XMLResponseParser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Nathan Day
+ */
+public class ContextSolrUnitTest extends ContextUnitTest {
+    protected CommonsHttpSolrServer mockSolrStatsServer;
+    public ContextSolrUnitTest() {
+        super();
+    }
+    @Before
+    public void setUp() {
+        super.setUp();
+        mockSolrStatsServer = mock(CommonsHttpSolrServer.class);
+    }
+    @After
+    public void tearDown() {
+        super.tearDown();
+    }
+
+  /**
+   * Given String path to solr XML response file, use it
+   * as the return value for the solr server method:
+   * query(SolrQuery, SolrRequest.METHOD)
+   * @param path
+   */
+  protected void setQueryResponse(String path, String enc) throws SolrServerException, FileNotFoundException {
+        QueryResponse response = makeResponse(path, enc);
+        when(mockSolrStatsServer.query(
+            any(SolrQuery.class), any(SolrRequest.METHOD.class)
+        )).thenReturn(response);
+    }
+
+  /**
+   * Return a solr QueryResponse object deserialized from the XML
+   * response file at the provided path.
+   * @param path path to XML response file
+   * @param enc response file encoding; default: utf-8
+   * @return
+   */
+    protected QueryResponse makeResponse(String path, String enc) throws FileNotFoundException {
+        if (enc == null)
+            enc = "utf-8";
+        FileInputStream fis = new FileInputStream(new File(path));
+        XMLResponseParser parser = new XMLResponseParser();
+        NamedList<Object> nl = parser.processResponse(fis,enc);
+        return new QueryResponse(nl, this.mockSolrStatsServer);
+    }
+}
+
