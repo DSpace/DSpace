b98b4681a4943f45685aa21f16e01249c6584340
Roeland Dillen <roeland@atmire.com>
2016-04-08 18:23:40 +0200

    partial method docs

--- dspace-api/src/main/java/org/dspace/importer/external/service/other/Imports.java
+++ dspace-api/src/main/java/org/dspace/importer/external/service/other/Imports.java
@@ -15,18 +15,29 @@ import org.dspace.importer.external.datamodel.ImportRecord;
 
 import java.util.Collection;
 
-/**
+/** Common interface for all import implementations.
  * Created by Roeland Dillen (roeland at atmire dot com)
  * Date: 17/09/12
  * Time: 14:08
  */
 public interface Imports {
+	/**
+     *
+     * @param query
+     * @return
+     * @throws MetadataSourceException
+     */
     public int getNbRecords(String query) throws MetadataSourceException;
     public int getNbRecords(Query query) throws MetadataSourceException;
     public Collection<ImportRecord> getRecords(String query, int start, int count)throws MetadataSourceException;
     public Collection<ImportRecord> getRecords(Query q)throws MetadataSourceException;
     public ImportRecord getRecord(String id)throws MetadataSourceException;
     public ImportRecord getRecord(Query q)throws MetadataSourceException;
+
+	/**
+     * The string that identifies this import implementation. Preferable a URI
+     * @return the identifying uri
+     */
     public String getImportSource();
 
     public Collection<ImportRecord> findMatchingRecords(Item item) throws MetadataSourceException;
