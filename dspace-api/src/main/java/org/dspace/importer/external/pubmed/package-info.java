7a42f9cb1be73278b6127ea42b9a41142fde4467
Roeland Dillen <roeland@atmire.com>
2016-04-13 13:56:38 +0200

    package infos

---
+++ dspace-api/src/main/java/org/dspace/importer/external/pubmed/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Pubmed implementation of {@link org.dspace.importer.external.service.other.Imports}
+ * Based on metadata support in {@link org.dspace.importer.external.metadatamapping}
+ * @author Roeland Dillen (roeland at atmire dot com)
+ */
+package org.dspace.importer.external.pubmed;
---
+++ dspace-api/src/main/java/org/dspace/importer/external/service/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Main import framework services.
+ * @author Roeland Dillen (roeland at atmire dot com)
+ */
+package org.dspace.importer.external.service;
