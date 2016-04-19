b7f796e70d9c9acd7bd28e290a5342a36e9cdb11
Mark H. Wood <mwood@IUPUI.Edu>
2016-04-19 13:11:33 -0400

    [DS-2199] Add missing default row ID.

---
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/h2/V6.0_2016.04.01__DS-2199-missing-default.sql
@@ -0,0 +1,23 @@
+--
+-- The contents of this file are subject to the license and copyright
+-- detailed in the LICENSE and NOTICE files at the root of the source
+-- tree and available online at
+--
+-- http://www.dspace.org/license/
+--
+
+-- ===============================================================
+-- WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING
+--
+-- DO NOT MANUALLY RUN THIS DATABASE MIGRATION. IT WILL BE EXECUTED
+-- AUTOMATICALLY (IF NEEDED) BY "FLYWAY" WHEN YOU STARTUP DSPACE.
+-- http://flywaydb.org/
+-- ===============================================================
+
+-------------------------------------------
+-- Add missing default value
+-------------------------------------------
+
+ALTER TABLE MetadataFieldRegistry
+  ALTER COLUMN metadata_field_id
+    SET DEFAULT nextval('metadatafieldregistry_seq');
