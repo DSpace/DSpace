28ac4edc92d79246cd2dcf06661a71cee6deafef
Terry Brady <terrywbrady@gmail.com>
2016-04-27 08:55:14 -0700

    Set default to work on demo.dspace.org
    Since any one of the 3 paths could be chosen, the default will be set to work in demo.dspace.org.

--- dspace-rest/src/main/webapp/static/reports/restReport.js
+++ dspace-rest/src/main/webapp/static/reports/restReport.js
@@ -10,7 +10,9 @@ var Report = function() {
 	this.COLL_LIMIT = 500;
 	this.COUNT_LIMIT = 500;
 	this.ITEM_LIMIT = 100;
-    this.ROOTPATH = "/xmlui/handle/"
+
+	//set default to work on demo.dspace.org
+	this.ROOTPATH = "/xmlui/handle/"
     //this.ROOTPATH = "/jspui/handle/"
 	//this.ROOTPATH = "/handle/"
 	
