2a558a29c77b95a414cea6e99f91b1b8f1f83a3b
Mark H. Wood <mwood@IUPUI.Edu>
2016-05-20 14:23:09 -0400

    [DS-2199] Use same DataSource and Connection for the test and Flyway.
    Poking at Travis to find out why it crashes here while the same code
    works locally.

--- dspace-api/src/test/java/org/dspace/storage/rdbms/DatabaseUtilsHelpers.java
+++ dspace-api/src/test/java/org/dspace/storage/rdbms/DatabaseUtilsHelpers.java
@@ -8,7 +8,9 @@
 
 package org.dspace.storage.rdbms;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 
 /**
  * A dirty cheat to make protected DatabaseUtils methods available for testing.
@@ -17,15 +19,16 @@ import java.sql.SQLException;
  */
 public class DatabaseUtilsHelpers
 {
-
     /**
      * Get Flyway to set up the database with tables and such.
      *
-     * @throws SQLException
+     * @param ds a data source for the update.
+     * @param cnx a database connection for the update.
+     * @throws SQLException passed through.
      */
-    static public void updateDatabase()
+    static public void updateDatabase(DataSource ds, Connection cnx)
             throws SQLException
     {
-        DatabaseUtils.updateDatabase();
+        DatabaseUtils.updateDatabase(ds, cnx);
     }
 }
