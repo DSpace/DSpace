e72578dcc01292e034883268200a9618a5c5373d
LuizClaudioSantos <luizsan@gmail.com>
2016-06-02 14:58:52 -0400

    Fix [DS-3107] - news in dspace.cfg do not load porperly in DSpace 5.

--- dspace-api/src/main/java/org/dspace/core/NewsServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/core/NewsServiceImpl.java
@@ -7,17 +7,41 @@
  */
 package org.dspace.core;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.dspace.core.service.NewsService;
+
 
 public class NewsServiceImpl implements NewsService {
 	private List<String> acceptableFilenames;
 	
 	public void setAcceptableFilenames(List<String> acceptableFilenames) {
-		this.acceptableFilenames = acceptableFilenames;
+		this.acceptableFilenames = addLocalesToAcceptableFilenames(acceptableFilenames);
 	}
-	
+
+
+	protected List<String> addLocalesToAcceptableFilenames(List<String> acceptableFilenames){
+		if(ConfigurationManager.getProperty("webui.supported.locales") == null) {
+			String[] locales = ConfigurationManager.getProperty("webui.supported.locales").split(",");
+			List<String> newAcceptableFilenames = new ArrayList<>();
+			newAcceptableFilenames.addAll(acceptableFilenames);
+
+			for (String local : locales) {
+				for (String acceptableFilename : acceptableFilenames) {
+					String[] values = acceptableFilename.split("\\.");
+					int lastPoint = acceptableFilename.lastIndexOf(".");
+					newAcceptableFilenames.add(values[0] + "_" + local + "." + values[1]);
+					newAcceptableFilenames.add(acceptableFilename.substring(0, lastPoint) + "_" + local + acceptableFilename.substring(lastPoint));
+				}
+			}
+			return newAcceptableFilenames;
+		}
+
+		return  acceptableFilenames;
+	}
+
+
 	@Override
 	public boolean validate(String newsName) {
 		if (acceptableFilenames != null) {
