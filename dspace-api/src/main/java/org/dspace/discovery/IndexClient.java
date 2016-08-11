9fe8de785ddf55d49600e87ee07fed70c1db1596
Chris Wilper <chris@atmire.com>
2016-08-11 13:14:10 -0400

    DS-3271 Also index children with -i, if applicable

--- dspace-api/src/main/java/org/dspace/discovery/IndexClient.java
+++ dspace-api/src/main/java/org/dspace/discovery/IndexClient.java
@@ -9,13 +9,20 @@ package org.dspace.discovery;
 
 import org.apache.log4j.Logger;
 import org.apache.commons.cli.*;
+import org.dspace.content.Collection;
+import org.dspace.content.Community;
 import org.dspace.content.DSpaceObject;
+import org.dspace.content.Item;
+import org.dspace.content.factory.ContentServiceFactory;
+import org.dspace.content.service.ItemService;
+import org.dspace.core.Constants;
 import org.dspace.core.Context;
 import org.dspace.handle.factory.HandleServiceFactory;
 import org.dspace.services.factory.DSpaceServicesFactory;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Iterator;
 
 /**
  * Class used to reindex dspace communities/collections/items into discovery
@@ -135,7 +142,12 @@ public class IndexClient {
                 throw new IllegalArgumentException("Cannot resolve " + handle + " to a DSpace object");
             }
             log.info("Forcibly Indexing " + handle);
-            indexer.indexContent(context, dso, true, true);
+            // Enable batch mode; we may be indexing a large number of items
+            context.enableBatchMode(true);
+            final long startTimeMillis = System.currentTimeMillis();
+            final long count = indexAll(indexer,  ContentServiceFactory.getInstance().getItemService(), context, dso);
+            final long seconds = (System.currentTimeMillis() - startTimeMillis ) / 1000;
+            log.info("Indexed " + count + " DSpace object" + (count > 1 ? "s" : "") + " in " + seconds + " seconds");
         } else {
             log.info("Updating and Cleaning Index");
             indexer.cleanIndex(line.hasOption("f"));
@@ -145,6 +157,60 @@ public class IndexClient {
 
         log.info("Done with indexing");
 	}
+
+    /**
+     * Indexes the given object and all children, if applicable.
+     */
+    private static long indexAll(final IndexingService indexingService,
+                                 final ItemService itemService,
+                                 final Context context,
+                                 final DSpaceObject dso) throws IOException, SearchServiceException, SQLException {
+        long count = 0;
+
+        indexingService.indexContent(context, dso, true, true);
+        count++;
+        if (dso.getType() == Constants.COMMUNITY) {
+            final Community community = (Community) dso;
+            for (final Community subcommunity : community.getSubcommunities()) {
+                count += indexAll(indexingService, itemService, context, subcommunity);
+            }
+            final Community reloadedCommunity = (Community) HandleServiceFactory.getInstance().getHandleService().resolveToObject(context, community.getHandle());
+            for (final Collection collection : reloadedCommunity.getCollections()) {
+                count++;
+                indexingService.indexContent(context, collection, true, true);
+                count += indexItems(indexingService, itemService, context, collection);
+            }
+        } else if (dso.getType() == Constants.COLLECTION) {
+            count += indexItems(indexingService, itemService, context, (Collection) dso);
+        }
+
+        return count;
+    }
+
+    /**
+     * Indexes all items in the given collection.
+     */
+    private static long indexItems(final IndexingService indexingService,
+                                   final ItemService itemService,
+                                   final Context context,
+                                   final Collection collection) throws IOException, SearchServiceException, SQLException {
+        long count = 0;
+
+        final Iterator<Item> itemIterator = itemService.findByCollection(context, collection);
+        while (itemIterator.hasNext()) {
+            indexingService.indexContent(context, itemIterator.next(), true, false);
+            count++;
+            // Every so often, clear the context cache to preserve
+            // memory, and commit the changes to the search index.
+            if (context.getCacheSize() > 50) {
+                context.clearCache();
+                indexingService.commit();
+            }
+        }
+        indexingService.commit();
+
+        return count;
+    }
 
     /**
      * Check the command line options and rebuild the spell check if active.
