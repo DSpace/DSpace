0db78afa2c1d4dd74aeba4a2d2f617f8b6fba997
Luigi Andrea Pascarelli <l_a_p82@yahoo.it>
2016-08-20 20:26:31 +0200

    [DS-1814] fix logic into for each iteration (remove the version from the identifier not the item self)

--- dspace-jspui/src/main/java/org/dspace/app/webui/util/VersionUtil.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/util/VersionUtil.java
@@ -199,10 +199,11 @@ public class VersionUtil
         {
             Item item = itemService.find(context, itemId);
             VersionHistory versionHistory = versionHistoryService.findByItem(context, item);
-
-            for (String id : versionIDs)
+            
+            for (String versionID : versionIDs)
             {
-                versioningService.removeVersion(context, item);
+            	Version version = versioningService.getVersion(context, Integer.parseInt(versionID));
+                versioningService.removeVersion(context, version);
             }
 
             // Retrieve the latest version of our history (IF any is even
