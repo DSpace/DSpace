c53b7a5a59d25d67ddc97c5daa332260b333a1cd
daisie_local <daisieh@gmail.com>
2016-08-31 13:37:23 -0700

    more funder info

--- dspace/modules/api/src/main/java/org/datadryad/api/DryadFunderConcept.java
+++ dspace/modules/api/src/main/java/org/datadryad/api/DryadFunderConcept.java
@@ -2,6 +2,7 @@ package org.datadryad.api;
 
 import org.apache.log4j.Logger;
 import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.DCValue;
 import org.dspace.content.authority.Concept;
 import org.dspace.content.authority.Scheme;
 import org.dspace.core.Context;
@@ -13,9 +14,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.lang.*;
 import java.lang.Exception;
 import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.datadryad.api.DryadJournalConcept.PAYMENT_PLAN;
 import static org.datadryad.api.DryadJournalConcept.WEBSITE;
+import org.dspace.content.Item;
 
 /**
  *
@@ -24,13 +28,21 @@ import static org.datadryad.api.DryadJournalConcept.WEBSITE;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DryadFunderConcept extends DryadOrganizationConcept {
     public static final String FUNDER_ID = "identifier";
+    public static final String PREFERRED_LABEL = "fullname";
+    public static final String ALT_LABEL = "altLabel";
+    public static final String COUNTRY = "country";
+
 
     private static Logger log = Logger.getLogger(DryadFunderConcept.class);
 
     static {
         metadataProperties.setProperty(FUNDER_ID, "funder.identifier");
+        metadataProperties.setProperty(ALT_LABEL, "funder.altLabel");
+        metadataProperties.setProperty(COUNTRY, "funder.country");
 
         defaultMetadataValues.setProperty(metadataProperties.getProperty(FUNDER_ID), "");
+        defaultMetadataValues.setProperty(metadataProperties.getProperty(ALT_LABEL), "");
+        defaultMetadataValues.setProperty(metadataProperties.getProperty(COUNTRY), "");
     }
 
     // these are mandatory elements
@@ -82,8 +94,89 @@ public class DryadFunderConcept extends DryadOrganizationConcept {
         }
     }
 
+    public String getFunderId() {
+        return getConceptMetadataValue(metadataProperties.getProperty(FUNDER_ID));
+    }
+
+    public void setFunderId(String value) {
+        setConceptMetadataValue(metadataProperties.getProperty(FUNDER_ID), value);
+    }
+
+    public String getAltLabel() {
+        return getConceptMetadataValue(metadataProperties.getProperty(FUNDER_ID));
+    }
+
+    public void setAltLabel(String value) {
+        setConceptMetadataValue(metadataProperties.getProperty(ALT_LABEL), value);
+    }
+
+    public void addAltLabel(String value) {
+        addConceptMetadataValue(metadataProperties.getProperty(ALT_LABEL), value);
+    }
+
+    public String getCountry() {
+        return getConceptMetadataValue(metadataProperties.getProperty(COUNTRY));
+    }
+
+    public void setCountry(String value) {
+        setConceptMetadataValue(metadataProperties.getProperty(COUNTRY), value);
+    }
+
     @JsonIgnore
     public static Boolean conceptIsValidFunder(Concept concept) {
-        return ((concept != null) && (concept.getSingleMetadata(metadataProperties.getProperty(FULLNAME)) != null));
+        return ((concept != null) && (concept.getSingleMetadata(metadataProperties.getProperty(FUNDER_ID)) != null));
+    }
+
+    public static DryadFunderConcept getFunderConceptMatchingFunderID(Context context, String funderID) {
+        DryadFunderConcept funderConcept = null;
+        Concept[] concepts = Concept.searchByMetadata(context, metadataProperties.getProperty(FUNDER_ID), funderID);
+        if (concepts.length > 0) {
+            funderConcept = new DryadFunderConcept(context, concepts[0]);
+        }
+        return funderConcept;
+    }
+
+    // In order to get all of the information into the fundingEntity string, the authority value will be the FundRef ID,
+    // while the value will be in the format "<grant number>@<funder name> (<country>)".
+
+    public static DCValue createFundingEntityMetadata(DryadFunderConcept funderConcept, String grantNumber, int confidence) {
+        DCValue result = new DCValue();
+        result.schema = "dryad";
+        result.element = "fundingEntity";
+        result.value = grantNumber + "@" + funderConcept.getFullName() + " (" + funderConcept.getCountry() + ")";
+        result.authority = funderConcept.getFunderId();
+        result.confidence = confidence;
+
+        return result;
+    }
+
+    public static String getFunderNameFromFundingEntity(DCValue fundingMetadata) {
+        Matcher matcher = Pattern.compile("(.*?)@(.+)\\s*(\\(.*\\))").matcher(fundingMetadata.value);
+        String result = null;
+        if (matcher.find()) {
+            result = matcher.group(2);
+        }
+
+        return result;
+    }
+
+    public static String getCountryFromFundingEntity(DCValue fundingMetadata) {
+        Matcher matcher = Pattern.compile("(.*?)@(.+)\\s*(\\(.*\\))").matcher(fundingMetadata.value);
+        String result = null;
+        if (matcher.find()) {
+            result = matcher.group(3);
+        }
+
+        return result;
+    }
+
+    public static String getGrantNumberFromFundingEntity(DCValue fundingMetadata) {
+        Matcher matcher = Pattern.compile("(.*?)@(.+)\\s*(\\(.*\\))").matcher(fundingMetadata.value);
+        String result = null;
+        if (matcher.find()) {
+            result = matcher.group(1);
+        }
+
+        return result;
     }
 }
