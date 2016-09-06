10de4e5968e93b395eb9eab2a18b4b0166870ea7
Luigi Andrea Pascarelli <l_a_p82@yahoo.it>
2016-09-06 11:05:21 +0200

    D4CRIS-30 fix date template generator

--- dspace-api/src/main/java/org/dspace/content/generator/DateValueGenerator.java
+++ dspace-api/src/main/java/org/dspace/content/generator/DateValueGenerator.java
@@ -11,44 +11,56 @@ import org.apache.solr.util.DateMathParser;
 import org.dspace.content.Item;
 import org.dspace.content.Metadatum;
 
-public class DateValueGenerator implements TemplateValueGenerator {
+public class DateValueGenerator implements TemplateValueGenerator
+{
 
-	Logger log = Logger.getLogger(DateValueGenerator.class);
-	@Override
-	public Metadatum[] generator(Item targetItem, Item templateItem,
-			Metadatum metadatum, String extraParams) {
-		
-		Metadatum[] m = new Metadatum[1];
-		m[0] = metadatum;		
-		String[] params = StringUtils.split(extraParams, "\\.");
-		String operazione="";
-		String formatter="";
-		
-		Date date = new Date();
-		DateMathParser dmp = new DateMathParser();
-		String value=""; 
-		if(params != null && params.length >1){
-			operazione= params[1];
-			formatter= params[0];
-			try {
-				date = dmp.parseMath(operazione);
-			} catch (ParseException e) {
-				log.error(e.getMessage(), e);
-			}finally{
-				DateFormat df = new SimpleDateFormat(formatter);
-				value = df.format(date);
-			}
-		}else if(params.length == 1){
-			formatter= params[0];
-			DateFormat df = new SimpleDateFormat(formatter);
-			value = df.format(date);
-		}else {
-			value = date.toString();
-		}
-		
-		metadatum.value= value;
-		return m;
-	}
+    Logger log = Logger.getLogger(DateValueGenerator.class);
 
+    @Override
+    public Metadatum[] generator(Item targetItem, Item templateItem,
+            Metadatum metadatum, String extraParams)
+    {
+
+        Metadatum[] m = new Metadatum[1];
+        m[0] = metadatum;
+        String[] params = StringUtils.split(extraParams, "\\.");
+        String operazione = "";
+        String formatter = "";
+
+        Date date = new Date();
+        DateMathParser dmp = new DateMathParser();
+        String value = "";
+        if (params != null && params.length > 1)
+        {
+            operazione = params[0];
+            formatter = params[1];
+            try
+            {
+                date = dmp.parseMath(operazione);
+            }
+            catch (ParseException e)
+            {
+                log.error(e.getMessage(), e);
+            }
+            finally
+            {
+                DateFormat df = new SimpleDateFormat(formatter);
+                value = df.format(date);
+            }
+        }
+        else if (params.length == 1)
+        {
+            formatter = params[0];
+            DateFormat df = new SimpleDateFormat(formatter);
+            value = df.format(date);
+        }
+        else
+        {
+            value = date.toString();
+        }
+
+        metadatum.value = value;
+        return m;
+    }
 
 }
