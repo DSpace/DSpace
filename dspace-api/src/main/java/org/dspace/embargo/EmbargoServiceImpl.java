57d67a5da4746df162ccccbb3492ff931c04099f
Terry Brady <terrywbrady@gmail.com>
2016-09-28 14:43:08 -0700

    Revert "Move recovery check to setEmbargo"
    This reverts commit f2181c7e740ea36cc560ce72d6eaf5d7e98cad2f.

--- dspace-api/src/main/java/org/dspace/embargo/EmbargoServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/embargo/EmbargoServiceImpl.java
@@ -84,15 +84,7 @@ public class EmbargoServiceImpl implements EmbargoService
         throws SQLException, AuthorizeException
     {
         // if lift is null, we might be restoring an item from an AIP
-        DCDate myLift = null;
-
-        try {
-            myLift = getEmbargoTermsAsDate(context, item);
-        } catch (IllegalArgumentException e) {
-            //DS-3321, handle lift dates in the past
-            log.info("Invalid lift date found for item, setting to null", e);
-        }
-
+        DCDate myLift = getEmbargoTermsAsDate(context, item);
         if (myLift == null)
         {
              if ((myLift = recoverEmbargoDate(item)) == null)
