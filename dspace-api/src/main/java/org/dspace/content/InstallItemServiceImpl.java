b500063775f1e439d5156899c230a2bd943dd683
Terry Brady <terrywbrady@gmail.com>
2016-09-28 14:09:22 -0700

    Revert "Handle lift date exception on import"
    This reverts commit 4df4d2fa08e08208907c371d2fa9a9535d601da2.

--- dspace-api/src/main/java/org/dspace/content/InstallItemServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/InstallItemServiceImpl.java
@@ -22,8 +22,6 @@ import org.dspace.embargo.service.EmbargoService;
 import org.dspace.event.Event;
 import org.dspace.identifier.IdentifierException;
 import org.dspace.identifier.service.IdentifierService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -45,8 +43,6 @@ public class InstallItemServiceImpl implements InstallItemService
     protected IdentifierService identifierService;
     @Autowired(required = true)
     protected ItemService itemService;
-
-    private static final Logger log = LoggerFactory.getLogger(InstallItemServiceImpl.class);
 
     protected InstallItemServiceImpl()
     {
@@ -158,14 +154,9 @@ public class InstallItemServiceImpl implements InstallItemService
         // be set when the embargo is lifted.
         // this will flush out fatal embargo metadata
         // problems before we set inArchive.
-        try {
-            if (embargoService.getEmbargoTermsAsDate(c, item) == null)
-            {
-                itemService.addMetadata(c, item, MetadataSchema.DC_SCHEMA, "date", "available", null, now.toString());
-            }            
-        } catch (IllegalArgumentException e) {
-            //DS-3321, do not cancel an import due to a lift date in the past
-            log.info("Ignoring invalid lift date for item", e);
+        if (embargoService.getEmbargoTermsAsDate(c, item) == null)
+        {
+            itemService.addMetadata(c, item, MetadataSchema.DC_SCHEMA, "date", "available", null, now.toString());
         }
 
         // If issue date is set as "today" (literal string), then set it to current date
