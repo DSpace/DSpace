e5ec8ca25f967454f9e0c24e22db01e6f8e4afc3
Terry Brady <terrywbrady@gmail.com>
2016-09-29 16:18:27 -0700

    remove duplicate header

--- dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/administrative/collection/SetupCollectionHarvestingForm.java
+++ dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/administrative/collection/SetupCollectionHarvestingForm.java
@@ -192,7 +192,6 @@ public class SetupCollectionHarvestingForm extends AbstractDSpaceTransformer
         oaiSetSettingRadio.addOption("all".equals(oaiSetIdValue) || oaiSetIdValue == null, "all", "All sets");
         oaiSetSettingRadio.addOption(!"all".equals(oaiSetIdValue) && oaiSetIdValue != null, "specific", "Specific sets");
 
-        settings.addLabel(T_label_setid);
         Text oaiSetId = settings.addItem().addText("oai_setid");
         oaiSetId.setSize(40);
         if(!"all".equals(oaiSetIdValue) && oaiSetIdValue != null)
