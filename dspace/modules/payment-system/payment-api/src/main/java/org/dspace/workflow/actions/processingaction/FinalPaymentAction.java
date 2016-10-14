d091f190fb35d1b60a75d5e667eccf29e815ff1e
daisie_local <daisieh@gmail.com>
2016-10-14 11:28:41 -0700

    fix indentations

--- dspace/modules/payment-system/payment-api/src/main/java/org/dspace/workflow/actions/processingaction/FinalPaymentAction.java
+++ dspace/modules/payment-system/payment-api/src/main/java/org/dspace/workflow/actions/processingaction/FinalPaymentAction.java
@@ -51,83 +51,81 @@ public class FinalPaymentAction extends ProcessingAction {
 
     @Override
     public ActionResult execute(Context c, WorkflowItem wfi, Step step, HttpServletRequest request) throws SQLException, AuthorizeException, IOException {
-	int itemID =  wfi.getItem().getID();
-	log.info("Verifying payment status of Item " + itemID);
-	
-        try{
-	    PaymentSystemService paymentSystemService = new DSpace().getSingletonService(PaymentSystemService.class);   
-	    ShoppingCart shoppingCart = paymentSystemService.getShoppingCartByItemId(c,itemID);
+        int itemID = wfi.getItem().getID();
+        log.info("Verifying payment status of Item " + itemID);
 
-	    // if fee waiver is in place, transaction is paid
-	    if(shoppingCart.getCountry() != null && shoppingCart.getCountry().length() > 0) {
-		log.info("processed fee waiver for Item " + itemID + ", country = " + shoppingCart.getCountry());
-		return new ActionResult(ActionResult.TYPE.TYPE_OUTCOME, ActionResult.OUTCOME_COMPLETE);
-	    }
+        try {
+            PaymentSystemService paymentSystemService = new DSpace().getSingletonService(PaymentSystemService.class);
+            ShoppingCart shoppingCart = paymentSystemService.getShoppingCartByItemId(c, itemID);
 
-
-        // if a valid voucher is in place, transaction is paid
-        Voucher voucher = Voucher.findById(c,shoppingCart.getVoucher());
-        log.debug("voucher is " + voucher);
-        if(voucher != null) {
-            log.debug("voucher status " + voucher.getStatus());
-            if(voucher.getStatus().equals(Voucher.STATUS_OPEN)) {
-                voucher.setStatus(Voucher.STATUS_USED);
-                voucher.update();
-                c.commit();
-                log.info("processed voucher for Item " + itemID + ", voucherID = " + voucher.getID());
+            // if fee waiver is in place, transaction is paid
+            if (shoppingCart.getCountry() != null && shoppingCart.getCountry().length() > 0) {
+                log.info("processed fee waiver for Item " + itemID + ", country = " + shoppingCart.getCountry());
                 return new ActionResult(ActionResult.TYPE.TYPE_OUTCOME, ActionResult.OUTCOME_COMPLETE);
             }
-        }
 
-	// if journal-based subscription is in place, transaction is paid
-	if(shoppingCart.getJournalSub()) {
-	    log.info("processed journal subscription for Item " + itemID + ", journal = " + shoppingCart.getJournal());
-	    log.debug("tally credit for journal = " + shoppingCart.getJournal());
-	    String success = "";
-	    Scheme scheme = Scheme.findByIdentifier(c,ConfigurationManager.getProperty("solrauthority.searchscheme.prism_publicationName"));
-	    Concept[] concepts = Concept.findByPreferredLabel(c,shoppingCart.getJournal(),scheme.getID());
-	    if(concepts!=null&&concepts.length!=0){
-		AuthorityMetadataValue[] metadataValues = concepts[0].getMetadata("journal", "customerID", null, Item.ANY);
-            if (metadataValues!=null&&metadataValues.length>0) {
-                try {
-                    String packageDOI = DOIIdentifierProvider.getDoiValue(wfi.getItem());
-                    shoppingCart.setStatus(ShoppingCart.STATUS_COMPLETED);
-                    Date date= new Date();
-                    shoppingCart.setPaymentDate(date);
-                    shoppingCart.update();
-                    success = AssociationAnywhere.tallyCredit(c, metadataValues[0].value, packageDOI);
-                    paymentSystemService.sendPaymentApprovedEmail(c,wfi,shoppingCart);
+
+            // if a valid voucher is in place, transaction is paid
+            Voucher voucher = Voucher.findById(c, shoppingCart.getVoucher());
+            log.debug("voucher is " + voucher);
+            if (voucher != null) {
+                log.debug("voucher status " + voucher.getStatus());
+                if (voucher.getStatus().equals(Voucher.STATUS_OPEN)) {
+                    voucher.setStatus(Voucher.STATUS_USED);
+                    voucher.update();
+                    c.commit();
+                    log.info("processed voucher for Item " + itemID + ", voucherID = " + voucher.getID());
                     return new ActionResult(ActionResult.TYPE.TYPE_OUTCOME, ActionResult.OUTCOME_COMPLETE);
-                } catch (Exception e) {
-                    log.error(e.getMessage(),e);
-                    paymentSystemService.sendPaymentErrorEmail(c, wfi, shoppingCart, "problem: credit not tallied successfully. \\n \\n " + e.getMessage());
-                    return new ActionResult(ActionResult.TYPE.TYPE_OUTCOME, 2);
                 }
-            } else {
-                log.error("unable to tally credit due to missing customerID");
             }
-	    } else {
-		    log.error("unable to tally credit due to missing concept");
-	    }
-	}
 
-			  
+            // if journal-based subscription is in place, transaction is paid
+            if (shoppingCart.getJournalSub()) {
+                log.info("processed journal subscription for Item " + itemID + ", journal = " + shoppingCart.getJournal());
+                log.debug("tally credit for journal = " + shoppingCart.getJournal());
+                String success = "";
+                Scheme scheme = Scheme.findByIdentifier(c, ConfigurationManager.getProperty("solrauthority.searchscheme.prism_publicationName"));
+                Concept[] concepts = Concept.findByPreferredLabel(c, shoppingCart.getJournal(), scheme.getID());
+                if (concepts != null && concepts.length != 0) {
+                    AuthorityMetadataValue[] metadataValues = concepts[0].getMetadata("journal", "customerID", null, Item.ANY);
+                    if (metadataValues != null && metadataValues.length > 0) {
+                        try {
+                            String packageDOI = DOIIdentifierProvider.getDoiValue(wfi.getItem());
+                            shoppingCart.setStatus(ShoppingCart.STATUS_COMPLETED);
+                            Date date = new Date();
+                            shoppingCart.setPaymentDate(date);
+                            shoppingCart.update();
+                            success = AssociationAnywhere.tallyCredit(c, metadataValues[0].value, packageDOI);
+                            paymentSystemService.sendPaymentApprovedEmail(c, wfi, shoppingCart);
+                            return new ActionResult(ActionResult.TYPE.TYPE_OUTCOME, ActionResult.OUTCOME_COMPLETE);
+                        } catch (Exception e) {
+                            log.error(e.getMessage(), e);
+                            paymentSystemService.sendPaymentErrorEmail(c, wfi, shoppingCart, "problem: credit not tallied successfully. \\n \\n " + e.getMessage());
+                            return new ActionResult(ActionResult.TYPE.TYPE_OUTCOME, 2);
+                        }
+                    } else {
+                        log.error("unable to tally credit due to missing customerID");
+                    }
+                } else {
+                    log.error("unable to tally credit due to missing concept");
+                }
+            }
 
-	    
-	    // process payment via PayPal
+
+            // process payment via PayPal
             PaymentService paymentService = new DSpace().getSingletonService(PaymentService.class);
-            if(paymentService.submitReferenceTransaction(c,wfi,request)){
-		log.info("processed PayPal payment for Item " + itemID);
+            if (paymentService.submitReferenceTransaction(c, wfi, request)) {
+                log.info("processed PayPal payment for Item " + itemID);
                 return new ActionResult(ActionResult.TYPE.TYPE_OUTCOME, ActionResult.OUTCOME_COMPLETE);
             }
 
 
-        } catch (Exception e){
-            log.error(e.getMessage(),e);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
         }
 
         //Send us to the re authorization of paypal payment
-	log.info("no payment processded for Item " + itemID + ", sending to revalidation step");
+        log.info("no payment processded for Item " + itemID + ", sending to revalidation step");
         WorkflowEmailManager.notifyOfReAuthorizationPayment(c, wfi);
         return new ActionResult(ActionResult.TYPE.TYPE_OUTCOME, 1);
 
