bb553e5ec49e5965b070befad148f04fcac27462
daisie_local <daisieh@gmail.com>
2016-11-28 17:42:17 -0800

    clean merged versions of conflict files
    dspace/modules/payment-system/payment-api/src/main/java/org/dspace/workf
    low/actions/processingaction/FinalPaymentAction.java
    dspace/modules/xmlui/src/main/java/org/datadryad/dspace/xmlui/aspect/bro
    wse/ItemViewer.java
    
    Conflicts:
    
    	dspace/modules/xmlui/src/main/java/org/datadryad/dspace/xmlui/aspect/browse/ItemViewer.java

--- dspace/modules/xmlui/src/main/java/org/datadryad/dspace/xmlui/aspect/browse/ItemViewer.java
+++ dspace/modules/xmlui/src/main/java/org/datadryad/dspace/xmlui/aspect/browse/ItemViewer.java
@@ -333,6 +333,11 @@ public class ItemViewer extends AbstractDSpaceTransformer implements
             for (DCValue metadata : item.getMetadata("dryad.fundingEntity")) {
                 pageMeta.addMetadata("dryad", "fundingEntity").addContent(metadata.value);
             }
+
+            for (DCValue metadata : item.getMetadata("dryad.fundingEntity")) {
+                pageMeta.addMetadata("dryad", "fundingEntity").addContent(metadata.value);
+            }
+
             // Data file metadata included on data package items (integrated view)
             for (DCValue metadata : item.getMetadata("dc.relation.haspart")) {
                 int skip = 0;
