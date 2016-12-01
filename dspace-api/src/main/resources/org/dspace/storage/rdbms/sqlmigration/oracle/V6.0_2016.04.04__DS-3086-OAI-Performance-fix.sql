f0e8afef542a44df66e90bb08a73024ab996cfb1
aroman <aroman@arvo.es>
2016-12-01 09:30:23 +0100

    Deleting collumns drop indexes

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V6.0_2016.04.04__DS-3086-OAI-Performance-fix.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V6.0_2016.04.04__DS-3086-OAI-Performance-fix.sql
@@ -12,7 +12,7 @@
 -- This script will create indexes on the key fields of the
 -- metadataschemaregistry and metadatafieldregistry tables to
 -- increase the performance of the queries. It will also add
--- "ON DELETE CASCADE" to improve the performance of Item deletion.
+-- "ON DELETE CASCADE" to improve the performance of Item deletion. 
 ---------------------------------------------------------------
 
 CREATE UNIQUE INDEX metadataschema_idx_short_id on metadataschemaregistry(short_id);
@@ -27,6 +27,8 @@ UPDATE RESOURCEPOLICY SET DSPACE_OBJECT_NEW = DSPACE_OBJECT;
 ALTER TABLE RESOURCEPOLICY DROP COLUMN DSPACE_OBJECT;
 ALTER TABLE RESOURCEPOLICY RENAME COLUMN DSPACE_OBJECT_NEW to DSPACE_OBJECT;
 
+CREATE INDEX resourcepolicy_object on resourcepolicy(dspace_object);
+
 ALTER TABLE RESOURCEPOLICY
 ADD CONSTRAINT RESOURCEPOLICY_DSPACE_OBJ_FK
 FOREIGN KEY (DSPACE_OBJECT)
@@ -34,10 +36,14 @@ REFERENCES dspaceobject(uuid)
 ON DELETE CASCADE;
 
 -- Add "ON DELETE CASCADE" to foreign key constraint to Item
+DROP index metadatavalue_field_object;
 ALTER TABLE METADATAVALUE ADD DSPACE_OBJECT_NEW RAW(16);
 UPDATE METADATAVALUE SET DSPACE_OBJECT_NEW = DSPACE_OBJECT_ID;
 ALTER TABLE METADATAVALUE DROP COLUMN DSPACE_OBJECT_ID;
 ALTER TABLE METADATAVALUE RENAME COLUMN DSPACE_OBJECT_NEW to DSPACE_OBJECT_ID;
+
+CREATE INDEX metadatavalue_object on metadatavalue(dspace_object_id);
+CREATE INDEX metadatavalue_field_object on metadatavalue(metadata_field_id, dspace_object_id);
 
 ALTER TABLE METADATAVALUE
 ADD CONSTRAINT METADATAVALUE_DSPACE_OBJECT_FK
