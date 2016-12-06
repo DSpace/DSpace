a3dc9e043ea22c2ffc540e7c14a99b739e220dab
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2016-12-06 18:46:53 +0100

    D4CRIS-90 add template for generate metadata with the item identifier

---
+++ dspace-api/src/main/java/org/dspace/content/generator/IdentifierValueGenerator.java
@@ -0,0 +1,31 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.content.generator;
+
+import org.apache.log4j.Logger;
+import org.dspace.content.Item;
+import org.dspace.content.Metadatum;
+
+public class IdentifierValueGenerator implements TemplateValueGenerator
+{
+
+    private static Logger log = Logger.getLogger(IdentifierValueGenerator.class);
+
+    @Override
+    public Metadatum[] generator(Item targetItem, Item templateItem,
+            Metadatum metadatum, String extraParams)
+    {
+
+        Metadatum[] m = new Metadatum[1];
+        m[0] = metadatum;
+        String value = ""+targetItem.getID();
+        metadatum.value = value;
+        return m;
+    }
+   
+}
