c873e554d38330ba0f03309eb677265e25f5bae4
Jonas Van Goolen <jonas@atmire.com>
2017-01-12 13:52:21 +0100

    DS-3448 Multi-select in submission for workflow and workspace items -> License headers in new files

--- dspace-xmlui-mirage2/src/main/webapp/scripts/workflow-multiSelect.js
+++ dspace-xmlui-mirage2/src/main/webapp/scripts/workflow-multiSelect.js
@@ -1,3 +1,10 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 $(document).ready(function() {
     var taskPoolCheckBoxes= $('input[name="workflowandstepID"]');
     var workflowIDCheckboxes = $('input[name="workflowID"]');
