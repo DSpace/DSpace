43afafb4e295bc8c177e9f754ed9c05be9703409
riccardo.fazio <riccardo.fazio@4science.it>
2017-02-13 17:08:18 +0100

    D4CRIS-190 change to facet query in order to add pagination

--- dspace-cris/jspui-api/src/main/java/org/dspace/app/webui/cris/servlet/StatsCleaner.java
+++ dspace-cris/jspui-api/src/main/java/org/dspace/app/webui/cris/servlet/StatsCleaner.java
@@ -11,14 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.SolrResponse;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.FacetField.Count;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.client.solrj.response.TermsResponse.Term;
 import org.dspace.app.webui.servlet.DSpaceServlet;
 import org.dspace.app.webui.util.JSPManager;
-import org.dspace.app.webui.util.UIUtil;
+
 import org.dspace.authorize.AuthorizeException;
 import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Context;
@@ -71,20 +71,63 @@ public class StatsCleaner extends DSpaceServlet {
 			solrServer.optimize();
 			request.setAttribute("deleted", true);
 		}
+		
+		String userAgentOffsetParam = request.getParameter("useragentoffset");
+		int userAgentOffset = 0;
 		SolrQuery solrQuery = new SolrQuery("*:*");
-		solrQuery.addTermsField("userAgent");
-		solrQuery.addTermsField("dns");
-		solrQuery.addTermsField("domaindns");
-		solrQuery.setTerms(true);
-		solrQuery.setTermsLimit(100);
-		solrQuery.setRequestHandler("/terms");
+		solrQuery.setFacet(true);
+		if(StringUtils.isNotBlank(userAgentOffsetParam)){
+			solrQuery.setParam("facet.offset", userAgentOffsetParam);
+			userAgentOffset = Integer.parseInt(userAgentOffsetParam);
+			request.setAttribute("previoususeragent", userAgentOffset - 100);	
+		}
+		solrQuery.addFacetField("userAgent");
+		solrQuery.setFacetLimit(100);
 		QueryResponse resp = solrServer.query(solrQuery);
-		List<Term> terms = resp.getTermsResponse().getTerms("userAgent");
-		request.setAttribute("terms", terms);
-		List<Term> dnsTerms = resp.getTermsResponse().getTerms("dns");
-		request.setAttribute("dnsterms", dnsTerms);
-		List<Term> domainDnsTerms = resp.getTermsResponse().getTerms("domaindns");
-		request.setAttribute("domaindnsterms", domainDnsTerms);
+		List<Count> counts = resp.getFacetField("userAgent").getValues();
+		request.setAttribute("terms", counts);
+		if(counts.size()==100){
+			request.setAttribute("nextuseragent", userAgentOffset + 100);
+		}
+
+
+		String dnsOffsetParam = request.getParameter("dnsoffset");		
+		int dnsOffset = 0;
+		SolrQuery solrQueryDns = new SolrQuery("*:*");
+		solrQueryDns.setFacet(true);
+		if(StringUtils.isNotBlank(dnsOffsetParam)){
+			solrQueryDns.setParam("facet.offset", dnsOffsetParam);
+			dnsOffset = Integer.parseInt(dnsOffsetParam);
+			request.setAttribute("previousdns", dnsOffset - 100);	
+		}
+		solrQueryDns.addFacetField("dns");
+		solrQueryDns.setFacetLimit(100);
+		QueryResponse respDns = solrServer.query(solrQueryDns);
+		List<Count> countsDns = respDns.getFacetField("dns").getValues();
+		request.setAttribute("dnsterms", countsDns);
+		if(counts.size()==100){
+			request.setAttribute("nextdns", dnsOffset + 100);
+		}
+
+		
+		String domainDnsOffsetParam = request.getParameter("domaindnsoffset");		
+		int domainDnsOffset = 0;
+		SolrQuery solrQueryDomainDns = new SolrQuery("*:*");
+		solrQueryDomainDns.setFacet(true);
+		if(StringUtils.isNotBlank(dnsOffsetParam)){
+			solrQueryDomainDns.setParam("facet.offset", domainDnsOffsetParam);
+			domainDnsOffset = Integer.parseInt(domainDnsOffsetParam);
+			request.setAttribute("previousdomaindns", domainDnsOffset - 100);	
+		}
+		solrQueryDomainDns.addFacetField("domaindns");
+		solrQueryDomainDns.setFacetLimit(100);
+		QueryResponse respDomainDns = solrServer.query(solrQueryDomainDns);
+		List<Count> countsDomainDns = respDomainDns.getFacetField("domaindns").getValues();
+		request.setAttribute("domaindnsterms", countsDomainDns);
+		if(counts.size()==100){
+			request.setAttribute("nextdomaindns", domainDnsOffset + 100);
+		}
+
 		JSPManager.showJSP(request, response, "/dspace-admin/stats-cleaner.jsp");
 		}catch (SolrServerException e){
 			log.error(e.getMessage(), e);
