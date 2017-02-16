fff418cbaeb45953366a2b56f2b9d704531b4350
Terry W Brady <twb27@georgetown.edu>
2017-02-16 15:20:51 -0800

    add import

--- dspace-api/src/test/java/org/dspace/discovery/FullTextContentStreamsTest.java
+++ dspace-api/src/test/java/org/dspace/discovery/FullTextContentStreamsTest.java
@@ -13,6 +13,7 @@ import org.dspace.content.Bitstream;
 import org.dspace.content.Bundle;
 import org.dspace.content.Item;
 import org.dspace.content.service.BitstreamService;
+import org.dspace.authorize.service.AuthorizeService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
