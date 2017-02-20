587df066814e62a8c5a691d2eaa9972e6d34144d
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2017-02-20 18:02:43 +0100

    D4CRIS-191 add strategy to build network by events place

--- dspace-cris/api/src/main/java/org/dspace/app/cris/network/VisualizationGraphEventsPlace.java
+++ dspace-cris/api/src/main/java/org/dspace/app/cris/network/VisualizationGraphEventsPlace.java
@@ -10,56 +10,58 @@ package org.dspace.app.cris.network;
 import java.util.LinkedList;
 import java.util.List;
 
-public class VisualizationGraphEventsPlace extends AVisualizationGraphModeFour
-{
+import org.apache.commons.lang.StringUtils;
 
-    public final static String CONNECTION_NAME = "eventsplace";
-
-    private static final String FIELD_QUERY = "crisrp.partecipatedin.networkevents";
-    
-    @Override
-    public String getType()
+public class VisualizationGraphEventsPlace extends AVisualizationGraphModeTwo
     {
-        return CONNECTION_NAME;
-    }
 
-    @Override
-    public String getLineWidthToOverride()
-    {
-        return "1.5";
-    }
+        public final static String CONNECTION_NAME = "eventsplace";
 
-    @Override
-    public String getConnectionName()
-    {
-        return CONNECTION_NAME;
-    }
-
-    @Override
-    protected String buildExtraCustom(String extra)
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    protected String getFacetFieldQuery()
-    {
-        return FIELD_QUERY;
-    }
-
-    @Override
-    protected List<String> getValues(String facetValue)
-    {
-        // e.g.
-        String[] temp = facetValue.split("\\|#\\|#\\|#");
-        String tt = temp[1];
-        String[] temp2 = tt.split("###");
-        List<String> result = new LinkedList<String>();
-        for(String ss : temp2) {
-            result.add(ss);
+        private static final String FIELD_QUERY = "crisrp.partecipatedin.networkevents";
+        
+        @Override
+        public String getType()
+        {
+            return CONNECTION_NAME;
         }
-        return result;
-    }
+
+        @Override
+        public String getLineWidthToOverride()
+        {
+            return "2.5";
+        }
+
+        @Override
+        public String getConnectionName()
+        {
+            return CONNECTION_NAME;
+        }
+
+        @Override
+        protected String getFacetFieldQuery()
+        {
+            return FIELD_QUERY;
+        }
+
+        @Override
+        protected String buildExtraCustom(String extra)
+        {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public String getFacet(String value)
+        {
+            return FA_VALUE;
+        }
     
+        protected List<String> transform(String values)
+        {
+            // e.g.
+            String tt = StringUtils.capitalise(values);
+            List<String> result = new LinkedList<String>();
+            result.add(tt);
+            return result;
+        }
 }
