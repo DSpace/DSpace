3bab4564d4505acd1c62180f9f29bced25cbc6cd
Terry W Brady <twb27@georgetown.edu>
2017-03-20 17:00:32 -0700

    Add link within site object

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/model/hateoas/SiteResource.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/model/hateoas/SiteResource.java
@@ -14,8 +14,11 @@
  */
 package org.dspace.app.rest.model.hateoas;
 
+import org.dspace.app.rest.CommunityResourceController;
 import org.dspace.app.rest.model.SiteRest;
 import org.dspace.app.rest.utils.Utils;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 /**
  * Item Rest HAL Resource. The HAL Resource wraps the REST Resource
@@ -28,5 +31,6 @@ import org.dspace.app.rest.utils.Utils;
 public class SiteResource extends DSpaceResource<SiteRest> {
 	public SiteResource(SiteRest site, Utils utils, String... rels) {
 		super(site, utils, rels);
+		super.add(linkTo(CommunityResourceController.class, "/tops").withSelfRel());
 	}
 }
