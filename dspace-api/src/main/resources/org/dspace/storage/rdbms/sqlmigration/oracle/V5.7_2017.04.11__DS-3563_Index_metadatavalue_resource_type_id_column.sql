9f531fb244a8e47a3b5bec58b4adb92cb65d6a8d
Tom Desair <tom.desair@atmire.com>
2017-04-24 11:05:53 +0200

    DS-3563: Conditional create index for Oracle

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V5.7_2017.04.11__DS-3563_Index_metadatavalue_resource_type_id_column.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V5.7_2017.04.11__DS-3563_Index_metadatavalue_resource_type_id_column.sql
@@ -10,5 +10,14 @@
 -- DS-3563 Missing database index on metadatavalue.resource_type_id
 ------------------------------------------------------
 -- Create an index on the metadata value resource_type_id column so that it can be searched efficiently.
+declare
+    index_not_exists EXCEPTION;
+  PRAGMA EXCEPTION_INIT(index_not_exists, -1418);
+begin
 
-CREATE INDEX metadatavalue_resource_type_id_idx ON metadatavalue (resource_type_id);
+  execute immediate 'DROP INDEX metadatavalue_type_id_idx';
+  exception
+  when index_not_exists then null;
+end;
+
+CREATE INDEX metadatavalue_type_id_idx ON metadatavalue (resource_type_id);
