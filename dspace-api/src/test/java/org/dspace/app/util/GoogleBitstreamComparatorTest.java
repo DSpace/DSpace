e2645a9e4e5d3a1f05810b8608f1ec3ef69d905d
frederic <frederic.van.reet@atmire.com>
2017-04-26 14:48:48 +0200

    Testclasses for GoogleBitstreamComparator and GoogleMetadata

--- dspace-api/src/test/java/org/dspace/app/util/GoogleBitstreamComparatorTest.java
+++ dspace-api/src/test/java/org/dspace/app/util/GoogleBitstreamComparatorTest.java
@@ -3,6 +3,7 @@ package org.dspace.app.util;
 import org.dspace.content.Bitstream;
 import org.dspace.content.BitstreamFormat;
 import org.dspace.content.Bundle;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -47,7 +48,10 @@ public class GoogleBitstreamComparatorTest {
     private HashMap<String, String> settings = new HashMap<>();
 
 
-
+    /**
+     * Create a bundle with three bitstreams
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
         when(bitstream1.getName()).thenReturn("bitstream1");
@@ -64,6 +68,10 @@ public class GoogleBitstreamComparatorTest {
         when(bitstream3.getFormat()).thenReturn(bitstreamFormat3);
     }
 
+    /**
+     * Create three pdf bitstreams and give them a different size, the largest one should come first
+     * @throws Exception
+     */
     @Test
     public void testPDFDifferentSize() throws Exception {
         when(bitstreamFormat1.getMIMEType()).thenReturn("application/pdf");
@@ -75,11 +83,15 @@ public class GoogleBitstreamComparatorTest {
 
         List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
         Collections.sort(toSort, new GoogleBitstreamComparator(settings));
-        assertEquals(toSort.get(0).getName(), "bitstream3");
-        assertEquals(toSort.get(1).getName(), "bitstream2");
-        assertEquals(toSort.get(2).getName(), "bitstream1");
+        assertEquals("bitstream3", toSort.get(0).getName());
+        assertEquals("bitstream2", toSort.get(1).getName());
+        assertEquals("bitstream1", toSort.get(2).getName());
     }
 
+    /**
+     * Create three bitstreams with different mimetypes, order is defined in settings
+     * @throws Exception
+     */
     @Test
     public void testDifferentMimeTypes() throws Exception {
         when(bitstreamFormat1.getMIMEType()).thenReturn("text/richtext");
@@ -88,13 +100,18 @@ public class GoogleBitstreamComparatorTest {
 
         List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
         Collections.sort(toSort, new GoogleBitstreamComparator(settings));
-        assertEquals("WORD should be first", toSort.get(0).getName(), "bitstream2");
-        assertEquals("RTF second", toSort.get(1).getName(), "bitstream1");
-        assertEquals("PS next",toSort.get(2).getName(), "bitstream3");
+        assertEquals("WORD should be first",  "bitstream2", toSort.get(0).getName());
+        assertEquals("RTF second", "bitstream1", toSort.get(1).getName());
+        assertEquals("PS next", "bitstream3", toSort.get(2).getName());
     }
 
+    /**
+     * Test for two bitstreams with same mimetype, but different size.
+     * Should be first ordered by mimetype and then by size (largest first)
+     * @throws Exception
+     */
     @Test
-    public void testDifferentMimeTypesDifferentSizes() throws Exception {
+    public void testMimeTypesDifferentSizes() throws Exception {
         when(bitstreamFormat1.getMIMEType()).thenReturn("text/richtext");
         when(bitstreamFormat2.getMIMEType()).thenReturn("text/richtext");
         when(bitstreamFormat3.getMIMEType()).thenReturn("application/x-photoshop");
@@ -104,11 +121,15 @@ public class GoogleBitstreamComparatorTest {
 
         List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
         Collections.sort(toSort, new GoogleBitstreamComparator(settings));
-        assertEquals(toSort.get(0).getName(), "bitstream2");
-        assertEquals(toSort.get(1).getName(), "bitstream1");
-        assertEquals(toSort.get(2).getName(), "bitstream3");
+        assertEquals("bitstream2", toSort.get(0).getName());
+        assertEquals("bitstream1", toSort.get(1).getName());
+        assertEquals("bitstream3", toSort.get(2).getName());
     }
 
+    /**
+     * Test for same Mimetype and same Size, if this is the case, then ordering is just as it was before sorting
+     * @throws Exception
+     */
     @Test
     public void testSameMimeTypeSameSize() throws Exception {
         when(bitstreamFormat1.getMIMEType()).thenReturn("application/pdf");
@@ -120,27 +141,35 @@ public class GoogleBitstreamComparatorTest {
 
         List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
         Collections.sort(toSort, new GoogleBitstreamComparator(settings));
-        assertEquals("Same size and mimetype, so just pick the first one", toSort.get(0).getName(), "bitstream1");
-        assertEquals(toSort.get(1).getName(), "bitstream2");
-        assertEquals(toSort.get(2).getName(), "bitstream3");
+        assertEquals("Same size and mimetype, so just pick the first one", "bitstream1", toSort.get(0).getName());
+        assertEquals("bitstream2", toSort.get(1).getName());
+        assertEquals("bitstream3", toSort.get(2).getName());
     }
 
+    /**
+     * Test if sorting still works when there is an undefined Mimetype, undefined mimetypes have the lowest priority
+     * @throws Exception
+     */
     @Test
     public void testUnknownMimeType() throws Exception {
         when(bitstreamFormat1.getMIMEType()).thenReturn("unknown");
         when(bitstreamFormat2.getMIMEType()).thenReturn("text/richtext");
         when(bitstreamFormat3.getMIMEType()).thenReturn("text/richtext");
-        when(bitstream1.getSize()).thenReturn(new Long(100));
+        when(bitstream1.getSize()).thenReturn(new Long(400));
         when(bitstream2.getSize()).thenReturn(new Long(200));
         when(bitstream3.getSize()).thenReturn(new Long(300));
 
         List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
         Collections.sort(toSort, new GoogleBitstreamComparator(settings));
-        assertEquals(toSort.get(0).getName(), "bitstream3");
-        assertEquals(toSort.get(1).getName(), "bitstream2");
-        assertEquals(toSort.get(2).getName(), "bitstream1");
+        assertEquals("bitstream3", toSort.get(0).getName());
+        assertEquals("bitstream2", toSort.get(1).getName());
+        assertEquals("bitstream1",toSort.get(2).getName());
     }
 
+    /**
+     * Test with all unknown mimetypes, ordered by size if this is the case
+     * @throws Exception
+     */
     @Test
     public void testAllUnknown() throws Exception {
         when(bitstreamFormat1.getMIMEType()).thenReturn("unknown");
@@ -152,11 +181,14 @@ public class GoogleBitstreamComparatorTest {
 
         List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
         Collections.sort(toSort, new GoogleBitstreamComparator(settings));
-        assertEquals(toSort.get(0).getName(), "bitstream2");
-        assertEquals(toSort.get(1).getName(), "bitstream1");
-        assertEquals(toSort.get(2).getName(), "bitstream3");
+        assertEquals("bitstream2", toSort.get(0).getName());
+        assertEquals("bitstream1", toSort.get(1).getName());
+        assertEquals("bitstream3", toSort.get(2).getName());
     }
 
+    /**
+     * Test to see if priority is configurable, order should change according to prioritized_types property
+     */
     @Test
     public void testChangePriority() {
         settings.put("citation.prioritized_types", "PS, RTF, WORD, PDF");
@@ -166,10 +198,161 @@ public class GoogleBitstreamComparatorTest {
 
         List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
         Collections.sort(toSort, new GoogleBitstreamComparator(settings));
-        assertEquals("PS should be first", toSort.get(0).getName(), "bitstream3");
-        assertEquals("RTF second", toSort.get(1).getName(), "bitstream1");
-        assertEquals("Word third",toSort.get(2).getName(), "bitstream2");
+        assertEquals("PS should be first", "bitstream3", toSort.get(0).getName());
+        assertEquals("RTF second", "bitstream1", toSort.get(1).getName());
+        assertEquals("Word third", "bitstream2", toSort.get(2).getName());
     }
+
+    /**
+     * Test what happens when bitstreams have no mimetype, should be just ordered by size
+     */
+    @Test
+    public void testNoMimeType(){
+        when(bitstream1.getSize()).thenReturn(new Long(200));
+        when(bitstream2.getSize()).thenReturn(new Long(300));
+        when(bitstream3.getSize()).thenReturn(new Long(100));
+
+        List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
+        Collections.sort(toSort, new GoogleBitstreamComparator(settings));
+        assertEquals("bitstream2", toSort.get(0).getName());
+        assertEquals("bitstream1", toSort.get(1).getName());
+        assertEquals("bitstream3", toSort.get(2).getName());
+    }
+
+    /**
+     * Three bitstreams without a size, just the same ordering as given
+     * @throws Exception
+     */
+    @Test
+    public void testNoSize() throws Exception {
+        when(bitstreamFormat1.getMIMEType()).thenReturn("unknown");
+        when(bitstreamFormat2.getMIMEType()).thenReturn("unknown");
+        when(bitstreamFormat3.getMIMEType()).thenReturn("unknown");
+
+        List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
+        Collections.sort(toSort, new GoogleBitstreamComparator(settings));
+        assertEquals("bitstream1", toSort.get(0).getName());
+        assertEquals("bitstream2", toSort.get(1).getName());
+        assertEquals("bitstream3", toSort.get(2).getName());
+    }
+
+    /**
+     * Make sure that it doesn't crash when bitstreams have no size or mimetype
+     */
+    @Test
+    public void testNoMimeTypeNoSize() {
+        List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
+        Collections.sort(toSort, new GoogleBitstreamComparator(settings));
+        assertEquals("bitstream1", toSort.get(0).getName());
+        assertEquals("bitstream2", toSort.get(1).getName());
+        assertEquals("bitstream3", toSort.get(2).getName());
+    }
+
+    /**
+     * Make sure it still works when the citation.prioritized_types property is empty
+     */
+    @Test
+    public void testNoPrioritizedTypes() {
+        settings.put("citation.prioritized_types", "");
+        when(bitstreamFormat1.getMIMEType()).thenReturn("text/richtext");
+        when(bitstreamFormat2.getMIMEType()).thenReturn("application/msword");
+        when(bitstreamFormat3.getMIMEType()).thenReturn("application/x-photoshop");
+        when(bitstream1.getSize()).thenReturn(new Long(100));
+        when(bitstream2.getSize()).thenReturn(new Long(200));
+        when(bitstream3.getSize()).thenReturn(new Long(300));
+
+        List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
+        Collections.sort(toSort, new GoogleBitstreamComparator(settings));
+        assertEquals("PS should be first", "bitstream3", toSort.get(0).getName());
+        assertEquals("RTF second", "bitstream2", toSort.get(1).getName());
+        assertEquals("Word third", "bitstream1", toSort.get(2).getName());
+    }
+
+    /**
+     * Test to make sure properties can be empty
+     */
+    @Test
+    public void testNoMimeTypesForType() {
+        settings.put("citation.mimetypes.PDF" , "");
+        when(bitstreamFormat1.getMIMEType()).thenReturn("text/richtext");
+        when(bitstreamFormat2.getMIMEType()).thenReturn("application/pdf");
+        when(bitstreamFormat3.getMIMEType()).thenReturn("application/x-photoshop");
+
+        List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
+        Collections.sort(toSort, new GoogleBitstreamComparator(settings));
+        assertEquals("RTF first because PDF is not defined","bitstream1", toSort.get(0).getName());
+        assertEquals("Word next", "bitstream3", toSort.get(1).getName());
+        assertEquals("Lastly pdf because it is not defined", "bitstream2", toSort.get(2).getName());
+    }
+
+    /**
+     * Test to check if types defined in citation.prioritized_types can be removed safely
+     */
+    @Test
+    public void testNoFieldForType() {
+        settings.remove("citation.mimetypes.PDF");
+        when(bitstreamFormat1.getMIMEType()).thenReturn("text/richtext");
+        when(bitstreamFormat2.getMIMEType()).thenReturn("application/pdf");
+        when(bitstreamFormat3.getMIMEType()).thenReturn("application/x-photoshop");
+
+        List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
+        Collections.sort(toSort, new GoogleBitstreamComparator(settings));
+        assertEquals("RTF first because PDF is not defined","bitstream1", toSort.get(0).getName());
+        assertEquals("Word next", "bitstream3", toSort.get(1).getName());
+        assertEquals("Lastly pdf because it is not defined", "bitstream2", toSort.get(2).getName());
+    }
+
+    /**
+     * Test if the wrong separator is used, types won't be recognized this way
+     */
+    @Test
+    public void testWrongSeparator() {
+        settings.put("citation.mimetypes.WORD", "application/msword; application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        when(bitstreamFormat1.getMIMEType()).thenReturn("text/richtext");
+        when(bitstreamFormat2.getMIMEType()).thenReturn("application/msword");
+        when(bitstreamFormat3.getMIMEType()).thenReturn("application/x-photoshop");
+        when(bitstream1.getSize()).thenReturn(new Long(100));
+        when(bitstream2.getSize()).thenReturn(new Long(200));
+        when(bitstream3.getSize()).thenReturn(new Long(300));
+
+        List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
+        Collections.sort(toSort, new GoogleBitstreamComparator(settings));
+        assertEquals("RTF", "bitstream1", toSort.get(0).getName());
+        assertEquals("PS", "bitstream3", toSort.get(1).getName());
+        assertEquals("Word last because wrong separator", "bitstream2", toSort.get(2).getName());
+    }
+
+    /**
+     * Test to check for no crash when nothing is configured, just checks for size
+     */
+    @Test
+    public void testNoConfig() {
+        settings.remove("citation.prioritized_types");
+        settings.remove("citation.mimetypes.PDF");
+        settings.remove("citation.mimetypes.WORD");
+        settings.remove("citation.mimetypes.RTF");
+        settings.remove("citation.mimetypes.PS");
+        when(bitstreamFormat1.getMIMEType()).thenReturn("text/richtext");
+        when(bitstreamFormat2.getMIMEType()).thenReturn("application/msword");
+        when(bitstreamFormat3.getMIMEType()).thenReturn("application/x-photoshop");
+        when(bitstream1.getSize()).thenReturn(new Long(100));
+        when(bitstream2.getSize()).thenReturn(new Long(200));
+        when(bitstream3.getSize()).thenReturn(new Long(300));
+
+        List<Bitstream> toSort = Arrays.asList(bundle.getBitstreams());
+        Collections.sort(toSort, new GoogleBitstreamComparator(settings));
+        assertEquals("bitstream3", toSort.get(0).getName());
+        assertEquals("bitstream2", toSort.get(1).getName());
+        assertEquals("bitstream1", toSort.get(2).getName());
+    }
+
+
+    @After
+    public void destroy()
+    {
+      settings = null;
+    }
+
 
 
 
--- dspace-api/src/test/java/org/dspace/content/GoogleMetadataTest.java
+++ dspace-api/src/test/java/org/dspace/content/GoogleMetadataTest.java
@@ -1,16 +1,22 @@
-package org.dspace.app.util;
+package org.dspace.content;
 
-import org.dspace.content.Bitstream;
-import org.dspace.content.BitstreamFormat;
-import org.dspace.content.Bundle;
-import org.dspace.content.Item;
+import org.apache.commons.io.Charsets;
+import org.apache.log4j.Logger;
+import org.dspace.AbstractUnitTest;
+import org.dspace.app.util.GoogleMetadata;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.*;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import sun.net.www.content.text.PlainTextInputStream;
 
+import java.io.*;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 import static org.junit.Assert.*;
@@ -19,54 +25,239 @@ import static org.mockito.Mockito.when;
 /**
  * Created by frederic on 25/04/17.
  */
-@RunWith(MockitoJUnitRunner.class)
-public class GoogleMetadataTest {
-    @InjectMocks
-    private GoogleMetadata googleMetadata;
+public class GoogleMetadataTest extends AbstractUnitTest {
 
-    @Mock
-    private GoogleBitstreamComparator googleBitstreamComparator;
+    /** log4j category */
+    private static final Logger log = Logger.getLogger(GoogleMetadataTest.class);
 
-    @Mock
-    private Item item;
+    /**
+     * Item instance for the tests
+     */
+    private Item it;
 
-    @Mock
-    private Bundle bundle;
-
-    @Mock
-    private Bitstream bitstream1;
-
-    @Mock
-    private Bitstream bitstream2;
-
-    @Mock
-    private Bitstream bitstream3;
-
-    @Mock
-    private BitstreamFormat bitstreamFormat1;
-
-    @Mock
-    private BitstreamFormat bitstreamFormat2;
-
-    @Mock
-    private BitstreamFormat bitstreamFormat3;
-
-
+    /**
+     * This method will be run before every test as per @Before. It will
+     * initialize resources required for the tests.
+     *
+     * Other methods can be annotated with @Before here or in subclasses
+     * but no execution order is guaranteed
+     */
     @Before
-    public void setUp() throws Exception {
-        HashMap<String, String> settings = new HashMap<>();
-        settings.put("citation.prioritized_types", "PDF, WORD, RTF, PS");
-        settings.put("citation.mimetypes.PDF" , "application/pdf");
-        settings.put("citation.mimetypes.WORD", "application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document");
-        settings.put("citation.mimetypes.RTF", "text/richtext");
-        settings.put("citation.mimetypes.PS", "application/x-photoshop");
-        googleBitstreamComparator = new GoogleBitstreamComparator(settings);
-        when(item.getBundles("ORIGINAL")).thenReturn(new Bundle[] {bundle});
+    @Override
+    public void init() {
+        super.init();
+        try
+        {
+            context.turnOffAuthorisationSystem();
+            Collection c = Collection.create(context);
+            WorkspaceItem wi = WorkspaceItem.create(context, c, false);
+            Item item = wi.getItem();
+
+            InstallItem.installItem(context, wi, null);
+            item.update();
+            context.restoreAuthSystemState();
+            context.commit();
+            it = item;
+        }
+        catch (AuthorizeException ex)
+        {
+            log.error("Authorization Error in init", ex);
+            fail("Authorization Error in init: " + ex.getMessage());
+        }
+        catch (SQLException ex)
+        {
+            log.error("SQL Error in init", ex);
+            fail("SQL Error in init: " + ex.getMessage());
+        }
+        catch (IOException ex) {
+            log.error("IO Error in init", ex);
+            fail("SQL Error in init: " + ex.getMessage());
+        }
     }
 
+    /**
+     * Test to see the priorities work, the PDF should be returned
+     * @throws Exception
+     */
     @Test
-    public void testFindLinkableFulltext() throws Exception {
+    public void testGetPDFURLDifferentMimeTypes() throws Exception {
+        context.turnOffAuthorisationSystem();
+        Bundle bundle = it.createBundle("ORIGINAL");
+        Bitstream b = bundle.createBitstream(new ByteArrayInputStream("Bitstream 1".getBytes(Charsets.UTF_8)));
+        b.setName("Word");
+        b.setFormat(BitstreamFormat.create(context));
+        b.getFormat().setMIMEType("application/msword");
+        bundle.addBitstream(b);
+        Bitstream b2 = bundle.createBitstream(new ByteArrayInputStream("Bitstream 2".getBytes(Charsets.UTF_8)));
+        b2.setName("Pdf");
+        b2.setFormat(BitstreamFormat.create(context));
+        b2.getFormat().setMIMEType("application/pdf");
+        bundle.addBitstream(b2);
+        Bitstream b3 = bundle.createBitstream(new ByteArrayInputStream("Bitstream 3".getBytes(Charsets.UTF_8)));
+        b3.setName("Rtf");
+        b3.setFormat(BitstreamFormat.create(context));
+        b3.getFormat().setMIMEType("text/richtext");
+        bundle.addBitstream(b3);
+        context.restoreAuthSystemState();
+        context.commit();
+        GoogleMetadata gm = new GoogleMetadata(this.context, it);
+        String[] urlSplitted = gm.getPDFURL().get(0).split("/");
+        assertEquals("Pdf", urlSplitted[urlSplitted.length - 1]);
+    }
 
+    /**
+     * When multiple bitstreams with the sametype are found, it returns the largest one
+     * @throws Exception
+     */
+    @Test
+    public void testGetPDFURLSameMimeTypes() throws Exception {
+        context.turnOffAuthorisationSystem();
+        Bundle bundle = it.createBundle("ORIGINAL");
+        Bitstream b = bundle.createBitstream(new ByteArrayInputStream("123456789".getBytes(Charsets.UTF_8)));
+        b.setName("size9");
+        b.setFormat(BitstreamFormat.create(context));
+        b.getFormat().setMIMEType("application/pdf");
+        bundle.addBitstream(b);
+        Bitstream b2 = bundle.createBitstream(new ByteArrayInputStream("1".getBytes(Charsets.UTF_8)));
+        b2.setName("size1");
+        b2.setFormat(BitstreamFormat.create(context));
+        b2.getFormat().setMIMEType("application/pdf");
+        bundle.addBitstream(b2);
+        Bitstream b3 = bundle.createBitstream(new ByteArrayInputStream("12345".getBytes(Charsets.UTF_8)));
+        b3.setName("size5");
+        b3.setFormat(BitstreamFormat.create(context));
+        b3.getFormat().setMIMEType("text/richtext");
+        bundle.addBitstream(b3);
+        context.restoreAuthSystemState();
+        context.commit();
+        GoogleMetadata gm = new GoogleMetadata(this.context, it);
+        String[] urlSplitted = gm.getPDFURL().get(0).split("/");
+        assertEquals("size9", urlSplitted[urlSplitted.length - 1]);
+    }
+
+    /**
+     * Multiple bitstreams with same mimetype and size, just returns the first one
+     * @throws Exception
+     */
+    @Test
+    public void testGetPDFURLSameMimeTypesSameSize() throws Exception {
+        context.turnOffAuthorisationSystem();
+        Bundle bundle = it.createBundle("ORIGINAL");
+        Bitstream b = bundle.createBitstream(new ByteArrayInputStream("1".getBytes(Charsets.UTF_8)));
+        b.setName("first");
+        b.setFormat(BitstreamFormat.create(context));
+        b.getFormat().setMIMEType("application/pdf");
+        bundle.addBitstream(b);
+        Bitstream b2 = bundle.createBitstream(new ByteArrayInputStream("1".getBytes(Charsets.UTF_8)));
+        b2.setName("second");
+        b2.setFormat(BitstreamFormat.create(context));
+        b2.getFormat().setMIMEType("application/pdf");
+        bundle.addBitstream(b2);
+        Bitstream b3 = bundle.createBitstream(new ByteArrayInputStream("1".getBytes(Charsets.UTF_8)));
+        b3.setName("third");
+        b3.setFormat(BitstreamFormat.create(context));
+        b3.getFormat().setMIMEType("application/pdf");
+        bundle.addBitstream(b3);
+        context.restoreAuthSystemState();
+        context.commit();
+        GoogleMetadata gm = new GoogleMetadata(this.context, it);
+        String[] urlSplitted = gm.getPDFURL().get(0).split("/");
+        assertEquals("first", urlSplitted[urlSplitted.length - 1]);
+    }
+
+    /**
+     * Test to see if that when an item is marked as primary, that it will still be the result of getPdfURL()
+     * @throws Exception
+     */
+    @Test
+    public void testGetPDFURLWithPrimaryBitstream() throws Exception {
+        context.turnOffAuthorisationSystem();
+        Bundle bundle = it.createBundle("ORIGINAL");
+        Bitstream b = bundle.createBitstream(new ByteArrayInputStream("Larger file than primary".getBytes(Charsets.UTF_8)));
+        b.setName("first");
+        b.setFormat(BitstreamFormat.create(context));
+        b.getFormat().setMIMEType("unknown");
+        bundle.addBitstream(b);
+        Bitstream b2 = bundle.createBitstream(new ByteArrayInputStream("Bitstream with more prioritized mimetype than primary".getBytes(Charsets.UTF_8)));
+        b2.setName("second");
+        b2.setFormat(BitstreamFormat.create(context));
+        b2.getFormat().setMIMEType("application/pdf");
+        bundle.addBitstream(b2);
+        Bitstream b3 = bundle.createBitstream(new ByteArrayInputStream("1".getBytes(Charsets.UTF_8)));
+        b3.setName("primary");
+        b3.setFormat(BitstreamFormat.create(context));
+        b3.getFormat().setMIMEType("Primary");
+        bundle.addBitstream(b3);
+        bundle.setPrimaryBitstreamID(b3.getID());
+        context.restoreAuthSystemState();
+        context.commit();
+        GoogleMetadata gm = new GoogleMetadata(this.context, it);
+        String[] urlSplitted = gm.getPDFURL().get(0).split("/");
+        assertEquals("primary", urlSplitted[urlSplitted.length - 1]);
+    }
+
+    /**
+     * Test to make sure mimetypes can be undefined in the property file, just give them lowest priority if
+     * this is the case and return the largest.
+     * @throws Exception
+     */
+    @Test
+    public void testGetPDFURLWithUndefinedMimeTypes() throws Exception {
+        context.turnOffAuthorisationSystem();
+        Bundle bundle = it.createBundle("ORIGINAL");
+        Bitstream b = bundle.createBitstream(new ByteArrayInputStream("12".getBytes(Charsets.UTF_8)));
+        b.setName("small");
+        b.setFormat(BitstreamFormat.create(context));
+        b.getFormat().setMIMEType("unknown type 1");
+        bundle.addBitstream(b);
+        Bitstream b2 = bundle.createBitstream(new ByteArrayInputStream("12121212".getBytes(Charsets.UTF_8)));
+        b2.setName("medium");
+        b2.setFormat(BitstreamFormat.create(context));
+        b2.getFormat().setMIMEType("unknown type 2");
+        bundle.addBitstream(b2);
+        Bitstream b3 = bundle.createBitstream(new ByteArrayInputStream("12121212121212".getBytes(Charsets.UTF_8)));
+        b3.setName("large");
+        b3.setFormat(BitstreamFormat.create(context));
+        b3.getFormat().setMIMEType("unknown type ");
+        bundle.addBitstream(b3);
+        context.restoreAuthSystemState();
+        context.commit();
+        GoogleMetadata gm = new GoogleMetadata(this.context, it);
+        String[] urlSplitted = gm.getPDFURL().get(0).split("/");
+        assertEquals("large", urlSplitted[urlSplitted.length - 1]);
+    }
+
+
+    /**
+     * Test for crash when no bundle is given
+     * @throws Exception
+     */
+    @Test
+    public void testGetPDFURLWithNoBundle() throws Exception {
+        GoogleMetadata gm = new GoogleMetadata(this.context, it);
+        assertEquals(0, gm.getPDFURL().size());
+    }
+
+    /**
+     * Test for crash when no bitstreams are in the bundle
+     * @throws Exception
+     */
+    @Test
+    public void testGetPDFURLWithNoBitstreams() throws Exception {
+        context.turnOffAuthorisationSystem();
+        Bundle bundle = it.createBundle("ORIGINAL");
+        context.restoreAuthSystemState();
+        context.commit();
+        GoogleMetadata gm = new GoogleMetadata(this.context, it);
+        assertEquals(0, gm.getPDFURL().size());
+    }
+
+    @After
+    @Override
+    public void destroy()
+    {
+        it = null;
+        super.destroy();
     }
 
 
