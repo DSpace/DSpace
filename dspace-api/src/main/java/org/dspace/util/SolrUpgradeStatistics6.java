27bb60f6553b510fc3ed4405aede2f0164b2240d
Terry W Brady <twb27@georgetown.edu>
2017-06-06 13:38:14 -0700

    simple launcher

--- dspace-api/src/main/java/org/dspace/util/SolrUpgradeStatistics6.java
+++ dspace-api/src/main/java/org/dspace/util/SolrUpgradeStatistics6.java
@@ -20,13 +20,21 @@ import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
 import org.apache.solr.client.solrj.request.CoreAdminRequest;
 import org.apache.solr.client.solrj.request.LukeRequest;
 import org.apache.solr.client.solrj.response.CoreAdminResponse;
+import org.apache.solr.client.solrj.response.FacetField;
 import org.apache.solr.client.solrj.response.FieldStatsInfo;
 import org.apache.solr.client.solrj.response.LukeResponse;
+import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.client.solrj.response.RangeFacet;
 import org.apache.solr.common.luke.FieldFlag;
 import org.apache.solr.common.params.CoreAdminParams;
 import org.apache.solr.common.params.FacetParams;
 import org.dspace.core.ConfigurationManager;
+import org.dspace.services.ConfigurationService;
+import org.dspace.services.factory.DSpaceServicesFactory;
+import org.dspace.statistics.ObjectCount;
+import org.dspace.statistics.factory.StatisticsServiceFactory;
+import org.dspace.statistics.service.SolrLoggerService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
 import java.io.FilenameFilter;
@@ -44,8 +52,22 @@ public class SolrUpgradeStatistics6
 	private static final String INDEX_NAME_OPTION = "i";
 	private static final String NUMREC_OPTION = "n";
         private static final String HELP_OPTION = "h";
-        private static final int numrec_default = 1000;
+        private static final int NUMREC_DEFAULT = 1000;
+        private static final String INDEX_DEFAULT = "statistics";
 	private static final Logger log = Logger.getLogger(SolrUpgradeStatistics6.class);
+	private ConfigurationService configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
+	private HttpSolrServer server;
+	private int numRec;
+
+	protected SolrLoggerService solrLoggerService = StatisticsServiceFactory.getInstance().getSolrLoggerService();
+
+	public SolrUpgradeStatistics6(String indexName, int numRec) {
+                String serverPath = configurationService.getProperty("solr-statistics.server");
+                serverPath = serverPath.replaceAll("statistics$", indexName);
+                System.out.println("Connecting to " + serverPath);
+                server = new HttpSolrServer(serverPath);
+                this.numRec = numRec;
+	}
 
 	/**
 	 * Entry point for command-line invocation
@@ -57,8 +79,8 @@ public class SolrUpgradeStatistics6
 		CommandLineParser parser = new PosixParser();
 		Options options = makeOptions();
 
-                String indexName = "statistics";
-                int numrec = numrec_default;
+                String indexName = INDEX_DEFAULT;
+                int numrec = NUMREC_DEFAULT;
 		try
 		{
 			CommandLine line = parser.parse(options, args);
@@ -69,15 +91,15 @@ public class SolrUpgradeStatistics6
 
 			if (line.hasOption(INDEX_NAME_OPTION))
 			{
-			    indexName = line.getOptionValue(INDEX_NAME_OPTION, "statistics");
+			    indexName = line.getOptionValue(INDEX_NAME_OPTION, INDEX_DEFAULT);
 			}
 			else
 			{
-			    System.err.println("No index name provided, defaulting to \"statistics\".");
+			    System.err.println("No index name provided, defaulting to : "+ INDEX_DEFAULT);
 			}
 			
-			if (line.hasOption(NUMREC_OPTION)) {
-			        numrec = Integer.parseInt(line.getOptionValue(NUMREC_OPTION,""+numrec_default));
+                        if (line.hasOption(NUMREC_OPTION)) {
+			        numrec = Integer.parseInt(line.getOptionValue(NUMREC_OPTION,""+NUMREC_DEFAULT));
 			}
 
 		}
@@ -87,10 +109,32 @@ public class SolrUpgradeStatistics6
 			printHelpAndExit(options, 1);
 		}
 		
-		System.out.println(String.format("==>%s: %d", indexName, numrec));
+		SolrUpgradeStatistics6 upgradeStats = new SolrUpgradeStatistics6(indexName, numrec);
+		try {
+                        upgradeStats.run();
+                } catch (SolrServerException e) {
+                        log.error("Error querying stats", e);
+                }
 	}
 
-	private static Options makeOptions() {
+	private void run() throws SolrServerException {
+                String query = "NOT(id:*-*) AND type:2";
+	        SolrQuery sQ = new SolrQuery();
+	        sQ.setQuery(query);
+	        sQ.setFacet(true);
+	        sQ.addFacetField("id");
+	        sQ.setFacetMinCount(1);
+	        QueryResponse sr = server.query(sQ);
+	        
+                for(FacetField ff: sr.getFacetFields()) {
+                        System.out.println(ff);
+                        for(FacetField.Count count: ff.getValues()) {
+                                System.out.println(String.format("\t%s: %d", count.getName(), count.getCount()));
+                        }
+                }
+        }
+
+        private static Options makeOptions() {
 		Options options = new Options();
 		options.addOption(HELP_OPTION, "help", false, "Get help on options for this command.");
 		options.addOption(INDEX_NAME_OPTION, "index-name", true,
