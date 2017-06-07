337da1952c0d11a026216781ec6464f14a0a0aa1
jsavell <jsavell@library.tamu.edu>
2017-06-07 16:44:42 -0500

    port AuthorizeAdmin to 6x

--- dspace/modules/additions/src/main/java/edu/tamu/dspace/curate/AuthorizeAdmin.java
+++ dspace/modules/additions/src/main/java/edu/tamu/dspace/curate/AuthorizeAdmin.java
@@ -3,12 +3,13 @@ package edu.tamu.dspace.curate;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.dspace.authorize.AuthorizeException;
-import org.dspace.authorize.AuthorizeManager;
 import org.dspace.authorize.ResourcePolicy;
+import org.dspace.authorize.factory.AuthorizeServiceFactory;
+import org.dspace.authorize.service.AuthorizeService;
+import org.dspace.authorize.service.ResourcePolicyService;
 import org.dspace.content.Bitstream;
 import org.dspace.content.Bundle;
 import org.dspace.content.DSpaceObject;
@@ -18,16 +19,26 @@ import org.dspace.core.Context;
 import org.dspace.curate.AbstractCurationTask;
 import org.dspace.curate.Curator;
 import org.dspace.eperson.Group;
+import org.dspace.eperson.factory.EPersonServiceFactory;
+import org.dspace.eperson.service.GroupService;
 
 public class AuthorizeAdmin extends AbstractCurationTask {
 
     private int result = Curator.CURATE_SUCCESS;
     private StringBuilder sb = new StringBuilder();
+    
+    private AuthorizeService authorizeService;
+    private GroupService groupService;
+    private ResourcePolicyService resourcePolicyService;
 
     @Override
     public void init(Curator curator, String taskId) throws IOException {
         sb = new StringBuilder();
         super.init(curator, taskId);
+        
+        authorizeService = AuthorizeServiceFactory.getInstance().getAuthorizeService();
+        resourcePolicyService = AuthorizeServiceFactory.getInstance().getResourcePolicyService();
+        groupService = EPersonServiceFactory.getInstance().getGroupService();
     }
 
     @Override
@@ -46,22 +57,22 @@ public class AuthorizeAdmin extends AbstractCurationTask {
     @Override
     protected void performItem(Item item) throws SQLException, IOException {
         Context context = Curator.curationContext();
-        Item workingItem = Item.find(context, item.getID());
+        Item workingItem = itemService.find(context, item.getID());
 
         try {
             removeReadPolicies(context, workingItem);
 
             // Generally, we want items themselves to be available for everyone to access, only restricting their actual content.
-            AuthorizeManager.addPolicy(context, workingItem, Constants.READ, Group.findByName(context, "Anonymous"));
+            authorizeService.addPolicy(context, workingItem, Constants.READ, groupService.findByName(context, "Anonymous"));
 
             for (Bundle bundle : workingItem.getBundles()) {
                 for (Bitstream bs : bundle.getBitstreams()) {
-                    AuthorizeManager.addPolicy(context, bs, Constants.READ, Group.findByName(context, "Administrator"));
+                    authorizeService.addPolicy(context, bs, Constants.READ, groupService.findByName(context, "Administrator"));
                 }
-                AuthorizeManager.addPolicy(context, bundle, Constants.READ, Group.findByName(context, "Administrator"));
+                authorizeService.addPolicy(context, bundle, Constants.READ, groupService.findByName(context, "Administrator"));
             }
 
-            workingItem.update();
+            itemService.update(context, workingItem);
             context.commit();
 
             sb.append(workingItem.getHandle() + " authorized for group \"Administrator\"\n");
@@ -77,32 +88,33 @@ public class AuthorizeAdmin extends AbstractCurationTask {
      * @param c
      * @param item
      * @throws SQLException
+     * @throws AuthorizeException 
      */
-    private void removeReadPolicies(Context c, Item item) throws SQLException {
+    private void removeReadPolicies(Context c, Item item) throws SQLException, AuthorizeException {
         // The special groups whose READ permissions we'll be deleting. Leave others alone.
         List<Group> affectedGroups = new ArrayList<Group>();
-        affectedGroups.add(Group.findByName(c, "Anonymous"));
-        affectedGroups.add(Group.findByName(c, "Administrator"));
-        affectedGroups.add(Group.findByName(c, "member"));
+        affectedGroups.add(groupService.findByName(c, "Anonymous"));
+        affectedGroups.add(groupService.findByName(c, "Administrator"));
+        affectedGroups.add(groupService.findByName(c, "member"));
 
         // Start with a blank slate.
         List<ResourcePolicy> allReadPolicies = new ArrayList<ResourcePolicy>();
 
         // Add item's READ policies to the bucket list
-        allReadPolicies.addAll(AuthorizeManager.getPoliciesActionFilter(c, item, Constants.READ));
+        allReadPolicies.addAll(authorizeService.getPoliciesActionFilter(c, item, Constants.READ));
 
         // Add in all the bundle and bitstream READ policies
         for (Bundle bundle : item.getBundles()) {
-            allReadPolicies.addAll(AuthorizeManager.getPoliciesActionFilter(c, bundle, Constants.READ));
+            allReadPolicies.addAll(authorizeService.getPoliciesActionFilter(c, bundle, Constants.READ));
             for (Bitstream bs : bundle.getBitstreams()) {
-                allReadPolicies.addAll(AuthorizeManager.getPoliciesActionFilter(c, bs, Constants.READ));
+                allReadPolicies.addAll(authorizeService.getPoliciesActionFilter(c, bs, Constants.READ));
             }
         }
 
         // Delete only those policies that fall into our special groups
         for (ResourcePolicy readPolicy : allReadPolicies) {
             if (affectedGroups.contains(readPolicy.getGroup())) {
-                readPolicy.delete();
+				resourcePolicyService.delete(c, readPolicy);
             }
         }
     }
