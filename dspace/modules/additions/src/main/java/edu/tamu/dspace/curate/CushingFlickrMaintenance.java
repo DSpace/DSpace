7ca09603f62d7235301589ad667cf52e1f00176a
jsavell <jsavell@library.tamu.edu>
2017-06-07 14:35:51 -0500

    port flickr task to 6x

--- dspace/modules/additions/src/main/java/edu/tamu/dspace/curate/CushingFlickrMaintenance.java
+++ dspace/modules/additions/src/main/java/edu/tamu/dspace/curate/CushingFlickrMaintenance.java
@@ -3,12 +3,19 @@ package edu.tamu.dspace.curate;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.Bitstream;
 import org.dspace.content.Bundle;
 import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
+import org.dspace.content.factory.ContentServiceFactory;
+import org.dspace.content.service.BitstreamService;
+import org.dspace.content.service.BundleService;
+import org.dspace.content.service.ItemService;
 import org.dspace.core.Constants;
 import org.dspace.curate.AbstractCurationTask;
 import org.dspace.curate.Curator;
@@ -66,58 +73,79 @@ public class CushingFlickrMaintenance extends AbstractCurationTask {
         try {
             int count = 0;
 
-            for (Bundle bundle : item.getBundles("ORIGINAL")) {
-                for (Bitstream bitstream : bundle.getBitstreams()) {
-                    // If we got a hit, remove the offending bitstream, download a replacement and delete the thumbnails
-                    if (bitstream.getChecksum().equals(BLANK_CHECKSUM)) {
+            BitstreamService bitstreamService = ContentServiceFactory.getInstance().getBitstreamService();
+            BundleService bundleService = ContentServiceFactory.getInstance().getBundleService();
+            ItemService itemService = ContentServiceFactory.getInstance().getItemService();
 
-                        // Extract image id from the bitstream name
-                        sb.append("Bitstream name: " + bitstream.getName());
+            Iterator<Bundle> ibi = item.getBundles().iterator();
+            
+            while (ibi.hasNext()) {
+            	Bundle bundle = ibi.next();
+            	if (bundle.getName().equals("ORIGINAL")) {
+	                for (Bitstream bitstream : bundle.getBitstreams()) {
+	                    // If we got a hit, remove the offending bitstream, download a replacement and delete the thumbnails
+	                    if (bitstream.getChecksum().equals(BLANK_CHECKSUM)) {
+	
+	                        // Extract image id from the bitstream name
+	                        sb.append("Bitstream name: " + bitstream.getName());
+	
+	                        flikrId = bitstream.getName().split("_")[0];
+	                        flikrSizesUrl = FLIRK_API_URL + flikrId;
+	
+	                        sb.append("flickrId:" + flikrId);
+	
+	                        // Contact Flikr API
+	                        Document availableSizes = builder.build(flikrSizesUrl);
+	                        Element root = availableSizes.getRootElement();
+	
+	                        // Verify that the response is error-free
+	                        if (!root.getAttributeValue("stat").equals("ok")) {
+	                            throw new JDOMException("Status was in error on id: " + flikrId);
+	                        }
+	
+	                        // Check to see if a large image exists (which it probably won't or this item wouldn't be in error in the first place)
+	                        XPath xpath = XPath.newInstance("//size[@label='Large']/@source");
+	                        Attribute sourceUrl = (Attribute) xpath.selectSingleNode(root);
+	                        URL imageUrl;
+	                        if (sourceUrl == null) {
+	                            xpath = XPath.newInstance("//size[@label='Original']/@source");
+	                            sourceUrl = (Attribute) xpath.selectSingleNode(root);
+	                        }
+	                        imageUrl = new URL(sourceUrl.getValue());
+	
+	                        // Create a bitstream from the image source
+	                        Bitstream bs = bitstreamService.create(Curator.curationContext(), bundle, imageUrl.openStream());
+	                        bs.setDescription(Curator.curationContext(), "Updated Flikr image");
+	                        bs.setName(Curator.curationContext(),bitstream.getName());
+	
+	                        bundleService.removeBitstream(Curator.curationContext(), bundle, bitstream);
+	                      
+	                        Iterator<Bundle> ir = item.getBundles().iterator();
+	                        List<Bitstream> removableBitstreams = new ArrayList<Bitstream>();
+	                        Bundle removableBundle = null;
+	                        while (ir.hasNext()) {
+	                        	Bundle tbundle = ir.next();
+	                        	if (tbundle.getName().equals("THUMBNAIL")) {
+	                        		removableBundle = tbundle;
+	                        		Iterator<Bitstream> bir = tbundle.getBitstreams().iterator();
+	                        		while (bir.hasNext()) {
+		                                removableBitstreams.add(bir.next());
+	                        		}
+	                        	}
+	                        }
+	                        
+	                        for (Bitstream removableBitstream : removableBitstreams) {
+	                        	bundleService.removeBitstream(Curator.curationContext(), removableBundle, removableBitstream);
+	                        }
+                            itemService.removeBundle(Curator.curationContext(), item, removableBundle);
+	                    } // end of if the bitstream has the blank image checksum
+	                } // end of for each bitstream in the bundle's bitstreams
+            	} // end of if bundle is the "ORIGINAL" bundle
+            } // end of for each bundle
 
-                        flikrId = bitstream.getName().split("_")[0];
-                        flikrSizesUrl = FLIRK_API_URL + flikrId;
-
-                        sb.append("flickrId:" + flikrId);
-
-                        // Contact Flikr API
-                        Document availableSizes = builder.build(flikrSizesUrl);
-                        Element root = availableSizes.getRootElement();
-
-                        // Verify that the response is error-free
-                        if (!root.getAttributeValue("stat").equals("ok")) {
-                            throw new JDOMException("Status was in error on id: " + flikrId);
-                        }
-
-                        // Check to see if a large image exists (which it probably won't or this item wouldn't be in error in the first place)
-                        XPath xpath = XPath.newInstance("//size[@label='Large']/@source");
-                        Attribute sourceUrl = (Attribute) xpath.selectSingleNode(root);
-                        URL imageUrl;
-                        if (sourceUrl == null) {
-                            xpath = XPath.newInstance("//size[@label='Original']/@source");
-                            sourceUrl = (Attribute) xpath.selectSingleNode(root);
-                        }
-                        imageUrl = new URL(sourceUrl.getValue());
-
-                        // Create a bitstream from the image source
-                        Bitstream bs = bundle.createBitstream(imageUrl.openStream());
-                        bs.setDescription("Updated Flikr image");
-                        bs.setName(bitstream.getName());
-
-                        bundle.removeBitstream(bitstream);
-
-                        for (Bundle tbundle : item.getBundles("THUMBNAIL")) {
-                            for (Bitstream tbitstream : tbundle.getBitstreams()) {
-                                tbundle.removeBitstream(tbitstream);
-                            }
-                            item.removeBundle(tbundle);
-                        }
-
-                    } // end of if the bitstream has the blank image checksum
-                } // end of for each bitstream in the bundle's bitstreams
-            } // end of for each bundle that is the "ORIGINAL" bundle
-
-            item.update();
+            itemService.update(Curator.curationContext(), item);
             sb.append(item.getHandle() + ": " + count + " images deleted. \n");
+            
         } catch (AuthorizeException e) {
             result = Curator.CURATE_ERROR;
             sb.append("Authorization failure on item: " + item.getHandle() + "\nAborting...");
