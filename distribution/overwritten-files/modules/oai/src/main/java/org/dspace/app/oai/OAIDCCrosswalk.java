97a0642ee89c450a6465afaf38dffd683fe07b18
FacundoAdorno <adorno.facundo@gmail.com>
2017-06-22 10:28:02 -0300

    -#4452 Se agregó un LICENSE_HEADER generico
    Se agregaron los headers de licencia necesarios para poder ejecutar el plugin
    "license-maven-plugin". Ahora se puede ejecutar sin error el "./build.bash update [--full]".
    
    Cuando se ejecuta el script "build.bash" propio de sedici, el cual trata de automatizar
    la instalación y la actualización del source de DSpace, se ejecuta la meta "license:format"
    la cual verifica que todos los sources tengan sus respectivas licencias, y si no la tienen,
    el plugin relacionado se las agrega. (http://code.mycila.com/license-maven-plugin).
    
    Ademas se corrigió un error de sintaxis en el "build.defaults".

--- distribution/overwritten-files/modules/oai/src/main/java/org/dspace/app/oai/OAIDCCrosswalk.java
+++ distribution/overwritten-files/modules/oai/src/main/java/org/dspace/app/oai/OAIDCCrosswalk.java
@@ -1,11 +1,9 @@
 /**
- * <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
- * <html><head>
- * <title>301 Moved Permanently</title>
- * </head><body>
- * <h1>Moved Permanently</h1>
- * <p>The document has moved <a href="https://svn.duraspace.org/dspace/licenses/LICENSE_HEADER">here</a>.</p>
- * </body></html>
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
  */
 package org.dspace.app.oai;
 
