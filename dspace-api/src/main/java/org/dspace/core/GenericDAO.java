801f39daebc0d62ff0bba557e8230f4a93b158af
Mark H. Wood <mhwood@Ameritech.Net>
2017-08-08 23:46:52 -0400

    [DS-3667] Document fundamental persistence support classes.

--- dspace-api/src/main/java/org/dspace/core/GenericDAO.java
+++ dspace-api/src/main/java/org/dspace/core/GenericDAO.java
@@ -7,8 +7,6 @@
  */
 package org.dspace.core;
 
-import org.dspace.content.DSpaceObject;
-
 import java.sql.SQLException;
 import java.util.List;
 import java.util.UUID;
@@ -16,17 +14,36 @@ import java.util.UUID;
 /**
  * Generic Database Access Object interface class that should be implemented by all DAOs.
  * It offers up a lot of general methods so these don't need to be declared again in each DAO.
- * The default hibernate implementation offers up a class that implements all these methods.
+ * The default Hibernate implementation offers up a class that implements all these methods.
  *
  * @author kevinvandevelde at atmire.com
- * @param <T> class type
+ * @param <T> type which is accessed by this DAO, for example Item.
  */
 public interface GenericDAO<T>
 {
+    /**
+     * Create a new instance of this type in the database.
+     * @param context current DSpace context.
+     * @param t type to be created.
+     * @return entity tracking the created instance.
+     * @throws SQLException
+     */
     public T create(Context context, T t) throws SQLException;
 
+    /**
+     * Persist this instance in the database.
+     * @param context current DSpace context.
+     * @param t type created here.
+     * @throws SQLException passed through.
+     */
     public void save(Context context, T t) throws SQLException;
 
+    /**
+     * Remove an instance from the database.
+     * @param context current DSpace context.
+     * @param t type of the instance to be removed.
+     * @throws SQLException passed through.
+     */
     public void delete(Context context, T t) throws SQLException;
 
     /**
@@ -49,8 +66,24 @@ public interface GenericDAO<T>
      */
     public T findUnique(Context context, String query) throws SQLException;
 
+    /**
+     * Fetch the entity identified by its legacy database identifier.
+     * @param context current DSpace context.
+     * @param clazz class of entity to be found.
+     * @param id legacy database record ID.
+     * @return the found entity.
+     * @throws SQLException passed through.
+     */
     public T findByID(Context context, Class clazz, int id) throws SQLException;
 
+    /**
+     * Fetch the entity identified by its UUID primary key.
+     * @param context current DSpace context.
+     * @param clazz class of entity to be found.
+     * @param id primary key of the database record.
+     * @return the found entity.
+     * @throws SQLException
+     */
     public T findByID(Context context, Class clazz, UUID id) throws SQLException;
 
     /**
