f613e62b3dbddb2660f4b84ef98d06cf7e6f347a
Peter Broadwell <pbroadwell@gmail.com>
2017-08-15 16:58:30 -0700

    Insert AND between terms of Getty TGN spatial extent autocomplete query

--- dspace-api/src/main/java/org/dspace/content/authority/TGNAuthority.java
+++ dspace-api/src/main/java/org/dspace/content/authority/TGNAuthority.java
@@ -23,9 +23,9 @@ public class TGNAuthority extends GettyAuthority {
 	@Override
 	public Choices getMatches(String field, String text, Collection collection, int start, int limit, String locale) {
 
-    /* PMB: Insert "AND" between terms so that queries like "Los Angeles" work better */
+		/* PMB: Insert "AND" between terms so that queries like "Los Angeles" work better */
 
-    String searchText = text.replace(" ", " AND ");
+		String searchText = text.replace(" ", " AND "); 
 
 		String sparQL = String.format(query, searchText);
 		Choices results = query(sparQL);
