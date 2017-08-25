59869b49cade650e0d8142fd5c820dba151215a4
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2017-08-25 19:07:09 +0200

    D4CRIS-271 consolidate metadata not-editable configuration

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V5.7.0.1__DSpaceCRIS-Upgrade-crismodule_5_to_57.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V5.7.0.1__DSpaceCRIS-Upgrade-crismodule_5_to_57.sql
@@ -11,9 +11,16 @@ BEGIN
 	'DELETE FROM JDYNA_VALUES WHERE id IN (SELECT value_id FROM CRIS_RP_PROP WHERE typo_id in (SELECT id from CRIS_RP_PDEF where SHORTNAME in ("system-orcid-token-orcid-works-create","system-orcid-token-funding-create","system-orcid-token-funding-update","system-orcid-token-activities-update")));
 	DELETE FROM CRIS_RP_PROP WHERE typo_id IN (SELECT id from CRIS_RP_PDEF where SHORTNAME in ("system-orcid-token-orcid-works-create","system-orcid-token-funding-create","system-orcid-token-funding-update","system-orcid-token-activities-update"));
 	DELETE FROM CRIS_RP_PDEF WHERE SHORTNAME IN ("system-orcid-token-orcid-works-create","system-orcid-token-funding-create","system-orcid-token-funding-update","system-orcid-token-activities-update");
+	DELETE FROM JDYNA_VALUES WHERE id IN (SELECT value_id FROM CRIS_RP_PROP WHERE typo_id in (SELECT id from CRIS_RP_PDEF where SHORTNAME in ("orcid-profile-pref-biography","orcid-profile-pref-email","orcid-profile-pref-fullName","orcid-profile-pref-preferredName","orcid-profile-pref-otheremails")));
+	DELETE FROM CRIS_RP_PROP WHERE typo_id IN (SELECT id from CRIS_RP_PDEF where SHORTNAME in ("orcid-profile-pref-biography","orcid-profile-pref-email","orcid-profile-pref-fullName","orcid-profile-pref-preferredName","orcid-profile-pref-otheremails"));	
 	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-token-activities-update" WHERE SHORTNAME = "system-orcid-token-orcid-works-update";
 	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-token-person-update" WHERE SHORTNAME = "system-orcid-token-orcid-bio-update";
-	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-token-read-limited" WHERE SHORTNAME = "system-orcid-token-orcid-profile-read-limited";'
+	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-token-read-limited" WHERE SHORTNAME = "system-orcid-token-orcid-profile-read-limited";
+	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-profile-pref-biography" WHERE SHORTNAME = "orcid-profile-pref-biography";
+	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-profile-pref-email" WHERE SHORTNAME = "orcid-profile-pref-email";
+	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-profile-pref-fullName" WHERE SHORTNAME = "orcid-profile-pref-fullName";
+	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-profile-pref-preferredName" WHERE SHORTNAME = "orcid-profile-pref-preferredName";
+	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-profile-pref-otheremails" WHERE SHORTNAME = "orcid-profile-pref-otheremails";'	
 	EXCEPTION
 	WHEN OTHERS
     THEN
--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V5.7.0.1__DSpaceCRIS-Upgrade-crismodule_5_to_57.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V5.7.0.1__DSpaceCRIS-Upgrade-crismodule_5_to_57.sql
@@ -8,14 +8,26 @@
 
 do $$
 begin
-	
+	-- delete scopes	
 	DELETE FROM JDYNA_VALUES WHERE id IN (SELECT value_id FROM CRIS_RP_PROP WHERE typo_id in (SELECT id from CRIS_RP_PDEF where SHORTNAME in ('system-orcid-token-orcid-works-create','system-orcid-token-funding-create','system-orcid-token-funding-update','system-orcid-token-activities-update')));
 	DELETE FROM CRIS_RP_PROP WHERE typo_id IN (SELECT id from CRIS_RP_PDEF where SHORTNAME in ('system-orcid-token-orcid-works-create','system-orcid-token-funding-create','system-orcid-token-funding-update','system-orcid-token-activities-update'));
 	DELETE FROM CRIS_RP_PDEF WHERE SHORTNAME IN ('system-orcid-token-orcid-works-create','system-orcid-token-funding-create','system-orcid-token-funding-update','system-orcid-token-activities-update');
+
+	-- delete metadata configuration
+	DELETE FROM JDYNA_VALUES WHERE id IN (SELECT value_id FROM CRIS_RP_PROP WHERE typo_id in (SELECT id from CRIS_RP_PDEF where SHORTNAME in ('orcid-profile-pref-biography','orcid-profile-pref-email','orcid-profile-pref-fullName','orcid-profile-pref-preferredName')));
+	DELETE FROM CRIS_RP_PROP WHERE typo_id IN (SELECT id from CRIS_RP_PDEF where SHORTNAME in ('orcid-profile-pref-biography','orcid-profile-pref-email','orcid-profile-pref-fullName','orcid-profile-pref-preferredName'));
 	
+	-- upgrade scopes
 	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = 'system-orcid-token-activities-update' WHERE SHORTNAME = 'system-orcid-token-orcid-works-update';
 	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = 'system-orcid-token-person-update' WHERE SHORTNAME = 'system-orcid-token-orcid-bio-update';
 	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = 'system-orcid-token-read-limited' WHERE SHORTNAME = 'system-orcid-token-orcid-profile-read-limited';
+	
+	-- upgrade metadata not editable
+	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = 'system-orcid-profile-pref-biography' WHERE SHORTNAME = 'orcid-profile-pref-biography';
+	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = 'system-orcid-profile-pref-email' WHERE SHORTNAME = 'orcid-profile-pref-email';
+	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = 'system-orcid-profile-pref-fullName' WHERE SHORTNAME = 'orcid-profile-pref-fullName';
+	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = 'system-orcid-profile-pref-preferredName' WHERE SHORTNAME = 'orcid-profile-pref-preferredName';
+	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = 'system-orcid-profile-pref-otheremails' WHERE SHORTNAME = 'orcid-profile-pref-otheremails';
 
 exception when others then
  
