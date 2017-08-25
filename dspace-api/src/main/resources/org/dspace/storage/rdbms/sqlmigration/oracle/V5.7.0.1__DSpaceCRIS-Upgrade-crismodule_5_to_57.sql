4f7fa8046b3c1952f884a202ceeeb820b7e3988f
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2017-08-25 09:19:11 +0200

    D4CRIS-271 consolidate orcid scopes

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V5.7.0.1__DSpaceCRIS-Upgrade-crismodule_5_to_57.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V5.7.0.1__DSpaceCRIS-Upgrade-crismodule_5_to_57.sql
@@ -8,7 +8,10 @@
 
 BEGIN
 	EXECUTE IMMEDIATE
-	'UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-token-activities-update" WHERE SHORTNAME = "system-orcid-token-orcid-works-update";
+	'DELETE FROM JDYNA_VALUES WHERE id IN (SELECT value_id FROM CRIS_RP_PROP WHERE typo_id in (SELECT id from CRIS_RP_PDEF where SHORTNAME in ("system-orcid-token-orcid-works-create","system-orcid-token-funding-create","system-orcid-token-funding-update","system-orcid-token-activities-update")));
+	DELETE FROM CRIS_RP_PROP WHERE typo_id IN (SELECT id from CRIS_RP_PDEF where SHORTNAME in ("system-orcid-token-orcid-works-create","system-orcid-token-funding-create","system-orcid-token-funding-update","system-orcid-token-activities-update"));
+	DELETE FROM CRIS_RP_PDEF WHERE SHORTNAME IN ("system-orcid-token-orcid-works-create","system-orcid-token-funding-create","system-orcid-token-funding-update","system-orcid-token-activities-update");
+	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-token-activities-update" WHERE SHORTNAME = "system-orcid-token-orcid-works-update";
 	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-token-person-update" WHERE SHORTNAME = "system-orcid-token-orcid-bio-update";
 	UPDATE CRIS_RPAGE_PDEF SET SHORTNAME = "system-orcid-token-read-limited" WHERE SHORTNAME = "system-orcid-token-orcid-profile-read-limited";'
 	EXCEPTION
