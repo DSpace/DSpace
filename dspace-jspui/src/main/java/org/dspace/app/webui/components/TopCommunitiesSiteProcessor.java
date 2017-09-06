7d2a0dfa64ee20cc9d90944f04b56f6255854633
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2017-09-06 12:12:46 +0200

    D4CRIS-288 RHD-251 add possibility to hide using community-list.topcommunity.show

--- dspace-jspui/src/main/java/org/dspace/app/webui/components/TopCommunitiesSiteProcessor.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/components/TopCommunitiesSiteProcessor.java
@@ -8,13 +8,21 @@
 package org.dspace.app.webui.components;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.dspace.authorize.AuthorizeException;
+import org.dspace.authorize.AuthorizeManager;
+import org.dspace.content.Collection;
 import org.dspace.content.Community;
+import org.dspace.core.ConfigurationManager;
+import org.dspace.core.Constants;
 import org.dspace.core.Context;
+import org.dspace.eperson.Group;
 import org.dspace.plugin.PluginException;
 import org.dspace.plugin.SiteHomeProcessor;
 
@@ -47,6 +55,31 @@ public class TopCommunitiesSiteProcessor implements SiteHomeProcessor
         try
         {
             communities = Community.findAllTop(context);
+            List<Community> topCom = new ArrayList<Community>();
+            String showCrisComm = ConfigurationManager.getProperty("community-list.topcommunity.show");
+
+            if(AuthorizeManager.isAdmin(context) || 
+                    StringUtils.equalsIgnoreCase(showCrisComm, "all") ||
+                    ( context.getCurrentUser() != null && StringUtils.equalsIgnoreCase(showCrisComm, "user") ) ){
+                for (int com = 0; com < communities.length; com++)
+                {
+                    topCom.add(communities[com]);
+                }
+            }else{                
+                for (int com = 0; com < communities.length; com++)
+                {
+                    Group[] groups = AuthorizeManager.getAuthorizedGroups(context, communities[com], Constants.READ);
+                    for(Group group : groups){
+                        if(group.getID()==0|| Group.isMember(context, group.getID())){
+                            topCom.add(communities[com]);
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            communities = new Community[topCom.size()];
+            communities = topCom.toArray(communities);
         }
         catch (SQLException e)
         {
