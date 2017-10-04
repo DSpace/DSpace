548407fdd7466dbb35ca3d66b3c5c5965c8a529a
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2017-10-04 07:52:27 +0200

    DS-3489: Added an @Override equals and hashCode function to rest classes

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/link/HalLinkFactory.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/link/HalLinkFactory.java
@@ -1,9 +1,13 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.app.rest.link;
 
-import org.apache.poi.ss.formula.functions.T;
-import org.dspace.app.rest.DiscoveryRestController;
 import org.dspace.app.rest.model.hateoas.HALResource;
-import org.dspace.app.rest.model.hateoas.SearchResultsResource;
 import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
