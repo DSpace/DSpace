e71c0361119579b7c42efa71e88155bcdbb1c0ed
Andrea Bollini <andrea.bollini@4science.it>
2017-11-02 12:28:04 +0100

    DS-3740 initial draft of the workspaceitems endpoint

---
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/model/WorkspaceItemRest.java
@@ -0,0 +1,83 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.model;
+
+import java.util.Date;
+
+import org.dspace.app.rest.RestResourceController;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * The WorkspaceItem REST Resource
+ * 
+ * @author Andrea Bollini (andrea.bollini at 4science.it)
+ *
+ */
+public class WorkspaceItemRest extends BaseObjectRest<Integer> {
+	public static final String NAME = "workspaceitem";
+	public static final String CATEGORY = RestModel.SUBMISSION;
+
+	private Date lastModified = new Date();
+
+	@JsonIgnore
+	private ItemRest item;
+	
+	@JsonIgnore
+	private SubmissionDefinitionRest submissionDefinition;
+	
+	@JsonIgnore
+	private EPersonRest submitter;
+	
+	@Override
+	public String getCategory() {
+		return CATEGORY;
+	}
+	
+	@Override
+	public String getType() {
+		return NAME;
+	}
+
+	public Date getLastModified() {
+		return lastModified;
+	}
+	
+	public void setLastModified(Date lastModified){
+		this.lastModified = lastModified;
+	}
+
+	public ItemRest getItem() {
+		return item;
+	}
+
+	public void setItem(ItemRest item) {
+		this.item = item;
+	}
+
+	public SubmissionDefinitionRest getSubmissionDefinition() {
+		return submissionDefinition;
+	}
+
+	public void setSubmissionDefinition(SubmissionDefinitionRest submissionDefinition) {
+		this.submissionDefinition = submissionDefinition;
+	}
+
+	public EPersonRest getSubmitter() {
+		return submitter;
+	}
+
+	public void setSubmitter(EPersonRest submitter) {
+		this.submitter = submitter;
+	}
+	
+	@Override
+	public Class getController() {
+		return RestResourceController.class;
+	}
+}
---
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/repository/WorkspaceItemRestRepository.java
@@ -0,0 +1,83 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.repository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.dspace.app.rest.converter.WorkspaceItemConverter;
+import org.dspace.app.rest.model.WorkspaceItemRest;
+import org.dspace.app.rest.model.hateoas.WorkspaceItemResource;
+import org.dspace.content.WorkspaceItem;
+import org.dspace.content.service.WorkspaceItemService;
+import org.dspace.core.Context;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+/**
+ * This is the repository responsible to manage WorkspaceItem Rest object
+ * 
+ * @author Andrea Bollini (andrea.bollini at 4science.it)
+ *
+ */
+
+@Component(WorkspaceItemRest.CATEGORY + "." + WorkspaceItemRest.NAME)
+public class WorkspaceItemRestRepository extends DSpaceRestRepository<WorkspaceItemRest, Integer> {
+
+	@Autowired
+	WorkspaceItemService wis;
+	
+	@Autowired
+	WorkspaceItemConverter converter;
+	
+	
+	public WorkspaceItemRestRepository() {
+	}
+
+	@Override
+	public WorkspaceItemRest findOne(Context context, Integer id) {
+		WorkspaceItem witem = null;
+		try {
+			witem = wis.find(context, id);
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage(), e);
+		}
+		if (witem == null) {
+			return null;
+		}
+		return converter.fromModel(witem);
+	}
+
+	@Override
+	public Page<WorkspaceItemRest> findAll(Context context, Pageable pageable) {
+		List<WorkspaceItem> witems = null;
+		int total = 0;
+		try {
+			total = wis.countTotal(context);
+			witems = wis.findAll(context, pageable.getPageSize(), pageable.getOffset());
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage(), e);
+		}
+		Page<WorkspaceItemRest> page = new PageImpl<WorkspaceItem>(witems, pageable, total).map(converter);
+		return page;
+	}
+	
+	@Override
+	public Class<WorkspaceItemRest> getDomainClass() {
+		return WorkspaceItemRest.class;
+	}
+	
+	@Override
+	public WorkspaceItemResource wrapResource(WorkspaceItemRest witem, String... rels) {
+		return new WorkspaceItemResource(witem, utils, rels);
+	}
+
+}
