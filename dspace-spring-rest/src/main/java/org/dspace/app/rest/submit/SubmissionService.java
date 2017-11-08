f5f6d130161f8a57719fd73e2fcc63e7c224bc3d
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2017-11-08 17:57:32 +0100

    DS-3740 add license and java doc

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/submit/SubmissionService.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/submit/SubmissionService.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.app.rest.submit;
 
 import java.util.UUID;
@@ -14,6 +21,12 @@ import org.dspace.services.model.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * Service to manipulate in-progress submissions.
+ * 
+ * @author Luigi Andrea Pascarelli (luigiandrea.pascarelli at 4science.it)
+ *
+ */
 @Component
 public class SubmissionService {
 
