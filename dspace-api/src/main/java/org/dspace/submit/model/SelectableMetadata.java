81f3b90ff3444fd98c89606a80d9042d0f28b6ca
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2017-11-10 23:18:45 +0100

    D4CRIS-338 add license

--- dspace-api/src/main/java/org/dspace/submit/model/SelectableMetadata.java
+++ dspace-api/src/main/java/org/dspace/submit/model/SelectableMetadata.java
@@ -1,5 +1,16 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.submit.model;
 
+/**
+ * @author Luigi Andrea Pascarelli (luigiandrea.pascarelli at 4science.it)
+ *
+ */
 public class SelectableMetadata {
 	private String metadata;
 	private String label;
