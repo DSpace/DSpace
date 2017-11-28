e81194b21466d215a977954df3bb5d6cc754bbb0
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2017-11-28 10:57:10 +0100

    [DS-3762] Wrote tests for the collection endpoint. /api/core/collections/search is not implemented yet and thus not tested

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/builder/CollectionBuilder.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/builder/CollectionBuilder.java
@@ -7,11 +7,20 @@
  */
 package org.dspace.app.rest.builder;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.CharEncoding;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Bitstream;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
 import org.dspace.content.MetadataSchema;
 import org.dspace.content.service.DSpaceObjectService;
 import org.dspace.core.Context;
+import org.dspace.discovery.SearchServiceException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
 
 /**
  * Builder to construct Collection objects
@@ -39,12 +48,23 @@ public class CollectionBuilder extends AbstractBuilder<Collection> {
         } catch (Exception e) {
             return handleException(e);
         }
-
         return this;
     }
 
     public CollectionBuilder withName(final String name) {
         return setMetadataSingleValue(collection, MetadataSchema.DC_SCHEMA, "title", null, name);
+    }
+
+    public CollectionBuilder withLogo(final String content) throws AuthorizeException, IOException, SQLException {
+
+        InputStream is = IOUtils.toInputStream(content, CharEncoding.UTF_8);
+        try {
+            collectionService.setLogo(context, collection, is);
+            return this;
+
+        } finally {
+            is.close();
+        }
     }
 
     @Override
