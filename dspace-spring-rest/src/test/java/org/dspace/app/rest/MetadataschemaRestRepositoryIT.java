9a1ab3e9f6ea79325548b0222fa51343278ee2c3
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2017-12-01 16:57:08 +0100

    [DS-3762] added tests for metadataschemas and added license headers for previous test classes

---
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/MetadataschemaRestRepositoryIT.java
@@ -0,0 +1,33 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest;
+
+import org.dspace.app.rest.matcher.MetadataschemaMatcher;
+import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class MetadataschemaRestRepositoryIT extends AbstractControllerIntegrationTest {
+
+    @Test
+    public void findAll() throws Exception{
+
+        getClient().perform(get("/api/core/metadataschemas"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$._embedded.metadataschemas", Matchers.hasItem(
+                        MetadataschemaMatcher.matchEntry()
+                )))
+                .andExpect(jsonPath("$._links.self.href", Matchers.containsString("/api/core/metadataschemas")));
+    }
+}
