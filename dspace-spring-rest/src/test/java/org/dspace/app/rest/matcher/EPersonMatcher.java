97007c08560e7f9a3a5990173f3c7bc837fbd9c2
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2017-12-01 16:08:28 +0100

    [DS-3762] added extra clauses for the EPerson matcher

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/matcher/EPersonMatcher.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/matcher/EPersonMatcher.java
@@ -7,23 +7,28 @@
  */
 package org.dspace.app.rest.matcher;
 
+import org.dspace.eperson.EPerson;
 import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 
 import java.util.UUID;
 
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class EPersonMatcher {
 
-    public static Matcher<? super Object> matchEPersonEntry(UUID uuid, String name) {
+    public static Matcher<? super Object> matchEPersonEntry(EPerson ePerson) {
         return allOf(
-                hasJsonPath("$.uuid", is(uuid.toString())),
-                hasJsonPath("$.name", is(name)),
+                hasJsonPath("$.uuid", is(ePerson.getID().toString())),
+                hasJsonPath("$.name", is(ePerson.getName())),
                 hasJsonPath("$.type", is("eperson")),
-                hasJsonPath("$._links.self.href", containsString("/api/eperson/epersons/" + uuid.toString()))
+                hasJsonPath("$.canLogIn", not(empty())),
+                hasJsonPath("$._links.self.href", containsString("/api/eperson/epersons/" + ePerson.getID().toString())),
+                hasJsonPath("$.metadata", Matchers.containsInAnyOrder(
+                        EPersonMetadataMatcher.matchFirstName(ePerson.getFirstName()),
+                        EPersonMetadataMatcher.matchLastName(ePerson.getLastName())
+                ))
         );
     }
 
