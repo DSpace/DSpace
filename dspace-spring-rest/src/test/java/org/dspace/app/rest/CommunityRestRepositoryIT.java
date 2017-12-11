41846dfe8a2565a908f7a9fa4c8522b88482017d
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2017-12-11 14:41:33 +0100

    [DS-3762] implemented requested changes

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/CommunityRestRepositoryIT.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/CommunityRestRepositoryIT.java
@@ -231,6 +231,7 @@ public class CommunityRestRepositoryIT extends AbstractControllerIntegrationTest
 
     //TODO The test fails, 404 resource not found. remove @Ignore when this is implemented
     @Test
+    @Ignore
     public void findAllSubCommunities() throws Exception{
 
         //We turn off the authorization system in order to create the structure as defined below
@@ -280,7 +281,44 @@ public class CommunityRestRepositoryIT extends AbstractControllerIntegrationTest
         ;
     }
 
+    //TODO The test fails, 404 resource not found. remove @Ignore when this is implemented
+    @Test
+    @Ignore
+    public void findAllSubCommunitiesWithoutUUID() throws Exception{
 
+        //We turn off the authorization system in order to create the structure as defined below
+        context.turnOffAuthorisationSystem();
+
+        //** GIVEN **
+        //1. A community-collection structure with one parent community with sub-community and one collection.
+        parentCommunity = CommunityBuilder.createCommunity(context)
+                .withName("Parent Community")
+                .withLogo("ThisIsSomeDummyText")
+                .build();
+
+        Community parentCommunity2 = CommunityBuilder.createCommunity(context)
+                .withName("Parent Community 2")
+                .withLogo("SomeTest")
+                .build();
+
+        Community child1 = CommunityBuilder.createSubCommunity(context, parentCommunity)
+                .withName("Sub Community")
+                .build();
+
+        Community child12 = CommunityBuilder.createSubCommunity(context, child1)
+                .withName("Sub Sub Community")
+                .build();
+
+        Community child2 = CommunityBuilder.createSubCommunity(context, parentCommunity2)
+                .withName("Sub2 Community")
+                .build();
+
+        Collection col1 = CollectionBuilder.createCollection(context, child1).withName("Collection 1").build();
+
+        getClient().perform(get("/api/core/communities/search/subCommunities"))
+                .andExpect(status().isUnprocessableEntity())
+        ;
+    }
 
 
     @Test
