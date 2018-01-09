d00386c7c52eea73e897997386dedc38f8a11d9e
Andrea Bollini <andrea.bollini@4science.it>
2018-01-09 17:12:49 +0100

    make DateDisplayStrategy tollerant to wrong values

--- dspace-jspui/src/main/java/org/dspace/app/webui/util/DateDisplayStrategy.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/util/DateDisplayStrategy.java
@@ -12,12 +12,16 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.dspace.content.DCDate;
 import org.dspace.content.Metadatum;
 import org.dspace.core.I18nUtil;
 
 public class DateDisplayStrategy extends ASimpleDisplayStrategy
 {
+	/** log4j category */
+    private static Logger log = Logger.getLogger(DateDisplayStrategy.class);
+    
     public String getMetadataDisplay(HttpServletRequest hrq, int limit,
             boolean viewFull, String browseType, int colIdx, int itemid, String field,
             Metadatum[] metadataArray, boolean disableCrossLinks, boolean emph)
@@ -32,7 +36,13 @@ public class DateDisplayStrategy extends ASimpleDisplayStrategy
                 }
                 else {
                     DCDate dd = new DCDate(value);
-                    metadata = UIUtil.displayDate(dd, false, false, hrq);
+                    try {
+                    	metadata = UIUtil.displayDate(dd, false, false, hrq);
+                    }
+                    catch (RuntimeException rte) {
+						log.error("Malformed value for the DateDiplayStrategy " + rte.getMessage() + " -" + value);
+                    	metadata = value;
+                    }
                 }
             }         	
         }
