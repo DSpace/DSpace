8557f095555e5da93cd00de8ef277f0b86efcff8
Tom Desair <tom.desair@atmire.com>
2018-01-18 17:20:11 +0100

    DS-3489: Make sure the HalLinkService also visits all elements inside an EmbeddedPage

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/matcher/CollectionMatcher.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/matcher/CollectionMatcher.java
@@ -7,6 +7,7 @@
  */
 package org.dspace.app.rest.matcher;
 
+import org.dspace.content.Bitstream;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 
@@ -20,6 +21,10 @@ import static org.hamcrest.Matchers.is;
 public class CollectionMatcher {
 
     public static Matcher<? super Object> matchCollectionEntry(String name, UUID uuid, String handle) {
+        return matchCollectionEntry(name, uuid, handle, null);
+    }
+
+    public static Matcher<? super Object> matchCollectionEntry(String name, UUID uuid, String handle, Bitstream logo) {
         return allOf(
                 hasJsonPath("$.uuid", is(uuid.toString())),
                 hasJsonPath("$.name", is(name)),
@@ -29,21 +34,25 @@ public class CollectionMatcher {
                         CollectionMetadataMatcher.matchTitle(name)
                 )),
                 matchLinks(uuid),
-                matchLogo()
+                matchLogo(logo)
         );
     }
 
-    private static Matcher<? super Object> matchLogo() {
-        return allOf(
-                hasJsonPath("$._embedded.logo", Matchers.not(Matchers.empty()))
-        );
-    }
-
-    public static Matcher<? super Object> matchLinks(UUID uuid){
+    private static Matcher<? super Object> matchLinks(UUID uuid){
         return allOf(
                 hasJsonPath("$._links.logo.href", containsString("api/core/collections/" + uuid.toString() + "/logo")),
                 hasJsonPath("$._links.self.href", containsString("api/core/collections/" + uuid.toString()))
         );
+    }
+
+    private static Matcher<? super Object> matchLogo(Bitstream logo) {
+        return logo == null ?
+                allOf(
+                    hasJsonPath("$._embedded.logo", Matchers.not(Matchers.empty()))
+                ) :
+                allOf(
+                    hasJsonPath("$._embedded.logo", BitstreamMatcher.matchBitstreamEntry(logo.getID(), logo.getSize()))
+                );
     }
 
 }
