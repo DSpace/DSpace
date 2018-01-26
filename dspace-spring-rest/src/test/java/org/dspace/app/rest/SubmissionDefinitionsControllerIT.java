3a09cd5f462685febe526f150a5940367d064174
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2018-01-26 13:28:21 +0100

    DS-3755 add test to check links on embeddedpage when the resource is retrieved with findrel

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/SubmissionDefinitionsControllerIT.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/SubmissionDefinitionsControllerIT.java
@@ -6,6 +6,9 @@
  * http://www.dspace.org/license/
  */
 package org.dspace.app.rest;
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static org.dspace.app.rest.test.AbstractControllerIntegrationTest.REST_SERVER_URL;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
@@ -16,9 +19,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.dspace.app.rest.builder.CollectionBuilder;
 import org.dspace.app.rest.builder.CommunityBuilder;
+import org.dspace.app.rest.matcher.MetadataFieldMatcher;
 import org.dspace.app.rest.matcher.SubmissionDefinitionsMatcher;
 import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
 import org.dspace.content.Collection;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
@@ -92,15 +97,24 @@ public class SubmissionDefinitionsControllerIT extends AbstractControllerIntegra
                 .andExpect(status().isOk());
     }
     
-    @Test
-    public void findSections() throws Exception {
-        getClient().perform(get("/api/config/submissiondefinitions/traditional/sections"))
-                //The status has to be 200 OK
-                .andExpect(status().isOk())
-                //We expect the content type to be "application/hal+json;charset=UTF-8"
-                .andExpect(content().contentType(contentType))
-                //Match only that a section exists with a submission configuration behind
-                .andExpect(jsonPath("$._embedded.submissionsections", hasSize(5)
-           		));
-    }
+	@Test
+	public void findSections() throws Exception {
+		getClient().perform(get("/api/config/submissiondefinitions/traditional/sections"))
+				// The status has to be 200 OK
+				.andExpect(status().isOk())
+				// We expect the content type to be "application/hal+json;charset=UTF-8"
+				.andExpect(content().contentType(contentType))
+				// Match only that a section exists with a submission configuration behind
+				.andExpect(jsonPath("$._embedded.submissionsections",hasSize(5)))
+				.andExpect(jsonPath("$._embedded.submissionsections",
+								Matchers.hasItem(
+										allOf(
+								                hasJsonPath("$.id", is("traditionalpageone")),
+								                hasJsonPath("$.sectionType", is("submission-form")),
+								                hasJsonPath("$.type", is("submissionsection")),
+								                hasJsonPath("$._links.config.href", is(REST_SERVER_URL + "config/submissionforms/traditionalpageone")),
+								                hasJsonPath("$._links.self.href", is(REST_SERVER_URL + "config/submissionsections/traditionalpageone"))
+								        ))))
+				;
+	}
 }
