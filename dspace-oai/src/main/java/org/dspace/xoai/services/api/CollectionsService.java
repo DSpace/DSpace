739d598f14d4bae4646f9eccb2385b8ab8616780
Tim Donohue <tdonohue@duraspace.org>
2018-02-12 16:13:51 -0600

    Fix dspace-oai module per new code style

--- dspace-oai/src/main/java/org/dspace/xoai/services/api/CollectionsService.java
+++ dspace-oai/src/main/java/org/dspace/xoai/services/api/CollectionsService.java
@@ -7,19 +7,22 @@
  */
 package org.dspace.xoai.services.api;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
+
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
 import org.dspace.content.Item;
 import org.dspace.core.Context;
 import org.dspace.xoai.services.api.context.ContextService;
 
-import java.sql.SQLException;
-import java.util.List;
-import java.util.UUID;
-
 public interface CollectionsService {
     List<UUID> getAllSubCollections(ContextService contextService, UUID communityId) throws SQLException;
+
     List<Community> flatParentCommunities(Collection collection) throws SQLException;
+
     List<Community> flatParentCommunities(Community community) throws SQLException;
+
     List<Community> flatParentCommunities(Context context, Item item) throws SQLException;
 }
