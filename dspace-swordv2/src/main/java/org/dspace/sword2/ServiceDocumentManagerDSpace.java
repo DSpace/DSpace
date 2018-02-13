db8b2f6e391129961fe9a55effa551501b1933de
Tim Donohue <tdonohue@duraspace.org>
2018-02-13 10:36:24 -0600

    Fix dspace-swordv2 module per new code style

--- dspace-swordv2/src/main/java/org/dspace/sword2/ServiceDocumentManagerDSpace.java
+++ dspace-swordv2/src/main/java/org/dspace/sword2/ServiceDocumentManagerDSpace.java
@@ -7,6 +7,8 @@
  */
 package org.dspace.sword2;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
@@ -26,25 +28,22 @@ import org.swordapp.server.SwordError;
 import org.swordapp.server.SwordServerException;
 import org.swordapp.server.SwordWorkspace;
 
-import java.util.List;
-
-public class ServiceDocumentManagerDSpace implements ServiceDocumentManager
-{
-    /** logger */
+public class ServiceDocumentManagerDSpace implements ServiceDocumentManager {
+    /**
+     * logger
+     */
     private static Logger log = Logger
-            .getLogger(ServiceDocumentManagerDSpace.class);
+        .getLogger(ServiceDocumentManagerDSpace.class);
 
     protected CommunityService communityService = ContentServiceFactory
-            .getInstance().getCommunityService();
+        .getInstance().getCommunityService();
 
     public ServiceDocument getServiceDocument(String sdUri,
-            AuthCredentials authCredentials, SwordConfiguration config)
-            throws SwordError, SwordServerException, SwordAuthException
-    {
+                                              AuthCredentials authCredentials, SwordConfiguration config)
+        throws SwordError, SwordServerException, SwordAuthException {
         SwordContext sc = null;
 
-        try
-        {
+        try {
             // first authenticate the request
             // note: this will build our various DSpace contexts for us
             SwordAuthenticator auth = new SwordAuthenticator();
@@ -54,61 +53,53 @@ public class ServiceDocumentManagerDSpace implements ServiceDocumentManager
             // ensure that this method is allowed
             WorkflowManagerFactory.getInstance().retrieveServiceDoc(context);
 
-            if (log.isDebugEnabled())
-            {
+            if (log.isDebugEnabled()) {
                 log.debug(LogManager
-                        .getHeader(context, "sword_do_service_document", ""));
+                              .getHeader(context, "sword_do_service_document", ""));
             }
 
             // log the request
             String un = authCredentials.getUsername() != null ?
-                    authCredentials.getUsername() :
-                    "NONE";
+                authCredentials.getUsername() :
+                "NONE";
             String obo = authCredentials.getOnBehalfOf() != null ?
-                    authCredentials.getOnBehalfOf() :
-                    "NONE";
+                authCredentials.getOnBehalfOf() :
+                "NONE";
             log.info(LogManager
-                    .getHeader(context, "sword_service_document_request",
-                            "username=" + un + ",on_behalf_of=" + obo));
+                         .getHeader(context, "sword_service_document_request",
+                                    "username=" + un + ",on_behalf_of=" + obo));
 
             return this.getServiceDocument(sc, sdUri,
-                    (SwordConfigurationDSpace) config);
-        }
-        catch (DSpaceSwordException e)
-        {
+                                           (SwordConfigurationDSpace) config);
+        } catch (DSpaceSwordException e) {
             log.error("caught exception: ", e);
             throw new SwordServerException(
-                    "The DSpace SWORD interface experienced an error", e);
-        }
-        finally
-        {
+                "The DSpace SWORD interface experienced an error", e);
+        } finally {
             // this is a read operation only, so there's never any need to commit the context
-            if (sc != null)
-            {
+            if (sc != null) {
                 sc.abort();
             }
         }
     }
 
     public ServiceDocument getServiceDocument(SwordContext context, String url,
-            SwordConfigurationDSpace swordConfig)
-            throws SwordError, SwordServerException, DSpaceSwordException
-    {
+                                              SwordConfigurationDSpace swordConfig)
+        throws SwordError, SwordServerException, DSpaceSwordException {
         // first check that the sword context have
         // been set
-        if (context == null)
-        {
+        if (context == null) {
             throw new DSpaceSwordException(
-                    "The Sword Context is null; please set it before calling getServiceDocument");
+                "The Sword Context is null; please set it before calling getServiceDocument");
         }
 
         // ensure that this method is allowed
         WorkflowManagerFactory.getInstance()
-                .retrieveServiceDoc(context.getContext());
+                              .retrieveServiceDoc(context.getContext());
 
         // get the URL manager
         SwordUrlManager urlManager = swordConfig
-                .getUrlManager(context.getContext(), swordConfig);
+            .getUrlManager(context.getContext(), swordConfig);
 
         // we'll need the authenticator
         SwordAuthenticator swordAuth = new SwordAuthenticator();
@@ -123,8 +114,7 @@ public class ServiceDocumentManagerDSpace implements ServiceDocumentManager
         // set the max upload size
         service.setMaxUploadSize(swordConfig.getMaxUploadSize());
 
-        if (url == null || urlManager.isBaseServiceDocumentUrl(url))
-        {
+        if (url == null || urlManager.isBaseServiceDocumentUrl(url)) {
             // we are dealing with the default service document
 
             // set the title of the workspace as per the name of the DSpace installation
@@ -134,67 +124,56 @@ public class ServiceDocumentManagerDSpace implements ServiceDocumentManager
 
             // next thing to do is determine whether the default is communities or collections
             boolean swordCommunities = ConfigurationManager
-                    .getBooleanProperty("swordv2-server", "expose-communities");
+                .getBooleanProperty("swordv2-server", "expose-communities");
 
-            if (swordCommunities)
-            {
+            if (swordCommunities) {
                 List<Community> comms = swordAuth
-                        .getAllowedCommunities(context);
-                for (Community comm : comms)
-                {
+                    .getAllowedCommunities(context);
+                for (Community comm : comms) {
                     SwordCollection scol = comGen
-                            .buildCollection(context.getContext(), comm,
-                                    swordConfig);
+                        .buildCollection(context.getContext(), comm,
+                                         swordConfig);
                     workspace.addCollection(scol);
                 }
-            }
-            else
-            {
+            } else {
                 List<Collection> cols = swordAuth
-                        .getAllowedCollections(context);
-                for (Collection col : cols)
-                {
+                    .getAllowedCollections(context);
+                for (Collection col : cols) {
                     SwordCollection scol = colGen
-                            .buildCollection(context.getContext(), col,
-                                    swordConfig);
+                        .buildCollection(context.getContext(), col,
+                                         swordConfig);
                     workspace.addCollection(scol);
                 }
             }
 
             service.addWorkspace(workspace);
-        }
-        else
-        {
+        } else {
             // we are dealing with a partial or sub-service document
             DSpaceObject dso = urlManager.extractDSpaceObject(url);
-            if (dso == null)
-            {
+            if (dso == null) {
                 throw new SwordError(404);
             }
 
-            if (dso instanceof Community)
-            {
+            if (dso instanceof Community) {
                 Community community = (Community) dso;
                 SwordWorkspace workspace = new SwordWorkspace();
                 workspace.setTitle(communityService.getName(community));
 
                 List<Collection> collections = swordAuth
-                        .getAllowedCollections(context, community);
-                for (Collection collection : collections)
-                {
+                    .getAllowedCollections(context, community);
+                for (Collection collection : collections) {
                     SwordCollection scol = colGen
-                            .buildCollection(context.getContext(), collection,
-                                    swordConfig);
+                        .buildCollection(context.getContext(), collection,
+                                         swordConfig);
                     workspace.addCollection(scol);
                 }
 
                 List<Community> communities = swordAuth
-                        .getCommunities(context, community);
-                for (Community comm : communities)
-                {
+                    .getCommunities(context, community);
+                for (Community comm : communities) {
                     SwordCollection scol = comGen
-                            .buildCollection(context.getContext(), comm,
-                                    swordConfig);
+                        .buildCollection(context.getContext(), comm,
+                                         swordConfig);
                     workspace.addCollection(scol);
                 }
 
