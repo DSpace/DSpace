ecce5fe9b28feead1e47868df7409f7ed0bd90b6
Tom Desair <tom.desair@atmire.com>
2018-02-28 17:10:47 +0100

    DS-3819: Return HTTP redirect when login URL is supported

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/security/StatelessLoginFilter.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/security/StatelessLoginFilter.java
@@ -10,7 +10,6 @@ package org.dspace.app.rest.security;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -19,11 +18,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.dspace.app.rest.utils.ContextUtil;
 import org.dspace.authenticate.AuthenticationMethod;
-import org.dspace.authenticate.factory.AuthenticateServiceFactory;
 import org.dspace.authenticate.service.AuthenticationService;
 import org.dspace.core.Context;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -59,25 +56,9 @@ public class StatelessLoginFilter extends AbstractAuthenticationProcessingFilter
         String user = req.getParameter("user");
         String password = req.getParameter("password");
 
-        try {
-            return authenticationManager.authenticate(
+        return authenticationManager.authenticate(
                 new DSpaceAuthentication(user, password, new ArrayList<>())
-            );
-        } catch (BadCredentialsException e) {
-            AuthenticationService authenticationService =
-                AuthenticateServiceFactory.getInstance().getAuthenticationService();
-            Iterator<AuthenticationMethod> authenticationMethodIterator =
-                authenticationService.authenticationMethodIterator();
-            while (authenticationMethodIterator.hasNext()) {
-                AuthenticationMethod authenticationMethod = authenticationMethodIterator.next();
-                Context context = ContextUtil.obtainContext(req);
-                String loginPageURL = authenticationMethod.loginPageURL(context, req, res);
-                if (StringUtils.isNotBlank(loginPageURL)) {
-                    res.addHeader("Location", loginPageURL);
-                }
-            }
-            throw e;
-        }
+        );
     }
 
 
@@ -89,5 +70,34 @@ public class StatelessLoginFilter extends AbstractAuthenticationProcessingFilter
 
         DSpaceAuthentication dSpaceAuthentication = (DSpaceAuthentication) auth;
         restAuthenticationService.addAuthenticationDataForUser(req, res, dSpaceAuthentication);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request,
+                                              HttpServletResponse response, AuthenticationException failed)
+            throws IOException, ServletException {
+
+        AuthenticationService authenticationService = restAuthenticationService.getAuthenticationService();
+
+        Iterator<AuthenticationMethod> authenticationMethodIterator
+                = authenticationService.authenticationMethodIterator();
+        Context context = ContextUtil.obtainContext(request);
+        String redirectUrl = null;
+
+        while (authenticationMethodIterator.hasNext()) {
+            AuthenticationMethod authenticationMethod = authenticationMethodIterator.next();
+
+            String loginPageURL = authenticationMethod.loginPageURL(context, request, response);
+            if (StringUtils.isNotBlank(loginPageURL)) {
+                redirectUrl = loginPageURL;
+            }
+        }
+
+        if (redirectUrl == null) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, failed.getMessage());
+        } else {
+            response.sendRedirect(redirectUrl);
+        }
+
     }
 }
