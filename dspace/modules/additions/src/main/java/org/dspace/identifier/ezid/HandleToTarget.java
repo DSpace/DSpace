84e00c594dce608964b93edf62a04c1a1afe81f5
Mohamed Abdul Rasheed <mohideen@umd.edu>
2018-03-30 16:29:24 -0400

    LIBDRUM-511. Refactored HandleToTarget for dspace-6 changes.
    https://issues.umd.edu/browse/LIBDRUM-511

--- dspace/modules/additions/src/main/java/org/dspace/identifier/ezid/HandleToTarget.java
+++ dspace/modules/additions/src/main/java/org/dspace/identifier/ezid/HandleToTarget.java
@@ -1,16 +1,19 @@
 package org.dspace.identifier.ezid;
 
-import org.dspace.core.ConfigurationManager;
+import org.dspace.services.ConfigurationService;
+import org.dspace.services.factory.DSpaceServicesFactory;
 
 public class HandleToTarget implements Transform
 {
 
+    private static final ConfigurationService configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
+
     @Override
     public String transform(String identifierURI) throws Exception
     {
-        String cPrefix = ConfigurationManager
+        String cPrefix = configurationService
                 .getProperty("handle.canonical.prefix");
-        String hPrefix = ConfigurationManager.getProperty("handle.prefix");
+        String hPrefix = configurationService.getProperty("handle.prefix");
         String prefix = cPrefix + hPrefix;
         if (identifierURI.startsWith(prefix))
         {
