e2d7c7ba924f7a4d5e5198d09b0f98104c2c2a65
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2018-04-06 18:04:47 +0200

    avoid NPE if the self-claim-rp group is not present

--- dspace-jspui/src/main/java/org/dspace/app/webui/servlet/MyDSpaceServlet.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/servlet/MyDSpaceServlet.java
@@ -860,7 +860,7 @@ public class MyDSpaceServlet extends DSpaceServlet
         {
             Group selfClaimGroup = Group.findByName(context,
                     nameGroupSelfClaim);
-            if (Group.isMember(context, selfClaimGroup.getID()))
+            if (selfClaimGroup != null && Group.isMember(context, selfClaimGroup.getID()))
             {
                 selfClaim = true;
             }
