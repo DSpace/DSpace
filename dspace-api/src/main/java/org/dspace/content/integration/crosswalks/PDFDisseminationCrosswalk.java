425f8254399c552128edc5cd0804bef6acea5c33
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2018-05-08 15:53:17 +0200

    [D4CRIS-521] Improve PDF generation

--- dspace-api/src/main/java/org/dspace/content/integration/crosswalks/PDFDisseminationCrosswalk.java
+++ dspace-api/src/main/java/org/dspace/content/integration/crosswalks/PDFDisseminationCrosswalk.java
@@ -11,7 +11,9 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.StringTokenizer;
 
@@ -30,7 +32,6 @@ import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
 import org.dspace.content.Metadatum;
 import org.dspace.content.crosswalk.CrosswalkException;
-import org.dspace.content.crosswalk.StreamDisseminationCrosswalk;
 import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Constants;
 import org.dspace.core.Context;
@@ -59,11 +60,7 @@ public class PDFDisseminationCrosswalk extends SelfNamedPlugin
 		PDPage page = new PDPage();
 		document.addPage(page);
 
-		// Create a new font object selecting one of the PDF base fonts
-		PDFont fontBold = PDType1Font.HELVETICA_BOLD;
-		PDFont font = PDType1Font.HELVETICA;
-
-		createPage(context, dso, document, page, fontBold, font);
+		createPage(context, dso, document, page, new HashMap<String, Integer>());
 
 		// Save the results and ensure that the document is properly closed:
 		try
@@ -78,22 +75,29 @@ public class PDFDisseminationCrosswalk extends SelfNamedPlugin
 		document.close();
 	}
 
-	private void createPage(Context context, DSpaceObject dso, PDDocument document, PDPage page, PDFont fontBold,
-			PDFont font) throws IOException, SQLException {
+	private void createPage(Context context, DSpaceObject dso, PDDocument document, PDPage page,
+			Map<String, Integer> alreadyWrote) throws IOException, SQLException {
+
+		// Create a new font object selecting one of the PDF base fonts
+		PDFont fontBold = PDType1Font.HELVETICA_BOLD;
+		PDFont font = PDType1Font.HELVETICA;
+
 		// Start a new content stream which will "hold" the to be created content
 		PDPageContentStream contentStream = new PDPageContentStream(document, page);
 
 		PDRectangle mediabox = page.findMediaBox();
 
-		float yCordinate = mediabox.getUpperRightY() - 30;
-		float startX = mediabox.getLowerLeftX() + 30;
-		float endX = mediabox.getUpperRightX() - 30;
+		float margin = 30;
+		float yCordinate = mediabox.getUpperRightY() - margin;
+		float startX = mediabox.getLowerLeftX() + margin;
+		float endX = mediabox.getUpperRightX() - margin;
 
-		float fontSize = 12;
+		float fontSize = 10;
 		float fontHeight = fontSize;
 		float leading = 1.5f * fontSize;
 
-		float width = mediabox.getWidth() - 2 * 30;
+		float width = mediabox.getWidth() - 2 * margin;
+		float height = mediabox.getHeight() - 2 * margin;
 
 		String configLine = ConfigurationManager.getProperty("webui.itemexport.pdf");
 		if (StringUtils.isBlank(configLine)) {
@@ -102,10 +106,14 @@ public class PDFDisseminationCrosswalk extends SelfNamedPlugin
 
 		StringTokenizer st = new StringTokenizer(configLine, ",");
 
-		while (st.hasMoreTokens()) {
+		primary: while (st.hasMoreTokens()) {
 			String field = st.nextToken().trim();
-			// Get the separate schema + element + qualifier
 
+			if (!alreadyWrote.containsKey(field)) {
+				alreadyWrote.put(field, 0);
+			}
+
+			// Get the separate schema + element + qualifier
 			String[] eq = field.split("\\.");
 			String schema = eq[0];
 			String element = eq[1];
@@ -136,21 +144,13 @@ public class PDFDisseminationCrosswalk extends SelfNamedPlugin
 					label = I18nUtil.getMessage("metadata." + field, context);
 				}
 
-				// Define a text content stream using the selected font, moving the cursor and
-				// drawing the text "Hello World"
-				contentStream.beginText();
-				contentStream.setFont(fontBold, fontSize);
-				contentStream.moveTextPositionByAmount(startX, yCordinate);
-				contentStream.drawString(label);
-				contentStream.endText();
-				
 				int size = (int) (fontSize * font.getStringWidth(label + PLACEHOLDER_SPACING) / 1000);
 
-				int i = 0;
+				List<String> lines = new ArrayList<String>();
+
 				for (Metadatum mm : values) {
 					String text = mm.value;
 
-					List<String> lines = new ArrayList<String>();
 					int lastSpace = -1;
 					while (text.length() > 0) {
 						int spaceIndex = text.indexOf(' ', lastSpace + 1);
@@ -172,23 +172,50 @@ public class PDFDisseminationCrosswalk extends SelfNamedPlugin
 							lastSpace = spaceIndex;
 						}
 					}
+				}
 
-					for (String line : lines) {
+				internal: for (int index = alreadyWrote.get(field); index < lines.size(); index++) {
+					if (height > 0 && index == 0) {
+						// Define a text content stream using the selected font, moving the cursor and
+						// drawing the text "Hello World"
 						contentStream.beginText();
-						contentStream.setFont(font, fontSize);
-						contentStream.moveTextPositionByAmount(startX + size, yCordinate);
-						contentStream.drawString(line);
-						yCordinate -= leading;
+						contentStream.setFont(fontBold, fontSize);
+						contentStream.moveTextPositionByAmount(startX, yCordinate);
+						contentStream.drawString(label);
 						contentStream.endText();
-						i++;
+
+					}
+
+					contentStream.beginText();
+					contentStream.setFont(font, fontSize);
+					contentStream.moveTextPositionByAmount(startX + size, yCordinate);
+					if (height <= 0) {
+						PDPage pdPage = new PDPage();
+						document.addPage(pdPage);
+						createPage(context, dso, document, pdPage, alreadyWrote);
+						break primary;
+					}
+					contentStream.drawString(lines.get(index));
+
+					yCordinate -= leading;
+					height -= leading;
+
+					contentStream.endText();
+					alreadyWrote.put(field, index+1);
+					
+					if (height > 0 && index==lines.size()-1) {
+						yCordinate -= fontHeight;
+						height -= fontHeight;
+
+						contentStream.moveTo(startX, yCordinate);
+						contentStream.lineTo(endX, yCordinate);
+						contentStream.stroke();
+						
+						yCordinate -= leading;
+						height -= leading;
 					}
 				}
-				yCordinate -= fontHeight;
 
-				contentStream.moveTo(startX, yCordinate);
-				contentStream.lineTo(endX, yCordinate);
-				contentStream.stroke();
-				yCordinate -= leading;				
 			}
 
 		}
@@ -206,29 +233,23 @@ public class PDFDisseminationCrosswalk extends SelfNamedPlugin
 		String result = ConfigurationManager.getProperty(FILE_NAME_EXPORT_PDF);
 		if (StringUtils.isNotEmpty(result))
 			return result;
-		return "export.pdf";
+		return "export";
 	}
 
 	@Override
 	public void disseminate(Context context, List<DSpaceObject> dso, OutputStream out)
 			throws CrosswalkException, IOException, SQLException, AuthorizeException {
 		PDDocument document = new PDDocument();
-		for(DSpaceObject dd : dso) {
-			
+		for (DSpaceObject dd : dso) {
+
 			PDPage page = new PDPage();
 			document.addPage(page);
 
-			// Create a new font object selecting one of the PDF base fonts
-			PDFont fontBold = PDType1Font.HELVETICA_BOLD;
-			PDFont font = PDType1Font.HELVETICA;
-			
-			createPage(context, dd, document, page, fontBold, font);
+			createPage(context, dd, document, page, new HashMap<String, Integer>());
 		}
 
 		// Save the results and ensure that the document is properly closed:
-		try
-
-		{
+		try {
 			// Finally Let's save the PDF
 			document.save(out);
 		} catch (COSVisitorException e) {
