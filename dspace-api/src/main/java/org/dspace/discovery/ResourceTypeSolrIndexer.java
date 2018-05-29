7caecc41cd06ebd31a751299880ac7ace365f5a1
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2018-05-29 14:57:27 +0200

    Finalizing enhancement inputform; add missing class; minor fixes; add license;

--- dspace-api/src/main/java/org/dspace/discovery/ResourceTypeSolrIndexer.java
+++ dspace-api/src/main/java/org/dspace/discovery/ResourceTypeSolrIndexer.java
@@ -3,7 +3,7 @@
  * detailed in the LICENSE and NOTICE files at the root of the source
  * tree and available online at
  *
- * https://github.com/CILEA/dspace-cris/wiki/License
+ * http://www.dspace.org/license/
  */
 package org.dspace.discovery;
 
