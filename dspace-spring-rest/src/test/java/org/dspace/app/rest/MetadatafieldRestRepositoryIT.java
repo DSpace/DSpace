7f68244c4e039426ee138013aeb650c46f4a254f
Andrea Bollini <andrea.bollini@4science.it>
2018-06-09 16:33:21 +0200

    Add test for search methods

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/MetadatafieldRestRepositoryIT.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/MetadatafieldRestRepositoryIT.java
@@ -14,11 +14,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.dspace.app.rest.builder.MetadataFieldBuilder;
+import org.dspace.app.rest.builder.MetadataSchemaBuilder;
 import org.dspace.app.rest.matcher.MetadataFieldMatcher;
 import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
 import org.dspace.content.MetadataField;
+import org.dspace.content.MetadataSchema;
 import org.hamcrest.Matchers;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class MetadatafieldRestRepositoryIT extends AbstractControllerIntegrationTest {
@@ -31,22 +32,23 @@ public class MetadatafieldRestRepositoryIT extends AbstractControllerIntegration
         MetadataField metadataField = MetadataFieldBuilder
             .createMetadataField(context, "AnElement", "AQualifier", "AScopeNote").build();
 
-        getClient().perform(get("/api/core/metadatafields"))
+        getClient().perform(get("/api/core/metadatafields")
+                        .param("size", String.valueOf(100)))
                    .andExpect(status().isOk())
                    .andExpect(content().contentType(contentType))
-                   .andExpect(jsonPath("$._embedded.metadatafields", Matchers.hasItem(
-                       MetadataFieldMatcher.matchMetadataField()
-                   )))
+                   .andExpect(jsonPath("$._embedded.metadatafields", Matchers.hasItems(
+                           MetadataFieldMatcher.matchMetadataFieldByKeys("dc","title", null),
+                           MetadataFieldMatcher.matchMetadataFieldByKeys("dc","date", "issued"))
+                   ))
                    .andExpect(jsonPath("$._links.first.href", Matchers.containsString("/api/core/metadatafields")))
                    .andExpect(jsonPath("$._links.self.href", Matchers.containsString("/api/core/metadatafields")))
                    .andExpect(jsonPath("$._links.next.href", Matchers.containsString("/api/core/metadatafields")))
                    .andExpect(jsonPath("$._links.last.href", Matchers.containsString("/api/core/metadatafields")))
 
-                   .andExpect(jsonPath("$.page.size", is(20)));
+                   .andExpect(jsonPath("$.page.size", is(100)));
     }
 
     @Test
-    @Ignore
     public void findOne() throws Exception {
 
         context.turnOffAuthorisationSystem();
@@ -56,10 +58,60 @@ public class MetadatafieldRestRepositoryIT extends AbstractControllerIntegration
         getClient().perform(get("/api/core/metadatafields/" + metadataField.getID()))
                    .andExpect(status().isOk())
                    .andExpect(content().contentType(contentType))
-                   .andExpect(jsonPath("$._embedded.metadatafields", Matchers.hasItem(
+                   .andExpect(jsonPath("$", Matchers.is(
                        MetadataFieldMatcher.matchMetadataField(metadataField)
-                   )))
+                   )));
+    }
 
+    @Test
+    public void searchMethodsExist() throws Exception {
+        getClient().perform(get("/api/core/metadatafields"))
+                   .andExpect(jsonPath("$._links.search.href", Matchers.notNullValue()));
+
+        getClient().perform(get("/api/core/metadatafields/search"))
+                   .andExpect(status().isOk())
+                   .andExpect(content().contentType(contentType))
+                   .andExpect(jsonPath("$._links.bySchema", Matchers.notNullValue()));
+    }
+
+    @Test
+    public void findBySchema() throws Exception {
+
+        context.turnOffAuthorisationSystem();
+        MetadataSchema schema = MetadataSchemaBuilder.createMetadataSchema(context, "ASchema",
+                "http://www.dspace.org/ns/aschema").build();
+
+        MetadataField metadataField = MetadataFieldBuilder
+            .createMetadataField(context, schema, "AnElement", "AQualifier", "AScopeNote").build();
+
+        getClient().perform(get("/api/core/metadatafields/search/bySchema")
+                        .param("schema", "dc"))
+                   .andExpect(status().isOk())
+                   .andExpect(content().contentType(contentType))
+                   .andExpect(jsonPath("$._embedded.metadatafields", Matchers.hasItems(
+                       MetadataFieldMatcher.matchMetadataFieldByKeys("dc","title", null),
+                       MetadataFieldMatcher.matchMetadataFieldByKeys("dc","date", "issued"))
+                   ))
                    .andExpect(jsonPath("$.page.size", is(20)));
+
+        getClient().perform(get("/api/core/metadatafields/search/bySchema")
+                .param("schema", schema.getName()))
+           .andExpect(status().isOk())
+           .andExpect(content().contentType(contentType))
+           .andExpect(jsonPath("$._embedded.metadatafields", Matchers.hasItem(metadataField)
+           ))
+           .andExpect(jsonPath("$.page.size", is(20)))
+           .andExpect(jsonPath("$.page.totalElements", is(1)));
+    }
+
+    @Test
+    public void findByUndefinedSchema() throws Exception {
+
+        getClient().perform(get("/api/core/metadatafields/search/bySchema")
+                        .param("schema", "undefined"))
+                   .andExpect(status().isOk())
+                   .andExpect(content().contentType(contentType))
+                   .andExpect(jsonPath("$.page.size", is(20)))
+                   .andExpect(jsonPath("$.page.totalElements", is(0)));
     }
 }
