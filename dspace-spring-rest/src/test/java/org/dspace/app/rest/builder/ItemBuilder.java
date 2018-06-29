fe2030d024473d1937981334b51ab37d660db14a
Pablo Prieto <pablo.prieto@ine.mx>
2018-06-29 16:51:11 -0500

    Working Implementation Wrote additional integration tests Created WorkspaceItemBuilder

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/builder/ItemBuilder.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/builder/ItemBuilder.java
@@ -36,12 +36,12 @@ public class ItemBuilder extends AbstractDSpaceObjectBuilder<Item> {
         ItemBuilder builder = new ItemBuilder(context);
         return builder.create(context, col);
     }
-     
+
     public static Item createTemplateItem(final Context context, final Collection collection) {
         ItemBuilder builder = new ItemBuilder(context);
         return builder.create_template_item(collection);
     }
-    
+
     private Item create_template_item(final Collection collection) {
         try {
             item = itemService.createTemplateItem(context, collection);
@@ -50,7 +50,7 @@ public class ItemBuilder extends AbstractDSpaceObjectBuilder<Item> {
             return handleException(e);
         }
     }
-    
+
     private ItemBuilder create(final Context context, final Collection col) {
         this.context = context;
 
@@ -84,7 +84,7 @@ public class ItemBuilder extends AbstractDSpaceObjectBuilder<Item> {
         item.setDiscoverable(false);
         return this;
     }
-    
+
     public ItemBuilder withEmbargoPeriod(String embargoPeriod) {
         return setEmbargo(embargoPeriod, item);
     }
@@ -113,7 +113,7 @@ public class ItemBuilder extends AbstractDSpaceObjectBuilder<Item> {
             return handleException(e);
         }
     }
-      
+
     protected void cleanup() throws Exception {
         delete(item);
     }
