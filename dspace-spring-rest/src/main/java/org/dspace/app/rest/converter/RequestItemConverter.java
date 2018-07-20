f0c1243c83ca6f4c29b1c57d2038a1fc340ac614
Mark H. Wood <mwood@IUPUI.Edu>
2018-07-20 14:13:11 -0400

    [DS-3952] First attempt.  It compiles.

---
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/converter/RequestItemConverter.java
@@ -0,0 +1,88 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+
+package org.dspace.app.rest.converter;
+
+import java.sql.SQLException;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+
+import org.dspace.app.requestitem.RequestItem;
+import org.dspace.app.requestitem.service.RequestItemService;
+import org.dspace.app.rest.model.RequestItemRest;
+import org.dspace.app.rest.utils.ContextUtil;
+import org.dspace.core.Context;
+import org.dspace.services.RequestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Convert between {@link org.dspace.app.requestitem.RequestItem} and
+ * {@link org.dspace.app.rest.model.RequestItemRest}.
+ *
+ * @author Mark H. Wood <mwood@iupui.edu>
+ */
+@Named
+public class RequestItemConverter
+        extends DSpaceConverter<RequestItem, RequestItemRest> {
+    private static final Logger LOG = LoggerFactory.getLogger(RequestItemConverter.class);
+
+    @Inject
+    protected BitstreamConverter bitstreamConverter;
+
+    @Inject
+    protected ItemConverter itemConverter;
+
+    @Inject
+    protected RequestItemService requestItemService;
+
+    @Inject
+    protected RequestService requestService;
+
+    @Override
+    public RequestItemRest fromModel(RequestItem requestItem) {
+        RequestItemRest requestItemRest = new RequestItemRest();
+        requestItemRest.setAccept_request(requestItem.isAccept_request());
+        requestItemRest.setAllfiles(requestItem.isAllfiles());
+        requestItemRest.setBitstream(bitstreamConverter.fromModel(requestItem.getBitstream()));
+        requestItemRest.setDecision_date(requestItem.getDecision_date());
+        requestItemRest.setExpires(requestItem.getExpires());
+        requestItemRest.setId(requestItem.getID());
+        requestItemRest.setItem(itemConverter.fromModel(requestItem.getItem()));
+        requestItemRest.setReq_email(requestItem.getReqEmail());
+        requestItemRest.setReq_message(requestItem.getReqMessage());
+        requestItemRest.setReq_name(requestItem.getReqName());
+        requestItemRest.setRequest_date(requestItem.getRequest_date());
+        requestItemRest.setToken(requestItem.getToken());
+        return requestItemRest;
+    }
+
+    @Override
+    public RequestItem toModel(RequestItemRest obj) {
+        HttpServletRequest request = requestService.getCurrentRequest().getHttpServletRequest();
+        Context context = ContextUtil.obtainContext(request);
+        String token = null;
+        try {
+            token = requestItemService.createRequest(context,
+                    obj.getBitstream(),
+                    obj.getItem(),
+                    obj.isAllfiles(),
+                    obj.getReq_email(),
+                    obj.getReq_name(),
+                    obj.getReq_message());
+        } catch (SQLException ex) {
+            LOG.error(ex.getMessage(), ex);
+        }
+        RequestItem requestItem = requestItemService.findByToken(context, token);
+        requestItem.setAccept_request(obj.isAccept_request());
+        requestItem.setDecision_date(obj.getDecision_date());
+        return requestItem;
+    }
+
+}
