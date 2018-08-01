7a59a46dd26dd384291d2367b7dd5c04f5f83291
pablo <pablo.cda@gmail.com>
2018-08-01 13:22:10 -0300

    diferencio tesauros por su rdf:type

--- dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/authority/TesauroAuthority.java
+++ dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/authority/TesauroAuthority.java
@@ -29,14 +29,39 @@ public class TesauroAuthority extends SimpleSPARQLAuthorityProvider {
 			String field, String text, String locale) {
 		ParameterizedSparqlString pqs = new ParameterizedSparqlString();
 
+		String rdfType = "";
+		switch (field) {
+		case "thesis_degree_name":
+			rdfType = "sedici:Grado";
+			break;
+		case "sedici_subject_descriptores":
+			rdfType = "sedici:Descriptor";
+			break;
+		case "sedici_subject_materias":
+			rdfType = "sedici:Materia";
+			break;
+		case "sedici_subject_eurovoc":
+			rdfType = "sedici:Eurovoc";
+			break;
+		case "sedici_subject_decs":
+			rdfType = "sedici:Decs";
+			break;
+		case "dc_coverage_spatial":
+			rdfType = "sedici:Spatial";
+			break;
+		default:
+			break;
+		}
+		
 		pqs.setNsPrefix("skos", NS_SKOS);
+		pqs.setNsPrefix("sedici", NS_SEDICI);
 
-		pqs.setCommandText("SELECT ?term ?label \n");
+		pqs.setCommandText("SELECT ?term ?label ?parent ?parentLabel \n");
 		pqs.append("WHERE {\n");
-		pqs.append("?term a skos:Concept; skos:prefLabel ?label .\n");
+		pqs.append("?term a "+ rdfType +"; skos:prefLabel ?label .\n");
+		pqs.append("OPTIONAL { ?term skos:broader ?parent . ?parent skos:prefLabel ?parentLabel } \n");
 		if (!"".equals(text)) {
-			pqs.append("FILTER(REGEX(?label, ?text, \"i\") || REGEX(?id, ?text, \"i\"))\n");
-			pqs.setLiteral("text", text);
+			pqs.append("FILTER(REGEX(?label, '"+text+"', 'i'))\n");
 		}
 		pqs.append("}\n");
 		pqs.append("ORDER BY ASC(?label)\n");
@@ -47,8 +72,13 @@ public class TesauroAuthority extends SimpleSPARQLAuthorityProvider {
 	@Override
 	protected Choice extractChoice(QuerySolution solution) {
 		String key = solution.getResource("term").getURI();
-		String id = solution.getLiteral("label").getString();
-		return new Choice(key, id, id + " (" + id + ")");
+		String value = solution.getLiteral("label").getString();
+		String label = "";
+		if (solution.contains("parentLabel")){
+			label = solution.getLiteral("parentLabel").getString() + " - ";			
+		}
+		label += solution.getLiteral("label").getString();
+		return new Choice(key, value, label);
 	}
 
 }
