c8bbabbbc374eeb7c198c6921fbe506c58e45cd8
Andrea Bollini <andrea.bollini@4science.it>
2018-08-17 12:20:34 +0200

    DS-3905 Implement a generic uuid lookup endpoint
    Refactor common code from the IdentifierRestController in a generic DSpaceObject converter
    Added ITs
    Add handling of standard Spring Bind exception to conform with REST return code (422) usage

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/UUIDLookupRestController.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/UUIDLookupRestController.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,10 +25,10 @@ import org.dspace.app.rest.model.DSpaceObjectRest;
 import org.dspace.app.rest.utils.ContextUtil;
 import org.dspace.content.DSpaceObject;
 import org.dspace.core.Context;
-import org.dspace.identifier.IdentifierNotFoundException;
-import org.dspace.identifier.IdentifierNotResolvableException;
-import org.dspace.identifier.factory.IdentifierServiceFactory;
-import org.dspace.identifier.service.IdentifierService;
+import org.dspace.discovery.DiscoverQuery;
+import org.dspace.discovery.DiscoverResult;
+import org.dspace.discovery.SearchService;
+import org.dspace.discovery.SearchServiceException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
@@ -40,23 +41,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This is an utility endpoint to lookup a generic DSpaceObject using the uuid in SOLR. If found the controller will
+ * respond with a redirection to the canonical endpoint for the actual object. Please note that currently it is limited
+ * to Community, Collection and Item as the other DSpaceObject are not yet indexed in SOLR
+ *
+ * @author Andrea Bollini (andrea.bollini at 4science.it)
+ */
 @RestController
-@RequestMapping("/api/" + IdentifierRestController.CATEGORY)
-public class IdentifierRestController implements InitializingBean {
-    public static final String CATEGORY = "pid";
+@RequestMapping("/api/" + UUIDLookupRestController.CATEGORY)
+public class UUIDLookupRestController implements InitializingBean {
+    public static final String CATEGORY = "dso";
 
     public static final String ACTION = "find";
 
-    public static final String PARAM = "id";
+    public static final String PARAM = "uuid";
 
     private static final Logger log =
-            Logger.getLogger(IdentifierRestController.class);
-
-    @Autowired
-    private GenericDSpaceObjectConverter converter;
+            Logger.getLogger(UUIDLookupRestController.class);
 
     @Autowired
     private DiscoverableEndpointsService discoverableEndpointsService;
+
+    @Autowired
+    private SearchService searchService;
+
+    @Autowired
+    private GenericDSpaceObjectConverter converter;
 
     @Override
     public void afterPropertiesSet() throws Exception {
@@ -74,32 +85,32 @@ public class IdentifierRestController implements InitializingBean {
     @SuppressWarnings("unchecked")
     public void getDSObyIdentifier(HttpServletRequest request,
                                    HttpServletResponse response,
-                                   @RequestParam(PARAM) String id)
-            throws IOException, SQLException {
+                                   @RequestParam(PARAM) UUID id)
+            throws IOException, SQLException, SearchServiceException {
 
-        DSpaceObject dso = null;
-        Context context = ContextUtil.obtainContext(request);
-        IdentifierService identifierService = IdentifierServiceFactory
-                .getInstance().getIdentifierService();
+        Context context = null;
         try {
-            dso = identifierService.resolve(context, id);
-            if (dso != null) {
-                DSpaceObjectRest dsor = converter.convert(dso);
-                URI link = linkTo(dsor.getController(), dsor.getCategory(),
-                        English.plural(dsor.getType()))
-                        .slash(dsor.getId()).toUri();
-                response.setStatus(HttpServletResponse.SC_FOUND);
-                response.sendRedirect(link.toString());
+            DSpaceObject dso = null;
+            context = ContextUtil.obtainContext(request);
+            DiscoverQuery query = new DiscoverQuery();
+            query.setQuery("search.resourceid:\"" + id.toString() + "\"");
+            DiscoverResult result = searchService.search(context, query);
+            if (result.getTotalSearchResults() == 1) {
+                dso = result.getDspaceObjects().get(0);
+                if (dso != null) {
+                    DSpaceObjectRest dsor = converter.convert(dso);
+                    URI link = linkTo(dsor.getController(), dsor.getCategory(), English.plural(dsor.getType()))
+                            .slash(dsor.getId()).toUri();
+                    response.setStatus(HttpServletResponse.SC_FOUND);
+                    response.sendRedirect(link.toString());
+                } else {
+                    response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                }
             } else {
                 response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             }
-        } catch (IdentifierNotFoundException e) {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        } catch (IdentifierNotResolvableException e) {
-            response.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
         } finally {
             context.abort();
         }
     }
-
 }
