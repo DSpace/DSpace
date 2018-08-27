d6d0e67017cd6dd5ce26eb88ea94c8f52a1422db
Mark H. Wood <mwood@IUPUI.Edu>
2018-08-27 10:30:06 -0400

    [DS-3989] Placate Checkstyle:  unused import.

--- dspace-api/src/main/java/org/dspace/curate/Curator.java
+++ dspace-api/src/main/java/org/dspace/curate/Curator.java
@@ -14,7 +14,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
