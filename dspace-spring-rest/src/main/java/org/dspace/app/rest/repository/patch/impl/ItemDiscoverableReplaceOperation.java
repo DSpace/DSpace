866c2de146e8b8d4a75f1f69ff616b7f52c1a54a
Michael W Spalti <mspalti@gmail.com>
2018-09-27 13:01:23 -0700

    Updated respository patch classes and ItemRestRepository.

---
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/repository/patch/impl/ItemDiscoverableReplaceOperation.java
@@ -0,0 +1,87 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.repository.patch.impl;
+
+import java.sql.SQLException;
+
+import org.apache.commons.lang.BooleanUtils;
+import org.apache.log4j.Logger;
+import org.dspace.app.rest.exception.PatchBadRequestException;
+import org.dspace.app.rest.model.patch.Operation;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Item;
+import org.dspace.content.service.ItemService;
+import org.dspace.core.Context;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * This is the implementation for Item resource patches.
+ *
+ * Example: <code>
+ * curl -X PATCH http://${dspace.url}/api/item/<:id-item> -H "
+ * Content-Type: application/json" -d '[{ "op": "replace", "path": "
+ * /discoverable", "value": "true|false"]'
+ * </code>
+ *
+ *  @author Michael Spalti
+ */
+@Component
+public class ItemDiscoverableReplaceOperation extends PatchOperation<Item, String>
+        implements ResourcePatchOperation<Item> {
+
+    @Autowired
+    ItemService is;
+
+    private static final Logger log = Logger.getLogger(ItemDiscoverableReplaceOperation.class);
+
+    /**
+     * Sets discoverable field on the item.
+     *
+     * @param item
+     * @param context
+
+     * @throws SQLException
+     * @throws AuthorizeException
+     */
+    public void perform(Context context, Item item, Operation operation)
+            throws SQLException, AuthorizeException, PatchBadRequestException {
+
+        replace(context, item, operation.getValue());
+
+    }
+
+    private void replace(Context context, Item item, Object value)
+            throws SQLException, AuthorizeException {
+
+        Boolean discoverable = BooleanUtils.toBooleanObject((String) value);
+
+        if (discoverable == null) {
+            throw new PatchBadRequestException(
+                    "Value is not a valid boolean expression");
+        }
+
+        try {
+            item.setDiscoverable(discoverable);
+            is.update(context, item);
+        } catch (SQLException | AuthorizeException e) {
+            log.error(e.getMessage(), e);
+            throw e;
+        }
+
+    }
+
+    protected Class<String[]> getArrayClassForEvaluation() {
+        return String[].class;
+    }
+
+    protected Class<String> getClassForEvaluation() {
+        return String.class;
+    }
+
+}
---
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/repository/patch/impl/PatchOperation.java
@@ -0,0 +1,54 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.repository.patch.impl;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dspace.app.rest.exception.PatchBadRequestException;
+import org.dspace.app.rest.model.patch.Operation;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.DSpaceObject;
+import org.dspace.core.Context;
+import org.springframework.data.rest.webmvc.json.patch.LateObjectEvaluator;
+
+public abstract class PatchOperation<DSO extends DSpaceObject, T extends Object>
+        implements ResourcePatchOperation<DSO> {
+
+    public abstract void perform(Context context, DSO resource, Operation operation)
+            throws SQLException, AuthorizeException, PatchBadRequestException;
+
+    public List<T> evaluateArrayObject(LateObjectEvaluator value) {
+        List<T> results = new ArrayList<T>();
+        T[] list = null;
+        if (value != null) {
+            LateObjectEvaluator object = (LateObjectEvaluator) value;
+            list = (T[]) object.evaluate(getArrayClassForEvaluation());
+        }
+
+        for (T t : list) {
+            results.add(t);
+        }
+        return results;
+    }
+
+    public T evaluateSingleObject(LateObjectEvaluator value) {
+        T single = null;
+        if (value != null) {
+            LateObjectEvaluator object = (LateObjectEvaluator) value;
+            single = (T) object.evaluate(getClassForEvaluation());
+        }
+        return single;
+    }
+
+    protected abstract Class<T[]> getArrayClassForEvaluation();
+
+    protected abstract Class<T> getClassForEvaluation();
+
+}
