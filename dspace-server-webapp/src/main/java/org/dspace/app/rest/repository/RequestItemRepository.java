25571f9a600e00df611a450e276d455bbc6f43ff
Mark H. Wood <mwood@IUPUI.Edu>
2018-09-28 17:10:14 -0400

    [DS-3952] Debug the integration tests (unfinished).

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/RequestItemRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/RequestItemRepository.java
@@ -8,22 +8,29 @@
 
 package org.dspace.app.rest.repository;
 
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.UUID;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.dspace.app.requestitem.RequestItem;
 import org.dspace.app.requestitem.service.RequestItemService;
 import org.dspace.app.rest.converter.RequestItemConverter;
 import org.dspace.app.rest.exception.RepositoryMethodNotImplementedException;
+import org.dspace.app.rest.exception.UnprocessableEntityException;
 import org.dspace.app.rest.model.RequestItemRest;
-import org.dspace.app.rest.model.hateoas.RequestItemResource;
+import org.dspace.app.rest.projection.DefaultProjection;
 import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Bitstream;
+import org.dspace.content.Item;
+import org.dspace.content.service.BitstreamService;
+import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.jdbc.UncategorizedSQLException;
 import org.springframework.stereotype.Component;
 
 /**
@@ -34,10 +41,14 @@ import org.springframework.stereotype.Component;
 @Component(RequestItemRest.CATEGORY + '.' + RequestItemRest.NAME)
 public class RequestItemRepository
         extends DSpaceRestRepository<RequestItemRest, String> {
-    private static final Logger LOG = LoggerFactory.getLogger(RequestItemRepository.class);
-
     @Autowired(required = true)
     protected RequestItemService requestItemService;
+
+    @Autowired(required = true)
+    protected BitstreamService bitstreamService;
+
+    @Autowired(required = true)
+    protected ItemService itemService;
 
     @Autowired(required = true)
     protected RequestItemConverter requestItemConverter;
@@ -45,7 +56,7 @@ public class RequestItemRepository
     @Override
     public RequestItemRest findOne(Context context, String id) {
         RequestItem requestItem = requestItemService.findByToken(context, id);
-        return requestItemConverter.fromModel(requestItem);
+        return requestItemConverter.convert(requestItem, new DefaultProjection());
     }
 
     @Override
@@ -55,16 +66,30 @@ public class RequestItemRepository
     }
 
     @Override
-    public RequestItemRest save(Context context, RequestItemRest ri) {
+    protected RequestItemRest createAndReturn(Context context)
+            throws SQLException {
+        HttpServletRequest httpRequest = getRequestService().getCurrentRequest()
+                .getHttpServletRequest();
+        ObjectMapper mapper = new ObjectMapper();
+        RequestItemRest requestItemRest = null;
         try {
-            requestItemService.createRequest(context, ri.getBitstream(),
-                    ri.getItem(), ri.isAllfiles(), ri.getReqEmail(),
-                    ri.getReqName(), ri.getReqMessage());
-        } catch (SQLException ex) {
-            LOG.error("New RequestItem not saved.", ex);
-            throw new UncategorizedSQLException("New RequestItem save", null, ex);
+            ServletInputStream input = httpRequest.getInputStream();
+            requestItemRest = mapper.readValue(input, RequestItemRest.class);
+        } catch (IOException e1) {
+            throw new UnprocessableEntityException("Error parsing request body", e1);
         }
-        return ri;
+        Bitstream bitstream = bitstreamService.find(context,
+                UUID.fromString(requestItemRest.getBitstream().getUuid()));
+        Item item = itemService.find(context,
+                UUID.fromString(requestItemRest.getItem().getUuid()));
+        String itemRequestId = requestItemService.createRequest(context,
+                bitstream, item,
+                requestItemRest.isAllfiles(),
+                requestItemRest.getReqEmail(),
+                requestItemRest.getReqName(),
+                requestItemRest.getReqMessage());
+        requestItemRest.setToken(itemRequestId);
+        return requestItemRest;
     }
 
     // NOTICE:  there is no service method for this -- requests are never deleted?
