d55189a4f23e89afebd915e04b706b37833e4ec0
Andrea Bollini <andrea.bollini@4science.it>
2018-10-17 16:58:04 +0200

    Add javadoc to the matcher class

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/matcher/SubmissionFormFieldMatcher.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/matcher/SubmissionFormFieldMatcher.java
@@ -15,15 +15,62 @@ import static org.hamcrest.Matchers.is;
 
 import org.hamcrest.Matcher;
 
+/**
+ * Helper class to simplify testing of the submission form configuration
+ * 
+ * @author Andrea Bollini (andrea.bollini at 4science.it)
+ *
+ */
 public class SubmissionFormFieldMatcher {
 
     private SubmissionFormFieldMatcher() { }
 
+    /**
+     * Shortcut for the
+     * {@link SubmissionFormFieldMatcher#matchFormFieldDefinition(String, String, String, boolean, String, String, String)}
+     * with a null style
+     * 
+     * @param type
+     *            the expected input type
+     * @param label
+     *            the expected label
+     * @param mandatoryMessage
+     *            the expected mandatoryMessage, can be null. If not empty the fiedl is expected to be flagged as
+     *            mandatory
+     * @param repeatable
+     *            the expected repeatable flag
+     * @param hints
+     *            the expected hints message
+     * @param metadata
+     *            the expected metadata
+     * @return a Matcher for all the condition above
+     */
     public static Matcher<? super Object> matchFormFieldDefinition(String type, String label, String mandatoryMessage,
             boolean repeatable, String hints, String metadata) {
         return matchFormFieldDefinition(type, label, mandatoryMessage, repeatable, hints, null, metadata);
     }
 
+    /**
+     * Check the json representation of a submission form
+     * 
+     * @param type
+     *            the expected input type
+     * @param label
+     *            the expected label
+     * @param mandatoryMessage
+     *            the expected mandatoryMessage, can be null. If not empty the field is expected to be flagged as
+     *            mandatory
+     * @param repeatable
+     *            the expected repeatable flag
+     * @param hints
+     *            the expected hints message
+     * @param style
+     *            the expected style for the field, can be null. If null the corresponding json path is expected to be
+     *            missing
+     * @param metadata
+     *            the expected metadata
+     * @return a Matcher for all the condition above
+     */
     public static Matcher<? super Object> matchFormFieldDefinition(String type, String label, String mandatoryMessage,
             boolean repeatable, String hints, String style, String metadata) {
         return allOf(
