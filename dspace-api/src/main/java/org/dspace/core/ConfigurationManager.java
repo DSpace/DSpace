3ec449f3dfd508a05d3bf708eaf70f6525a00a27
Tim Donohue <tdonohue@duraspace.org>
2018-10-19 14:56:38 -0500

    Update dspace-api code to Commons Config v2

--- dspace-api/src/main/java/org/dspace/core/ConfigurationManager.java
+++ dspace-api/src/main/java/org/dspace/core/ConfigurationManager.java
@@ -10,8 +10,8 @@ package org.dspace.core;
 import java.util.Enumeration;
 import java.util.Properties;
 
-import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.ConfigurationConverter;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.ConfigurationConverter;
 import org.apache.log4j.Logger;
 import org.dspace.services.factory.DSpaceServicesFactory;
 
