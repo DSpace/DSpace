86cd0c702582df8bd916385ea6d03cf89c44cd9b
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2018-11-15 09:27:56 +0100

    [Task 57103] added javadoc

--- dspace-api/src/main/java/org/dspace/content/virtual/Concatenate.java
+++ dspace-api/src/main/java/org/dspace/content/virtual/Concatenate.java
@@ -17,30 +17,63 @@ import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * A bean implementing the {@link VirtualBean} interface to achieve the generation of Virtual metadata
+ */
 public class Concatenate implements VirtualBean {
 
     @Autowired
     private ItemService itemService;
 
+    /**
+     * The fields for which the metadata will be retrieved
+     */
     private List<String> fields;
+    /**
+     * The seperator that will be used to concatenate the values retrieved from the above mentioned fields
+     */
     private String separator;
 
+    /**
+     * Generic getter for the fields property
+     * @return The list of fields to be used in this bean
+     */
     public List<String> getFields() {
         return fields;
     }
 
+    /**
+     * Generic setter for the fields property
+     * @param fields    the list of fields to which the fields property will be set to
+     */
     public void setFields(List<String> fields) {
         this.fields = fields;
     }
 
+    /**
+     * Generic getter for the seperator
+     * @return the seperator to be used by this bean
+     */
     public String getSeparator() {
         return separator;
     }
 
+    /**
+     * Generic setter for the seperator property
+     * @param separator The String seperator value to which this seperator value will be set to
+     */
     public void setSeparator(String separator) {
         this.separator = separator;
     }
 
+    /**
+     * this method will retrieve the metadata values from the given item for all the metadata fields listed
+     * in the fields property and it'll concatenate all those values together with the seperrator specified
+     * in this class
+     * @param context   The relevant DSpace context
+     * @param item      The item that will be used to either retrieve metadata values from
+     * @return The String value for all of the retrieved metadatavalues combined with the seperator
+     */
     public String getValue(Context context, Item item) {
 
         List<String> resultValues = new LinkedList<>();
--- dspace-api/src/main/java/org/dspace/content/virtual/Related.java
+++ dspace-api/src/main/java/org/dspace/content/virtual/Related.java
@@ -17,6 +17,10 @@ import org.dspace.content.service.RelationshipTypeService;
 import org.dspace.core.Context;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * A bean implementing the {@link VirtualBean} interface to achieve the generation of Virtual metadata
+ * by traversing the path of relation specified in the config for this bean
+ */
 public class Related implements VirtualBean {
 
     @Autowired
@@ -31,34 +35,77 @@ public class Related implements VirtualBean {
     @Autowired
     private EntityService entityService;
 
+    /**
+     * The String representing the relationshipType that needs to be used to find the next item
+     */
     private String relationshipTypeString;
+    /**
+     * The left or right place that this relationship needs to have to retrieve the proper item
+     */
     private Integer place;
+    /**
+     * The next bean to call its getValue() method on
+     */
     private VirtualBean virtualBean;
 
+    /**
+     * Generic getter for the relationshipTypeString property of this class
+     * @return  The relationshipTypeString property
+     */
     public String getRelationshipTypeString() {
         return relationshipTypeString;
     }
 
+    /**
+     * Generic setter for the relationshipTypeString property of this class
+     * @param relationshipTypeString    The String to which the relationshipTypeString will be set to
+     */
     public void setRelationshipTypeString(String relationshipTypeString) {
         this.relationshipTypeString = relationshipTypeString;
     }
 
+    /**
+     * Generic getter for the place property of this class
+     * @return  The place property
+     */
     public Integer getPlace() {
         return place;
     }
 
+    /**
+     * Generic setter for the place property of this class
+     * @param place The Integer to which the place property will be set to
+     */
     public void setPlace(Integer place) {
         this.place = place;
     }
 
+    /**
+     * Generic getter for the virtualBean property of this class
+     * @return  The virtualBean property
+     */
     public VirtualBean getVirtualBean() {
         return virtualBean;
     }
 
+    /**
+     * Generic setter for the virtualBean property of this class
+     * @param virtualBean   The VirtualBean to which the virtualBean property will be set to
+     */
     public void setVirtualBean(VirtualBean virtualBean) {
         this.virtualBean = virtualBean;
     }
 
+    /**
+     * This method will find the correct Relationship from the given item to retrieve the other item from it
+     * and pass this along to the next VirtualBean that's stored in this class.
+     * @param context   The relevant DSpace context
+     * @param item      The item that will be used to find the related item through its relationships
+     * @return          The String value of the metadata fields concatened with a seperator as defined
+     *                  in the deepest Concatened bean in the chain
+     *                  Will return null if no relationships are found
+     * @throws SQLException If something goes wrong
+     */
     public String getValue(Context context, Item item) throws SQLException {
         Entity entity = entityService.findByItemId(context, item.getID());
         EntityType entityType = entityService.getType(context, entity);
