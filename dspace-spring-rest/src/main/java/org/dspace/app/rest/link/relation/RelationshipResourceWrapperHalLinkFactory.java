59e79b412450a72df949784cbb11e2cd7b9d9fcd
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2018-11-27 08:33:10 +0100

    [Task 57441] fixed pagination on the /label endpoint

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/link/relation/RelationshipResourceWrapperHalLinkFactory.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/link/relation/RelationshipResourceWrapperHalLinkFactory.java
@@ -4,18 +4,36 @@ import java.util.LinkedList;
 
 import org.dspace.app.rest.RelationshipRestController;
 import org.dspace.app.rest.link.HalLinkFactory;
+import org.dspace.app.rest.model.RelationshipRestWrapper;
+import org.dspace.app.rest.model.hateoas.EmbeddedPageHeader;
+import org.dspace.app.rest.model.hateoas.RelationshipResource;
 import org.dspace.app.rest.model.hateoas.RelationshipResourceWrapper;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class RelationshipResourceWrapperHalLinkFactory
     extends HalLinkFactory<RelationshipResourceWrapper, RelationshipRestController> {
     protected void addLinks(RelationshipResourceWrapper halResource, Pageable pageable, LinkedList<Link> list)
         throws Exception {
-        list.add(buildLink(Link.REL_SELF, getMethodOn()
-            .retrieveByLabel(null, null, halResource.getContent().getLabel(), halResource.getContent().getDsoId())));
+
+        PageImpl<RelationshipResource> page = new PageImpl<>(halResource.getFullList(), pageable,
+                                                             halResource.getTotalElements());
+        halResource.setPageHeader(new EmbeddedPageHeader(getSelfLink(halResource.getContent(), pageable),
+                                                         page, true));
+    }
+
+    public String getSelfLink(RelationshipRestWrapper content, Pageable pageable) throws Exception {
+        if (content != null) {
+            UriComponentsBuilder uriBuilderSelfLink = uriBuilder(getMethodOn()
+                                                                     .retrieveByLabel(null, null, content.getLabel(),
+                                                                                      content.getDsoId(), pageable));
+            return uriBuilderSelfLink.build().toString();
+        }
+        return null;
     }
 
     protected Class<RelationshipRestController> getControllerClass() {
