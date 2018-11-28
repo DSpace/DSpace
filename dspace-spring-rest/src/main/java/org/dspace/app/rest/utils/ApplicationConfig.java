d71f3e15f79d0b880f0e76f178a5e9a75c160f94
Tim Donohue <tdonohue@duraspace.org>
2018-11-28 17:11:08 +0000

    Refactor Application startup to load DSpace configs early in boot process. Also fixes DS-3492

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/utils/ApplicationConfig.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/utils/ApplicationConfig.java
@@ -13,26 +13,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 /**
- * This class provide extra configuration for our Spring Boot Application
+ * This class provides extra configuration for our Spring Boot Application
  * <p>
- * NOTE: @ComponentScan on "org.dspace.app.configuration" provides a way for other modules or plugins
- * to "inject" their own configurations / subpaths into our Spring Boot webapp.
+ * NOTE: @ComponentScan on "org.dspace.app.configuration" provides a way for other DSpace modules or plugins
+ * to "inject" their own Spring configurations / subpaths into our Spring Boot webapp.
+ *
  * @author Andrea Bollini (andrea.bollini at 4science.it)
+ * @author Tim Donohue
  */
 @Configuration
 @EnableSpringDataWebSupport
 @ComponentScan( {"org.dspace.app.rest.converter", "org.dspace.app.rest.repository", "org.dspace.app.rest.utils",
                  "org.dspace.app.configuration"})
 public class ApplicationConfig {
-    @Value("${dspace.dir}")
-    private String dspaceHome;
-
-    @Value("${cors.allowed-origins}")
+    // Allowed CORS origins. Defaults to * (everywhere)
+    // Can be overridden in DSpace configuration
+    @Value("${rest.cors.allowed-origins:*}")
     private String corsAllowedOrigins;
-
-    public String getDspaceHome() {
-        return dspaceHome;
-    }
 
     public String[] getCorsAllowedOrigins() {
         if (corsAllowedOrigins != null) {
