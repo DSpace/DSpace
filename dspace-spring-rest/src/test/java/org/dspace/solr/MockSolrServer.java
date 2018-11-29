975ebb0b5bec309997da67ced0ab7b038da58ac4
Mark H. Wood <mwood@IUPUI.Edu>
2018-11-29 13:40:55 -0500

    [DS-3695] Document what I puzzled out ot MockSolrServer, and small cleanups.

--- dspace-spring-rest/src/test/java/org/dspace/solr/MockSolrServer.java
+++ dspace-spring-rest/src/test/java/org/dspace/solr/MockSolrServer.java
@@ -13,6 +13,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -21,29 +22,65 @@ import org.apache.solr.core.CoreContainer;
 import org.dspace.app.rest.test.AbstractDSpaceIntegrationTest;
 
 /**
- * Abstract class to mock a service that uses SOLR
+ * Factory of connections to an in-process embedded Solr service.
+ * Each instance of this class returns connections to a single core.
+ * Each connection behaves as would a client connection to a remote service.
+ *
+ * <p>
+ * The service is started as needed.
+ * There is at most one open connection to each core; connections are shared and
+ * reference-counted.  When the last connection to the last core is closed,
+ * the service is shut down.
+ *
+ * <p>
+ * To use this:
+ * <ol>
+ *   <li>{@code SolrServer mycore = new MockSolrServer("mycore").getSolrServer();}</li>
+ *   <li>{@code mycore.this(); mycore.that();}</li>
+ *   <li>{@code mycore.destroy();}</li>
+ * </ol>
  */
 public class MockSolrServer {
 
-    private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(MockSolrServer.class);
+    private static final Logger log = LogManager.getLogger();
+
+    /** Shared embedded Solr connections, by name. */
     private static final ConcurrentMap<String, SolrClient> loadedCores = new ConcurrentHashMap<>();
+
+    /** Reference counts for each core. */
     private static final ConcurrentMap<String, AtomicLong> usersPerCore = new ConcurrentHashMap<>();
+
+    /** Container for embedded Solr cores. */
     private static CoreContainer container = null;
 
+    /** Name of this connection's core. */
     private final String coreName;
 
+    /** This instance's connection. */
     private SolrClient solrServer = null;
 
-
+    /**
+     * Wrap an instance of embedded Solr.
+     *
+     * @param coreName name of the core to serve.
+     * @throws Exception passed through.
+     */
     public MockSolrServer(final String coreName) throws Exception {
         this.coreName = coreName;
         initSolrServer();
     }
 
+    /**
+     * @return the wrapped SolrServer.
+     */
     public SolrClient getSolrServer() {
         return solrServer;
     }
 
+    /**
+     * Ensure that this instance's core is loaded.  Create it if necessary.
+     * @throws Exception passed through.
+     */
     protected void initSolrServer() throws Exception {
         solrServer = loadedCores.get(coreName);
         if (solrServer == null) {
@@ -54,6 +91,13 @@ public class MockSolrServer {
         usersPerCore.get(coreName).incrementAndGet();
     }
 
+    /**
+     * Create a SolrServer for this instance's core.  Initialize the container
+     * if no cores are loaded.  Delete all records in the core.
+     *
+     * @param coreName name of the core to "connect".
+     * @return connection to the named core.
+     */
     private static synchronized SolrClient initSolrServerForCore(final String coreName) {
         SolrClient server = loadedCores.get(coreName);
         if (server == null) {
@@ -75,6 +119,13 @@ public class MockSolrServer {
         return server;
     }
 
+    /**
+     * Decrease the reference count for connection to the current core.
+     * If now zero, shut down the connection and discard it.  If no connections
+     * remain, destroy the container.
+     *
+     * @throws Exception passed through.
+     */
     public void destroy() throws Exception {
         if (solrServer != null) {
             long remainingUsers = usersPerCore.get(coreName).decrementAndGet();
@@ -91,6 +142,10 @@ public class MockSolrServer {
         }
     }
 
+    /**
+     * Ensure that a Solr CoreContainer is allocated, aimed at the test Solr
+     * home directory (which should contain test cores).
+     */
     private static synchronized void initSolrContainer() {
         if (container == null) {
             String solrDir = AbstractDSpaceIntegrationTest.getDspaceDir() + File.separator + "solr";
@@ -101,9 +156,11 @@ public class MockSolrServer {
         }
     }
 
+    /**
+     * Discard the embedded Solr container.
+     */
     private static synchronized void destroyContainer() {
         container = null;
         log.info("SOLR CoreContainer destroyed");
     }
-
 }
