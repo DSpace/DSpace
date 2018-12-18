efd579f054abc59ecc0e525aff2694109d7199fd
Ben Bosman <ben@atmire.com>
2018-12-18 09:58:57 +0100

    typing error

--- dspace-api/src/main/java/org/dspace/content/ItemServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/ItemServiceImpl.java
@@ -1291,8 +1291,7 @@ prevent the generation of resource policy entry values with null dspace_object a
      */
     @Override
     public List<MetadataValue> getMetadata(Item item, String schema, String element, String qualifier, String lang) {
-        //Fields of the relationship schema are virtual metadata except for relationship.type which is the type of item in the model
-        //TODO: the "type" should be migrated to a constant
+        //Fields of the relation schema are virtual metadata except for relation.type which is the type of item in the model
         if (StringUtils.equals(schema, MetadataSchemaEnum.RELATION.getName()) && !StringUtils.equals(element, "type")) {
 
             List<MetadataValue> relationMetadata = getRelationshipMetadata(item, false);
