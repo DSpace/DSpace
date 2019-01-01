bbd37213714895faa81e4f3845d37097dc595f2a
Andrea Bollini <andrea.bollini@4science.it>
2019-01-01 18:04:35 +0100

    Fix H2 sql for xml workflow

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/workflow/h2/xmlworkflow/V6.0_2015.08.11__DS-2701_Xml_Workflow_Migration.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/workflow/h2/xmlworkflow/V6.0_2015.08.11__DS-2701_Xml_Workflow_Migration.sql
@@ -17,13 +17,15 @@ UPDATE collection SET workflow_step_3 = null;
 
 DROP INDEX cwf_workflowitem_coll_fk_idx;
 
-ALTER TABLE cwf_workflowitem RENAME COLUMN item_id to item_legacy_id;
-ALTER TABLE cwf_workflowitem ADD item_id RAW(16) REFERENCES Item(uuid);
+ALTER TABLE cwf_workflowitem ALTER COLUMN item_id RENAME TO item_legacy_id;
+ALTER TABLE cwf_workflowitem ADD COLUMN item_id UUID;
+ALTER TABLE cwf_workflowitem ADD FOREIGN KEY (item_id) REFERENCES Item(uuid);
 UPDATE cwf_workflowitem SET item_id = (SELECT item.uuid FROM item WHERE cwf_workflowitem.item_legacy_id = item.item_id);
 ALTER TABLE cwf_workflowitem DROP COLUMN item_legacy_id;
 
-ALTER TABLE cwf_workflowitem RENAME COLUMN collection_id to collection_legacy_id;
-ALTER TABLE cwf_workflowitem ADD collection_id RAW(16) REFERENCES Collection(uuid);
+ALTER TABLE cwf_workflowitem ALTER COLUMN collection_id RENAME TO collection_legacy_id;
+ALTER TABLE cwf_workflowitem ADD COLUMN collection_id UUID;
+ALTER TABLE cwf_workflowitem ADD FOREIGN KEY (collection_id) REFERENCES Collection(uuid);
 UPDATE cwf_workflowitem SET collection_id = (SELECT collection.uuid FROM collection WHERE cwf_workflowitem.collection_legacy_id = collection.collection_id);
 ALTER TABLE cwf_workflowitem DROP COLUMN collection_legacy_id;
 
@@ -39,13 +41,15 @@ ALTER TABLE cwf_collectionrole DROP CONSTRAINT cwf_collectionrole_unique;
 DROP INDEX cwf_cr_coll_role_fk_idx;
 DROP INDEX cwf_cr_coll_fk_idx;
 
-ALTER TABLE cwf_collectionrole RENAME COLUMN collection_id to collection_legacy_id;
-ALTER TABLE cwf_collectionrole ADD collection_id RAW(16) REFERENCES Collection(uuid);
+ALTER TABLE cwf_collectionrole ALTER COLUMN collection_id RENAME TO collection_legacy_id;
+ALTER TABLE cwf_collectionrole ADD COLUMN collection_id UUID;
+ALTER TABLE cwf_collectionrole ADD FOREIGN KEY (collection_id) REFERENCES Collection(uuid);
 UPDATE cwf_collectionrole SET collection_id = (SELECT collection.uuid FROM collection WHERE cwf_collectionrole.collection_legacy_id = collection.collection_id);
 ALTER TABLE cwf_collectionrole DROP COLUMN collection_legacy_id;
 
-ALTER TABLE cwf_collectionrole RENAME COLUMN group_id to group_legacy_id;
-ALTER TABLE cwf_collectionrole ADD group_id RAW(16) REFERENCES epersongroup(uuid);
+ALTER TABLE cwf_collectionrole ALTER COLUMN group_id RENAME TO group_legacy_id;
+ALTER TABLE cwf_collectionrole ADD COLUMN group_id UUID;
+ALTER TABLE cwf_collectionrole ADD FOREIGN KEY (group_id) REFERENCES epersongroup(uuid);
 UPDATE cwf_collectionrole SET group_id = (SELECT epersongroup.uuid FROM epersongroup WHERE cwf_collectionrole.group_legacy_id = epersongroup.eperson_group_id);
 ALTER TABLE cwf_collectionrole DROP COLUMN group_legacy_id;
 
@@ -62,13 +66,15 @@ ALTER TABLE cwf_workflowitemrole DROP CONSTRAINT cwf_workflowitemrole_unique;
 DROP INDEX cwf_wfir_item_role_fk_idx;
 DROP INDEX cwf_wfir_item_fk_idx;
 
-ALTER TABLE cwf_workflowitemrole RENAME COLUMN group_id to group_legacy_id;
-ALTER TABLE cwf_workflowitemrole ADD group_id RAW(16) REFERENCES epersongroup(uuid);
+ALTER TABLE cwf_workflowitemrole ALTER COLUMN group_id RENAME TO group_legacy_id;
+ALTER TABLE cwf_workflowitemrole ADD COLUMN group_id UUID;
+ALTER TABLE cwf_workflowitemrole ADD FOREIGN KEY (group_id) REFERENCES epersongroup(uuid);
 UPDATE cwf_workflowitemrole SET group_id = (SELECT epersongroup.uuid FROM epersongroup WHERE cwf_workflowitemrole.group_legacy_id = epersongroup.eperson_group_id);
 ALTER TABLE cwf_workflowitemrole DROP COLUMN group_legacy_id;
 
-ALTER TABLE cwf_workflowitemrole RENAME COLUMN eperson_id to eperson_legacy_id;
-ALTER TABLE cwf_workflowitemrole ADD eperson_id RAW(16) REFERENCES eperson(uuid);
+ALTER TABLE cwf_workflowitemrole ALTER COLUMN eperson_id RENAME TO eperson_legacy_id;
+ALTER TABLE cwf_workflowitemrole ADD COLUMN eperson_id UUID;
+ALTER TABLE cwf_workflowitemrole ADD FOREIGN KEY (eperson_id) REFERENCES eperson(uuid);
 UPDATE cwf_workflowitemrole SET eperson_id = (SELECT eperson.uuid FROM eperson WHERE cwf_workflowitemrole.eperson_legacy_id = eperson.eperson_id);
 ALTER TABLE cwf_workflowitemrole DROP COLUMN eperson_legacy_id;
 
@@ -84,13 +90,15 @@ CREATE INDEX cwf_wfir_item_fk_idx ON cwf_workflowitemrole(workflowitem_id);
 DROP INDEX cwf_pt_eperson_fk_idx;
 DROP INDEX cwf_pt_workflow_eperson_fk_idx;
 
-ALTER TABLE cwf_pooltask RENAME COLUMN group_id to group_legacy_id;
-ALTER TABLE cwf_pooltask ADD group_id RAW(16) REFERENCES epersongroup(uuid);
+ALTER TABLE cwf_pooltask ALTER COLUMN group_id RENAME TO group_legacy_id;
+ALTER TABLE cwf_pooltask ADD COLUMN group_id UUID;
+ALTER TABLE cwf_pooltask ADD FOREIGN KEY (group_id) REFERENCES epersongroup(uuid);
 UPDATE cwf_pooltask SET group_id = (SELECT epersongroup.uuid FROM epersongroup WHERE cwf_pooltask.group_legacy_id = epersongroup.eperson_group_id);
 ALTER TABLE cwf_pooltask DROP COLUMN group_legacy_id;
 
-ALTER TABLE cwf_pooltask RENAME COLUMN eperson_id to eperson_legacy_id;
-ALTER TABLE cwf_pooltask ADD eperson_id RAW(16) REFERENCES eperson(uuid);
+ALTER TABLE cwf_pooltask ALTER COLUMN eperson_id RENAME TO eperson_legacy_id;
+ALTER TABLE cwf_pooltask ADD COLUMN eperson_id UUID;
+ALTER TABLE cwf_pooltask ADD FOREIGN KEY (eperson_id) REFERENCES eperson(uuid);
 UPDATE cwf_pooltask SET eperson_id = (SELECT eperson.uuid FROM eperson WHERE cwf_pooltask.eperson_legacy_id = eperson.eperson_id);
 ALTER TABLE cwf_pooltask DROP COLUMN eperson_legacy_id;
 
@@ -107,8 +115,10 @@ DROP INDEX cwf_ct_wfs_fk_idx;
 DROP INDEX cwf_ct_wfs_action_fk_idx;
 DROP INDEX cwf_ct_wfs_action_e_fk_idx;
 
-ALTER TABLE cwf_claimtask RENAME COLUMN owner_id to eperson_legacy_id;
-ALTER TABLE cwf_claimtask ADD owner_id RAW(16) REFERENCES eperson(uuid);
+ALTER TABLE cwf_claimtask ALTER COLUMN owner_id RENAME TO eperson_legacy_id;
+ALTER TABLE cwf_claimtask ADD COLUMN owner_id UUID;
+ALTER TABLE cwf_claimtask ADD FOREIGN KEY (owner_id) REFERENCES eperson(uuid);
+
 UPDATE cwf_claimtask SET owner_id = (SELECT eperson.uuid FROM eperson WHERE cwf_claimtask.eperson_legacy_id = eperson.eperson_id);
 ALTER TABLE cwf_claimtask DROP COLUMN eperson_legacy_id;
 
@@ -128,8 +138,9 @@ ALTER TABLE cwf_in_progress_user DROP CONSTRAINT cwf_in_progress_user_unique;
 DROP INDEX cwf_ipu_workflow_fk_idx;
 DROP INDEX cwf_ipu_eperson_fk_idx;
 
-ALTER TABLE cwf_in_progress_user RENAME COLUMN user_id to eperson_legacy_id;
-ALTER TABLE cwf_in_progress_user ADD user_id RAW(16) REFERENCES eperson(uuid);
+ALTER TABLE cwf_in_progress_user ALTER COLUMN user_id RENAME TO eperson_legacy_id;
+ALTER TABLE cwf_in_progress_user ADD COLUMN user_id UUID;
+ALTER TABLE cwf_in_progress_user ADD FOREIGN KEY (user_id) REFERENCES eperson(uuid);
 UPDATE cwf_in_progress_user SET user_id = (SELECT eperson.uuid FROM eperson WHERE cwf_in_progress_user.eperson_legacy_id = eperson.eperson_id);
 ALTER TABLE cwf_in_progress_user DROP COLUMN eperson_legacy_id;
 UPDATE cwf_in_progress_user SET finished = '0' WHERE finished IS NULL;
