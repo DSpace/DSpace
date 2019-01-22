4847620a4ce8d364eb80325ed3adbfdb355039f8
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2019-01-22 14:58:43 +0100

    Fixed issues and wrote IT tests

--- dspace-api/src/main/java/org/dspace/content/RelationshipServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/RelationshipServiceImpl.java
@@ -22,6 +22,7 @@ import org.dspace.content.dao.RelationshipDAO;
 import org.dspace.content.service.ItemService;
 import org.dspace.content.service.RelationshipService;
 import org.dspace.content.service.RelationshipTypeService;
+import org.dspace.core.Constants;
 import org.dspace.core.Context;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -51,13 +52,15 @@ public class RelationshipServiceImpl implements RelationshipService {
 
     public Relationship create(Context context, Relationship relationship) throws SQLException, AuthorizeException {
         if (isRelationshipValidToCreate(context, relationship)) {
-            if (!authorizeService.isAdmin(context)) {
+            if (authorizeService.authorizeActionBoolean(context, relationship.getLeftItem(), Constants.WRITE) ||
+                authorizeService.authorizeActionBoolean(context, relationship.getRightItem(), Constants.WRITE)) {
+                updatePlaceInRelationship(context, relationship);
+                return relationshipDAO.create(context, relationship);
+            } else {
                 throw new AuthorizeException(
-                    "Only administrators can modify relationship");
+                    "You do not have write rights on this relationship's items");
             }
-            updatePlaceInRelationship(context, relationship);
 
-            return relationshipDAO.create(context, relationship);
         } else {
             throw new IllegalArgumentException("The relationship given was not valid");
         }
@@ -219,13 +222,15 @@ public class RelationshipServiceImpl implements RelationshipService {
 
     public void delete(Context context, Relationship relationship) throws SQLException, AuthorizeException {
         if (isRelationshipValidToDelete(context, relationship)) {
-            if (!authorizeService.isAdmin(context)) {
+            if (authorizeService.authorizeActionBoolean(context, relationship.getLeftItem(), Constants.WRITE) ||
+                authorizeService.authorizeActionBoolean(context, relationship.getRightItem(), Constants.WRITE)) {
+                relationshipDAO.delete(context, relationship);
+                updatePlaceInRelationship(context, relationship);
+            } else {
                 throw new AuthorizeException(
-                    "Only administrators can delete relationship");
+                    "You do not have write rights on this relationship's items");
             }
-            relationshipDAO.delete(context, relationship);
 
-            updatePlaceInRelationship(context, relationship);
         } else {
             throw new IllegalArgumentException("The relationship given was not valid");
         }
