7d66dc739f0c8431bccc6dcde9dd6775befdaeab
Terry Brady <terrywbrady@gmail.com>
2019-02-19 10:16:21 -0800

    update sql

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/update-sequences.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/update-sequences.sql
@@ -23,7 +23,9 @@
 -- you can find incseq.sql at: http://www.akadia.com/services/scripts/incseq.sql
 -- Here that script was renamed to updateseq.sql.
 
-DECLARE
+-- 
+
+create or replace
  PROCEDURE updateseq ( seq IN VARCHAR,
                        tbl IN VARCHAR,
                        attr IN VARCHAR,
@@ -41,27 +43,49 @@ DECLARE
              || ' START WITH '
              || NVL(curr, 1);
    END;
-BEGIN
- updateseq('seq', 'tbl', 'attr', '');
-END;
-/
 
-execute updateseq(bitstreamformatregistry_seq, bitstreamformatregistry, bitstream_format_id, "");
-execute updateseq(fileextension_seq, fileextension, file_extension_id, "");
-execute updateseq(resourcepolicy_seq, resourcepolicy, policy_id, "");
-execute updateseq(workspaceitem_seq, workspaceitem, workspace_item_id, "");
-execute updateseq(workflowitem_seq, workflowitem, workflow_id, "");
-execute updateseq(tasklistitem_seq, tasklistitem, tasklist_id, "");
-execute updateseq(registrationdata_seq, registrationdata, registrationdata_id, "");
-execute updateseq(subscription_seq, subscription, subscription_id, "");
-execute updateseq(metadatafieldregistry_seq, metadatafieldregistry, metadata_field_id, "");
-execute updateseq(metadatavalue_seq, metadatavalue, metadata_value_id, "");
-execute updateseq(metadataschemaregistry_seq, metadataschemaregistry, metadata_schema_id, "");
-execute updateseq(harvested_collection_seq, harvested_collection, id, "");
-execute updateseq(harvested_item_seq, harvested_item, id, "");
-execute updateseq(webapp_seq, webapp, webapp_id, "");
-execute updateseq(requestitem_seq, requestitem, requestitem_id, "");
-execute updateseq(handle_id_seq, handle, handle_id, "");
+create or replace procedure updateseq(
+  seq IN VARCHAR,
+  tbl IN VARCHAR,
+  attr IN VARCHAR,
+  cond IN VARCHAR ) 
+IS
+  l_val number := 0;
+  new_val number:= 0;
+  offset number:=0;
+BEGIN
+  execute immediate
+  'select ' || seq || '.nextval from dual' INTO l_val;
+
+  execute immediate
+  'select max(' || attr || ') from ' || tbl || ' ' || cond || '' INTO new_val;
+
+  execute immediate
+  'alter sequence ' || seq || ' increment by ' || (new_val - l_val + 1) || ' minvalue 0';
+
+  execute immediate
+  'select ' || seq || '.nextval from dual' INTO l_val;
+
+  execute immediate
+  'alter sequence ' || seq || ' increment by 1 minvalue 0';
+END;
+
+execute updateseq('bitstreamformatregistry_seq', 'bitstreamformatregistry', 'bitstream_format_id', '');
+execute updateseq('fileextension_seq', 'fileextension', 'file_extension_id', '');
+execute updateseq('resourcepolicy_seq', 'resourcepolicy', 'policy_id', '');
+execute updateseq('workspaceitem_seq', 'workspaceitem', 'workspace_item_id', '');
+execute updateseq('workflowitem_seq', 'workflowitem', 'workflow_id', '');
+execute updateseq('tasklistitem_seq', 'tasklistitem', 'tasklist_id', '');
+execute updateseq('registrationdata_seq', 'registrationdata', 'registrationdata_id', '');
+execute updateseq('subscription_seq', 'subscription', 'subscription_id', '');
+execute updateseq('metadatafieldregistry_seq', 'metadatafieldregistry', 'metadata_field_id', '');
+execute updateseq('metadatavalue_seq', 'metadatavalue', 'metadata_value_id', '');
+execute updateseq('metadataschemaregistry_seq', 'metadataschemaregistry', 'metadata_schema_id', '');
+execute updateseq('harvested_collection_seq', 'harvested_collection', 'id', '');
+execute updateseq('harvested_item_seq', 'harvested_item', 'id', '');
+execute updateseq('webapp_seq', 'webapp', 'webapp_id', '');
+execute updateseq('requestitem_seq', 'requestitem', 'requestitem_id', '');
+execute updateseq('handle_id_seq', 'handle', 'handle_id', '');
 
 -- Handle Sequence is a special case.  Since Handles minted by DSpace use the 'handle_seq',
 -- we need to ensure the next assigned handle will *always* be unique.  So, 'handle_seq'
@@ -70,4 +94,4 @@ execute updateseq(handle_id_seq, handle, handle_id, "");
 --  For all 'handle' values which have a number in their suffix (after '/'), find the maximum
 --  suffix value, convert it to a number, and set the 'handle_seq' to start at the next value
 -- (see updateseq.sql script for more)
-execute updateseq(handle_seq, handle, "to_number(regexp_replace(handle, '.*/', ''), '999999999999')", "WHERE REGEXP_LIKE(handle, '^.*/[0123456789]*$')");
+execute updateseq('handle_seq', 'handle', "to_number(regexp_replace(handle, '.*/', ''), '999999999999')", "WHERE REGEXP_LIKE(handle, '^.*/[0123456789]*$')");
