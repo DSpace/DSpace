eee2929e51814f03f1e8ef427bf910e07b215ae6
Andrea Bollini <andrea.bollini@4science.it>
2019-03-15 00:42:16 +0100

    DS-3851 remove unused import

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/converter/PoolTaskConverter.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/converter/PoolTaskConverter.java
@@ -7,7 +7,6 @@
  */
 package org.dspace.app.rest.converter;
 
-import org.apache.log4j.Logger;
 import org.dspace.app.rest.model.PoolTaskRest;
 import org.dspace.browse.BrowsableObject;
 import org.dspace.xmlworkflow.storedcomponents.PoolTask;
