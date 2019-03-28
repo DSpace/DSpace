8f5251211976be042c338559316114b138da0890
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2019-03-28 13:16:06 -0300

    -#4747 Hice un refactoring de la jerarquía de Authority Providers
    Repliqué lo hecho en CIC-Digital, creando una query sparql general en la clase GeneralSEDICIAuthorityProvider (ex SimpleSPARQLAuthorityProvider), que delega en las subclases (Intitution y Tesauro) la consulta específica en la busqueda por texto y el armado del SELECT.
    También eliminé la clase AdvancedSPARQLAuthorityProvider, dejando que AuthorAuthority herede directamente de SPARQLAuthorityProvider

--- dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/authority/InstitutionAuthority.java
+++ dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/authority/InstitutionAuthority.java
@@ -3,56 +3,29 @@ package ar.edu.unlp.sedici.dspace.authority;
 import org.dspace.content.authority.Choice;
 import com.hp.hpl.jena.query.ParameterizedSparqlString;
 import com.hp.hpl.jena.query.QuerySolution;
-public class InstitutionAuthority extends SimpleSPARQLAuthorityProvider {
-
+public class InstitutionAuthority extends GeneralSEDICIAuthorityProvider {
 
 	@Override
-	protected ParameterizedSparqlString getSparqlSearchByIdQuery(String field,
-			String key, String locale) {
-		ParameterizedSparqlString pqs = new ParameterizedSparqlString();
-
-		pqs.setNsPrefix("foaf", NS_FOAF);
-		pqs.setNsPrefix("dc", NS_DC);
-
-		pqs.setCommandText("SELECT ?institution ?label \n");
-		pqs.append("WHERE {\n");
-		pqs.append("?institution a foaf:Organization ; foaf:name ?label .\n");
-		pqs.append("FILTER(REGEX(?institution, ?key, \"i\"))\n");
-		pqs.append("}\n");
-
-		pqs.setLiteral("key", key);
-		return pqs;
+	protected String getSelectQueryFields(boolean idSearch) {
+		String selectFields= super.getSelectQueryFields(idSearch);
+		//Si la query se hace por text hay que agregar mas variables
+		if (!idSearch)
+			selectFields +=" ?initials ?labelpadre ?initpadre";
+		return selectFields;
 	}
 
-	@Override
-	protected ParameterizedSparqlString getSparqlSearchByTextQuery(
-			String field, String text, String locale) {
-		ParameterizedSparqlString pqs = new ParameterizedSparqlString();
-
-		pqs.setNsPrefix("foaf", NS_FOAF);
-		pqs.setNsPrefix("dc", NS_DC);
-		pqs.setNsPrefix("sioc", NS_SIOC);
-		pqs.setNsPrefix("skos", NS_SKOS);
-
-		pqs.setCommandText("SELECT ?institution ?label ?initials ?labelpadre ?initpadre \n");
-		pqs.append("WHERE {\n");
-		pqs.append("?institution a foaf:Organization ; foaf:name ?label .\n");
-		pqs.append("OPTIONAL { ?institution sioc:id ?initials } . \n");
-		pqs.append("OPTIONAL { ?institution skos:broader ?padre . ?padre foaf:name ?labelpadre  OPTIONAL { ?padre sioc:id  ?initpadre } . } \n");    
-		if (!"".equals(text)) {
+	protected void getTextFilterQuery(ParameterizedSparqlString pqs, String filter) {
+		pqs.append("OPTIONAL { ?concept sioc:id ?initials } . \n");
+		pqs.append("OPTIONAL { ?concept skos:broader ?padre . ?padre foaf:name ?labelpadre  OPTIONAL { ?padre sioc:id  ?initpadre } . } \n");
+		if (!"".equals(filter)) {
 			pqs.append("FILTER(REGEX(?label, ?text, \"i\") || REGEX(?initials, ?text, \"i\"))\n");
-			text = normalizeTextForParserSPARQL10(text);
-			pqs.setLiteral("text", text.trim());
+			pqs.setLiteral("text", filter.trim());
 		}
-		pqs.append("}\n");
-		pqs.append("ORDER BY ASC(?label)\n");
-
-		return pqs;
 	}
 
 	@Override
 	protected Choice extractChoice(QuerySolution solution) {
-		String key = solution.getResource("institution").getURI();
+		String key = solution.getResource("concept").getURI();
 		String label = "";
 		String value = solution.getLiteral("label").getString();
 		if (solution.contains("labelpadre") && !"".equals(solution.getLiteral("labelpadre").getString())) {
--- dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/authority/TesauroAuthority.java
+++ dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/authority/TesauroAuthority.java
@@ -5,76 +5,25 @@ import org.dspace.content.authority.Choice;
 import com.hp.hpl.jena.query.ParameterizedSparqlString;
 import com.hp.hpl.jena.query.QuerySolution;
 
-public class TesauroAuthority extends SimpleSPARQLAuthorityProvider {
+public class TesauroAuthority extends GeneralSEDICIAuthorityProvider {
 
 	@Override
-	protected ParameterizedSparqlString getSparqlSearchByIdQuery(String field,
-			String key, String locale) {
-		ParameterizedSparqlString pqs = new ParameterizedSparqlString();
-
-		pqs.setNsPrefix("skos", NS_SKOS);
-
-		pqs.setCommandText("SELECT ?term ?label \n");
-		pqs.append("WHERE {\n");
-		pqs.append("?term a skos:Concept; skos:prefLabel ?label .\n");
-		pqs.append("FILTER(REGEX(?term, ?key, \"i\"))\n");
-		pqs.append("}\n");
-
-		pqs.setLiteral("key", key);
-		return pqs;
+	protected String getSelectQueryFields(boolean idSearch) {
+		String selectFields= super.getSelectQueryFields(idSearch);
+		//Si la query se hace por text hay que agregar mas variables
+		if (!idSearch)
+			selectFields +=" ?parent ?parentLabel";
+		return selectFields;
 	}
 
-	@Override
-	protected ParameterizedSparqlString getSparqlSearchByTextQuery(
-			String field, String text, String locale) {
-		ParameterizedSparqlString pqs = new ParameterizedSparqlString();
-
-		String rdfType = "";
-		switch (field) {
-		case "thesis_degree_name":
-			rdfType = "sedici:Grado";
-			break;
-		case "sedici_subject_descriptores":
-			rdfType = "sedici:Descriptor";
-			break;
-		case "sedici_subject_materias":
-			rdfType = "sedici:Materia";
-			break;
-		case "sedici_subject_eurovoc":
-			rdfType = "sedici:Eurovoc";
-			break;
-		case "sedici_subject_decs":
-			rdfType = "sedici:Decs";
-			break;
-		case "dc_coverage_spatial":
-			rdfType = "sedici:Spatial";
-			break;
-		default:
-			break;
-		}
-		
-		text = normalizeTextForParserSPARQL10(text);
-
-		pqs.setNsPrefix("skos", NS_SKOS);
-		pqs.setNsPrefix("sedici", NS_SEDICI);
-
-		pqs.setCommandText("SELECT ?term ?label ?parent ?parentLabel \n");
-		pqs.append("WHERE {\n");
-		pqs.append("?term a "+ rdfType +"; skos:prefLabel ?label .\n");
-		pqs.append("OPTIONAL { ?term skos:broader ?parent . ?parent skos:prefLabel ?parentLabel } \n");
-		if (!"".equals(text.trim())) {
-			pqs.append("FILTER(REGEX(?label, ?text, 'i'))\n");
-			pqs.setLiteral("text", text.trim());
-		}
-		pqs.append("}\n");
-		pqs.append("ORDER BY ASC(?label)\n");
-		
-		return pqs;
+	protected void getTextFilterQuery(ParameterizedSparqlString pqs, String filter) {
+		pqs.append("OPTIONAL { ?concept skos:broader ?parent . ?parent skos:prefLabel ?parentLabel } \n");
+		super.getTextFilterQuery(pqs, filter);
 	}
 
 	@Override
 	protected Choice extractChoice(QuerySolution solution) {
-		String key = solution.getResource("term").getURI();
+		String key = solution.getResource("concept").getURI();
 		String value = solution.getLiteral("label").getString();
 		String label = "";
 		if (solution.contains("parentLabel")){
