f716bd9d536dab1bb2089a12409c4496a0ef8786
pcb <per.christian.bjelke@bibsys.no>
2019-04-05 13:15:20 +0200

    tester multiple git repos

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,39 +1,45 @@
 pipeline {
     agent any
 
+    checkout scm
+
+    dir('artifacts') {
+        git url: 'https://github.com/BIBSYSDEV/2019-03-05_FaaSPresentation.git'
+    }
+
     environment {
         VERSION = "${env.BRANCH_NAME}".replaceAll('/', '_').toLowerCase()
     }
 
     stages {
-        stage('Maven Build') {
+//        stage('Maven Build') {
+//            steps {
+//                sh 'mvn package -Dmirage2.on=true -P !dspace-lni,!dspace-sword,!dspace-jspui,!dspace-rdf'
+//            }
+//        }
+        stage('Doing stuff in workspace') {
             steps {
-                sh 'mvn package -Dmirage2.on=true -P !dspace-lni,!dspace-sword,!dspace-jspui,!dspace-rdf'
-            }
-        }
-        stage('Copy files to server') {
-            steps {
-                sh 'echo "Copying files to server"'
+                sh 'echo "Doing stuff"'
                 sh 'pwd'
             }
         }
     }
 
-    post {
-        failure {
-            script {
-                def message = "${currentBuild.fullDisplayName} - Failure after ${currentBuild.durationString.replaceFirst(" and counting", "")}"
-                emailext(
-                        subject: "FAILURE: ${currentBuild.fullDisplayName}",
-                        body: "${message}\n Open: ${env.BUILD_URL}",
-                        to: 'pcb@unit.no',
-                        attachlog: true,
-                        compresslog: true,
-                        recipientProviders: [[$class: 'CulpritsRecipientProvider']]
-                )
-
-            }
-        }
-    }
+//    post {
+//        failure {
+//            script {
+//                def message = "${currentBuild.fullDisplayName} - Failure after ${currentBuild.durationString.replaceFirst(" and counting", "")}"
+//                emailext(
+//                        subject: "FAILURE: ${currentBuild.fullDisplayName}",
+//                        body: "${message}\n Open: ${env.BUILD_URL}",
+//                        to: 'pcb@unit.no',
+//                        attachlog: true,
+//                        compresslog: true,
+//                        recipientProviders: [[$class: 'CulpritsRecipientProvider']]
+//                )
+//
+//            }
+//        }
+//    }
 }
 
