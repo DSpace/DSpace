50db4b8fc7a231625ef53199bb9f81b812c263bf
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2019-04-12 14:48:25 +0200

    [DS-4217] applied the name refactor and rewrote BadRequestException to use this class instead

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/repository/patch/factories/impl/EPersonLoginReplaceOperation.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/repository/patch/factories/impl/EPersonLoginReplaceOperation.java
@@ -7,7 +7,7 @@
  */
 package org.dspace.app.rest.repository.patch.factories.impl;
 
-import org.dspace.app.rest.exception.PatchBadRequestException;
+import org.dspace.app.rest.exception.DSpaceBadRequestException;
 import org.dspace.app.rest.model.EPersonRest;
 import org.dspace.app.rest.model.patch.Operation;
 import org.springframework.stereotype.Component;
@@ -38,7 +38,7 @@ public class EPersonLoginReplaceOperation extends ReplacePatchOperation<EPersonR
     @Override
     void checkModelForExistingValue(EPersonRest resource, Operation operation) {
         if ((Object) resource.isCanLogIn() == null) {
-            throw new PatchBadRequestException("Attempting to replace a non-existent value.");
+            throw new DSpaceBadRequestException("Attempting to replace a non-existent value.");
         }
     }
 
