71cd11b558d8e95a473cc5b96858c9f0997572b5
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2019-05-07 09:59:32 +0200

    Implemented the InitializeEntitiesIT changes

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/InitializeEntitiesIT.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/InitializeEntitiesIT.java
@@ -32,6 +32,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * This class is used to verify the behavior of the {@link org.dspace.app.util.InitializeEntities} script
+ * It will take the relationship-types.xml as initial input and check if all the objects that created properly.
+ * It will then also update and verify these objects by using a second XML to verify the update behaviour
+ * of this script.
+ * This will ensure that all the EntityTypes and RelationshipTypes are instantiated correctly.
+ */
 public class InitializeEntitiesIT extends AbstractControllerIntegrationTest {
 
     @Autowired
@@ -95,42 +102,21 @@ public class InitializeEntitiesIT extends AbstractControllerIntegrationTest {
      * Verify whether the initialize-entities script can update the relationship types correctly
      */
     @Test
-    public void test() throws Exception {
+    public void updateRelationshipTypesTest() throws Exception {
         List<RelationshipType> relationshipTypes = relationshipTypeService.findAll(context);
-
-        getClient().perform(get("/api/core/relationshiptypes"))
-
-                   //We expect a 200 OK status
-                   .andExpect(status().isOk())
-                   //10 relationship types should be created
-                   .andExpect(jsonPath("$.page.totalElements", is(10)))
-                   //There needs to be a self link to this endpoint
-                   .andExpect(jsonPath("$._links.self.href", containsString("api/core/relationshiptypes")))
-                   //We have 10 relationship types, they need to all be present in the embedded section
-                   .andExpect(jsonPath("$._embedded.relationshiptypes", containsInAnyOrder(
-                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(0)),
-                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(1)),
-                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(2)),
-                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(3)),
-                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(4)),
-                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(5)),
-                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(6)),
-                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(7)),
-                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(8)),
-                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(9)))
-                   ));
-
-        //Verify the left min cardinality of the first relationship type (isAuthorOfPublication) is 0
-        getClient().perform(get("/api/core/relationshiptypes/" + relationshipTypes.get(0).getID()))
-                   .andExpect(status().isOk())
-                   .andExpect(jsonPath("$.leftMinCardinality", is(0)));
 
         //Update the relationships using a different test XML with the initialize-entities script
         String pathToFile = configurationService.getProperty("dspace.dir") +
             File.separator + "config" + File.separator + "entities" + File.separator + "relationship-types-update.xml";
         runDSpaceScript("initialize-entities", "-f", pathToFile);
 
-        //This is a helper object to compare whether the update was successful
+        // This is a helper object to compare whether the update was successful. We're simply taking the first
+        // RelationshipType object in the list and altering this by setting the LeftMinCardinality on 10. We've
+        // Made sure that this RelationshipType object is altered in the same way in the relationship-types-update.xml
+        // File and thus by running the script, the RelationshipType object in the Database should be the same as our
+        // alteredRelationshipType object that we just created.
+        // It's important to note that this object will not alter anything in the Database, this object is merely
+        // made for the comparison between the REST response and we expect to have happened through the script
         RelationshipType alteredRelationshipType = relationshipTypes.get(0);
         alteredRelationshipType.setLeftMinCardinality(10);
         getClient().perform(get("/api/core/relationshiptypes"))
@@ -160,5 +146,41 @@ public class InitializeEntitiesIT extends AbstractControllerIntegrationTest {
         getClient().perform(get("/api/core/relationshiptypes/" + relationshipTypes.get(0).getID()))
                    .andExpect(status().isOk())
                    .andExpect(jsonPath("$.leftMinCardinality", is(10)));
+    }
+
+    /**
+     * Verifies that the initialize-entities script ran properly and that the objects are created properly
+     * @throws Exception
+     */
+    @Test
+    public void getAllRelationshipTypesTest() throws Exception{
+        List<RelationshipType> relationshipTypes = relationshipTypeService.findAll(context);
+
+        getClient().perform(get("/api/core/relationshiptypes"))
+
+                   //We expect a 200 OK status
+                   .andExpect(status().isOk())
+                   //10 relationship types should be created
+                   .andExpect(jsonPath("$.page.totalElements", is(10)))
+                   //There needs to be a self link to this endpoint
+                   .andExpect(jsonPath("$._links.self.href", containsString("api/core/relationshiptypes")))
+                   //We have 10 relationship types, they need to all be present in the embedded section
+                   .andExpect(jsonPath("$._embedded.relationshiptypes", containsInAnyOrder(
+                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(0)),
+                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(1)),
+                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(2)),
+                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(3)),
+                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(4)),
+                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(5)),
+                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(6)),
+                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(7)),
+                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(8)),
+                       RelationshipTypeMatcher.matchRelationshipTypeEntry(relationshipTypes.get(9)))
+                   ));
+
+        //Verify the left min cardinality of the first relationship type (isAuthorOfPublication) is 0
+        getClient().perform(get("/api/core/relationshiptypes/" + relationshipTypes.get(0).getID()))
+                   .andExpect(status().isOk())
+                   .andExpect(jsonPath("$.leftMinCardinality", is(0)));
     }
 }
