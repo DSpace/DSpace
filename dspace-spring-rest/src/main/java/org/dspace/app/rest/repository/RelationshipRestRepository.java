4430a31ea6c7c612649a47ad858e6d974ff17b65
Ben Bosman <ben@atmire.com>
2019-05-07 19:54:20 +0200

    Temporarily disabled for https://github.com/DSpace/Rest7Contract/pull/57#discussion_r272605397

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/repository/RelationshipRestRepository.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/repository/RelationshipRestRepository.java
@@ -34,7 +34,6 @@ import org.dspace.eperson.EPerson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 
