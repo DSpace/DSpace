9777d3cde8733273afcbde23b2b53f62635ef2db
Marie Verdonck <maria.verdonck@atmire.com>
2019-05-31 14:49:59 +0200

    Thown sql exception with message & checkstyle

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/repository/BitstreamFormatRestRepository.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/repository/BitstreamFormatRestRepository.java
@@ -2,35 +2,38 @@
  * The contents of this file are subject to the license and copyright
  * detailed in the LICENSE and NOTICE files at the root of the source
  * tree and available online at
- * <p>
+ *
  * http://www.dspace.org/license/
  */
 package org.dspace.app.rest.repository;
 
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.dspace.app.rest.converter.BitstreamFormatConverter;
 import org.dspace.app.rest.converter.MetadataConverter;
 import org.dspace.app.rest.exception.UnprocessableEntityException;
 import org.dspace.app.rest.model.BitstreamFormatRest;
 import org.dspace.app.rest.model.hateoas.BitstreamFormatResource;
+
 import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.BitstreamFormat;
 import org.dspace.content.service.BitstreamFormatService;
 import org.dspace.core.Context;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.HttpServletRequest;
 
 
 /**
@@ -96,11 +99,11 @@ public class BitstreamFormatRestRepository extends DSpaceRestRepository<Bitstrea
         BitstreamFormat bitstreamFormat = null;
         try {
             bitstreamFormat = bitstreamFormatService.create(context);
-            bitstreamFormatService.update(context, bitstreamFormat);
             this.setAllValuesOfRest(context, bitstreamFormat, bitstreamFormatRest);
             bitstreamFormatService.update(context, bitstreamFormat);
         } catch (SQLException e) {
-            throw new RuntimeException(e.getMessage(), e);
+            throw new RuntimeException("Unable to create new bitstream format with description: "
+                    + bitstreamFormatRest.getShortDescription(), e);
         }
 
         return converter.convert(bitstreamFormat);
@@ -117,7 +120,8 @@ public class BitstreamFormatRestRepository extends DSpaceRestRepository<Bitstrea
             throw new UnprocessableEntityException("Error parsing collection json: " + e.getMessage());
         }
         BitstreamFormat bitstreamFormat = null;
-        String notFoundException = apiCategory + "." + model + " with id: " + id + " not found";
+        String notFoundException = "ResourceNotFoundException:" + apiCategory + "." + model
+                + " with id: " + id + " not found";
         try {
             bitstreamFormat = bitstreamFormatService.find(context, id);
             if (bitstreamFormat == null) {
@@ -141,26 +145,27 @@ public class BitstreamFormatRestRepository extends DSpaceRestRepository<Bitstrea
     @PreAuthorize("hasAuthority('ADMIN')")
     protected void delete(Context context, Integer id)  throws AuthorizeException {
         BitstreamFormat bitstreamFormat = null;
-        String notFoundException = BitstreamFormatRest.CATEGORY + "." + BitstreamFormatRest.NAME
-                + " with id: " + id + " not found";
+        String notFoundException = "ResourceNotFoundException:" + BitstreamFormatRest.CATEGORY + "."
+                + BitstreamFormatRest.NAME + " with id: " + id + " not found";
         try {
             bitstreamFormat = bitstreamFormatService.find(context, id);
             if (bitstreamFormat == null) {
                 throw new ResourceNotFoundException(notFoundException);
             }
         } catch (SQLException e) {
-            throw new ResourceNotFoundException(notFoundException);
+            throw new RuntimeException("RuntimeException: Unable to find BitstreamFormat with id = " + id, e);
         }
         try {
             bitstreamFormatService.delete(context, bitstreamFormat);
         } catch (SQLException e) {
-            throw new RuntimeException("Unable to delete BitstreamFormat with id  = " + id, e);
+            throw new RuntimeException("RuntimeException: Unable to delete BitstreamFormat with id  = " + id, e);
         }
     }
 
 
 
-    private void setAllValuesOfRest(Context c, BitstreamFormat bitstreamFormat, BitstreamFormatRest bitstreamFormatRest) {
+    private void setAllValuesOfRest
+            (Context c, BitstreamFormat bitstreamFormat, BitstreamFormatRest bitstreamFormatRest) {
         try {
             bitstreamFormat.setShortDescription(c, bitstreamFormatRest.getShortDescription());
         } catch (SQLException e) {
