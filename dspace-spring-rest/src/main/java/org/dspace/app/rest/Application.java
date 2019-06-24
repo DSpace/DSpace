590d5631974ed087e529369956d9ef56ddbb5261
Mark H. Wood <mwood@IUPUI.Edu>
2019-06-24 17:23:18 -0400

    [DS-3872] Finish removing autoconfigure from Application.

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/Application.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/Application.java
@@ -22,7 +22,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.velocity.VelocityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
