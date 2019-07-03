5aecac781bdfa24559633aebe59ea374284a51cf
Chris Herron <christopher.herron@atmire.com>
2019-07-03 10:30:45 -0400

    DS-4292 Fix Browse pagination with starts-with

--- dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/ConfigurableBrowse.java
+++ dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/ConfigurableBrowse.java
@@ -1099,6 +1099,7 @@ class BrowseParams
         paramMap.put(BrowseParams.RESULTS_PER_PAGE, Integer
                 .toString(this.scope.getResultsPerPage()));
         paramMap.put(BrowseParams.ETAL, Integer.toString(this.etAl));
+        paramMap.put(BrowseParams.STARTS_WITH, this.scope.getStartsWith());
 
         return paramMap;
     }
