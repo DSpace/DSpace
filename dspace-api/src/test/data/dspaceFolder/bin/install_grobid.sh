af81e3250d14cb0dd2af54ff9e5bd85d5f65002f
Giuseppe Digilio <giuseppe.digilio@4science.it>
2019-07-24 17:49:01 +0200

    [D4CRIS-777] Redirect stdout of grobid installation script to /dev/null

--- dspace-api/src/test/data/dspaceFolder/bin/install_grobid.sh
+++ dspace-api/src/test/data/dspaceFolder/bin/install_grobid.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 wget https://github.com/kermitt2/grobid/archive/0.5.5.zip -P ../
-unzip ../0.5.5.zip -d ../
+unzip ../0.5.5.zip -d ../ 1>/dev/null
 cd ../grobid-0.5.5/
 ./gradlew clean install
 nohup ./gradlew run &
