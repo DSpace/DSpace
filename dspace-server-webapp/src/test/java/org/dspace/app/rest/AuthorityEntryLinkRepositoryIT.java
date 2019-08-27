0ad00fa41bab9708ab1bc09170d10a350f02df35
Giuseppe Digilio <giuseppe.digilio@4science.it>
2019-08-27 16:50:52 +0200

    [D4CRIS-750] Improved integration test for AuthorityEntryLink

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/AuthorityEntryLinkRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/AuthorityEntryLinkRepositoryIT.java
@@ -12,8 +12,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.List;
-
 import org.dspace.app.rest.builder.CollectionBuilder;
 import org.dspace.app.rest.builder.CommunityBuilder;
 import org.dspace.app.rest.builder.ItemBuilder;
@@ -21,8 +19,6 @@ import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
 import org.dspace.content.Item;
-import org.dspace.services.ConfigurationService;
-import org.dspace.services.factory.DSpaceServicesFactory;
 import org.junit.Test;
 
 /**
@@ -34,11 +30,11 @@ public class AuthorityEntryLinkRepositoryIT extends AbstractControllerIntegratio
 
     @Test
     /**
-     * The workspaceitem endpoint must provide proper pagination
+     * The authorities endpoint must provide proper pagination
      *
      * @throws Exception
      */
-    public void entriesTest() throws Exception {
+    public void authorAuthorityEntriesTest() throws Exception {
         context.turnOffAuthorisationSystem();
 
         //1. A community-collection structure with one parent community with sub-community and two collections.
@@ -53,58 +49,245 @@ public class AuthorityEntryLinkRepositoryIT extends AbstractControllerIntegratio
         Collection col3 = CollectionBuilder.createCollection(context, child1).withName("OrgUnits").build();
 
         Item author1 = ItemBuilder.createItem(context, col1)
-                                  .withTitle("Author1")
-                                  .withIssueDate("2017-10-17")
-                                  .withAuthor("Smith, Donald")
-                                  .withRelationshipType("Person")
-                                  .build();
+            .withTitle("Smith, Donald")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Smith, Donald")
+            .withRelationshipType("Person")
+            .build();
 
         Item author2 = ItemBuilder.createItem(context, col2)
-                                  .withTitle("Author2")
-                                  .withIssueDate("2016-02-13")
-                                  .withAuthor("Smith, Maria")
-                                  .withRelationshipType("Person")
-                                  .build();
+            .withTitle("Smith, Maria")
+            .withIssueDate("2016-02-13")
+            .withAuthor("Smith, Maria")
+            .withRelationshipType("Person")
+            .build();
 
         Item author3 = ItemBuilder.createItem(context, col2)
-                                  .withTitle("Author3")
-                                  .withIssueDate("2016-02-13")
-                                  .withAuthor("Maybe, Maybe")
-                                  .withRelationshipType("Person")
-                                  .build();
+            .withTitle("Smith, Joe")
+            .withIssueDate("2016-12-13")
+            .withAuthor("Smith, Joe")
+            .withRelationshipType("Person")
+            .build();
 
-        Item orgUnit1 = ItemBuilder.createItem(context, col3)
-                                   .withTitle("OrgUnit1")
-                                   .withAuthor("Testy, TEst")
-                                   .withIssueDate("2015-01-01")
-                                   .withRelationshipType("OrgUnit")
-                                   .build();
+        Item author4 = ItemBuilder.createItem(context, col2)
+            .withTitle("Smith, Jack")
+            .withIssueDate("2016-09-20")
+            .withAuthor("Smith, Jack")
+            .withRelationshipType("Person")
+            .build();
 
-        Item project1 = ItemBuilder.createItem(context, col3)
-                                   .withTitle("Project1")
-                                   .withAuthor("Testy, TEst")
-                                   .withIssueDate("2015-01-01")
-                                   .withRelationshipType("Project")
-                                   .build();
+        Item author5 = ItemBuilder.createItem(context, col2)
+            .withTitle("Smith, J.")
+            .withIssueDate("2016-01-13")
+            .withAuthor("Smith, J.")
+            .withRelationshipType("Person")
+            .build();
+
+        Item author6 = ItemBuilder.createItem(context, col1)
+            .withTitle("Foe, Donald")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Smith, Donald")
+            .withRelationshipType("Person")
+            .build();
 
         context.restoreAuthSystemState();
 
         String token = getAuthToken(admin.getEmail(), password);
 
-        ConfigurationService configService = DSpaceServicesFactory.getInstance().getConfigurationService();
-        List<String> list = configService.getPropertyKeys("choices.plugin.");
-        String wosUser = configService.getProperty("submission.lookup.webofknowledge.user");
-        String wosPassword = configService.getProperty("submission.lookup.webofknowledge.password");
-        String grobid = configService.getProperty("metadata.extraction.grobid.url");
-
         getClient(token).perform(get("/api/integration/authorities/AuthorAuthority/entries")
-                .param("query", "Author1")
-                .param("metadata", "dc.contributor.author")
-                .param("uuid", col1.getID().toString())
-                .param("size", "10"))
+            .param("query", "Smith")
+            .param("metadata", "dc.contributor.author")
+            .param("uuid", col1.getID().toString())
+            .param("size", "10"))
             //We expect a 200 OK status
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.page.size", is(10)))
-            .andExpect(jsonPath("$.page.totalElements", is(1)));
+            .andExpect(jsonPath("$.page.totalElements", is(5)))
+            .andExpect(jsonPath("$._embedded.authorityEntries[0].value", is("Smith, Donald")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[1].value", is("Smith, Maria")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[2].value", is("Smith, Jack")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[3].value", is("Smith, Joe")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[4].value", is("Smith, J.")));
+
+        getClient(token).perform(get("/api/integration/authorities/AuthorAuthority/entries")
+            .param("query", "Smith, J")
+            .param("metadata", "dc.contributor.author")
+            .param("uuid", col1.getID().toString())
+            .param("size", "10"))
+            //We expect a 200 OK status
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.page.size", is(10)))
+            .andExpect(jsonPath("$.page.totalElements", is(3)))
+            .andExpect(jsonPath("$._embedded.authorityEntries[0].value", is("Smith, J.")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[1].value", is("Smith, Jack")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[2].value", is("Smith, Joe")));
+
+        getClient(token).perform(get("/api/integration/authorities/AuthorAuthority/entries")
+            .param("query", "Smith, J.")
+            .param("metadata", "dc.contributor.author")
+            .param("uuid", col1.getID().toString())
+            .param("size", "10"))
+            //We expect a 200 OK status
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.page.size", is(10)))
+            .andExpect(jsonPath("$.page.totalElements", is(1)))
+            .andExpect(jsonPath("$._embedded.authorityEntries[0].value", is("Smith, J.")));
+
+        getClient(token).perform(get("/api/integration/authorities/AuthorAuthority/entries")
+            .param("query", "Donald")
+            .param("metadata", "dc.contributor.author")
+            .param("uuid", col1.getID().toString())
+            .param("size", "10"))
+            //We expect a 200 OK status
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.page.size", is(10)))
+            .andExpect(jsonPath("$.page.totalElements", is(2)))
+            .andExpect(jsonPath("$._embedded.authorityEntries[0].value", is("Smith, Donald")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[1].value", is("Foe, Donald")));
+    }
+
+    @Test
+    /**
+     * The authorities endpoint must provide proper pagination
+     *
+     * @throws Exception
+     */
+    public void journalAuthorityEntriesTest() throws Exception {
+        context.turnOffAuthorisationSystem();
+
+        //1. A community-collection structure with one parent community with sub-community and two collections.
+        parentCommunity = CommunityBuilder.createCommunity(context)
+                                          .withName("Parent Community")
+                                          .build();
+        Community child1 = CommunityBuilder.createSubCommunity(context, parentCommunity)
+                                           .withName("Sub Community")
+                                           .build();
+        Collection col1 = CollectionBuilder.createCollection(context, child1).withName("Collection 1").build();
+        Collection col2 = CollectionBuilder.createCollection(context, child1).withName("Collection 2").build();
+        Collection col3 = CollectionBuilder.createCollection(context, child1).withName("OrgUnits").build();
+
+        Item journal = ItemBuilder.createItem(context, col1)
+            .withTitle("Science Technological journal")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Science Technological journal")
+            .withRelationshipType("Journal")
+            .build();
+
+        Item journal1 = ItemBuilder.createItem(context, col1)
+            .withTitle("Science China-Earth Sciences")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Science China-Earth Sciences")
+            .withRelationshipType("Journal")
+            .build();
+
+        Item journal2 = ItemBuilder.createItem(context, col1)
+            .withTitle("Science")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Science")
+            .withRelationshipType("Journal")
+            .build();
+
+        Item journal3 = ItemBuilder.createItem(context, col1)
+            .withTitle("Ocean Science")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Ocean Science")
+            .withRelationshipType("Journal")
+            .build();
+
+        Item journal4 = ItemBuilder.createItem(context, col1)
+            .withTitle("Management science")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Management science")
+            .withRelationshipType("Journal")
+            .build();
+
+        Item journal5 = ItemBuilder.createItem(context, col1)
+            .withTitle("Service Science")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Service Science")
+            .withRelationshipType("Journal")
+            .build();
+
+        Item journal6 = ItemBuilder.createItem(context, col1)
+            .withTitle("Annals of science")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Annals of science")
+            .withRelationshipType("Journal")
+            .build();
+
+        Item journal7 = ItemBuilder.createItem(context, col1)
+            .withTitle("Bioresource technology journal")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Bioresource technology journal")
+            .withRelationshipType("Journal")
+            .build();
+
+        Item journal8 = ItemBuilder.createItem(context, col1)
+            .withTitle("Energy economics")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Energy economics")
+            .withRelationshipType("Journal")
+            .build();
+
+        Item journal9 = ItemBuilder.createItem(context, col1)
+            .withTitle("Nuclear Energy-journal Of The British Nuclear Energy Society")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Nuclear Energy-journal Of The British Nuclear Energy Society")
+            .withRelationshipType("Journal")
+            .build();
+
+        Item journal10 = ItemBuilder.createItem(context, col1)
+            .withTitle("Journal of High Energy Astrophysics")
+            .withIssueDate("2017-10-17")
+            .withAuthor("Journal of High Energy Astrophysics")
+            .withRelationshipType("Journal")
+            .build();
+
+        context.restoreAuthSystemState();
+
+        String token = getAuthToken(admin.getEmail(), password);
+
+        getClient(token).perform(get("/api/integration/authorities/JournalAuthority/entries")
+            .param("query", "Journal")
+            .param("metadata", "dc.relation.journal")
+            .param("uuid", col1.getID().toString())
+            .param("size", "10"))
+            //We expect a 200 OK status
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.page.size", is(10)))
+            .andExpect(jsonPath("$.page.totalElements", is(4)))
+            .andExpect(jsonPath("$._embedded.authorityEntries[0].value", is("Science Technological journal")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[1].value", is("Bioresource technology journal")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[2].value", is("Journal of High Energy Astrophysics")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[3].value",
+                    is("Nuclear Energy-journal Of The British Nuclear Energy Society")));
+
+        getClient(token).perform(get("/api/integration/authorities/JournalAuthority/entries")
+            .param("query", "science")
+            .param("metadata", "dc.relation.journal")
+            .param("uuid", col1.getID().toString())
+            .param("size", "10"))
+            //We expect a 200 OK status
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.page.size", is(10)))
+            .andExpect(jsonPath("$.page.totalElements", is(7)))
+            .andExpect(jsonPath("$._embedded.authorityEntries[0].value", is("Science")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[1].value", is("Science China-Earth Sciences")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[2].value", is("Management science")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[3].value", is("Ocean Science")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[4].value", is("Service Science")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[5].value", is("Annals of science")))
+            .andExpect(jsonPath("$._embedded.authorityEntries[6].value", is("Science Technological journal")));
+
+        getClient(token).perform(get("/api/integration/authorities/JournalAuthority/entries")
+            .param("query", "Annals of science")
+            .param("metadata", "dc.relation.journal")
+            .param("uuid", col1.getID().toString())
+            .param("size", "10"))
+            //We expect a 200 OK status
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.page.size", is(10)))
+            .andExpect(jsonPath("$.page.totalElements", is(1)))
+            .andExpect(jsonPath("$._embedded.authorityEntries[0].value", is("Annals of science")));
     }
 }
