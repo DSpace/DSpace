1d08e15254971b996be803f3b4b1011f24a03140
Ben Bosman <ben@atmire.com>
2019-09-04 13:22:56 +0200

    solr authority

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/AuthorityRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/AuthorityRestRepositoryIT.java
@@ -12,14 +12,48 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
+import org.dspace.authority.PersonAuthorityValue;
+import org.dspace.authority.factory.AuthorityServiceFactory;
+import org.dspace.services.ConfigurationService;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class AuthorityRestRepositoryIT extends AbstractControllerIntegrationTest {
 
+    @Autowired
+    ConfigurationService configurationService;
+
+    @Before
+    public void setup() throws Exception {
+        super.setUp();
+        configurationService.setProperty("plugin.named.org.dspace.content.authority.ChoiceAuthority",
+                "org.dspace.content.authority.SolrAuthority = SolrAuthorAuthority");
+
+        configurationService.setProperty("solr.authority.server",
+                "${solr.server}/authority");
+        configurationService.setProperty("choices.plugin.dc.contributor.author",
+                "SolrAuthorAuthority");
+        configurationService.setProperty("choices.presentation.dc.contributor.author",
+                "authorLookup");
+        configurationService.setProperty("authority.controlled.dc.contributor.author",
+                "true");
+
+        configurationService.setProperty("authority.author.indexer.field.1",
+                "dc.contributor.author");
+
+        PersonAuthorityValue person1 = new PersonAuthorityValue();
+        person1.setLastName("Shirasaka");
+        person1.setFirstName("Seiko");
+        person1.setValue("Shirasaka, Seiko");
+        person1.setField("dc_contributor_author");
+        AuthorityServiceFactory.getInstance().getAuthorityIndexingService().indexContent(person1);
+    }
+
     @Test
-    public void correctQueryTest() throws Exception {
+    public void correctSrscQueryTest() throws Exception {
         String token = getAuthToken(admin.getEmail(), password);
         getClient(token).perform(
             get("/api/integration/authorities/srsc/entries")
@@ -31,7 +65,7 @@ public class AuthorityRestRepositoryIT extends AbstractControllerIntegrationTest
     }
 
     @Test
-    public void incorrectQueryTest() throws Exception {
+    public void noResultsSrscQueryTest() throws Exception {
         String token = getAuthToken(admin.getEmail(), password);
         getClient(token).perform(
             get("/api/integration/authorities/srsc/entries")
@@ -47,7 +81,7 @@ public class AuthorityRestRepositoryIT extends AbstractControllerIntegrationTest
     /**
      * This functionality is currently broken, it returns all 22 values
      */
-    public void commonTypesTest() throws Exception {
+    public void correctCommonTypesTest() throws Exception {
         String token = getAuthToken(admin.getEmail(), password);
         getClient(token).perform(
             get("/api/integration/authorities/common_types/entries")
@@ -56,6 +90,18 @@ public class AuthorityRestRepositoryIT extends AbstractControllerIntegrationTest
                 .param("size", "1000"))
                         .andExpect(status().isOk())
                         .andExpect(jsonPath("$.page.totalElements", Matchers.is(2)));
+    }
+
+    @Test
+    public void correctSolrQueryTest() throws Exception {
+        String token = getAuthToken(admin.getEmail(), password);
+        getClient(token).perform(
+                get("/api/integration/authorities/SolrAuthorAuthority/entries")
+                        .param("metadata", "dc.contributor.author")
+                        .param("query", "Shirasaka")
+                        .param("size", "1000"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.page.totalElements", Matchers.is(1)));
     }
 
     @Test
@@ -87,5 +133,11 @@ public class AuthorityRestRepositoryIT extends AbstractControllerIntegrationTest
                 get("/api/integration/authorities/common_types/entryValues/Learning%20Object"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.page.totalElements", Matchers.is(1)));
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        AuthorityServiceFactory.getInstance().getAuthorityIndexingService().cleanIndex();
+        super.destroy();
     }
 }
