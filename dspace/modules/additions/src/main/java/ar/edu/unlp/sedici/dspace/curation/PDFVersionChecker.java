77a949a718de1a133769ba5d3d9e63ce84e41a7d
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2019-10-01 15:29:17 -0300

    -#5642 Termino la tarea de curation PDFVersionChecker
    - Implemento la función isPDF(), la cual dado un bitstream retorna true si es un pdf
    - Cambio el nombre de algunos métodos
    - Modifico manejo de excepciones
    - Ahora si la tarea no generó ningún reporte para un item, entonces directamente no imprime nada. Antes se imprimía una línea vacía

--- dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/curation/PDFVersionChecker.java
+++ dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/curation/PDFVersionChecker.java
@@ -16,6 +16,8 @@ public class PDFVersionChecker extends AbstractCurationTask {
 
 	private final float MIN_VERSION_ALLOWED = (float) 1.4;
 
+	private final String PDF_MIME_YPE = "application/pdf";
+
 	@Override
 	public int perform(DSpaceObject dso) throws IOException {
 		StringBuilder reporter = new StringBuilder();
@@ -29,37 +31,39 @@ public class PDFVersionChecker extends AbstractCurationTask {
 					if ("ORIGINAL".equals(bundle.getName())) {
 						for (Bitstream bitstream : bundle.getBitstreams()) {
 							if (isPDF(bitstream)) {
-								parseBitstream(bitstream, item.getHandle(), reporter);
+								try {
+									checkPDFVersion(bitstream, item.getHandle(), reporter);
+								} catch (Exception e) {
+									// Something went wrong
+									reportError(e, reporter, bitstream, item.getHandle());
+								}
 							}
 						}
 					}
 				}
 			} catch (SQLException sqle) {
-				// Something went wrong
-				reportError(sqle, reporter, item.getHandle());
-				status = Curator.CURATE_ERROR;
-			} catch (AuthorizeException e) {
-				// Something went wrong
-				reportError(e, reporter, item.getHandle());
+				reporter.append(sqle.getMessage());
+				sqle.printStackTrace();
 				status = Curator.CURATE_ERROR;
 			}
-			report(reporter.toString());
+			String report = reporter.toString();
+			if (!report.isEmpty()) {
+				report(report);
+			}
 		}
 		setResult(reporter.toString());
 		return status;
 	}
 
-	private void parseBitstream(Bitstream bitstream, String itemHandle, StringBuilder reporter)
+	private void checkPDFVersion(Bitstream bitstream, String itemHandle, StringBuilder reporter)
 			throws IOException, SQLException, AuthorizeException {
 		PDDocument pdfDoc = null;
 		try {
 			pdfDoc = PDDocument.load(bitstream.retrieve());
 			float pdfVersion = pdfDoc.getVersion();
 			if (MIN_VERSION_ALLOWED > pdfVersion) {
-				reportVersionError(pdfVersion, bitstream, itemHandle, reporter);
+				reportOldPDFVersion(pdfVersion, bitstream, itemHandle, reporter);
 			}
-		} catch (IOException ioe) {
-			reportError(ioe, reporter, itemHandle);
 		} finally {
 			if (pdfDoc != null) {
 				pdfDoc.close();
@@ -68,21 +72,27 @@ public class PDFVersionChecker extends AbstractCurationTask {
 	}
 
 	private boolean isPDF(Bitstream bitstream) {
-		return true;
+		return bitstream.getFormat().getMIMEType().equalsIgnoreCase(PDF_MIME_YPE);
 	}
 
-	private void reportVersionError(float pdfVersion, Bitstream bitstream, String itemHandle, StringBuilder reporter) {
-		reporter.append("- ERROR: Item ");
-		reporter.append(itemHandle);
-		reporter.append(" Bitstream id: ");
-		reporter.append(bitstream.getID());
-		reporter.append(". PDF version not allowed: ");
+	private void reportOldPDFVersion(float pdfVersion, Bitstream bitstream, String itemHandle, StringBuilder reporter) {
+		reporter.append("- ERROR: PDF version not allowed: ");
 		reporter.append(pdfVersion);
+		reporter.append(". Item ");
+		reporter.append(itemHandle);
+		reporter.append(". Bitstream '");
+		reporter.append(bitstream.getName());
+		reporter.append("' with id ");
+		reporter.append(bitstream.getID());
 		reporter.append("\n");
 	}
 
-	private void reportError(Exception e, StringBuilder reporter, String itemHandle) {
-		reporter.append("There was a problem while processing bitstream of item ");
+	private void reportError(Exception e, StringBuilder reporter, Bitstream bitstream, String itemHandle) {
+		reporter.append("- ERROR: There was a problem while processing bitstream '");
+		reporter.append(bitstream.getName());
+		reporter.append("' with id ");
+		reporter.append(bitstream.getID());
+		reporter.append(" of item ");
 		reporter.append(itemHandle);
 		reporter.append(":\n");
 		reporter.append(e.getMessage());
