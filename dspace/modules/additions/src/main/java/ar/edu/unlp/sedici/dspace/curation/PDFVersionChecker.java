89f1b74e8f0974f2e79846f0e9cc13811b2f5311
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2019-10-02 12:53:51 -0300

    -#5642 Ahora la tarea de curation PDFVersionChecker también reporta los pdfs que están encryptados.
    Cambio el manejo de las excepciones.
    Refactorizo algunos métodos.

--- dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/curation/PDFVersionChecker.java
+++ dspace/modules/additions/src/main/java/ar/edu/unlp/sedici/dspace/curation/PDFVersionChecker.java
@@ -1,6 +1,7 @@
 package ar.edu.unlp.sedici.dspace.curation;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -9,6 +10,7 @@ import org.dspace.content.Bitstream;
 import org.dspace.content.Bundle;
 import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
+import org.dspace.core.Constants;
 import org.dspace.curate.AbstractCurationTask;
 import org.dspace.curate.Curator;
 
@@ -18,33 +20,27 @@ public class PDFVersionChecker extends AbstractCurationTask {
 
 	private final String PDF_MIME_YPE = "application/pdf";
 
+	private int status;
+
 	@Override
 	public int perform(DSpaceObject dso) throws IOException {
 		StringBuilder reporter = new StringBuilder();
-		int status = Curator.CURATE_SKIP;
+		status = Curator.CURATE_SKIP;
 
 		if (dso instanceof Item) {
 			status = Curator.CURATE_SUCCESS;
 			Item item = (Item) dso;
 			try {
-				for (Bundle bundle : item.getBundles()) {
-					if ("ORIGINAL".equals(bundle.getName())) {
-						for (Bitstream bitstream : bundle.getBitstreams()) {
-							if (isPDF(bitstream)) {
-								try {
-									checkPDFVersion(bitstream, item.getHandle(), reporter);
-								} catch (Exception e) {
-									// Something went wrong
-									reportError(e, reporter, bitstream, item.getHandle());
-								}
-							}
+				for (Bundle bundle : item.getBundles(Constants.DEFAULT_BUNDLE_NAME)) {
+					for (Bitstream bitstream : bundle.getBitstreams()) {
+						if (isPDF(bitstream)) {
+							checkValidPDF(bitstream, item.getHandle(), reporter);
 						}
 					}
 				}
-			} catch (SQLException sqle) {
-				reporter.append(sqle.getMessage());
-				sqle.printStackTrace();
-				status = Curator.CURATE_ERROR;
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new IOException(e);
 			}
 			String report = reporter.toString();
 			if (!report.isEmpty()) {
@@ -55,20 +51,27 @@ public class PDFVersionChecker extends AbstractCurationTask {
 		return status;
 	}
 
-	private void checkPDFVersion(Bitstream bitstream, String itemHandle, StringBuilder reporter)
+	private void checkValidPDF(Bitstream bitstream, String itemHandle, StringBuilder reporter)
 			throws IOException, SQLException, AuthorizeException {
 		PDDocument pdfDoc = null;
+		InputStream file = bitstream.retrieve();
 		try {
-			pdfDoc = PDDocument.load(bitstream.retrieve());
-			float pdfVersion = pdfDoc.getVersion();
-			if (MIN_VERSION_ALLOWED > pdfVersion) {
-				reportOldPDFVersion(pdfVersion, bitstream, itemHandle, reporter);
-			}
+			pdfDoc = PDDocument.load(file);
+		} catch (IOException e) {
+			reportPDFException(e, bitstream, itemHandle, reporter);
 		} finally {
 			if (pdfDoc != null) {
 				pdfDoc.close();
 			}
 		}
+		float pdfVersion = pdfDoc.getVersion();
+		if (MIN_VERSION_ALLOWED > pdfVersion) {
+			reportOldPDFVersion(pdfVersion, bitstream, itemHandle, reporter);
+		}
+		if (pdfDoc.isEncrypted()) {
+			reportEncryptedPDF(bitstream, itemHandle, reporter);
+		}
+
 	}
 
 	private boolean isPDF(Bitstream bitstream) {
@@ -76,27 +79,32 @@ public class PDFVersionChecker extends AbstractCurationTask {
 	}
 
 	private void reportOldPDFVersion(float pdfVersion, Bitstream bitstream, String itemHandle, StringBuilder reporter) {
-		reporter.append("- ERROR: PDF version not allowed: ");
-		reporter.append(pdfVersion);
-		reporter.append(". Item ");
-		reporter.append(itemHandle);
-		reporter.append(". Bitstream '");
-		reporter.append(bitstream.getName());
-		reporter.append("' with id ");
-		reporter.append(bitstream.getID());
-		reporter.append("\n");
+		String errorMsge = "PDF version not allowed: " + pdfVersion;
+		reportError(errorMsge, bitstream, itemHandle, reporter);
 	}
 
-	private void reportError(Exception e, StringBuilder reporter, Bitstream bitstream, String itemHandle) {
-		reporter.append("- ERROR: There was a problem while processing bitstream '");
-		reporter.append(bitstream.getName());
-		reporter.append("' with id ");
-		reporter.append(bitstream.getID());
-		reporter.append(" of item ");
-		reporter.append(itemHandle);
-		reporter.append(":\n");
+	private void reportEncryptedPDF(Bitstream bitstream, String itemHandle, StringBuilder reporter) {
+		String errorMsge = "PDF encrypted";
+		reportError(errorMsge, bitstream, itemHandle, reporter);
+	}
+
+	private void reportPDFException(Exception e, Bitstream bitstream, String itemHandle, StringBuilder reporter) {
+		String errorMsge = "There was a problem while processing bitstream";
+		reportError(errorMsge, bitstream, itemHandle, reporter);
 		reporter.append(e.getMessage());
 		e.printStackTrace();
+		status = Curator.CURATE_ERROR;
 	}
 
+	private void reportError(String errorMessage, Bitstream bitstream, String itemHandle, StringBuilder reporter) {
+		reporter.append("- ERROR: ");
+		reporter.append(errorMessage);
+		reporter.append(". Item #");
+		reporter.append(itemHandle);
+		reporter.append("#. Bitstream with id #");
+		reporter.append(bitstream.getID());
+		reporter.append("# and name #");
+		reporter.append(bitstream.getDescription());
+		reporter.append("#\n");
+	}
 }
