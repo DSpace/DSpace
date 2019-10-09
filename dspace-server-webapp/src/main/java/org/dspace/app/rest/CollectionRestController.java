3f552078b9fdf22dded01407f75917f24401bbf1
Peter Nijs <peter.nijs@atmire.com>
2019-10-09 20:07:13 +0200

    65257: update and delete for collection template items

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/CollectionRestController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/CollectionRestController.java
@@ -7,10 +7,12 @@
  */
 package org.dspace.app.rest;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.dspace.app.rest.model.CollectionRest;
 import org.dspace.app.rest.model.ItemRest;
 import org.dspace.app.rest.model.hateoas.ItemResource;
@@ -26,6 +28,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -70,5 +73,31 @@ public class CollectionRestController {
         ItemRest templateItem = collectionRestRepository.getTemplateItem(context, uuid);
 
         return new ItemResource(templateItem, utils);
+    }
+
+    @PreAuthorize("hasAuthority('AUTHENTICATED')")
+    @RequestMapping(method = RequestMethod.PATCH,
+            value = REGEX_REQUESTMAPPING_IDENTIFIER_AS_UUID + "/itemtemplate")
+    public ResponseEntity<ResourceSupport> replaceTemplateItem(HttpServletRequest request, @PathVariable UUID uuid,
+                                                               @RequestBody(required = true) JsonNode jsonNode)
+            throws SQLException, AuthorizeException {
+
+        Context context = ContextUtil.obtainContext(request);
+        ItemRest templateItem = collectionRestRepository.patchTemplateItem(context, uuid, jsonNode);
+
+        return ControllerUtils.toResponseEntity(HttpStatus.OK, null,
+                new ItemResource(templateItem, utils));
+    }
+
+    @PreAuthorize("hasAuthority('AUTHENTICATED')")
+    @RequestMapping(method = RequestMethod.DELETE,
+            value = REGEX_REQUESTMAPPING_IDENTIFIER_AS_UUID + "/itemtemplate")
+    public ResponseEntity<ResourceSupport> deleteTemplateItem(HttpServletRequest request, @PathVariable UUID uuid)
+            throws SQLException, AuthorizeException, IOException {
+
+        Context context = ContextUtil.obtainContext(request);
+        collectionRestRepository.removeTemplateItem(context, uuid);
+
+        return ControllerUtils.toEmptyResponse(HttpStatus.NO_CONTENT);
     }
 }
