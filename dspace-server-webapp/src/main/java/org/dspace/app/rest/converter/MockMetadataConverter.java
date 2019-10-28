c90e52f429e127cd699bab8045c40b2914c555e2
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2019-10-28 09:21:50 +0100

    Fixed checkstyle issue in MockMetadataConverter

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/converter/MockMetadataConverter.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/converter/MockMetadataConverter.java
@@ -7,13 +7,10 @@
  */
 package org.dspace.app.rest.converter;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
