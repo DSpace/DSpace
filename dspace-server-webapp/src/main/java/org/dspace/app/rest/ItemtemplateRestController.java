99522b2e9640f6b0f314d9804cc552f63eb4c6c7
Peter Nijs <peter.nijs@atmire.com>
2019-10-28 13:06:45 +0100

    65543: Collection item template
    - Moved endpoints

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/ItemtemplateRestController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/ItemtemplateRestController.java
@@ -9,11 +9,13 @@ package org.dspace.app.rest;
 
 import static org.dspace.app.rest.utils.RegexUtils.REGEX_REQUESTMAPPING_IDENTIFIER_AS_UUID;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.UUID;
+
 import javax.servlet.http.HttpServletRequest;
 
-import org.dspace.app.rest.model.CollectionRest;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.dspace.app.rest.model.ItemRest;
 import org.dspace.app.rest.model.hateoas.ItemResource;
 import org.dspace.app.rest.repository.CollectionRestRepository;
@@ -28,14 +30,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping("/api/" + CollectionRest.CATEGORY + "/" + CollectionRest.PLURAL_NAME)
-public class CollectionRestController {
+@RequestMapping("/api/core/itemtemplates" + REGEX_REQUESTMAPPING_IDENTIFIER_AS_UUID + "/")
+public class ItemtemplateRestController {
 
     @Autowired
     private Utils utils;
@@ -44,27 +46,37 @@ public class CollectionRestController {
     private CollectionRestRepository collectionRestRepository;
 
     @PreAuthorize("hasAuthority('AUTHENTICATED')")
-    @RequestMapping(method = RequestMethod.POST,
-            value = REGEX_REQUESTMAPPING_IDENTIFIER_AS_UUID + "/itemtemplate")
-    public ResponseEntity<ResourceSupport> createTemplateItem(HttpServletRequest request, @PathVariable UUID uuid)
-            throws SQLException, AuthorizeException {
-
-        Context context = ContextUtil.obtainContext(request);
-        ItemRest templateItem = collectionRestRepository.createTemplateItem(context, uuid);
-
-        return ControllerUtils.toResponseEntity(HttpStatus.CREATED, null,
-                new ItemResource(templateItem, utils));
-    }
-
-    @PreAuthorize("hasAuthority('AUTHENTICATED')")
-    @RequestMapping(method = RequestMethod.GET,
-            value = REGEX_REQUESTMAPPING_IDENTIFIER_AS_UUID + "/itemtemplate")
+    @RequestMapping(method = RequestMethod.GET)
     public ItemResource getTemplateItem(HttpServletRequest request, @PathVariable UUID uuid)
-            throws SQLException {
+        throws SQLException {
 
         Context context = ContextUtil.obtainContext(request);
         ItemRest templateItem = collectionRestRepository.getTemplateItem(context, uuid);
 
         return new ItemResource(templateItem, utils);
+    }
+
+    @PreAuthorize("hasAuthority('AUTHENTICATED')")
+    @RequestMapping(method = RequestMethod.PATCH)
+    public ResponseEntity<ResourceSupport> replaceTemplateItem(HttpServletRequest request, @PathVariable UUID uuid,
+                                                               @RequestBody(required = true) JsonNode jsonNode)
+        throws SQLException, AuthorizeException {
+
+        Context context = ContextUtil.obtainContext(request);
+        ItemRest templateItem = collectionRestRepository.patchTemplateItem(context, uuid, jsonNode);
+
+        return ControllerUtils.toResponseEntity(HttpStatus.OK, null,
+            new ItemResource(templateItem, utils));
+    }
+
+    @PreAuthorize("hasAuthority('AUTHENTICATED')")
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<ResourceSupport> deleteTemplateItem(HttpServletRequest request, @PathVariable UUID uuid)
+        throws SQLException, AuthorizeException, IOException {
+
+        Context context = ContextUtil.obtainContext(request);
+        collectionRestRepository.removeTemplateItem(context, uuid);
+
+        return ControllerUtils.toEmptyResponse(HttpStatus.NO_CONTENT);
     }
 }
