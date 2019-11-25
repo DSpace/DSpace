de4c0bceb18619f1a1c81110627427d674d4bd74
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2019-11-25 11:34:26 +0100

    Refactored the findOne and findAll methods to work through the RestResourceController instead of a custom one

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/ExternalSourceRestRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/ExternalSourceRestRepository.java
@@ -14,6 +14,8 @@ import org.dspace.app.rest.converter.ExternalSourceEntryRestConverter;
 import org.dspace.app.rest.converter.ExternalSourceRestConverter;
 import org.dspace.app.rest.model.ExternalSourceEntryRest;
 import org.dspace.app.rest.model.ExternalSourceRest;
+import org.dspace.app.rest.model.hateoas.ExternalSourceResource;
+import org.dspace.core.Context;
 import org.dspace.external.model.ExternalDataObject;
 import org.dspace.external.provider.ExternalDataProvider;
 import org.dspace.external.service.ExternalDataService;
@@ -29,7 +31,7 @@ import org.springframework.stereotype.Component;
  * {@link org.dspace.app.rest.ExternalSourcesRestController}
  */
 @Component(ExternalSourceRest.CATEGORY + "." + ExternalSourceRest.NAME)
-public class ExternalSourceRestRepository extends AbstractDSpaceRestRepository {
+public class ExternalSourceRestRepository extends DSpaceRestRepository<ExternalSourceRest, String> {
 
     @Autowired
     private ExternalDataService externalDataService;
@@ -41,33 +43,6 @@ public class ExternalSourceRestRepository extends AbstractDSpaceRestRepository {
     private ExternalSourceEntryRestConverter externalSourceEntryRestConverter;
 
     /**
-     * This method will retrieve all the ExternalSources that are available to be used to look up external data
-     *
-     * @param pageable  The pagination object
-     * @return          A Page containing the ExternalSourceRest objects
-     */
-    public Page<ExternalSourceRest> getAllExternalSources(Pageable pageable) {
-        List<ExternalDataProvider> externalSources = externalDataService.getExternalDataProviders();
-        Page<ExternalSourceRest> page = utils.getPage(externalSources, pageable).map(externalSourceRestConverter);
-        return page;
-
-    }
-
-    /**
-     * This method will retrieve one external source in particular through the given AuthorityName parameter
-     * @param authorityName The authorityName parameter which will define which ExternalSource is returned
-     * @return              The ExternalSourceRest object corresponding with the AuthorityName
-     */
-    public ExternalSourceRest getExternalSource(String authorityName) {
-        ExternalDataProvider externalDataProvider = externalDataService.getExternalDataProvider(authorityName);
-        if (externalDataProvider == null) {
-            throw new ResourceNotFoundException("ExternalDataProvider for: " + authorityName + " couldn't be found");
-        }
-        return externalSourceRestConverter.fromModel(externalDataProvider);
-
-    }
-
-    /**
      * This method will retrieve one ExternalSourceEntryResource based on the ExternalSource for the given
      * AuthorityName and with the given entryId
      * @param authorityName The authorityName that defines which ExternalDataProvider is used
@@ -75,7 +50,7 @@ public class ExternalSourceRestRepository extends AbstractDSpaceRestRepository {
      * @return              An ExternalSourceEntryRest object that complies with the above params
      */
     public ExternalSourceEntryRest getExternalSourceEntryValue(String authorityName, String entryId) {
-        if (getExternalSource(authorityName) == null) {
+        if (externalDataService.getExternalDataProvider(authorityName) == null) {
             throw new ResourceNotFoundException("The externalSource for: " + authorityName + " couldn't be found");
         }
         Optional<ExternalDataObject> externalDataObject = externalDataService.getExternalDataObject(authorityName,
@@ -96,7 +71,7 @@ public class ExternalSourceRestRepository extends AbstractDSpaceRestRepository {
      */
     public Page<ExternalSourceEntryRest> getExternalSourceEntries(String authorityName, String query, String parent,
                                                                   Pageable pageable) {
-        if (getExternalSource(authorityName) == null) {
+        if (externalDataService.getExternalDataProvider(authorityName) == null) {
             throw new ResourceNotFoundException("The externalSource for: " + authorityName + " couldn't be found");
         }
         List<ExternalDataObject> externalDataObjects = externalDataService
@@ -105,5 +80,28 @@ public class ExternalSourceRestRepository extends AbstractDSpaceRestRepository {
         Page<ExternalSourceEntryRest> page = new PageImpl(externalDataObjects, pageable, numberOfResults)
                                                 .map(externalSourceEntryRestConverter);
         return page;
+    }
+
+    @Override
+    public ExternalSourceRest findOne(Context context, String authorityName) {
+        ExternalDataProvider externalDataProvider = externalDataService.getExternalDataProvider(authorityName);
+        if (externalDataProvider == null) {
+            throw new ResourceNotFoundException("ExternalDataProvider for: " + authorityName + " couldn't be found");
+        }
+        return externalSourceRestConverter.fromModel(externalDataProvider);
+    }
+
+    @Override
+    public Page<ExternalSourceRest> findAll(Context context, Pageable pageable) {
+        List<ExternalDataProvider> externalSources = externalDataService.getExternalDataProviders();
+        Page<ExternalSourceRest> page = utils.getPage(externalSources, pageable).map(externalSourceRestConverter);
+        return page;    }
+
+    public Class<ExternalSourceRest> getDomainClass() {
+        return ExternalSourceRest.class;
+    }
+
+    public ExternalSourceResource wrapResource(ExternalSourceRest model, String... rels) {
+        return new ExternalSourceResource(model, utils, rels);
     }
 }
