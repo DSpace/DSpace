b8671643fbd3ce1eb8a29eaff41a9f09b4126604
Marie Verdonck <maria.verdonck@atmire.com>
2019-12-12 10:42:04 +0100

    67694: checkstyle and licenses

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/converter/WorkflowDefinitionConverter.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/converter/WorkflowDefinitionConverter.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.app.rest.converter;
 
 import org.dspace.app.rest.model.WorkflowDefinitionRest;
@@ -7,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * Converter to translate Workflow to a Workflow Definition
+ *
  * @author Maria Verdonck (Atmire) on 11/12/2019
  */
 @Component
--- dspace-server-webapp/src/main/java/org/dspace/app/rest/model/WorkflowDefinitionRest.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/model/WorkflowDefinitionRest.java
@@ -1,13 +1,18 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.app.rest.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.dspace.app.rest.RestResourceController;
 
-import java.io.Serializable;
-//import org.dspace.app.rest.WorkflowDefinitionController;
-
 /**
  * The rest resource used for workflow definitions
+ *
  * @author Maria Verdonck (Atmire) on 11/12/2019
  */
 public class WorkflowDefinitionRest extends BaseObjectRest<String> {
@@ -27,7 +32,6 @@ public class WorkflowDefinitionRest extends BaseObjectRest<String> {
 
     @Override
     public Class getController() {
-//        return WorkflowDefinitionController.class;
         return RestResourceController.class;
     }
 
