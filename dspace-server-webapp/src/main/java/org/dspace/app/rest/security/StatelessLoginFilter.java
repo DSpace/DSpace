ba9d2095dc4317587ad2d42a57cba9f15e8362ea
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-01-10 10:25:58 +0100

    Revert "Check session salt in shib auth"
    This reverts commit 462f20a3389b6e9641a9af7f6992f3266bce6b45.

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/security/StatelessLoginFilter.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/security/StatelessLoginFilter.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
