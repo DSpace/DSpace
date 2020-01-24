576f699e08af8b95e056c7c81a34120607e6021f
Tim Donohue <tim.donohue@lyrasis.org>
2020-01-24 14:20:31 -0600

    Minor code cleanup / import fixes

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/converter/DiscoverFacetConfigurationConverterTest.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/converter/DiscoverFacetConfigurationConverterTest.java
@@ -21,7 +21,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 /**
  * This class has the purpose to test the DiscoverFacetConfigurationConverter
--- dspace-server-webapp/src/test/java/org/dspace/app/rest/security/jwt/JWTTokenHandlerTest.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/security/jwt/JWTTokenHandlerTest.java
@@ -32,7 +32,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.security.crypto.keygen.StringKeyGenerator;
