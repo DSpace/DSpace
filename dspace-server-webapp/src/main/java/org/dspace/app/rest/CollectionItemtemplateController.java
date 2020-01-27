36f2f0aa9726a7701d573f0923aefc5b6a488b25
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2020-01-27 14:48:35 +0100

    [Task 68059] removed superfluous information and links from templateItem endpoints and added/fixed tests

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/CollectionItemtemplateController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/CollectionItemtemplateController.java
@@ -20,8 +20,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.dspace.app.rest.converter.ConverterService;
 import org.dspace.app.rest.exception.UnprocessableEntityException;
 import org.dspace.app.rest.model.CollectionRest;
-import org.dspace.app.rest.model.ItemRest;
-import org.dspace.app.rest.model.hateoas.ItemResource;
+import org.dspace.app.rest.model.TemplateItemRest;
+import org.dspace.app.rest.model.hateoas.TemplateItemResource;
 import org.dspace.app.rest.repository.CollectionRestRepository;
 import org.dspace.app.rest.utils.ContextUtil;
 import org.dspace.app.rest.utils.Utils;
@@ -112,19 +112,20 @@ public class CollectionItemtemplateController {
         Context context = ContextUtil.obtainContext(request);
         Collection collection = getCollection(context, uuid);
 
-        ItemRest inputItemRest;
+        TemplateItemRest inputTemplateItemRest;
         try {
             ObjectMapper mapper = new ObjectMapper();
-            inputItemRest = mapper.readValue(itemBody.toString(), ItemRest.class);
+            inputTemplateItemRest = mapper.readValue(itemBody.toString(), TemplateItemRest.class);
         } catch (IOException e1) {
             throw new UnprocessableEntityException("Error parsing request body", e1);
         }
 
-        ItemRest templateItem = collectionRestRepository.createTemplateItem(context, collection, inputItemRest);
+        TemplateItemRest templateItem =
+            collectionRestRepository.createTemplateItem(context, collection, inputTemplateItemRest);
         context.commit();
 
-        return ControllerUtils.toResponseEntity(HttpStatus.CREATED, new HttpHeaders(),
-                new ItemResource(templateItem, utils));
+        return ControllerUtils.toResponseEntity(HttpStatus.CREATED, null,
+                converter.toResource(templateItem));
     }
 
     /**
@@ -144,12 +145,12 @@ public class CollectionItemtemplateController {
      */
     @PreAuthorize("hasPermission(#uuid, 'COLLECTION', 'READ')")
     @RequestMapping(method = RequestMethod.GET)
-    public ItemResource getTemplateItem(HttpServletRequest request, @PathVariable UUID uuid)
+    public TemplateItemResource getTemplateItem(HttpServletRequest request, @PathVariable UUID uuid)
             throws SQLException {
 
         Context context = ContextUtil.obtainContext(request);
         Collection collection = getCollection(context, uuid);
-        ItemRest templateItem = collectionRestRepository.getTemplateItem(collection);
+        TemplateItemRest templateItem = collectionRestRepository.getTemplateItem(collection);
 
         return converter.toResource(templateItem);
     }
---
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/TemplateItemRestRepository.java
@@ -0,0 +1,117 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.repository;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.dspace.app.rest.converter.JsonPatchConverter;
+import org.dspace.app.rest.model.ItemRest;
+import org.dspace.app.rest.model.TemplateItemRest;
+import org.dspace.app.rest.model.patch.Patch;
+import org.dspace.app.rest.model.wrapper.TemplateItem;
+import org.dspace.app.rest.projection.Projection;
+import org.dspace.app.rest.repository.patch.ItemPatch;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Collection;
+import org.dspace.content.Item;
+import org.dspace.content.service.CollectionService;
+import org.dspace.content.service.ItemService;
+import org.dspace.core.Context;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component(TemplateItemRest.CATEGORY + "." + TemplateItemRest.NAME)
+public class TemplateItemRestRepository extends DSpaceRestRepository<TemplateItemRest, String> {
+
+    @Autowired
+    private ItemService itemService;
+
+    @Autowired
+    private ItemRestRepository itemRestRepository;
+
+    @Autowired
+    private ItemPatch itemPatch;
+
+    @Autowired
+    private CollectionService collectionService;
+
+    public TemplateItemRest findOne(Context context, String s) {
+        Item item = null;
+        try {
+            item = itemService.find(context, UUID.fromString(s));
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+        if (item == null) {
+            return null;
+        }
+
+        try {
+            return converter.toRest(new TemplateItem(item), Projection.DEFAULT);
+        } catch (IllegalArgumentException e) {
+            throw new ResourceNotFoundException("The item with id " + item.getID() + " is not a template item");
+        }
+    }
+
+    public Page<TemplateItemRest> findAll(Context context, Pageable pageable) {
+        return null;
+    }
+
+    public Class<TemplateItemRest> getDomainClass() {
+        return TemplateItemRest.class;
+    }
+
+    /**
+     * Modify a template Item which is a template Item
+     * @param templateItem          The Item to be modified
+     * @param jsonNode      The patch to be applied
+     * @return The Item as it is after applying the patch
+     * @throws SQLException
+     * @throws AuthorizeException
+     */
+    public TemplateItemRest patchTemplateItem(TemplateItem templateItem, JsonNode jsonNode)
+        throws SQLException, AuthorizeException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonPatchConverter patchConverter = new JsonPatchConverter(mapper);
+        Patch patch = patchConverter.convert(jsonNode);
+
+        ItemRest patchedItemRest =
+            itemPatch.patch(converter.toRest(templateItem.getItem(), Projection.DEFAULT), patch.getOperations());
+        itemRestRepository.updateDSpaceObject(templateItem.getItem(), patchedItemRest);
+
+        return converter.toRest(templateItem, Projection.DEFAULT);
+    }
+
+    /**
+     * Remove an Item which is a template for a Collection.
+     *
+     * Note: The caller is responsible for checking that this item is in fact a template item.
+     *
+     * @param context
+     * @param templateItem          The item to be removed
+     * @throws SQLException
+     * @throws IOException
+     * @throws AuthorizeException
+     */
+    public void removeTemplateItem(Context context, TemplateItem templateItem)
+        throws SQLException, IOException, AuthorizeException {
+
+        Collection collection = templateItem.getItem().getTemplateItemOf();
+        collectionService.removeTemplateItem(context, collection);
+        collectionService.update(context, collection);
+    }
+
+}
