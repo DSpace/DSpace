2c7d65c8b033fe9502d9455a6930ea9490fe9f48
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2020-02-07 15:38:25 +0100

    [Task 68607] added links to the parentCommunity for collections and communities on their respective endpoints

---
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/CommunityParentCommunityLinkRepository.java
@@ -0,0 +1,44 @@
+package org.dspace.app.rest.repository;
+
+import java.sql.SQLException;
+import java.util.UUID;
+import javax.annotation.Nullable;
+import javax.servlet.http.HttpServletRequest;
+
+import org.dspace.app.rest.model.CommunityRest;
+import org.dspace.app.rest.projection.Projection;
+import org.dspace.content.Community;
+import org.dspace.content.service.CommunityService;
+import org.dspace.core.Context;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component(CommunityRest.CATEGORY + "." + CommunityRest.NAME + "." + CommunityRest.PARENT_COMMUNITY)
+public class CommunityParentCommunityLinkRepository extends AbstractDSpaceRestRepository
+    implements LinkRestRepository {
+
+    @Autowired
+    private CommunityService communityService;
+
+    public CommunityRest getParentCommunity(@Nullable HttpServletRequest httpServletRequest,
+                                            UUID communityId,
+                                            @Nullable Pageable optionalPageable,
+                                            Projection projection) {
+        try {
+            Context context = obtainContext();
+            Community community = communityService.find(context, communityId);
+            Community parentCommunity = (Community) communityService.getParentObject(context, community);
+            if (community == null) {
+                throw new ResourceNotFoundException("No such community: " + community);
+            }
+            if (parentCommunity == null) {
+                return null;
+            }
+            return converter.toRest(parentCommunity, projection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
