c71c52bb4634196156604254439c5342b460f286
Mykhaylo Boychuk <mykhaylo.boychuk@4science.it>
2020-02-10 18:06:04 +0100

    DS-4428 enable tests for bad request

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/ResourcePolicyRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/ResourcePolicyRestRepositoryIT.java
@@ -55,7 +55,6 @@ import org.dspace.eperson.EPerson;
 import org.dspace.eperson.Group;
 import org.dspace.eperson.factory.EPersonServiceFactory;
 import org.hamcrest.Matchers;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -371,7 +370,6 @@ public class ResourcePolicyRestRepositoryIT extends AbstractControllerIntegratio
     }
 
     @Test
-    @Ignore("Currently fail due to https://jira.lyrasis.org/browse/DS-4428")
     public void findResoucesPoliciesEPersonWithoutParametersBadRequestTest() throws Exception {
         context.turnOffAuthorisationSystem();
 
@@ -620,7 +618,6 @@ public class ResourcePolicyRestRepositoryIT extends AbstractControllerIntegratio
   }
 
   @Test
-  @Ignore("Currently fail due to https://jira.lyrasis.org/browse/DS-4428")
   public void findResoucesPoliciesOfResourceWithoutParametersBadRequestTest() throws Exception {
       context.turnOffAuthorisationSystem();
 
@@ -831,7 +828,6 @@ public class ResourcePolicyRestRepositoryIT extends AbstractControllerIntegratio
     }
 
     @Test
-    @Ignore("Currently fail due to https://jira.lyrasis.org/browse/DS-4428")
     public void findResoucesPoliciesByGroupWithoutParametersBadRequestTest() throws Exception {
         context.turnOffAuthorisationSystem();
 
