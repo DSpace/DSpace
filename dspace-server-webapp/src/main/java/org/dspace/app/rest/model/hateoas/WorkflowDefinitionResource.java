207f7455125e8ac51685add70a10ce8683cbd7c1
Marie Verdonck <maria.verdonck@atmire.com>
2020-02-12 18:29:08 +0100

    [DS-2646]: test and checkstyle fixes

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/model/hateoas/WorkflowDefinitionResource.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/model/hateoas/WorkflowDefinitionResource.java
@@ -11,8 +11,6 @@ import org.dspace.app.rest.model.WorkflowDefinitionRest;
 import org.dspace.app.rest.model.hateoas.annotations.RelNameDSpaceResource;
 import org.dspace.app.rest.utils.Utils;
 
-import javax.ws.rs.Path;
-
 /**
  * WorkflowDefinition Rest HAL Resource. The HAL Resource wraps the REST Resource
  * adding support for the links and embedded resources
