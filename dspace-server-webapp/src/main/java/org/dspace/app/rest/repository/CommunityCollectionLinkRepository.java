2f48b1c7203023e02ae81d9d132e1c4be843be49
Mykhaylo Boychuk <mykhaylo.boychuk@4science.it>
2020-02-25 18:32:32 +0100

    added check to prove that community/collection are public

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/CommunityCollectionLinkRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/CommunityCollectionLinkRepository.java
@@ -8,6 +8,7 @@
 package org.dspace.app.rest.repository;
 
 import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 import javax.annotation.Nullable;
@@ -16,9 +17,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.dspace.app.rest.model.CollectionRest;
 import org.dspace.app.rest.model.CommunityRest;
 import org.dspace.app.rest.projection.Projection;
+import org.dspace.authorize.service.AuthorizeService;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
 import org.dspace.content.service.CommunityService;
+import org.dspace.core.Constants;
 import org.dspace.core.Context;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -37,6 +40,9 @@ public class CommunityCollectionLinkRepository extends AbstractDSpaceRestReposit
     @Autowired
     CommunityService communityService;
 
+    @Autowired
+    AuthorizeService authorizeService;
+
     @PreAuthorize("hasPermission(#communityId, 'COMMUNITY', 'READ')")
     public Page<CollectionRest> getCollections(@Nullable HttpServletRequest request,
                                                UUID communityId,
@@ -49,7 +55,13 @@ public class CommunityCollectionLinkRepository extends AbstractDSpaceRestReposit
                 throw new ResourceNotFoundException("No such community: " + communityId);
             }
             List<Collection> collections = community.getCollections();
-            return converter.toRestPage(utils.getPage(collections, optionalPageable), projection);
+            List<Collection> publicCollection = new LinkedList<Collection>();
+            for (Collection c : collections ) {
+                if (authorizeService.authorizeActionBoolean(context, c, Constants.READ)) {
+                    publicCollection.add(c);
+                }
+            }
+            return converter.toRestPage(utils.getPage(publicCollection, optionalPageable), projection);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/CommunitySubcommunityLinkRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/CommunitySubcommunityLinkRepository.java
@@ -8,6 +8,7 @@
 package org.dspace.app.rest.repository;
 
 import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 import javax.annotation.Nullable;
@@ -15,8 +16,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.dspace.app.rest.model.CommunityRest;
 import org.dspace.app.rest.projection.Projection;
+import org.dspace.authorize.service.AuthorizeService;
 import org.dspace.content.Community;
 import org.dspace.content.service.CommunityService;
+import org.dspace.core.Constants;
 import org.dspace.core.Context;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -35,6 +38,9 @@ public class CommunitySubcommunityLinkRepository extends AbstractDSpaceRestRepos
     @Autowired
     CommunityService communityService;
 
+    @Autowired
+    AuthorizeService authorizeService;
+
     @PreAuthorize("hasPermission(#communityId, 'COMMUNITY', 'READ')")
     public Page<CommunityRest> getSubcommunities(@Nullable HttpServletRequest request,
                                                  UUID communityId,
@@ -46,8 +52,14 @@ public class CommunitySubcommunityLinkRepository extends AbstractDSpaceRestRepos
             if (community == null) {
                 throw new ResourceNotFoundException("No such community: " + communityId);
             }
-            List<Community> subcommunities = community.getSubcommunities();
-            return converter.toRestPage(utils.getPage(subcommunities, optionalPageable), projection);
+            List<Community> allSubcommunities = community.getSubcommunities();
+            List<Community> publicSubcommunities = new LinkedList<Community>();
+            for (Community subCommunity : allSubcommunities ) {
+                if (authorizeService.authorizeActionBoolean(context, subCommunity, Constants.READ)) {
+                    publicSubcommunities.add(subCommunity);
+                }
+            }
+            return converter.toRestPage(utils.getPage(publicSubcommunities, optionalPageable), projection);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
