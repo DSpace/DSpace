18a10fe2a0dfbd8da55fe8e676ab39a175db4967
April Herron <christopher.herron@atmire.com>
2020-02-27 12:12:41 -0500

    DS-4442 Ensure underlying s3object is closed when S3InputStream is closed

---
+++ dspace-api/src/main/java/org/dspace/storage/bitstore/S3FilterInputStream.java
@@ -0,0 +1,36 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+
+package org.dspace.storage.bitstore;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+
+import com.amazonaws.services.s3.model.S3Object;
+
+/**
+ * Simple wrapper for S3 InputStreams which properly closes the underlying S3Object when the stream is closed.
+ */
+public class S3FilterInputStream extends FilterInputStream {
+
+    private S3Object s3Object;
+
+    protected S3FilterInputStream(S3Object object) {
+        super(object.getObjectContent());
+        s3Object = object;
+    }
+
+    @Override
+    public void close() throws IOException {
+        super.close();
+        if (s3Object != null) {
+            s3Object.close();
+        }
+    }
+
+}
