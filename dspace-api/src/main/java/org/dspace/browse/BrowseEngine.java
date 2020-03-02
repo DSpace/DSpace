031dbb3956c464c9740fedbd9e5e21f375baf8fc
Toni Prieto <antonio.juan.prieto@upc.edu>
2020-03-02 17:09:06 +0100

    Add StripDiacritics to OrderFormat classes for author and text and normalize the starts_with param text submitted

--- dspace-api/src/main/java/org/dspace/browse/BrowseEngine.java
+++ dspace-api/src/main/java/org/dspace/browse/BrowseEngine.java
@@ -381,7 +381,7 @@ public class BrowseEngine {
             // this is the distinct table constrained to either community or collection
             dao.setTable(browseIndex.getDistinctTableName());
 
-            dao.setStartsWith(StringUtils.lowerCase(scope.getStartsWith()));
+            dao.setStartsWith(normalizeJumpToValue(scope.getStartsWith()));
             // remind the DAO that this is a distinct value browse, so it knows what sort
             // of query to build
             dao.setDistinct(true);
