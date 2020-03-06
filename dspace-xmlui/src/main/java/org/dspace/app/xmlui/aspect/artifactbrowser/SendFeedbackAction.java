52c84c31aada1c39d68f3afac3c299c75a5d3f6b
Saiful Amin <saiful-semantic@users.noreply.github.com>
2020-03-06 11:12:49 +0530

    Added braces in if statement

--- dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/SendFeedbackAction.java
+++ dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/SendFeedbackAction.java
@@ -65,7 +65,9 @@ public class SendFeedbackAction extends AbstractAction
             // arrives at the installation; e.g. presence or absence of "www"
             int lastDot = host.lastIndexOf('.');
             if (host.substring(0, lastDot).contains("."))
+            {
                 basicHost = host.substring(host.substring(0, lastDot).lastIndexOf('.'));
+            }
         }
 
         if ((fromPage == null) || ((!fromPage.contains(basicHost)) && (!isValidReferral(fromPage))))
