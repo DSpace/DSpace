ad7dc2022e621239c7406ae5ecdd44a7a8d1d753
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2020-04-06 09:09:59 +0200

    [DS-4443] moved test to dspace-api. Succeeds in a single run, fails in full test run

--- dspace-api/src/test/java/org/dspace/discovery/MockSolrSearchCore.java
+++ dspace-api/src/test/java/org/dspace/discovery/MockSolrSearchCore.java
@@ -7,19 +7,38 @@
  */
 package org.dspace.discovery;
 
+import org.dspace.solr.MockSolrServer;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 
 /**
- * Mock SOLR service for the Search Core
+ * Mock SOLR service for the Search Core.
+ *
+ * <p>
+ * <strong>NOTE:</strong>  this class overrides one <em>of the same name</em>
+ * defined in dspace-api and declared as a bean there.
+ * See {@code test/data/dspaceFolder/config/spring/api/solr-services.xml}.  Some kind of classpath
+ * magic makes this work.
  */
 @Service
-public class MockSolrSearchCore extends SolrSearchCore implements InitializingBean {
+public class MockSolrSearchCore extends SolrSearchCore implements InitializingBean, DisposableBean {
+
+    private MockSolrServer mockSolrServer;
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        //We don't use SOLR in the tests of this module
-        solr = null;
+        mockSolrServer = new MockSolrServer("search");
+        solr = mockSolrServer.getSolrServer();
     }
 
+    /** Clear all records from the search core. */
+    public void reset() {
+        mockSolrServer.reset();
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        mockSolrServer.destroy();
+    }
 }
