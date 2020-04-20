cb8b5c8efc3baaa535b1545f31313c8a3364e02f
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2020-04-20 11:31:34 +0200

    [Task 70399] created the POST EPerson endpoint with token and added tests

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/EPersonRestRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/EPersonRestRepository.java
@@ -15,17 +15,29 @@ import javax.servlet.http.HttpServletRequest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.dspace.app.rest.Parameter;
 import org.dspace.app.rest.SearchRestMethod;
+import org.dspace.app.rest.authorization.AuthorizationFeature;
+import org.dspace.app.rest.authorization.AuthorizationFeatureService;
 import org.dspace.app.rest.exception.UnprocessableEntityException;
 import org.dspace.app.rest.model.EPersonRest;
+import org.dspace.app.rest.model.MetadataRest;
+import org.dspace.app.rest.model.MetadataValueRest;
+import org.dspace.app.rest.model.SiteRest;
 import org.dspace.app.rest.model.patch.Operation;
 import org.dspace.app.rest.model.patch.Patch;
+import org.dspace.app.rest.projection.Projection;
 import org.dspace.authorize.AuthorizeException;
 import org.dspace.authorize.service.AuthorizeService;
+import org.dspace.content.Site;
+import org.dspace.content.service.SiteService;
 import org.dspace.core.Context;
 import org.dspace.eperson.EPerson;
+import org.dspace.eperson.RegistrationData;
+import org.dspace.eperson.service.AccountService;
 import org.dspace.eperson.service.EPersonService;
+import org.dspace.eperson.service.RegistrationDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -43,8 +55,22 @@ import org.springframework.stereotype.Component;
 @Component(EPersonRest.CATEGORY + "." + EPersonRest.NAME)
 public class EPersonRestRepository extends DSpaceObjectRestRepository<EPerson, EPersonRest> {
 
+    private static final Logger log = Logger.getLogger(EPersonRestRepository.class);
+
     @Autowired
     AuthorizeService authorizeService;
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private AuthorizationFeatureService authorizationFeatureService;
+
+    @Autowired
+    private SiteService siteService;
+
+    @Autowired
+    private RegistrationDataService registrationDataService;
 
     private final EPersonService es;
 
@@ -66,7 +92,21 @@ public class EPersonRestRepository extends DSpaceObjectRestRepository<EPerson, E
         } catch (IOException e1) {
             throw new UnprocessableEntityException("error parsing the body... maybe this is not the right error code");
         }
+        String token = req.getParameter("token");
+        if (StringUtils.isNotBlank(token)) {
+            try {
+                return createAndReturn(context, epersonRest, token);
+            } catch (SQLException e) {
+                log.error(e.getMessage(), e);
+                throw new RuntimeException("Something with wrong in the creation of an EPerson with token: " + token);
+            }
+        }
+        EPerson eperson = createEPersonFromRestObject(context, epersonRest);
 
+        return converter.toRest(eperson, utils.obtainProjection());
+    }
+
+    private EPerson createEPersonFromRestObject(Context context, EPersonRest epersonRest) throws AuthorizeException {
         EPerson eperson = null;
         try {
             eperson = es.create(context);
@@ -84,8 +124,61 @@ public class EPersonRestRepository extends DSpaceObjectRestRepository<EPerson, E
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage(), e);
         }
+        return eperson;
+    }
 
-        return converter.toRest(eperson, utils.obtainProjection());
+    private EPersonRest createAndReturn(Context context, EPersonRest epersonRest, String token)
+        throws AuthorizeException, SQLException {
+        RegistrationData registrationData = registrationDataService.findByToken(context, token);
+        if (registrationData == null) {
+            throw new AccessDeniedException("The token given as parameter: " + token + " does not exist" +
+                                                " in the database");
+        }
+        if (es.findByEmail(context, registrationData.getEmail()) != null) {
+            throw new AccessDeniedException("The token given already contains an email address that resolves" +
+                                                "to an eperson");
+        }
+        String emailFromJson = epersonRest.getEmail();
+        if (StringUtils.isNotBlank(emailFromJson)) {
+            if (!StringUtils.equalsIgnoreCase(registrationData.getEmail(), emailFromJson)) {
+                throw new AccessDeniedException("The email resulting from the token does not match the email given" +
+                                                    " in the json body. Email from token: " +
+                                                    registrationData.getEmail() + " email from the json body: "
+                                                    + emailFromJson);
+            }
+        }
+        if (epersonRest.isSelfRegistered() != null && !epersonRest.isSelfRegistered()) {
+            throw new AccessDeniedException("The self registered property cannot be set to false using this method" +
+                                                " with a token");
+        }
+        checkRequiredProperties(epersonRest);
+        AuthorizationFeature epersonRegistration = authorizationFeatureService.find("epersonRegistration");
+        Site site = siteService.findSite(context);
+        SiteRest siteRest = converter.toRest(site, Projection.DEFAULT);
+        if (!authorizationFeatureService.isAuthorized(context, epersonRegistration, siteRest)) {
+            throw new AccessDeniedException(
+                "Registration is disabled, you are not authorized to create a new Authorization");
+        }
+        EPerson ePerson = createEPersonFromRestObject(context, epersonRest);
+        accountService.deleteToken(context, token);
+        return converter.toRest(ePerson, utils.obtainProjection());
+    }
+
+    private void checkRequiredProperties(EPersonRest epersonRest) {
+        MetadataRest metadataRest = epersonRest.getMetadata();
+        if (metadataRest != null) {
+            List<MetadataValueRest> epersonFirstName = metadataRest.getMap().get("eperson.firstname");
+            List<MetadataValueRest> epersonLastName = metadataRest.getMap().get("eperson.lastname");
+            if (epersonFirstName == null || epersonLastName == null ||
+                epersonFirstName.isEmpty() || epersonLastName.isEmpty()) {
+                throw new AccessDeniedException("The eperson.firstname and eperson.lastname values need to be " +
+                                                    "filled in");
+            }
+        }
+        String password = epersonRest.getPassword();
+        if (StringUtils.isBlank(password)) {
+            throw new AccessDeniedException("the password cannot be left blank");
+        }
     }
 
     @Override
