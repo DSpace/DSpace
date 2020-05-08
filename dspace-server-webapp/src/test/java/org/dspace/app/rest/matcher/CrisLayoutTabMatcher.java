00d34797a5b58e3c7453cdf4a3ae37b28f1e8205
Danilo Di Nuzzo <dnl.dinuzzo@gmail.com>
2020-05-08 21:41:35 +0200

    Builders for tests

---
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/matcher/CrisLayoutTabMatcher.java
@@ -0,0 +1,31 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.matcher;
+
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import org.dspace.layout.CrisLayoutTab;
+import org.hamcrest.Matcher;
+
+public class CrisLayoutTabMatcher {
+
+    private CrisLayoutTabMatcher() {}
+
+    public static Matcher<? super Object> matchTab(CrisLayoutTab tab) {
+        return allOf(
+                hasJsonPath("$.id", is(tab.getID())),
+                hasJsonPath("$.shortname", is(tab.getShortName())),
+                hasJsonPath("$.header", is(tab.getHeader())),
+                hasJsonPath("$.priority", is(tab.getPriority())),
+                hasJsonPath("$.security", is(tab.getSecurity()))
+        );
+    }
+
+}
