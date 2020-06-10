56629fd34e82b9a29ada8b65a7b1a41557b99f7f
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2020-06-10 11:53:24 +0200

    [Task 71335] removed wrongly added preAuthorize on bitstreamFormatRestRepository#findAll

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/BitstreamFormatRestRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/BitstreamFormatRestRepository.java
@@ -57,7 +57,6 @@ public class BitstreamFormatRestRepository extends DSpaceRestRepository<Bitstrea
     }
 
     @Override
-    @PreAuthorize("permitAll()")
     public Page<BitstreamFormatRest> findAll(Context context, Pageable pageable) {
         try {
             List<BitstreamFormat> bit = bitstreamFormatService.findAll(context);
