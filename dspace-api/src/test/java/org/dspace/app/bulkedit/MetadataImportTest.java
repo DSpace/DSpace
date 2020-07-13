ec00b59ba01c6bdcdc1818e748b20747e467455f
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2020-07-13 11:48:21 +0200

    [Task 71780] added support for the person schema in the csv import and added a test for it

--- dspace-api/src/test/java/org/dspace/app/bulkedit/MetadataImportTest.java
+++ dspace-api/src/test/java/org/dspace/app/bulkedit/MetadataImportTest.java
@@ -10,20 +10,23 @@ package org.dspace.app.bulkedit;
 import static junit.framework.TestCase.assertTrue;
 
 import java.io.File;
+import java.sql.SQLException;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.dspace.AbstractIntegrationTest;
 import org.dspace.app.launcher.ScriptLauncher;
 import org.dspace.app.scripts.handler.impl.TestDSpaceRunnableHandler;
+import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
 import org.dspace.content.Item;
+import org.dspace.content.MetadataValue;
 import org.dspace.content.factory.ContentServiceFactory;
 import org.dspace.content.service.CollectionService;
 import org.dspace.content.service.CommunityService;
 import org.dspace.content.service.ItemService;
-import org.dspace.services.ConfigurationService;
-import org.dspace.services.factory.DSpaceServicesFactory;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MetadataImportTest extends AbstractIntegrationTest {
@@ -31,16 +34,35 @@ public class MetadataImportTest extends AbstractIntegrationTest {
     private ItemService itemService = ContentServiceFactory.getInstance().getItemService();
     private CollectionService collectionService = ContentServiceFactory.getInstance().getCollectionService();
     private CommunityService communityService = ContentServiceFactory.getInstance().getCommunityService();
-    private ConfigurationService configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
 
-    @Test
-    public void metadataImportTest() throws Exception {
+    @Before
+    public void setup() throws SQLException, AuthorizeException {
         context.turnOffAuthorisationSystem();
         Community community = communityService.create(null, context);
         Collection collection = collectionService.create(context, community);
         context.restoreAuthSystemState();
-
+    }
+    @Test
+    public void metadataImportTest() throws Exception {
         String fileLocation = new File(testProps.get("test.importcsv").toString()).getAbsolutePath();
+        String[] args = new String[] {"metadata-import", "-f", fileLocation, "-e", eperson.getEmail(), "-s"};
+        TestDSpaceRunnableHandler testDSpaceRunnableHandler = new TestDSpaceRunnableHandler();
+
+        ScriptLauncher.handleScript(args, ScriptLauncher.getConfig(kernelImpl), testDSpaceRunnableHandler, kernelImpl);
+        Item importedItem = itemService.findAll(context).next();
+        List<MetadataValue> metadata = itemService.getMetadata(importedItem, "dc", "contributor", "author", Item.ANY);
+        assertTrue(StringUtils.equals(metadata.get(0).getValue(), "Donald, SmithImported"));
+        context.turnOffAuthorisationSystem();
+        itemService.delete(context, itemService.find(context, importedItem.getID()));
+        context.commit();
+        context.restoreAuthSystemState();
+
+    }
+
+    @Test
+    public void personMetadataImportTest() throws Exception {
+
+        String fileLocation = new File(testProps.get("test.importpersoncsv").toString()).getAbsolutePath();
         String[] args = new String[] {"metadata-import", "-f", fileLocation, "-e", eperson.getEmail(), "-s"};
         TestDSpaceRunnableHandler testDSpaceRunnableHandler = new TestDSpaceRunnableHandler();
 
@@ -48,13 +70,11 @@ public class MetadataImportTest extends AbstractIntegrationTest {
         Item importedItem = itemService.findAll(context).next();
         assertTrue(
             StringUtils.equals(
-                itemService.getMetadata(importedItem, "dc", "contributor", "author", Item.ANY).get(0).getValue(),
-                "Donald, SmithImported"));
-
+                itemService.getMetadata(importedItem, "person", "birthDate", null, Item.ANY)
+                           .get(0).getValue(), "2000"));
         context.turnOffAuthorisationSystem();
         itemService.delete(context, itemService.find(context, importedItem.getID()));
-        collectionService.delete(context, collectionService.find(context, collection.getID()));
-        communityService.delete(context, communityService.find(context, community.getID()));
+        context.commit();
         context.restoreAuthSystemState();
     }
 }
