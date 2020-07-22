7501170b101b9484dff6ed89f41b1e0cd5a5f86c
Mark H. Wood <mwood@iu.edu>
2020-07-22 12:02:15 -0400

    Fix bad LGTM default that suppresses test JARs.

---
+++ lgtm-build.sh
@@ -0,0 +1,13 @@
+/opt/dist/tools/preload_tracer /opt/out/snapshot/workspace/apache-maven-3.6.0/bin/mvn clean package \
+	-f  pom.xml \
+	-B -V -e \
+	-Dfindbugs.skip \
+	-Dcheckstyle.skip \
+	-Dpmd.skip=true \
+	-Denforcer.skip \
+	-Dmaven.javadoc.skip \
+	-DskipTests \
+	-Dlicense.skip=true \
+	-Drat.skip=true \
+	-Dmaven.repo.local=/opt/work/semmle_data/maven_repo \
+	-t /opt/work/.m2/toolchains.xml
