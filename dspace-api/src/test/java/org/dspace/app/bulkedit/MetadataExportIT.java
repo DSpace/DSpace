33cfd49ba52a18cb0980cad3368158153a3844a1
Mark H. Wood <mwood@iu.edu>
2020-07-29 12:44:09 -0400

    #2893 More proper use of Builders in demo test suite.

--- dspace-api/src/test/java/org/dspace/app/bulkedit/MetadataExportIT.java
+++ dspace-api/src/test/java/org/dspace/app/bulkedit/MetadataExportIT.java
@@ -19,36 +19,29 @@ import org.dspace.app.launcher.ScriptLauncher;
 import org.dspace.app.scripts.handler.impl.TestDSpaceRunnableHandler;
 import org.dspace.builder.CollectionBuilder;
 import org.dspace.builder.CommunityBuilder;
-import org.dspace.builder.WorkspaceItemBuilder;
+import org.dspace.builder.ItemBuilder;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
 import org.dspace.content.Item;
-import org.dspace.content.WorkspaceItem;
-import org.dspace.content.factory.ContentServiceFactory;
-import org.dspace.content.service.InstallItemService;
-import org.dspace.content.service.ItemService;
 import org.dspace.services.ConfigurationService;
 import org.dspace.services.factory.DSpaceServicesFactory;
 import org.junit.Test;
 
 public class MetadataExportIT
         extends AbstractIntegrationTestWithDatabase {
-    private final ItemService itemService
-            = ContentServiceFactory.getInstance().getItemService();
-    private final InstallItemService installItemService
-            = ContentServiceFactory.getInstance().getInstallItemService();
     private final ConfigurationService configurationService
             = DSpaceServicesFactory.getInstance().getConfigurationService();
 
     @Test
     public void metadataExportToCsvTest() throws Exception {
         context.turnOffAuthorisationSystem();
-        Community community = CommunityBuilder.createCommunity(context).build();
-        Collection collection = CollectionBuilder.createCollection(context, community).build();
-        WorkspaceItem wi = WorkspaceItemBuilder.createWorkspaceItem(context, collection).build();
-        Item item = wi.getItem();
-        itemService.addMetadata(context, item, "dc", "contributor", "author", null, "Donald, Smith");
-        item = installItemService.installItem(context, wi);
+        Community community = CommunityBuilder.createCommunity(context)
+                .build();
+        Collection collection = CollectionBuilder.createCollection(context, community)
+                .build();
+        Item item = ItemBuilder.createItem(context, collection)
+                .withAuthor("Donald, Smith")
+                .build();
         context.restoreAuthSystemState();
         String fileLocation = configurationService.getProperty("dspace.dir")
                 + testProps.get("test.exportcsv").toString();
@@ -65,9 +58,5 @@ public class MetadataExportIT
         String fileContent = IOUtils.toString(new FileInputStream(file), StandardCharsets.UTF_8);
         assertTrue(fileContent.contains("Donald, Smith"));
         assertTrue(fileContent.contains(String.valueOf(item.getID())));
-
-        context.turnOffAuthorisationSystem();
-        itemService.delete(context, itemService.find(context, item.getID()));
-        context.restoreAuthSystemState();
     }
 }
