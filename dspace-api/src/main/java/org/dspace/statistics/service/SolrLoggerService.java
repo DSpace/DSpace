ebcd1fc6cf65a5111991ec78619e6d13dc238000
Samuel <samuelcambien@gmail.com>
2020-09-30 18:11:33 +0200

    DS-4440 GDPR - Anonymize statistics feature - add typedocs

--- dspace-api/src/main/java/org/dspace/statistics/service/SolrLoggerService.java
+++ dspace-api/src/main/java/org/dspace/statistics/service/SolrLoggerService.java
@@ -118,6 +118,19 @@ public interface SolrLoggerService {
                        List<String> fieldNames, List<List<Object>> fieldValuesList)
         throws SolrServerException, IOException;
 
+    /**
+     * Update the solr core.
+     * @param query
+     *      query indicating which documents to update
+     * @param action
+     *      the update action keyword
+     * @param fieldNames
+     *      the fields to update
+     * @param fieldValuesList
+     *      the values for the fields to update
+     * @param commit
+     *      whether to commit the changes
+     */
     public void update(String query, String action,
                        List<String> fieldNames, List<List<Object>> fieldValuesList, boolean commit)
             throws SolrServerException, IOException;
@@ -178,18 +191,84 @@ public interface SolrLoggerService {
     public ObjectCount queryTotal(String query, String filterQuery, int facetMinCount)
         throws SolrServerException, IOException;
 
+    /**
+     * Perform a solr query.
+     *
+     * @param query         the query to be used
+     * @param filterQuery   filter query
+     * @param facetField    field to facet the results by
+     * @param rows          the max number of results to return
+     * @param max           the max number of facets to return
+     * @param dateType      the type to be used (example: DAY, MONTH, YEAR)
+     * @param dateStart     the start date Format:(-3, -2, ..) the date is calculated
+     *                      relatively on today
+     * @param dateEnd       the end date stop Format (-2, +1, ..) the date is calculated
+     *                      relatively on today
+     * @param facetQueries  list of facet queries
+     * @param sort          the sort field
+     * @param ascending     the sort direction (true: ascending)
+     * @param facetMinCount Minimum count of results facet must have to return a result
+     * @throws SolrServerException Exception from the Solr server to the solrj Java client.
+     * @throws java.io.IOException passed through.
+     */
     public QueryResponse query(String query, String filterQuery,
                                String facetField, int rows, int max, String dateType, String dateStart,
                                String dateEnd, List<String> facetQueries, String sort, boolean ascending,
                                int facetMinCount)
         throws SolrServerException, IOException;
 
+    /**
+     * Perform a solr query.
+     *
+     * @param query         the query to be used
+     * @param filterQuery   filter query
+     * @param facetField    field to facet the results by
+     * @param rows          the max number of results to return
+     * @param max           the max number of facets to return
+     * @param dateType      the type to be used (example: DAY, MONTH, YEAR)
+     * @param dateStart     the start date Format:(-3, -2, ..) the date is calculated
+     *                      relatively on today
+     * @param dateEnd       the end date stop Format (-2, +1, ..) the date is calculated
+     *                      relatively on today
+     * @param facetQueries  list of facet queries
+     * @param sort          the sort field
+     * @param ascending     the sort direction (true: ascending)
+     * @param facetMinCount Minimum count of results facet must have to return a result
+     * @param defaultFilterQueries
+     *                      use the default filter queries
+     * @throws SolrServerException Exception from the Solr server to the solrj Java client.
+     * @throws java.io.IOException passed through.
+     */
     public QueryResponse query(String query, String filterQuery,
                                String facetField, int rows, int max, String dateType, String dateStart,
                                String dateEnd, List<String> facetQueries, String sort, boolean ascending,
                                int facetMinCount, boolean defaultFilterQueries)
             throws SolrServerException, IOException;
 
+    /**
+     * Perform a solr query.
+     *
+     * @param query         the query to be used
+     * @param filterQuery   filter query
+     * @param facetField    field to facet the results by
+     * @param rows          the max number of results to return
+     * @param max           the max number of facets to return
+     * @param dateType      the type to be used (example: DAY, MONTH, YEAR)
+     * @param dateStart     the start date Format:(-3, -2, ..) the date is calculated
+     *                      relatively on today
+     * @param dateEnd       the end date stop Format (-2, +1, ..) the date is calculated
+     *                      relatively on today
+     * @param facetQueries  list of facet queries
+     * @param sort          the sort field
+     * @param ascending     the sort direction (true: ascending)
+     * @param facetMinCount Minimum count of results facet must have to return a result
+     * @param defaultFilterQueries
+     *                      use the default filter queries
+     * @param includeShardField
+     *                      include the shard field in the result documents
+     * @throws SolrServerException Exception from the Solr server to the solrj Java client.
+     * @throws java.io.IOException passed through.
+     */
     public QueryResponse query(String query, String filterQuery,
                                String facetField, int rows, int max, String dateType, String dateStart,
                                String dateEnd, List<String> facetQueries, String sort, boolean ascending,
@@ -221,10 +300,23 @@ public interface SolrLoggerService {
      */
     public void exportHits() throws Exception;
 
+    /**
+     * Commit the solr core.
+     */
     public void commit() throws Exception;
 
+    /**
+     * Commit a solr shard.
+     * @param shard
+     *      The shard to commit.
+     */
     public void commitShard(String shard) throws Exception;
 
+    /**
+     * Anonymize a given ip
+     * @param ip
+     *      The ip to anonymize.
+     */
     public Object anonymizeIp(String ip) throws UnknownHostException;
 
 }
