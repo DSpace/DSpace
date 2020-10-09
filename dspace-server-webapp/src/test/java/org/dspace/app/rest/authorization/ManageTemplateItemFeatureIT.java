97a9fd7e6222984aa3ffd23d74b76aaa14de6776
Peter Nijs <peter.nijs@atmire.com>
2020-10-09 16:58:38 +0200

    73168: Edit Collection Permissions in REST Feature Integration Tests - feedback

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/authorization/ManageTemplateItemFeatureIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/authorization/ManageTemplateItemFeatureIT.java
@@ -13,7 +13,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.dspace.app.rest.converter.CollectionConverter;
+import org.dspace.app.rest.converter.CommunityConverter;
+import org.dspace.app.rest.model.CollectionRest;
+import org.dspace.app.rest.model.CommunityRest;
+import org.dspace.app.rest.projection.Projection;
 import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
+import org.dspace.app.rest.utils.Utils;
 import org.dspace.authorize.ResourcePolicy;
 import org.dspace.builder.CollectionBuilder;
 import org.dspace.builder.CommunityBuilder;
@@ -33,11 +39,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class ManageTemplateItemFeatureIT extends AbstractControllerIntegrationTest {
 
     @Autowired
-    SiteService siteService;
+    private SiteService siteService;
+
+    @Autowired
+    private Utils utils;
+
+    @Autowired
+    private CommunityConverter communityConverter;
+
+    @Autowired
+    private CollectionConverter collectionConverter;
 
     private Site site;
     private Community communityA;
+    private CommunityRest communityARest;
     private Collection collectionA;
+    private CollectionRest collectionARest;
+    private String adminToken;
+    private String epersonToken;
+
+    final String feature = "canManageTemplateItem";
 
     @Override
     @Before
@@ -54,82 +75,97 @@ public class ManageTemplateItemFeatureIT extends AbstractControllerIntegrationTe
             .build();
 
         context.restoreAuthSystemState();
+
+        communityARest = communityConverter.convert(communityA, Projection.DEFAULT);
+        collectionARest = collectionConverter.convert(collectionA, Projection.DEFAULT);
+
+        adminToken = getAuthToken(admin.getEmail(), password);
+        epersonToken = getAuthToken(eperson.getEmail(), password);
     }
 
     @Test
-    public void adminTests() throws Exception {
-        String adminToken = getAuthToken(admin.getEmail(), password);
-        final String feature = "canManageTemplateItem";
-
+    public void adminCollectionTestSuccess() throws Exception {
         getClient(adminToken).perform(
             get("/api/authz/authorizations/search/object")
                 .param("embed", "feature")
                 .param("feature", feature)
-                .param("uri", "http://localhost/api/core/collections/" + collectionA.getID()))
+                .param("uri", utils.linkToSingleResource(collectionARest, "self").getHref()))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.page.totalElements", greaterThan(0)))
             .andExpect(jsonPath("$._embedded").exists());
+    }
 
+    @Test
+    public void adminCommunityTestNotFound() throws Exception {
         getClient(adminToken).perform(
             get("/api/authz/authorizations/search/object")
                 .param("embed", "feature")
                 .param("feature", feature)
-                .param("uri", "http://localhost/api/core/communities/" + communityA.getID()))
+                .param("uri", utils.linkToSingleResource(communityARest, "self").getHref()))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.page.totalElements", is(0)))
             .andExpect(jsonPath("$._embedded").doesNotExist());
     }
 
     @Test
-    public void anonymousTests() throws Exception {
-        final String feature = "canManageTemplateItem";
-
+    public void anonymousCollectionTestNotFound() throws Exception {
         getClient().perform(
             get("/api/authz/authorizations/search/object")
                 .param("embed", "feature")
                 .param("feature", feature)
-                .param("uri", "http://localhost/api/core/collections/" + collectionA.getID()))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.page.totalElements", is(0)))
-            .andExpect(jsonPath("$._embedded").doesNotExist());
-
-        getClient().perform(
-            get("/api/authz/authorizations/search/object")
-                .param("embed", "feature")
-                .param("feature", feature)
-                .param("uri", "http://localhost/api/core/communities/" + communityA.getID()))
+                .param("uri", utils.linkToSingleResource(collectionARest, "self").getHref()))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.page.totalElements", is(0)))
             .andExpect(jsonPath("$._embedded").doesNotExist());
     }
 
     @Test
-    public void collectionAdminTests() throws Exception {
+    public void anonymousCommunityTestNotFound() throws Exception {
+        getClient().perform(
+            get("/api/authz/authorizations/search/object")
+                .param("embed", "feature")
+                .param("feature", feature)
+                .param("uri", utils.linkToSingleResource(communityARest, "self").getHref()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.page.totalElements", is(0)))
+            .andExpect(jsonPath("$._embedded").doesNotExist());
+    }
+
+    @Test
+    public void collectionAdminCollectionTestSuccess() throws Exception {
         context.turnOffAuthorisationSystem();
-        ResourcePolicy removePermission = ResourcePolicyBuilder.createResourcePolicy(context)
+        ResourcePolicy adminPermissionForEPersonOnCollectionA = ResourcePolicyBuilder.createResourcePolicy(context)
             .withDspaceObject(collectionA)
             .withAction(Constants.ADMIN)
             .withUser(eperson)
             .build();
         context.restoreAuthSystemState();
 
-        String adminToken = getAuthToken(eperson.getEmail(), password);
-        final String feature = "canManageTemplateItem";
-
-        getClient(adminToken).perform(
+        getClient(epersonToken).perform(
             get("/api/authz/authorizations/search/object")
                 .param("embed", "feature")
                 .param("feature", feature)
-                .param("uri", "http://localhost/api/core/collections/" + collectionA.getID()))
+                .param("uri", utils.linkToSingleResource(collectionARest, "self").getHref()))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.page.totalElements", greaterThan(0)))
             .andExpect(jsonPath("$._embedded").exists());
+    }
 
-        getClient(adminToken).perform(
+    @Test
+    public void collectionAdminCommunityTestNotFound() throws Exception {
+        context.turnOffAuthorisationSystem();
+        ResourcePolicy adminPermissionForEPersonOnCollectionA = ResourcePolicyBuilder.createResourcePolicy(context)
+            .withDspaceObject(collectionA)
+            .withAction(Constants.ADMIN)
+            .withUser(eperson)
+            .build();
+        context.restoreAuthSystemState();
+
+        getClient(epersonToken).perform(
             get("/api/authz/authorizations/search/object")
                 .param("embed", "feature")
                 .param("feature", feature)
-                .param("uri", "http://localhost/api/core/communities/" + communityA.getID()))
+                .param("uri", utils.linkToSingleResource(communityARest, "self").getHref()))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.page.totalElements", is(0)))
             .andExpect(jsonPath("$._embedded").doesNotExist());
