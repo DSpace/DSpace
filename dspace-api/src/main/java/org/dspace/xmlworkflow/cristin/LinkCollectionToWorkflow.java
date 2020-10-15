47dccee0d08c6ce5e09020bcaab9788cbe13bc78
Gregor Gabriel <gregor.gabriel@unit.no>
2020-10-15 13:39:44 +0200

    license headers

--- dspace-api/src/main/java/org/dspace/xmlworkflow/cristin/LinkCollectionToWorkflow.java
+++ dspace-api/src/main/java/org/dspace/xmlworkflow/cristin/LinkCollectionToWorkflow.java
@@ -1,8 +1,8 @@
 /**
- * The contents of this file are subject to the license and copyright detailed
- * in the LICENSE and NOTICE files at the root of the source tree and available
- * online at
- * <p>
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
  * http://www.dspace.org/license/
  */
 package org.dspace.xmlworkflow.cristin;
--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V5.5_2016.09.21__B-03529_Alle_DC_kvalifikatorer_skal_kun_ha_små_bokstaver.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V5.5_2016.09.21__B-03529_Alle_DC_kvalifikatorer_skal_kun_ha_små_bokstaver.sql
@@ -1,3 +1,11 @@
+--
+-- The contents of this file are subject to the license and copyright
+-- detailed in the LICENSE and NOTICE files at the root of the source
+-- tree and available online at
+--
+-- http://www.dspace.org/license/
+--
+
 -- Kjør disse to kommandoene på metadatafieldregistrytabellen (database: brage-dspace-5.5) etter genering av ny base fra backup
 
 -- Migrering av dc.identifier.cristinID til dc.identifier.cristin
