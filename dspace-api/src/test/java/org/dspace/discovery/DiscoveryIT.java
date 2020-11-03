2466731028f0f9e990ff1618c045a1d21e3da023
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2020-11-03 16:38:30 +0100

    Rewrote the test class using builders

--- dspace-api/src/test/java/org/dspace/discovery/DiscoveryIT.java
+++ dspace-api/src/test/java/org/dspace/discovery/DiscoveryIT.java
@@ -8,44 +8,27 @@
 package org.dspace.discovery;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
-import java.sql.SQLException;
 import java.util.List;
 
-import org.apache.logging.log4j.Logger;
-import org.dspace.AbstractIntegrationTest;
-import org.dspace.authorize.AuthorizeException;
+import org.dspace.AbstractIntegrationTestWithDatabase;
+import org.dspace.builder.CollectionBuilder;
+import org.dspace.builder.CommunityBuilder;
+import org.dspace.builder.WorkspaceItemBuilder;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
-import org.dspace.content.Item;
 import org.dspace.content.WorkspaceItem;
 import org.dspace.content.factory.ContentServiceFactory;
-import org.dspace.content.service.CollectionService;
-import org.dspace.content.service.CommunityService;
-import org.dspace.content.service.InstallItemService;
-import org.dspace.content.service.ItemService;
 import org.dspace.content.service.WorkspaceItemService;
-import org.dspace.services.factory.DSpaceServicesFactory;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
  * This class will aim to test Discovery related use cases
  */
-public class DiscoveryIT extends AbstractIntegrationTest {
+public class DiscoveryIT extends AbstractIntegrationTestWithDatabase {
 
-    private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(DiscoveryIT.class);
-
-    protected CommunityService communityService = ContentServiceFactory.getInstance().getCommunityService();
-    protected CollectionService collectionService = ContentServiceFactory.getInstance().getCollectionService();
-    protected ItemService itemService = ContentServiceFactory.getInstance().getItemService();
-    protected InstallItemService installItemService = ContentServiceFactory.getInstance().getInstallItemService();
     protected WorkspaceItemService workspaceItemService = ContentServiceFactory.getInstance().getWorkspaceItemService();
-    protected IndexingService indexingService = DSpaceServicesFactory.getInstance().getServiceManager()
-                                                                     .getServiceByName(IndexingService.class.getName(),
-                                                                                       IndexingService.class);
     protected SearchService searchService = SearchUtils.getSearchService();
 
 
@@ -64,61 +47,17 @@ public class DiscoveryIT extends AbstractIntegrationTest {
      */
     @Before
     @Override
-    public void init() {
-        super.init();
-        try {
-            context.turnOffAuthorisationSystem();
-            community = communityService.create(null, context);
+    public void setUp() throws Exception {
+        super.setUp();
+        context.turnOffAuthorisationSystem();
+        community = CommunityBuilder.createCommunity(context).withName("Parent Community").build();
 
-            col = collectionService.create(context, community);
-            leftIs = workspaceItemService.create(context, col, false);
-            rightIs = workspaceItemService.create(context, col, false);
+        col = CollectionBuilder.createCollection(context, community).build();
 
-            itemService.addMetadata(context, leftIs.getItem(), "dc", "description", "abstract", null, "headache");
-            itemService.addMetadata(context, rightIs.getItem(), "dc", "description", "abstract", null, "headache");
+        leftIs = WorkspaceItemBuilder.createWorkspaceItem(context, col).withAbstract("headache").build();
+        rightIs = WorkspaceItemBuilder.createWorkspaceItem(context, col).withAbstract("headache").build();
 
-            workspaceItemService.update(context, leftIs);
-            workspaceItemService.update(context, rightIs);
-
-            context.dispatchEvents();
-            indexingService.commit();
-
-            context.restoreAuthSystemState();
-        } catch (AuthorizeException ex) {
-            log.error("Authorization Error in init", ex);
-            fail("Authorization Error in init: " + ex.getMessage());
-        } catch (SQLException ex) {
-            log.error("SQL Error in init", ex);
-            fail("SQL Error in init: " + ex.getMessage());
-        } catch (Exception ex) {
-            log.error("Error in init", ex);
-            fail("Error in init: " + ex.getMessage());
-        }
-    }
-
-    /**
-     * This method will be run after every test as per @After. It will
-     * clean resources initialized by the @Before methods.
-     *
-     * Other methods can be annotated with @After here or in subclasses
-     * but no execution order is guaranteed
-     */
-    @After
-    @Override
-    public void destroy() {
-        try {
-            context.turnOffAuthorisationSystem();
-            Item secondItem = rightIs.getItem();
-            workspaceItemService.deleteWrapper(context, rightIs);
-            itemService.delete(context, secondItem);
-            collectionService.delete(context, col);
-            communityService.delete(context, community);
-            context.restoreAuthSystemState();
-        } catch (Exception e) {
-            log.error(e);
-            fail(e.getMessage());
-        }
-        super.destroy();
+        context.restoreAuthSystemState();
     }
 
     @Test
