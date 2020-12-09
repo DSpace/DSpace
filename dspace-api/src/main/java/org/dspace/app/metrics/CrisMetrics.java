2d58623cc424d3673b5bb0ace1e5c50339885ccf
Mykhaylo <mykhaylo.boychuk@4science.it>
2020-12-09 19:02:31 +0100

    [CSTPER-266] update annotation

--- dspace-api/src/main/java/org/dspace/app/metrics/CrisMetrics.java
+++ dspace-api/src/main/java/org/dspace/app/metrics/CrisMetrics.java
@@ -7,7 +7,9 @@
  */
 package org.dspace.app.metrics;
 import java.util.Date;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -43,7 +45,7 @@ public class CrisMetrics  implements ReloadableEntity<Integer> {
 
     private Date endDate;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST})
     @JoinColumn(name = "resource_id")
     protected Item resource;
 
