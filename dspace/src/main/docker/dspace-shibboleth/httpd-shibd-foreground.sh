81ae88000915e10d436d741583fc3c2014d12883
Tim Donohue <tim.donohue@lyrasis.org>
2020-12-14 09:55:01 -0600

    Add missing license headers & ignore .conf for license header check

--- dspace/src/main/docker/dspace-shibboleth/httpd-shibd-foreground.sh
+++ dspace/src/main/docker/dspace-shibboleth/httpd-shibd-foreground.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+# The contents of this file are subject to the license and copyright
+# detailed in the LICENSE and NOTICE files at the root of the source
+# tree and available online at
+#
+# http://www.dspace.org/license/
+#
+
+# This script simply starts up the Shibboleth Daemon & Apache in the foreground
 set -e
 
 # Replace the ${APACHE_SERVER_NAME} placeholder in our shibboleth2.xml with the current value of the
