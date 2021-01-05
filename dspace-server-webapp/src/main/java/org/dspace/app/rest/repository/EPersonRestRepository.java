c878293b99d0b8eea3060ca3b9ed0859e4b74cca
Bruno Roemers <bruno.roemers@atmire.com>
2021-01-05 14:48:36 +0100

    75451: Show custom error message when removing last user from workflow group

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/EPersonRestRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/EPersonRestRepository.java
@@ -22,6 +22,7 @@ import org.dspace.app.rest.Parameter;
 import org.dspace.app.rest.SearchRestMethod;
 import org.dspace.app.rest.authorization.AuthorizationFeatureService;
 import org.dspace.app.rest.exception.DSpaceBadRequestException;
+import org.dspace.app.rest.exception.RESTEmptyWorkflowGroupException;
 import org.dspace.app.rest.exception.UnprocessableEntityException;
 import org.dspace.app.rest.model.EPersonRest;
 import org.dspace.app.rest.model.MetadataRest;
@@ -34,6 +35,7 @@ import org.dspace.authorize.service.AuthorizeService;
 import org.dspace.content.service.SiteService;
 import org.dspace.core.Context;
 import org.dspace.eperson.EPerson;
+import org.dspace.eperson.EmptyWorkflowGroupException;
 import org.dspace.eperson.RegistrationData;
 import org.dspace.eperson.service.AccountService;
 import org.dspace.eperson.service.EPersonService;
@@ -313,8 +315,10 @@ public class EPersonRestRepository extends DSpaceObjectRestRepository<EPerson, E
             es.delete(context, eperson);
         } catch (SQLException | IOException e) {
             throw new RuntimeException(e.getMessage(), e);
+        } catch (EmptyWorkflowGroupException e) {
+            throw new RESTEmptyWorkflowGroupException(e.getLocalizedMessage(), e);
         } catch (IllegalStateException e) {
-            throw  new UnprocessableEntityException(e.getMessage(), e);
+            throw new UnprocessableEntityException(e.getMessage(), e);
         }
     }
 
