db586dc625cd9a81941763016dc96b41154aa443
Andrea Bollini <andrea.bollini@4science.it>
2021-01-18 22:59:12 +0100

    CSTPER-318 use solr to retrieve available entity types

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/EntityTypeRestRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/EntityTypeRestRepository.java
@@ -7,19 +7,32 @@
  */
 package org.dspace.app.rest.repository;
 
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.FacetField;
+import org.apache.solr.client.solrj.response.FacetField.Count;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.params.FacetParams;
 import org.dspace.app.rest.SearchRestMethod;
 import org.dspace.app.rest.model.EntityTypeRest;
-import org.dspace.content.Collection;
+import org.dspace.authorize.service.AuthorizeService;
 import org.dspace.content.EntityType;
 import org.dspace.content.service.CollectionService;
 import org.dspace.content.service.EntityTypeService;
-import org.dspace.core.Constants;
 import org.dspace.core.Context;
+import org.dspace.discovery.SearchService;
+import org.dspace.discovery.indexobject.IndexableCollection;
+import org.dspace.eperson.EPerson;
+import org.dspace.eperson.Group;
+import org.dspace.eperson.service.GroupService;
 import org.dspace.external.service.ExternalDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -40,6 +53,12 @@ public class EntityTypeRestRepository extends DSpaceRestRepository<EntityTypeRes
     private CollectionService collectionService;
     @Autowired
     private ExternalDataService externalDataService;
+    @Autowired(required = true)
+    protected AuthorizeService authorizeService;
+    @Autowired(required = true)
+    protected GroupService groupService;
+    @Autowired(required = true)
+    protected SearchService searchService;
 
     @Override
     @PreAuthorize("permitAll()")
@@ -74,23 +93,20 @@ public class EntityTypeRestRepository extends DSpaceRestRepository<EntityTypeRes
     public Page<EntityTypeRest> findAllByAuthorizedCollection(Pageable pageable) {
         try {
             Context context = obtainContext();
-            List<Collection> collections = collectionService.findAuthorized(context, null, Constants.ADD);
-
-            List<EntityType> entityTypes =
-                collections.stream().map(type -> type.getRelationshipType()).distinct()
-                    .map(type -> {
-                        if (StringUtils.isBlank(type)) {
-                            return null;
-                        }
-                        try {
-                            return entityTypeService.findByEntityType(context, type);
-                        } catch (SQLException e) {
-                            throw new RuntimeException(e.getMessage(), e);
-                        }
-                    }).filter(x -> x != null).collect(Collectors.toList());
+            List<String> types = getSubmitAuthorizedTypes(pageable, context);
+            List<EntityType> entityTypes = types.stream().map(type -> {
+                if (StringUtils.isBlank(type)) {
+                    return null;
+                }
+                try {
+                    return entityTypeService.findByEntityType(context, type);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e.getMessage(), e);
+                }
+            }).filter(x -> x != null).collect(Collectors.toList());
             return converter.toRestPage(entityTypes, pageable, utils.obtainProjection());
-        } catch (SQLException e) {
-            throw new RuntimeException(e.getMessage(), e);
+        } catch (SQLException | SolrServerException | IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -98,8 +114,8 @@ public class EntityTypeRestRepository extends DSpaceRestRepository<EntityTypeRes
     public Page<EntityTypeRest> findAllByAuthorizedExternalSource(Pageable pageable) {
         try {
             Context context = obtainContext();
-            List<Collection> collections = collectionService.findAuthorized(context, null, Constants.ADD);
-            List<EntityType> entityTypes = collections.stream().map(type -> type.getRelationshipType()).distinct()
+            List<String> types = getSubmitAuthorizedTypes(pageable, context);
+            List<EntityType> entityTypes = types.stream()
                     .map(type -> {
                         if (StringUtils.isBlank(type)) {
                             return null;
@@ -113,8 +129,48 @@ public class EntityTypeRestRepository extends DSpaceRestRepository<EntityTypeRes
                     .filter(x -> externalDataService.getExternalDataProvidersForEntityType(x.getLabel()).size() > 0)
                     .collect(Collectors.toList());
             return converter.toRestPage(entityTypes, pageable, utils.obtainProjection());
-        } catch (SQLException e) {
+        } catch (SQLException | SolrServerException | IOException e) {
             throw new RuntimeException(e.getMessage(), e);
         }
+    }
+    private List<String> getSubmitAuthorizedTypes(Pageable pageable, Context context)
+            throws SQLException, SolrServerException, IOException {
+        List<String> types = new ArrayList<>();
+        StringBuilder query = new StringBuilder();
+        EPerson currentUser = context.getCurrentUser();
+        if (!authorizeService.isAdmin(context)) {
+            String userId = "";
+            if (currentUser != null) {
+                userId = currentUser.getID().toString();
+            }
+            query.append("submit:(e").append(userId);
+            Set<Group> groups = groupService.allMemberGroupsSet(context, currentUser);
+            for (Group group : groups) {
+                query.append(" OR g").append(group.getID());
+            }
+            query.append(")");
+        } else {
+            query.append("*:*");
+        }
+
+        SolrQuery sQuery = new SolrQuery(query.toString());
+        sQuery.addFilterQuery("search.resourcetype:" + IndexableCollection.TYPE);
+        sQuery.setRows(0);
+        sQuery.addFacetField("search.entitytype");
+        sQuery.setFacetLimit((int) (pageable.getPageSize() + pageable.getOffset()));
+        sQuery.setFacetSort(FacetParams.FACET_SORT_INDEX);
+        QueryResponse qResp = searchService.getSolrSearchCore().getSolr().query(sQuery);
+        FacetField ff = qResp.getFacetField("search.entitytype");
+        if (ff != null) {
+            int skipped = 0;
+            for (Count c : ff.getValues()) {
+                if (skipped < pageable.getOffset()) {
+                    skipped++;
+                    continue;
+                }
+                types.add(c.getName());
+            }
+        }
+        return types;
     }
 }
