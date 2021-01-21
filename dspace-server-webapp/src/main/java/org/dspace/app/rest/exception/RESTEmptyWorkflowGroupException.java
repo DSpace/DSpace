b3a3dd42a5395e82964dc025b44aa10d61cab9e3
Bruno Roemers <bruno.roemers@atmire.com>
2021-01-21 15:11:04 +0100

    75451: Make user exposed error messages translatable

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/exception/RESTEmptyWorkflowGroupException.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/exception/RESTEmptyWorkflowGroupException.java
@@ -7,6 +7,12 @@
  */
 package org.dspace.app.rest.exception;
 
+import java.text.MessageFormat;
+
+import org.dspace.core.Context;
+import org.dspace.core.I18nUtil;
+import org.dspace.eperson.EmptyWorkflowGroupException;
+
 /**
  * <p>Extend {@link UnprocessableEntityException} to provide a specific error message
  * in the REST response. The error message is added to the response in
@@ -17,14 +23,41 @@ package org.dspace.app.rest.exception;
  *
  * @author Bruno Roemers (bruno.roemers at atmire.com)
  */
-public class RESTEmptyWorkflowGroupException extends UnprocessableEntityException {
+public class RESTEmptyWorkflowGroupException extends UnprocessableEntityException implements TranslatableException {
 
-    public RESTEmptyWorkflowGroupException(String message, Throwable cause) {
-        super(message, cause);
+    /**
+     * @param formatStr string with placeholders, ideally obtained using {@link I18nUtil}
+     * @param cause {@link EmptyWorkflowGroupException}, from which EPerson id and group id are obtained
+     * @return message with EPerson id and group id substituted
+     */
+    public static String formatMessage(String formatStr, EmptyWorkflowGroupException cause) {
+        MessageFormat fmt = new MessageFormat(formatStr);
+        String[] values = {
+            cause.getEPersonId().toString(), // {0} in formatStr
+            cause.getGroupId().toString(), // {1} in formatStr
+        };
+        return fmt.format(values);
     }
 
-    public RESTEmptyWorkflowGroupException(String message) {
-        super(message);
+    public static final String MESSAGE_KEY = "org.dspace.app.rest.exception.RESTEmptyWorkflowGroupException.message";
+
+    private final EmptyWorkflowGroupException cause;
+
+    public RESTEmptyWorkflowGroupException(EmptyWorkflowGroupException cause) {
+        super(formatMessage(
+            I18nUtil.getMessage(MESSAGE_KEY), cause
+        ), cause);
+        this.cause = cause;
+    }
+
+    public String getMessageKey() {
+        return MESSAGE_KEY;
+    }
+
+    public String getLocalizedMessage(Context context) {
+        return formatMessage(
+            I18nUtil.getMessage(MESSAGE_KEY, context), cause
+        );
     }
 
 }
