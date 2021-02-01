663fd56ff3a1ef275a63e34e9285d60688371205
Corrado Lombardi <corrado.lombardi@4science.it>
2021-02-01 12:04:57 +0100

    fixed license header

--- dspace-api/src/test/java/org/dspace/importer/external/service/DoiCheckTest.java
+++ dspace-api/src/test/java/org/dspace/importer/external/service/DoiCheckTest.java
@@ -3,7 +3,7 @@
  * detailed in the LICENSE and NOTICE files at the root of the source
  * tree and available online at
  *
- *  http://www.dspace.org/license/
+ * http://www.dspace.org/license/
  */
 package org.dspace.importer.external.service;
 
