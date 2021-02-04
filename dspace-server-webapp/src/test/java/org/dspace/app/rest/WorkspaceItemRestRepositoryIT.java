42aac7d81060f540a039eb6ce5d178c1c741a244
Luca Giamminonni <luca.giamminonni@4Science.it>
2021-02-04 16:01:33 +0100

    [CSTPER-458] Users of  shared collections cannot delete other user's workspace items

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/WorkspaceItemRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/WorkspaceItemRestRepositoryIT.java
@@ -5527,7 +5527,16 @@ public class WorkspaceItemRestRepositoryIT extends AbstractControllerIntegration
     public void testWorkspaceItemPoliciesWithSharedWorkspace() throws Exception {
 
         context.turnOffAuthorisationSystem();
-        context.setCurrentUser(eperson);
+
+        EPerson submitter1 = EPersonBuilder.createEPerson(context)
+            .withEmail("submitter1@test.com")
+            .withPassword(password)
+            .build();
+
+        EPerson submitter2 = EPersonBuilder.createEPerson(context)
+            .withEmail("submitter2@test.com")
+            .withPassword(password)
+            .build();
 
         parentCommunity = CommunityBuilder.createCommunity(context)
             .withName("Parent Community")
@@ -5536,7 +5545,7 @@ public class WorkspaceItemRestRepositoryIT extends AbstractControllerIntegration
         Collection col1 = CollectionBuilder.createCollection(context, parentCommunity)
             .withName("Collection")
             .withRelationshipType("Publication")
-            .withSubmitterGroup(eperson)
+            .withSubmitterGroup(submitter1, submitter2)
             .withSharedWorkspace()
             .build();
 
@@ -5545,9 +5554,7 @@ public class WorkspaceItemRestRepositoryIT extends AbstractControllerIntegration
         AtomicReference<Integer> idRef = new AtomicReference<>();
         try {
 
-            String authToken = getAuthToken(eperson.getEmail(), password);
-
-            getClient(authToken).perform(post("/api/submission/workspaceitems")
+            getClient(getAuthToken(submitter1.getEmail(), password)).perform(post("/api/submission/workspaceitems")
                 .param("owningCollection", col1.getID().toString())
                 .contentType(org.springframework.http.MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
@@ -5560,16 +5567,35 @@ public class WorkspaceItemRestRepositoryIT extends AbstractControllerIntegration
             List<ResourcePolicy> policies = authorizeService.getPolicies(context, workspaceItem.getItem());
             assertThat(policies, hasSize(10));
 
-            assertThat(policies, hasItem(matches(Constants.READ, eperson, TYPE_SUBMISSION)));
-            assertThat(policies, hasItem(matches(Constants.WRITE, eperson, TYPE_SUBMISSION)));
-            assertThat(policies, hasItem(matches(Constants.DELETE, eperson, TYPE_SUBMISSION)));
-            assertThat(policies, hasItem(matches(Constants.REMOVE, eperson, TYPE_SUBMISSION)));
-            assertThat(policies, hasItem(matches(Constants.ADD, eperson, TYPE_SUBMISSION)));
+            assertThat(policies, hasItem(matches(Constants.READ, submitter1, TYPE_SUBMISSION)));
+            assertThat(policies, hasItem(matches(Constants.WRITE, submitter1, TYPE_SUBMISSION)));
+            assertThat(policies, hasItem(matches(Constants.DELETE, submitter1, TYPE_SUBMISSION)));
+            assertThat(policies, hasItem(matches(Constants.REMOVE, submitter1, TYPE_SUBMISSION)));
+            assertThat(policies, hasItem(matches(Constants.ADD, submitter1, TYPE_SUBMISSION)));
             assertThat(policies, hasItem(matches(Constants.READ, col1.getSubmitters(), TYPE_SUBMISSION)));
             assertThat(policies, hasItem(matches(Constants.WRITE, col1.getSubmitters(), TYPE_SUBMISSION)));
             assertThat(policies, hasItem(matches(Constants.DELETE, col1.getSubmitters(), TYPE_SUBMISSION)));
             assertThat(policies, hasItem(matches(Constants.REMOVE, col1.getSubmitters(), TYPE_SUBMISSION)));
             assertThat(policies, hasItem(matches(Constants.ADD, col1.getSubmitters(), TYPE_SUBMISSION)));
+
+            Operation addOperation = new AddOperation("/sections/publication/dc.contributor.author",
+                List.of(Map.of("value", "White, Walter")));
+
+            String patchBody = getPatchContent(List.of(addOperation));
+            getClient(getAuthToken(submitter2.getEmail(), password))
+                .perform(patch("/api/submission/workspaceitems/" + workspaceItem.getID())
+                    .content(patchBody)
+                    .contentType(MediaType.APPLICATION_JSON_PATCH_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.sections.publication['dc.contributor.author'][0].value", is("White, Walter")));
+
+            getClient(getAuthToken(submitter2.getEmail(), password))
+                .perform(delete("/api/submission/workspaceitems/" + workspaceItem.getID()))
+                .andExpect(status().isNoContent());
+
+            getClient(getAuthToken(submitter2.getEmail(), password))
+                .perform(get("/api/submission/workspaceitems/" + workspaceItem.getID()))
+                .andExpect(status().isNotFound());
 
         } finally {
             WorkspaceItemBuilder.deleteWorkspaceItem(idRef.get());
@@ -5580,7 +5606,16 @@ public class WorkspaceItemRestRepositoryIT extends AbstractControllerIntegration
     public void testWorkspaceItemPoliciesWithoutSharedWorkspace() throws Exception {
 
         context.turnOffAuthorisationSystem();
-        context.setCurrentUser(eperson);
+
+        EPerson submitter1 = EPersonBuilder.createEPerson(context)
+            .withEmail("submitter1@test.com")
+            .withPassword(password)
+            .build();
+
+        EPerson submitter2 = EPersonBuilder.createEPerson(context)
+            .withEmail("submitter2@test.com")
+            .withPassword(password)
+            .build();
 
         parentCommunity = CommunityBuilder.createCommunity(context)
             .withName("Parent Community")
@@ -5589,7 +5624,7 @@ public class WorkspaceItemRestRepositoryIT extends AbstractControllerIntegration
         Collection col1 = CollectionBuilder.createCollection(context, parentCommunity)
             .withName("Collection")
             .withRelationshipType("Publication")
-            .withSubmitterGroup(eperson)
+            .withSubmitterGroup(submitter1, submitter2)
             .build();
 
         context.restoreAuthSystemState();
@@ -5597,9 +5632,8 @@ public class WorkspaceItemRestRepositoryIT extends AbstractControllerIntegration
         AtomicReference<Integer> idRef = new AtomicReference<>();
         try {
 
-            String authToken = getAuthToken(eperson.getEmail(), password);
 
-            getClient(authToken).perform(post("/api/submission/workspaceitems")
+            getClient(getAuthToken(submitter1.getEmail(), password)).perform(post("/api/submission/workspaceitems")
                 .param("owningCollection", col1.getID().toString())
                 .contentType(org.springframework.http.MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
@@ -5612,11 +5646,25 @@ public class WorkspaceItemRestRepositoryIT extends AbstractControllerIntegration
             List<ResourcePolicy> policies = authorizeService.getPolicies(context, workspaceItem.getItem());
             assertThat(policies, hasSize(5));
 
-            assertThat(policies, hasItem(matches(Constants.READ, eperson, TYPE_SUBMISSION)));
-            assertThat(policies, hasItem(matches(Constants.WRITE, eperson, TYPE_SUBMISSION)));
-            assertThat(policies, hasItem(matches(Constants.DELETE, eperson, TYPE_SUBMISSION)));
-            assertThat(policies, hasItem(matches(Constants.REMOVE, eperson, TYPE_SUBMISSION)));
-            assertThat(policies, hasItem(matches(Constants.ADD, eperson, TYPE_SUBMISSION)));
+            assertThat(policies, hasItem(matches(Constants.READ, submitter1, TYPE_SUBMISSION)));
+            assertThat(policies, hasItem(matches(Constants.WRITE, submitter1, TYPE_SUBMISSION)));
+            assertThat(policies, hasItem(matches(Constants.DELETE, submitter1, TYPE_SUBMISSION)));
+            assertThat(policies, hasItem(matches(Constants.REMOVE, submitter1, TYPE_SUBMISSION)));
+            assertThat(policies, hasItem(matches(Constants.ADD, submitter1, TYPE_SUBMISSION)));
+
+            Operation addOperation = new AddOperation("/sections/publication/dc.contributor.author",
+                List.of(Map.of("value", "White, Walter")));
+
+            String patchBody = getPatchContent(List.of(addOperation));
+            getClient(getAuthToken(submitter2.getEmail(), password))
+                .perform(patch("/api/submission/workspaceitems/" + workspaceItem.getID())
+                    .content(patchBody)
+                    .contentType(MediaType.APPLICATION_JSON_PATCH_JSON))
+                .andExpect(status().isForbidden());
+
+            getClient(getAuthToken(submitter2.getEmail(), password))
+                .perform(delete("/api/submission/workspaceitems/" + workspaceItem.getID()))
+                .andExpect(status().isForbidden());
 
         } finally {
             WorkspaceItemBuilder.deleteWorkspaceItem(idRef.get());
