0a54bf55cd8076d324d3c88406ac42ef9d7c0dd0
Tim Donohue <tim.donohue@lyrasis.org>
2021-02-11 16:59:48 -0600

    Ensure HAL Browser is checking for CSRF token changes during error responses

--- dspace-server-webapp/src/main/webapp/js/hal/http/client.js
+++ dspace-server-webapp/src/main/webapp/js/hal/http/client.js
@@ -109,9 +109,11 @@ HAL.Http.Client.prototype.get = function(url) {
                 headers: jqXHR.getAllResponseHeaders()
             });
         },
-        error: function() {
-            self.vent.trigger('fail-response', { jqxhr: jqxhr });
-            var contentTypeResponseHeader = jqxhr.getResponseHeader("content-type");
+        error: function(jqXHR, textStatus, errorThrown) {
+            // Also check for updated token during errors. E.g. when a login failure occurs, token may be changed.
+            checkForUpdatedCSRFTokenInResponse(jqXHR);
+            self.vent.trigger('fail-response', { jqxhr: jqXHR });
+            var contentTypeResponseHeader = jqXHR.getResponseHeader("content-type");
             if (contentTypeResponseHeader != undefined
                     && !contentTypeResponseHeader.startsWith("application/hal")
                     && !contentTypeResponseHeader.startsWith("application/json")) {
@@ -135,6 +137,11 @@ HAL.Http.Client.prototype.request = function(opts) {
     // Also check response to see if CSRF Token has been updated
     opts.success = function(resource, textStatus, jqXHR) {
         checkForUpdatedCSRFTokenInResponse(jqXHR);
+    };
+
+    // Also check error responses to see if CSRF Token has been updated
+    opts.error = function(jqXHR, textStatus, errorThrown) {
+         checkForUpdatedCSRFTokenInResponse(jqXHR);
     };
 
     self.vent.trigger('location-change', { url: opts.url });
