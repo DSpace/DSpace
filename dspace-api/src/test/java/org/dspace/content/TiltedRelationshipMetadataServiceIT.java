fd37a4ac54c48b6a8786f199b42db56ac79d5c4b
Bruno Roemers <bruno.roemers@atmire.com>
2021-03-05 11:29:27 +0100

    77489: Copy #testGetAuthorRelationshipMetadata for override

--- dspace-api/src/test/java/org/dspace/content/TiltedRelationshipMetadataServiceIT.java
+++ dspace-api/src/test/java/org/dspace/content/TiltedRelationshipMetadataServiceIT.java
@@ -7,6 +7,13 @@
  */
 package org.dspace.content;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+
 /**
  * This class carries out the same test cases as {@link RelationshipMetadataServiceIT} with a few modifications.
  */
@@ -25,6 +32,55 @@ public class TiltedRelationshipMetadataServiceIT extends RelationshipMetadataSer
         relationshipTypeService.update(context, isAuthorOfPublicationRelationshipType);
 
         context.restoreAuthSystemState();
+    }
+
+    @Test
+    @Override
+    public void testGetAuthorRelationshipMetadata() throws Exception {
+        initPublicationAuthor();
+        //leftItem is the publication
+        //verify the dc.contributor.author virtual metadata
+        List<MetadataValue> authorList = itemService.getMetadata(leftItem, "dc", "contributor", "author", Item.ANY);
+        assertThat(authorList.size(), equalTo(1));
+        assertThat(authorList.get(0).getValue(), equalTo("familyName, firstName"));
+
+        //verify the relation.isAuthorOfPublication virtual metadata
+        List<MetadataValue> leftRelationshipMetadataList = itemService
+            .getMetadata(leftItem, MetadataSchemaEnum.RELATION.getName(), "isAuthorOfPublication", null, Item.ANY);
+        assertThat(leftRelationshipMetadataList.size(), equalTo(1));
+        assertThat(leftRelationshipMetadataList.get(0).getValue(), equalTo(String.valueOf(rightItem.getID())));
+
+        //request the virtual metadata of the publication only
+        List<RelationshipMetadataValue> leftList = relationshipMetadataService
+            .getRelationshipMetadata(leftItem, true);
+        assertThat(leftList.size(), equalTo(2));
+        assertThat(leftList.get(0).getValue(), equalTo("familyName, firstName"));
+        assertThat(leftList.get(0).getMetadataField().getMetadataSchema().getName(), equalTo("dc"));
+        assertThat(leftList.get(0).getMetadataField().getElement(), equalTo("contributor"));
+        assertThat(leftList.get(0).getMetadataField().getQualifier(), equalTo("author"));
+        assertThat(leftList.get(0).getAuthority(), equalTo("virtual::" + relationship.getID()));
+
+        assertThat(leftList.get(1).getValue(), equalTo(String.valueOf(rightItem.getID())));
+        assertThat(leftList.get(1).getMetadataField().getMetadataSchema().getName(),
+            equalTo(MetadataSchemaEnum.RELATION.getName()));
+        assertThat(leftList.get(1).getMetadataField().getElement(), equalTo("isAuthorOfPublication"));
+        assertThat(leftList.get(1).getAuthority(), equalTo("virtual::" + relationship.getID()));
+
+        // rightItem is the author
+        List<MetadataValue> rightRelationshipMetadataList = itemService
+            .getMetadata(rightItem, MetadataSchemaEnum.RELATION.getName(), "isPublicationOfAuthor", null, Item.ANY);
+        assertThat(rightRelationshipMetadataList.size(), equalTo(1));
+        assertThat(rightRelationshipMetadataList.get(0).getValue(), equalTo(String.valueOf(leftItem.getID())));
+
+        //request the virtual metadata of the publication
+        List<RelationshipMetadataValue> rightList = relationshipMetadataService
+            .getRelationshipMetadata(rightItem, true);
+        assertThat(rightList.size(), equalTo(1));
+        assertThat(rightList.get(0).getValue(), equalTo(String.valueOf(leftItem.getID())));
+        assertThat(rightList.get(0).getMetadataField().getMetadataSchema().getName(),
+            equalTo(MetadataSchemaEnum.RELATION.getName()));
+        assertThat(rightList.get(0).getMetadataField().getElement(), equalTo("isPublicationOfAuthor"));
+        assertThat(rightList.get(0).getAuthority(), equalTo("virtual::" + relationship.getID()));
     }
 
 }
