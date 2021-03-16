4467d09c355c954109550f0bbce042bc9219c6f5
Kim Shepherd <kim@shepherd.nz>
2021-03-16 14:32:11 +1300

    [DS-4522] Apply bugfix to InCollectionCondition - better 'owning collection' check

--- dspace-api/src/main/java/org/dspace/content/logic/condition/InCollectionCondition.java
+++ dspace-api/src/main/java/org/dspace/content/logic/condition/InCollectionCondition.java
@@ -7,13 +7,17 @@
  */
 package org.dspace.content.logic.condition;
 
+import java.sql.SQLException;
 import java.util.List;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.dspace.content.Collection;
+import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
 import org.dspace.content.logic.LogicalStatementException;
 import org.dspace.content.service.CollectionService;
+import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -25,10 +29,11 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @version $Revision$
  */
 public class InCollectionCondition extends AbstractCondition {
-    private static Logger log = Logger.getLogger(InCollectionCondition.class);
+    private static Logger log = LogManager.getLogger(InCollectionCondition.class);
 
     @Autowired(required = true)
     protected CollectionService collectionService;
+    protected ItemService itemService;
 
     /**
      * Return true if item is in one of the specified collections
@@ -42,8 +47,10 @@ public class InCollectionCondition extends AbstractCondition {
     public Boolean getResult(Context context, Item item) throws LogicalStatementException {
 
         List<String> collectionHandles = (List<String>)getParameters().get("collections");
-        List<Collection> itemCollections = item.getCollections();
 
+        // Look for the handle among an archived item's collections - this test will only work after submission
+        // and archival is complete
+        List<Collection> itemCollections = item.getCollections();
         for (Collection collection : itemCollections) {
             if (collectionHandles.contains(collection.getHandle())) {
                 log.debug("item " + item.getHandle() + " is in collection "
@@ -52,6 +59,27 @@ public class InCollectionCondition extends AbstractCondition {
             }
         }
 
+        // Look for the parent object of the item. This is important as the item.getOwningCollection method
+        // may return null, even though the item itself does have a parent object, at the point of archival
+        try {
+            DSpaceObject parent = itemService.getParentObject(context, item);
+            if (parent != null) {
+                log.debug("Got parent DSO for item: " + parent.getID().toString());
+                log.debug("Parent DSO handle: " + parent.getHandle());
+                if (collectionHandles.contains(parent.getHandle())) {
+                    log.debug("item " + item.getHandle() + " is in collection "
+                        + parent.getHandle() + ", returning true");
+                    return true;
+                }
+            } else {
+                log.debug("Parent DSO is null...");
+            }
+        } catch (SQLException e) {
+            log.error("Error obtaining parent DSO", e);
+            throw new LogicalStatementException(e);
+        }
+
+        // If we reach this statement, the item did not appear in any of the collections from the parameters
         log.debug("item " + item.getHandle() + " not found in the passed collection handle list");
 
         return false;
