86bf766db16fe98b9b5d51cc2eea9a39d63a79a1
Michael Spalti <mspalti@gmail.com>
2021-03-25 15:48:31 -0700

    Added classes for parsing the info.json object.

---
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/iiif/model/info/AnnotationModel.java
@@ -0,0 +1,30 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.iiif.model.info;
+
+public class AnnotationModel {
+
+    private String motivation;
+    private String id;
+
+    public void setMotivation(String motivation) {
+        this.motivation = motivation;
+    }
+
+    public String getMotivation() {
+        return motivation;
+    }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
