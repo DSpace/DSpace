6ed7bedaa9e5fe0d286435ad53d0c22268494047
Michael Spalti <mspalti@gmail.com>
2021-03-25 15:54:34 -0700

    Added iiif controller.

---
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/IIIFController.java
@@ -0,0 +1,107 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest;
+
+import java.util.UUID;
+
+import org.dspace.app.rest.iiif.IIIFRestRepository;
+import org.dspace.app.rest.repository.AbstractDSpaceRestRepository;
+import org.dspace.core.Context;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller for IIIF Presentation and Search API.
+ */
+@RestController
+@RequestMapping("/api/iiif")
+public class IIIFController extends AbstractDSpaceRestRepository {
+
+    @Autowired
+    IIIFRestRepository iiifRestRepository;
+
+    /**
+     * The manifest response contains sufficient information for the client to initialize
+     * itself and begin to display something quickly to the user. The manifest resource
+     * represents a single object and any intellectual work or works embodied within that
+     * object. In particular it includes the descriptive, rights and linking information
+     * for the object. It then embeds the sequence(s) of canvases that should be rendered
+     * to the user.
+     *
+     * Called with GET to retrieve the manifest for a single DSpace item.
+     *
+     * @param id DSpace Item uuid
+     * @return manifest as JSON
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}/manifest")
+    public String findOne(@PathVariable UUID id) {
+        Context context = obtainContext();
+        return iiifRestRepository.getManifest(context, id);
+    }
+
+    /**
+     * Any resource in the Presentation API may have a search service associated with it.
+     * The resource determines the scope of the content that will be searched. A service
+     * associated with a manifest will search all of the annotations on canvases or other
+     * objects below the manifest, a service associated with a particular range will only
+     * search the canvases within the range, or a service on a canvas will search only
+     * annotations on that particular canvas. The URIs for services associated with different
+     * resources must be different to allow the client to use the correct one for the desired
+     * scope of the search.
+     *
+     * This endpoint for searches within the manifest scope (by DSpace item uuid).
+     *
+     * @param id DSpace Item uuid
+     * @param q query terms
+     * @return AnnotationList as JSON
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}/manifest/search")
+    public String searchInManifest(@PathVariable UUID id,
+                                   @RequestParam(name = "q") String q) {
+        return iiifRestRepository.searchInManifest(id, q);
+    }
+
+    /**
+     * All resources can link to semantic descriptions of themselves via the seeAlso property.
+     * These could be METS, ALTO, full text, or a schema.org descriptions.
+     *
+     * Since there's currently no reliable way to associate "seeAlso" links and individual
+     * canvases (e.g. associate a single image with its ALTO file) the
+     * scope is the entire manifest (or DSpace Item).
+     *
+     * @param id DSpace Item uuid
+     * @return AnnotationList as JSON
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}/manifest/seeAlso")
+    public String findSeeAlsoList(@PathVariable UUID id) {
+        Context context = obtainContext();
+        return iiifRestRepository.getSeeAlsoAnnotations(context, id);
+    }
+
+    /**
+     * The canvas represents an individual page or view and acts as a central point for
+     * laying out the different content resources that make up the display. This information
+     * should be embedded within a sequence.
+     *
+     * This endpoint allows canvases to be dereferenced separately from the manifest. This
+     * is an atypical use case.
+     *
+     * @param id DSpace Item uuid
+     * @param cid canvas identifier
+     * @return canvas as JSON
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}/canvas/{cid}")
+    public String findCanvas(@PathVariable UUID id, @PathVariable String cid) {
+        Context context = obtainContext();
+        return iiifRestRepository.getCanvas(context, id, cid);
+    }
+}
