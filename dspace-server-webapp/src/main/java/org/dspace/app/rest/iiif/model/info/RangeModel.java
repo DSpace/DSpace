151992efefefc0183a6c9410d5e7c282f66273c5
Michael Spalti <mspalti@gmail.com>
2021-03-25 15:48:31 -0700

    Added classes for parsing the info.json object.

---
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/iiif/model/info/RangeModel.java
@@ -0,0 +1,31 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.iiif.model.info;
+
+public class RangeModel {
+
+    private String label;
+    private int start;
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+}
