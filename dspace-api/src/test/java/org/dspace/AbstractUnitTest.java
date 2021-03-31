b0bec7d2e42b4f2559493b68904b5645b9baec0f
Luca Giamminonni <luca.giamminonni@4Science.it>
2021-03-31 19:29:37 +0200

    Set UnitTests Context mode to READ_WRITE

--- dspace-api/src/test/java/org/dspace/AbstractUnitTest.java
+++ dspace-api/src/test/java/org/dspace/AbstractUnitTest.java
@@ -93,7 +93,7 @@ public class AbstractUnitTest extends AbstractDSpaceTest {
     public void init() {
         try {
             //Start a new context
-            context = new Context(Context.Mode.BATCH_EDIT);
+            context = new Context(Context.Mode.READ_ONLY);
             context.turnOffAuthorisationSystem();
 
             //Find our global test EPerson account. If it doesn't exist, create it.
