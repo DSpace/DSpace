bd0b06dfe6ecd429de56784bdfaee4beaa19186d
Michael Spalti <mspalti@gmail.com>
2021-07-11 06:50:17 -0700

    Updated cache config and renamed classes.

---
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/iiif/cache/CacheConfig.java
@@ -0,0 +1,10 @@
+package org.dspace.app.rest.iiif.cache;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    // Spring boot cache configuration.
+}
