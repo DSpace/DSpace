16ffffc3a73472910efc7e059311e70ea4f4f067
tysonlt <tysonlt@gmail.com>
2021-07-15 11:32:13 +1000

    fix checkstyle violations

--- dspace-api/src/main/java/org/dspace/app/util/RelationshipUtils.java
+++ dspace-api/src/main/java/org/dspace/app/util/RelationshipUtils.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.app.util;
 
 import java.util.List;
@@ -6,45 +13,49 @@ import org.dspace.content.RelationshipType;
 
 public class RelationshipUtils {
 
+    private RelationshipUtils() {
+    }
+
     /**
     * Matches two Entity types to a Relationship Type from a set of Relationship Types.
-    *
+     *
     * @param relTypes set of Relationship Types.
-    * @param targetType entity type of target.
-    * @param originType entity type of origin referer.
-    * @return null or matched Relationship Type.
-    */
-    public static  RelationshipType matchRelationshipType(List<RelationshipType> relTypes, String targetType, String originType, String originTypeName) {
-       RelationshipType foundRelationshipType = null;
-       if (originTypeName.split("\\.").length > 1) {
-           originTypeName = originTypeName.split("\\.")[1];
-       }
-       for (RelationshipType relationshipType : relTypes) {
-           // Is origin type leftward or righward
-           boolean isLeft = false;
-           if (relationshipType.getLeftType().getLabel().equalsIgnoreCase(originType)) {
-               isLeft = true;
-           }
-           if (isLeft) {
-               // Validate typeName reference
-               if (!relationshipType.getLeftwardType().equalsIgnoreCase(originTypeName)) {
-                   continue;
-               }
-               if (relationshipType.getLeftType().getLabel().equalsIgnoreCase(originType) &&
-                   relationshipType.getRightType().getLabel().equalsIgnoreCase(targetType)) {
-                   foundRelationshipType = relationshipType;
-               }
-           } else {
-               if (!relationshipType.getRightwardType().equalsIgnoreCase(originTypeName)) {
-                   continue;
-               }
-               if (relationshipType.getLeftType().getLabel().equalsIgnoreCase(targetType) &&
-                   relationshipType.getRightType().getLabel().equalsIgnoreCase(originType)) {
-                   foundRelationshipType = relationshipType;
-               }
-           }
-       }
-       return foundRelationshipType;
-   }
-    
+     * @param targetType entity type of target.
+     * @param originType entity type of origin referer.
+     * @return null or matched Relationship Type.
+     */
+    public static RelationshipType matchRelationshipType(List<RelationshipType> relTypes, String targetType,
+            String originType, String originTypeName) {
+        RelationshipType foundRelationshipType = null;
+        if (originTypeName.split("\\.").length > 1) {
+            originTypeName = originTypeName.split("\\.")[1];
+        }
+        for (RelationshipType relationshipType : relTypes) {
+            // Is origin type leftward or righward
+            boolean isLeft = false;
+            if (relationshipType.getLeftType().getLabel().equalsIgnoreCase(originType)) {
+                isLeft = true;
+            }
+            if (isLeft) {
+                // Validate typeName reference
+                if (!relationshipType.getLeftwardType().equalsIgnoreCase(originTypeName)) {
+                    continue;
+                }
+                if (relationshipType.getLeftType().getLabel().equalsIgnoreCase(originType)
+                        && relationshipType.getRightType().getLabel().equalsIgnoreCase(targetType)) {
+                    foundRelationshipType = relationshipType;
+                }
+            } else {
+                if (!relationshipType.getRightwardType().equalsIgnoreCase(originTypeName)) {
+                    continue;
+                }
+                if (relationshipType.getLeftType().getLabel().equalsIgnoreCase(targetType)
+                        && relationshipType.getRightType().getLabel().equalsIgnoreCase(originType)) {
+                    foundRelationshipType = relationshipType;
+                }
+            }
+        }
+        return foundRelationshipType;
+    }
+
 }
