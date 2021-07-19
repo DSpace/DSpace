c87f85bc11a47003dc356698956dcb8b37483bb5
Nathan Buckingham <nathan.buckingham@atmire.com>
2021-07-19 11:12:48 -0400

    Packager tests more conscise

--- dspace-api/src/test/java/org/dspace/app/packager/PackagerIT.java
+++ dspace-api/src/test/java/org/dspace/app/packager/PackagerIT.java
@@ -38,87 +38,138 @@ import org.dspace.content.crosswalk.MetadataValidationException;
 import org.dspace.content.factory.ContentServiceFactory;
 import org.dspace.content.packager.METSManifest;
 import org.dspace.content.service.ItemService;
-import org.dspace.content.service.RelationshipService;
-import org.dspace.content.service.RelationshipTypeService;
 import org.jdom.Element;
 import org.junit.Test;
 
-// See CsvImportIT for other examples involving rels
 public class PackagerIT extends AbstractIntegrationTestWithDatabase {
 
     private ItemService itemService = ContentServiceFactory.getInstance().getItemService();
     protected RelationshipMetadataService relationshipMetadataService = ContentServiceFactory
             .getInstance().getRelationshipMetadataService();
+    protected Community child1;
+    protected Collection col1;
+    protected Item article;
+    protected Item author;
+    File tempFile;
+    File resultFile;
 
     @Test
     public void packagerExportUUIDTest() throws Exception {
         context.turnOffAuthorisationSystem();
-        Item article = createTemplate();
-
-        File tempFile = File.createTempFile("packagerExportTest", ".zip");
-        String path = tempFile.getAbsolutePath().split("\\.")[0];
-        File resultFile = new File(path + "_ITEM@" +
-                article.getHandle().replace("/", "-") + ".zip");
+        createTemplate();
+        createFiles();
         try {
             performExportScript(article.getHandle(), tempFile);
             assertTrue(resultFile.length() > 0);
-            String idStr = getID(resultFile);
+            String idStr = getID();
             assertEquals(idStr, article.getID().toString());
-        } catch (Exception e) {
-            throw new Exception(e);
         } finally {
             tempFile.delete();
+            resultFile.delete();
         }
     }
 
     @Test
     public void packagerImportUUIDTest() throws Exception {
         context.turnOffAuthorisationSystem();
-        Item article = createTemplate();
-
-
-        File tempFile = File.createTempFile("packagerExportTest", ".zip");
-        String path = tempFile.getAbsolutePath().split("\\.")[0];
-        File resultFile = new File(path + "_ITEM@" +
-                article.getHandle().replace("/", "-") + ".zip");
+        createTemplate();
+        createFiles();
         try {
             performExportScript(article.getHandle(), tempFile);
-            String idStr = getID(resultFile);
+            String idStr = getID();
             itemService.delete(context, article);
             performImportScript(resultFile);
             Item item = itemService.find(context, UUID.fromString(idStr));
             assertNotNull(item);
-        } catch (Exception e) {
-            throw new Exception(e);
         } finally {
             tempFile.delete();
+            resultFile.delete();
         }
     }
 
     @Test
-    public void packagerRelationShipTest() throws Exception {
+    public void packagerExportRelationshipTest() throws Exception {
+        //Test sees if the mets created by the packager includes the metadatafields
+        //required to do a relationship restore
         context.turnOffAuthorisationSystem();
+        createTemplate();
+        createRels();
+        createFiles();
+        try {
+            performExportScript(article.getHandle(), tempFile);
+            METSManifest manifest = null;
+            ZipFile zip = new ZipFile(resultFile);
+            ZipEntry manifestEntry = zip.getEntry(METSManifest.MANIFEST_FILE);
+            if (manifestEntry != null) {
+                // parse the manifest and sanity-check it.
+                manifest = METSManifest.create(zip.getInputStream(manifestEntry),
+                        false, "AIP");
+                for (Element element : manifest.getItemDmds()) {
+                    //check to see if the familyName is in the metadata for the article if it is the export
+                    // exported the relationship virtual metadata
+                    assertTrue(element.getValue().contains("familyName"));
+                }
+            }
+        } finally {
+            tempFile.delete();
+            resultFile.delete();
+        }
+    }
+
+    @Test
+    public void packagerImportRelationshipTest() throws Exception {
+        //Tests if a import will restore the relationship of an item
+        context.turnOffAuthorisationSystem();
+        createTemplate();
+        createRels();
+        createFiles();
+        context.turnOffAuthorisationSystem();
+        try {
+            performExportScript(article.getHandle(), tempFile);
+            List<RelationshipMetadataValue> leftList = relationshipMetadataService
+                    .getRelationshipMetadata(article, true);
+            assertThat(leftList.size(), equalTo(2));
+            assertThat(leftList.get(0).getValue(), equalTo("familyName, firstName"));
+            String id = getID();
+            performImportScript(resultFile);
+            //get the new item create by the import
+            Item item2 = itemService.findByIdOrLegacyId(context, id);
+            leftList = relationshipMetadataService
+                    .getRelationshipMetadata(item2, true);
+            //check to see if the metadata exists in the new item
+            assertThat(leftList.size(), equalTo(2));
+            assertThat(leftList.get(0).getValue(), equalTo("familyName, firstName"));
+        } finally {
+            tempFile.delete();
+            resultFile.delete();
+        }
+    }
+
+    protected void createTemplate() {
         parentCommunity = CommunityBuilder.createCommunity(context)
                 .withName("Parent Community")
                 .build();
-        Community child1 = CommunityBuilder.createSubCommunity(context, parentCommunity)
+        child1 = CommunityBuilder.createSubCommunity(context, parentCommunity)
                 .withName("Sub Community")
                 .build();
-        Collection col1 = CollectionBuilder.createCollection(context, child1).withName("Collection 2").build();
+        col1 = CollectionBuilder.createCollection(context, child1).withName("Collection 2").build();
 
-        Item article = ItemBuilder.createItem(context, col1)
+        // Create a new Publication (which is an Article)
+        article = ItemBuilder.createItem(context, col1)
                 .withTitle("Article")
                 .withIssueDate("2017-10-17")
                 .withEntityType("Publication")
                 .build();
-        Item author = ItemBuilder.createItem(context, col1)
+
+        author = ItemBuilder.createItem(context, col1)
                 .withPersonIdentifierLastName("familyName")
                 .withPersonIdentifierFirstName("firstName")
                 .withEntityType("Person")
                 .build();
-        RelationshipService relationshipService = ContentServiceFactory.getInstance().getRelationshipService();
-        RelationshipTypeService relationshipTypeService = ContentServiceFactory
-                .getInstance().getRelationshipTypeService();
+    }
+
+    protected void createRels() {
+        //created the entity types needed for creating relations and related them to the items created previously
         EntityType publicationEntityType = EntityTypeBuilder.createEntityTypeBuilder(context, "Publication").build();
         EntityType authorEntityType = EntityTypeBuilder.createEntityTypeBuilder(context, "Person").build();
         RelationshipType isAuthorOfPublication =
@@ -126,51 +177,21 @@ public class PackagerIT extends AbstractIntegrationTestWithDatabase {
                         "isAuthorOfPublication", "isPublicationOfAuthor",
                         null, null, null, null).build();
         RelationshipBuilder.createRelationshipBuilder(context, article, author, isAuthorOfPublication).build();
-        File tempFile = File.createTempFile("packagerExportTest", ".zip");
+    }
+
+    protected void createFiles() throws IOException {
+        //After packager makes a file it appends a _ITEM@HANDLE.zip to the provided
+        //path and thus we need to get the resulting file
+        tempFile = File.createTempFile("packagerExportTest", ".zip");
         String path = tempFile.getAbsolutePath().split("\\.")[0];
-        File resultFile = new File(path + "_ITEM@" +
+        resultFile = new File(path + "_ITEM@" +
                 article.getHandle().replace("/", "-") + ".zip");
-        try {
-            List<RelationshipMetadataValue> leftList = relationshipMetadataService
-                    .getRelationshipMetadata(article, true);
-            assertThat(leftList.size(), equalTo(2));
-            assertThat(leftList.get(0).getValue(), equalTo("familyName, firstName"));
-            performExportScript(article.getHandle(), tempFile);
-            getID(resultFile);
-            performImportScript(resultFile);
-            Item item = itemService.find(context, article.getID());
-            leftList = relationshipMetadataService
-                    .getRelationshipMetadata(item, true);
-            assertThat(leftList.size(), equalTo(2));
-            assertThat(leftList.get(0).getValue(), equalTo("familyName, firstName"));
-        } catch (Exception e) {
-            throw new Exception(e);
-        } finally {
-            tempFile.delete();
-            resultFile.delete();
-        }
     }
 
-    private Item createTemplate() {
-        parentCommunity = CommunityBuilder.createCommunity(context)
-                .withName("Parent Community")
-                .build();
-        Community child1 = CommunityBuilder.createSubCommunity(context, parentCommunity)
-                .withName("Sub Community")
-                .build();
-        Collection col1 = CollectionBuilder.createCollection(context, child1).withName("Collection 2").build();
-
-        // Create a new Publication (which is an Article)
-        return ItemBuilder.createItem(context, col1)
-                .withTitle("Article")
-                .withIssueDate("2017-10-17")
-                .withEntityType("Publication")
-                .build();
-    }
-
-    private String getID(File tempFile) throws IOException, MetadataValidationException {
+    private String getID() throws IOException, MetadataValidationException {
+        //this method gets the UUID from the mets file thats stored in the attribute element
         METSManifest manifest = null;
-        ZipFile zip = new ZipFile(tempFile);
+        ZipFile zip = new ZipFile(resultFile);
         ZipEntry manifestEntry = zip.getEntry(METSManifest.MANIFEST_FILE);
         if (manifestEntry != null) {
             // parse the manifest and sanity-check it.
@@ -192,7 +213,7 @@ public class PackagerIT extends AbstractIntegrationTestWithDatabase {
     }
 
     private void performImportScript(File outputFile) throws Exception {
-        runDSpaceScript("packager", "-r", "-f", "-u", "-e", "admin@email.com", "-t",
+        runDSpaceScript("packager", "-r", "-f", "-z", "*", "-u", "-e", "admin@email.com", "-t",
                 "AIP", outputFile.getPath());
     }
 }
