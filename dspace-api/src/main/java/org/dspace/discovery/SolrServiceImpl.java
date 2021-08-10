5b05868e007a3a94d663f49532ef8b31afaca466
Luca Giamminonni <luca.giamminonni@4Science.it>
2021-08-10 15:30:01 +0200

    Fixed update relation for item

--- dspace-api/src/main/java/org/dspace/discovery/SolrServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/discovery/SolrServiceImpl.java
@@ -1572,7 +1572,10 @@ public class SolrServiceImpl implements SearchService, IndexingService {
 
         try {
             SolrInputDocument solrInDoc = new SolrInputDocument();
-            solrInDoc.addField(SearchUtils.RESOURCE_UNIQUE_ID, "Item-" + itemId);
+            String itemType = IndexableItem.TYPE;
+            solrInDoc.addField(SearchUtils.RESOURCE_UNIQUE_ID, itemType + "-" + itemId);
+            solrInDoc.addField(SearchUtils.RESOURCE_TYPE_FIELD, itemType);
+            solrInDoc.addField(SearchUtils.RESOURCE_ID_FIELD, itemId);
             final String field = "relation." + relationLabel;
             solrInDoc.addField(field,
                                Collections.<String, Object>singletonMap("set", relatedItems));
