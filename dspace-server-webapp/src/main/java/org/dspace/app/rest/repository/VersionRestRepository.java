52bfcafca166f41323a1a195bd8ce1f62c220760
Mykhaylo <mykhaylo.boychuk@4science.it>
2021-09-08 15:35:48 +0200

    refactored versions end point

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/VersionRestRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/VersionRestRepository.java
@@ -7,19 +7,15 @@
  */
 package org.dspace.app.rest.repository;
 import java.sql.SQLException;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
-import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.NotFoundException;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.Logger;
-import org.dspace.app.rest.Parameter;
-import org.dspace.app.rest.SearchRestMethod;
 import org.dspace.app.rest.converter.ConverterService;
 import org.dspace.app.rest.exception.DSpaceBadRequestException;
+import org.dspace.app.rest.exception.DSpaceForbiddenException;
 import org.dspace.app.rest.exception.RepositoryMethodNotImplementedException;
 import org.dspace.app.rest.exception.UnprocessableEntityException;
 import org.dspace.app.rest.model.VersionRest;
@@ -27,14 +23,18 @@ import org.dspace.app.rest.model.patch.Operation;
 import org.dspace.app.rest.model.patch.Patch;
 import org.dspace.app.rest.repository.handler.service.UriListHandlerService;
 import org.dspace.authorize.AuthorizeException;
+import org.dspace.authorize.service.AuthorizeService;
 import org.dspace.content.Item;
-import org.dspace.content.service.ItemService;
+import org.dspace.content.WorkspaceItem;
 import org.dspace.content.service.WorkspaceItemService;
 import org.dspace.core.Context;
+import org.dspace.eperson.EPerson;
+import org.dspace.services.ConfigurationService;
 import org.dspace.versioning.Version;
 import org.dspace.versioning.VersionHistory;
 import org.dspace.versioning.service.VersionHistoryService;
 import org.dspace.versioning.service.VersioningService;
+import org.dspace.workflow.WorkflowItem;
 import org.dspace.workflow.WorkflowItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -54,19 +54,22 @@ public class VersionRestRepository extends DSpaceRestRepository<VersionRest, Int
     private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(VersionRestRepository.class);
 
     @Autowired
+    private ConfigurationService configurationService;
+
+    @Autowired
+    private AuthorizeService authorizeService;
+
+    @Autowired
     private VersioningService versioningService;
+
+    @Autowired
+    private VersionHistoryService versionHistoryService;
 
     @Autowired
     private ConverterService converterService;
 
     @Autowired
     private UriListHandlerService uriListHandlerService;
-
-    @Autowired
-    private ItemService itemService;
-
-    @Autowired
-    private VersionHistoryService versionHistoryService;
 
     @Autowired
     private WorkspaceItemService workspaceItemService;
@@ -91,86 +94,51 @@ public class VersionRestRepository extends DSpaceRestRepository<VersionRest, Int
     }
 
     @Override
-    @PreAuthorize("hasAuthority('ADMIN')")
-    protected void delete(Context context, Integer id) throws AuthorizeException {
-        Version version = null;
-        try {
-            version = versioningService.getVersion(context, id);
-            if (Objects.isNull(version)) {
-                throw new ResourceNotFoundException(
-                          VersionRest.CATEGORY + "." + VersionRest.NAME + " with id: " + id + " not found");
-            }
-            versioningService.delete(context, version);
-        } catch (SQLException e) {
-            throw new RuntimeException("Unable to delete Version with id = " + id, e);
-        }
-    }
-
-    @Override
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAuthority('AUTHENTICATED')")
     protected VersionRest createAndReturn(Context context, List<String> stringList)
         throws AuthorizeException, SQLException, RepositoryMethodNotImplementedException {
 
         HttpServletRequest req = getRequestService().getCurrentRequest().getHttpServletRequest();
         String summary = req.getParameter("summary");
+
         Item item = uriListHandlerService.handle(context, req, stringList, Item.class);
         if (Objects.isNull(item)) {
             throw new UnprocessableEntityException("The given URI list could not be properly parsed to one result");
         }
-        if (Objects.nonNull(workspaceItemService.findByItem(context, item)) ||
-            Objects.nonNull(workflowItemService.findByItem(context, item))) {
+
+        EPerson submitter = item.getSubmitter();
+        boolean isAdmin = authorizeService.isAdmin(context);
+        boolean canCreateVersion = configurationService.getBooleanProperty("versioning.submitterCanCreateNewVersion");
+        if (!isAdmin && !(canCreateVersion && Objects.equals(submitter, context.getCurrentUser()))) {
+            throw new DSpaceForbiddenException("");
+        }
+
+        WorkflowItem workflowItem = null;
+        WorkspaceItem workspaceItem = null;
+        VersionHistory versionHistory = versionHistoryService.findByItem(context, item);
+        if (Objects.nonNull(versionHistory)) {
+            Version lastVersion = versionHistoryService.getLatestVersion(context, versionHistory);
+            if (Objects.nonNull(lastVersion)) {
+                workflowItem = workflowItemService.findByItem(context, lastVersion.getItem());
+                workspaceItem = workspaceItemService.findByItem(context, lastVersion.getItem());
+            }
+        } else {
+            workflowItem = workflowItemService.findByItem(context, item);
+            workspaceItem = workspaceItemService.findByItem(context, item);
+        }
+
+        if (Objects.nonNull(workflowItem) || Objects.nonNull(workspaceItem)) {
             throw new UnprocessableEntityException("");
         }
-        Version version = null;
-        if (StringUtils.isNotBlank(summary)) {
-            version = versioningService.createNewVersion(context, item, summary);
-        } else {
-            version = versioningService.createNewVersion(context, item);
-        }
+
+        Version version = StringUtils.isNotBlank(summary) ?
+                          versioningService.createNewVersion(context, item, summary) :
+                          versioningService.createNewVersion(context, item);
         return converter.toRest(version, utils.obtainProjection());
     }
 
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @SearchRestMethod(name = "findByItem")
-    public VersionRest findByItem(@Parameter(value = "itemUuid", required = true) UUID itemUuid) {
-        Context context = obtainContext();
-        Version version = null;
-        try {
-            Item item = itemService.find(context, itemUuid);
-            if (Objects.nonNull(item)) {
-                version = versioningService.getVersion(context, item);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e.getMessage(), e);
-        }
-        return Objects.nonNull(version) ? converter.toRest(version, utils.obtainProjection()) : null;
-    }
-
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @SearchRestMethod(name = "findByHistory")
-    public Page<VersionRest> findByHistory(@Parameter(value = "historyId", required = true) Integer historyId,
-                                           Pageable pageable) {
-        Context context = obtainContext();
-        List<Version> versions = new LinkedList<Version>();;
-        int total = 0;
-        try {
-            VersionHistory versionHistory = versionHistoryService.find(context, historyId);
-            if (Objects.isNull(versionHistory)) {
-                throw new DSpaceBadRequestException(
-                      "This given id:" + historyId + " does not resolve to a VersionHistory");
-            }
-            versions = versioningService.getVersionsByHistory(context, versionHistory,
-                                         Math.toIntExact(pageable.getOffset()),
-                                         Math.toIntExact(pageable.getPageSize()));
-            total = versioningService.countVersionsByHistory(context, versionHistory);
-        } catch (SQLException e) {
-            throw new RuntimeException(e.getMessage(), e);
-        }
-        return converter.toRestPage(versions, pageable, total, utils.obtainProjection());
-    }
-
     @Override
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasPermission(#versionId, 'version', 'ADMIN')")
     protected void patch(Context context, HttpServletRequest request, String apiCategory, String model,
                          Integer versionId, Patch patch) throws AuthorizeException, SQLException {
         for (Operation operation : patch.getOperations()) {
@@ -180,7 +148,7 @@ public class VersionRestRepository extends DSpaceRestRepository<VersionRest, Int
             }
             Version version = versioningService.getVersion(context, versionId);
             if (Objects.isNull(version)) {
-                throw new NotFoundException("This given id:" + versionId + " does not resolve to a Version");
+                throw new ResourceNotFoundException("This given id:" + versionId + " does not resolve to a Version");
             }
             switch (operation.getOp()) {
                 case "remove":
@@ -211,4 +179,5 @@ public class VersionRestRepository extends DSpaceRestRepository<VersionRest, Int
     public Class<VersionRest> getDomainClass() {
         return VersionRest.class;
     }
+
 }
