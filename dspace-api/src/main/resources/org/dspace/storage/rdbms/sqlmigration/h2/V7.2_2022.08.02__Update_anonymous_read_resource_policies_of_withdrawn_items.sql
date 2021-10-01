31c79e4b0cdd3e8349637a1004f4e94791961559
Bruno Roemers <bruno.roemers@atmire.com>
2021-10-01 17:57:34 +0200

    83793: BUGFIX: Update anonymous read resource policies of withdrawn items

---
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/h2/V7.2_2022.08.02__Update_anonymous_read_resource_policies_of_withdrawn_items.sql
@@ -0,0 +1,20 @@
+--
+-- The contents of this file are subject to the license and copyright
+-- detailed in the LICENSE and NOTICE files at the root of the source
+-- tree and available online at
+--
+-- http://www.dspace.org/license/
+--
+
+-- ===============================================================
+-- WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING
+--
+-- DO NOT MANUALLY RUN THIS DATABASE MIGRATION. IT WILL BE EXECUTED
+-- AUTOMATICALLY (IF NEEDED) BY "FLYWAY" WHEN YOU STARTUP DSPACE.
+-- http://flywaydb.org/
+-- ===============================================================
+
+-- change action_id from READ to WITHDRAWN_READ for withdrawn items
+UPDATE resourcepolicy SET action_id = 12 where action_id = 0 and dspace_object in (
+    SELECT uuid from item where withdrawn = true
+);
