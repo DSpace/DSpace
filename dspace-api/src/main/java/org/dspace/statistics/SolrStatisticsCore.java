5b21803c9c8573d45980f8d8d170737c09b2d5c3
Mark H. Wood <mwood@IUPUI.Edu>
2021-10-04 13:58:17 -0400

    First attempt at a single pool of HTTP connections for Solr.

--- dspace-api/src/main/java/org/dspace/statistics/SolrStatisticsCore.java
+++ dspace-api/src/main/java/org/dspace/statistics/SolrStatisticsCore.java
@@ -12,6 +12,7 @@ import static org.apache.logging.log4j.LogManager.getLogger;
 import org.apache.logging.log4j.Logger;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.dspace.service.impl.HttpConnectionPoolService;
 import org.dspace.services.ConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,12 +21,15 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class SolrStatisticsCore {
 
-    private static Logger log = getLogger(SolrStatisticsCore.class);
+    private static final Logger log = getLogger();
 
     protected SolrClient solr = null;
 
     @Autowired
     private ConfigurationService configurationService;
+
+    @Autowired
+    private HttpConnectionPoolService httpConnectionPoolService;
 
     /**
      * Returns the {@link SolrClient} for the Statistics core.
@@ -50,7 +54,9 @@ public class SolrStatisticsCore {
         log.info("usage-statistics.dbfile:  {}", configurationService.getProperty("usage-statistics.dbfile"));
 
         try {
-            solr = new HttpSolrClient.Builder(solrService).build();
+            solr = new HttpSolrClient.Builder(solrService)
+                    .withHttpClient(httpConnectionPoolService.getClient())
+                    .build();
         } catch (Exception e) {
             log.error("Error accessing Solr server configured in 'solr-statistics.server'", e);
         }
