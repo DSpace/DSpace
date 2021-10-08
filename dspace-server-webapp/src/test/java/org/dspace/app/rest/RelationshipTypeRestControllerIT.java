38a38658818def1e05fcb84093c967c4d74754b5
Luca Giamminonni <luca.giamminonni@4science.it>
2021-10-08 17:31:33 +0200

    [CSTPER-237] Removed @Ignore on RelationshipTypeRestControllerIT and RelationshipTypeRestRepositoryIT

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/RelationshipTypeRestControllerIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/RelationshipTypeRestControllerIT.java
@@ -30,11 +30,9 @@ import org.dspace.content.Relationship;
 import org.dspace.content.RelationshipType;
 import org.dspace.content.service.EntityTypeService;
 import org.dspace.content.service.RelationshipTypeService;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Ignore
 public class RelationshipTypeRestControllerIT extends AbstractEntityIntegrationTest {
 
     @Autowired
