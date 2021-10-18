d1ea78281e71e657a1bd9f31ff185efd2b9215e3
April Herron <april.herron@atmire.com>
2021-10-18 13:45:31 -0400

    DS-2904 GA Code cleanup

--- dspace-api/src/main/java/org/dspace/google/GoogleAnalyticsEvent.java
+++ dspace-api/src/main/java/org/dspace/google/GoogleAnalyticsEvent.java
@@ -23,31 +23,46 @@ public class GoogleAnalyticsEvent {
     private long time;
 
     GoogleAnalyticsEvent(String cid, String uip, String ua, String dr, String dp, String dt, long time) {
-        this.cid = cid;                      // Client ID
-        this.uip = uip;                      // User IP
-        this.ua = ua;                        // User Agent
-        this.dr = dr;                        // Document Referrer
-        this.dp = dp;                        // Document Path
-        this.dt = dt;                        // Document Title
-        this.time = time;                    // Time of event
+        this.cid = cid;
+        this.uip = uip;
+        this.ua = ua;
+        this.dr = dr;
+        this.dp = dp;
+        this.dt = dt;
+        this.time = time;
     }
 
+    /**
+     * Return Client ID
+     */
     public String getCid() {
         return cid;
     }
 
+    /**
+     * Set Client ID
+     */
     public void setCid(String cid) {
         this.cid = cid;
     }
 
+    /**
+     * Return User IP
+     */
     public String getUip() {
         return uip;
     }
 
+    /**
+     * Set User IP
+     */
     public void setUip(String uip) {
         this.uip = uip;
     }
 
+    /**
+     * Returns User Agent
+     */
     public String getUa() {
         if (ua == null) {
             return "";
@@ -56,10 +71,16 @@ public class GoogleAnalyticsEvent {
         }
     }
 
+    /**
+     * Set User Agent
+     */
     public void setUa(String ua) {
         this.ua = ua;
     }
 
+    /**
+     * Return Document Referrer
+     */
     public String getDr() {
         if (dr == null) {
             return "";
@@ -68,30 +89,51 @@ public class GoogleAnalyticsEvent {
         }
     }
 
+    /**
+     * Set Document Referrer
+     */
     public void setDr(String dr) {
         this.dr = dr;
     }
 
+    /**
+     * Return Document Path
+     */
     public String getDp() {
         return dp;
     }
 
+    /**
+     * Set Document Path
+     */
     public void setDp(String dp) {
         this.dp = dp;
     }
 
+    /**
+     * Return Document Title
+     */
     public String getDt() {
         return dt;
     }
 
+    /**
+     * Set Document Title
+     */
     public void setDt(String dt) {
         this.dt = dt;
     }
 
+    /**
+     * Return Time of event
+     */
     public long getTime() {
         return time;
     }
 
+    /**
+     * Set Time of event
+     */
     public void setTime(long time) {
         this.time = time;
     }
