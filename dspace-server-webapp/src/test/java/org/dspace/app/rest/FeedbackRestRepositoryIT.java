3a600e930abc4ed276499753f5b069f79f81342f
Mykhaylo <michele.boychuk@gmail.com>
2021-12-03 17:05:27 +0100

    refactoring tests

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/FeedbackRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/FeedbackRestRepositoryIT.java
@@ -6,13 +6,22 @@
  * http://www.dspace.org/license/
  */
 package org.dspace.app.rest;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNotNull;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.dspace.app.rest.model.FeedbackRest;
+import org.dspace.app.rest.repository.FeedbackRestRepository;
 import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
+import org.dspace.content.FeedbackServiceImpl;
+import org.dspace.content.service.FeedbackService;
 import org.dspace.services.ConfigurationService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +35,8 @@ public class FeedbackRestRepositoryIT extends AbstractControllerIntegrationTest 
 
     @Autowired
     private ConfigurationService configurationService;
+    @Autowired
+    private FeedbackRestRepository feedbackRestRepository;
 
     @Test
     public void findAllTest() throws Exception {
@@ -43,47 +54,80 @@ public class FeedbackRestRepositoryIT extends AbstractControllerIntegrationTest 
 
     @Test
     public void sendFeedbackTest() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        FeedbackRest feedbackRest = new FeedbackRest();
+        configurationService.setProperty("feedback.recipient", "recipient.email@test.com");
+        FeedbackService originFeedbackService = feedbackRestRepository.getFeedbackService();
+        try {
+            FeedbackService feedbackServiceMock = mock (FeedbackServiceImpl.class);
+            feedbackRestRepository.setFeedbackService(feedbackServiceMock);
 
-        feedbackRest.setEmail("misha.boychuk@test.com");
-        feedbackRest.setMessage("My feedback!");
+            ObjectMapper mapper = new ObjectMapper();
+            FeedbackRest feedbackRest = new FeedbackRest();
 
-        String authToken = getAuthToken(admin.getEmail(), password);
-        getClient(authToken).perform(post("/api/core/feedbacks")
-                            .content(mapper.writeValueAsBytes(feedbackRest))
-                            .contentType(contentType))
-                            .andExpect(status().isCreated());
+            feedbackRest.setEmail("misha.boychuk@test.com");
+            feedbackRest.setMessage("My feedback!");
+
+            String authToken = getAuthToken(admin.getEmail(), password);
+            getClient(authToken).perform(post("/api/core/feedbacks")
+                                .content(mapper.writeValueAsBytes(feedbackRest))
+                                .contentType(contentType))
+                                .andExpect(status().isCreated());
+
+            verify(feedbackServiceMock).sendEmail(isNotNull(), isNotNull(), eq("recipient.email@test.com"),
+                    eq("misha.boychuk@test.com"), eq("My feedback!"), isNull());
+
+            verifyNoMoreInteractions(feedbackServiceMock);
+        } finally {
+            feedbackRestRepository.setFeedbackService(originFeedbackService);
+        }
+
     }
 
     @Test
     public void sendFeedbackWithRecipientEmailNotConfiguredTest() throws Exception {
         configurationService.setProperty("feedback.recipient", null);
-        ObjectMapper mapper = new ObjectMapper();
-        FeedbackRest feedbackRest = new FeedbackRest();
+        FeedbackService originFeedbackService = feedbackRestRepository.getFeedbackService();
+        try {
+            FeedbackService feedbackServiceMock = mock (FeedbackServiceImpl.class);
+            feedbackRestRepository.setFeedbackService(feedbackServiceMock);
+            ObjectMapper mapper = new ObjectMapper();
+            FeedbackRest feedbackRest = new FeedbackRest();
 
-        feedbackRest.setEmail("misha.boychuk@test.com");
-        feedbackRest.setMessage("My feedback!");
+            feedbackRest.setEmail("misha.boychuk@test.com");
+            feedbackRest.setMessage("My feedback!");
 
-        String authToken = getAuthToken(admin.getEmail(), password);
-        getClient(authToken).perform(post("/api/core/feedbacks")
-                            .content(mapper.writeValueAsBytes(feedbackRest))
-                            .contentType(contentType))
-                            .andExpect(status().isNotFound());
+            String authToken = getAuthToken(admin.getEmail(), password);
+            getClient(authToken).perform(post("/api/core/feedbacks")
+                                .content(mapper.writeValueAsBytes(feedbackRest))
+                                .contentType(contentType))
+                                .andExpect(status().isNotFound());
+
+            verifyNoMoreInteractions(feedbackServiceMock);
+        } finally {
+            feedbackRestRepository.setFeedbackService(originFeedbackService);
+        }
     }
 
     @Test
     public void sendFeedbackBadRequestTest() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        FeedbackRest feedbackRest = new FeedbackRest();
+        FeedbackService originFeedbackService = feedbackRestRepository.getFeedbackService();
+        try {
+            FeedbackService feedbackServiceMock = mock (FeedbackServiceImpl.class);
+            feedbackRestRepository.setFeedbackService(feedbackServiceMock);
+            ObjectMapper mapper = new ObjectMapper();
+            FeedbackRest feedbackRest = new FeedbackRest();
 
-        feedbackRest.setMessage("My feedback!");
+            feedbackRest.setMessage("My feedback!");
 
-        String authToken = getAuthToken(admin.getEmail(), password);
-        getClient(authToken).perform(post("/api/core/feedbacks")
-                            .content(mapper.writeValueAsBytes(feedbackRest))
-                            .contentType(contentType))
-                            .andExpect(status().isBadRequest());
+            String authToken = getAuthToken(admin.getEmail(), password);
+            getClient(authToken).perform(post("/api/core/feedbacks")
+                                .content(mapper.writeValueAsBytes(feedbackRest))
+                                .contentType(contentType))
+                                .andExpect(status().isBadRequest());
+
+            verifyNoMoreInteractions(feedbackServiceMock);
+        } finally {
+            feedbackRestRepository.setFeedbackService(originFeedbackService);
+        }
     }
 
 }
