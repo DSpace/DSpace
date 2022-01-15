7e19ddf93aa38ed53bed46757b5f7a4345ffb5cf
Michael Spalti <mspalti@gmail.com>
2022-01-14 16:00:59 -0800

    Update manifestService after rebase.

--- dspace-iiif/src/main/java/org/dspace/app/iiif/service/ManifestService.java
+++ dspace-iiif/src/main/java/org/dspace/app/iiif/service/ManifestService.java
@@ -143,8 +143,10 @@ public class ManifestService extends AbstractResourceService {
 
         // Set the root Range for this manifest.
         rangeService.setRootRange(manifestId);
-        // Get bundles that can contain IIIF manifest data.
+        // Get bundles that contain manifest data.
         List<Bundle> bundles = utils.getIIIFBundles(item);
+        // Set the default canvas dimensions.
+        canvasService.setCanvasDimensions(bundles);
         for (Bundle bnd : bundles) {
             String bundleToCPrefix = null;
             if (bundles.size() > 1) {
@@ -152,13 +154,13 @@ public class ManifestService extends AbstractResourceService {
                 bundleToCPrefix = utils.getBundleIIIFToC(bnd);
             }
             for (Bitstream bitstream : utils.getIIIFBitstreams(context, bnd)) {
-                // Add the Canvas to the manifest Sequence.
+                // Add the Canvas to the Sequence.
                 CanvasGenerator canvas = sequenceService.addCanvas(context, item, bnd, bitstream);
                 // Update the Ranges.
                 rangeService.updateRanges(bitstream, bundleToCPrefix, canvas);
             }
         }
-        // If Ranges were created, add them to manifest Structures element.
+        // If Ranges were created, add them to manifest.
         Map<String, RangeGenerator> tocRanges = rangeService.getTocRanges();
         if (tocRanges != null && tocRanges.size() > 0) {
             RangeGenerator rootRange = rangeService.getRootRange();
