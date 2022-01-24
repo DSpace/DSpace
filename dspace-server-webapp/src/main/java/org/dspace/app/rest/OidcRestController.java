4cf38c4a8e0b276d7065e2b4049de6cf8db1b3b7
Hardy Pottinger <hardy.pottinger@gmail.com>
2022-01-24 13:19:22 -0600

    Fix checkstyle error introduced by VScode's 'organize imports' suggestion

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/OidcRestController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/OidcRestController.java
@@ -10,7 +10,6 @@ package org.dspace.app.rest;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletResponse;
 
