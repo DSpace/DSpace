9569c441befe36074d9255c55d7432728a2dc335
William Welling <8352733+wwelling@users.noreply.github.com>
2022-02-28 23:05:16 -0600

    add ldn release event handler

---
+++ dspace-ldn/src/main/java/org/dspace/app/ldn/consumer/LDNReleaseEventConsumer.java
@@ -0,0 +1,33 @@
+package org.dspace.app.ldn.consumer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dspace.core.Context;
+import org.dspace.event.Consumer;
+import org.dspace.event.Event;
+
+public class LDNReleaseEventConsumer implements Consumer {
+
+    private final static Logger log = LogManager.getLogger(LDNReleaseEventConsumer.class);
+
+    @Override
+    public void initialize() throws Exception {
+        log.info("LDN Release Event consumer started");
+    }
+
+    @Override
+    public void consume(Context ctx, Event event) throws Exception {
+        log.info("LDN Release Event consumer consumed {} {}", event.getObjectTypeAsString(), event.getEventTypeAsString());
+    }
+
+    @Override
+    public void end(Context ctx) throws Exception {
+        log.info("LDN Release Event consumer ended");
+    }
+
+    @Override
+    public void finish(Context ctx) throws Exception {
+        log.info("LDN Release Event consumer finished");
+    }
+
+}
