02e3a1ea92cc4e27cc6319d08f5551d695abb0d2
Joost <joost.fock@atmire.com>
2022-03-02 09:15:45 +0100

    Revert "removing autowired in dspace-api querybuilder"
    This reverts commit 252ef2476510f753dbc36c5fd981c70ec3aaa3f7.

--- dspace-api/src/main/java/org/dspace/discovery/utils/DiscoverQueryBuilder.java
+++ dspace-api/src/main/java/org/dspace/discovery/utils/DiscoverQueryBuilder.java
@@ -39,8 +39,6 @@ import org.dspace.discovery.configuration.DiscoverySortFieldConfiguration;
 import org.dspace.discovery.indexobject.factory.IndexFactory;
 import org.dspace.discovery.utils.parameter.QueryBuilderSearchFilter;
 import org.dspace.services.ConfigurationService;
-import org.dspace.services.factory.DSpaceServicesFactory;
-import org.dspace.utils.DSpace;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -48,11 +46,14 @@ public class DiscoverQueryBuilder implements InitializingBean {
 
     private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(DiscoverQueryBuilder.class);
 
-    private SearchService searchService = new DSpace().getServiceManager().getServicesByType(SearchService.class).get(0);
+    @Autowired
+    private SearchService searchService;
 
-    private ConfigurationService configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
+    @Autowired
+    private ConfigurationService configurationService;
 
-    private List<IndexFactory> indexableFactories = new DSpace().getServiceManager().getServicesByType(IndexFactory.class);
+    @Autowired
+    private List<IndexFactory> indexableFactories;
 
     private int pageSizeLimit;
 
