460e11d38f28395031b7242e2211dd50129b26b2
Mark H. Wood <mwood@IUPUI.Edu>
2022-04-04 16:34:47 -0400

    Ported from v5.6.  #8234

---
+++ dspace-api/src/main/java/org/dspace/disseminate/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Bibliographic data for DSpace objects.
+ * <ul>
+ *  <li>{@link CitationDocumentServiceImpl} is for appending a citation / cover
+ *      page to a PDF.</li>
+ *  <li>{@link CSLBibliographyGenerator} is for generating embeddable citations in
+ *      various formats.</li>
+ * </ul>
+ */
+package org.dspace.disseminate;
