fb31905ed5f7bb20f014d142395cef8f3ef11ee4
eskander <mohamed.eskander@atis.al>
2022-04-04 15:58:06 +0200

    [CST-5534] canSynchronizeWithORCID Authorization feature.

---
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/authorization/impl/CanSynchronizeWithORCID.java
@@ -0,0 +1,73 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.authorization.impl;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.function.Predicate;
+
+import org.apache.commons.lang3.StringUtils;
+import org.dspace.app.rest.authorization.AuthorizationFeature;
+import org.dspace.app.rest.authorization.AuthorizationFeatureDocumentation;
+import org.dspace.app.rest.model.BaseObjectRest;
+import org.dspace.app.rest.model.ItemRest;
+import org.dspace.content.Item;
+import org.dspace.content.MetadataValue;
+import org.dspace.content.service.ItemService;
+import org.dspace.core.Context;
+import org.dspace.eperson.EPerson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * The synchronization with ORCID feature. It can be used to verify
+ * if the user can synchronize with ORCID.
+ *
+ * @author Mohamed Eskander (mohamed.eskander at 4science.it)
+ */
+@Component
+@AuthorizationFeatureDocumentation(name = CanSynchronizeWithORCID.NAME,
+    description = "It can be used to verify if the user can synchronize with ORCID")
+public class CanSynchronizeWithORCID implements AuthorizationFeature {
+
+    public static final String NAME = "canSynchronizeWithORCID";
+
+    @Autowired
+    private ItemService itemService;
+
+    @Override
+    public boolean isAuthorized(Context context, BaseObjectRest object) throws SQLException {
+
+        EPerson ePerson = context.getCurrentUser();
+
+        if (!(object instanceof ItemRest) || Objects.isNull(ePerson)) {
+            return false;
+        }
+
+        String id = ((ItemRest) object).getId();
+        Item item = itemService.find(context, UUID.fromString(id));
+
+        return isDspaceObjectOwner(ePerson, item);
+    }
+
+    @Override
+    public String[] getSupportedTypes() {
+        return new String[] { ItemRest.CATEGORY + "." + ItemRest.NAME };
+    }
+
+    private boolean isDspaceObjectOwner(EPerson eperson, Item item) {
+        if (eperson == null) {
+            return false;
+        }
+        List<MetadataValue> owners = itemService.getMetadataByMetadataString(item, "dspace.object.owner");
+        Predicate<MetadataValue> checkOwner = v -> StringUtils.equals(v.getAuthority(), eperson.getID().toString());
+        return owners.stream().anyMatch(checkOwner);
+    }
+}
