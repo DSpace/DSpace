55106c2351944b486370255f3c6b20a24b852b4f
Luca Giamminonni <luca.giamminonni@4science.it>
2022-04-07 19:13:26 +0200

    [CST-5288] Added Order to the VersionInfoContributor

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/configuration/ActuatorConfiguration.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/configuration/ActuatorConfiguration.java
@@ -29,6 +29,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.hateoas.Link;
 
 /**
@@ -90,6 +91,7 @@ public class ActuatorConfiguration {
     }
 
     @Bean
+    @Order
     public VersionInfoContributor versionInfoContributor() {
         return new VersionInfoContributor();
     }
