d8b0111c9c88aacfacddc47e977590c82edd8fa3
Joost <joost.fock@atmire.com>
2022-04-25 12:59:23 +0200

    [task 90900] checkstyle fixes

--- dspace-api/src/test/java/org/dspace/app/bulkedit/MetadataExportSearchIT.java
+++ dspace-api/src/test/java/org/dspace/app/bulkedit/MetadataExportSearchIT.java
@@ -10,7 +10,6 @@ package org.dspace.app.bulkedit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
