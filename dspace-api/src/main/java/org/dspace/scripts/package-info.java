b65fa09e82b91ff4a4e6da84743cc03749bf327c
Mark H. Wood <mwood@iu.edu>
2022-05-06 15:35:45 -0400

    [DS-3673] Satisfy license plugin.

--- dspace-api/src/main/java/org/dspace/scripts/package-info.java
+++ dspace-api/src/main/java/org/dspace/scripts/package-info.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
  /**
   * Support for tools that can be run either from the command line or from the
   * DSpace GUI.
