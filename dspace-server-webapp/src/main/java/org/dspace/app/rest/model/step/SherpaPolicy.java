dc100eb5b8719b53d965d9bd763a2afc2da1e551
Mykhaylo <michele.boychuk@gmail.com>
2022-05-16 23:15:39 +0200

    [CST-5269] added missing JavaDoc

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/model/step/SherpaPolicy.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/model/step/SherpaPolicy.java
@@ -12,6 +12,8 @@ import java.util.Date;
 import org.dspace.app.sherpa.v2.SHERPAResponse;
 
 /**
+ * Java Bean to expose Sherpa policies during in progress submission.
+ * 
  * @author Mykhaylo Boychuk (mykhaylo.boychuk at 4science.com)
  */
 public class SherpaPolicy implements SectionData {
@@ -34,6 +36,10 @@ public class SherpaPolicy implements SectionData {
         return sherpaResponse;
     }
 
+    /**
+     * Setting a sherpaResponse will automatically set the retrievealTime
+     * of the section copying the value from the response if not null
+     */
     public void setSherpaResponse(SHERPAResponse sherpaResponse) {
         this.sherpaResponse = sherpaResponse;
         this.retrievalTime = sherpaResponse.getRetrievalTime();
