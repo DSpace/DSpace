ed03b107db44e300db171deb8049b84661b99d8b
Vincenzo Mecca <vincenzo.mecca@4science.com>
2022-06-15 14:39:13 +0200

    feature: Added handle in ItemBuilder
    Partial porting of [CST-2501] for withHandle method in ItemBuilder

--- dspace-api/src/test/java/org/dspace/builder/ItemBuilder.java
+++ dspace-api/src/test/java/org/dspace/builder/ItemBuilder.java
@@ -36,6 +36,7 @@ import org.dspace.eperson.Group;
 public class ItemBuilder extends AbstractDSpaceObjectBuilder<Item> {
 
     private boolean withdrawn = false;
+    private String handle = null;
     private WorkspaceItem workspaceItem;
     private Item item;
     private Group readerGroup = null;
@@ -163,6 +164,11 @@ public class ItemBuilder extends AbstractDSpaceObjectBuilder<Item> {
         return this;
     }
 
+    public ItemBuilder withHandle(String handle) {
+        this.handle = handle;
+        return this;
+    }
+
     /**
      * Withdrawn the item under build. Please note that an user need to be loggedin the context to avoid NPE during the
      * creation of the provenance metadata
@@ -202,7 +208,7 @@ public class ItemBuilder extends AbstractDSpaceObjectBuilder<Item> {
     @Override
     public Item build() {
         try {
-            installItemService.installItem(context, workspaceItem);
+            installItemService.installItem(context, workspaceItem, this.handle);
             itemService.update(context, item);
 
             //Check if we need to make this item private. This has to be done after item install.
