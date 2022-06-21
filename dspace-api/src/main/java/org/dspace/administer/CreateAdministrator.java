3ad33675e3fba162cd4d5b607ec59549d2da5387
ammanabrolua <aditya.ammanabrolu@tib.eu>
2022-06-21 19:26:17 +0200

    added conditionals

--- dspace-api/src/main/java/org/dspace/administer/CreateAdministrator.java
+++ dspace-api/src/main/java/org/dspace/administer/CreateAdministrator.java
@@ -101,7 +101,8 @@ public final class CreateAdministrator {
                                    line.getOptionValue("c"), line.getOptionValue("p"));
         } else {
 
-        if(line.hasOption("h"))
+        if(line.hasOption("h") && (!line.hasOption("e") || !line.hasOption("f") || !line.hasOption("l") ||
+            !line.hasOption("c") || !line.hasOption("p")))
 		
         {
 			String header = "\nA command-line tool for creating an initial administrator for setting up a" +
@@ -113,7 +114,17 @@ public final class CreateAdministrator {
 			HelpFormatter formatter = new HelpFormatter();
 			formatter.printHelp("dspace create-administrator", header, options, footer, true);
             return;
-		}
+		} else { 
+            String header = "\nA command-line tool for creating an initial administrator for setting up a" +
+					" DSpace site. Unless all the required parameters are passed it will prompt for an e-mail" +
+					" address, last name, first name and password from standard input. An administrator group is" +
+					" then created and the data passed in used to create an e-person in that group.\n\n";
+			String footer = "\n";
+
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("dspace create-administrator", header, options, footer, true);
+            return;
+        }
 
         ConfigurationService cfg = DSpaceServicesFactory.getInstance().getConfigurationService();
 
