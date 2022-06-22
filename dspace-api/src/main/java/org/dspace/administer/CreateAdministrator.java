9362d9692df3bd9dbde7a5b23af7d360f0c10f6e
ammanabrolua <aditya.ammanabrolu@tib.eu>
2022-06-22 08:41:10 +0200

    solve checkstyle errors

--- dspace-api/src/main/java/org/dspace/administer/CreateAdministrator.java
+++ dspace-api/src/main/java/org/dspace/administer/CreateAdministrator.java
@@ -87,12 +87,12 @@ public final class CreateAdministrator {
         try {
 
             line = parser.parse(options, argv);
-        
+
         } catch (Exception e) {
 		
             System.out.println(e.getMessage() + "\nTry \"dspace create-administrator -h\" to print help information.");
 		    System.exit(1);
-		
+
         }
 
         if (line.hasOption("e") && line.hasOption("f") && line.hasOption("l") &&
