6060070eac843fe6c98fb1742089a538a678301f
ammanabrolua <aditya.ammanabrolu@tib.eu>
2022-06-22 09:00:48 +0200

    fix indentation

--- dspace-api/src/main/java/org/dspace/administer/CreateAdministrator.java
+++ dspace-api/src/main/java/org/dspace/administer/CreateAdministrator.java
@@ -106,9 +106,9 @@ public final class CreateAdministrator {
                 !line.hasOption("c") || !line.hasOption("p"))) {
                 String header = "\nA command-line tool for creating an initial administrator for setting up a" +
                     " DSpace site. Unless all the required parameters are passed it will" +
-					" prompt for an e-mail address, last name, first name and password from standard input." +
-					" An administrator group is then created and the data passed in used" +
-                    " to create an e-person in that group.\n\n";
+					" prompt for an e-mail address, last name, first name and password from" +
+					" standard input.. An administrator group is then created and the data passed" +
+                    "  in used to create an e-person in that group.\n\n";
 			    String footer = "\n";
 
 			    HelpFormatter formatter = new HelpFormatter();
@@ -117,15 +117,15 @@ public final class CreateAdministrator {
 		} else if (line.hasOption("h") && (line.hasOption("e") || line.hasOption("f") || line.hasOption("l") ||
                   line.hasOption("c") || line.hasOption("p"))) { 
                 String header = "\nA command-line tool for creating an initial administrator for setting up a" +
-					" DSpace site. Unless all the required parameters are passed it will" +
-					" prompt for an e-mail address, last name, first name and password from standard input." +
-					" An administrator group is then created and the data passed in used" +
-                    " to create an e-person in that group.\n\n";
-			String footer = "\n";
+                    " DSpace site. Unless all the required parameters are passed it will" +
+					" prompt for an e-mail address, last name, first name and password from" +
+					" standard input.. An administrator group is then created and the data passed" +
+                    "  in used to create an e-person in that group.\n\n";
+			    String footer = "\n";
 
-			HelpFormatter formatter = new HelpFormatter();
-			formatter.printHelp("dspace create-administrator", header, options, footer, true);
-            return;
+			    HelpFormatter formatter = new HelpFormatter();
+			    formatter.printHelp("dspace create-administrator", header, options, footer, true);
+                return;
         } else {
 
             ConfigurationService cfg = DSpaceServicesFactory.getInstance().getConfigurationService();
