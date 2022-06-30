9bc9f8e6e79cbe74fb6f1aba320e123145a7c183
MilanMajchrák <milan.majchak@dataquest.sk>
2022-06-30 14:25:16 +0200

    updated script names

--- scripts/envs/__basic.example.bat
+++ scripts/envs/__basic.example.bat
@@ -1,4 +1,4 @@
 rem Set those paths to relevant places in your computer, copy this file and rename the copy to "paths.bat"
-set dspace_source=C:\Data\proj\DSpace\
-set tomcat=C:\soft\apache-tomcat-9.0.64\
+set dspace_source=C:\workspace\DSpace\
+set tomcat=C:\apache-tomcat-9.0.64\
 set dspace_application=C:\dspace\
