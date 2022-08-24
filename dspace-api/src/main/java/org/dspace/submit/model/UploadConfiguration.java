5e8a4ed3974a6558499c6136336d461b1448344a
Mark H. Wood <mwood@IUPUI.Edu>
2022-08-24 12:32:42 -0400

    Document this interface.

--- dspace-api/src/main/java/org/dspace/submit/model/UploadConfiguration.java
+++ dspace-api/src/main/java/org/dspace/submit/model/UploadConfiguration.java
@@ -13,6 +13,7 @@ import javax.inject.Inject;
 import org.dspace.services.ConfigurationService;
 
 /**
+ * A collection of conditions to be met when uploading Bitstreams.
  * @author Luigi Andrea Pascarelli (luigiandrea.pascarelli at 4science.it)
  */
 public class UploadConfiguration {
@@ -26,22 +27,43 @@ public class UploadConfiguration {
     private Boolean required;
     private String name;
 
+    /**
+     * The list of access restriction types from which a submitter may choose.
+     * @return choices for restricting access to Bitstreams.
+     */
     public List<AccessConditionOption> getOptions() {
         return options;
     }
 
+    /**
+     * Set the list of access restriction types from which to choose.
+     * Required.  May be empty.
+     * @param options choices for restricting access to Bitstreams.
+     */
     public void setOptions(List<AccessConditionOption> options) {
         this.options = options;
     }
 
+    /**
+     * Name of the submission form to which these conditions are attached.
+     * @return the form's name.
+     */
     public String getMetadata() {
         return metadataDefinition;
     }
 
+    /**
+     * Name the submission form to which these conditions are attached.
+     * @param metadata the form's name.
+     */
     public void setMetadata(String metadata) {
         this.metadataDefinition = metadata;
     }
 
+    /**
+     * Limit on the maximum size of an uploaded Bitstream.
+     * @return maximum upload size in bytes.
+     */
     public Long getMaxSize() {
         if (maxSize == null) {
             maxSize = configurationService.getLongProperty("upload.max");
@@ -49,10 +71,18 @@ public class UploadConfiguration {
         return maxSize;
     }
 
+    /**
+     * Limit the maximum size of an uploaded Bitstream.
+     * @param maxSize maximum upload size in bytes.
+     */
     public void setMaxSize(Long maxSize) {
         this.maxSize = maxSize;
     }
 
+    /**
+     * Is at least one Bitstream required when submitting a new Item?
+     * @return true if a Bitstream is required.
+     */
     public Boolean isRequired() {
         if (required == null) {
             //defaults to true
@@ -62,17 +92,27 @@ public class UploadConfiguration {
         return required;
     }
 
+    /**
+     * Is at least one Bitstream required when submitting a new Item?
+     * @param required true if a Bitstream is required.
+     */
     public void setRequired(Boolean required) {
         this.required = required;
     }
 
+    /**
+     * The unique name of this configuration.
+     * @return configuration's name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Give this configuration a unique name.  Required.
+     * @param name configuration's name.
+     */
     public void setName(String name) {
         this.name = name;
     }
-
-
 }
