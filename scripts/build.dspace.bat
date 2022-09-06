fdd6e572d09d77e5a8da613492b99371c9304dab
MajoBerger <marian.berger@dataquest.sk>
2022-09-06 08:32:44 +0200

    skip overwrite prompt in our script

--- scripts/build.dspace.bat
+++ scripts/build.dspace.bat
@@ -3,4 +3,4 @@ call mvn clean package
 cd %dspace_installer% || echo 'failure'
 call ant fresh_install 
 rd /s /q %server%
-xcopy /e /h /i /q %dspace_webapps% %tomcat_webapps% 
+xcopy /e /h /i /q /y %dspace_webapps% %tomcat_webapps% 
