abf65cd48c09bef520828fb54c04ee4c2bf48d65
Kim Shepherd <kim@shepherd.nz>
2022-09-16 10:17:54 +1200

    [TLC-249] Licence header fixes

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/model/IdentifiersRest.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/model/IdentifiersRest.java
@@ -2,7 +2,7 @@
  * The contents of this file are subject to the license and copyright
  * detailed in the LICENSE and NOTICE files at the root of the source
  * tree and available online at
- * <p>
+ *
  * http://www.dspace.org/license/
  */
 package org.dspace.app.rest.model;
