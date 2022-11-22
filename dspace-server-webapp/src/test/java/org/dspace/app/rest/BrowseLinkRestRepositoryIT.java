135385816a873538854df25a7113908810c1a022
Kim Shepherd <kim@shepherd.nz>
2022-11-22 16:47:28 +1300

    [TLC-380] Further checkstyle fixes

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/BrowseLinkRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/BrowseLinkRestRepositoryIT.java
@@ -10,8 +10,8 @@ package org.dspace.app.rest;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
