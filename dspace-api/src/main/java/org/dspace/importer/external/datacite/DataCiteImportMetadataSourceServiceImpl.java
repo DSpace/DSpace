360bc85ead8207a09ea37e705b000b7f24224f83
Philipp Rumpf <philipp.rumpf@uni-bamberg.de>
2022-12-08 14:57:15 +0000

    Merge pull request #5
    * DataCite plugin for import
    
    * start copying Crossref test
    
    * use dataCite original JSON
    
    * snapshot
    
    * adjust DataCite mapping to grab all titles
    
    * simplify DataCiteImportMetadataSourceServiceImpl. Avoid GSON, JsonPat…
    
    * rewrite DataCiteImportMetadataSourceServiceIT based on CrossRef IT.

--- dspace-api/src/main/java/org/dspace/importer/external/datacite/DataCiteImportMetadataSourceServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/importer/external/datacite/DataCiteImportMetadataSourceServiceImpl.java
@@ -9,29 +9,28 @@ package org.dspace.importer.external.datacite;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.Callable;
+import java.util.Map;
 import javax.el.MethodNotFoundException;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
 
-import com.google.gson.Gson;
-import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.ReadContext;
-import net.minidev.json.JSONArray;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.dspace.content.Item;
 import org.dspace.importer.external.datamodel.ImportRecord;
 import org.dspace.importer.external.datamodel.Query;
 import org.dspace.importer.external.exception.MetadataSourceException;
+import org.dspace.importer.external.liveimportclient.service.LiveImportClient;
 import org.dspace.importer.external.service.AbstractImportMetadataSourceService;
 import org.dspace.importer.external.service.DoiCheck;
 import org.dspace.importer.external.service.components.QuerySource;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Implements a data source for querying Datacite
@@ -39,13 +38,21 @@ import org.dspace.importer.external.service.components.QuerySource;
  *
  * optional Affiliation informations are not part of the API request.
  * https://support.datacite.org/docs/can-i-see-more-detailed-affiliation-information-in-the-rest-api
- * 
+ *
  * @author Pasquale Cavallo (pasquale.cavallo at 4science dot it)
  * @author Florian Gantner (florian.gantner@uni-bamberg.de)
  *
  */
 public class DataCiteImportMetadataSourceServiceImpl
-    extends AbstractImportMetadataSourceService<String> implements QuerySource {
+        extends AbstractImportMetadataSourceService<String> implements QuerySource {
+    private final static Logger log = LogManager.getLogger();
+
+    @Autowired
+    private LiveImportClient liveImportClient;
+
+    private final int timeoutMs = 180000;
+
+    private final String url = "https://api.datacite.org/dois/";
 
     private WebTarget webTarget;
 
@@ -56,91 +63,97 @@ public class DataCiteImportMetadataSourceServiceImpl
 
     @Override
     public void init() throws Exception {
-        Client client = ClientBuilder.newClient();
-        webTarget = client.target("https://api.datacite.org/dois/");
     }
 
     @Override
     public ImportRecord getRecord(String recordId) throws MetadataSourceException {
-        List<ImportRecord> records = null;
-        String id = getID(recordId);
-        if (StringUtils.isNotBlank(id)) {
-            records = retry(new SearchByIdCallable(id));
-        } else {
-            records = retry(new SearchByIdCallable(recordId));
+        Collection<ImportRecord> records = getRecords(recordId, 0, 1);
+        if (records.size() == 0) {
+            return null;
         }
-        return records == null || records.isEmpty() ? null : records.get(0);
+        return records.stream().findFirst().get();
     }
 
     @Override
     public int getRecordsCount(String query) throws MetadataSourceException {
-        String id = getID(query);
-        if (StringUtils.isNotBlank(id)) {
-            return retry(new DoiCheckCallable(id));
-        }
-        return retry(new CountByQueryCallable(query));
+        Collection<ImportRecord> records = getRecords(query, 0, -1);
+        return records == null ? 0 : records.size();
     }
 
     @Override
     public int getRecordsCount(Query query) throws MetadataSourceException {
         String id = getID(query.toString());
-        if (StringUtils.isNotBlank(id)) {
-            return retry(new DoiCheckCallable(id));
-        }
-        return retry(new CountByQueryCallable(query));
+        return getRecordsCount(StringUtils.isBlank(id) ? query.toString() : id);
     }
 
 
     @Override
     public Collection<ImportRecord> getRecords(String query, int start, int count) throws MetadataSourceException {
-        String id = getID(query.toString());
-        if (StringUtils.isNotBlank(id)) {
-            return retry(new SearchByIdCallable(id));
+        List<ImportRecord> records = new ArrayList<>();
+        String id = getID(query);
+        Map<String, Map<String, String>> params = new HashMap<>();
+        Map<String, String> uriParameters = new HashMap<>();
+        params.put("uriParameters", uriParameters);
+        if (StringUtils.isBlank(id)) {
+            id = query;
         }
-        return retry(new SearchByQueryCallable(query, count, start));
+        uriParameters.put("query", id);
+        String responseString = liveImportClient.executeHttpGetRequest(timeoutMs, url, params);
+        JsonNode jsonNode = convertStringJsonToJsonNode(responseString);
+        JsonNode dataNode = jsonNode.at("/data");
+        if (dataNode.isArray()) {
+            Iterator<JsonNode> iterator = dataNode.iterator();
+            while (iterator.hasNext()) {
+                JsonNode singleDoiNode = iterator.next();
+                String json = singleDoiNode.at("/attributes").toString();
+                records.add(transformSourceRecords(json));
+            }
+        } else {
+            String json = dataNode.at("/attributes").toString();
+            records.add(transformSourceRecords(json));
+        }
+
+        return records;
+    }
+
+    private JsonNode convertStringJsonToJsonNode(String json) {
+        try {
+            return new ObjectMapper().readTree(json);
+        } catch (JsonProcessingException e) {
+            log.error("Unable to process json response.", e);
+        }
+        return null;
     }
 
     @Override
     public Collection<ImportRecord> getRecords(Query query) throws MetadataSourceException {
         String id = getID(query.toString());
-        if (StringUtils.isNotBlank(id)) {
-            return retry(new SearchByIdCallable(id));
-        }
-        return retry(new SearchByQueryCallable(query));
+        return getRecords(StringUtils.isBlank(id) ? query.toString() : id, 0, -1);
     }
 
     @Override
     public ImportRecord getRecord(Query query) throws MetadataSourceException {
-        List<ImportRecord> records = null;
         String id = getID(query.toString());
-        if (StringUtils.isNotBlank(id)) {
-            records = retry(new SearchByIdCallable(id));
-        } else {
-            records = retry(new SearchByIdCallable(query));
-        }
-        return records == null || records.isEmpty() ? null : records.get(0);
+        return getRecord(StringUtils.isBlank(id) ? query.toString() : id);
     }
 
     @Override
     public Collection<ImportRecord> findMatchingRecords(Query query) throws MetadataSourceException {
         String id = getID(query.toString());
-        if (StringUtils.isNotBlank(id)) {
-            return retry(new SearchByIdCallable(id));
-        }
-        return retry(new FindMatchingRecordCallable(query));
+        return getRecords(StringUtils.isBlank(id) ? query.toString() : id, 0, -1);
     }
 
 
     @Override
     public Collection<ImportRecord> findMatchingRecords(Item item) throws MetadataSourceException {
-        throw new MethodNotFoundException("This method is not implemented for Datacite");
+        throw new MethodNotFoundException("This method is not implemented for DataCite");
     }
 
     public String getID(String query) {
         if (DoiCheck.isDoi(query)) {
             return query;
         }
-        //Workaround for encoded slashes.
+        // Workaround for encoded slashes.
         if (query.contains("%252F")) {
             query = query.replace("%252F", "/");
         }
@@ -149,231 +162,4 @@ public class DataCiteImportMetadataSourceServiceImpl
         }
         return StringUtils.EMPTY;
     }
-
-    private class SearchByQueryCallable implements Callable<List<ImportRecord>> {
-
-        private Query query;
-
-        private SearchByQueryCallable(String queryString, Integer maxResult, Integer start) {
-            query = new Query();
-            query.addParameter("query", queryString);
-            query.addParameter("count", maxResult);
-            query.addParameter("start", start);
-        }
-
-        private SearchByQueryCallable(Query query) {
-            this.query = query;
-        }
-
-        @Override
-        public List<ImportRecord> call() throws Exception {
-            List<ImportRecord> results = new ArrayList<>();
-            HttpGet method = null;
-            try {
-                Integer count = query.getParameterAsClass("count", Integer.class);
-                Integer start = query.getParameterAsClass("start", Integer.class);
-                WebTarget local = webTarget.queryParam("query", query.getParameterAsClass("query", String.class));
-                if (count != null) {
-                    local = local.queryParam("rows", count);
-                }
-                if (start != null) {
-                    local = local.queryParam("offset", start);
-                }
-                Invocation.Builder invocationBuilder = local.request();
-                Response response = invocationBuilder.get();
-                if (response.getStatus() != 200) {
-                    return null;
-                }
-                String responseString = response.readEntity(String.class);
-                ReadContext ctx = JsonPath.parse(responseString);
-                Object o = ctx.read("$.data.attributes");
-                if (o.getClass().isAssignableFrom(JSONArray.class)) {
-                    JSONArray array = (JSONArray)o;
-                    int size = array.size();
-                    for (int index = 0; index < size; index++) {
-                        Gson gson = new Gson();
-                        String innerJson = gson.toJson(array.get(index), LinkedHashMap.class);
-                        results.add(transformSourceRecords(innerJson));
-                    }
-                } else {
-                    results.add(transformSourceRecords(o.toString()));
-                }
-                return results;
-            } catch (Exception e) {
-                throw new RuntimeException(e.getMessage(), e);
-            } finally {
-                if (method != null) {
-                    method.releaseConnection();
-                }
-            }
-        }
-
-    }
-
-    private class SearchByIdCallable implements Callable<List<ImportRecord>> {
-        private Query query;
-
-        private SearchByIdCallable(Query query) {
-            this.query = query;
-        }
-
-        private SearchByIdCallable(String id) {
-            this.query = new Query();
-            query.addParameter("id", id);
-        }
-
-        @Override
-        public List<ImportRecord> call() throws Exception {
-            List<ImportRecord> results = new ArrayList<>();
-            HttpGet method = null;
-            try {
-                WebTarget local = webTarget.path(query.getParameterAsClass("id", String.class));
-                Invocation.Builder invocationBuilder = local.request();
-                Response response = invocationBuilder.get();
-                if (response.getStatus() != 200) {
-                    return null;
-                }
-                String responseString = response.readEntity(String.class);
-                ReadContext ctx = JsonPath.parse(responseString);
-                Object o = ctx.read("$.data.attributes");
-                if (o.getClass().isAssignableFrom(JSONArray.class)) {
-                    JSONArray array = (JSONArray)o;
-                    int size = array.size();
-                    for (int index = 0; index < size; index++) {
-                        Gson gson = new Gson();
-                        String innerJson = gson.toJson(array.get(index), LinkedHashMap.class);
-                        results.add(transformSourceRecords(innerJson));
-                    }
-                } else {
-                    Gson gson = new Gson();
-                    results.add(transformSourceRecords(gson.toJson(o, Object.class)));
-                }
-                return results;
-            } catch (Exception e) {
-                throw new RuntimeException(e.getMessage(), e);
-            } finally {
-                if (method != null) {
-                    method.releaseConnection();
-                }
-            }
-        }
-    }
-
-    private class FindMatchingRecordCallable implements Callable<List<ImportRecord>> {
-
-        private Query query;
-
-        private FindMatchingRecordCallable(Query q) {
-            query = q;
-        }
-
-        @Override
-        public List<ImportRecord> call() throws Exception {
-            String queryValue = query.getParameterAsClass("query", String.class);
-            Integer count = query.getParameterAsClass("count", Integer.class);
-            Integer start = query.getParameterAsClass("start", Integer.class);
-            List<ImportRecord> results = new ArrayList<>();
-            HttpGet method = null;
-            try {
-                WebTarget local = webTarget;
-                if (queryValue != null) {
-                    local = local.queryParam("query", queryValue);
-                }
-                if (count != null) {
-                    local = local.queryParam("page[size]", count);
-                }
-                if (start != null) {
-                    local = local.queryParam("page[number]", start);
-                }
-                Invocation.Builder invocationBuilder = local.request();
-                Response response = invocationBuilder.get();
-                if (response.getStatus() != 200) {
-                    return null;
-                }
-                String responseString = response.readEntity(String.class);
-                ReadContext ctx = JsonPath.parse(responseString);
-                Object o = ctx.read("$.data.attributes");
-                if (o.getClass().isAssignableFrom(JSONArray.class)) {
-                    JSONArray array = (JSONArray)o;
-                    int size = array.size();
-                    for (int index = 0; index < size; index++) {
-                        Gson gson = new Gson();
-                        String innerJson = gson.toJson(array.get(index), LinkedHashMap.class);
-                        results.add(transformSourceRecords(innerJson));
-                    }
-                } else {
-                    results.add(transformSourceRecords(o.toString()));
-                }
-                return results;
-            } catch (Exception e) {
-                throw new RuntimeException(e.getMessage(), e);
-            } finally {
-                if (method != null) {
-                    method.releaseConnection();
-                }
-            }
-        }
-
-    }
-
-    private class CountByQueryCallable implements Callable<Integer> {
-        private Query query;
-
-
-        private CountByQueryCallable(String queryString) {
-            query = new Query();
-            query.addParameter("query", queryString);
-        }
-
-        private CountByQueryCallable(Query query) {
-            this.query = query;
-        }
-
-
-        @Override
-        public Integer call() throws Exception {
-            HttpGet method = null;
-            try {
-                WebTarget local = webTarget.queryParam("query", query.getParameterAsClass("query", String.class));
-                Invocation.Builder invocationBuilder = local.request();
-                Response response = invocationBuilder.get();
-                if (response.getStatus() != 200) {
-                    return null;
-                }
-                String responseString = response.readEntity(String.class);
-                ReadContext ctx = JsonPath.parse(responseString);
-                return ctx.read("$.meta.total");
-            } catch (Exception e) {
-                throw new RuntimeException(e.getMessage(), e);
-            } finally {
-                if (method != null) {
-                    method.releaseConnection();
-                }
-            }
-        }
-    }
-
-    private class DoiCheckCallable implements Callable<Integer> {
-
-        private final Query query;
-
-        private DoiCheckCallable(final String id) {
-            final Query query = new Query();
-            query.addParameter("id", id);
-            this.query = query;
-        }
-
-        private DoiCheckCallable(final Query query) {
-            this.query = query;
-        }
-
-        @Override
-        public Integer call() throws Exception {
-            WebTarget local = webTarget.path(query.getParameterAsClass("id", String.class));
-            Invocation.Builder invocationBuilder = local.request();
-            Response response = invocationBuilder.head();
-            return response.getStatus() == 200 ? 1 : 0;
-        }
-    }
-
 }
