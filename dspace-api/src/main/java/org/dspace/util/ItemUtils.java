5b51d4910d444c9a48b98b14d90a4d0976d7c343
Kim Shepherd <kim@shepherd.nz>
2023-01-16 13:51:47 +1300

    [TLC-277] Remove unused method from ItemUtils

--- dspace-api/src/main/java/org/dspace/util/ItemUtils.java
+++ dspace-api/src/main/java/org/dspace/util/ItemUtils.java
@@ -7,15 +7,11 @@
  */
 package org.dspace.util;
 
-import java.io.IOException;
 import java.sql.SQLException;
 
-import org.dspace.authorize.AuthorizeException;
-import org.dspace.content.InProgressSubmission;
 import org.dspace.content.Item;
 import org.dspace.content.WorkspaceItem;
 import org.dspace.content.factory.ContentServiceFactory;
-import org.dspace.content.service.InProgressSubmissionService;
 import org.dspace.content.service.ItemService;
 import org.dspace.content.service.WorkspaceItemService;
 import org.dspace.core.Context;
@@ -31,43 +27,6 @@ public class ItemUtils {
     public final static int WITHDRAWN = 3;
 
     private ItemUtils() {
-    }
-
-    public static void removeOrWithdrawn(Context context, Item item)
-            throws SQLException, AuthorizeException, IOException {
-        // Find item in workspace or workflow...
-        InProgressSubmissionService inprogressService = getWorkspaceItemService();
-        InProgressSubmission inprogress = /* WorkspaceItem */getWorkspaceItemService().findByItem(context, item);
-        if (inprogress == null) {
-            inprogressService = getWorkflowService();
-            inprogress = /* WorkflowItem */getWorkflowService().findByItem(context, item);
-        }
-        // if we have an item that has been public at some time, better to keep
-        // it for history
-        if (item.getHandle() != null) {
-
-            // Reopened
-            if (inprogress != null) {
-                item.setOwningCollection(inprogress.getCollection());
-            }
-//            item.withdraw();
-            getItemService().withdraw(context, item);
-//            item.update();
-            getItemService().update(context, item);
-
-            // Delete wrapper
-            if (inprogress != null) {
-                inprogressService.deleteWrapper(context, inprogress);
-//                inprogress.deleteWrapper();
-            }
-
-        } else {
-            inprogressService.deleteWrapper(context, inprogress);
-//            inprogress.deleteWrapper();
-            getItemService().delete(context, item);
-//            item.delete();
-
-        }
     }
 
     public static int getItemStatus(Context context, Item item) throws SQLException {
