dbcd1b9525c83f788994f188c0049ee6b4d31eb0
Kim Shepherd <kim@shepherd.nz>
2023-01-25 14:27:19 +1300

    [TLC-249] Remove unused subclass DataIdentifier

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/model/step/DataIdentifiers.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/model/step/DataIdentifiers.java
@@ -60,30 +60,4 @@ public class DataIdentifiers implements SectionData {
         return sb.toString();
     }
 
-    protected class DataIdentifier {
-        private String type;
-        private String value;
-
-        public DataIdentifier(String type, String value) {
-            this.type = type;
-            this.value = value;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public String getValue() {
-            return value;
-        }
-
-        public void setValue(String value) {
-            this.value = value;
-        }
-    }
-
 }
