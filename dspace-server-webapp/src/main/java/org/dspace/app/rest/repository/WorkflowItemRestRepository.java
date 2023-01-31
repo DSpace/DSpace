3b1bc789cc287fea7ba43b49281719400a01c0d3
MajoBerger <88670521+MajoBerger@users.noreply.github.com>
2023-01-31 22:33:48 +0100

    feature/laf-2-oai-update-after-db-change (#184) create, delete, update item
    * solr reindex implemented
    
    * fix for PR
    
    * fix
    
    * try fix
    
    * nondelete
    
    * Throwing the Runtime Error during deleting or reindexing the Item was changed to logging an error.
    
    * Fixed checkstyle issue.
    
    * Do not throw Runtime Exception in the test
    
    ---------
    
    Co-authored-by: MilanMajchrák <milan.majchak@dataquest.sk>

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/WorkflowItemRestRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/WorkflowItemRestRepository.java
@@ -27,6 +27,8 @@ import org.dspace.app.rest.model.WorkflowItemRest;
 import org.dspace.app.rest.model.patch.Operation;
 import org.dspace.app.rest.model.patch.Patch;
 import org.dspace.app.rest.submit.SubmissionService;
+import org.dspace.app.rest.utils.SolrOAIReindexer;
+import org.dspace.app.util.SubmissionConfigReader;
 import org.dspace.app.util.SubmissionConfigReaderException;
 import org.dspace.authorize.AuthorizeException;
 import org.dspace.authorize.service.AuthorizeService;
@@ -110,7 +112,10 @@ public class WorkflowItemRestRepository extends DSpaceRestRepository<WorkflowIte
     @Autowired
     protected XmlWorkflowFactory workflowFactory;
 
-    private SubmissionConfigService submissionConfigService;
+    @Autowired
+    private SolrOAIReindexer solrOAIReindexer;
+
+    private final SubmissionConfigReader submissionConfigReader;
 
     public WorkflowItemRestRepository() throws SubmissionConfigReaderException {
         submissionConfigService = SubmissionServiceFactory.getInstance().getSubmissionConfigService();
@@ -177,10 +182,13 @@ public class WorkflowItemRestRepository extends DSpaceRestRepository<WorkflowIte
             throw new RuntimeException("SQLException in " + this.getClass() + "#findBySubmitter trying to create " +
                 "a workflow and adding it to db.", e);
         }
+        solrOAIReindexer.reindexItem(source.getItem());
         //if the item go directly in published status we have to manage a status code 204 with no content
         if (source.getItem().isArchived()) {
             return null;
         }
+
+
         return converter.toRest(source, utils.obtainProjection());
     }
 
