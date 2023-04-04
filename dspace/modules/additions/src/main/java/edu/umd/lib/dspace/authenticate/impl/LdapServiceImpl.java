7fc21974b64c02b4fe93dfe75fadfb619071d11d
David P. Steelman <dsteelma@umd.edu>
2023-04-04 07:13:20 -0400

    LIBDRUM-769. Refactored LdapServiceImpl to facilitate testing
    Refactored LdapServiceImpl, creating an LdapClient inner
    class that configures and performs operations against the real
    LDAP server. Intent is to make the actual LDAP server operations
    simpler to replace with a mock implementation for testing.
    
    https://umd-dit.atlassian.net/browse/LIBDRUM-769

--- dspace/modules/additions/src/main/java/edu/umd/lib/dspace/authenticate/impl/LdapServiceImpl.java
+++ dspace/modules/additions/src/main/java/edu/umd/lib/dspace/authenticate/impl/LdapServiceImpl.java
@@ -29,8 +29,6 @@ public class LdapServiceImpl implements LdapService {
     /** log4j category */
     private static Logger log = LogManager.getLogger(LdapServiceImpl.class);
 
-    private org.dspace.core.Context context = null;
-    private DirContext ctx = null;
     private String strUid = null;
     private SearchResult entry = null;
 
@@ -43,34 +41,30 @@ public class LdapServiceImpl implements LdapService {
         DSpaceServicesFactory.getInstance().getConfigurationService();
 
     /**
+     * The client for calls to the LDAP server. Made protected for use in
+     * tests
+     */
+    protected LdapClient client;
+
+    /**
      * Configures the LDAP connection, based on configuration and environment
      * variables.
      */
     public LdapServiceImpl(org.dspace.core.Context context) throws NamingException {
-        this.context = context;
+        this.client = createLdapClient(context);
+    }
 
-        String strUrl = configurationService.getProperty("drum.ldap.url");
-        String strBindAuth = configurationService.getProperty("drum.ldap.bind.auth");
-        String strBindPassword =  configurationService.getProperty("drum.ldap.bind.password");
-        String strConnectTimeout =  configurationService.getProperty("drum.ldap.connect.timeout");
-        String strReadTimeout =  configurationService.getProperty("drum.ldap.read.timeout");
-
-        // Setup the JNDI environment
-        Properties env = new Properties();
-
-        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-        env.put(Context.REFERRAL, "follow");
-
-        env.put(Context.PROVIDER_URL, strUrl);
-        env.put(Context.SECURITY_PROTOCOL, "ssl");
-        env.put(javax.naming.Context.SECURITY_PRINCIPAL, strBindAuth);
-        env.put(javax.naming.Context.SECURITY_CREDENTIALS, strBindPassword);
-        env.put("com.sun.jndi.ldap.connect.timeout", strConnectTimeout);
-        env.put("com.sun.jndi.ldap.read.timeout", strReadTimeout);
-
-        // Create the directory context
-        log.debug("Initailizing new LDAP context");
-        ctx = new InitialDirContext(env);
+    /**
+     * Returns the LdapClient for use in querying the LDAP server
+     *
+     * This class it provided to enable tests to override calls to the LDAP
+     * server.
+     *
+     * @return the LdapClient for use in querying the LDAP server
+     */
+    protected LdapClient createLdapClient(org.dspace.core.Context context)
+            throws NamingException {
+        return new LdapClient(context);
     }
 
     /**
@@ -82,57 +76,8 @@ public class LdapServiceImpl implements LdapService {
     @Override
     public Ldap queryLdap(String strUid) {
         try {
-            if (ctx == null) {
-                return null;
-            }
-
-            this.strUid = strUid;
-            String strFilter = "uid=" + strUid;
-
-            // Setup the search controls
-            SearchControls sc =  new SearchControls();
-            sc.setReturningAttributes(strRequestAttributes);
-            sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
-
-            // Search
-            NamingEnumeration<SearchResult> entries = ctx.search("", strFilter, sc);
-
-            // Make sure we got something
-            if (entries == null) {
-                log.warn(LogHelper.getHeader(context,
-                                              "null returned on ctx.search for " + strFilter,
-                                              ""));
-                return null;
-            }
-
-            // Check for a match
-            if (!entries.hasMore()) {
-                log.debug(LogHelper.getHeader(context,
-                                              "no matching entries for " + strFilter,
-                                              ""));
-                return null;
-            }
-
-
-            // Get entry
-            entry = (SearchResult)entries.next();
-            log.debug(LogHelper.getHeader(context,
-                                          "matching entry for " + strUid + ": " + entry.getName(),
-                                          ""));
-            Ldap ldap = new Ldap(strUid, entry);
-
-            // Check for another match
-            if (entries.hasMore()) {
-                entry = null;
-                log.warn(LogHelper.getHeader(context,
-                                              "multiple matching entries for " + strFilter,
-                                              ""));
-                return null;
-            }
-
-            log.debug(LogHelper.getHeader(context,
-                                          "ldap entry:\n" + entry,
-                                          ""));
+            Ldap ldap = null;
+            ldap = client.queryLdapService(strUid);
             return ldap;
         } catch (NamingException ne) {
             log.error("LDAP NamingException for '" + strUid + "'", ne);
@@ -145,13 +90,8 @@ public class LdapServiceImpl implements LdapService {
      */
     @Override
     public void close() {
-        if (ctx != null) {
-            try {
-                ctx.close();
-                ctx = null;
-            } catch (NamingException e) {
-                // Do nothing
-            }
+        if (client != null) {
+            client.close();
         }
     }
 
@@ -161,6 +101,121 @@ public class LdapServiceImpl implements LdapService {
             return "null";
         }
         return strUid + " (" + entry.getName() + ")";
+    }
+
+    /**
+     * Implementation that constructs an LDAP client and performs operations
+     * against a real LDAP server.
+     */
+    public static class LdapClient {
+        private SearchResult entry;
+        private org.dspace.core.Context context;
+        private DirContext ctx;
+
+        public LdapClient(org.dspace.core.Context context)
+                throws NamingException {
+            String strUrl = configurationService.getProperty("drum.ldap.url");
+            String strBindAuth = configurationService.getProperty("drum.ldap.bind.auth");
+            String strBindPassword =  configurationService.getProperty("drum.ldap.bind.password");
+            String strConnectTimeout =  configurationService.getProperty("drum.ldap.connect.timeout");
+            String strReadTimeout =  configurationService.getProperty("drum.ldap.read.timeout");
+
+            // Setup the JNDI environment
+            Properties env = new Properties();
+
+            env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+            env.put(Context.REFERRAL, "follow");
+
+            env.put(Context.PROVIDER_URL, strUrl);
+            env.put(Context.SECURITY_PROTOCOL, "ssl");
+            env.put(javax.naming.Context.SECURITY_PRINCIPAL, strBindAuth);
+            env.put(javax.naming.Context.SECURITY_CREDENTIALS, strBindPassword);
+            env.put("com.sun.jndi.ldap.connect.timeout", strConnectTimeout);
+            env.put("com.sun.jndi.ldap.read.timeout", strReadTimeout);
+
+
+            log.debug("Initializing new LDAP context");
+            this.ctx = new InitialDirContext(env);
+
+            this.context = context;
+        }
+
+        /**
+         * Returns an Ldap object returned by an LDAP server for the given user
+         * id, or null, of the user id is not found.
+         *
+         * @param strUid the user id to search for in the LDAP server.
+         * @return an Ldap object returned by an LDAP server for the given user
+         * id, or null, of the user id is not found.
+         */
+        public Ldap queryLdapService(String strUid) throws NamingException {
+            if (ctx == null) {
+                return null;
+            }
+
+            String strFilter = "uid=" + strUid;
+
+            // Setup the search controls
+            SearchControls sc =  new SearchControls();
+            sc.setReturningAttributes(strRequestAttributes);
+            sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
+
+            // Search
+            log.debug("Searching LDAP server for {}", strUid);
+            NamingEnumeration<SearchResult> entries = ctx.search("", strFilter, sc);
+
+            // Make sure we got something
+            if (entries == null) {
+                log.warn(LogHelper.getHeader(context,
+                                                "null returned on ctx.search for " + strFilter,
+                                                ""));
+                return null;
+            }
+
+            // Check for a match
+            if (!entries.hasMore()) {
+                log.debug(LogHelper.getHeader(context,
+                                                "no matching entries for " + strFilter,
+                                                ""));
+                return null;
+            }
+
+
+            // Get entry
+            entry = (SearchResult)entries.next();
+            log.debug(LogHelper.getHeader(context,
+                                            "matching entry for " + strUid + ": " + entry.getName(),
+                                            ""));
+            Ldap ldap = new Ldap(strUid, entry);
+
+            // Check for another match
+            if (entries.hasMore()) {
+                entry = null;
+                log.warn(LogHelper.getHeader(context,
+                                                "multiple matching entries for " + strFilter,
+                                                ""));
+                return null;
+            }
+
+            log.debug(LogHelper.getHeader(context,
+                                            "ldap entry:\n" + entry,
+                                            ""));
+            return ldap;
+        }
+
+        /**
+         * Close the ldap connection
+         */
+        public void close() {
+            if (ctx != null) {
+                try {
+                    ctx.close();
+                    ctx = null;
+                } catch (NamingException e) {
+                    // Do nothing
+                }
+            }
+        }
     }
 }
 
