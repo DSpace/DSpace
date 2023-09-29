9880e697b65c2284aea9d626008a96010af664de
Yana De Pauw <yana@atmire.com>
2023-09-29 15:36:53 +0200

    106812: Add h2 sql file

---
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/h2/V7.6_2023.09.28__enforce_group_or_eperson_for_resourcepolicy.sql
@@ -0,0 +1,10 @@
+--
+-- The contents of this file are subject to the license and copyright
+-- detailed in the LICENSE and NOTICE files at the root of the source
+-- tree and available online at
+--
+-- http://www.dspace.org/license/
+--
+
+ALTER TABLE ResourcePolicy ADD CONSTRAINT resourcepolicy_eperson_and_epersongroup_not_nullobject_chk
+    CHECK (eperson_id is not null or epersongroup_id is not null) ;
