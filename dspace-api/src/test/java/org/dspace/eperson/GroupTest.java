9d271b24b9721741a53142a690b86287efb738fe
Tim Donohue <tim.donohue@lyrasis.org>
2023-10-05 16:15:43 -0500

    Add isNotMemberOf for groups, including unit and integration tests

--- dspace-api/src/test/java/org/dspace/eperson/GroupTest.java
+++ dspace-api/src/test/java/org/dspace/eperson/GroupTest.java
@@ -680,6 +680,109 @@ public class GroupTest extends AbstractUnitTest {
         }
     }
 
+    @Test
+    // Tests searchNonMembers() and searchNonMembersCount()
+    // NOTE: This does not test pagination as that is tested in GroupRestRepositoryIT in server-webapp
+    public void searchAndCountNonMembers() throws SQLException, AuthorizeException, IOException {
+        // Create a parent group with 2 child groups
+        Group parentGroup = createGroup("Some Parent Group");
+        Group someStaffGroup = createGroup("Some Other Staff");
+        Group someStudentsGroup = createGroup("Some Students");
+        groupService.addMember(context, parentGroup, someStaffGroup);
+        groupService.addMember(context, parentGroup, someStudentsGroup);
+        groupService.update(context, parentGroup);
+
+        // Create a separate parent which is not a member of the first & add two child groups to it
+        Group studentsNotInParentGroup = createGroup("Students not in Parent");
+        Group otherStudentsNotInParentGroup = createGroup("Other Students");
+        Group someOtherStudentsNotInParentGroup = createGroup("Some Other Students");
+        groupService.addMember(context, studentsNotInParentGroup, otherStudentsNotInParentGroup);
+        groupService.addMember(context, studentsNotInParentGroup, someOtherStudentsNotInParentGroup);
+        groupService.update(context, studentsNotInParentGroup);
+
+        try {
+            // Assert that all Groups *not* in parent group match an empty search
+            List<Group> notInParent = Arrays.asList(studentsNotInParentGroup, otherStudentsNotInParentGroup,
+                                                    someOtherStudentsNotInParentGroup);
+            List<Group> nonMembersSearch = groupService.searchNonMembers(context, "", parentGroup, -1, -1);
+            // NOTE: Because others unit tests create groups, this search will return an undetermined number of results.
+            // Therefore, we just verify that our expected groups are included and others are NOT included.
+            assertTrue(nonMembersSearch.containsAll(notInParent));
+            // Verify it does NOT contain members of parentGroup
+            assertFalse(nonMembersSearch.contains(someStaffGroup));
+            assertFalse(nonMembersSearch.contains(someStudentsGroup));
+            // Verify it also does NOT contain the parentGroup itself
+            assertFalse(nonMembersSearch.contains(parentGroup));
+            // Verify the count for empty search matches the size of the search results
+            assertEquals(nonMembersSearch.size(), groupService.searchNonMembersCount(context, "", parentGroup));
+
+            // Assert a search on "Students" matches all those same groups (as they all include that word in their name)
+            nonMembersSearch = groupService.searchNonMembers(context, "Students", parentGroup, -1, -1);
+            assertTrue(nonMembersSearch.containsAll(notInParent));
+            //Verify an existing member group with "Students" in its name does NOT get returned
+            assertFalse(nonMembersSearch.contains(someStudentsGroup));
+            assertEquals(nonMembersSearch.size(),
+                         groupService.searchNonMembersCount(context, "Students", parentGroup));
+
+
+            // Assert a search on "other" matches just two groups
+            // (this also tests search is case insensitive)
+            nonMembersSearch = groupService.searchNonMembers(context, "other", parentGroup, -1, -1);
+            assertTrue(nonMembersSearch.containsAll(
+                Arrays.asList(otherStudentsNotInParentGroup, someOtherStudentsNotInParentGroup)));
+            // Verify an existing member group with "Other" in its name does NOT get returned
+            assertFalse(nonMembersSearch.contains(someStaffGroup));
+            assertEquals(nonMembersSearch.size(), groupService.searchNonMembersCount(context, "other", parentGroup));
+
+            // Assert a search on "Parent" matches just one group
+            nonMembersSearch = groupService.searchNonMembers(context, "Parent", parentGroup, -1, -1);
+            assertTrue(nonMembersSearch.contains(studentsNotInParentGroup));
+            // Verify Parent Group itself does NOT get returned
+            assertFalse(nonMembersSearch.contains(parentGroup));
+            assertEquals(nonMembersSearch.size(), groupService.searchNonMembersCount(context, "Parent", parentGroup));
+
+            // Assert a UUID search matching a non-member group will return just that one group
+            nonMembersSearch = groupService.searchNonMembers(context,
+                                                             someOtherStudentsNotInParentGroup.getID().toString(),
+                                                             parentGroup, -1, -1);
+            assertEquals(1, nonMembersSearch.size());
+            assertTrue(nonMembersSearch.contains(someOtherStudentsNotInParentGroup));
+            assertEquals(nonMembersSearch.size(),
+                         groupService.searchNonMembersCount(context,
+                                                            someOtherStudentsNotInParentGroup.getID().toString(),
+                                                            parentGroup));
+
+            // Assert a UUID search matching an EXISTING member will return NOTHING
+            // (as this group is excluded from the search)
+            nonMembersSearch = groupService.searchNonMembers(context, someStudentsGroup.getID().toString(),
+                                                             parentGroup,-1, -1);
+            assertEquals(0, nonMembersSearch.size());
+            assertEquals(nonMembersSearch.size(),
+                         groupService.searchNonMembersCount(context, someStudentsGroup.getID().toString(),
+                                                            parentGroup));
+
+            // Assert a UUID search matching Parent Group *itself* will return NOTHING
+            // (as this group is excluded from the search)
+            nonMembersSearch = groupService.searchNonMembers(context, parentGroup.getID().toString(),
+                                                             parentGroup,-1, -1);
+            assertEquals(0, nonMembersSearch.size());
+            assertEquals(nonMembersSearch.size(),
+                         groupService.searchNonMembersCount(context, parentGroup.getID().toString(),
+                                                            parentGroup));
+        } finally {
+            // Clean up our data
+            context.turnOffAuthorisationSystem();
+            groupService.delete(context, parentGroup);
+            groupService.delete(context, someStaffGroup);
+            groupService.delete(context, someStudentsGroup);
+            groupService.delete(context, studentsNotInParentGroup);
+            groupService.delete(context, otherStudentsNotInParentGroup);
+            groupService.delete(context, someOtherStudentsNotInParentGroup);
+            context.restoreAuthSystemState();
+        }
+
+    }
+
 
     protected Group createGroup(String name) throws SQLException, AuthorizeException {
         context.turnOffAuthorisationSystem();
