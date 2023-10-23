717d8f48bde201e47c044dfe5ecdb23dd96107d2
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2023-10-23 13:18:30 +0100

    Add integration test

---
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/matcher/ConfigurationMatcher.java
@@ -0,0 +1,23 @@
+package org.dspace.app.rest.matcher;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+
+
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+public class ConfigurationMatcher {
+
+    private ConfigurationMatcher() { }
+
+    // Matcher for configuration
+    public static Matcher<? super Object> matchConfiguration(String name) {
+        return allOf(
+                hasJsonPath("$.name", is(name)),
+                hasJsonPath("$.values", Matchers.not(Matchers.empty())),
+                hasJsonPath("$.type", is("property"))
+        );
+    }
+}
