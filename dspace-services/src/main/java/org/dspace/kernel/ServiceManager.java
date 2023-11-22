c514fc7430d40ac3c80698a568954a546024d321
mohamed eskander <mohamed.eskander@4science.com>
2023-11-22 18:15:36 +0200

    [CST-12752] updated javadoc of new method

--- dspace-services/src/main/java/org/dspace/kernel/ServiceManager.java
+++ dspace-services/src/main/java/org/dspace/kernel/ServiceManager.java
@@ -77,14 +77,11 @@ public interface ServiceManager {
     public List<String> getServicesNames();
 
     /**
-     * Allows developers to get the desired service singleton by the provided type. <br>
-     * This should return all instantiated objects of the type specified with their names
-     * (may not all be singletons).
+     * Get the names of all registered service singletons.  By
+     * convention, the name typically matches the fully qualified class
+     * name).
      *
-     * @param <T>  Class type
-     * @param type the type for the requested service (this will typically be the interface class but can be concrete
-     *            as well)
-     * @return map with service's name and service singletons
+     * @return the list of all current registered services
      */
     public <T> Map<String, T> getServicesWithNamesByType(Class<T> type);
 
