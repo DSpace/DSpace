481f0de80aa4a7d4659ece08a8a7ce0bb6b00ce1
Mykhaylo Boychuk <mykhaylo.boychuk@4science.it>
2023-11-28 01:04:34 +0100

    [CST-12108] added javadoc

--- dspace-api/src/main/java/org/dspace/correctiontype/CorrectionType.java
+++ dspace-api/src/main/java/org/dspace/correctiontype/CorrectionType.java
@@ -22,20 +22,62 @@ import org.dspace.qaevent.service.dto.QAMessageDTO;
  */
 public interface CorrectionType {
 
+    /**
+     * Retrieves the unique identifier associated to the CorrectionType.
+     */
     public String getId();
 
+    /**
+     * Retrieves the topic associated with the to the CorrectionType.
+     */
     public String getTopic();
 
     public String getCreationForm();
 
+    /**
+     * Checks whether the CorrectionType required related item.
+     */
     public boolean isRequiredRelatedItem();
 
+    /**
+     * Checks whether target item is allowed for current CorrectionType
+     * 
+     * @param context              Current DSpace session
+     * @param targetItem           Target item
+     * @throws AuthorizeException  if authorize error
+     * @throws SQLException        if there's a database problem
+     */
     public boolean isAllowed(Context context, Item targetItem) throws AuthorizeException, SQLException;
 
+    /**
+     * Checks whether target item and related item are allowed for current CorrectionType
+     * 
+     * @param context               Current DSpace session
+     * @param targetItem            Target item
+     * @param relatedItem           Related item
+     * @throws AuthorizeException   if authorize error
+     * @throws SQLException         if there's a database problem
+     */
     public boolean isAllowed(Context context, Item targetItem, Item relatedItem) throws AuthorizeException,SQLException;
 
+    /**
+     * Creates a QAEvent for a specific target item.
+     * 
+     * @param context      Current DSpace session
+     * @param targetItem   Target item
+     * @param reason       Reason
+     * @return             QAEvent
+     */
     public QAEvent createCorrection(Context context, Item targetItem, QAMessageDTO reason);
 
+    /**
+     *  Creates a QAEvent for a target item and related item.
+     * @param context      Current DSpace session
+     * @param targetItem   Target item
+     * @param relatedItem  Related item
+     * @param reason       Reason
+     * @return             QAEvent
+     */
     public QAEvent createCorrection(Context context, Item targetItem, Item relatedItem, QAMessageDTO reason);
 
 }
--- dspace-api/src/main/java/org/dspace/correctiontype/service/CorrectionTypeService.java
+++ dspace-api/src/main/java/org/dspace/correctiontype/service/CorrectionTypeService.java
@@ -22,12 +22,37 @@ import org.dspace.correctiontype.CorrectionType;
  */
 public interface CorrectionTypeService {
 
+    /**
+     * Retrieves a CorrectionType object from the system based on a unique identifier.
+     * 
+     * @param id The unique identifier of the CorrectionType object to be retrieved.
+     * @return   The CorrectionType object corresponding to the provided identifier,
+     *           or null if no object is found.
+     */
     public CorrectionType findOne(String id);
 
+    /**
+     * Retrieves a list of all CorrectionType objects available in the system.
+     * 
+     * @return Returns a List containing all CorrectionType objects in the system.
+     */
     public List<CorrectionType> findAll();
 
+    /**
+     * Retrieves a list of CorrectionType objects related to the provided Item.
+     * 
+     * @param context               Current DSpace session.
+     * @param item                  Target item
+     * @throws AuthorizeException   If authorize error
+     * @throws SQLException         If a database error occurs during the operation.
+     */
     public List<CorrectionType> findByItem(Context context, Item item) throws AuthorizeException, SQLException;
 
+    /**
+     * Retrieves a CorrectionType object associated with a specific topic.
+     * 
+     * @param topic  The topic for which the CorrectionType object is to be retrieved.
+     */
     public CorrectionType findByTopic(String topic);
 
 }
