dc57aceeaf74204326862a4d0efb781789c2b9aa
nwoodward <woodward.nicholas@gmail.com>
2024-01-16 13:28:40 -0600

    catch exceptions stemming from invalid id's
    (cherry picked from commit 848df25984b8d6cabb8b6b0c831070ffed66919a)

--- dspace-api/src/main/java/org/dspace/eperson/EPersonServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/eperson/EPersonServiceImpl.java
@@ -142,10 +142,15 @@ public class EPersonServiceImpl extends DSpaceObjectServiceImpl<EPerson> impleme
 
     @Override
     public EPerson findByIdOrLegacyId(Context context, String id) throws SQLException {
-        if (StringUtils.isNumeric(id)) {
-            return findByLegacyId(context, Integer.parseInt(id));
-        } else {
-            return find(context, UUID.fromString(id));
+        try {
+            if (StringUtils.isNumeric(id)) {
+                return findByLegacyId(context, Integer.parseInt(id));
+            } else {
+                return find(context, UUID.fromString(id));
+            }
+        } catch (IllegalArgumentException e) {
+            // Not a valid legacy ID or valid UUID
+            return null;
         }
     }
 
