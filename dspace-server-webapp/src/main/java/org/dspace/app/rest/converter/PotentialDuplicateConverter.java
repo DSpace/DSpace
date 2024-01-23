4515ded85f9687a79846df649925d998784239fc
Kim Shepherd <kim@shepherd.nz>
2024-01-24 11:57:35 +1300

    Fix out of date licence headers on PotentialDuplicateConverter/Resource

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/converter/PotentialDuplicateConverter.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/converter/PotentialDuplicateConverter.java
@@ -2,10 +2,9 @@
  * The contents of this file are subject to the license and copyright
  * detailed in the LICENSE and NOTICE files at the root of the source
  * tree and available online at
- * <p>
+ *
  * http://www.dspace.org/license/
  */
-
 package org.dspace.app.rest.converter;
 
 import org.dspace.app.rest.model.MetadataValueList;
