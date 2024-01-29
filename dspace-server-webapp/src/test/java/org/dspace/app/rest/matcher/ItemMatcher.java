69d069d2e5c09f99b6177cfffd39e27598907060
Kim Shepherd <kim@shepherd.nz>
2024-01-30 07:44:04 +1300

    [TLC-674] Update ItemMatcher embeds with duplicates link

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/matcher/ItemMatcher.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/matcher/ItemMatcher.java
@@ -52,6 +52,7 @@ public class ItemMatcher {
         return matchEmbeds(
                 "accessStatus",
                 "bundles[]",
+                "duplicates",
                 "identifiers",
                 "mappedCollections[]",
                 "owningCollection",
@@ -69,6 +70,7 @@ public class ItemMatcher {
         return HalMatcher.matchLinks(REST_SERVER_URL + "core/items/" + uuid,
                 "accessStatus",
                 "bundles",
+                "duplicates",
                 "identifiers",
                 "mappedCollections",
                 "owningCollection",
