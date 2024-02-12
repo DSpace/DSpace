55979e420e6f1a5adca1616e1b6bfe425c548a7b
Mark H. Wood <mwood@iu.edu>
2024-02-12 16:43:25 -0500

    Remove SLF4J from dspace-api.

--- dspace-api/src/main/java/org/dspace/statistics/util/SpiderDetectorServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/statistics/util/SpiderDetectorServiceImpl.java
@@ -21,10 +21,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.configuration2.ex.ConversionException;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.dspace.service.ClientInfoService;
 import org.dspace.services.ConfigurationService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -39,18 +39,18 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class SpiderDetectorServiceImpl implements SpiderDetectorService {
 
-    private static final Logger log = LoggerFactory.getLogger(SpiderDetectorServiceImpl.class);
+    private static final Logger log = LogManager.getLogger();
 
     private Boolean useCaseInsensitiveMatching;
 
     private final List<Pattern> agents
-        = Collections.synchronizedList(new ArrayList<Pattern>());
+        = Collections.synchronizedList(new ArrayList<>());
 
     private final List<Pattern> domains
-        = Collections.synchronizedList(new ArrayList<Pattern>());
+        = Collections.synchronizedList(new ArrayList<>());
 
-    private ConfigurationService configurationService;
-    private ClientInfoService clientInfoService;
+    private final ConfigurationService configurationService;
+    private final ClientInfoService clientInfoService;
 
     /**
      * Sparse HashTable structure to hold IP address ranges.
@@ -63,6 +63,7 @@ public class SpiderDetectorServiceImpl implements SpiderDetectorService {
         this.clientInfoService = clientInfoService;
     }
 
+    @Override
     public IPTable getTable() {
         return table;
     }
@@ -79,6 +80,7 @@ public class SpiderDetectorServiceImpl implements SpiderDetectorService {
      * @param agent    User-Agent header value, or null.
      * @return true if the client matches any spider characteristics list.
      */
+    @Override
     public boolean isSpider(String clientIP, String proxyIPs, String hostname, String agent) {
         // See if any agent patterns match
         if (null != agent) {
@@ -144,6 +146,7 @@ public class SpiderDetectorServiceImpl implements SpiderDetectorService {
      * @return a vector full of patterns
      * @throws IOException could not happen since we check the file be4 we use it
      */
+    @Override
     public Set<String> readPatterns(File patternFile)
         throws IOException {
         Set<String> patterns = new HashSet<>();
@@ -191,7 +194,7 @@ public class SpiderDetectorServiceImpl implements SpiderDetectorService {
                     patterns = readPatterns(file);
                 } catch (IOException ex) {
                     log.error("Patterns not read from {}:  {}",
-                              file.getPath(), ex.getMessage());
+                            file::getPath, ex::getMessage);
                     continue;
                 }
                 //If case insensitive matching is enabled, lowercase the patterns so they can be lowercase matched
@@ -203,10 +206,10 @@ public class SpiderDetectorServiceImpl implements SpiderDetectorService {
                 }
 
 
-                log.info("Loaded pattern file:  {}", file.getPath());
+                log.info("Loaded pattern file:  {}", file::getPath);
             }
         } else {
-            log.info("No patterns loaded from {}", patternsDir.getPath());
+            log.info("No patterns loaded from {}", patternsDir::getPath);
         }
     }
 
@@ -216,6 +219,7 @@ public class SpiderDetectorServiceImpl implements SpiderDetectorService {
      * @param request
      * @return true|false if the request was detected to be from a spider.
      */
+    @Override
     public boolean isSpider(HttpServletRequest request) {
         return isSpider(request.getRemoteAddr(),
                         request.getHeader("X-Forwarded-For"),
@@ -229,6 +233,7 @@ public class SpiderDetectorServiceImpl implements SpiderDetectorService {
      * @param ip
      * @return if is spider IP
      */
+    @Override
     public boolean isSpider(String ip) {
         if (table == null) {
             loadSpiderIpAddresses();
@@ -248,6 +253,7 @@ public class SpiderDetectorServiceImpl implements SpiderDetectorService {
     /*
      *  loader to populate the table from files.
      */
+    @Override
     public synchronized void loadSpiderIpAddresses() {
 
         if (table == null) {
