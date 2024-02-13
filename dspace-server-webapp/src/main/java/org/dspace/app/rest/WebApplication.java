dea8a79a68d946859a668579d4ac4354226469c2
Mark H. Wood <mwood@iu.edu>
2024-02-13 11:02:55 -0500

    Remove SLF4J from dspace-server-webapp.

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/WebApplication.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/WebApplication.java
@@ -22,8 +22,6 @@ import org.dspace.app.solrdatabaseresync.SolrDatabaseResyncCli;
 import org.dspace.app.util.DSpaceContextListener;
 import org.dspace.google.GoogleAsyncEventListener;
 import org.dspace.utils.servlet.DSpaceWebappServletFilter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -52,8 +50,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableCaching
 @Configuration
 public class WebApplication {
-
-    private static final Logger log = LoggerFactory.getLogger(WebApplication.class);
 
     @Autowired
     private ApplicationConfig configuration;
