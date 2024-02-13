8736bfd57d6cc59888ccd0e2b8e14e4523ff8bf2
Jean-François Morin <jean-francois.morin@bibl.ulaval.ca>
2024-02-13 11:29:49 -0500

    Disable findByMetadataQuery test to diagnose errors in other tests

--- dspace-api/src/test/java/org/dspace/content/service/ItemServiceIT.java
+++ dspace-api/src/test/java/org/dspace/content/service/ItemServiceIT.java
@@ -66,6 +66,7 @@ import org.dspace.versioning.Version;
 import org.dspace.versioning.factory.VersionServiceFactory;
 import org.dspace.versioning.service.VersioningService;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ItemServiceIT extends AbstractIntegrationTestWithDatabase {
@@ -934,6 +935,7 @@ public class ItemServiceIT extends AbstractIntegrationTestWithDatabase {
         assertThat(metadataValue.getPlace(), equalTo(place));
     }
 
+    @Ignore
     @Test
     public void testFindByMetadataQuery() throws Exception {
         context.turnOffAuthorisationSystem();
