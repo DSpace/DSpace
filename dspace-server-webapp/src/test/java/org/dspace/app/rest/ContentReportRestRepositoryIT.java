464ca4a2cc1ff325bec1d4538fde06aae6844ab4
Jean-François Morin <jean-francois.morin@bibl.ulaval.ca>
2024-02-16 16:07:56 -0500

    Attempt to fix ContentReportRestRepositoryIT tests

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/ContentReportRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/ContentReportRestRepositoryIT.java
@@ -19,6 +19,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.dspace.app.rest.contentreport.Filter;
@@ -107,7 +108,7 @@ public class ContentReportRestRepositoryIT extends AbstractControllerIntegration
                    )))
                    .andExpect(jsonPath("type", is("filteredcollectionsreport")))
                    .andExpect(jsonPath("$.summary",
-                           FilteredCollectionMatcher.matchFilteredCollectionSummary(3, 2)))
+                           FilteredCollectionMatcher.matchFilteredCollectionSummary(3, 3)))
                    .andExpect(jsonPath("$._links.self.href",
                            Matchers.containsString("/api/contentreport/filteredcollections")));
     }
@@ -127,7 +128,10 @@ public class ContentReportRestRepositoryIT extends AbstractControllerIntegration
         List<Item> items = new ArrayList<>();
 
         //2. Three items, two of which are available and discoverable...
+        UUID id1 = UUID.randomUUID();
         Item publicItem1 = ItemBuilder.createItem(context, col1)
+                                      .withIdentifier(id1.toString())
+                                      .withHandle("hdl1")
                                       .withTitle("Public item 1")
                                       .withIssueDate("2017-10-17")
                                       .withAuthor("Smith, Donald").withAuthor("Doe, John")
@@ -135,7 +139,10 @@ public class ContentReportRestRepositoryIT extends AbstractControllerIntegration
                                       .build();
         items.add(publicItem1);
 
+        UUID id2 = UUID.randomUUID();
         Item publicItem2 = ItemBuilder.createItem(context, col2)
+                                      .withIdentifier(id2.toString())
+                                      .withHandle("hdl2")
                                       .withTitle("Public item 2")
                                       .withIssueDate("2016-02-13")
                                       .withAuthor("Smith, Maria").withAuthor("Doe, Jane")
@@ -144,7 +151,10 @@ public class ContentReportRestRepositoryIT extends AbstractControllerIntegration
         items.add(publicItem2);
 
         // ... and one will be available in a few days.
+        UUID id3 = UUID.randomUUID();
         Item publicItem3 = ItemBuilder.createItem(context, col2)
+                                      .withIdentifier(id3.toString())
+                                      .withHandle("hdl3")
                                       .withTitle("Public item 3")
                                       .withIssueDate("2016-02-13")
                                       .withAuthor("Smith, Maria").withAuthor("Doe, Jane")
