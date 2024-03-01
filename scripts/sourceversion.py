a570122e3a3d94c93aa9af090e2208b58bfddf01
MajoBerger <88670521+MajoBerger@users.noreply.github.com>
2024-03-01 08:51:47 +0100

    Improve version info, add release-tag action
    add run link to version info
    
    correct image for tests (remove dspace-dependencies from migrate to 7.5)
    
    add action to release on tagging

--- scripts/sourceversion.py
+++ scripts/sourceversion.py
@@ -1,14 +1,13 @@
 import subprocess
+import sys
 from datetime import datetime
 
 if __name__ == '__main__':
     ts = datetime.now()
-    print(f"timestamp: {ts}")
+    print(f"This info was generated on: {ts}")
 
-    cmd = 'git log -1 --pretty=format:"%h - %ai"'
-    print(f">{cmd}")
+    cmd = 'git log -1 --pretty=format:"Git hash: %H Date of commit: %ai"'
     subprocess.check_call(cmd, shell=True)
 
-    cmd = 'git status --porcelain'
-    print(f">{cmd}:")
-    subprocess.check_call(cmd, shell=True)
+    link = sys.argv[1] + sys.argv[2]
+    print(' Build run: ' + link + ' ')
