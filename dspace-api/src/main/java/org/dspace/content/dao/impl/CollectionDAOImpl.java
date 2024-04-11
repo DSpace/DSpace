d27331a0b3ab4fba393f697e9cb48fc8b9760894
nwoodward <woodward.nicholas@gmail.com>
2024-04-11 11:01:21 -0500

    make sure collections list is unique

--- dspace-api/src/main/java/org/dspace/content/dao/impl/CollectionDAOImpl.java
+++ dspace-api/src/main/java/org/dspace/content/dao/impl/CollectionDAOImpl.java
@@ -159,7 +159,7 @@ public class CollectionDAOImpl extends AbstractHibernateDSODAO<Collection> imple
 
     @Override
     public List<Collection> findCollectionsWithSubscribers(Context context) throws SQLException {
-        return list(createQuery(context, "SELECT c FROM Collection c JOIN Subscription s ON c.id = " +
+        return list(createQuery(context, "SELECT DISTINCT c FROM Collection c JOIN Subscription s ON c.id = " +
                 "s.dSpaceObject"));
     }
 
