ee54549cecec142800f84aa38b4345705be44127
Istvan Vig <vig.istvan@bme.hu>
2024-06-18 13:30:44 +0200

    Page number based thumbnail generation

--- dspace-api/src/main/java/org/dspace/app/mediafilter/PDFBoxThumbnail.java
+++ dspace-api/src/main/java/org/dspace/app/mediafilter/PDFBoxThumbnail.java
@@ -16,6 +16,9 @@ import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.dspace.content.Item;
 
+import org.dspace.content.MetadataSchemaEnum;
+import org.dspace.content.service.ItemService;
+
 /**
  * Create JPEG thumbnails from PDF cover page using PDFBox.
  * Based on JPEGFilter:
@@ -70,10 +73,26 @@ public class PDFBoxThumbnail extends MediaFilter {
         throws Exception {
         BufferedImage buf;
 
+        String pageNumberStr =  currentItem.getItemService()
+            .getMetadataFirstValue(currentItem, "dspace", "filter-media", "page", Item.ANY);
+
+        int pageNumber = 0;
+
+        try {
+            if (pageNumberStr != null) {
+                pageNumber = Integer.parseInt(pageNumberStr);
+                // because of the zero base indexing
+                pageNumber -= 1;
+            }
+        } catch (Exception e) {
+            log.error("dspace.filter-media.page metadata contains an invalid page number: {}",
+                    pageNumberStr);
+        }
+
         // Render the page image.
         try ( PDDocument doc = PDDocument.load(source); ) {
             PDFRenderer renderer = new PDFRenderer(doc);
-            buf = renderer.renderImage(0);
+            buf = renderer.renderImage(pageNumber);
         } catch (InvalidPasswordException ex) {
             log.error("PDF is encrypted. Cannot create thumbnail (item: {})", currentItem::getHandle);
             return null;
