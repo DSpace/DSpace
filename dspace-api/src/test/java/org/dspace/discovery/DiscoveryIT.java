7d83ce0be3c8de584fae5beadc57efdb9e1cf5ef
Gantner, Florian Klaus <florian.gantner@uni-bamberg.de>
2024-07-10 18:46:34 +0200

    option to exclude the submitter being indexed to solr in archived items
    https://github.com/DSpace/DSpace/issues/9660

--- dspace-api/src/test/java/org/dspace/discovery/DiscoveryIT.java
+++ dspace-api/src/test/java/org/dspace/discovery/DiscoveryIT.java
@@ -11,6 +11,7 @@ import static org.dspace.discovery.SolrServiceWorkspaceWorkflowRestrictionPlugin
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.util.Assert.doesNotContain;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -21,6 +22,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
 import org.dspace.AbstractIntegrationTestWithDatabase;
 import org.dspace.app.launcher.ScriptLauncher;
 import org.dspace.app.scripts.handler.impl.TestDSpaceRunnableHandler;
@@ -98,6 +103,8 @@ public class DiscoveryIT extends AbstractIntegrationTestWithDatabase {
 
     MetadataAuthorityService metadataAuthorityService = ContentAuthorityServiceFactory.getInstance()
                                                                                       .getMetadataAuthorityService();
+    SolrSearchCore solrSearchCore = DSpaceServicesFactory.getInstance().getServiceManager()
+        .getServicesByType(SolrSearchCore.class).get(0);
 
     @Override
     @Before
@@ -793,6 +800,105 @@ public class DiscoveryIT extends AbstractIntegrationTestWithDatabase {
         assertFalse(lastModifieds.isEmpty());
         for (int i = 1; i < lastModifieds.size() - 1; i++) {
             assertTrue(lastModifieds.get(i).compareTo(lastModifieds.get(i + 1)) >= 0);
+        }
+    }
+
+    /**
+     * Test designed to check if the anonymization of the submitter name for archived items is working.
+     * @throws SearchServiceException
+     */
+    @Test
+    public void searchWithAnonymizedSubmitterTest() throws SearchServiceException {
+
+        configurationService.setProperty("discovery.index.item.submitter.enabled", true);
+        configurationService.setProperty("discovery.index.item.submitter.name", "John Doe");
+
+        DiscoveryConfiguration defaultConf = SearchUtils.getDiscoveryConfiguration(context, "default", null);
+
+        // Populate the testing objects: create items in eperson's workspace and perform search in it
+        int numberItems = 10;
+        context.turnOffAuthorisationSystem();
+        EPerson submitter = null;
+        try {
+            submitter = EPersonBuilder.createEPerson(context).withEmail("submitter@example.org")
+                .withNameInMetadata("Peter", "Funny").build();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        context.setCurrentUser(submitter);
+        Community community = CommunityBuilder.createCommunity(context).build();
+        Collection collection = CollectionBuilder.createCollection(context, community).build();
+        for (int i = 0; i < numberItems; i++) {
+            ItemBuilder.createItem(context, collection)
+                .withTitle("item " + i)
+                .build();
+        }
+        context.restoreAuthSystemState();
+
+        // Build query with default parameters (except for workspaceConf)
+        QueryResponse result = null;
+        try {
+            result = solrSearchCore.getSolr().query(new SolrQuery(String.format(
+                "search.resourcetype:\"Item\"")));
+        } catch (SolrServerException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(result.getResults().size(), numberItems);
+        for (SolrDocument doc : result.getResults()) {
+            assertEquals(doc.getFirstValue("submitter_keyword"), "John Doe");
+            doesNotContain("Peter", (String) doc.getFirstValue("submitter_keyword"), "");
+            doesNotContain("Funny", (String) doc.getFirstValue("submitter_keyword"), "");
+            assertEquals(doc.getFirstValue("submitter_authority"), submitter.getID().toString());
+        }
+    }
+
+    /**
+     * Test designed to check if the submitter is not indexed in all in solr documents for items
+     * @throws SearchServiceException
+     */
+    @Test
+    public void searchWithNoSubmitterTest() throws SearchServiceException {
+
+        configurationService.setProperty("discovery.index.item.submitter.enabled", false);
+        DiscoveryConfiguration defaultConf = SearchUtils.getDiscoveryConfiguration(context, "default", null);
+
+        // Populate the testing objects: create items in eperson's workspace and perform search in it
+        int numberItems = 10;
+        context.turnOffAuthorisationSystem();
+        EPerson submitter = null;
+        try {
+            submitter = EPersonBuilder.createEPerson(context).withEmail("submitter@example.org")
+                .withNameInMetadata("Peter", "Funny").build();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        context.setCurrentUser(submitter);
+        Community community = CommunityBuilder.createCommunity(context).build();
+        Collection collection = CollectionBuilder.createCollection(context, community).build();
+        for (int i = 0; i < numberItems; i++) {
+            ItemBuilder.createItem(context, collection)
+                .withTitle("item " + i)
+                .build();
+        }
+        context.restoreAuthSystemState();
+
+        // Build query with default parameters (except for workspaceConf)
+        QueryResponse result = null;
+        try {
+            result = solrSearchCore.getSolr().query(new SolrQuery(String.format(
+                "search.resourcetype:\"Item\"")));
+        } catch (SolrServerException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(result.getResults().size(), numberItems);
+        for (SolrDocument doc : result.getResults()) {
+            for (String fieldname : doc.getFieldNames()) {
+                assertFalse(fieldname.startsWith("submitter"));
+            }
         }
     }
 
