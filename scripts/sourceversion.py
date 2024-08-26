c488c861bf3f9c7d94d1624a31e9b7e594e10074
MajoBerger <marian.berger@dataquest.sk>
2024-08-26 11:38:41 +0200

    using approx time with offset

--- scripts/sourceversion.py
+++ scripts/sourceversion.py
@@ -1,11 +1,14 @@
 import subprocess
 import sys
-import pytz
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
+
+def get_time_in_timezone(timezone_offset):
+    return datetime.now(timezone.utc) + timedelta(hours=timezone_offset)
 
 if __name__ == '__main__':
-    ts = datetime.now().astimezone(pytz.timezone("Europe/Bratislava"))
-    print(f"This info was generated on: {ts}")
+    cet_offset = 1
+    ts = get_time_in_timezone(cet_offset)
+    print(f"This info was generated on: {ts.strftime('%Y-%m-%d %H:%M:%S %Z%z')}")
 
     cmd = 'git log -1 --pretty=format:"Git hash: %H Date of commit: %ai"'
     subprocess.check_call(cmd, shell=True)
