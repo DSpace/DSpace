855fa656fc4151d68f0d4c8caf47fccfe8d7f5b7
jm <jm@maz>
2024-08-27 11:31:04 +0200

    be does not have html

--- scripts/sourceversion.py
+++ scripts/sourceversion.py
@@ -14,12 +14,10 @@ def get_time_in_timezone(timezone_offset):
 if __name__ == '__main__':
     cet_offset = 1
     ts = get_time_in_timezone(cet_offset)
-    # we have html tags, since this script ends up creating VERSION_D.html
-    print(f"<h4>This info was generated on: <br> <strong> {ts.strftime('%Y-%m-%d %H:%M:%S %Z%z')} </strong> </h4>")
+    print(f"This info was generated on: {ts.strftime('%Y-%m-%d %H:%M:%S %Z%z')}")
 
-    cmd = 'git log -1 --pretty=format:"<h4>Git hash: <br><strong> %H </strong> <br> Date of commit: <br> <strong> %ai </strong></h4>"'
+    cmd = 'git log -1 --pretty=format:"Git hash: %H Date of commit: %ai"'
     subprocess.check_call(cmd, shell=True)
 
-    # when adding argparse, this should be a bit more obvious
     link = sys.argv[1] + sys.argv[2]
-    print('<br> <h4>Build run: </h4> <a href="' + link + '"> ' + link + '</a> ')
+    print(' Build run: ' + link + ' ')
