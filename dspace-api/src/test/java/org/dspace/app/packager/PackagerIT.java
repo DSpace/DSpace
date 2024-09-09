47b45b612b890e21b4cc5b1666448ec1adee06c0
milanmajchrak <90026355+milanmajchrak@users.noreply.github.com>
2024-09-09 16:20:28 +0200

    Cherry picked fix about integration tests (#750)
    Co-authored-by: Tim Donohue <tim.donohue@lyrasis.org>

--- dspace-api/src/test/java/org/dspace/app/packager/PackagerIT.java
+++ dspace-api/src/test/java/org/dspace/app/packager/PackagerIT.java
@@ -24,6 +24,7 @@ import org.dspace.AbstractIntegrationTestWithDatabase;
 import org.dspace.builder.CollectionBuilder;
 import org.dspace.builder.CommunityBuilder;
 import org.dspace.builder.ItemBuilder;
+import org.dspace.builder.WorkspaceItemBuilder;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
 import org.dspace.content.Item;
@@ -159,7 +160,7 @@ public class PackagerIT extends AbstractIntegrationTestWithDatabase {
         performExportScript(article.getHandle(), tempFile);
         UUID id = article.getID();
         itemService.delete(context, article);
-        WorkspaceItem workspaceItem = workspaceItemService.create(context, col1, id, false);
+        WorkspaceItem workspaceItem = WorkspaceItemBuilder.createWorkspaceItem(context, col1, id).build();
         installItemService.installItem(context, workspaceItem, "123456789/0100");
         performImportNoForceScript(tempFile);
         Iterator<Item> items = itemService.findByCollection(context, col1);
