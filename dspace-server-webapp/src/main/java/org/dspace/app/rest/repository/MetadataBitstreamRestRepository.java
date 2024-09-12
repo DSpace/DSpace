218f7f246002445b5ff8646fb02d7f25027e916f
milanmajchrak <90026355+milanmajchrak@users.noreply.github.com>
2024-09-12 10:56:31 +0200

    UFAL/The files are not rendered in the Item UI (#752)
    * Do not commit a session during processing the previews. Catch the error when the ZIP is broken because it wont render files in the UI.
    
    * Fixed checkstyle issues

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/MetadataBitstreamRestRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/MetadataBitstreamRestRepository.java
@@ -173,7 +173,6 @@ public class MetadataBitstreamRestRepository extends DSpaceRestRepository<Metada
                         for (FileInfo fi : fileInfos) {
                             createPreviewContent(context, bitstream, fi);
                         }
-                        context.commit();
                     } else {
                         for (PreviewContent pc : prContents) {
                             fileInfos.add(createFileInfo(pc));
@@ -186,6 +185,7 @@ public class MetadataBitstreamRestRepository extends DSpaceRestRepository<Metada
                 metadataValueWrappers.add(bts);
                 rs.add(metadataBitstreamWrapperConverter.convert(bts, utils.obtainProjection()));
             }
+            context.commit();
         }
 
         return new PageImpl<>(rs, pageable, rs.size());
@@ -318,7 +318,11 @@ public class MetadataBitstreamRestRepository extends DSpaceRestRepository<Metada
             String data = "";
             if (bitstream.getFormat(context).getMIMEType().equals("application/zip")) {
                 data = extractFile(inputStream, "zip");
-                fileInfos = FileTreeViewGenerator.parse(data);
+                try {
+                    fileInfos = FileTreeViewGenerator.parse(data);
+                } catch (Exception e) {
+                    log.error("Cannot extract file content because: {}", e.getMessage());
+                }
             } else if (bitstream.getFormat(context).getMIMEType().equals("application/x-tar")) {
                 ArchiveInputStream is = new ArchiveStreamFactory().createArchiveInputStream(ArchiveStreamFactory.TAR,
                         inputStream);
