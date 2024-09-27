8f3aebe3ed3481ad8ad2021bdbdcde0668d0ae5f
Giuseppe Digilio <giuseppe.digilio@4science.com>
2024-09-27 12:11:40 +0200

    [DURACOM-288] Fix test

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/AuthorizationRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/AuthorizationRestRepositoryIT.java
@@ -842,6 +842,7 @@ public class AuthorizationRestRepositoryIT extends AbstractControllerIntegration
      *
      * @throws Exception
      */
+    @Test
     public void findByObjectBadRequestSSRTest() throws Exception {
         Site site = siteService.findSite(context);
         SiteRest siteRest = siteConverter.convert(site, DefaultProjection.DEFAULT);
@@ -963,7 +964,6 @@ public class AuthorizationRestRepositoryIT extends AbstractControllerIntegration
             .andExpect(jsonPath("$.page.totalElements", greaterThanOrEqualTo(1)));
     }
 
-    @Test
     /**
      * Verify that the findByObject return the 401 Unauthorized response when an eperson is involved
      *
