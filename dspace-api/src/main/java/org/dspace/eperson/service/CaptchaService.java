2982e16b5fd92dc8eea53193230dcb49f27b3855
Kim Shepherd <kim@shepherd.nz>
2025-02-13 14:03:08 +0100

    Request-a-copy improvement: Altcha Recaptcha service

--- dspace-api/src/main/java/org/dspace/eperson/service/CaptchaService.java
+++ dspace-api/src/main/java/org/dspace/eperson/service/CaptchaService.java
@@ -7,6 +7,9 @@
  */
 package org.dspace.eperson.service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.dspace.eperson.InvalidReCaptchaException;
 
 /**
@@ -26,5 +29,31 @@ public interface CaptchaService {
      * @throws InvalidReCaptchaException if reCaptcha was not successfully validated
      */
     public void processResponse(String response, String action) throws InvalidReCaptchaException;
+
+    /**
+     * Encode bytes to hex string
+     * @param bytes bytes to encode
+     * @return hex string
+     */
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte b : bytes) {
+            stringBuilder.append(String.format("%02x", b));
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Calculate a hex string from a digest, given an input string
+     * @param input input string
+     * @param algorithm algorithm key, eg. SHA-256
+     * @return
+     * @throws NoSuchAlgorithmException
+     */
+    public static String calculateHash(String input, String algorithm) throws NoSuchAlgorithmException {
+        MessageDigest sha256 = MessageDigest.getInstance(algorithm);
+        byte[] hashBytes = sha256.digest(input.getBytes());
+        return bytesToHex(hashBytes);
+    }
 
 }
