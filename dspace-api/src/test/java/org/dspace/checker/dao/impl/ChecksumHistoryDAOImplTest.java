4871d1273c78a97ac0dd88c62ecbfb22b3d63ad2
Tim Donohue <tim.donohue@lyrasis.org>
2025-02-21 10:03:11 -0600

    Refactor ChecksumHistoryDAOImplTest to lookup database rows via "id" instead of an exact date. Fixes bug where exact date match is not always succeeding even when the row exists.

--- dspace-api/src/test/java/org/dspace/checker/dao/impl/ChecksumHistoryDAOImplTest.java
+++ dspace-api/src/test/java/org/dspace/checker/dao/impl/ChecksumHistoryDAOImplTest.java
@@ -70,9 +70,7 @@ public class ChecksumHistoryDAOImplTest
             "INSERT INTO checksum_history"
                 + "(check_id, process_end_date, result, bitstream_id)"
                 + " VALUES (:id, :date, :result, :bitstream)");
-        int checkId = 0;
 
-        // Row with matching result code
         BitstreamService bss = ContentServiceFactory.getInstance().getBitstreamService();
         InputStream is = new ByteArrayInputStream(new byte[0]);
         Bitstream bs = bss.create(context, is);
@@ -80,27 +78,28 @@ public class ChecksumHistoryDAOImplTest
         bss.update(context, bs);
         context.restoreAuthSystemState();
 
+        // Add a past date row with matching result code
         Instant matchDate = retentionDate.minus(1, ChronoUnit.DAYS);
-        checkId++;
-        qry.setParameter("id", checkId);
+        int matchId = 0;
+        qry.setParameter("id", matchId);
         qry.setParameter("date", matchDate);
         qry.setParameter("result", ChecksumResultCode.CHECKSUM_MATCH.name());
         qry.setParameter("bitstream", bs.getID()); // FIXME identifier not being set???
         qry.executeUpdate();
 
-        // Row with nonmatching result code
+        // Add a past date row with a nonmatching result code
         Instant noMatchDate = retentionDate.minus(2, ChronoUnit.DAYS);
-        checkId++;
-        qry.setParameter("id", checkId);
+        int noMatchId = 1;
+        qry.setParameter("id", noMatchId);
         qry.setParameter("date", noMatchDate);
         qry.setParameter("result", ChecksumResultCode.CHECKSUM_NO_MATCH.name());
         qry.setParameter("bitstream", bs.getID()); // FIXME identifier not being set???
         qry.executeUpdate();
 
-        // Create one newer row
+        // Add a future date row with a matching result code
         Instant futureDate = retentionDate.plus(3, ChronoUnit.DAYS);
-        checkId++;
-        qry.setParameter("id", checkId);
+        int futureMatchId = 2;
+        qry.setParameter("id", futureMatchId);
         qry.setParameter("date", futureDate);
         qry.setParameter("result", ChecksumResultCode.CHECKSUM_MATCH.name());
         qry.setParameter("bitstream", bs.getID()); // FIXME identifier not being set???
@@ -115,20 +114,20 @@ public class ChecksumHistoryDAOImplTest
 
         // See if matching old row is gone.
         qry = dbc.getSession().createQuery(
-            "SELECT COUNT(*) FROM ChecksumHistory WHERE processEndDate = :date");
+            "SELECT COUNT(*) FROM ChecksumHistory WHERE id = :id");
         long count;
 
-        qry.setParameter("date", matchDate);
+        qry.setParameter("id", matchId);
         count = (Long) qry.getSingleResult();
         assertEquals("Should find no row at matchDate", 0, count);
 
         // See if nonmatching old row is still present.
-        qry.setParameter("date", noMatchDate);
+        qry.setParameter("id", noMatchId);
         count = (Long) qry.getSingleResult();
         assertEquals("Should find one row at noMatchDate", 1, count);
 
-        // See if new row is still present.
-        qry.setParameter("date", futureDate);
+        // See if future date row is still present.
+        qry.setParameter("id", futureMatchId);
         count = (Long) qry.getSingleResult();
         assertEquals("Should find one row at futureDate", 1, count);
     }
