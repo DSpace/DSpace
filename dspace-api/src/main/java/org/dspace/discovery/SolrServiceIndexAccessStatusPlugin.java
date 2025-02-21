c135a4f7a3ad6e6beed2b9fbbc8e6176fc18429c
Paulo Graça <paulo1978@gmail.com>
2025-02-21 12:37:35 +0000

    new solr service AccessStatus plugin

---
+++ dspace-api/src/main/java/org/dspace/discovery/SolrServiceIndexAccessStatusPlugin.java
@@ -0,0 +1,62 @@
+package org.dspace.discovery;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.solr.common.SolrInputDocument;
+import org.dspace.access.status.factory.AccessStatusServiceFactory;
+import org.dspace.access.status.service.AccessStatusService;
+import org.dspace.access.status.DefaultAccessStatusHelper;
+import org.dspace.content.Bitstream;
+import org.dspace.content.Bundle;
+import org.dspace.content.Item;
+import org.dspace.core.Context;
+import org.dspace.discovery.indexobject.IndexableItem;
+
+
+/**
+ * This plugin enables the indexing of access status for the item as a filter
+ * and keyword
+ * @author pgraca
+ *
+ */
+public class SolrServiceIndexAccessStatusPlugin implements SolrServiceIndexPlugin {
+
+    AccessStatusService accessStatusService = AccessStatusServiceFactory.getInstance().getAccessStatusService();
+
+    @Override
+    public void additionalIndex(Context context, IndexableObject indexableObject, SolrInputDocument document) {
+        if (indexableObject instanceof IndexableItem) {
+            Item item = ((IndexableItem) indexableObject).getIndexedObject();
+            String accessStatus;
+
+            try {
+                accessStatus = retrieveItemAccessStatus(context,item);
+            } catch (SQLException e) {
+                accessStatus = DefaultAccessStatusHelper.UNKNOWN;
+            }
+
+            // _keyword and _filter because
+            // they are needed in order to work as a facet and filter.
+            document.addField("access_status_keyword", accessStatus);
+            document.addField("access_status_filter", accessStatus);
+
+        }
+    }
+
+    /**
+     * Checks whether the given item has a bundle with the name ORIGINAL
+     * containing at least one bitstream.
+     *
+     * @param item to check
+     * @return String with one of the following controlled values 
+        EMBARGO = "embargo"
+        METADATA_ONLY = "metadata.only"
+        OPEN_ACCESS = "open.access"
+        RESTRICTED = "restricted"
+        UNKNOWN = "unknown"
+     */
+    private String retrieveItemAccessStatus(Context context, Item item) throws SQLException {
+        return accessStatusService.getAccessStatus(context, item);
+    }
+}
