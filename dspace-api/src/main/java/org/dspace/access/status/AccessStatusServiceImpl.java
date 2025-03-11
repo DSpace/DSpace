582507f1329a5f5582b8f4d7367653a065609020
Nicolas Boulay <NIBOU230@ulaval.ca>
2025-03-11 16:13:29 -0400

    Add configurations for access status calculation

--- dspace-api/src/main/java/org/dspace/access/status/AccessStatusServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/access/status/AccessStatusServiceImpl.java
@@ -11,6 +11,9 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.ZoneId;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.dspace.access.status.service.AccessStatusService;
 import org.dspace.content.Bitstream;
 import org.dspace.content.Item;
@@ -23,10 +26,15 @@ import org.springframework.beans.factory.annotation.Autowired;
  * Implementation for the access status calculation service.
  */
 public class AccessStatusServiceImpl implements AccessStatusService {
+    private static final Logger log = LogManager.getLogger(AccessStatusServiceImpl.class);
+
     // Plugin implementation, set from the DSpace configuration by init().
     protected AccessStatusHelper helper = null;
 
     protected LocalDate forever_date = null;
+
+    protected String itemCalculationType = null;
+    protected String bitstreamCalculationType = null;
 
     @Autowired(required = true)
     protected ConfigurationService configurationService;
@@ -60,12 +68,15 @@ public class AccessStatusServiceImpl implements AccessStatusService {
                     .atStartOfDay()
                     .atZone(ZoneId.systemDefault())
                     .toLocalDate();
+
+            itemCalculationType = getAccessStatusCalculationType("access.status.for-user.item");
+            bitstreamCalculationType = getAccessStatusCalculationType("access.status.for-user.bitstream");
         }
     }
 
     @Override
     public String getAccessStatus(Context context, Item item) throws SQLException {
-        return helper.getAccessStatusFromItem(context, item, forever_date);
+        return helper.getAccessStatusFromItem(context, item, forever_date, itemCalculationType);
     }
 
     @Override
@@ -74,12 +85,23 @@ public class AccessStatusServiceImpl implements AccessStatusService {
     }
 
     @Override
-    public Date getAvailabilityDateFromBitstream(Context context, Bitstream bitstream) throws SQLException {
-        return helper.getAvailabilityDateFromBitstream(context, bitstream, forever_date);
+    public LocalDate getAvailabilityDateFromBitstream(Context context, Bitstream bitstream) throws SQLException {
+        return helper.getAvailabilityDateFromBitstream(context, bitstream, forever_date, bitstreamCalculationType);
     }
 
     @Override
-    public String getAccessStatusFromAvailabilityDate(Date availabilityDate) {
+    public String getAccessStatusFromAvailabilityDate(LocalDate availabilityDate) {
         return helper.getAccessStatusFromAvailabilityDate(availabilityDate, forever_date);
+    }
+
+    private String getAccessStatusCalculationType(String key) {
+        String value = configurationService.getProperty(key);
+        if (StringUtils.equalsIgnoreCase(value, "anonymous")) {
+            return value;
+        } else if (StringUtils.equalsIgnoreCase(value, "current")) {
+            return value;
+        }
+        log.warn("The configuration parameter \"" + key + "\" contains an invalid value.");
+        return "anonymous";
     }
 }
