390967abed1ffe8008638ad109e444c070f80da3
Sascha Szott <szott@gmx.de>
2025-04-02 22:41:38 +0200

    add static imports
    (cherry picked from commit 8ad19c42df754daa6ed70c6e9d1c9146e72c0b32)

--- dspace-api/src/main/java/org/dspace/browse/SolrBrowseDAO.java
+++ dspace-api/src/main/java/org/dspace/browse/SolrBrowseDAO.java
@@ -36,6 +36,9 @@ import org.dspace.discovery.configuration.DiscoveryConfiguration;
 import org.dspace.discovery.indexobject.IndexableItem;
 import org.dspace.services.factory.DSpaceServicesFactory;
 
+import static org.dspace.discovery.SearchUtils.RESOURCE_ID_FIELD;
+import static org.dspace.discovery.SearchUtils.RESOURCE_TYPE_FIELD;
+
 /**
  * @author Andrea Bollini (CILEA)
  * @author Adán Román Ruiz at arvo.es (bugfix)
