23468d4ee357363dddd0996a1a13366be88527c8
Piaget Bouaka Donfack <piaget.bouaka@4science.com>
2025-04-16 17:11:09 +0200

    [DURACOM-346] SubscribeServiceImpl : the method "isSubscribed" returns incorrect result

--- dspace-api/src/test/java/org/dspace/eperson/SubscribeServiceIT.java
+++ dspace-api/src/test/java/org/dspace/eperson/SubscribeServiceIT.java
@@ -213,6 +213,7 @@ public class SubscribeServiceIT extends AbstractIntegrationTestWithDatabase {
                                                               secondCollection, 100, 0);
 
         assertEquals(subscriptions.size(), 1);
+        assertThat(subscribeService.isSubscribed(context, subscribingUser, secondCollection), is(true));
 
 
         subscribeService.unsubscribe(context, subscribingUser, secondCollection);
@@ -222,6 +223,7 @@ public class SubscribeServiceIT extends AbstractIntegrationTestWithDatabase {
                                                               secondCollection, 100, 0);
 
         assertEquals(subscriptions.size(), 0);
+        assertThat(subscribeService.isSubscribed(context, subscribingUser, secondCollection), is(false));
     }
 
     @Test(expected = AuthorizeException.class)
