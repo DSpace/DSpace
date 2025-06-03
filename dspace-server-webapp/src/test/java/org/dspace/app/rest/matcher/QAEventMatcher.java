f75e0fb0ce4eb7cabc41e0687ef591eb277959cd
Alexandre Vryghem <alexandre@atmire.com>
2025-06-03 13:11:42 +0200

    131448: Fixed QAEventMatcher#matchQAEventNotifyEntry sometimes failing when your default local uses a comma as a decimal separator
    You can view your default decimal separator using new DecimalFormatSymbols(Locale.getDefault()).getDecimalSeparator()
    
    (cherry picked from commit d74c3bd9cd3082b05cf82ba6379d25880e28ecd9)

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/matcher/QAEventMatcher.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/matcher/QAEventMatcher.java
@@ -77,10 +77,11 @@ public class QAEventMatcher {
     public static Matcher<? super Object> matchQAEventNotifyEntry(QAEvent event) {
         try {
             ObjectMapper jsonMapper = new JsonMapper();
+            DecimalFormat decimalFormat = new DecimalFormat("0.000", new DecimalFormatSymbols(Locale.ENGLISH));
             return allOf(hasJsonPath("$.id", is(event.getEventId())),
                     hasJsonPath("$.originalId", is(event.getOriginalId())),
                     hasJsonPath("$.title", is(event.getTitle())),
-                    hasJsonPath("$.trust", is(new DecimalFormat("0.000").format(event.getTrust()))),
+                    hasJsonPath("$.trust", is(decimalFormat.format(event.getTrust()))),
                     hasJsonPath("$.status", Matchers.equalToIgnoringCase(event.getStatus())),
                     hasJsonPath("$.message",
                             matchMessage(event.getTopic(), jsonMapper.readValue(event.getMessage(),
