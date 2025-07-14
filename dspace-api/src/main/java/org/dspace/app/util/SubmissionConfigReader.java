ae4564568fc6ec8959b33ad142c58f514754736c
Kim Shepherd <kim@shepherd.nz>
2025-07-14 13:06:08 +0200

    Remove unused imports

--- dspace-api/src/main/java/org/dspace/app/util/SubmissionConfigReader.java
+++ dspace-api/src/main/java/org/dspace/app/util/SubmissionConfigReader.java
@@ -16,7 +16,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.FactoryConfigurationError;
 
 import org.apache.commons.lang3.StringUtils;
