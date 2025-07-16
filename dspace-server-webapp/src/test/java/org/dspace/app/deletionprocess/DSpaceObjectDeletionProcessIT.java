3ffe82e06ed62d2ec9ceb0e4aa4d8b41ef55ed37
Mykhaylo Boychuk <mykhaylo.boychuk@4science.it>
2025-07-16 12:03:28 +0200

    [DURACOM-383] add ITs

--- dspace-server-webapp/src/test/java/org/dspace/app/deletionprocess/DSpaceObjectDeletionProcessIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/deletionprocess/DSpaceObjectDeletionProcessIT.java
@@ -7,13 +7,28 @@
  */
 package org.dspace.app.deletionprocess;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.InputStream;
+
+import org.apache.commons.codec.CharEncoding;
+import org.apache.commons.io.IOUtils;
 import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
+import org.dspace.app.scripts.handler.impl.TestDSpaceRunnableHandler;
+import org.dspace.builder.BitstreamBuilder;
+import org.dspace.builder.BundleBuilder;
 import org.dspace.builder.CollectionBuilder;
 import org.dspace.builder.CommunityBuilder;
 import org.dspace.builder.ItemBuilder;
+import org.dspace.content.Bitstream;
+import org.dspace.content.Bundle;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
 import org.dspace.content.Item;
+import org.dspace.deletion.process.DSpaceObjectDeletionProcess;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 /**
@@ -26,6 +41,12 @@ public class DSpaceObjectDeletionProcessIT extends AbstractControllerIntegration
     private Item item1;
     private Item item2;
     private Community community;
+    private Bitstream bitstream1;
+    private Bitstream bitstream2;
+    private Bitstream bitstream3;
+    private Bitstream bitstream4;
+    private Bitstream bitstream5;
+    private Bitstream bitstream6;
     private Collection collection;
 
     @Override
@@ -51,21 +72,128 @@ public class DSpaceObjectDeletionProcessIT extends AbstractControllerIntegration
                            .withType("website_content")
                            .build();
 
+        Bundle bundleOfItem1 = BundleBuilder.createBundle(context, item1)
+                                     .withName("ORIGINAL")
+                                     .build();
+
+        try (InputStream is = IOUtils.toInputStream("Dummy content 1", CharEncoding.UTF_8)) {
+            bitstream1 = BitstreamBuilder.createBitstream(context, bundleOfItem1, is)
+                                         .withName("bitstream 1")
+                                         .build();
+        }
+
+        try (InputStream is = IOUtils.toInputStream("Dummy content 2", CharEncoding.UTF_8)) {
+            bitstream2 = BitstreamBuilder.createBitstream(context, bundleOfItem1, is)
+                                         .withName("bitstream 2")
+                                         .build();
+        }
+
+        try (InputStream is = IOUtils.toInputStream("Dummy content 3", CharEncoding.UTF_8)) {
+            bitstream3 = BitstreamBuilder.createBitstream(context, bundleOfItem1, is)
+                                         .withName("bitstream 3")
+                                         .build();
+        }
+
+        try (InputStream is = IOUtils.toInputStream("Dummy content 4", CharEncoding.UTF_8)) {
+            bitstream4 = BitstreamBuilder.createBitstream(context, bundleOfItem1, is)
+                                         .withName("bitstream 4")
+                                         .build();
+        }
+
+        Bundle bundleOfItem2 = BundleBuilder.createBundle(context, item2)
+                                            .withName("ORIGINAL")
+                                            .build();
+
+        try (InputStream is = IOUtils.toInputStream("TEST 1", CharEncoding.UTF_8)) {
+            bitstream5 = BitstreamBuilder.createBitstream(context, bundleOfItem2, is)
+                                         .withName("test title 1 item2")
+                                         .build();
+        }
+
+        try (InputStream is = IOUtils.toInputStream("TEST 2", CharEncoding.UTF_8)) {
+            bitstream6 = BitstreamBuilder.createBitstream(context, bundleOfItem2, is)
+                                         .withName("test title 2 item2")
+                                         .build();
+        }
+
         context.restoreAuthSystemState();
     }
 
     @Test
     public void asyncDetetionOfItemTest() throws Exception {
+        String tokenAdmin = getAuthToken(admin.getEmail(), password);
+        getClient(tokenAdmin).perform(get("/api/core/items/" + item1.getID()))
+                             .andExpect(status().isOk())
+                             .andExpect(jsonPath("$.id", Matchers.is(item1.getID().toString())))
+                             .andExpect(jsonPath("$.name", Matchers.is(item1.getName())));
 
+        getClient(tokenAdmin).perform(get("/api/core/bitstreams/" + bitstream1.getID()))
+                             .andExpect(status().isOk())
+                             .andExpect(jsonPath("$.id", Matchers.is(bitstream1.getID().toString())))
+                             .andExpect(jsonPath("$.name", Matchers.is(bitstream1.getName())));
+
+        getClient(tokenAdmin).perform(get("/api/core/bitstreams/" + bitstream2.getID()))
+                             .andExpect(status().isOk())
+                             .andExpect(jsonPath("$.id", Matchers.is(bitstream2.getID().toString())))
+                             .andExpect(jsonPath("$.name", Matchers.is(bitstream2.getName())));
+
+        getClient(tokenAdmin).perform(get("/api/core/bitstreams/" + bitstream3.getID()))
+                             .andExpect(status().isOk())
+                             .andExpect(jsonPath("$.id", Matchers.is(bitstream3.getID().toString())))
+                             .andExpect(jsonPath("$.name", Matchers.is(bitstream3.getName())));
+
+        getClient(tokenAdmin).perform(get("/api/core/bitstreams/" + bitstream4.getID()))
+                             .andExpect(status().isOk())
+                             .andExpect(jsonPath("$.id", Matchers.is(bitstream4.getID().toString())))
+                             .andExpect(jsonPath("$.name", Matchers.is(bitstream4.getName())));
+
+        String[] args = new String[]{ "dspace-object-deletion", "-i", item1.getID().toString() };
+        TestDSpaceRunnableHandler handler = new TestDSpaceRunnableHandler();
+        DSpaceObjectDeletionProcess deletionProcess = new DSpaceObjectDeletionProcess();
+        deletionProcess.initialize(args, handler, admin);
+        deletionProcess.run();
+
+        getClient(tokenAdmin).perform(get("/api/core/items/" + item1.getID()))
+                             .andExpect(status().isNotFound());
+
+        getClient(tokenAdmin).perform(get("/api/core/bitstreams/" + bitstream1.getID()))
+                             .andExpect(status().isNotFound());
+
+        getClient(tokenAdmin).perform(get("/api/core/bitstreams/" + bitstream2.getID()))
+                             .andExpect(status().isNotFound());
+
+        getClient(tokenAdmin).perform(get("/api/core/bitstreams/" + bitstream3.getID()))
+                             .andExpect(status().isNotFound());
+
+        getClient(tokenAdmin).perform(get("/api/core/bitstreams/" + bitstream4.getID()))
+                             .andExpect(status().isNotFound());
     }
 
     @Test
     public void asyncDetetionOfCollectionTest() throws Exception {
 
+        String tokenAdmin = getAuthToken(admin.getEmail(), password);
+        getClient(tokenAdmin).perform(get("/api/core/collections/" + collection.getID()))
+                             .andExpect(status().isOk())
+                             .andExpect(jsonPath("$.id", Matchers.is(collection.getID().toString())))
+                             .andExpect(jsonPath("$.name", Matchers.is(collection.getName())));
+
+        String[] args = new String[]{ "dspace-object-deletion", "-i", collection.getID().toString() };
+        TestDSpaceRunnableHandler handler = new TestDSpaceRunnableHandler();
+        DSpaceObjectDeletionProcess deletionProcess = new DSpaceObjectDeletionProcess();
+        deletionProcess.initialize(args, handler, admin);
+        deletionProcess.run();
+
     }
 
     @Test
     public void asyncDetetionOfCommunityTest() throws Exception {
+
+        String[] args = new String[]{ "dspace-object-deletion", "-i", community.getID().toString() };
+        TestDSpaceRunnableHandler handler = new TestDSpaceRunnableHandler();
+        DSpaceObjectDeletionProcess deletionProcess = new DSpaceObjectDeletionProcess();
+        deletionProcess.initialize(args, handler, admin);
+        deletionProcess.run();
 
     }
 
