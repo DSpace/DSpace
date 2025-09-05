8eb6d203037209756b3e8b7713ef3a500755f54d
Jesiel Viana <jesielviana@proton.me>
2025-09-05 15:33:15 -0300

    Add familyName and givenName when creating profile author and fix EPerson#getName
    (cherry picked from commit df0dd01776b6e9365b9a621cc093424317c53025)

--- dspace-api/src/main/java/org/dspace/eperson/EPerson.java
+++ dspace-api/src/main/java/org/dspace/eperson/EPerson.java
@@ -369,7 +369,7 @@ public class EPerson extends CacheableDSpaceObject implements DSpaceObjectLegacy
 
     @Override
     public String getName() {
-        return getEmail();
+        return this.getFullName();
     }
 
     String getDigestAlgorithm() {
