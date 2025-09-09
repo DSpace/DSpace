18305f28a5c72a0965d8386b6889743ef0b6c09c
Paulo Graça <paulo1978@gmail.com>
2025-09-09 17:46:11 +0100

    command annotation test

--- dspace-shell/src/main/java/org/dspace/shell/commands/EmbargoCommands.java
+++ dspace-shell/src/main/java/org/dspace/shell/commands/EmbargoCommands.java
@@ -10,10 +10,11 @@ package org.dspace.shell.commands;
 import org.springframework.shell.command.annotation.Command;
 import org.springframework.shell.command.annotation.Option;
 
+@Command
 public class EmbargoCommands {
 
     @Command(command = "embargo-lifter", description="this command executes embargo lifter by reindexing content")
-    public String embargoLifter(@Option(longNames="nDays", defaultValue="1") int nDays) throws Exception {
+    public String embargoLifter(@Option(longNames="nDays", description="The number of past days to process embargo. As an example, nDays=5 means, process the past 5 days.", defaultValue="1") int nDays) throws Exception {
         return "number of days " + nDays;
     }
 
