29e13b77fc9f7bff159c76f61e40126a759cf43d
nwoodward <woodward.nicholas@gmail.com>
2025-10-01 13:46:40 -0500

    checkstyle fix

--- dspace-api/src/main/java/org/dspace/content/dao/impl/CollectionDAOImpl.java
+++ dspace-api/src/main/java/org/dspace/content/dao/impl/CollectionDAOImpl.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
