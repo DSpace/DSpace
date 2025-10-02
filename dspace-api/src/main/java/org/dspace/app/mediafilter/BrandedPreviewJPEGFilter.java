35b7c145d0ec2ee72b799a44c0f3f49169a8139c
zaybournand <zaybournand@gmail.com>
2025-10-02 00:09:19 -0400

    Add BrandedPreviewJPEGFilter for custom JPEG previews

--- dspace-api/src/main/java/org/dspace/app/mediafilter/BrandedPreviewJPEGFilter.java
+++ dspace-api/src/main/java/org/dspace/app/mediafilter/BrandedPreviewJPEGFilter.java
@@ -1,13 +1,25 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.app.mediafilter;
 
 import java.awt.image.BufferedImage;
-import java.io.FileInputStream;
 import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 
 import org.dspace.content.Item;
 import org.dspace.services.ConfigurationService;
 import org.dspace.services.factory.DSpaceServicesFactory;
 
+/**
+ * BrandedPreviewJPEGFilter generates JPEG previews with branding. Extends
+ * JPEGFilter to reuse thumbnail generation methods.
+ */
 public class BrandedPreviewJPEGFilter extends JPEGFilter {
 
     @Override
@@ -28,10 +40,15 @@ public class BrandedPreviewJPEGFilter extends JPEGFilter {
     @Override
     public InputStream getDestinationStream(Item currentItem, InputStream source, boolean verbose)
             throws Exception {
-        BufferedImage buf = javax.imageio.ImageIO.read(source);
 
-        ConfigurationService configurationService
-                = DSpaceServicesFactory.getInstance().getConfigurationService();
+        // Read the source image
+        BufferedImage buf = ImageIO.read(source);
+
+        // Get configuration parameters
+        ConfigurationService configurationService = DSpaceServicesFactory
+                .getInstance()
+                .getConfigurationService();
+
         float xmax = (float) configurationService.getIntProperty("webui.preview.maxwidth");
         float ymax = (float) configurationService.getIntProperty("webui.preview.maxheight");
         boolean blurring = configurationService.getBooleanProperty("webui.preview.blurring");
@@ -40,17 +57,8 @@ public class BrandedPreviewJPEGFilter extends JPEGFilter {
         String brandFont = configurationService.getProperty("webui.preview.brand.font");
         int brandFontPoint = configurationService.getIntProperty("webui.preview.brand.fontpoint");
 
+        // Use JPEGFilter's method directly
         return getThumbDim(currentItem, buf, verbose, xmax, ymax, blurring, hqscaling,
                 brandHeight, brandFontPoint, brandFont);
-    }
-
-    // Temporary main method for local testing
-    public static void main(String[] args) throws Exception {
-        BrandedPreviewJPEGFilter filter = new BrandedPreviewJPEGFilter();
-        InputStream in = new FileInputStream("test.jpg"); // make sure test.jpg exists
-        Item item = null; // dummy item
-
-        InputStream out = filter.getDestinationStream(item, in, true);
-        System.out.println("Filter ran successfully: " + (out != null));
     }
 }
