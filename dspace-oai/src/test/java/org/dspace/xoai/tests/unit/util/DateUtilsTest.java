94cc93ab0358d8ed24691c6c87482318e4b646ba
LucasVaroneW <LucasVarone.Dev@gmail.com>
2025-10-14 22:45:48 +0200

    Fix Checkstyle issues
    (cherry picked from commit 7790e56c50a90ef48a42dc9682d217a37f195b20)

--- dspace-oai/src/test/java/org/dspace/xoai/tests/unit/util/DateUtilsTest.java
+++ dspace-oai/src/test/java/org/dspace/xoai/tests/unit/util/DateUtilsTest.java
@@ -7,16 +7,18 @@
  */
 package org.dspace.xoai.tests.unit.util;
 
-import org.dspace.xoai.util.DateUtils;
+import java.time.Instant;
+
 import org.junit.Test;
 
-import java.time.Instant;
+import org.dspace.xoai.util.DateUtils;
+
 import static org.junit.Assert.assertEquals;
 
 public class DateUtilsTest {
 
     @Test
-    public void testFormatOaiDate(){
+    public void testFormatOaiDate() {
         Instant date = Instant.parse("2025-10-09T18:53:58.376565922Z");
         String formatted = DateUtils.format(date);
         assertEquals("2025-10-09T18:53:58Z", formatted);
