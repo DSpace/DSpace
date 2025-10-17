388f82db33fcb20df16cbe4885e12f435105b50b
Jesiel Viana <jesielviana@proton.me>
2025-10-17 11:07:25 -0300

    fix checkstyle

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/model/BibliographyRest.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/model/BibliographyRest.java
@@ -1,10 +1,17 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.app.rest.model;
-
-import org.dspace.disseminate.CSLBibliography;
-import org.dspace.disseminate.CSLBibliographyGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.dspace.disseminate.CSLBibliography;
+
 
 public class BibliographyRest extends BaseObjectRest<String> {
 
