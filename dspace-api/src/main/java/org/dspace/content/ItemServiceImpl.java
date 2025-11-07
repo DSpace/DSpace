37c396b67b9b17572c18bdb8584350f308e001d2
Jesiel Viana <jesielviana@proton.me>
2025-11-07 17:24:35 -0300

    rename findByMetadataField to findArchivedByMetadataField to improve clarity

--- dspace-api/src/main/java/org/dspace/content/ItemServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/ItemServiceImpl.java
@@ -1321,79 +1321,19 @@ public class ItemServiceImpl extends DSpaceObjectServiceImpl<Item> implements It
             || workflowItemService.findByItem(context, item) != null;
     }
 
-    /*
-    With every finished submission a bunch of resource policy entries which have null value for the dspace_object
-    column are generated in the database.
-prevent the generation of resource policy entry values with null dspace_object as value
 
-    */
-
-
-
-    /**
-     * Returns an iterator of Items possessing the passed metadata field, or only
-     * those matching the passed value, if value is not Item.ANY
-     *
-     * @param context   DSpace context object
-     * @param schema    metadata field schema
-     * @param element   metadata field element
-     * @param qualifier metadata field qualifier
-     * @param value     field value or Item.ANY to match any value
-     * @return an iterator over the items matching that authority value
-     * @throws SQLException       if database error
-     *                            An exception that provides information on a database access error or other errors.
-     * @throws AuthorizeException if authorization error
-     *                            Exception indicating the current user of the context does not have permission
-     *                            to perform a particular action.
-     */
-    @Override
-    public Iterator<Item> findArchivedByMetadataField(Context context,
-                                                      String schema, String element, String qualifier, String value)
-            throws SQLException, AuthorizeException {
-        MetadataSchema mds = metadataSchemaService.find(context, schema);
-        if (mds == null) {
-            throw new IllegalArgumentException("No such metadata schema: " + schema);
-        }
-        MetadataField mdf = metadataFieldService.findByElement(context, mds, element, qualifier);
-        if (mdf == null) {
-            throw new IllegalArgumentException(
-                    "No such metadata field: schema=" + schema + ", element=" + element + ", qualifier=" + qualifier);
-        }
-
-        if (Item.ANY.equals(value)) {
-            return itemDAO.findByMetadataField(context, mdf, null, true);
-        }
-        return itemDAO.findByMetadataField(context, mdf, value, true);
-    }
 
     @Override
     public Iterator<Item> findArchivedByMetadataField(Context context, String metadataField, String value)
-            throws SQLException, AuthorizeException {
+            throws SQLException, AuthorizeException, IOException {
         String[] mdValueByField = getMDValueByField(metadataField);
         return findArchivedByMetadataField(context, mdValueByField[0], mdValueByField[1], mdValueByField[2], value);
     }
 
-    /**
-     * Returns an iterator of Items possessing the passed metadata field, or only
-     * those matching the passed value, if value is not Item.ANY
-     *
-     * @param context   DSpace context object
-     * @param schema    metadata field schema
-     * @param element   metadata field element
-     * @param qualifier metadata field qualifier
-     * @param value     field value or Item.ANY to match any value
-     * @return an iterator over the items matching that authority value
-     * @throws SQLException       if database error
-     *                            An exception that provides information on a database access error or other errors.
-     * @throws AuthorizeException if authorization error
-     *                            Exception indicating the current user of the context does not have permission
-     *                            to perform a particular action.
-     * @throws IOException        if IO error
-     *                            A general class of exceptions produced by failed or interrupted I/O operations.
-     */
+
     @Override
-    public Iterator<Item> findByMetadataField(Context context,
-                                              String schema, String element, String qualifier, String value)
+    public Iterator<Item> findArchivedByMetadataField(Context context,
+                                                      String schema, String element, String qualifier, String value)
         throws SQLException, AuthorizeException, IOException {
         MetadataSchema mds = metadataSchemaService.find(context, schema);
         if (mds == null) {
@@ -1402,7 +1342,7 @@ prevent the generation of resource policy entry values with null dspace_object a
         MetadataField mdf = metadataFieldService.findByElement(context, mds, element, qualifier);
         if (mdf == null) {
             throw new IllegalArgumentException(
-                "No such metadata field: schema=" + schema + ", element=" + element + ", qualifier=" + qualifier);
+                    "No such metadata field: schema=" + schema + ", element=" + element + ", qualifier=" + qualifier);
         }
 
         if (Item.ANY.equals(value)) {
