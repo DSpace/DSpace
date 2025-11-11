ufal/clarin-dspace

metadata-added-provenance (#730)
* added provenance metadata for update, remove and add

* added provenance metadata for update and remove bitstream

* item test json added metadata provenance

* control provenance only by tests of item

* update and remove license

* provenance access control and upload bitstream

* provenance move item to different collection

* provenance make item non-discoverable

* provenance for mapped collection

* don't write provenance for bitstream without item

* move addProvenance to parent class

* separated class for provenance

* create class for provenance management

* added metadata item and bitstream tests

* tests for metadata provenance

* problem with access control test

* better service management:

* delete clarin license mapping

* added json to resources

* modified provenance patch messages

* messages bags

* used correct json

* log exception, replace ! by non conds, added doc comments

* added logs for replacing mtd

* make object from provenance service

* removed interface prom provenance msg provider

* Refactored method `removeReadPolicies` - add provenance after removing policy

* The message templates are loaded from the Enum instead of json file.

* The `getMessage` methods was overloaded.

* Renamed `getItem` to `findItemByBitstream` to clarify the method's purpose.

* Do not throw exception when adding to provenance

* The exceptions are caught in the ProvenanceProvider and changed order when calling provenance methods.

* Renamed ProvenanceProvider to ProvenanceService and refactored it to be used as Spring Service.

* The ProvenanceService wasn't initialized in the BundleAccessControl

* Removed the code without any effect

* Removed the logging which is not required

* Moved calling od the provenance service to better place.

---------

Co-authored-by: milanmajchrak <milan.majchrak@dataquest.sk>

dspace-server-webapp/src/test/java/org/dspace/app/rest/test/MetadataPatchSuite.java
@@ -11,10 +11,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.ws.rs.core.MediaType;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.Assert;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -24,6 +27,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
  * Utility class for performing metadata patch tests sourced from a common json file (see constructor).
  */
 public class MetadataPatchSuite {
+    static String PROVENANCE = "dc.description.provenance";
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final JsonNode suite;
 
@@ -34,6 +38,16 @@ public class MetadataPatchSuite {
      */
     public MetadataPatchSuite() throws Exception {
         suite = objectMapper.readTree(getClass().getResourceAsStream("metadata-patch-suite.json"));
+    }
+
+    /**
+     * Initializes the suite by parsing the json file of tests.
+     *
+     * @param name  name of resource
+     * @throws Exception if there is an error reading the file.
+     */
+    public MetadataPatchSuite(String name) throws Exception {
+        suite = objectMapper.readTree(getClass().getResourceAsStream(name));
     }
 
     /**
@@ -78,13 +92,32 @@ public class MetadataPatchSuite {
                 .contentType(MediaType.APPLICATION_JSON_PATCH_JSON))
                 .andExpect(status().is(expectedStatus));
         if (expectedStatus >= 200 && expectedStatus < 300) {
-          String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-          JsonNode responseJson =  objectMapper.readTree(responseBody);
-          String responseMetadata = responseJson.get("metadata").toString();
-          if (!responseMetadata.equals(expectedMetadata)) {
-              Assert.fail("Expected metadata in " + verb + " response: " + expectedMetadata
-                      + "\nGot metadata in " + verb + " response: " + responseMetadata);
-          }
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+            JsonNode responseJson =  objectMapper.readTree(responseBody);
+            JsonNode responseMetadataJson = responseJson.get("metadata");
+            if (responseMetadataJson.get(PROVENANCE) != null) {
+                // In the provenance metadata, there is a timestamp indicating when they were added.
+                // To ensure accurate comparison, remove that date.
+                String rspProvenance = responseMetadataJson.get(PROVENANCE).toString();
+                // Regex to match the date pattern
+                String datePattern = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z";
+                Pattern pattern = Pattern.compile(datePattern);
+                Matcher matcher = pattern.matcher(rspProvenance);
+                String rspModifiedProvenance = rspProvenance;
+                while (matcher.find()) {
+                    String dateString = matcher.group(0);
+                    rspModifiedProvenance = rspModifiedProvenance.replaceAll(dateString, "");
+                }
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNodePrv = objectMapper.readTree(rspModifiedProvenance);
+                // Replace the origin metadata with a value with the timestamp removed
+                ((ObjectNode) responseJson.get("metadata")).put(PROVENANCE, jsonNodePrv);
+            }
+            String responseMetadata = responseJson.get("metadata").toString();
+            if (!responseMetadata.equals(expectedMetadata)) {
+                Assert.fail("Expected metadata in " + verb + " response: " + expectedMetadata
+                        + "\nGot metadata in " + verb + " response: " + responseMetadata);
+            }
         }
     }
 }

DSpace/DSpace

dspace-server-webapp/src/test/java/org/dspace/app/rest/test/MetadataPatchSuite.java
@@ -7,13 +7,18 @@
  */
 package org.dspace.app.rest.test;
 
+import static org.apache.jena.vocabulary.DCTerms.MediaType;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import jakarta.ws.rs.core.MediaType;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.ws.rs.core.MediaType;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.Assert;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -23,6 +28,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
  * Utility class for performing metadata patch tests sourced from a common json file (see constructor).
  */
 public class MetadataPatchSuite {
+    static String PROVENANCE = "dc.description.provenance";
     private final ObjectMapper mapper;
 
     private final JsonNode suite;
@@ -80,13 +86,32 @@ public class MetadataPatchSuite {
                 .contentType(MediaType.APPLICATION_JSON_PATCH_JSON))
                 .andExpect(status().is(expectedStatus));
         if (expectedStatus >= 200 && expectedStatus < 300) {
-          String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-          JsonNode responseJson =  mapper.readTree(responseBody);
-          String responseMetadata = responseJson.get("metadata").toString();
-          if (!responseMetadata.equals(expectedMetadata)) {
-              Assert.fail("Expected metadata in " + verb + " response: " + expectedMetadata
-                      + "\nGot metadata in " + verb + " response: " + responseMetadata);
-          }
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+            JsonNode responseJson =  mapper.readTree(responseBody);
+            JsonNode responseMetadataJson = responseJson.get("metadata");
+            if (responseMetadataJson.get(PROVENANCE) != null) {
+                // In the provenance metadata, there is a timestamp indicating when they were added.
+                // To ensure accurate comparison, remove that date.
+                String rspProvenance = responseMetadataJson.get(PROVENANCE).toString();
+                // Regex to match the date pattern
+                String datePattern = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z";
+                Pattern pattern = Pattern.compile(datePattern);
+                Matcher matcher = pattern.matcher(rspProvenance);
+                String rspModifiedProvenance = rspProvenance;
+                while (matcher.find()) {
+                    String dateString = matcher.group(0);
+                    rspModifiedProvenance = rspModifiedProvenance.replaceAll(dateString, "");
+                }
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNodePrv = objectMapper.readTree(rspModifiedProvenance);
+                // Replace the origin metadata with a value with the timestamp removed
+                ((ObjectNode) responseJson.get("metadata")).put(PROVENANCE, jsonNodePrv);
+            }
+            String responseMetadata = responseJson.get("metadata").toString();
+            if (!responseMetadata.equals(expectedMetadata)) {
+                Assert.fail("Expected metadata in " + verb + " response: " + expectedMetadata
+                        + "\nGot metadata in " + verb + " response: " + responseMetadata);
+            }
         }
     }
 }