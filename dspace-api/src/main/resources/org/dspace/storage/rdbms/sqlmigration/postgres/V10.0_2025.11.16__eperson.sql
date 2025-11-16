a23b0560e4f5583a478a40c6a1269e15ac9ad753
VivianKailany <viviankailanymarques@gmail.com>
2025-11-16 16:56:09 -0300

    Eperson config.

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V10.0_2025.11.16__eperson.sql
+++
@@ -1,5 +0,0 @@
-UPDATE eperson
-SET password = '11a8c9399e41d1dd4165b7463347fd8cb58e058ef7cb8ce5ae36505903f8bfc7c5143ae03b2cce910fb1c3d1ca426d119633c726ed166e56b5954d40424ec503',
-    salt = '819cacb57085485fea1f82256e62fb04',
-    digest_algorithm = 'SHA-512'
-WHERE email = 'curador@ltap.ifce.edu.br';
