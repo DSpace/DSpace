5aecee7a7f613a0647e181751e459abd2fa861e1
JohnnyMendesC <177888064+JohnnyMendesC@users.noreply.github.com>
2025-11-19 10:55:16 -0300

    Fixed checkstyle errors
    (cherry picked from commit deccd07d7d2821dc593e43cc0d2697d263a1905c)

--- dspace-api/src/test/java/org/dspace/authenticate/ShibAuthenticationTest.java
+++ dspace-api/src/test/java/org/dspace/authenticate/ShibAuthenticationTest.java
@@ -8,25 +8,22 @@
 package org.dspace.authenticate;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Collections;
 import jakarta.servlet.http.HttpServletRequest;
 import org.dspace.AbstractUnitTest;
-import org.dspace.content.service.MetadataFieldService;
 import org.dspace.content.MetadataField;
-import org.dspace.content.MetadataSchema;
+import org.dspace.content.service.MetadataFieldService;
 import org.dspace.core.Context;
 import org.dspace.eperson.EPerson;
 import org.dspace.eperson.service.EPersonService;
@@ -55,7 +52,8 @@ public class ShibAuthenticationTest extends AbstractUnitTest {
         when(configurationService.getProperty("authentication-shibboleth.email-header")).thenReturn("SHIB-MAIL");
         when(configurationService.getArrayProperty("authentication-shibboleth.eperson.metadata"))
             .thenReturn(new String[]{"SHIB-telephone => eperson.phone"});
-        when(configurationService.getBooleanProperty("authentication-shibboleth.eperson.metadata.autocreate", true)).thenReturn(true);
+        when(configurationService.getBooleanProperty("authentication-shibboleth.eperson.metadata.autocreate", true))
+            .thenReturn(true);
         MetadataFieldService metadataFieldService = mock(MetadataFieldService.class);
         shibAuthentication.metadataFieldService = metadataFieldService;
 
@@ -78,7 +76,8 @@ public class ShibAuthenticationTest extends AbstractUnitTest {
         when(request.getAttribute("SHIB-telephone")).thenReturn(phoneValue);
         shibAuthentication.initialize(context);
         assertNotNull("metadataHeaderMap should be initialized", shibAuthentication.metadataHeaderMap);
-        assertTrue("metadataHeaderMap should contain SHIB-telephone", shibAuthentication.metadataHeaderMap.containsKey("SHIB-telephone"));
+        assertTrue("metadataHeaderMap should contain SHIB-telephone", shibAuthentication.metadataHeaderMap
+            .containsKey("SHIB-telephone"));
         shibAuthentication.updateEPerson(context, request, eperson);
         ArgumentCaptor<String> languageCaptor = ArgumentCaptor.forClass(String.class);
         ArgumentCaptor<String> valueCaptor = ArgumentCaptor.forClass(String.class);
@@ -92,7 +91,7 @@ public class ShibAuthenticationTest extends AbstractUnitTest {
             languageCaptor.capture(),
             valueCaptor.capture()
         );
-                
+
         assertNull("The language argument should be NULL.", languageCaptor.getValue());
         assertEquals("The value argument should be the phone number.", phoneValue, valueCaptor.getValue());
     }
@@ -108,8 +107,10 @@ public class ShibAuthenticationTest extends AbstractUnitTest {
         shibAuthentication.initialize(context);
 
         assertNotNull("metadataHeaderMap should be initialized", shibAuthentication.metadataHeaderMap);
-        assertTrue("metadataHeaderMap should contain SHIB-telephone", shibAuthentication.metadataHeaderMap.containsKey("SHIB-telephone"));
-        assertTrue("metadataHeaderMap should contain SHIB-dept", shibAuthentication.metadataHeaderMap.containsKey("SHIB-dept"));
+        assertTrue("metadataHeaderMap should contain SHIB-telephone", shibAuthentication.metadataHeaderMap
+            .containsKey("SHIB-telephone"));
+        assertTrue("metadataHeaderMap should contain SHIB-dept", shibAuthentication.metadataHeaderMap
+            .containsKey("SHIB-dept"));
     }
 
     @Test
