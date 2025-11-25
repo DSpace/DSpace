3a58ce78e58a3e217a7cacdac25f80f2dcee4a49
Quasipoint <224759579+quasipoint@users.noreply.github.com>
2025-11-25 14:43:31 +0100

    DOIIdentifierProvider: javax -> jakarta

--- dspace-api/src/main/java/org/dspace/identifier/DOIIdentifierProvider.java
+++ dspace-api/src/main/java/org/dspace/identifier/DOIIdentifierProvider.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
