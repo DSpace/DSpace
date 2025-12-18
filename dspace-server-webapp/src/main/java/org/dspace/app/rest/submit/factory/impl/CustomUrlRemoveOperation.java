54fafd96a6a774edce510732645966b0226e78a7
Adamo <adamo.fapohunda@4science.com>
2025-12-18 13:31:04 +0100

    [DURACOM-412] Updated CustomUrlRemoveOperation to extend RemovePatchOperation instead of ReplacePatchOperation

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/submit/factory/impl/CustomUrlRemoveOperation.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/submit/factory/impl/CustomUrlRemoveOperation.java
@@ -21,14 +21,14 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author Luca Giamminonni (luca.giamminonni at 4science.it)
  *
  */
-public class CustomUrlRemoveOperation extends ReplacePatchOperation<CustomUrl> {
+public class CustomUrlRemoveOperation extends RemovePatchOperation<CustomUrl> {
 
     @Autowired
     private CustomUrlService customUrlService;
 
     @Override
     @SuppressWarnings("rawtypes")
-    void replace(Context context, HttpServletRequest currentRequest, InProgressSubmission source, String path,
+    void remove(Context context, HttpServletRequest currentRequest, InProgressSubmission source, String path,
                  Object value) throws Exception {
 
         Item item = source.getItem();
