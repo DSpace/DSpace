954373b19c94532537be78b5c587688d646be42f
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2026-02-02 12:38:53 +0100

    [DURACOM-111] fix compilation errors

--- dspace-iiif/src/main/java/org/dspace/app/iiif/service/CanvasService.java
+++ dspace-iiif/src/main/java/org/dspace/app/iiif/service/CanvasService.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Strings;
 import org.apache.logging.log4j.Logger;
 import org.dspace.app.iiif.model.generator.CanvasGenerator;
