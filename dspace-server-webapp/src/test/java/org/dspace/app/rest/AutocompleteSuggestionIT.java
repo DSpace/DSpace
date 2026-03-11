9228e0c99aa8783ad183ed32626114eb7211b04e
Kim Shepherd <kim@shepherd.nz>
2026-03-11 08:36:45 +0100

    Tests for metadata privacy, item privacy, flat file lookup

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/AutocompleteSuggestionIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/AutocompleteSuggestionIT.java
@@ -20,24 +20,98 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.dspace.app.rest.matcher.SubmissionFormFieldMatcher;
 import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
+import org.dspace.app.util.service.MetadataExposureService;
 import org.dspace.builder.CollectionBuilder;
 import org.dspace.builder.CommunityBuilder;
 import org.dspace.builder.ItemBuilder;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
 import org.dspace.content.Item;
+import org.dspace.content.service.ItemService;
+import org.dspace.discovery.IndexingService;
 import org.dspace.discovery.SolrSearchCore;
+import org.dspace.discovery.SolrSuggestService;
+import org.dspace.eperson.Group;
+import org.dspace.eperson.service.GroupService;
+import org.dspace.services.ConfigurationService;
 import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * Tests for suggestion dictionaries
+ * Note: Dictionaries are "snapshots" and won't apply incremenetal updates
+ * if data is deleted / removed from the main search document of a given object.
+ * Build must be run by an administrator with/without other reindex commands or a GET
+ * to /discover/suggest/build
+ * In this test, we call rebuildAllDictionaries directly to simulate that behaviour
+ * so we can at least test that the subsequent dictionary will not contain values
+ * we expect to be missing, after a change to data or configuration.
+ */
 public class AutocompleteSuggestionIT extends AbstractControllerIntegrationTest {
     // Items with subject metadata, commit, test REST method for expected results
     // Test test submission for expected vocabulary type
     @Autowired
     private SolrSearchCore solrSearchCore;
+    @Autowired
+    private ConfigurationService configurationService;
+    @Autowired
+    private GroupService groupService;
+    @Autowired
+    private ItemService itemService;
+    @Autowired
+    private IndexingService indexingService;
+    @Autowired
+    private SolrSuggestService solrSuggestService;
+    @Autowired
+    private MetadataExposureService metadataExposureService;
 
     Logger log = LogManager.getLogger(this.getClass());
+
+    Group adminGroup;
+    String handle = "autocomplete/test";
+
+    /**
+     * Reset configuration back to its pre-test state
+     */
+    private void resetConfiguration() {
+        configurationService.reloadConfig();
+    }
+
+    /*
+     * Reindex item
+     * Because of the way the mock solr search core works
+     * we have to commit "extra hard" here...
+     */
+    private void reindexItem(Item item) throws Exception {
+        indexingService.updateIndex(context, true);
+        indexingService.commit();
+        solrSearchCore.getSolr().commit(true, true);
+    }
+
+    @BeforeClass
+    public static void beforeAll() throws Exception {
+
+    }
+
+    @Before
+    public void beforeEach() throws Exception {
+        configurationService.setProperty("discovery.suggest.field",
+                new String[]{"dc.subject", "dc.contributor.author"});
+        configurationService.setProperty("discovery.suggest.allowed-dictionaries",
+                new String[]{"subject", "authors", "countries_file"});
+        adminGroup = groupService.findByName(context, Group.ADMIN);
+        solrSuggestService.rebuildAllDictionaries();
+    }
+
+    @After
+    public void afterEach() {
+        resetConfiguration();
+        metadataExposureService.reload();
+    }
 
     @Test
     public void findFieldWithValuePairsConfig() throws Exception {
@@ -62,7 +136,7 @@ public class AutocompleteSuggestionIT extends AbstractControllerIntegrationTest 
     }
 
     @Test
-    public void findSubjectTermSuggestions() throws Exception {
+    public void findTermSuggestionsFromMetadata() throws Exception {
         context.turnOffAuthorisationSystem();
 
         parentCommunity = CommunityBuilder.createCommunity(context)
@@ -73,7 +147,7 @@ public class AutocompleteSuggestionIT extends AbstractControllerIntegrationTest 
                 .build();
         Collection col1 = CollectionBuilder.createCollection(context, child1).withName("Collection 1").build();
 
-        Item publicItem1 = ItemBuilder.createItem(context, col1)
+        Item item1 = ItemBuilder.createItem(context, col1)
                 .withTitle("Public item 1")
                 .withIssueDate("2017-10-17")
                 .withAuthor("Smith, Donald")
@@ -108,6 +182,238 @@ public class AutocompleteSuggestionIT extends AbstractControllerIntegrationTest 
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(jsonPath("$.suggest.subject.test.numFound", is(0)));
+
+        context.ignoreAuthorization();
+        solrSuggestService.rebuildAllDictionaries();
+        context.restoreAuthSystemState();
+    }
+
+    @Test
+    public void findTermSuggestionsFromFlatFile() throws Exception {
+        String token = getAuthToken(eperson.getEmail(), password);
+        getClient(token).perform(get("/api/discover/suggest")
+                        .queryParam("dict", "countries_file")
+                        .queryParam("q", "madagascar")
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.suggest.countries_file.madagascar.numFound", is(1)));
+    }
+
+    @Test
+    public void dontFindHiddenMetadatTermSuggestions() throws Exception {
+        context.turnOffAuthorisationSystem();
+        configurationService.addPropertyValue("metadata.hide.dc.subject", true);
+        metadataExposureService.reload();
+
+        parentCommunity = CommunityBuilder.createCommunity(context)
+                .withName("Parent Community")
+                .build();
+        Community child1 = CommunityBuilder.createSubCommunity(context, parentCommunity)
+                .withName("Sub Community")
+                .build();
+        Collection col1 = CollectionBuilder.createCollection(context, child1).withName("Collection 1").build();
+
+        Item item1 = ItemBuilder.createItem(context, col1)
+                .withTitle("Public item 1")
+                .withIssueDate("2017-10-17")
+                .withAuthor("Smith, Donald")
+                .withAuthor("Doe, John")
+                .withSubject("Totora Grove")
+                .withSubject("Punga Grove")
+                .withSubject("Kowhai Flower")
+                .withSubject("Flower Arranging")
+                .build();
+        reindexItem(item1);
+        solrSuggestService.rebuildAllDictionaries();
+        context.restoreAuthSystemState();
+
+        String token = getAuthToken(eperson.getEmail(), password);
+
+        // Expect ZERO terms for "grove" - this field is hidden
+        getClient(token).perform(get("/api/discover/suggest")
+                        .queryParam("dict", "subject")
+                        .queryParam("q", "grove")
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.suggest.subject.grove.numFound", is(0)));
+
+        resetConfiguration();
+    }
+
+    @Test
+    public void testSuggestTermsFromRestrictedItem() throws Exception {
+        context.turnOffAuthorisationSystem();
+
+        parentCommunity = CommunityBuilder.createCommunity(context)
+                .withName("Parent Community")
+                .build();
+        Community child1 = CommunityBuilder.createSubCommunity(context, parentCommunity)
+                .withName("Sub Community")
+                .build();
+        Collection col1 = CollectionBuilder.createCollection(context, child1).withName("Collection 1").build();
+
+        Item restrictedItem = ItemBuilder.createItem(context, col1)
+                .withTitle("Restricted item 1")
+                .withIssueDate("2017-10-17")
+                .withAuthor("Smith, Donald")
+                .withAuthor("Doe, John")
+                .withSubject("Totara Grove")
+                .withSubject("Punga Grove")
+                .withReaderGroup(adminGroup)
+                .build();
+
+        context.restoreAuthSystemState();
+
+        reindexItem(restrictedItem);
+        solrSuggestService.rebuildAllDictionaries();
+
+        String token = getAuthToken(eperson.getEmail(), password);
+
+        // Expect ZERO terms - this item is not publicly readable
+        getClient(token).perform(get("/api/discover/suggest")
+                        .queryParam("dict", "subject")
+                        .queryParam("q", "grove")
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.suggest.subject.grove.numFound", is(0)));
+    }
+
+    @Test
+    public void testSuggestTermsFromWithdrawnItem() throws Exception {
+        context.turnOffAuthorisationSystem();
+        parentCommunity = CommunityBuilder.createCommunity(context)
+                .withName("Parent Community")
+                .build();
+        Community child1 = CommunityBuilder.createSubCommunity(context, parentCommunity)
+                .withName("Sub Community")
+                .build();
+        Collection col1 = CollectionBuilder.createCollection(context, child1).withName("Collection 1").build();
+
+        Item item1 = ItemBuilder.createItem(context, col1)
+                .withTitle("Public item 1")
+                .withIssueDate("2017-10-17")
+                .withAuthor("Smith, Donald")
+                .withAuthor("Doe, John")
+                .withSubject("Totora Grove")
+                .withSubject("Kowhai Grove")
+                .withSubject("Punga Grove")
+                .withSubject("Kowhai Flower")
+                .withSubject("Flower Arranging")
+                .withdrawn()
+                .build();
+        itemService.withdraw(context, item1);
+        reindexItem(item1);
+        solrSuggestService.rebuildAllDictionaries();
+        context.restoreAuthSystemState();
+
+        String token = getAuthToken(eperson.getEmail(), password);
+
+        // Expect ZERO terms for "grove" - this item is withdrawn
+        getClient(token).perform(get("/api/discover/suggest")
+                        .queryParam("dict", "subject")
+                        .queryParam("q", "grove")
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.suggest.subject.grove.numFound", is(0)));
+
+    }
+
+    @Test
+    public void testSuggestTermsFromNonDiscoverableItem() throws Exception {
+        context.turnOffAuthorisationSystem();
+
+        parentCommunity = CommunityBuilder.createCommunity(context)
+                .withName("Parent Community")
+                .build();
+        Community child1 = CommunityBuilder.createSubCommunity(context, parentCommunity)
+                .withName("Sub Community")
+                .build();
+        Collection col1 = CollectionBuilder.createCollection(context, child1).withName("Collection 1").build();
+
+        Item item1 = ItemBuilder.createItem(context, col1)
+                .withTitle("Public item 1")
+                .withIssueDate("2017-10-17")
+                .withAuthor("Smith, Donald")
+                .withAuthor("Doe, John")
+                .withSubject("Totora Grove")
+                .withSubject("Punga Grove")
+                .withSubject("Kowhai Flower")
+                .withSubject("Flower Arranging")
+                .build();
+        item1.setDiscoverable(false);
+        itemService.update(context, item1);
+        reindexItem(item1);
+        context.restoreAuthSystemState();
+
+        String token = getAuthToken(eperson.getEmail(), password);
+
+        // Expect ZERO terms for "grove" - this item is set as undiscoverable
+        getClient(token).perform(get("/api/discover/suggest")
+                        .queryParam("dict", "subject")
+                        .queryParam("q", "grove")
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.suggest.subject.grove.numFound", is(0)));
+    }
+
+    @Test
+    public void testSuggestTermsFromNonArchivedItem() throws Exception {
+        context.turnOffAuthorisationSystem();
+
+        parentCommunity = CommunityBuilder.createCommunity(context)
+                .withName("Parent Community")
+                .build();
+        Community child1 = CommunityBuilder.createSubCommunity(context, parentCommunity)
+                .withName("Sub Community")
+                .build();
+        Collection col1 = CollectionBuilder.createCollection(context, child1).withName("Collection 1").build();
+
+        Item item1 = ItemBuilder.createItem(context, col1)
+                .withTitle("Public item 1")
+                .withIssueDate("2017-10-17")
+                .withAuthor("Smith, Donald")
+                .withAuthor("Doe, John")
+                .withSubject("Totora Grove")
+                .withSubject("Punga Grove")
+                .withSubject("Kowhai Flower")
+                .withSubject("Flower Arranging")
+                .build();
+        item1.setArchived(false);
+        itemService.update(context, item1);
+        reindexItem(item1);
+        solrSuggestService.rebuildAllDictionaries();
+        context.restoreAuthSystemState();
+
+        String token = getAuthToken(eperson.getEmail(), password);
+
+        // Expect ZERO terms for "grove" - this item is set as 'not archived'
+        // (wrapped by in-progress item, withdrawn, etc)
+        getClient(token).perform(get("/api/discover/suggest")
+                        .queryParam("dict", "subject")
+                        .queryParam("q", "grove")
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.suggest.subject.grove.numFound", is(0)));
+    }
+
+    /**
+     * Test that a non-admin request to /build (rebuild dictionaries)
+     * fails with 401, with or without a parameter
+     */
+    @Test
+    public void nonAdminDictionaryBuildShouldFail() throws Exception {
+        getClient().perform(
+                get("/api/discover/suggest/build")
+        ).andExpect(status().isUnauthorized());
+        getClient().perform(
+                get("/api/discover/suggest/build").param("dict", "subject")
+        ).andExpect(status().isUnauthorized());
     }
 
     /**
@@ -116,9 +422,9 @@ public class AutocompleteSuggestionIT extends AbstractControllerIntegrationTest 
     @Test
     public void unauthenticatedRequestShouldReturn401() throws Exception {
         getClient().perform(
-                get("/api/discover/suggest")
-                        .param("dict", "subject")
-                        .param("q", "test"))
+                        get("/api/discover/suggest")
+                                .param("dict", "subject")
+                                .param("q", "test"))
                 .andExpect(status().isUnauthorized());
     }
 
@@ -130,9 +436,9 @@ public class AutocompleteSuggestionIT extends AbstractControllerIntegrationTest 
         String userToken = getAuthToken(eperson.getEmail(), password);
 
         getClient(userToken).perform(
-                get("/api/discover/suggest")
-                        .param("dict", "not_in_allowlist")
-                        .param("q", "test"))
+                        get("/api/discover/suggest")
+                                .param("dict", "not_in_allowlist")
+                                .param("q", "test"))
                 .andExpect(status().isForbidden());
     }
 
@@ -145,14 +451,14 @@ public class AutocompleteSuggestionIT extends AbstractControllerIntegrationTest 
 
         // Missing 'q' parameter
         getClient(userToken).perform(
-                get("/api/discover/suggest")
-                        .param("dict", "subject"))
+                        get("/api/discover/suggest")
+                                .param("dict", "subject"))
                 .andExpect(status().isBadRequest());
 
         // Missing 'dict' parameter
         getClient(userToken).perform(
-                get("/api/discover/suggest")
-                        .param("q", "test"))
+                        get("/api/discover/suggest")
+                                .param("q", "test"))
                 .andExpect(status().isBadRequest());
     }
 }
