a6d67afeef5a6bedf36856822f4e89ca4e7d4c9c
AdamF42 <adamo.fapohunda@4science.com>
2026-03-13 16:30:24 +0100

    [DURACOM-427] Update to remove unused variables

--- dspace-api/src/main/java/org/dspace/core/CrisConstants.java
+++ dspace-api/src/main/java/org/dspace/core/CrisConstants.java
@@ -7,7 +7,6 @@
  */
 package org.dspace.core;
 
-import org.dspace.content.MetadataFieldName;
 
 /**
  * Class with constants specific to CRIS features in DSpace
@@ -21,11 +20,6 @@ public class CrisConstants {
      * same number as the parent leading metadata
      */
     public static final String PLACEHOLDER_PARENT_METADATA_VALUE = "#PLACEHOLDER_PARENT_METADATA_VALUE#";
-    public static final MetadataFieldName MD_ENTITY_TYPE = new MetadataFieldName("dspace", "entity", "type");
-    public static final MetadataFieldName MD_SUBMISSION_TYPE = new MetadataFieldName("dspace", "submission",
-                                                                                     "definition");
-    public static final MetadataFieldName MD_WORKFLOW_NAME = new MetadataFieldName("dspace", "workflow", "name");
-    public static final MetadataFieldName MD_SHARED_WORKSPACE = new MetadataFieldName("dspace", "workspace", "shared");
 
     /**
      * Make the constructor private as it is an utility class
