a2a4efdd9457a9b40b4a824f673beebec84d72c3
Bram Maegerman <bram.maegerman@telenet.be>
2026-04-03 10:27:56 +0200

    140245: added tracking wrapper class for input streams

---
+++ dspace-api/src/main/java/org/dspace/storage/bitstore/LeakTrackingInputStream.java
@@ -0,0 +1,72 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.storage.bitstore;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.Cleaner;
+import java.time.Instant;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Wrapper Class for InputStream
+ * Keeps track of open InputStreams and adds information to be able to track down the origins of the InputStream
+ *
+ * @author Kevin Van de Velde (kevin at atmire dot com)
+ */
+public final class LeakTrackingInputStream extends FilterInputStream {
+
+    private static final Cleaner CLEANER = Cleaner.create();
+    private static final ConcurrentMap<UUID, BitstreamInputstreamOpenInfo> OPEN = new ConcurrentHashMap<>();
+
+    private final UUID id = UUID.randomUUID();
+    private final Cleaner.Cleanable cleanable;
+    private volatile boolean closed = false;
+
+    private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(LeakTrackingInputStream.class);
+
+    /**
+     * Constructor of LeakTrackingInputStream
+     * Wraps an InputStream with information to keep track of open InputStreams
+     *
+     * @param in    the InputStream to be wrapped
+     * @param kind  where the InputStream was opened or retrieved
+     */
+    public LeakTrackingInputStream(InputStream in, String kind) {
+        super(in);
+        BitstreamInputstreamOpenInfo info =
+                new BitstreamInputstreamOpenInfo(id, kind, Instant.now(), new Exception("Opened here"));
+        OPEN.put(id, info);
+        this.cleanable = CLEANER.register(this, () -> {
+            BitstreamInputstreamOpenInfo leaked = OPEN.remove(id);
+            if (leaked != null) {
+                log.error("LEAKED {} stream {}", leaked.kind(), leaked.id(), leaked.openedAtThrowable());
+            }
+        });
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (!closed) {
+            closed = true;
+            OPEN.remove(id);
+            cleanable.clean();
+        }
+        super.close();
+    }
+
+    public static Map<UUID, BitstreamInputstreamOpenInfo> snapshotOpenStreams() {
+        return Map.copyOf(OPEN);
+    }
+}
