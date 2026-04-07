0bf022e0cfbf6bf339fddd7bcd012e79c624ee0e
Jens Vannerum <jens.vannerum@atmire.com>
2026-04-07 16:27:47 +0200

    140361: add flyaway migration for already corrupted databases

---
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V10.0_2026.04.09__fix_bundle2bitstream_order_issues.sql
@@ -0,0 +1,48 @@
+--
+-- The contents of this file are subject to the license and copyright
+-- detailed in the LICENSE and NOTICE files at the root of the source
+-- tree and available online at
+--
+-- http://www.dspace.org/license/
+--
+--------------------------------------------------------
+-- repair script for corrupted bundle2bitstream tables.
+--------------------------------------------------------
+
+UPDATE bitstream
+SET deleted = false
+WHERE uuid IN (
+    SELECT b.uuid
+    FROM bitstream b
+             INNER JOIN bundle2bitstream b2b ON b.uuid = b2b.bitstream_id
+             INNER JOIN item2bundle i2b      ON b2b.bundle_id = i2b.bundle_id
+    WHERE b.deleted = true
+);
+
+DELETE FROM bundle2bitstream a
+    USING (
+    SELECT MAX(ctid) AS ctid, bundle_id, bitstream_id
+    FROM bundle2bitstream
+    GROUP BY bundle_id, bitstream_id
+    HAVING COUNT(*) > 1
+) b
+WHERE a.bundle_id    = b.bundle_id
+  AND a.bitstream_id = b.bitstream_id
+  AND a.ctid        <> b.ctid;
+
+ALTER TABLE bundle2bitstream ADD COLUMN bitstream_order_fix INT;
+
+WITH numbered AS (
+    SELECT bundle_id,
+           bitstream_id,
+           ROW_NUMBER() OVER (PARTITION BY bundle_id ORDER BY bitstream_order) - 1 AS rn
+    FROM bundle2bitstream
+)
+UPDATE bundle2bitstream
+SET bitstream_order_fix = numbered.rn
+    FROM numbered
+WHERE bundle2bitstream.bundle_id    = numbered.bundle_id
+  AND bundle2bitstream.bitstream_id = numbered.bitstream_id;
+
+ALTER TABLE bundle2bitstream DROP COLUMN bitstream_order;
+ALTER TABLE bundle2bitstream RENAME COLUMN bitstream_order_fix TO bitstream_order;
