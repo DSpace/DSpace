2f9b772bcfdd13d8b429deddae5645868a1237f7
Kim Shepherd <kim@shepherd.nz>
2026-04-28 18:50:23 +0200

    Remove unused imports

--- dspace-api/src/main/java/org/dspace/curate/AbstractCurationTask.java
+++ dspace-api/src/main/java/org/dspace/curate/AbstractCurationTask.java
@@ -15,8 +15,6 @@ import java.util.List;
 import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.dspace.app.util.factory.UtilServiceFactory;
 import org.dspace.app.util.service.DSpaceObjectUtils;
 import org.dspace.content.Collection;
