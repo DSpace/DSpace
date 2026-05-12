c410bf2dcd5dae27def6168874b4521c32e72e9c
AdamF42 <adamo.fapohunda@4science.com>
2026-05-12 15:09:33 +0200

    [DURACOM-481] Updated OpenAireProjectImportMetadataSourceServiceImpl to use DSpaceHttpClientFactory

--- dspace-api/src/main/java/org/dspace/importer/external/openaire/service/OpenAireProjectImportMetadataSourceServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/importer/external/openaire/service/OpenAireProjectImportMetadataSourceServiceImpl.java
@@ -15,24 +15,18 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import org.apache.commons.io.Charsets;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.config.RequestConfig.Builder;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.dspace.app.client.DSpaceHttpClientFactory;
 import org.dspace.app.util.XMLUtils;
 import org.dspace.content.Item;
 import org.dspace.importer.external.datamodel.ImportRecord;
@@ -40,12 +34,10 @@ import org.dspace.importer.external.datamodel.Query;
 import org.dspace.importer.external.exception.MetadataSourceException;
 import org.dspace.importer.external.service.AbstractImportMetadataSourceService;
 import org.dspace.importer.external.service.components.QuerySource;
-import org.dspace.services.ConfigurationService;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Implements a data source for querying OpenAIRE
@@ -59,11 +51,6 @@ public class OpenAireProjectImportMetadataSourceServiceImpl extends AbstractImpo
     private static final String ENDPOINT_SEARCH_OPENAIRE = "http://api.openaire.eu/search/projects";
 
     private int timeout = 1000;
-
-    private Supplier<HttpClientBuilder> httpClientBuilderSupplier = HttpClients::custom;
-
-    @Autowired
-    private ConfigurationService configurationService;
 
     /**
      * Initialize the class
@@ -159,42 +146,28 @@ public class OpenAireProjectImportMetadataSourceServiceImpl extends AbstractImpo
         public List<ImportRecord> call() throws Exception {
             List<ImportRecord> results = new ArrayList<ImportRecord>();
             String queryString = query.getParameterAsClass("query", String.class);
-            String proxyHost = configurationService.getProperty("http.proxy.host");
-            String proxyPort = configurationService.getProperty("http.proxy.port");
-            HttpGet method = null;
             try {
-                HttpClientBuilder hcBuilder = httpClientBuilderSupplier.get();
-                Builder requestConfigBuilder = RequestConfig.custom();
-                requestConfigBuilder.setConnectionRequestTimeout(timeout);
-
-                if (StringUtils.isNotBlank(proxyHost) && StringUtils.isNotBlank(proxyPort)) {
-                    HttpHost proxy = new HttpHost(proxyHost, Integer.parseInt(proxyPort), "http");
-                    DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
-                    hcBuilder.setRoutePlanner(routePlanner);
-                }
-
-                HttpClient client = hcBuilder.build();
-                // open session
-                method = new HttpGet(ENDPOINT_SEARCH_OPENAIRE + "?name=" + URLEncoder.encode(queryString));
-                method.setConfig(requestConfigBuilder.build());
-                // Execute the method.
-                HttpResponse httpResponse = client.execute(method);
-                int statusCode = httpResponse.getStatusLine().getStatusCode();
-                if (statusCode != HttpStatus.SC_OK) {
-                    throw new RuntimeException("WS call failed: " + statusCode);
-                }
-                InputStream is = httpResponse.getEntity().getContent();
-                String response = IOUtils.toString(is, Charsets.UTF_8);
-                List<Element> omElements = splitToRecords(response);
-                for (Element record : omElements) {
-                    results.add(transformSourceRecords(record));
+                RequestConfig config = RequestConfig.custom()
+                    .setConnectionRequestTimeout(timeout)
+                    .build();
+                try (CloseableHttpClient client =
+                         DSpaceHttpClientFactory.getInstance().buildWithRequestConfig(config);
+                     CloseableHttpResponse httpResponse =
+                         client.execute(new HttpGet(
+                             ENDPOINT_SEARCH_OPENAIRE + "?name=" + URLEncoder.encode(queryString)))) {
+                    int statusCode = httpResponse.getStatusLine().getStatusCode();
+                    if (statusCode != HttpStatus.SC_OK) {
+                        throw new RuntimeException("WS call failed: " + statusCode);
+                    }
+                    InputStream is = httpResponse.getEntity().getContent();
+                    String response = IOUtils.toString(is, Charsets.UTF_8);
+                    List<Element> omElements = splitToRecords(response);
+                    for (Element record : omElements) {
+                        results.add(transformSourceRecords(record));
+                    }
                 }
             } catch (Exception e1) {
                 log.error(e1.getMessage(), e1);
-            } finally {
-                if (method != null) {
-                    method.releaseConnection();
-                }
             }
             return results;
         }
@@ -219,53 +192,31 @@ public class OpenAireProjectImportMetadataSourceServiceImpl extends AbstractImpo
         public List<ImportRecord> call() throws Exception {
             List<ImportRecord> results = new ArrayList<ImportRecord>();
             String id = query.getParameterAsClass("id", String.class);
-            String proxyHost = configurationService.getProperty("http.proxy.host");
-            String proxyPort = configurationService.getProperty("http.proxy.port");
-            HttpGet method = null;
             try {
-                HttpClientBuilder hcBuilder = httpClientBuilderSupplier.get();
-                Builder requestConfigBuilder = RequestConfig.custom();
-                requestConfigBuilder.setConnectionRequestTimeout(timeout);
-
-                if (StringUtils.isNotBlank(proxyHost) && StringUtils.isNotBlank(proxyPort)) {
-                    HttpHost proxy = new HttpHost(proxyHost, Integer.parseInt(proxyPort), "http");
-                    DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
-                    hcBuilder.setRoutePlanner(routePlanner);
-                }
-                HttpClient client = hcBuilder.build();
-                // open session
-                method = new HttpGet(ENDPOINT_SEARCH_OPENAIRE + "?grantID=" + URLEncoder.encode(id));
-                method.setConfig(requestConfigBuilder.build());
-                // Execute the method.
-                HttpResponse httpResponse = client.execute(method);
-                int statusCode = httpResponse.getStatusLine().getStatusCode();
-                if (statusCode != HttpStatus.SC_OK) {
-                    throw new RuntimeException("WS call failed: " + statusCode);
-                }
-                InputStream is = httpResponse.getEntity().getContent();
-                String response = IOUtils.toString(is, Charsets.UTF_8);
-                List<Element> omElements = splitToRecords(response);
-                for (Element record : omElements) {
-                    results.add(transformSourceRecords(record));
+                RequestConfig config = RequestConfig.custom()
+                    .setConnectionRequestTimeout(timeout)
+                    .build();
+                try (CloseableHttpClient client =
+                         DSpaceHttpClientFactory.getInstance().buildWithRequestConfig(config);
+                     CloseableHttpResponse httpResponse =
+                         client.execute(new HttpGet(
+                             ENDPOINT_SEARCH_OPENAIRE + "?grantID=" + URLEncoder.encode(id)))) {
+                    int statusCode = httpResponse.getStatusLine().getStatusCode();
+                    if (statusCode != HttpStatus.SC_OK) {
+                        throw new RuntimeException("WS call failed: " + statusCode);
+                    }
+                    InputStream is = httpResponse.getEntity().getContent();
+                    String response = IOUtils.toString(is, Charsets.UTF_8);
+                    List<Element> omElements = splitToRecords(response);
+                    for (Element record : omElements) {
+                        results.add(transformSourceRecords(record));
+                    }
                 }
             } catch (Exception e1) {
                 log.error(e1.getMessage(), e1);
-            } finally {
-                if (method != null) {
-                    method.releaseConnection();
-                }
             }
             return results;
         }
-    }
-
-    /**
-     * Override the default HTTP client builder supplier (useful for testing).
-     *
-     * @param httpClientBuilderSupplier the supplier to use
-     */
-    public void setHttpClientBuilderSupplier(Supplier<HttpClientBuilder> httpClientBuilderSupplier) {
-        this.httpClientBuilderSupplier = httpClientBuilderSupplier;
     }
 
     /**
