4dfe23b0a4205731a341155496db4ddb8340ae23
Alfeu U. Tavares <114295815+autavares-dev@users.noreply.github.com>
2026-07-13 18:27:32 -0300

    Fix MetadataValidatorTest

--- dspace-api/src/test/java/org/dspace/validation/MetadataValidatorTest.java
+++ dspace-api/src/test/java/org/dspace/validation/MetadataValidatorTest.java
@@ -36,7 +36,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner']
+import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MetadataValidatorTest {
